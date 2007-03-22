bbabc8493d67ef9bc64b69828a55f631c404f483
thepler <thepler@genome.wustl.edu>
2007-03-22 20:50:00 +0000

    new and improved smoke testing
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@23473 127c0c0e-971a-0410-ad48-868c1a942d8d

--- inc/Test/TAP/Model/LSF.pm
+++ inc/Test/TAP/Model/LSF.pm
@@ -1,11 +1,12 @@
 package Test::TAP::Model::LSF;
 
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 use Moose;
 use Path::Class ();
-use File::Temp ();
-use YAML ();
+use YAML::Syck;
+use File::chdir '$CWD';
+use English;
 
 extends 'Test::TAP::Model::Visual';
 
@@ -15,16 +16,32 @@ has 'base_dir' => (
     required => 1,
 );
 
-has 'result_iteration' => (
-    is      => 'rw',
-    isa     => 'Int',
-    default => 0,
-);
-
 has 'callback' => (
     is      => 'rw',
     isa     => 'CodeRef',
 );
+
+has 'smoke_dir' => (
+    is       => 'ro',
+    isa      => 'Path::Class::Dir',
+    required => 1,
+    lazy     => 1,
+    default  => sub { Path::Class::dir($CWD)->subdir('smoke_db') },
+);
+
+has 'test_files' => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+    lazy     => 1,
+    default  => sub { [] },
+);
+
+sub add_test_file {
+    my ($self, $file) = @_;
+    my $files = $self->test_files;
+    push @$files, $file;
+}
 
 sub event {
     my $self = shift;
@@ -36,8 +53,7 @@ sub run_tests {
     my ($self, @tests) = @_;
     $self->{_timing}{start} = time;
 
-    $self->result_iteration(0);
-
+    $self->smoke_dir->rmtree;
     for my $test (@tests) {
         $self->dispatch_test($test);
     }
@@ -48,43 +64,26 @@ sub run_tests {
         $self->{_timing}{end} - $self->{_timing}{start};
 };
 
-sub temp_dir {
-    my $self = shift;
-    if ( !$self->{temp_dir} ) {
-        my $d = File::Temp::tempdir(
-            '_smoke_lsf.XXXXX',
-            DIR      => $self->base_dir,
-            CLEANUP  => 1
-        );
-        $self->{temp_dir} = Path::Class::dir($d);
-    }
-    return $self->{temp_dir};
-}
-
-sub remote_cmd {
-    my ($self, $test) = @_;
-    my $cmd = $self->base_dir->file('remote_lsf.pl');
-    my $nrf = $self->next_result_file;
-    return "$^X $cmd $test $nrf";
-#    return "$^X -d:ptkdb $cmd $test $nrf";
-}
-
-sub next_result_file {
-    my $self = shift;
-    my $it = $self->result_iteration;
-    $it++;
-    $self->result_iteration($it);
-    my $dir = $self->temp_dir;
-    return $dir->file("$it.yml");
-}
-
 sub dispatch_test {
-    my ($self, $test) = @_;
-    my $rcmd = $self->remote_cmd($test);
-    my $err = $self->temp_dir->file($self->result_iteration . '.out');
+    my ( $self, $test ) = @_;
+
+    my $smoke_dir   = $self->smoke_dir;
+    my $stdout_file = $smoke_dir->file("$test.stdout");
+    my $stderr_file = $smoke_dir->file("$test.stderr");
+    my $yaml_file   = $smoke_dir->file("$test.yml");
+    my $script      = $self->base_dir->file('run_single_test.pl');
+    my $rcmd        = "$EXECUTABLE_NAME $script $test $yaml_file";
+
+    my $test_file = Test::TAP::Model::LSF::TestFile->new(
+        test_file   => $test,
+        stdout_file => $stdout_file,
+        stderr_file => $stderr_file,
+        yaml_file   => $yaml_file,
+    ) or die;
+    $self->add_test_file($test_file);
+
     local $ENV{PERL5LIB} = $self->_INC2PERL5LIB;
-    my $cmd = "bsub -q short -oo $err $rcmd";
-#    my $cmd = "$rcmd";
+    my $cmd = "bsub -q short -N -o $stdout_file -e $stderr_file $rcmd";
     $self->event("dispatching $test...\n");
     my @out = `$cmd 2>&1`;
     $self->event("@out\n");
@@ -95,57 +94,98 @@ sub dispatch_test {
 
 sub wait_for_results {
     my ($self) = @_;
-    my $dir = $self->temp_dir;
-    my $it = $self->result_iteration;
-    $self->event("waiting for jobs to finish.");
-    while (1) {
-        my $all_done = 1;
-        for my $i ( 1 .. $it ) {
-            my $file = $dir->file("$i.yml");
-            if ( !-f $file ) {
-                $all_done = 0;
+    my %files = map { $_->test_file => $_ } @{ $self->test_files };
+    while ( keys %files ) {
+        my $found;
+        for my $file ( values %files ) {
+            if ( $file->is_finished_running ) {
+                $found = $file;
                 last;
             }
         }
-        if ($all_done) {
-            $self->event("finished\n");
-            return;
+        if ($found) {
+            delete $files{ $found->test_file };
+            # TODO: make this do the harness-y thing instead
+            $self->event($found->test_file . "\n");
         }
-        $self->event(".");
-        sleep 5;
+        else {
+            sleep 3;
+        }
     }
 }
 
 sub gather_results {
     my ($self) = @_;
     $self->wait_for_results;
-    my $dir = $self->temp_dir;
-    my $it = $self->result_iteration;
-    for my $i ( 1 .. $it ) {
-        my $file  = $dir->file("$i.yml");
-        my $chunk = YAML::LoadFile($file)
+    my @files = map { $_->yaml_file } @{ $self->test_files };
+    for my $file (@files) {
+        $self->event("loading $file\n");
+        my $chunk = LoadFile($file)
             or die "can't parse chunk ($file)";
+        $self->event("pushing $file data\n");
         push @{ $self->{meat}{test_files} }, @{ $chunk->{test_files} };
-        unlink $file or die "unlink failed: $!";
     }
-    $dir->rmtree;
 }
 
 sub emit_chunk {
     my ( $self, $result_file ) = @_;
-    YAML::DumpFile( $result_file, $self->structure );
+    DumpFile( $result_file, $self->structure );
 }
 
 sub emit {
     my ( $self, $file ) = @_;
-    YAML::DumpFile(
-        $file,
-        {   meat => $self->structure,
-            map { $_ => $self->{"_$_"} }
-                qw( build_info smoker config revision timing )
-        }
-    );
+    DumpFile( $file, {
+        meat => $self->structure,
+        map { $_ => $self->{"_$_"} }
+            qw( build_info smoker config revision timing )
+    });
+}
+
+# repeat the line to STDOUT
+sub _analyze_line {
+    my $self = shift;
+    print $_[0];
+    return $self->SUPER::_analyze_line(@_);
+}
+
+package Test::TAP::Model::LSF::TestFile;
+
+use strict;
+use warnings FATAL => 'all';
+use Moose;
+use Path::Class ();
+
+has 'test_file' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has 'stdout_file' => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+);
+
+has 'stderr_file' => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+);
+
+has 'yaml_file' => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+);
+
+sub is_finished_running {
+    my $self = shift;
+    return ( -e $self->yaml_file && -e $self->stdout_file );
+
 }
 
 1;
+
+__END__
 
