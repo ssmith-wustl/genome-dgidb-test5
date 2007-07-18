3665b6064e6a3000c624a33573c7c0dda5f68ff8
wschroed <wschroed@genome.wustl.edu>
2007-07-18 23:03:26 +0000

    Temporary return of the legendary $DB::single=1 line in response to the sound of a million voices in a fishbowl crying out in terror...
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@26612 127c0c0e-971a-0410-ad48-868c1a942d8d

--- CSP.pm
+++ CSP.pm
@@ -1371,6 +1371,7 @@ sub confirm_scheduled_pse {
     $rv = eval{
         # call confirm_job
 
+        $DB::single=1; # Debugger command, to put a breakpoint; ignored if debugger isn't used
         $pse->status_message("calling confirm_job on pse_id $pse_id ($process_to)");
         unless($pse->confirm_job(@params)){
             $pse->error_message("Failed to confirm the job: ".$pse->error_message);
