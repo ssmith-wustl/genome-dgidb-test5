551316244ed026018ae48c02ea077a64d3adba15
bshore <bshore@genome.wustl.edu>
2007-08-10 17:52:08 +0000

    Initial versions.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@27215 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/AlignReads/454.pm
@@ -0,0 +1,84 @@
+package Genome::Model::Command::AlignReads::454;
+
+use strict;
+use warnings;
+
+use UR;
+use Command;
+use File::Path;
+
+UR::Object::Class->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+    has => [                                # Specify the command's properties (parameters) <--- 
+        'sample'   => { type => 'String',  doc => "sample name"},
+        'sffdir'   => { type => 'String',  doc => "sff (input) directory--default is sff", is_optional => 1},
+        'outdir'   => { type => 'String',  doc => "alignment (output) directory"},
+        'dir'   => { type => 'String',  doc => "project alignment (output) directory"},
+        'refseq'   => { type => 'String',      doc => "reference sequence file"}
+    ], 
+);
+
+sub help_synopsis {                         # Replace the text below with real examples <---
+    return <<EOS
+genome-model align-reads 454 --outdir=454/ccds/alignment --dir=all --refseq=reference_sequence/CCDS_nucleotide.20070227.fa --sample=H_GW-454_EST_S_8977
+EOS
+}
+
+sub help_brief {
+    "launch the aligner for a given set of new reads"
+}
+
+sub help_detail {                       
+    return <<EOS 
+
+
+EOS
+}
+
+#sub create {                               # Rarely implemented.  Initialize things before execute <---
+#    my $class = shift;
+#    my %params = @_;
+#    my $self = $class->SUPER::create(%params);
+#    # ..do initialization here
+#    return $self;
+#}
+
+#sub validate_params {                      # Pre-execute checking.  Not requiried <---
+#    my $self = shift;
+#    return unless $self->SUPER::validate_params(@_);
+#    # ..do real checks here
+#    return 1;
+#}
+
+sub execute {
+    my $self = shift;
+
+		my($outdir, $dir, $sample, $sffdir, $refseq) = 
+				 ($self->outdir, $self->dir, $self->sample, $self->sffdir, $self->refseq);
+		return unless ( defined($outdir) && defined($dir) &&
+										defined($sample) && defined($refseq)
+									);
+
+		$sffdir ||= 'sff';
+		$outdir =~ s/ \/ $ //x;			# Remove any trailing slash
+		$dir =~ s/ \/ $ //x;				# Remove any trailing slash
+
+		# Make sure the output directory exists
+		unless (-e $outdir) {
+			mkpath $outdir;
+		}
+
+		show_system("runMapping -o $outdir/$dir $refseq $sffdir/$sample.sff");
+
+    return 1;
+}
+
+sub show_system {
+  my ($command) = @_;
+	print STDERR "$command\n";
+	system($command);
+}
+
+1;
+
