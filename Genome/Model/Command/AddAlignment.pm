bedd723f9ac489a84ddabf6c5380d0917b4eb45a
bshore <bshore@genome.wustl.edu>
2007-08-10 17:54:05 +0000

    Initial version.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@27216 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/AddAlignment.pm
@@ -0,0 +1,28 @@
+
+# Rename the final word in the full class name <---
+package Genome::Model::Command::AddAlignment;
+
+use strict;
+use warnings;
+
+use UR;
+use Command;
+
+UR::Object::Class->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+);
+
+sub help_brief {
+    "add an alignment to a genome model"
+}
+
+sub help_detail {                           # This is what the user will see with --help <---
+    return <<EOS 
+
+
+EOS
+}
+
+1;
+
---
+++ Genome/Model/Command/AddAlignment/Polybayes.pm
@@ -0,0 +1,66 @@
+package Genome::Model::Command::AddAlignment::Polybayes;
+
+use strict;
+use warnings;
+
+use UR;
+use Command;
+use File::Path;
+
+UR::Object::Class->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+    has => [                                # Specify the command's properties (parameters) <--- 
+        'sample'   => { type => 'String',  doc => "sample name"},
+        'dir'   => { type => 'String',  doc => "Polybayes alignment (output) directory"},
+        'bindir'   => { type => 'String',  doc => "directory for binary executables for polybayes", is_optional => 1}
+    ], 
+);
+
+sub help_brief {
+    "add an alignment to Polybayes"
+}
+
+sub help_detail {                           # This is what the user will see with --help <---
+    return <<EOS 
+add an alignment to Polybayes
+EOS
+}
+
+#sub create {                               # Rarely implemented.  Initialize things before execute <---
+#    my $class = shift;
+#    my %params = @_;
+#    my $self = $class->SUPER::create(%params);
+#    # ..do initialization here
+#    return $self;
+#}
+
+#sub validate_params {                      # Pre-execute checking.  Not requiried <---
+#    my $self = shift;
+#    return unless $self->SUPER::validate_params(@_);
+#    # ..do real checks here
+#    return 1;
+#}
+
+sub execute {
+    my $self = shift;
+		my($sample, $dir, $bindir) = 
+				 ($self->sample, $self->dir, $self->bindir);
+		$bindir ||= '/gscmnt/sata114/info/medseq/pkg/bin64';
+		return unless ( defined($sample) && defined($dir)
+									);
+		my $ace2baa ||= "$bindir/ace2Baa";
+
+		$dir =~ s/ \/ $ //x;					# Remove any trailing slash
+
+		# Make sure the output directory exists
+		unless (-e $dir) {
+			mkpath $dir;
+		}
+
+		system("cd $dir ; $ace2baa --ace edit_dir/$sample.ace --baa $sample.baa");
+}
+
+1;
+
+
