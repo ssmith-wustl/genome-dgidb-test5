f3a65df3c44e2c215a879ff04404bd3a3a55c37d
ssmith <ssmith@genome.wustl.edu>
2007-08-14 14:48:58 +0000

    Added base object around which the commands run.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@27265 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model.pm
+++ Genome/Model.pm
@@ -1,18 +1,107 @@
+
 package Genome::Model;
 
+use strict;
+use warnings;
+
+use UR;
 use IO::File;
 use Carp;
 
-our $RUNS_LIST_FILE = '/gscmnt/sata114/info/medseq/aml/run_listing.txt';
+UR::Object::Class->define(
+    class_name  => __PACKAGE__,
+    id_by       => ['name'],
+    has         => [    
+        name                        =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The name of this model.  Unique.", 
+                                        },
+
+        sample_name                 =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The name of the sample whose genome is being modeled", 
+                                        },
+
+        dna_type                    =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "Either whole or cdna",
+                                        },
+                                            
+        reference_sequence_name     =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The reference sequence set name to use.  " . 
+                                                           "Combined with the DNA type, yields an explicit set of sequences.", 
+                                        },
+
+        read_calibrator_name        =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The calibration algorithm to use, if any.", 
+                                        },
+
+        aligner_name                =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The alignment algorithm to use.", 
+                                        },
+
+        
+        genotyper_name              =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The genotyping algorithm to use.", 
+                                        },
+        
+        indel_finder_name           =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The indel finding algorithm to use.", 
+                                        },
+        
+        prior_model_name            =>  {   type        => "String",
+                                            is_optional => 0,
+                                            doc         => "The indel finding algorithm to use.", 
+                                        },
+    ]
+);
 
 
+our $BASE_DIR = "/gscmnt/sata114/info/medseq/";
+
+sub sample_base_directory {
+    my $self = shift;
+    return "$BASE_DIR/sample_data";
+}
+
+sub sample_directory {
+    my $self = shift;
+    return $self->sample_base_directory . '/' . $self->sample_name;
+}
+
+sub genome_model_base_directory {
+    my $self = shift;
+    return $self->sample_directory . '/genome-models';
+}
+
+sub genome_model_directory {
+    my $self = shift;
+    my $shorter_name = $self->name;
+    $shorter_name =~ s/^.*?\+//;
+    return $self->genome_model_base_directory . '/' . $shorter_name;
+}
+
+use YAML;
+sub write_to_filesystem {
+    my $self = shift;
+    print YAML::Dump($self);
+    print "\n";
+    print "sample directory = " . $self->sample_directory . "\n";
+}
+
+our $RUNS_LIST_FILE = $BASE_DIR . 'aml/run_listing.txt';
 sub get_runs_info {
     my($class) = @_;
 
     my $retval = {};
-# Format is one record per line, each line is tab seperated
-# run number, comma sep list of lanes, bustard path for run, sample data path for run
-
+    
+    # Format is one record per line, each line is tab seperated
+    # run number, comma sep list of lanes, bustard path for run, sample data path for run
     my $f = IO::File->new($RUNS_LIST_FILE);
     unless ($f) {
         Carp::croak("Can't open runs list file $RUNS_LIST_FILE for reading: $!");
@@ -68,3 +157,4 @@ sub add_run_info {
 }
 
 1;
+
