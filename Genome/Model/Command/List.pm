c5774476a0b24bd3074f3d9c51c05dc498da1763
rricht <rricht@genome.wustl.edu>
2007-08-15 20:45:19 +0000

    stubbed in missing command
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@27362 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/List.pm
@@ -0,0 +1,43 @@
+
+package Genome::Model::Command::List;
+
+use strict;
+use warnings;
+
+use UR;
+use Command; 
+
+UR::Object::Class->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+);
+
+sub help_brief {
+    "list all genome-models available for manipulation"
+}
+
+sub help_synopsis {
+    return <<"EOS"
+
+Write a subclass of this.  
+
+Give it a name which is an extension of this class name.
+
+Implement a new viewer for some part  of a genome model.
+
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+
+This module is an abstract base class for commands which resolve coverage.
+
+Subclasses will implement different per-base consensus calling algorithms.  This module
+should handle common coverage parameters, typically for handling the results. 
+
+EOS
+}
+
+1;
+
