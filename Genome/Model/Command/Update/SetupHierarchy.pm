6027103c0e707dc0b9ad14f8a80120317ca95d96
ssmith <ssmith@genome.wustl.edu>
2007-08-31 00:48:58 +0000

    Moved SetupHierarchy to this directory, so old functionality is present while we migrate.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@27992 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Update/SetupHierarchy.pm
@@ -0,0 +1,59 @@
+
+package Genome::Model::Command::Update::SetupHierarchy;
+
+use strict;
+use warnings;
+
+use UR;
+use Command;
+use Genome::Model;
+use File::Path;
+
+UR::Object::Class->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+		has => [ 'run', 'lanes', 'sample_path', 'bustard_path' ]
+);
+
+sub help_brief {
+    "add reads to a genome model"
+}
+
+sub help_detail {                           # This is what the user will see with --help <---
+    return <<EOS 
+
+
+EOS
+}
+
+sub execute {
+    my $self = shift;
+    $DB::single=1;
+    Genome::Model->add_run_info( 
+        run_number => $self->run,
+        lanes => $self->lanes,
+        bustard_path => $self->bustard_path,
+        sample_path => $self->sample_path
+    );
+    # make path
+    my $sample_path = $self->sample_path . '/runs/solexa/aml' . $self->run;
+    mkpath $sample_path;
+    my $base_dir = '/gscmnt/sata114/info/medseq/aml';
+    # make symbolic link to path
+    my $base_run_dir = $base_dir . '/aml' .$self->run;
+    if (-e $base_run_dir) {
+            $self->error_message("$base_run_dir already exists");
+            return;
+    }
+    # make symbolic link to bustard
+    my $bustard_dir = $self->bustard_path;
+    if (-e "$sample_path/prb_src") {
+            $self->error_message("$sample_path/prb_src already exists");
+            return;
+    }
+    system("ln -s $sample_path $base_run_dir");
+    system("ln -s $bustard_dir $sample_path/prb_src");
+    return 1;
+}
+1;
+
