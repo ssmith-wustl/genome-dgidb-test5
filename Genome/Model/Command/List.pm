aa018f712104eb7754b568c584955f9016875db1
ssmith <ssmith@genome.wustl.edu>
2007-08-31 03:37:44 +0000

    Fixed docs.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@27999 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/List.pm
+++ Genome/Model/Command/List.pm
@@ -19,24 +19,13 @@ sub help_brief {
 
 sub help_synopsis {
     return <<"EOS"
-
-Write a subclass of this.  
-
-Give it a name which is an extension of this class name.
-
-Implement a new viewer for some part  of a genome model.
-
+genome-model list 
 EOS
 }
 
 sub help_detail {
     return <<"EOS"
-
-This module is an abstract base class for commands which resolve coverage.
-
-Subclasses will implement different per-base consensus calling algorithms.  This module
-should handle common coverage parameters, typically for handling the results. 
-
+Lists all known genome models.
 EOS
 }
 
@@ -48,25 +37,6 @@ sub execute {
         print $_->pretty_print_text;
     }
 
-}
-
-sub print_model {
-    my $self = shift;
-    my $model = shift;
-
-    print Term::ANSIColor::colored("Model: " . $model->name, 'bold red'), "\n\n";
-    print Term::ANSIColor::colored("Configured Properties:", 'bold red'), "\n";
-
-    for my $prop (grep {$_ ne "name"} $model->property_names) {
-        
-        if (defined $model->$prop) {
-
-            print "\t", Term::ANSIColor::colored($prop, 'bold red'), "\t\t",
-            Term::ANSIColor::colored($model->$prop, "red"), "\n";
-        }
-    }
-
-    print "\n\n";
 }
 
 1;
