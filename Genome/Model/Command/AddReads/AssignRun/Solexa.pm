6e485712bee3c4873ba2a596544defb8f91eca0d
abrummet <abrummet@genome.wustl.edu>
2007-09-04 21:57:37 +0000

    First try at moving the old SetupHierarchy command to add-reads assign-run solexa but it's completely wrong
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@28054 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/AssignRun/Solexa.pm
+++ Genome/Model/Command/AddReads/AssignRun/Solexa.pm
@@ -19,61 +19,63 @@ UR::Object::Class->define(
 );
 
 sub help_brief {
-    my $self = shift;
-    return "empty implementation of " . $self->command_name_brief;
+    "Creates the appropriate items on the filesystem for a new Solexa run"
 }
 
 sub help_detail {                           
     return <<EOS 
-not implemented
+This command is normally run automaticly as part of add-reads
 EOS
 }
 
 sub execute {
     my $self = shift;
-    my $model = Genome::Model->get(name=>$self->model);
+
+    $DB::single=1;
+
     my $run = Genome::Run->get(run_id => $self->run_id);
     unless ($run) {
         $self->error_message("Did not find run info for run_id ".$self->run_id);
         return 0;
     }
 
-    $self->error_message("running " . $self->command_name . " on " . $model->name . "!");
-    $self->status_message("Model Info:\n" . $model->pretty_print_text);
-    $self->status_message(sprintf("Run info: run_id %d full_path %s limit_regions %s sequencing_platform %s\n",
-                                  $run->run_id, $run->full_path, $run->limit_regions, $run->sequencing_platform));
-
-    return 0; 
-}
-
-# old logic goes here and needs cleaning up
-sub Xexecute {
-    my $self = shift;
-    $DB::single=1;
-    Genome::Model->add_run_info( 
-        run_number => $self->run,
-        lanes => $self->lanes,
-        bustard_path => $self->bustard_path,
-        sample_path => $self->sample_path
-    );
     # make path
-    my $sample_path = $self->sample_path . '/runs/solexa/aml' . $self->run;
+    #my $sample_path = $self->sample_path . '/runs/solexa/aml' . $self->run;
+    my $sample_path = $self->full_path;
     mkpath $sample_path;
+    unless (-d $sample_path) {
+        $self->error_message("Sample pathname $sample_path was not created");
+        return;
+    }
+
     my $base_dir = '/gscmnt/sata114/info/medseq/aml';
     # make symbolic link to path
-    my $base_run_dir = $base_dir . '/aml' .$self->run;
+    #my $base_run_dir = $base_dir . '/aml' .$self->run;
+    my $base_run_dir = $base_dir . '/aml' .$self->run_id;
     if (-e $base_run_dir) {
             $self->error_message("$base_run_dir already exists");
             return;
     }
+
     # make symbolic link to bustard
+    die "FIXME: where can we get ahold of the bustard_path from?";
     my $bustard_dir = $self->bustard_path;
     if (-e "$sample_path/prb_src") {
             $self->error_message("$sample_path/prb_src already exists");
             return;
     }
-    system("ln -s $sample_path $base_run_dir");
-    system("ln -s $bustard_dir $sample_path/prb_src");
+    #system("ln -s $sample_path $base_run_dir");
+    unless (symlink($sample_path,$base_run_dir)) {
+        $self->error_message("Can't create symlink $base_run_dir pointing to $sample_path: $!");
+        return;
+    }
+ 
+    #system("ln -s $bustard_dir $sample_path/prb_src");
+    unless (symlink($bustard_dir, $sample_path . "/prb_src")) {
+        $self->error_message("Can't create symlink $sample_path/prb_src pointing to $bustard_dir: $!");
+        unlink($base_run_dir);  # Clean up symlink created above
+        return;
+    }
     return 1;
 }
 1;
