9c8c6cb4bd006a561f2e0f0aeb7b181532489251
abrummet <abrummet@genome.wustl.edu>
2007-09-05 19:02:08 +0000

    add-reads assign-run solexa makes a couple of directories that will be needed later
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@28106 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/AssignRun/Solexa.pm
+++ Genome/Model/Command/AddReads/AssignRun/Solexa.pm
@@ -4,18 +4,13 @@ use strict;
 use warnings;
 
 use UR;
-use Command;
-use Genome::Model;
 use File::Path;
-use Data::Dumper;
-use Genome::Model::FileSystemInfo;
 
 UR::Object::Class->define(
     class_name => __PACKAGE__,
-    is => 'Command',
+    is => 'Genome::Model::Event',
     has => [ 
         model   => { is => 'String', is_optional => 0, doc => 'the genome model on which to operate' },
-        run_id  => { is => 'Integer' },
     ]
 );
 
@@ -39,31 +34,31 @@ sub execute {
         $self->error_message("Did not find run info for run_id " . $self->run_id);
         return 0;
     }
-    
-    my $model = Genome::Model->get( name => $self->model );
 
-    # create a space for links to bustards for this sample / platform / DNA type combination, unless exists
-    my $run_sample_path = $model->sample_path . '/runs/' . $run->sequencing_platform . '/' . $model->dna_type;
-    mkpath $run_sample_path unless (-e $run_sample_path && -d $run_sample_path);
-    unless (-d $run_sample_path) {
-        $self->error_message("Run Sample pathname $run_sample_path was not created");
-        return;
-    }
-
-    # ensure symbolic link to bustard not already present
-    my $bustard_dir = $run->full_path;
-    if (-e "$run_sample_path/prb_src") {
-            $self->error_message("$run_sample_path/prb_src already exists");
+    unless (-d  $run->data_parent_directory) {
+        mkdir $run->data_parent_directory;
+        unless(-d $run->data_parent_directory) {
+            $self->error_message("Failed to create data parent directory: ".$run->data_parent_directory. ": $!");
             return;
+        }
     }
- 
-    # make symbolic link to bustard
-    unless (symlink($bustard_dir, $run_sample_path . "/prb_src")) {
-        $self->error_message("Can't create symlink $run_sample_path/prb_src pointing to $bustard_dir: $!");
+
+    my $run_dir = sprintf('%s/runs/%s/%s', $run->data_parent_directory,
+                                           $run->sequencing_platform,
+                                           $run->run_name);
+    if (-d $run_dir) {
+        $self->error_message("Run directory $run_dir already exists");
         return;
     }
     
+    eval { mkpath($run_dir) };
+    if ($@) {
+        $self->error_message("Couldn't create run directory path $run_dir: $@");
+        return;
+    }
+
     return 1;
 }
+
 1;
 
