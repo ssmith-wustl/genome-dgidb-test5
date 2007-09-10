4ae9ba2d08b312a0e5f1949d11a8f4f492bc8ee3
boberkfe <boberkfe@genome.wustl.edu>
2007-09-10 19:45:51 +0000

    list runs by lane, pretty print it all, don't create run records on the fly anymore
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@28256 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/List/Runs.pm
+++ Genome/Model/Command/List/Runs.pm
@@ -35,40 +35,146 @@ EOS
 
 sub execute {
     my $self = shift;
+
+    # find run records in lims and in the genome-model database
+    # they do get or create so there should be no functional difference
+    # based on what order you call these
+    
+    $self->_collect_solexa_run_records_from_lims();
+    $self->_collect_local_run_records();
+    
+    # now print everything we know all pretty-like
+    
+    $self->_summarize_all_run_records();
+ 
+    return 1;
+}
+
+sub _collect_solexa_run_records_from_lims {
+    my $self = shift;
     
     my @gerald_pses = @{ $self->_get_all_gerald_pses() };
     
     foreach my $pse (@gerald_pses)  {
         
         my $gerald_path = $self->_get_gerald_path_from_pse($pse);
+        my %lane_mapping = %{ $self->_get_dna_to_solexa_load_lane_map_for_pse($pse) };
+        
+        foreach my $sample (keys %lane_mapping) {
+            my @lanes = split //, $lane_mapping{$sample};
+            
+            foreach my $lane (@lanes) {
+                my $rr = Genome::Model::Command::List::Runs::InternalRunRecord->get_or_create(
+                         sample_name => $sample,
+                         full_path => $gerald_path,
+                         date_analyzed => $pse->date_scheduled,
+                         lane=>$lane);
+            }
+        }
+    }
+}
+
+sub _collect_local_run_records {
+    my $self = shift;
+    
+    my @runs = Genome::RunChunk->get();
+    
+    for my $r (@runs) {
+        my $rr = Genome::Model::Command::List::Runs::InternalRunRecord->get_or_create(
+            full_path=>$r,
+            lane=>$r->limit_regions);
+        $rr->run_chunk_id($r->id);
+    }   
+}
+
+sub _summarize_all_run_records {
+    my ($self) = @_;
+    
+    my @irrs = Genome::Model::Command::List::Runs::InternalRunRecord->get();
+      
+    my %grouped_by_run_path;
+    for (@irrs) {
+        push @{$grouped_by_run_path{$_->full_path}}, $_;     
+    }
+    
+        
+    for my $path (keys %grouped_by_run_path) {
+        print $path, "\n";
 
         my @out = ();
-
-        push @out, @{
-                     $self->_get_sample_and_model_output_lines_by_plate_lanes_for_pse($pse)
-                     };
-
+        
+        my @sorted_recs = sort {$a->lane <=> $b->lane} @{$grouped_by_run_path{$path}};
+        
+        my $date_analyzed = (defined $sorted_recs[0]->date_analyzed ?
+                             $sorted_recs[0]->date_analyzed : 'unknown' );
+        
         push @out, [
-                    Term::ANSIColor::colored("Gerald Path:", 'red'),
-                    $gerald_path
+                    Term::ANSIColor::colored("Run Path:", 'red'),
+                    $path
                     ];
         
         push @out, [
-                   Term::ANSIColor::colored("Gerald Date:", 'red'),
-                   $pse->date_scheduled
+                   Term::ANSIColor::colored("Date Analyzed:", 'red'),
+                   $date_analyzed
                    ];
-        
-        Genome::Model::EqualColumnWidthTableizer->new->convert_table_to_equal_column_widths_in_place( \@out );
-        
-        print join( "\n",
                    
+        for (@sorted_recs) {
+            
+            my $sample = (defined $_->sample_name ? $_->sample_name : 'unknown');
+            
+            push @out, [Term::ANSIColor::colored("Lane:", 'blue'),
+                                $_->lane
+                        ];
+            
+            push @out, ["\t" . Term::ANSIColor::colored("Sample:", 'blue'),
+                                $sample
+                        ];
+            
+        # This ought to use the class definition instead of a get BUT that doesnt seem to work ATM.   
+        #my @model_names = sort keys %{ { map { $_->name => 1 } $run->models } };
+        if ($_->run_chunk_id) {
+        
+            my @model_names = sort keys %{  {map { $_->name => 1 }
+                                 map {$_->model}
+                                 Genome::Model::Event->get( run_id => $_->run_chunk_id )
+                                } };
+            
+            my $model_text = "In Model";
+            
+            unless( scalar(@model_names) ){
+                @model_names = Term::ANSIColor::colored("none", 'magenta');
+                $model_text .= ':';
+            }else{
+                if(scalar(@model_names) > 1){
+                    $model_text .= 's:';
+                }else{
+                    $model_text .= ':';
+                }
+            }
+            
+            push @out, [
+                       "\t" . Term::ANSIColor::colored("Run ID:", 'green'),
+                       Term::ANSIColor::colored($_->run_chunk_id, "green")
+                       ];
+            
+            push @out, [
+                       "\t" . Term::ANSIColor::colored($model_text, 'red'),
+                       join(", ", @model_names),
+                       ];
+        }
+            
+            
+            Genome::Model::EqualColumnWidthTableizer->new->convert_table_to_equal_column_widths_in_place( \@out );
+        
+            
+        }
+        print join( "\n",
                    map { " @$_ " }
                         @out
-                   
                    ), "\n\n\n";
+       
     }
- 
-    return 1;
+    
 }
 
 sub _get_gerald_path_from_pse{
@@ -107,77 +213,6 @@ sub _get_dna_to_solexa_load_lane_map_for_pse{
     return $lane_mapping;
 }
 
-sub _get_sample_and_model_output_lines_by_plate_lanes_for_pse{
-    my ($self, $pse) = @_;
-    
-    my @out;
-    
-    my %lane_mapping = %{ $self->_get_dna_to_solexa_load_lane_map_for_pse($pse) };
-
-    foreach my $dna (keys %lane_mapping) {
-        my $locs = $lane_mapping{$dna};
-        
-        my $lanes_text = 'Sample';
-        unless($locs eq '12345678'){
-            $lanes_text .= ' in Lane';
-            $lanes_text .= 's' if( length($locs) > 1 );
-            $lanes_text .= ': ';
-            $lanes_text .= $locs;
-        }else{
-            $lanes_text .= ': ';
-        }
-        
-        my $run = Genome::RunChunk->get_or_create(
-                                  full_path             => $self->_get_gerald_path_from_pse($pse),
-                                  limit_regions         => $locs,
-                                  sequencing_platform   => 'solexa',
-                    );
-        
-        my $run_id = $run->id;
-        
-        Carp::croak("Error no RunChunk got or created!") unless $run_id;
-
-        # This ought to use the class definition instead of a get BUT that doesnt seem to work ATM.   
-        #my @model_names = sort keys %{ { map { $_->name => 1 } $run->models } };
-        
-        my @model_names = sort keys %{  {map { $_->name => 1 }
-                             map {$_->model}
-                             Genome::Model::Event->get( run_id => $run_id )
-                            } };
-        
-        my $model_text = "In Model";
-        
-        unless( scalar(@model_names) ){
-            @model_names = Term::ANSIColor::colored("none", 'magenta');
-            $model_text .= ':';
-        }else{
-            if(scalar(@model_names) > 1){
-                $model_text .= 's:';
-            }else{
-                $model_text .= ':';
-            }
-        }
-        
-        push @out, [
-                   Term::ANSIColor::colored("Run ID:", 'green'),
-                   Term::ANSIColor::colored($run_id, "green")
-                   ];
-        
-        push @out, [
-                    Term::ANSIColor::colored($lanes_text, 'red'),
-                    Term::ANSIColor::colored($dna, "bold")
-                    ];
-        
-        push @out, [
-                   Term::ANSIColor::colored($model_text, 'red'),
-                   join(", ", @model_names),
-                   ];
-        
-    }
-
-    return \@out;
-}
-
 sub _get_all_gerald_pses{
     my $self = shift;
         
@@ -196,6 +231,23 @@ sub _get_all_gerald_pses{
     
     return \@gerald_pses;
 }
+
+package Genome::Model::Command::List::Runs::InternalRunRecord;
+
+UR::Object::Class->define(
+    class_name => 'Genome::Model::Command::List::Runs::InternalRunRecord',
+    english_name => 'genome model command list runs internal run record',
+    id_properties => ['full_path', 'lane'],
+    
+    properties => [
+        sample_name                => { is => 'varchar(255)', is_optional => 1 },
+        full_path                  => { is => 'varchar(1000)' },
+        run_chunk_id               => { is => 'integer', is_optional => 1 },
+        date_analyzed              => { is => 'varchar(255)', is_optional=>1},
+        lane                       => { is => 'integer'}
+    ],
+    
+);
 
 
 1;
