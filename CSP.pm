957109ecb21a175ff66028ea220745cc46e93db8
sleonard <sleonard@genome.wustl.edu>
2007-10-18 21:47:54 +0000

    adding allocate and deallocate configurations
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@29317 127c0c0e-971a-0410-ad48-868c1a942d8d

--- CSP.pm
+++ CSP.pm
@@ -653,6 +653,8 @@ my %csp_priority = (
     'make default offsets'                    => 1,
     'configure alignment'                     => 1,
     'import sequence annotation'              => 2,
+    'allocate disk space'                     => 1,
+    'deallocate disk space'                   => 1,
 
 #    these were given their own cron to get around the
 #    problem of running as the lims user instead of seqmgr
