fafbb81195ee37b008a6c3bc5a0a2f4e66a0a4f1
boberkfe <boberkfe@genome.wustl.edu>
2007-11-20 21:15:17 +0000

    add services cmd category
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@30318 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Services.pm
@@ -0,0 +1,21 @@
+
+package Genome::Model::Command::Services;
+
+use strict;
+use warnings;
+
+use above "Genome";
+use Command; 
+
+class Genome::Model::Command::Services {
+    is => 'Command',
+};
+
+sub help_brief {
+    "services intended to be run out of cron or other task scheduler"
+}
+
+sub sub_command_sort_position { 11 }
+
+1;
+
