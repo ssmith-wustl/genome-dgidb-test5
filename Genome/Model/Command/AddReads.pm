e3b9b67cbfe338c5fd022bd6e18ea2699990ce79
abrummet <abrummet@genome.wustl.edu>
2008-03-20 15:22:23 +0000

    The top-level add-reads no longer can take a run-name param, only full-path
    Removed cruft
    
    commands executed as bsubbed jobs are handled with the bsub-helper
    command 
    
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@33115 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads.pm
+++ Genome/Model/Command/AddReads.pm
@@ -15,11 +15,7 @@ class Genome::Model::Command::AddReads {
         sequencing_platform => { is => 'String',
                                 doc => 'Type of sequencing instrument used to generate the data'},
         full_path           => { is => 'String',
-                                doc => 'Pathname for the data produced by the run (GERALD dir for Solexa runs)',
-                                is_optional => 1 },
-        run_name            => { is => 'String',
-                                 doc => "Name of the run.  It will determine the pathname automaticly and add all lanes for the model's sample",
-                                 is_optional => 1 },
+                                doc => 'Pathname to the directory containing unique and duplicate fastq files for that run',},
     ],
     has_optional => [
         limit_regions       =>  { is => 'String',
@@ -75,32 +71,7 @@ sub execute {
     my @sub_command_classes = @{ $self->_get_sorted_sub_command_classes };
 
 $DB::single=1;
-    # Determine the pathname for the run
-    my $full_path;
-    if ($self->full_path) {
-        $full_path = $self->full_path;
-
-    } elsif ($self->run_name) {
-        require GSCApp;
-        my @paths = $self->_find_full_path_by_run_name_and_sequencing_platform();
-
-        if (! @paths) {
-            @paths = $self->_find_full_path_by_run_name_and_sequencing_platform_old();
-        }
-
-        if (! @paths) {
-            $self->error_message("No analysis pathname found for that run name");
-            return;
-        } elsif (@paths > 1) {
-            my $message = "Multiple analysis pathnames found:\n" . join("\n",@paths);
-            $self->warning_message($message);
-            $self->error_message("Use the --full-path option to directly specify one pathname");
-            return;
-        } else {
-            $full_path = $paths[0];
-        }
-    }
-
+    my $full_path = $self->full_path;
     unless (-d $full_path) {
         $self->error_message("full_path $full_path directory does not exist");
         return;
@@ -114,16 +85,12 @@ $DB::single=1;
     } else {
         # The default will differ depengin on what the sequencing_patform is
         $regions = $self->_determine_default_limit_regions();
+        $self->limit_regions($regions);
     }
-
     unless ($regions) {
         $self->error_message("limit_regions is empty!");
         return;
     }
-
-    $self->full_path($full_path);
-    $self->limit_regions($regions);
-    $self->status_message("Using full_path: ".$self->full_path."\nlimit_regions: ".$self->limit_regions);
 
     # Make a RunChunk object for each region
     my $model = $self->model;
@@ -152,18 +119,28 @@ $DB::single=1;
         foreach my $command_class ( @sub_command_classes ) {
             my $command = $command_class->create(run_id => $run->id,
                                                  model_id => $self->model_id);
-    
-            if ($self->bsub) {
-                $last_bsub_job_id = $self->run_command_with_bsub($command,$run,$last_bsub_job_id);
-                $command->lsf_job_id($last_bsub_job_id);
-            } elsif (! $self->test) {
-                $last_bsub_job_id = $command->execute();
+            unless ($command) {
+                $self->error_message("Problem creating subcommand for class $command_class run id ".$run->id." model id ".$self->model_id);
+                return;
             }
+            
+            if (ref($command)) {   # If there's a command to be done at this step
+                my $should_bsub = 0;
+                if ($command->can('should_bsub')) {
+                    $should_bsub = $command->should_bsub;
+                }
 
-
-            # This will be false if something went wrong.
-            # We should probably stop the pipeline at this point
-            return unless $last_bsub_job_id;
+                if ($should_bsub && $self->bsub) {
+                    #$last_bsub_job_id = $self->run_command_with_bsub($command,$run,$last_bsub_job_id);
+                    $last_bsub_job_id = $self->run_command_with_bsub($command,$last_bsub_job_id);
+                    $command->lsf_job_id($last_bsub_job_id);
+                } elsif (! $self->test) {
+                    $last_bsub_job_id = $command->execute();
+                }
+                # This will be false if something went wrong.
+                # We should probably stop the pipeline at this point
+                return unless $last_bsub_job_id;
+            }
 
             # For catching up on all the old runs... remove later
             # This will submit only the assign-run step
@@ -175,123 +152,6 @@ $DB::single=1;
 }
 
 
-sub _find_full_path_by_run_name_and_sequencing_platform_old {
-    my($self) = @_;
-
-    my $run_name = $self->run_name;
-    my $sequencing_platform = $self->sequencing_platform;
-
-    unless ($sequencing_platform eq 'solexa') {
-        $self->error_message("Don't know how to determine run paths for sequencing platform $sequencing_platform");
-        return;
-    }
-
-    my $solexa_run = GSC::Equipment::Solexa::Run->get(run_name => $run_name);
-    unless ($solexa_run) {
-        $self->error_message("No Solexa run found by that name");
-        return;
-    }
-
-    my $config_image_analysis_pse = GSC::PSE->get(pse_id => $solexa_run->creation_event_id);
-    my $sample_name = $self->model->sample_name();
-    my @this_sample_lanes = map { GSC::DNALocation->get($_->dl_id)->location_order }
-                            grep { $_->get_dna->dna_name eq $sample_name }
-                            GSC::DNAPSE->get(pse_id => $config_image_analysis_pse);
-
-    $self->status_message("Sample $sample_name is in lanes: ".join(',',sort @this_sample_lanes));
-
-    my $glob = $solexa_run->run_directory . '/Data/*Firecrest*/Bustard*/GERALD*/';
-    my @possible_gerald_dirs = glob($glob);
-    my @gerald_dirs;
-    foreach my $gerald_dir ( @possible_gerald_dirs ) {
-        my $found = 1;
-        foreach my $lane ( @this_sample_lanes ) {
-            my $lane_pathname = sprintf('%s/s_%s_sequence.txt', $gerald_dir, $lane);
-            unless (-f $lane_pathname) {
-                $found = 0;
-                last;
-            }
-        }
-        push(@gerald_dirs, $gerald_dir) if $found;
-    }
-
-    return @gerald_dirs;
-}
-
-# For solexa runs, return the gerald directory path for the 
-# analysis on this model's samples
-# NOTE: Data is broken on OLTP for flow cell 9133
-sub _find_full_path_by_run_name_and_sequencing_platform {
-    my $self = shift;
-
-    my $run_name = $self->run_name;
-    my $sequencing_platform = $self->sequencing_platform;
-
-    unless ($sequencing_platform eq 'solexa') {
-        $self->error_message("Don't know how to determine run paths for sequencing platform $sequencing_platform");
-        return;
-    }
-
-    my $solexa_run = GSC::Equipment::Solexa::Run->get(run_name => $run_name);
-    unless ($solexa_run) {
-        $self->error_message("No Solexa run found by that name");
-        return;
-    }
-
-    my $config_image_analysis_pse = GSC::PSE->get(pse_id => $solexa_run->creation_event_id);
-    my $sample_name = $self->model->sample_name();
-    my @involved_lanes = map { GSC::DNALocation->get($_->dl_id)->location_order }
-                         grep { $_->get_dna->dna_name eq $sample_name }
-                         GSC::DNAPSE->get(pse_id => $config_image_analysis_pse);
-
-    unless ( @involved_lanes ) {
-        $self->warning_message("Sample $sample_name does not appear to be in this run");
-        return;
-    }
-
-    my @possible_configure_alignment_pses = grep { $_->process_to eq 'configure alignment' and
-                                                   $_->pse_status eq 'completed' and
-                                                   $_->pse_result eq 'successful' }
-                                            $config_image_analysis_pse->get_subsequent_pses_recurse;
-    
-    my @config_alignment_pses = grep { my @l = GSC::PSEParam->get(param_name => 'lanes',
-                                                                  param_value => \@involved_lanes,
-                                                                  pse_id => \@possible_configure_alignment_pses)
-                                     }
-                                @possible_configure_alignment_pses;
-  
-
-    if (@config_alignment_pses != 1) {
-        $self->warning_message("Found ".scalar(@config_alignment_pses)." 'configure alignment' PSE for run $run_name, sample $sample_name");
-        return;
-    }
-
-    my @gerald_dir_param = GSC::PSEParam->get(param_name => 'gerald_directory',
-                                              pse_id => $config_alignment_pses[0]);
-    unless (@gerald_dir_param) {
-        $self->error_message("No gerald_directory PSEParam for pse ".$config_alignment_pses[0]->pse_id);
-        return;
-    }
-
-    my @gerald_dirs = map { $_->param_value }
-                      @gerald_dir_param;
-
-    # the real gerald_dir may have been archived since running gerald.
-    # in that case, the run_directory is the right top-level dir, but
-    # the rest of the path to the gerald_directory must come from the
-    # gerald_directory PSE parameter
-    my $run_directory = $solexa_run->run_directory;
-    foreach ( @gerald_dirs ) {
-        unless (m/^$run_directory/) {
-            s/^.*\/$run_name/$run_directory/;
-        }
-    }
-
-    return @gerald_dirs;
-}
-
-
-
 sub _determine_default_limit_regions {
     my($self) = @_;
 
@@ -299,41 +159,12 @@ sub _determine_default_limit_regions {
         $self->error_message("Don't know how to determine limit-regions for sequencing platform ".$self->sequencing_platform);
         return;
     }
-  
-    my $flowcell;
-    my $run_name = $self->run_name;
-    unless ($run_name) {
-        my @path_parts = split('/', $self->full_path);
-        foreach my $part ( @path_parts ) {
-            ($run_name) = m/.*\/(.*?_\d+_\d+)$/;
-            last if $run_name;;
-        }
-        unless ($run_name) {
-            $self->error_message("Couldn't determine run name from --full-path ".$self->full_path);
-            return;
-        }
-    }
-
-    my $solexa_run = GSC::Equipment::Solexa::Run->get(run_name => $run_name);
-    unless ($solexa_run) {
-        $self->error_message("No Solexa run record for run_name $run_name");
-        return;
-    }
-
-    my @dnapses = GSC::DNAPSE->get(pse_id => $solexa_run->creation_event_id);
-    my $model = Genome::Model->get(genome_model_id => $self->model_id);
-
-    my %location_to_dna =
-               map { GSC::DNALocation->get(dl_id => $_->dl_id)->location_order => GSC::DNA->get(dna_id => $_->dna_id) }
-               grep { $_->get_dna->dna_name eq $model->sample_name }
-               @dnapses;
-
-    return join('',keys %location_to_dna);
+    return '12345678';
 }
 
 
 sub run_command_with_bsub {
-    my($self,$command,$run,$last_bsub_job_id) = @_;
+    my($self,$command,$last_bsub_job_id) = @_;
 
     my $queue = $self->bsub_queue;
     my $bsub_args = $self->bsub_args;
@@ -342,18 +173,15 @@ sub run_command_with_bsub {
         $bsub_args .= ' ' . $command->bsub_rusage;
     }
 
-    # In case the command to run on the blades is different than 'genome-model'
-    my $cmd = $command->command_name;
-    $cmd =~ s/^\S+/$GENOME_MODEL_BSUBBED_COMMAND/;
+    my $cmd = 'genome-model bsub-helper';
 
-    my $run_id = $run->id;
+    my $event_id = $command->genome_model_event_id;
     my $model_id = $self->model_id;
-
     my $cmdline;
     { no warnings 'uninitialized';
         $cmdline = "bsub -q $queue $bsub_args" .
                    ($last_bsub_job_id && " -w $last_bsub_job_id") .
-                   " $cmd --model-id $model_id --run-id $run_id";
+                   " $cmd --model-id $model_id --event-id $event_id";
     }
 
     if ($self->test) {
