91fdf8e59884e2dd8af0461b1bf07e6876df94d1
abrummet <abrummet@genome.wustl.edu>
2008-03-26 14:23:11 +0000

    Don't delete the whole-lane mapfile.  We'll need it at the next step
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@33230 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/AlignReads/Maq.pm
+++ Genome/Model/Command/AddReads/AlignReads/Maq.pm
@@ -105,11 +105,11 @@ $DB::single = 1;
 
         print "running: $cmdline\n";
         system($cmdline);
-#        if ($?) {
-#            my $rv = $? >> 8;
-#            $self->error_message("got a nonzero exit code ($rv) from maq map; something went wrong.  cmdline was $cmdline rv was $rv");
-#            return;
-#        }
+        if ($?) {
+            my $rv = $? >> 8;
+            $self->error_message("got a nonzero exit code ($rv) from maq map; something went wrong.  cmdline was $cmdline rv was $rv");
+            return;
+        }
 
         # use submap if necessary
         my @subsequences = grep {$_ ne "all_sequences" } $model->get_subreference_names(reference_extension=>'bfa');
@@ -134,8 +134,9 @@ $DB::single = 1;
                 }
             }
 
-            # Don't need the whole-lane map file anymore
-            unlink($alignment_file);
+            ## Don't need the whole-lane map file anymore
+            # Actually we do, at the next step...
+            #unlink($alignment_file);
         }  
 
     } # end foreach unique, duplicate
