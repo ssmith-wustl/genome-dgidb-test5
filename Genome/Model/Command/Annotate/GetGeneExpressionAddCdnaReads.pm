122d779815fbed64a31f38a865a8c39fa3b76fcd
gsanders <gsanders@genome.wustl.edu>
2008-03-26 22:00:34 +0000

    Combined GetGeneExpression and AddCdnaReads into one module.
    The two reads and two writes have been combined into reading from AmlReport
    output and outputting once, combining the work of GetGeneExpression and
    AddCdnaReads without having an additional unnecessary file io iteration.
    
    This has not been tested yet since it is not currently known what to provide
    AddCdnaReads for parameters.
    
    
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@33290 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Annotate/GetGeneExpressionAddCdnaReads.pm
+++ Genome/Model/Command/Annotate/GetGeneExpressionAddCdnaReads.pm
@@ -14,20 +14,20 @@ use above "Genome";
 class Genome::Model::Command::Annotate::GetGeneExpressionAddCdnaReads {
     is  => 'Command',
     has => [
-        dev    => { type => 'String', doc => "The database to use" },
-        infile => { type => 'String', doc => "The infile (full report file so far)" },
-        outfile => { type => 'String', doc => "The outfile" },
-        read_hash_unique_dna    => { type => 'String', doc => "ARGV[1]" },
-        read_hash_cdna          => { type => 'String', doc => "ARGV[2]" },
-        read_hash_unique_cdna   => { type => 'String', doc => "ARGV[3]" },
-        read_hash_relapse_cdna  => { type => 'String', doc => "ARGV[4]" },
+        dev    				   => { type => 'String', doc => "The database to use" },
+        infile 				   => { type => 'String', doc => "The infile (full report file so far)" },
+        outfile                => { type => 'String', doc => "The outfile" },
+        read_hash_unique_dna   => { type => 'String', doc => "ARGV[1]" },
+        read_hash_cdna         => { type => 'String', doc => "ARGV[2]" },
+        read_hash_unique_cdna  => { type => 'String', doc => "ARGV[3]" },
+        read_hash_relapse_cdna => { type => 'String', doc => "ARGV[4]" },
     ],
 };
 
 sub sub_command_sort_position { 12 }
 
 sub help_brief {
-    "This command is does both the GetGeneExpression stuff and the AddCdnaReads stuff."
+"This command is does both the GetGeneExpression stuff and the AddCdnaReads stuff."
 }
 
 sub help_synopsis {
@@ -58,8 +58,9 @@ EOS
 #}
 
 sub execute {
-    my $self = shift;
+    my $self    = shift;
     my %options = ( 'dev' => $self->dev, );
+
     #GetOptions( 'devel=s' => \$options{'dev'}, );
 
     unless ( defined( $options{'dev'} ) ) {
@@ -75,7 +76,8 @@ sub execute {
     my ($X);
 
     #$X cannot have 'my($X)' or else it will close every time.
-    ( $X = DBI->connect( "DBI:mysql:$database:$Srv", $Uid, $Pwd ) ) or ( die "fail to connect to database \n" );
+    ( $X = DBI->connect( "DBI:mysql:$database:$Srv", $Uid, $Pwd ) )
+      or ( die "fail to connect to database \n" );
 
     my $sql = qq{
         select expression_intensity, detection from gene_expression ge 
@@ -96,29 +98,40 @@ sub execute {
     close(SUB);
     open( IN, "<", $self->infile ) or die "Can't open " . $self->infile . " $!";
     open( OUT, ">", $self->outfile ) or die "Can't open " . $self->outfile . " $!";
+
     print OUT
-qq{"dbSNP(0:no; 1:yes)",Gene_name,Chromosome,"Start_position (B36)","End_position (B36)",Variant_allele,"# of genomic reads supporting variant allele","# of cDNA reads supporting variant allele",Reference_allele,"# of genomic reads supporting reference allele","# of cDNA reads supporting reference allele",Gene_expression,Detection,Ensembl_transcript_id,Transcript_stranding,Variant_type,Transcript_position,Amino_acid_change,Polyphen_prediction,"submit(0:no; 1:yes)"
-};
+qq{"dbSNP(0:no; 1:yes)",Gene_name,Chromosome,"Start_position (B36)","End_position (B36)",Variant_allele,"# of genomic reads supporting variant allele","# of cDNA reads supporting variant allele","# of unique genomic reads supporting variant allele(starting point)","# of unique genomic reads supporting variant allele(context)","# of unique cDNA reads supporting variant allele(starting point)","# of unique cDNA reads supporting variant allele(context)","# of relapse cDNA reads supporting variant allele",Reference_allele,"# of genomic reads supporting reference allele","# of cDNA reads supporting reference allele","# of unique genomic reads supporting reference allele(starting point)","# of unique genomic reads supporting reference allele(context)","# of unique cDNA reads supporting reference allele(starting point)","# of unique cDNA reads supporting reference allele(context)","# of relapse cDNA reads supporting reference allele",Gene_expression,Detection,Ensembl_transcript_id,Transcript_stranding,Variant_type,Transcript_position,Amino_acid_change,Polyphen_prediction,"submit(0:no; 1:yes)"\n};
 
 #print OUT "dbSNP(0:no; 1:yes),Gene_name,Chromosome,Start_position (B36),End_position (B36),Reference_allele,Variant_allele,# of genomic reads supporting reference allele,# of cDNA reads supporting reference allele,# of genomic reads supporting variant allele,# of cDNA reads supporting variant allele,Gene_expression,Detection,Ensembl_transcript_id,Transcript_stranding,Variant_type,Transcript_position,Amino_acid_change,Polyphen_prediction\n";
+
+    # begin addCdnaReads stuff
+    my $read_hash_unique_dna = MG::Analysis::VariantAnnotation->add_unique_reads_count( $self->read_hash_unique_dna );
+    my $read_hash_cDNA = MG::Analysis::VariantAnnotation->add_reads_count( $self->read_hash_cdna );
+    my $read_hash_unique_cDNA = MG::Analysis::VariantAnnotation->add_unique_reads_count( $self->read_hash_unique_cdna );
+    my $read_hash_relapse_cDNA = MG::Analysis::VariantAnnotation->add_reads_count( $self->read_hash_relapse_dna );
+
     while (<IN>) {
         next if (/dbSNP/);
         chomp();
-        my $line           = $_;
+        my $line = $_;
+
+        # common declarations to both modules
         my $pph_prediction = "NULL";
         my (
-            $dbsnp,     $chromosome, $start,    $end,       $al1,
-            $al1_read1, $al1_read2,  $al2,      $al2_read1, $al2_read2,
-            $gene,      $transcript, $strand,   $trv_type,  $c_position,
-            $pro_str,   $polyphen,   $gene_exp, $gene_det,  $rgg_id
+            $dbsnp,     $chromosome, $start,  $end,       $al1,
+            $al1_read1, $al1_read2,  $al2,    $al2_read1, $al2_read2,
+            $gene,      $transcript, $strand, $trv_type,  $c_position,
+            $pro_str,   $polyphen,   $rgg_id
           )
           = split(/,/);
+
         print "check  $chromosome,$start,$end,$al1,$al2,$transcript...............\n";
 
         #only check flanking region as 10k bp
         if ( $trv_type =~ /flank/ ) {
             my ($tr) = MPSampleData::Transcript->search( "transcript_name" => $transcript );
-            next unless ( ( $tr->transcript_start > $start && $start >= $tr->transcript_start - 10000) || (   $tr->transcript_stop < $start && $start <= $tr->transcript_stop + 10000 ));
+            next unless ( ( $tr->transcript_start > $start && $start >= $tr->transcript_start - 10000)
+                || (   $tr->transcript_stop < $start && $start <= $tr->transcript_stop + 10000 ));
         }
         $gene_hash->{$gene} = MG::Analysis::VariantAnnotation->get_gene_expression( $sth, $gene ) unless ( defined $gene_hash->{$gene} );
 
@@ -127,75 +140,34 @@ qq{"dbSNP(0:no; 1:yes)",Gene_name,Chromosome,"Start_position (B36)","End_positio
         if ( exists $submitted->{"$chromosome,$start,$end"} && $submitted->{"$chromosome,$start,$end"} == 1 ) {
             $submit = 1;
         }
-        else { 
-			$submit = 0; 
-		}
-        print OUT
-"$dbsnp,$gene,$chromosome,$start,$end,$al1,$al1_read1,$al1_read2,$al2,$al2_read1,$al2_read2,",
-          $gene_hash->{$gene}->{exp}, ",", $gene_hash->{$gene}->{det},
-",$transcript,$strand,$trv_type,$c_position,$pro_str,$pph_prediction,$submit,$rgg_id\n";
-    }
-
-    print "final finished!\n";
-
-    close(IN);
-    close(OUT);
-
-    #subroutine
-
-    # check if submitted or -+20bp
-    sub check_submitted {
-        open( FILTER, ">$options{'file'}.filted" ) or die "Can't open $options{'file'}.filted. $!";
-        open( IN, "<$options{'file'}.prioritized" ) or die "Can't open $options{'file'}.prioritized. $!";
-        while (<IN>) { print FILTER $_; last; }
-        while (<IN>) {
-            my $grep = 0;
-            chomp();
-            my @sp = split(/,/);
-            open( CHECK, "</gscuser/xshi/work/AML_SNP/AML_SNP_set.091507.txt" ) or die "Can't open /gscuser/xshi/work/AML_SNP/AML_SNP_set.091507.txt. $!";
-            while (<CHECK>) { last; }
-            while (<CHECK>) {
-                chomp();
-                my @fi = split(/,/);
-                if ( lc( $fi[1] ) eq lc( $sp[1] ) && ( $sp[2] >= $fi[2] - 20 && $sp[3] <= $fi[3] + 20 ) ) {
-                    { $grep = 1; last; }
-                }
-            }
-            close(CHECK);
-            print FILTER join( ",", @sp ), "\n" if ( $grep == 0 );
+        else {
+            $submit = 0;
         }
-        close(IN);
-        close(FILTER);
 
-    }
-	
-	
-	# begin addCdnaReads stuff
-    my $read_hash_unique_dna = MG::Analysis::VariantAnnotation->add_unique_reads_count( $self->read_hash_unique_dna );
-    my $read_hash_cDNA = MG::Analysis::VariantAnnotation->add_reads_count( $self->read_hash_cdna );
-    my $read_hash_unique_cDNA = MG::Analysis::VariantAnnotation->add_unique_reads_count( $self->read_hash_unique_cdna );
-    my $read_hash_relapse_cDNA = MG::Analysis::VariantAnnotation->add_reads_count( $self->read_hash_relapse_dna );
+	   # addCdnaReads stuff
 
-    open( IN, "<", $self->outfile ) or die "Can't open " . $self->outfile . "$!";
-    open( OUT, ">", $self->outfile."read" ) or die "Can't open " . $self->outfile."read" . "$!";
+   	    # This is a list of all of the variables that AddCdnaReads needs that are not
+   		# used in GetGeneExpression with the same variable names.
+   		# AddCdnaReads input variable = GetGeneExpression printed variable name
+   		# $al1_read_hg = $al1_read1
+   		# $al1_read_cDNA = $al1_read2
+   		# $al2_read_hg = $al2_read1
+   		# $al2_read_cDNA = $al2_read2
+   		# $gene_exp = $gene_hash->{$gene}->{exp}
+		# $gene_det = $gene_hash->{$gene}->{det}
+   		# all of the other variables are named the same way in both modules.
 
-    print OUT
-qq{"dbSNP(0:no; 1:yes)",Gene_name,Chromosome,"Start_position (B36)","End_position (B36)",Variant_allele,"# of genomic reads supporting variant allele","# of cDNA reads supporting variant allele","# of unique genomic reads supporting variant allele(starting point)","# of unique genomic reads supporting variant allele(context)","# of unique cDNA reads supporting variant allele(starting point)","# of unique cDNA reads supporting variant allele(context)","# of relapse cDNA reads supporting variant allele",Reference_allele,"# of genomic reads supporting reference allele","# of cDNA reads supporting reference allele","# of unique genomic reads supporting reference allele(starting point)","# of unique genomic reads supporting reference allele(context)","# of unique cDNA reads supporting reference allele(starting point)","# of unique cDNA reads supporting reference allele(context)","# of relapse cDNA reads supporting reference allele",Gene_expression,Detection,Ensembl_transcript_id,Transcript_stranding,Variant_type,Transcript_position,Amino_acid_change,Polyphen_prediction,"submit(0:no; 1:yes)"\n};
+       	# note gene_exp and gene_det were declared in GetGeneExpression but never
+       	# used... $gene_hash->{$gene}->{exp} and ->{det} were referenced instead
+        my $al1_read_hg   = $al1_read1;
+        my $al1_read_cDNA = $al1_read2;
+        my $al2_read_hg   = $al2_read1;
+        my $al2_read_cDNA = $al2_read2;
+        my $gene_exp      = $gene_hash->{$gene}->{exp};
+        my $gene_det      = $gene_hash->{$gene}->{det};
 
-    while (<IN>) {
-        chomp();
-        my $line = $_;
-        next if ( $line =~ /dbSNP/ );
         my (
-            $dbsnp,      $gene,        $chromosome,     $start,
-            $end,        $al1,         $al1_read_hg,    $al1_read_cDNA,
-            $al2,        $al2_read_hg, $al2_read_cDNA,  $gene_exp,
-            $gene_det,   $transcript,  $strand,         $trv_type,
-            $c_position, $pro_str,     $pph_prediction, $submit,
-            $rgg_id
-          )
-          = split(/,/);
-        my ( $al1_read_unique_dna_start,   $al2_read_unique_dna_start,
+            $al1_read_unique_dna_start,   $al2_read_unique_dna_start,
             $al1_read_unique_dna_context, $al2_read_unique_dna_context
         );
         my (
@@ -254,11 +226,45 @@ qq{"dbSNP(0:no; 1:yes)",Gene_name,Chromosome,"Start_position (B36)","End_positio
         }
         print OUT
 "$dbsnp,$gene,$chromosome,$start,$end,$al2,$al2_read_hg,$al2_read_cDNA,$al2_read_unique_dna_start,$al2_read_unique_dna_context,$al2_read_unique_cDNA_start,$al2_read_unique_cDNA_context,$al2_read_relapse_cDNA,$al1,$al1_read_hg,$al1_read_cDNA,$al1_read_unique_dna_start,$al1_read_unique_dna_context,$al1_read_unique_cDNA_start,$al1_read_unique_cDNA_context,$al1_read_relapse_cDNA,$gene_exp,$gene_det,$transcript,$strand,$trv_type,$c_position,$pro_str,$pph_prediction,$submit,$rgg_id\n";
+
     }
+
     print "final finished!\n";
 
     close(IN);
     close(OUT);
+
+    # check if submitted or -+20bp
+    sub check_submitted {
+        open( FILTER, ">$options{'file'}.filted" )
+          or die "Can't open $options{'file'}.filted. $!";
+        open( IN, "<$options{'file'}.prioritized" )
+          or die "Can't open $options{'file'}.prioritized. $!";
+        while (<IN>) { print FILTER $_; last; }
+        while (<IN>) {
+            my $grep = 0;
+            chomp();
+            my @sp = split(/,/);
+            open( CHECK, "</gscuser/xshi/work/AML_SNP/AML_SNP_set.091507.txt" )
+              or die
+"Can't open /gscuser/xshi/work/AML_SNP/AML_SNP_set.091507.txt. $!";
+            while (<CHECK>) { last; }
+            while (<CHECK>) {
+                chomp();
+                my @fi = split(/,/);
+                if ( lc( $fi[1] ) eq lc( $sp[1] )
+                    && ( $sp[2] >= $fi[2] - 20 && $sp[3] <= $fi[3] + 20 ) )
+                {
+                    { $grep = 1; last; }
+                }
+            }
+            close(CHECK);
+            print FILTER join( ",", @sp ), "\n" if ( $grep == 0 );
+        }
+        close(IN);
+        close(FILTER);
+
+    }
 
     return 0;
 }
