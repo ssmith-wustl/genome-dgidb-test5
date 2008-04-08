d0ad3281ff97f35a3b50f4e90d6e1bcbe1a87529
abrummet <abrummet@genome.wustl.edu>
2008-04-08 16:42:39 +0000

    genome-model list jobs now works
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@33488 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/List/Jobs.pm
+++ Genome/Model/Command/List/Jobs.pm
@@ -17,8 +17,10 @@ UR::Object::Type->define(
     class_name => __PACKAGE__,
     is => 'Command',
     has => [
-         done => { type=> 'boolean', is_optional => 1, doc => 'Include info for completed jobs' },
-         user => { type => 'string', is_optional => 1, doc => 'Show info for jobs owned by this user insetad of your own.  Use "all" to show jobs by every user' },
+        done => { type => 'Boolean', doc => 'Include info for successfully completed jobs', is_optional => 1 },
+        user => { type => 'String', doc => 'Show info for jobs owned by this user instead of your own.  Use "all" to show jobs by every user', is_optional => 1 },
+        model_name => { type => 'String', doc => 'Show only jobs for this model name', is_optional => 1 },
+        model_id   => { type => 'String', doc => 'Show only jobs for this model id', is_optional => 1 },
     ],
 );
 
@@ -34,7 +36,7 @@ EOS
 
 sub help_detail {
     return <<"EOS"
-Lists information about bsub-ed jobs.  By default, it only prints information on running or failed jobs.
+Lists information about genome-model related jobs.  By default, it only prints information on scheduled, running or failed jobs.
 EOS
 }
 
@@ -53,55 +55,78 @@ sub execute {
         push(@get_args, 'user_name' => $ENV{'USER'});
     }
 
-    # Get jobs currently running
-    my @jobs = Genome::Model::Event->get(@get_args, date_completed => undef);
-    
-    # failed jobs, too
-    my @failed_jobs = Genome::Model::Event->get(@get_args, event_status => 'Failed');
+    my @model_objs;
+    if ($self->model_name) {
+        @model_objs = Genome::Model->get(name => $self->model_name);
+    } elsif ($self->model_id) {
+        @model_objs = Genome::Model->get(genome_model_id => $self->model_id);
+    }
+    if (@model_objs) {
+        push @get_args, 'model_id', [map { $_->id } @model_objs];
+    }
 
-    my @done_jobs = Genome::Model::Event->get(@get_args, event_status => 'Succeeded');
-    unless ($self->done) {
+    my @statuses = ('Running', 'Scheduled', 'Failed', 'Crashed');
+    if ($self->done) {
+        push @statuses, 'Succeeded';
+    } 
+    push @get_args, 'event_status', \@statuses;
+
+    my @events = Genome::Model::Event->get(@get_args);
+    my %events_by_status;
+    foreach my $event ( @events ) {
+        push @{$events_by_status{$event->event_status}}, $event;
+    }
+
+    
+    if ($self->done) {
         # Only show jobs done in the last day
         my $start_date = Date::Manip::ParseDate("yesterday");
-        @done_jobs = grep { Date::Manip::Date_Cmp($start_date, Date::Manip::ParseDate($_->date_completed)) <= 0 } @done_jobs;
+        my @done_jobs = grep { Date::Manip::Date_Cmp($start_date, Date::Manip::ParseDate($_->date_completed)) <= 0 }
+                        @{$events_by_status{'Succeeded'}};
+
+        print "Done Events in the last 24 hours:\n";
+        $self->_summarize_jobs(@done_jobs);
+        print "\n";
     }
 
-    foreach my $stuff ( ['Done jobs:', \@done_jobs],
-                        ['Currently running jobs:', \@jobs],
-                        ['Failed jobs:', \@failed_jobs],
-                      ) {
 
-        if (@{$stuff->[1]}) {
-            $self->_summarize_jobs(@$stuff);
-            print "\n";
-        }
-    }
+    print "Running jobs:\n";
+    $self->_summarize_jobs(@{$events_by_status{'Running'}}, @{$events_by_status{'Scheduled'}});
+
+    print "\nFailed Jobs:\n";
+    $self->_summarize_jobs(@{$events_by_status{'Failed'}}, @{$events_by_status{'Crashed'}});
 
     return 1;
 }
 
 sub _summarize_jobs {
-    my($self, $header, $events) = @_;
+    my($self, @events) = @_;
 
-    print "$header\n";
+    my $job_info = $self->_get_job_info(map {$_->lsf_job_id} @events);
 
-    my $job_info = $self->_get_job_info(map {$_->lsf_job_id} @$events);
-
-    print("JobID    User     Status     Model                  Run Path                       Command\n");
-    foreach my $event ( @$events ) {
+    my $is_orphaned = 0;
+    print("JobID    User     Status     Model                       Run Path                            Command\n");
+    foreach my $event ( @events ) {
         my $jobid = $event->lsf_job_id || '<none>';
         my $model = $event->model;
         my $run = $event->run;
 
         my $this_job_info = $job_info->{$jobid};
-        printf("%-8s %-8s %-10s %-22s %-30s %s\n",
+        if ($event->event_status eq 'Running' and !$this_job_info) {
+            $jobid .= '*';
+            $is_orphaned = 1;
+        }
+        printf("%-8s %-8s %-10s %-27s %-35s %s\n",
                $jobid,
                $this_job_info->{'user'} || $event->user_name || '<unknown>',
                $this_job_info->{'stat'} || $event->event_status || 'running',
                $model->name,
-               substr($run->full_path, -30),
+               substr($run->full_path, -35),
                $event->event_type || '<unknown>',
             );
+    }
+    if ($is_orphaned) {
+        print "* denotes an event listed as 'Running' but the LSF job no longer exists\n";
     }
 
     return 1;
@@ -115,18 +140,28 @@ sub _get_job_info {
     my $arg = join(' ',grep { $_ } @jobids);
     return {} unless $arg;
 
-    my $fh = IO::File->new("bjobs $arg 2>/dev/null |");
+    my $fh = IO::File->new("bjobs -w $arg 2>/dev/null |");
 
-    $fh->getline();  # Throw away the header info
+    my $header_line = $fh->getline();
 
     my $job_info;
     while (<$fh>) {
         next if (m/is not found/);  # unknown job id
         chomp;
 
-        my $info = {};
-        @$info{'jobid', 'user', 'stat', 'queue', 'from_host', 'exec_host', 'job_name', 'submit_time'} = split(/\s\s+/, $_);
-        $job_info->{$info->{'jobid'}} = $info;
+        my @fields = split(/\s+/);
+
+        my %info;
+        $info{'jobid'} = shift @fields;
+        $info{'user'} = shift @fields;
+        $info{'stat'} = shift @fields;
+        $info{'queue'} = shift @fields;
+        $info{'from_host'} = shift @fields;
+        $info{'exec_host'} = shift @fields;
+        $info{'submit_time'} = join(' ', splice(@fields, -3, 3));
+        $info{'job_name'} = join(' ',@fields);
+
+        $job_info->{$info{'jobid'}} = \%info;
     }
     return $job_info;
 }
