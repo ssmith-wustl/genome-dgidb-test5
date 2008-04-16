3385a623316ebb24d0bd36fa985fbde63ce91b45
ssmith <ssmith@genome.wustl.edu>
2008-04-16 00:34:06 +0000

    Updated to reflect fields in Genome::RunChunk.  All other MG classes were brought into the namespace, but those are not committed.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@33712 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/RunChunk.pm
+++ Genome/RunChunk.pm
@@ -6,24 +6,24 @@ use warnings;
 use File::Basename;
 
 use Genome;
-UR::Object::Type->define(
-    class_name => 'Genome::RunChunk',
-    english_name => 'run chunk',
+class Genome::RunChunk {
+    type_name => 'run chunk',
     table_name => 'GENOME_MODEL_RUN',
     id_by => [
         genome_model_run_id => { is => 'NUMBER', len => 11 },
     ],
     has => [
-        events              => { is => 'Genome::Model::Event', is_many => 1, reverse_id_by => 'model'},
-        models              => { via => 'events', to => 'model'},
+        events              => { is => 'Genome::Model::Event', is_many => 1, reverse_id_by => "run" },
         full_path           => { is => 'VARCHAR2', len => 767 },
         limit_regions       => { is => 'VARCHAR2', len => 32, is_optional => 1 },
-        sequencing_platform => { is => 'VARCHAR2', len => 255 },
+        run_name            => { is => 'VARCHAR2', len => 500, is_optional => 1 },
         sample_name         => { is => 'VARCHAR2', len => 255 },
+        seq_id              => { is => 'NUMBER', len => 15, is_optional => 1 },
+        sequencing_platform => { is => 'VARCHAR2', len => 255 },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
-);
+};
 
 
 
