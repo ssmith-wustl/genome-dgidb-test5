df05e26e3b4cb7b079f4ab1660a9e8372252dd93
ebelter <ebelter@genome.wustl.edu>
2008-04-18 15:06:07 +0000

    updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@33836 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Annotate/AmlReport.pm
+++ Genome/Model/Command/Annotate/AmlReport.pm
@@ -15,6 +15,7 @@ class Genome::Model::Command::Annotate::AmlReport
     ], 
 };
 
+use Data::Dumper;
 use IO::File;
 use MG::Analysis::VariantAnnotation;
 use MPSampleData::DBI;
@@ -39,81 +40,110 @@ CHANGE THIS BLOCK OF TEXT IN THE MODULE TO CHANGE THE HELP OUTPUT.
 EOS
 }
 
-sub execute { 
+sub execute 
+{ 
     my $self = shift;
 
-   MPSampleData::DBI->connect($self->db_name);
+    return $self->_annotate;
+}
 
-   my $file = $self->file;
-   my $fh = IO::File->new("< $file");
-   $self->error_message("Can't open file ($file): $!")
-       and return unless $fh;
+sub _annotate
+{
+    my $self = shift;
 
-   my $err_file = "$file.error";
+    MPSampleData::DBI->connect($self->db_name);
+
+    my $file = $self->file;
+    my $fh = IO::File->new("< $file");
+    $self->error_message("Can't open file ($file): $!")
+        and return unless $fh;
+
+    my $err_file = "$file.error";
     open (ERROR,"> $err_file") or die "Can't open $err_file\: $!";
-   my $out_file = "$file.out";
-    open (OUT, "> $out_file") or die "Can't open $out_file\: $!";
-    print OUT "dbSNP(0:no; 1:yes),Chromosome,Start_position (B36),End_position (B36),Reference_allele,# of cDNA reads supporting reference allele,# of genomic reads supporting reference allele,Variant_allele,# of cDNA reads supporting variant allele,# of genomic reads supporting variant allele,Gene_name,Ensembl_transcript_id,Transcript_stranding,Variant_type,Transcript_position,Amino_acid_change,Polyphen_prediction,Gene_expression,Detection\n";
+
+    my $outfile = "$file.out";
+    my $outfh = IO::File->new("> $outfile");
+    $self->error_message("Can't open file ($outfile): $!")
+        and return unless $outfh;
+
+    $outfh->print("dbSNP(0:no; 1:yes),Chromosome,Start_position (B36),End_position (B36),Reference_allele,# of cDNA reads supporting reference allele,# of genomic reads supporting reference allele,Variant_allele,# of cDNA reads supporting variant allele,# of genomic reads supporting variant allele,Gene_name,Ensembl_transcript_id,Transcript_stranding,Variant_type,Transcript_position,Amino_acid_change,Polyphen_prediction,Gene_expression,Detection\n");
 
     while ( my $id = $fh->getline )
     {
         chomp $id;
         my $genotype = MPSampleData::ReadGroupGenotype->retrieve($id);
-        # check!
+        $self->error_message("Can't find read group genotype for id ($id)")
+            and return unless 0;#$genotype;
 
-=pod
-        print join
-        (
-            "\t",
-            map(
-                { defined($_) ? $_ : 0 }
-                $genotype->chrom_id->chromosome_name,
-                $genotype->start,
-                $genotype->end,
-                $genotype->allele1,
-                $genotype->allele2,
-                $genotype->allele1_type,
-                $genotype->allele2_type,
-                $genotype->num_reads1,
-                $genotype->num_reads2,
-            )
-        ), "\n";
-=cut 
-
-        my ($chromosome,$start,$end,$allele1,$allele1_read1,$allele1_read2,$allele2,$allele2_read1,$allele2_read2,$allele1_type,$allele2_type) ; 
-       ( $chromosome,$start,$end,$allele1,$allele2,$allele1_type,$allele2_type,$allele1_read1,$allele2_read1) =  (
+        my (
+            $chromosome_name,
+            $start,
+            $end,
+            $allele1,
+            $allele2,
+            $allele1_type,
+            $allele2_type,
+            $allele1_read1,
+            $allele2_read1
+        ) =  (
             $genotype->chrom_id->chromosome_name,
-                $genotype->start,
-                $genotype->end,
-                $genotype->allele1,
-                $genotype->allele2,
-                $genotype->allele1_type,
-                $genotype->allele2_type,
-                $genotype->num_reads1,
-                $genotype->num_reads2
+            $genotype->start,
+            $genotype->end,
+            $genotype->allele1,
+            $genotype->allele2,
+            $genotype->allele1_type,
+            $genotype->allele2_type,
+            $genotype->num_reads1,
+            $genotype->num_reads2
+        );
+
+        my ($allele1_read2, $allele2_read2);
+
+        print "check   $chromosome_name,$start,$end,$allele1,$allele2 ...............\t";
+
+        next if $genotype->allele1_type eq 'ref' and $genotype->allele2_type eq 'ref';	
+
+        my $variant_annotation = MG::Analysis::VariantAnnotation->new
+        (
+            type => $allele2_type,
+            chromosome => $chromosome_name,
+            start => $start,
+            end => $end,
+            filter => 1,
+        );
+
+        my $dbsnp = $variant_annotation->check_dbsnp;
+
+        $allele1_read1 = 0 unless defined $allele1_read1;
+        $allele1_read2 = 0 unless defined $allele1_read2;
+        $allele2_read1 = 0 unless defined $allele2_read1;
+        $allele2_read2 = 0 unless defined $allele2_read2;
+
+        my $ref_allele = $genotype->chrom_id->get_reference_allele($start, $end);
+        if ( $allele1_type eq 'ref' and $allele1 ne $ref_allele ) 
+        { 
+            $self->error_message
+            (
+                sprintf
+                (
+                    'Reference allele (%s on chromosome %s from %d to %d) does not match given allele (%s)',
+                    $ref_allele,
+                    $chromosome_name,
+                    $start,
+                    $end,
+                    $allele1,
+                )
             );
-
-        print "check   $chromosome,$start,$end,$allele1,$allele2 ...............\t";
-        next if($allele1_type eq 'ref' && $allele2_type eq 'ref');	
-        my $self=MG::Analysis::VariantAnnotation->new(type=>$allele2_type,chromosome=>$chromosome,start=>$start,end=>$end,filter=>1);
-        my $dbsnp=$self->check_dbsnp;
-# 	next if($dbsnp==1);
-        $allele1_read1=0 if(!defined $allele1_read1);
-        $allele1_read2=0 if(!defined $allele1_read2);
-        $allele2_read1=0 if(!defined $allele2_read1);
-        $allele2_read2=0 if(!defined $allele2_read2);
-
-        #get reference allele
-        my $ref_a=$self->get_referrence_allele($chromosome,$start,$end);
-
-        if($allele1_type eq 'ref'&& $allele1 ne $ref_a) { 
-            print ERROR "reference allele not match:$chromosome,$start,$end,$allele1,$allele1_read1,$allele2,$allele2_read1,$allele1_type,$allele2_type\n"; 
             next;
         }
+
+
+
+
         my (@allele);
         if($allele1_type eq 'SNP' && $allele2_type eq 'SNP'){
-            push(@allele,"$ref_a,0,0,$allele1,$allele1_read1,$allele1_read2") if($allele1 ne $allele2);	
-            push(@allele,"$ref_a,0,0,$allele2,$allele2_read1,$allele2_read2");	
+            push(@allele,"$ref_allele,0,0,$allele1,$allele1_read1,$allele1_read2") if($allele1 ne $allele2);	
+            push(@allele,"$ref_allele,0,0,$allele2,$allele2_read1,$allele2_read2");	
         }
         else{
             push(@allele,"$allele1,$allele1_read1,$allele1_read2,$allele2,$allele2_read1,$allele2_read2");	
@@ -122,76 +152,45 @@ sub execute {
         while(my $allele=shift @allele){
             my ($al1,$al1_read1,$al1_read2,$al2,$al2_read1,$al2_read2)=split(",",$allele);
             print " [anno $al1,$al2] ";
-            my $result=$self->annotate(allele1=>$al1,allele2=>$al2); 
+            my $result=$variant_annotation->annotate(allele1=>$al1,allele2=>$al2); 
 #get the annotation result
-            foreach my $gene (keys %{$self->{annotation}}){
+
+            #print Dumper($variant_annotation->{annotation});
+
+            foreach my $gene (keys %{$variant_annotation->{annotation}}){
                 my @trs;
-                if($self->{filter} eq 1) {
-                    push(@trs,$self->{annotation}->{$gene}->{choice}) if(exists $self->{annotation}->{$gene}->{choice} && defined $self->{annotation}->{$gene}->{choice});
+                if($variant_annotation->{filter} eq 1) {
+                    push(@trs,$variant_annotation->{annotation}->{$gene}->{choice}) if(exists $variant_annotation->{annotation}->{$gene}->{choice} && defined $variant_annotation->{annotation}->{$gene}->{choice});
                 }
                 else {
-                    @trs=keys %{$self->{annotation}->{$gene}->{transcript}} if(exists $self->{annotation}->{$gene}->{transcript} && defined $self->{annotation}->{$gene}->{transcript});  
+                    @trs=keys %{$variant_annotation->{annotation}->{$gene}->{transcript}} if(exists $variant_annotation->{annotation}->{$gene}->{transcript} && defined $variant_annotation->{annotation}->{$gene}->{transcript});  
                 } 
-                unless(@trs) {print ERROR "no result: $id\n"; next;}
+                unless(@trs) 
+                {
+                    print ERROR "no result: $id\n";
+                    next;
+                }
 
                 foreach my $transcript (@trs) {
 # 			my $pph_prediction="NULL"; 
-                    print OUT $dbsnp,",",$chromosome,",$start,$end,$al1,$al1_read1,$al1_read2,$al2,$al2_read1,$al2_read2,",$gene,",",$transcript,",",$self->{annotation}->{$gene}->{transcript}->{$transcript}->{strand},",",$self->{annotation}->{$gene}->{transcript}->{$transcript}->{trv_type},",c.",$self->{annotation}->{$gene}->{transcript}->{$transcript}->{c_position},",",$self->{annotation}->{$gene}->{transcript}->{$transcript}->{pro_str},",NULL,0,0\n";
+                    $outfh->print( join('', $dbsnp,",",$chromosome_name,",$start,$end,$al1,$al1_read1,$al1_read2,$al2,$al2_read1,$al2_read2,",$gene,",",$transcript,",",$variant_annotation->{annotation}->{$gene}->{transcript}->{$transcript}->{strand},",",$variant_annotation->{annotation}->{$gene}->{transcript}->{$transcript}->{trv_type},",c.",$variant_annotation->{annotation}->{$gene}->{transcript}->{$transcript}->{c_position},",",$variant_annotation->{annotation}->{$gene}->{transcript}->{$transcript}->{pro_str},",NULL,0,0\n") );
                 }
             }
-            for my $error_count (keys %{$self->{error}}){
-                print ERROR $self->{error}->{$error_count},"\n"; 
+            for my $error_count (keys %{$variant_annotation->{error}}){
+                print ERROR $variant_annotation->{error}->{$error_count},"\n"; 
             }
-
-
         }
         print "\n";	
     }
 
     print "final finished!\n";
 
-    close(ERROR);
-    close(OUT);
-
     $fh->close;
+    $outfh->close;
+
+    close(ERROR);
 
     return 1;
-}
-
-# check if submitted or -+20bp
-sub check_submitted{
-    # added $self and options
-    my $self = shift;
-    my %options = ( 'file'         => $self->file,
-
-
-        'dev'         => $self->dev,
-
-    );
-
-
-    open (FILTER, ">$options{'file'}.filted") or die "Can't open $options{'file'}.filted. $!";
-    open (IN, "<$options{'file'}.prioritized") or die "Can't open $options{'file'}.prioritized. $!";
-    while(<IN>){print FILTER $_; last;}
-    while(<IN>){
-        my $grep=0;
-        chomp();
-        my @sp=split(/,/);
-        open (CHECK, "</gscuser/xshi/work/AML_SNP/AML_SNP_set.091507.txt") or die "Can't open /gscuser/xshi/work/AML_SNP/AML_SNP_set.091507.txt. $!";
-        while(<CHECK>) {last;}
-        while(<CHECK>) {
-            chomp();
-            my @fi=split(/,/);
-            if(lc($fi[1]) eq lc($sp[1])&&($sp[2]>=$fi[2]-20&&$sp[3]<=$fi[3]+20)) {
-                {$grep=1;last;}
-            }
-        }
-        close(CHECK);
-        print FILTER join(",",@sp),"\n" if($grep==0);
-    }
-    close(IN); 
-    close(FILTER);
-
 }
 
 1;
