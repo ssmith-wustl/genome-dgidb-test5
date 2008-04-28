3ba1c7dcbe21d052698479d219d3fa7ffe8c9b8d
abrummet <abrummet@genome.wustl.edu>
2008-04-28 19:11:56 +0000

    typo fix
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34070 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/MaqSubclasser.pm
+++ Genome/Model/Command/MaqSubclasser.pm
@@ -41,7 +41,7 @@ sub proper_maq_pathname {
 # There's a perl script that's a frontend for some 
 # extra maq functionality.  Assumme it's in the same
 # location as the maq binary, with a ".pl" extention
-sub proper maq_pl_pathname {
+sub proper_maq_pl_pathname {
     my($self, $model_param_name) = @_;
 
     my $path = $self->proper_maq_pathname($model_param_name);
