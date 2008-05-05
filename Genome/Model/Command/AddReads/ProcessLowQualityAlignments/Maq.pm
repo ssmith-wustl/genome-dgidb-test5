8f309ace64aa4721b4cffcd533866ed6051af31b
jwalker <jwalker@genome.wustl.edu>
2008-05-05 15:41:41 +0000

    Add a stop-check to avoid stomping data and fail when expected data is not found.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34227 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/ProcessLowQualityAlignments/Maq.pm
+++ Genome/Model/Command/AddReads/ProcessLowQualityAlignments/Maq.pm
@@ -38,15 +38,25 @@ sub execute {
     my $self = shift;
     
 $DB::single = 1;
+    my $model = Genome::Model->get(id => $self->model_id);
 
     my $unique_reads = $self->unaligned_unique_reads_file_for_lane();
     if (-f $unique_reads) {
         return unless $self->_make_fastq_from_unaligned_file($unique_reads, $self->unaligned_unique_fastq_file_for_lane);
+    } else {
+        $self->error_message("Could not find unique reads file '$unique_reads'");
+        return;
     }
- 
+
     my $dup_reads = $self->unaligned_duplicate_reads_file_for_lane();
     if (-f $dup_reads) {
         return unless $self->_make_fastq_from_unaligned_file($dup_reads, $self->unaligned_duplicate_fastq_file_for_lane);
+    } else {
+        if (! $model->multi_read_fragment_strategy or
+            $model->multi_read_fragment_strategy ne 'EliminateAllDuplicates') {
+            $self->error_message("Could not find duplicate reads file '$dup_reads'");
+            #We do not fail here because some runs are missing the duplicate data after being faked through CQADR
+        }
     }
 
     unless ($self->verify_successful_completion) {
@@ -59,6 +69,12 @@ $DB::single = 1;
 
 sub _make_fastq_from_unaligned_file {
     my($self,$in,$fastq) = @_;
+
+    if (-f $fastq && -s $fastq) {
+        $self->error_message("fastq file already exists '$fastq'");
+        return;
+    }
+
     my $command = Genome::Model::Command::Tools::UnalignedDataToFastq->create(
                            in => $in,
                            fastq => $fastq,
@@ -80,17 +96,27 @@ sub verify_successful_completion {
     my $self = shift;
     my $f;
 
+    my $model = Genome::Model->get(id => $self->model_id);
     my $unique_reads = $self->unaligned_unique_reads_file_for_lane();
     if (-f $unique_reads) {
         unless (-f ($f = $self->unaligned_unique_fastq_file_for_lane)) {
             $self->error_message("No unaligned duplicate fastq file $f for " . $self->desc);
         }
+    } else {
+        $self->error_message("Could not find unique reads file '$unique_reads'");
+        return;
     }
  
     my $dup_reads = $self->unaligned_duplicate_reads_file_for_lane();
     if (-f $dup_reads) {
         unless (-f ($f = $self->unaligned_duplicate_fastq_file_for_lane)) {
             $self->error_message("No unaligned duplicate fastq file $f for " . $self->desc);
+        }
+    } else {
+        if (! $model->multi_read_fragment_strategy or
+            $model->multi_read_fragment_strategy ne 'EliminateAllDuplicates') {
+            $self->error_message("Could not find duplicate reads file '$dup_reads'");
+            #We do not fail here because some runs are missing the duplicate data after being faked through CQADR
         }
     }
 
