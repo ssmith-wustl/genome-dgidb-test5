d4f8e5a14523452cef45721d61faba1f4ede5215
ssmith <ssmith@genome.wustl.edu>
2008-05-13 17:35:50 +0000

    Updated pretty_print_text to be smarter and faster.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34473 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model.pm
+++ Genome/Model.pm
@@ -216,14 +216,38 @@ sub unlock_resource {
 }
 
 
+my @printable_property_names;
 sub pretty_print_text {
     my $self = shift;
-    $DB::single = 1;
+    unless (@printable_property_names) {
+        # do this just once...
+        my $class_meta = $self->get_class_object;
+        for my $name ($class_meta->all_property_names) {
+            next if $name eq 'name';
+            print $name,"\n";
+            my $property_meta = $class_meta->get_property_meta_by_name($name);
+            unless ($property_meta->is_delegated or $property_meta->is_calculated) {
+                push @printable_property_names, $name;
+            }
+        }
+    }
     my @out;
-    for my $prop (grep {$_ ne "name"} $self->property_names) {
-        if (defined $self->$prop) {
-            my $value = $self->$prop;
+    for my $prop (@printable_property_names) {
+        if (my @values = $self->$prop) {
+            my $value;
+            if (@values > 1) {
+                if (grep { ref($_) } @values) {
+                    next;
+                }
+                $value = join(", ", grep { defined $_ } @values);
+            }
+            else {
+                $value = $values[0];
+            }
+            next if not defined $value;
             next if ref $value;
+            next if $value eq '';
+            
             push @out, [
                 Term::ANSIColor::colored($prop, 'red'),
                 Term::ANSIColor::colored($value, "cyan")
