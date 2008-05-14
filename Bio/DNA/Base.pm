718e19fc95d2b869dfe78a4b054340699bf1d0e8
ssmith <ssmith@genome.wustl.edu>
2008-05-14 16:55:37 +0000

    Placeholder class for nucleotide tags.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34485 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Bio/DNA/Base.pm
@@ -0,0 +1,12 @@
+
+package Bio::DNA::CDS;
+use base 'Bio::Dynamic';
+
+use strict;
+use warnings;
+
+create App::Object::Class    
+    class_name => "Bio::DNA::Base",
+    properties    => ['name'],
+    id_properties => ['name'];
+
