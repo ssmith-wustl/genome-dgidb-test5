45ad45da5772dd25267e8762bb6782edd07a3234
ssmith <ssmith@genome.wustl.edu>
2008-05-15 03:00:46 +0000

    Module to manage jobs.  Possibly to be obsoleted by workflow modules.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34702 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Services/JobMonitor.pm
@@ -0,0 +1,82 @@
+package Genome::Model::Command::Services::JobMonitor;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::Model::Command::Services::JobMonitor {
+    is => 'Command',
+};
+
+sub help_brief {
+    return <<EOS
+executes scheduled steps, rescues failed jobs
+EOS
+}
+
+sub help_synopsis {
+    return <<EOS
+genome-model services job-monitor
+EOS
+}
+
+sub help_detail {
+    return <<EOS 
+Monitors and possibly launches jobs.
+EOS
+}
+
+sub execute {
+    my $self = shift;
+
+    $DB::single = 1;
+
+    my @launchable_events = Genome::Model::Event->get(
+        event_status => 'Scheduled', # how did this get an uppercase everywhere?
+        lsf_job_id => undef,
+        ref_seq_id => undef,
+        -order_by => ['model_id','date_scheduled'],
+    );
+
+    $self->_launch_events(@launchable_events);
+
+    my @launchable_events = Genome::Model::Event->get(
+        event_status => 'Scheduled', # how did this get an uppercase everywhere?
+        lsf_job_id => undef,
+        run_id => undef,
+        -order_by => ['model_id','-ref_seq_id'],
+    );
+
+    $self->_launch_events(@launchable_events);
+
+    return 1;
+}
+
+sub _launch_events {
+    my $self = shift;
+    my @launchable_events = @_;
+    
+    my $last_event;
+    while (my $event = shift @launchable_events) {
+        if ( $last_event and ($event->model_id != $last_event->model_id) ) {
+            $last_event = undef;
+        }
+        $last_bsub_job_id = $self->Genome::Model::Event::run_command_with_bsub($event,$last_event);
+        unless ($last_bsub_job_id) {
+            $self->error_message("Error running bsub for event " . $event->id);
+            # skip on to the events for the next model
+            $last_event = $event;
+            while ($event->model_id eq $last_event->model_id) {
+                $self->warning_message("Skipping event " . $event->id . " due to previous error.");
+                $event = shift @launchable_events;
+                last if not defined $event;
+            }
+            redo;
+        }
+        $event->lsf_job_id($last_bsub_job_id);
+        $last_event  = $event;
+    }
+}
+
+1;
