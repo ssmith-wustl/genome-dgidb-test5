e0a449efb59977f2fe32056d1f0d6902b3475da4
jwalker <jwalker@genome.wustl.edu>
2008-05-16 20:23:10 +0000

    Add verify succesful completion method. Move all file path creation logic to auto-created accessors.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34778 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/FindVariations/Maq.pm
+++ Genome/Model/Command/AddReads/FindVariations/Maq.pm
@@ -12,7 +12,70 @@ use File::Temp;
 use IO::File;
 
 class Genome::Model::Command::AddReads::FindVariations::Maq {
-    is => ['Genome::Model::Command::AddReads::FindVariations', 'Genome::Model::Command::MaqSubclasser'],
+    is => [
+           'Genome::Model::Command::AddReads::FindVariations',
+           'Genome::Model::Command::MaqSubclasser'
+       ],
+    has => [
+        analysis_base_path => {
+            doc => "the path at which all analysis output is stored",
+            calculate_from => ['model'],
+            calculate => q|
+                return $model->data_directory. "/identified_variations";
+            |,
+            is_constant => 1,
+        },
+        snip_resource_name => {
+            doc => "basename of the snp output file as well as resource lock name",
+            calculate_from => ['ref_seq_id'],
+            calculate => q|
+                return sprintf("snips%s",defined $ref_seq_id ? "_". $ref_seq_id : "");
+            |,
+        },
+        indel_resource_name => {
+            doc => "basename of the indel output file as well as resource lock name",
+            calculate_from => ['ref_seq_id'],
+            calculate => q|
+                return sprintf("indels%s",defined $ref_seq_id ? "_". $ref_seq_id : "");
+            |,
+        },
+        pileup_resource_name => {
+            doc => "basename of the pileup output file as well as resource lock name",
+            calculate_from => ['ref_seq_id'],
+            calculate => q|
+                return sprintf("pileup%s",defined $ref_seq_id ? "_". $ref_seq_id : "");
+            |,
+        },
+        snip_output_file => {
+            doc => "",
+            calculate_from => ['analysis_base_path','snip_resource_name'],
+            calculate => q|
+                return $analysis_base_path ."/". $snip_resource_name;
+            |,
+        },
+        filtered_snip_output_file => {
+            doc => "",
+            calculate_from => ['snip_output_file'],
+            calculate => q|
+                return $snip_output_file .".filtered";
+            |,
+        },
+        indel_output_file => {
+            doc => "",
+            calculate_from => ['analysis_base_path','indel_resource_name'],
+            calculate => q|
+                return $analysis_base_path ."/". $indel_resource_name;
+            |,
+        },
+        pileup_output_file => {
+            doc => "",
+            calculate_from => ['analysis_base_path','pileup_resource_name'],
+            calculate => q|
+                return $analysis_base_path ."/". $pileup_resource_name;
+            |,
+        },
+
+    ],
 };
 
 sub help_brief {
@@ -40,8 +103,9 @@ sub should_bsub { 1;}
 
 sub execute {
     my $self = shift;
-    
-    my $model = Genome::Model->get(id => $self->model_id);
+
+    my $model = $self->model;
+
     my $maq_pathname = $self->proper_maq_pathname('indel_finder_name');
     my $maq_pl_pathname = $self->proper_maq_pl_pathname('indel_finder_name');
 
@@ -52,43 +116,27 @@ sub execute {
         return;
     }
 
-    my $model_dir = $model->data_directory;
-
-   
-    my $analysis_base_path = $model_dir . "/identified_variations";
-    unless (-d $analysis_base_path) {
-        mkdir($analysis_base_path);
+    unless (-d $self->analysis_base_path) {
+        mkdir($self->analysis_base_path);
     }
 
-    my ($assembly_output_file) =  $model->assembly_file_for_refseq($self->ref_seq_id);   
+    my ($assembly_output_file) =  $model->assembly_file_for_refseq($self->ref_seq_id);
     unless (-f $assembly_output_file) {
         $self->error_message("Assembly output file $assembly_output_file was not found.  It should have been created by a prior run of update-genotype-probabilities maq");
         return;
     }
 
-    my $snp_resource_name = sprintf("snips%s",
-                                    defined $self->ref_seq_id ? "_".$self->ref_seq_id
-                                                              : "");
-
-    my $indel_resource_name = sprintf("indels%s",
-                                    defined $self->ref_seq_id ? "_".$self->ref_seq_id
-                                                              : "");
-
-    my $pileup_resource_name = sprintf("pileup%s",
-                                    defined $self->ref_seq_id ? "_".$self->ref_seq_id
-                                                              : "");
-
-    foreach my $resource ( $snp_resource_name, $indel_resource_name, $pileup_resource_name) {
+    foreach my $resource ( $self->snip_resource_name, $self->indel_resource_name, $self->pileup_resource_name) {
         unless ($model->lock_resource(resource_id=>$resource)) {
             $self->error_message("Can't get lock for resource $resource");
             return undef;
         }
     }
 
-    my $snip_output_file =  $analysis_base_path . "/" . $snp_resource_name;
-    my $filtered_snip_output_file = $snip_output_file . '.filtered';
-    my $indel_output_file =  $analysis_base_path . "/" . $indel_resource_name;
-    my $pileup_output_file = $analysis_base_path . "/" . $pileup_resource_name;
+    my $snip_output_file =  $self->snip_output_file;
+    my $filtered_snip_output_file = $self->filtered_snip_output_file;
+    my $indel_output_file =  $self->indel_output_file;
+    my $pileup_output_file = $self->pileup_output_file;
 
     my $retval = system("$maq_pathname cns2snp $assembly_output_file > $snip_output_file");
     unless ($retval == 0) {
@@ -146,9 +194,21 @@ sub execute {
         return;
     }
 
-    return 1;
+    return $self->verify_succesful_completion;
 }
 
+sub verify_succesful_completion {
+    my $self = shift;
+
+    for my $file ($self->snip_output_file, $self->filtered_snip_output_file, $self->indel_output_file, $self->pileup_output_file) {
+        unless (-e $file && -s $file) {
+            $self->error_message("file does not exist or is zero size $file");
+            return;
+        }
+    }
+
+    return 1;
+}
 
 
 
