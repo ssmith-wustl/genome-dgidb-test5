2ca5a3658200035794c7590b1d4b9b298b95b685
ssmith <ssmith@genome.wustl.edu>
2008-05-21 17:47:35 +0000

    Modules to handle the new i/o hang-offs of Event, with test case.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34873 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Event/Input.pm
@@ -0,0 +1,19 @@
+package Genome::Model::Event::Input;
+
+use strict;
+use warnings;
+
+use Genome;
+class Genome::Model::Event::Input {
+    type_name => 'genome model event input',
+    table_name => 'GENOME_MODEL_EVENT_INPUT',
+    id_by => [
+        event           => { is => 'Genome::Model::Event', id_by => 'event_id', constraint_name => 'GMEI_GME_FK' },
+        name            => { is => 'VARCHAR2', len => 100, column_name => 'PARAM_NAME' },
+        value           => { is => 'VARCHAR2', len => 1000, column_name => 'PARAM_VALUE' },
+    ],
+    schema_name => 'GMSchema',
+    data_source => 'Genome::DataSource::GMSchema',
+};
+
+1;
