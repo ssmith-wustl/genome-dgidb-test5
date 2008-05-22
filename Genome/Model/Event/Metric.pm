e04606287ae3253fa12a38f7ac32e534d3fa6aa8
ssmith <ssmith@genome.wustl.edu>
2008-05-22 16:30:06 +0000

    reset column name for metric value
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34908 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Event/Metric.pm
+++ Genome/Model/Event/Metric.pm
@@ -9,10 +9,10 @@ class Genome::Model::Event::Metric {
     table_name => 'GENOME_MODEL_EVENT_METRIC',
     id_by => [
         event   => { is => 'Genome::Model::Event', id_by => 'event_id', constraint_name => 'GMEM_GME_FK' },
-        name    => { is => 'VARCHAR2', len => 100 },
+        name    => { is => 'VARCHAR2', len => 100, column_name => 'METRIC_NAME' },
     ],
     has => [
-        value   => { is => 'VARCHAR2', len => 1000, is_optional => 1 },
+        value   => { is => 'VARCHAR2', len => 1000, is_optional => 1, column_name => 'METRIC_VALUE' },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
