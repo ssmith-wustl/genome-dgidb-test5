4033e1d6ad457e80bc275f5762a5c767c33b35a9
abrummet <abrummet@genome.wustl.edu>
2008-05-22 21:21:11 +0000

    last_event param to execute_with_bsub can be the current event's prior_event(), not just the last event we happened to have scheduled
    Fixed whitespace/intenting
    
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@34942 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Services/JobMonitor.pm
+++ Genome/Model/Command/Services/JobMonitor.pm
@@ -54,13 +54,11 @@ sub execute {
     my @launchable_events =
         grep { $_->event_type !~ /create/ }
         sort { ($a->model_id <=> $b->model_id) || ($a->date_scheduled <=> $b->date_scheduled) }
-        Genome::Model::Event->get(
-            event_status => 'Scheduled', # how did this get an uppercase everywhere?
-            lsf_job_id => undef,
-            ref_seq_id => undef,
-            #-order_by => ['model_id','date_scheduled'],
-        );
-    $DB::single = 1;
+        Genome::Model::Event->get( event_status => 'Scheduled', # how did this get an uppercase everywhere?
+                                   lsf_job_id => undef,
+                                   ref_seq_id => undef,
+                                   #-order_by => ['model_id','date_scheduled'],
+                                 );
     
     if ($self->model_id) {
         @launchable_events = grep {$_->model_id == $self->model_id} @launchable_events;
@@ -72,18 +70,16 @@ sub execute {
        @launchable_events = grep {$_->id == $self->event_id} @launchable_events;
     }
    
-   
     $self->_launch_events(@launchable_events);
 
     @launchable_events =
         grep { $_->event_type !~ /create/ }
         sort { ($a->model_id <=> $b->model_id) || ($a->date_scheduled <=> $b->date_scheduled) }
-        Genome::Model::Event->get(
-            event_status => 'Scheduled', # how did this get an uppercase everywhere?
-            lsf_job_id => undef,
-            run_id => undef,
-            #-order_by => ['model_id','-ref_seq_id'],
-        );
+        Genome::Model::Event->get( event_status => 'Scheduled', # how did this get an uppercase everywhere?
+                                   lsf_job_id => undef,
+                                   run_id => undef,
+                                   #-order_by => ['model_id','-ref_seq_id'],
+                                 );
 
     if ($self->model_id) {
         @launchable_events = grep {$_->model_id == $self->model_id} @launchable_events;
@@ -107,76 +103,71 @@ sub _launch_events {
     my $last_event;
     while (my $event = shift @launchable_events) {
         if( $event->run_id) {
-            $self->status_message(
-                join("\t", 
-                $event->id,
-                $event->event_type,
-                $event->read_set->full_path,
-            )
-        );
-        } elsif($event->ref_seq_id) {
-        $self->status_message(
-            join("\t", 
-            $event->id,
-            $event->event_type,
-            $event->ref_seq_id,
-            )
-        );
-    }else {
-        $self->status_message($event->id . " does not have run or ref_seq... continuing.\n");
-        next;
-    }
+            $self->status_message( join("\t", 
+                                        $event->id,
+                                        $event->event_type,
+                                        $event->read_set->full_path,
+                                 ));
 
-if ($last_event) {
-    no warnings;
-    if (
-        $event->model_id != $last_event->model_id
-            or $event->ref_seq_id ne $last_event->ref_seq_id
-            or $event->run_id ne $last_event->run_id
-    ) {
-        $last_event = undef;
-    }
-}
-
-if ($self->dispatcher eq "inline") {
-    my $result = $event->execute();
-    $event->date_completed(UR::Time->now);
-    if ($result) {
-        $event->event_status("Succeeded");
-    }
-    else {
-        $event->event_status("Failed");
-    }
-}
-elsif ($self->dispatcher eq "lsf") {
-    my $last_bsub_job_id = $event->execute_with_bsub(
-                                                     last_event => $last_event,
-                                                     bsub_queue => $self->bsub_queue,
-                                                     bsub_args => $self->bsub_args,
-                                                 );
-    unless ($last_bsub_job_id) {
-        $self->error_message("Error running bsub for event " . $event->id);
-        # skip on to the events for the next model
-        $last_event = $event;
-        while ($event->model_id eq $last_event->model_id) {
-            $self->warning_message("Skipping event " . $event->id . " due to previous error.");
-            $event = shift @launchable_events;
-            last if not defined $event;
+        } elsif ($event->ref_seq_id) {
+            $self->status_message( join("\t", 
+                                        $event->id,
+                                        $event->event_type,
+                                        $event->ref_seq_id,
+                                  ));
+        } else {
+            $self->status_message($event->id . " does not have run or ref_seq... continuing.\n");
+            next;
         }
-        redo;
-    }
-    $event->date_scheduled(UR::Time->now);
-    $event->lsf_job_id($last_bsub_job_id);
-}
-else {
-    $self->error_message("Unknown dispatcher: " . (defined $self->dispatcher ? $self->dispatcher : ''));
-    return;
-}
 
-$self->context->commit;
+        if ($last_event) {
+            no warnings;
+            if (     $event->model_id != $last_event->model_id
+                 or $event->ref_seq_id ne $last_event->ref_seq_id
+                 or $event->run_id ne $last_event->run_id
+               ) {
+                $last_event = undef;
+            }
+        }
 
-$last_event  = $event;
-    }
+        if ($self->dispatcher eq "inline") {
+            my $result = $event->execute();
+            $event->date_completed(UR::Time->now);
+            if ($result) {
+                $event->event_status("Succeeded");
+            } else {
+                $event->event_status("Failed");
+            }
+
+        } elsif ($self->dispatcher eq "lsf") {
+            my $last_bsub_job_id = $event->execute_with_bsub( last_event => $self->prior_event || $last_event,
+                                                              bsub_queue => $self->bsub_queue,
+                                                              bsub_args => $self->bsub_args,
+                                                            );
+            unless ($last_bsub_job_id) {
+                $self->error_message("Error running bsub for event " . $event->id);
+                # skip on to the events for the next model
+                $last_event = $event;
+                while ($event->model_id eq $last_event->model_id) {
+                    $self->warning_message("Skipping event " . $event->id . " due to previous error.");
+                    $event = shift @launchable_events;
+                    last if not defined $event;
+                }
+                redo;
+            }
+            $event->date_scheduled(UR::Time->now);
+            $event->lsf_job_id($last_bsub_job_id);
+
+        } else {
+            $self->error_message("Unknown dispatcher: " . (defined $self->dispatcher ? $self->dispatcher : ''));
+            return;
+        }
+
+        $self->context->commit;
+
+        $last_event = $event;
+
+    } # end while @launchable_events
 }
 
 1;
