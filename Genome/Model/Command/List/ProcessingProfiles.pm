ef1a8c2c7a7bec3639a03b128587da43617d492d
gsanders <gsanders@genome.wustl.edu>
2008-06-02 16:07:54 +0000

    Added list ProcessingProfiles.pm. This will list all of the available processing profiles.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35144 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/List/ProcessingProfiles.pm
@@ -0,0 +1,43 @@
+
+package Genome::Model::Command::List::ProcessingProfiles;
+
+use strict;
+use warnings;
+
+use above "Genome";
+use Command; 
+use Data::Dumper;
+
+UR::Object::Type->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+);
+
+sub help_brief {
+    "list all processing profiles available for manipulation"
+}
+
+sub help_synopsis {
+    return <<"EOS"
+genome-model list processing-profiles
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+Lists all known processing profiles.
+EOS
+}
+
+sub execute {
+    my $self = shift;
+    my @processing_profiles = Genome::ProcessingProfile->get();
+    
+    for my $profile (@processing_profiles) {
+        print $profile->pretty_print_text;
+    }
+}
+
+
+1;
+
