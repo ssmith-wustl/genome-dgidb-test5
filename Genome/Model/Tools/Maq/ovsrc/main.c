b7daeff36401bcdbc3386c2d8ad22514cb304a86
bshore <bshore@genome.wustl.edu>
2008-06-09 19:29:32 +0000

    Changed usage to reflect current argument order.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35338 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Maq/ovsrc/main.c
+++ Genome/Model/Tools/Maq/ovsrc/main.c
@@ -4,7 +4,7 @@
 int main (int argc, char **argv)
 {
   if (argc < 3) {
-    fprintf(stderr, "Usage: maqval <location.tsv> <in.map>\n");
+    fprintf(stderr, "Usage: maqval <in.map> <location.tsv>\n");
     return 1;
   }
 	return ovc_filter_variations(argv[1],argv[2]);
