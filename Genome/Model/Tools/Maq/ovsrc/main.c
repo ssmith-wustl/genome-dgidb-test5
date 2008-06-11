a837d3e4f40120c66b0428179295eb30e276bedc
bshore <bshore@genome.wustl.edu>
2008-06-11 20:06:34 +0000

    Changed to reflect the new arguments.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35501 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Maq/ovsrc/main.c
+++ Genome/Model/Tools/Maq/ovsrc/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern int ovc_filter_variations(char *mapfilename,char *snpfilename, int qual_cutoff);
+
 int main (int argc, char **argv)
 {
-  if (argc < 3) {
-    fprintf(stderr, "Usage: maqval <in.map> <location.tsv>\n");
+  if (argc < 4) {
+    fprintf(stderr, "Usage: maqval <in.map> <location.tsv> <quality>\n");
     return 1;
   }
-	return ovc_filter_variations(argv[1],argv[2]);
+	return ovc_filter_variations(argv[1],argv[2], atoi(argv[3]));
 }
