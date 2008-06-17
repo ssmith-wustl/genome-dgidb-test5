4b43d286e431fa839e42e0ab294bfc9f650986d4
jweible <jweible@genome.wustl.edu>
2008-06-17 16:00:16 +0000

    Added unique constraints.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35625 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Metric.pm
+++ Genome/Model/Metric.pm
@@ -9,11 +9,14 @@ class Genome::Model::Metric {
     type_name => 'genome model metric',
     table_name => 'GENOME_MODEL_METRIC',
     id_by => [
-        model   => { is => 'Genome::Model', id_by => 'model_id', constraint_name => 'GMM_GME_FK' },
-        name    => { is => 'VARCHAR2', len => 100, column_name => 'METRIC_NAME' },
+        model    => { is => 'Genome::Model', id_by => 'model_id', constraint_name => 'GMM_GM_FK' },
+        name     => { is => 'VARCHAR2', len => 100, column_name => 'METRIC_NAME' },
     ],
     has => [
-        value   => { is => 'VARCHAR2', len => 1000, is_optional => 1, column_name => 'METRIC_VALUE' },
+        value    => { is => 'VARCHAR2', len => 1000, is_optional => 1, column_name => 'METRIC_VALUE' },
+    ],
+    unique_constraints => [
+        { properties => [qw/model_id name/], sql => 'GMM_PK' },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
