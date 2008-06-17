3ae82f6eabab24212d222d1026198f0c7615a210
jwalker <jwalker@genome.wustl.edu>
2008-06-17 20:52:37 +0000

    Added the read set specific file methods from Event.pm and moved several more to the solexa specific event.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35640 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/EventWithReadSet.pm
+++ Genome/Model/EventWithReadSet.pm
@@ -8,18 +8,33 @@ use above "Genome";
 class Genome::Model::EventWithReadSet {
     is => 'Genome::Model::Event',
     is_abstract => 1,
-    has => [         
+    has => [
         read_set            => { is => 'Genome::RunChunk', id_by => 'run_id', is_optional => 0, constraint_name => 'event_run' },
-        read_set_id         => { via => 'run', to => 'seq_id' }, # not really the fk currently (run_id), see below...
-        
+        read_set_id         => { via => 'read_set', to => 'seq_id' }, # not really the fk currently (run_id), see below...
+
         run_name            => { via => 'read_set' },
         run_short_name      => { via => 'read_set', to => 'short_name' },
         run_subset_name     => { via => 'read_set', to => 'subset_name' },
-        
+        read_set_directory  => {
+                                calculate_from => ['model','read_set'],
+                                calculate => q|
+                                    return sprintf('%s/runs/%s/%s',$model->data_directory,
+                                                                   $read_set->sequencing_platform,
+                                                                   $read_set->name);
+                                |,
+                            },
+
         library_name        => { via => 'read_set' },
         sample_name         => { via => 'read_set' },
-        
+
         # deprecated
+        resolve_run_directory => {
+                                  calculate_from => ['read_set_directory'],
+                                  calculate => q|
+                                      return $read_set_directory,
+                                  |,
+                                  is_deprecated => 1,
+                              },
         run_id              => { is => 'NUMBER', len => 11, is_optional => 0, doc => 'the genome_model_run on which to operate', is_deprecated => 1 }, # don't use
         run                 => { is => 'Genome::RunChunk', id_by => 'run_id', is_deprecated => 1 }, # use read_set
     ],
@@ -57,6 +72,104 @@ sub invalid {
     }
     return @tags;
 }
+
+
+# maq map file for all this lane's alignments
+sub unique_alignment_file_for_lane {
+    my($self) = @_;
+
+    return $self->resolve_run_directory . '/unique_alignments_lane_' . $self->run_subset_name . '.map';
+}
+
+sub duplicate_alignment_file_for_lane {
+    my($self) = @_;
+
+    return $self->resolve_run_directory . '/duplicate_alignments_lane_' . $self->run_subset_name . '.map';
+}
+
+
+# fastq file for all the reads in this lane
+sub fastq_file_for_lane {
+    my($self) = @_;
+
+    return sprintf("%s/s_%s_sequence.fastq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+# a file containing sequence\tread_name\tquality sorted by sequence
+sub sorted_fastq_file_for_lane {
+    my($self) = @_;
+
+    return sprintf("%s/s_%s_sequence.sorted.fastq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub Xoriginal_sorted_unique_fastq_file_for_lane {
+    my($self) = @_;
+
+    return sprintf("%s/%s_sequence.unique.sorted.fastq", $self->run->full_path, $self->run_subset_name);
+}
+
+sub Xsorted_unique_fastq_file_for_lane {
+    my($self) = @_;
+
+    return sprintf("%s/s_%s_sequence.unique.sorted.fastq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub Xoriginal_sorted_duplicate_fastq_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/%s_sequence.duplicate.sorted.fastq", $self->run->full_path, $self->run_subset_name);
+
+}
+
+sub Xsorted_duplicate_fastq_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.duplicate.sorted.fastq", $self->resolve_run_directory, $self->run_subset_name);
+
+}
+
+# The maq bfq file that goes with that lane's fastq file
+sub unique_bfq_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.unique.bfq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub duplicate_bfq_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.duplicate.bfq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub unaligned_unique_reads_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.unaligned.unique", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub unaligned_distinct_fastq_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.unaligned.distinct.fastq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub unaligned_duplicate_reads_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.unaligned.duplicate", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub unaligned_redundant_fastq_file_for_lane {
+    my($self) = @_;
+    return sprintf("%s/s_%s_sequence.unaligned.redundant.fastq", $self->resolve_run_directory, $self->run_subset_name);
+}
+
+sub aligner_unique_output_file_for_lane {
+    return $_[0]->unique_alignment_file_for_lane . '.aligner_output';
+}
+
+sub aligner_duplicate_output_file_for_lane {
+    return $_[0]->duplicate_alignment_file_for_lane . '.aligner_output';
+}
+
+sub alignment_submaps_dir_for_lane {
+    my $self = shift;
+    return sprintf("%s/alignments_lane_%s.submaps", $self->resolve_run_directory, $self->run_subset_name)
+}
+
 
 1;
 
