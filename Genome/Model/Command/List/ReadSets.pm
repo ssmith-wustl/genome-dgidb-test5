6d5be19832ca1c7be53e615b97c233805c1d8ab7
ssmith <ssmith@genome.wustl.edu>
2008-06-19 13:50:53 +0000

    Replaced list "Runs", which was poorly named in that it listed solexa data by lane, with list "ReadSets", which will list solexa lanes, 454 regions, and other chunks of reads.  The underlying class is the Genome::RunChunk, which links to GSC::RunLaneSolexa and/or GSC::RunRegion454.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35705 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/List/ReadSets.pm
@@ -0,0 +1,34 @@
+#!/gsc/bin/perl
+
+package Genome::Model::Command::List::ReadSets;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::Model::Command::List::ReadSets {
+    is => 'UR::Object::Command::List',
+    has => [
+        subject_class_name  => { is_constant => 1, value => 'Genome::RunChunk' },
+        model               => { is_optional => 1 },
+        show                => { default_value => 'run_name,subset_name,sample_name' },
+    ]    
+};
+
+sub sub_command_sort_position {  }
+
+sub help_brief {
+    'list availale reads by "set": i.e. solexa lane, 454 region, etc..'
+}
+sub help_synopsis {
+    return <<EOS
+EOS
+}
+
+sub help_detail {
+    return <<EOS 
+EOS
+}
+
+1;
