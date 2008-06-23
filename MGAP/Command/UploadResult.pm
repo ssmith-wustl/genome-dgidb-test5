86575f1403ea4b2b3ba59f15ecc1554493e556fc
mjohnson <mjohnson@genome.wustl.edu>
2008-06-23 23:41:27 +0000

    -First pass at MGAP/Command/UploadResult.pm.  Not 100% tested, but I'm checking it in now so eclark can get at it ASAP.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@35831 127c0c0e-971a-0410-ad48-868c1a942d8d

--- MGAP/Command/UploadResult.pm
+++ MGAP/Command/UploadResult.pm
@@ -3,11 +3,16 @@ package MGAP::Command::UploadResult;
 use strict;
 use warnings;
 
+use BAP::DB::SequenceSet;
+use BAP::DB::Sequence;
+use BAP::DB::tRNAGene;
 use Workflow;
 
 class MGAP::Command::UploadResult {
     is => ['MGAP::Command'],
     has => [
+        dev => { is => 'SCALAR', doc => "if true set $BAP::DB::DBI::db_env = 'dev'" },
+        seq_set_id => { is => 'SCALAR', doc => 'identifies a whole assembly' },
         bio_seq_features => { is => 'ARRAY', doc => 'array of Bio::Seq::Feature' },
     ],
 };
@@ -35,11 +40,82 @@ EOS
 }
 
 sub execute {
+    
     my $self = shift;
-    $DB::single=1;
+    
+    
+    if ($self->dev()) {
+        $BAP::DB::DBI::db_env = 'dev'; 
+    }
+    
+    my $sequence_set_id = $self->seq_set_id();
 
+    my $sequence_set = BAP::DB::SequenceSet->retrieve($sequence_set_id);
 
-    1;
+    my @sequences = $sequence_set->sequences();
+
+    foreach my $sequence (@sequences) {
+
+        my @coding_genes = $sequence->coding_genes();
+        my @trna_genes   = $sequence->trna_genes();
+        my @rna_genes    = $sequence->rna_genes();
+        
+        foreach my $gene (
+                          @coding_genes,
+                          @trna_genes,
+                          @rna_genes
+                      ) { $gene->delete(); }
+        
+    }
+    
+    my %features   = ( );
+    my %gene_count = ( );
+    
+    foreach my $feature (@{$self->bio_seq_feature()}) {
+        push @{$features{$feature->seq_id()}}, $feature;
+    }
+    
+    foreach my $seq_id (keys %features) {
+
+        $gene_count{$seq_id} = 0;
+        
+        my $sequence = BAP::DB::Sequence->retrieve(
+                                                   sequence_set_id => $sequence_set_id,
+                                                   sequence_name   => $seq_id,
+                                               );
+        
+        my @features = @{$features{$seq_id}};
+        
+        foreach my $feature (@features) {
+            
+            my $source = $feature->source_tag();
+            
+            if ($source eq 'trnascan') {
+                
+                my $gene_name = join('.', $seq_id, (join('', 't', $gene_count{$seq_id})));
+                my ($codon)   = $feature->each_tag_value('Codon');
+                my ($aa)      = $feature->each_tag_value('AminoAcid');
+                
+                BAP::DB::tRNAGene->insert({
+                                           gene_name   => $gene_name,
+                                           sequence_id => $sequence->sequence_id(),
+                                           start       => $feature->start(),
+                                           end         => $feature->end(),
+                                           strand      => $feature->strand(),
+                                           source      => $source,
+                                           score       => $feature->score(),
+                                           codon       => $codon,
+                                           aa          => $aa,
+                                       });
+                
+            }
+            
+        }
+        
+    }
+    
+    return 1;
+    
 }
- 
+
 1;
