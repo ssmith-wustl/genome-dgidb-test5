122082fe7b56c8655de4ac344b3c0fed0b233537
jwalker <jwalker@genome.wustl.edu>
2008-06-30 16:16:59 +0000

    An empty implementation of a newbler subcommand.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36059 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/AddReads/ProcessLowQualityAlignments/Newbler.pm
@@ -0,0 +1,46 @@
+package Genome::Model::Command::AddReads::ProcessLowQualityAlignments::Newbler;
+
+use strict;
+use warnings;
+
+use above "Genome";
+use Command;
+use Genome::Model;
+
+class Genome::Model::Command::AddReads::ProcessLowQualityAlignments::Newbler {
+    is => 'Genome::Model::Command::AddReads::ProcessLowQualityAlignments',
+    has => [
+            alignment_file => {via => 'prior_event'},
+        ]
+};
+
+sub help_brief {
+    "Not sure yet";
+}
+
+sub help_synopsis {
+    return <<"EOS"
+    genome-model add-reads process-low-quality-alignments newbler --model-id 5 --read-set-id 10
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+This command is usually called as part of the add-reads process
+EOS
+}
+
+sub should_bsub { 1;}
+
+
+sub execute {
+    my $self = shift;
+
+    my $model = $self->model;
+    $self->status_message('Not Implemented: '. $self->command_name .' on '. $model->name);
+    return 1;
+}
+
+
+1;
+
