02e0ca82ce7d9438dd05f1ea5f3f54759ecc43f7
jwalker <jwalker@genome.wustl.edu>
2008-06-30 16:19:47 +0000

    Empty implementations that only pass the reads for use downstream.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36060 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/AddReads/AcceptReads/Newbler.pm
@@ -0,0 +1,47 @@
+package Genome::Model::Command::AddReads::AcceptReads::Newbler;
+
+use strict;
+use warnings;
+
+use above "Genome";
+use Command;
+use Genome::Model;
+
+class Genome::Model::Command::AddReads::AcceptReads::Newbler {
+    is => [
+           'Genome::Model::Command::AddReads::AcceptReads',
+       ],
+};
+
+sub help_brief {
+    "Not sure what criteria will be used here";
+}
+
+sub help_synopsis {
+    return <<"EOS"
+    genome-model add-reads accept-reads newbler --model-id 5 --read-set-id 10
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+This command is usually called as part of the add-reads process
+EOS
+}
+
+sub should_bsub {1;}
+
+sub execute {
+    my $self = shift;
+
+    my $model = $self->model;
+
+    # for now everything passes until criteria are determined
+    $self->add_metric(name => 'read set pass fail', value => 'pass');
+
+    return 1;
+}
+
+
+1;
+
