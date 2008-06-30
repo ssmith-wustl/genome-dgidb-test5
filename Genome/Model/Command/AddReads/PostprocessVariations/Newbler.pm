7cc767029454be60efbc271e3e5d2efca8f1896c
jwalker <jwalker@genome.wustl.edu>
2008-06-30 16:47:10 +0000

    An empty implementation of postprocess-variations for newbler.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36070 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/AddReads/PostprocessVariations/Newbler.pm
@@ -0,0 +1,44 @@
+package Genome::Model::Command::AddReads::PostprocessVariations::Newbler;
+
+use strict;
+use warnings;
+
+use above "Genome";
+use Command;
+use Genome::Model;
+
+class Genome::Model::Command::AddReads::PostprocessVariations::Newbler {
+    is => [
+           'Genome::Model::Command::AddReads::PostprocessVariations',
+       ],
+    has => [ ],
+};
+
+sub help_brief {
+    my $self = shift;
+    return "empty implementation of " . $self->command_name_brief;
+}
+
+sub help_synopsis {
+    return <<"EOS"
+    genome-model postprocess-alignments identify-variation newbler --model-id 5 --ref-seq-id 10
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+This command is usually called as part of the postprocess-alignments process
+EOS
+}
+
+sub execute {
+    my $self = shift;
+    my $model = $self->model;
+    $self->status_message('Not Implemented: ' . $self->command_name . ' on ' . $model->name);
+    return 1;
+}
+
+
+
+1;
+
