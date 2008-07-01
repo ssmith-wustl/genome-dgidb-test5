6ea56ea7b755f64201f676f70009fecc358f4fed
bshore <bshore@genome.wustl.edu>
2008-07-01 14:11:56 +0000

    Moved to MG name space from the test_project.  Hooked into genome-model report.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36100 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Report/MutationDiagram.pm
@@ -0,0 +1,53 @@
+
+package Genome::Model::Command::Report::MutationDiagram;
+
+use strict;
+use warnings;
+
+use above "Genome";
+use MG::MutationDiagram;
+#use Data::Dumper;
+#$Data::Dumper::Indent = 1;
+
+UR::Object::Type->define(
+    class_name => __PACKAGE__,
+    is => 'Command',
+    has => [
+						'maf'   => { type => 'String',  doc => "MAF file"},
+						'genes'   => { type => 'String',  doc => "comma separated list of (hugo) gene names (uppercase)--default is ALL", is_optional => 1},
+    ],
+);
+
+sub help_brief {
+    "report mutations as a (svg) diagram"
+}
+
+sub help_synopsis {
+    return <<"EOS"
+genome-model report mutation-diagram  
+                    --maf my.maf
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+Generates (gene) mutation diagrams from a MAF file.
+EOS
+}
+
+sub execute {
+    $DB::single = 1;
+    my $self = shift;
+    my $maf_file = $self->maf;
+		my $maf_obj = new MG::MutationDiagram(maf_file => $maf_file,
+																			hugos => $self->genes
+																		 );
+#		my $data = $maf_obj->Data();
+#		print Dumper($data);
+    return 1;
+}
+
+
+1;
+
+__END__
