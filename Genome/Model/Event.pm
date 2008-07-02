5c762fc55155bffb39ea081f00d8db9b75ca01f5
charris <charris@genome.wustl.edu>
2008-07-02 16:36:38 +0000

    Removed the ssh command and all the stuff that came with it.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36146 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Event.pm
+++ Genome/Model/Event.pm
@@ -309,21 +309,9 @@ sub execute_with_bsub {
 
     my $class = $self->class;
     my $id = $self->id;
-    #my $cmd = qq|perl -e 'use above "Genome"; $class->get($id)->execute() && UR::Context->commit();'|;
 
-    # THE SSH COMMAND AND PARTICULARLY THE NO HOST KEY CHECK IS NOT A GREATE IDEA BUT WORKS FOR GETTING pam_limits
-    my @paths = UR::Util->used_libs();
+    my $cmd = "genome-model bsub-helper";
 
-    my $genome_model_cmd;
-    if (@paths) {
-        my $path = join ' ', @paths;
-        $genome_model_cmd = 'perl -I '. $path .' `which genome-model`';
-    } else {
-        $genome_model_cmd = 'genome-model';
-    }
-    
-    my $cmd = "ssh -o stricthostkeychecking=no -F /etc/ssh/ssh_config localhost $genome_model_cmd bsub-helper";
-    
     my $event_id = $self->genome_model_event_id;
     my $prior_event_id = $last_event->genome_model_event_id if defined $last_event;
 
