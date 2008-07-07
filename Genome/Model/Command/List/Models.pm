02b2c03ce82a718882ae6fab577bc14f2192bf02
ebelter <ebelter@genome.wustl.edu>
2008-07-07 20:18:46 +0000

    updated Models and new Events that inherit from UR::Object::Command::List
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36256 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/List/Models.pm
+++ Genome/Model/Command/List/Models.pm
@@ -1,4 +1,3 @@
-
 package Genome::Model::Command::List::Models;
 
 use strict;
@@ -8,36 +7,17 @@ use above "Genome";
 use Command; 
 use Data::Dumper;
 
-UR::Object::Type->define(
-    class_name => __PACKAGE__,
-    is => 'Command',
-);
-
-sub help_brief {
-    "list all genome-models available for manipulation"
-}
-
-sub help_synopsis {
-    return <<"EOS"
-genome-model list models
-EOS
-}
-
-sub help_detail {
-    return <<"EOS"
-Lists all known genome models.
-EOS
-}
-
-sub execute {
-    my $self = shift;
-    my @models = Genome::Model->get();
-    
-    for (@models) {
-        print $_->pretty_print_text;
-    }
-
-}
+class Genome::Model::Command::List::Models {
+    is => 'UR::Object::Command::List',
+    has => [
+    subject_class_name  => {
+         is_constant => 1, 
+        value => 'Genome::Model' 
+    }, 
+    ],
+};
 
 1;
 
+#$HeadURL$
+#$Id$
