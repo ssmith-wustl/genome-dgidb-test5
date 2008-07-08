7359cd0109d334e73345ae9d0b03f39eaf6d17c1
ebelter <ebelter@genome.wustl.edu>
2008-07-08 20:59:29 +0000

    Object lister
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36327 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ UR/Namespace/Command/List/Objects.pm
@@ -0,0 +1,15 @@
+package UR::Namespace::Command::List::Objects;
+
+use strict;
+use warnings;
+
+use above "UR";
+
+class UR::Namespace::Command::List::Objects { 
+  is =>  'UR::Object::Command::List',
+};
+
+1;
+
+#$HeadURL$
+#$Id$
