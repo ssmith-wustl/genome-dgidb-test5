e26e94b958263e7d1e8ca5ebff0037304471fabf
jwalker <jwalker@genome.wustl.edu>
2008-07-09 20:06:35 +0000

    Move the call to read_aligner_name out of constructor and into ShortRead constructor.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36387 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model.pm
+++ Genome/Model.pm
@@ -94,29 +94,12 @@ class Genome::Model {
 
 sub create {
     my $class = shift;
-    
+
     my $self = $class->SUPER::create(@_);
     if ($^P) {
         $self->test(1);
     }
-    if ($self->read_aligner_name eq 'newbler') {
-        my $new_mapping = Genome::Model::Tools::454::Newbler::NewMapping->create(
-                                                                            dir => $self->alignments_directory,
-                                                                        );
-        unless ($self->new_mapping) {
-            $self->error_message('Could not setup newMapping for newbler in directory '. $self->alignments_directory);
-            return;
-        }
-        my @fasta_files = grep {$_ !~ /all_sequences/} $self->get_subreference_paths(reference_extension => 'fasta');
-        my $set_ref = Genome::Model::Tools::454::Newbler::SetRef->create(
-                                                                    dir => $self->alignments_directory,
-                                                                    reference_fasta_files => \@fasta_files,
-                                                                );
-        unless ($set_ref->execute) {
-            $self->error_message('Could not set refrence setRef for newbler in directory '. $self->alignments_directory);
-            return;
-        }
-    }
+
     return $self;
 }
 
