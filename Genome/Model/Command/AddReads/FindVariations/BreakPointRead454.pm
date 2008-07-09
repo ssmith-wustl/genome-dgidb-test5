dbed0508c0c8c9a1139eb518f268850bd668b2a3
jwalker <jwalker@genome.wustl.edu>
2008-07-09 16:50:56 +0000

    No longer parse the blat output. Just run the variant detection.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36364 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/FindVariations/BreakPointRead454.pm
+++ Genome/Model/Command/AddReads/FindVariations/BreakPointRead454.pm
@@ -13,7 +13,6 @@ class Genome::Model::Command::AddReads::FindVariations::BreakPointRead454 {
        ],
     has => [
             merged_alignments_file => {via => 'prior_event'},
-            merged_fasta_file => {via => 'prior_event'},
             insertions_file => {
                                 calculate_from => ['merged_alignments_file'],
                                 calculate => q|
@@ -59,31 +58,16 @@ sub execute {
 
     my $break_point_path = 'perl ~jwalker/svn/perl_modules/breakPointRead/breakPointRead454.pl';
 
-    my $merged_alignments_file = $self->merged_alignments_file;
-    unless ($merged_alignments_file && -s $merged_alignments_file) {
-        $self->error_message("merged alignments file '$merged_alignments_file' does not exist or has zero size");
-        return;
-    }
-    my $cmd = $break_point_path .' --blat-file '. $merged_alignments_file;
-    $self->status_message('Running: '. $cmd);
-    my $rv = system($cmd);
-    unless ($rv == 0) {
-        $self->error_message("non-zero exit code '$rv' from comamnd $cmd");
-        return;
-    }
-    unless ($self->verify_successful_completion) {
-        $self->error_message('Failed to verify successful completion of event '. $self->id .' on  ref seq '. $self->ref_seq_id);
-        return;
-    }
-    return 1;
-}
+    my $insertions_cmd = $break_point_path .' --combine-indels '. $self->insertions_file;
+    my $deletions_cmd = $break_point_path .' --combine-indels '. $self->deletions_file;
+    my $substitutions_cmd = $break_point_path .' --combine-snps '. $self->substitutions_file;
 
-sub verify_successful_completion {
-    my $self = shift;
-    my @files_to_check = ($self->insertions_file, $self->deletions_file, $self->substitutions_file);
-    for my $file (@files_to_check) {
-        unless (-s $file) {
-            $self->error_message("file '$file' does not exist or has zero size");
+    my @cmds = ($insertions_cmd,$deletions_cmd,$substitutions_cmd);
+    for my $cmd (@cmds) {
+        $self->status_message('Running: '. $cmd);
+        my $rv = system($cmd);
+        unless ($rv == 0) {
+            $self->error_message("non-zero exit code '$rv' returned from '$cmd'");
             return;
         }
     }
