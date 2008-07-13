1d3df7a82d36a7212f59c41f700628700e058cac
ssmith <ssmith@genome.wustl.edu>
2008-07-13 01:42:49 +0000

    Added logic to kill prior LSF jobs, in case we decide to rerun things which have not failed yet or will not explicitly fail.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36512 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/ReLaunch.pm
+++ Genome/Model/Command/ReLaunch.pm
@@ -45,7 +45,8 @@ sub execute {
     my $model = $self->model;
     unless ($model) {
         $self->error_message("No model!?");
-    }    
+    } 
+   
     my $running_build_event = $model->running_build_event;
     unless ($running_build_event) {
         $self->error_message("No in-progress assembly event found.  Run a new one!");
@@ -58,19 +59,25 @@ sub execute {
         "event_type like" => $self->events_matching
     );
 
+    my @lsf_jobs;
     for my $e (@e) {
         $e->event_status('Failed');
-        print $e->id(), "\t", $e->event_type,"\n";
+        push @lsf_jobs, $e->lsf_job_id if $e->lsf_job_id;
+        print $e->id(), "\t", $e->event_type," for ref seq ",$e->ref_seq_id,"\n";
         my $next = $e;
         my $indent = 0;
         while ($next = Genome::Model::Event->get(prior_event => $next)) {
             $next->event_status('Failed');
+            push @lsf_jobs, $next->lsf_job_id if $next->lsf_job_id;
             $indent ++;
             print ((" " x $indent) . $next->id(), "\t", $next->event_type,"\n");
         }
     }
     print "failed " . scalar(@e) . " processes and their subsequent steps.\n";
-    
+ 
+    # TODO: make this more robuts 
+    system "bkill @lsf_jobs";      
+
     return Genome::Model::Command::Services::JobMonitor->execute(model_id => $model->id);
 }
 
