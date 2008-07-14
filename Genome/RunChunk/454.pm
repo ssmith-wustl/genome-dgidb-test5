621c3368ba6e58cbd6ff9b2d7e71953c8fd3c0f9
ssmith <ssmith@genome.wustl.edu>
2008-07-14 02:01:13 +0000

    Added a common method to return the related GSC:: class for the chunk.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36535 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/RunChunk/454.pm
+++ Genome/RunChunk/454.pm
@@ -7,28 +7,24 @@ use above "Genome";
 use Genome::RunChunk;
 
 class Genome::RunChunk::454 {
-    is => [
-           'Genome::RunChunk',
-       ],
+    is  => 'Genome::RunChunk',
     has => [
-            # move both of these into a solexa subclass
-            _run_region_454    => {
-                                   doc => 'Lane representation from LIMS.  This class should eventually be a base class for data like this.',
-                                   is => 'GSC::RunRegion454',
-                                   calculate => q|
-                                    GSC::RunRegion454->get($seq_id);
-                                |,
-                                   calculate_from => ['seq_id']
-                               },
-            library_name       => { via => "_run_region_454" },
-            total_reads        => { via => "_run_region_454", to => "total_key_pass" },
-            dump_fasta         => { via => "_run_region_454", to => "dump_library_region_fasta_file" },
-            dump_qual          => { via => "_run_region_454", to => "dump_library_region_qual_file" },
-            dump_sff           => { via => "_run_region_454" },
-            is_paired_end      => { calculate => q| return "unknown"; | },
-
-        ],
+            _run_region_454     => {
+                                    doc => 'Lane representation from LIMS.  This class should eventually be a base class for data like this.',
+                                    is => 'GSC::RunRegion454',
+                                    calculate => q| GSC::RunRegion454->get($seq_id); |,
+                                    calculate_from => ['seq_id']
+                                },
+            library_name        => { via => "_run_region_454" },
+            total_reads         => { via => "_run_region_454", to => "total_key_pass" },
+            dump_fasta          => { via => "_run_region_454", to => "dump_library_region_fasta_file" },
+            dump_qual           => { via => "_run_region_454", to => "dump_library_region_qual_file" },
+            dump_sff            => { via => "_run_region_454" },
+            is_paired_end       => { calculate => q| return "unknown"; | },
+    ],
 };
 
+sub _dw_class { 'GSC::RunRegion454' }
 
 1;
+
--- Genome/RunChunk/Solexa.pm
+++ Genome/RunChunk/Solexa.pm
@@ -9,41 +9,42 @@ use Genome::RunChunk;
 class Genome::RunChunk::Solexa {
     is => [
            'Genome::RunChunk',
-       ],
+    ],
     has => [
-            _run_lane_solexa    => {
-                                    doc => 'Lane representation from LIMS.  This class should eventually be a base class for data like this.',
-                                    is => 'GSC::RunLaneSolexa',
-                                    calculate => q| GSC::RunLaneSolexa->get($seq_id); |,
-                                    calculate_from => ['seq_id']
-                                },
-            short_name          => {
-                                    doc => 'The essential portion of the run name which identifies the run.  The rest is redundent information about the instrument, date, etc.',
-                                    is => 'String', 
-                                    calculate_from => ['run_name'],
-                                    calculate => q|($run_name =~ /_([^_]+)$/)[0]|
-                                },
-            library_name                    => { via => "_run_lane_solexa" },
-            unique_reads_across_library     => { via => "_run_lane_solexa" },
-            duplicate_reads_across_library  => { via => "_run_lane_solexa" },
-            read_length                     => { via => "_run_lane_solexa" }, 
+        _run_lane_solexa    => {
+                                doc => 'Lane representation from LIMS.  This class should eventually be a base class for data like this.',
+                                is => 'GSC::RunLaneSolexa',
+                                calculate => q| GSC::RunLaneSolexa->get($seq_id); |,
+                                calculate_from => ['seq_id']
+                            },
+        short_name          => {
+                                doc => 'The essential portion of the run name which identifies the run.  The rest is redundent information about the instrument, date, etc.',
+                                is => 'String', 
+                                calculate_from => ['run_name'],
+                                calculate => q|($run_name =~ /_([^_]+)$/)[0]|
+                            },
+        library_name                    => { via => "_run_lane_solexa" },
+        unique_reads_across_library     => { via => "_run_lane_solexa" },
+        duplicate_reads_across_library  => { via => "_run_lane_solexa" },
+        read_length                     => { via => "_run_lane_solexa" }, 
 
-            #rename not to be platform specific and move up
-            clusters                        => { via => "_run_lane_solexa" },
-            is_paired_end                   => { via => "_run_lane_solexa",
-                                                 calculate_from => ['run_type'],
-                                                 calculate => q| if ($run_type =~ m/Paired End/) {
-                                                                    return 1;
-                                                                 }
-                                                                 else {
-                                                                     return 0;
-                                                                 } |
-                                               },
-            run_type                        => { via => "_run_lane_solexa" },
-            gerald_directory                => { via => "_run_lane_solexa" },
-        ],
-    };
+        #rename not to be platform specific and move up
+        clusters                        => { via => "_run_lane_solexa" },
+        is_paired_end                   => { via => "_run_lane_solexa",
+                                             calculate_from => ['run_type'],
+                                             calculate => q| if ($run_type =~ m/Paired End/) {
+                                                                return 1;
+                                                             }
+                                                             else {
+                                                                 return 0;
+                                                             } |
+                                           },
+        run_type                        => { via => "_run_lane_solexa" },
+        gerald_directory                => { via => "_run_lane_solexa" },
+    ],
+};
 
+sub _dw_class { 'GSC::RunLaneSolexa' }
 
 
 1;
