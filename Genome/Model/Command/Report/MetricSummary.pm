bcd5ef1f1445885befa419b9b9857242bad22719
ssmith <ssmith@genome.wustl.edu>
2008-07-24 19:07:22 +0000

    Added the option to exclude chromosomes.  Ultimately, we'll have distinct metrics which exclude sex chromosomes.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@36930 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Report/MetricSummary.pm
+++ Genome/Model/Command/Report/MetricSummary.pm
@@ -9,7 +9,13 @@ use IO::File;
 
 class Genome::Model::Command::Report::MetricSummary {
     is => 'Genome::Model::Command',
-    has => [ ],
+    has => [ 
+        exclude_chromosomes => {
+            is => 'Text',
+            is_optional => 1,
+            doc => 'comma-separated list of chromosomes to exclude from metrics'
+        }
+    ],
 };
 
 sub execute {
@@ -18,6 +24,12 @@ sub execute {
 
     my $dbh = Genome::DataSource::GMSchema->get_default_dbh();
     
+    my $ref_seq_exclusion;
+    if (my $exclude = $self->exclude_chromosomes) {
+        my @exclude = split(",",$exclude);
+        $ref_seq_exclusion = ' and ref_seq_id not in (' . join(",",map { "'$_'"  } @exclude) . ')';     
+    }
+
     print "Stage 1: Run/Lane-Centric Metrics\n"; 
     my $sql_stage1 = qq|
         select v.metric_name,e.event_type,count(distinct r.run_name) run_count,count(distinct e.run_id) lane_count,to_char(to_number(sum(metric_value)),'999,999,999,999,999,999') 
@@ -38,7 +50,7 @@ sub execute {
             join mg.genome_Model_event_metric v on v.event_id = e.genome_model_event_id 
         where e.model_id = $model_id
             and e.run_id is null  
-            and e.ref_seq_id != 'Y'
+            $ref_seq_exclusion 
         group by v.metric_name,e.event_type 
         order by sum(metric_value) desc,event_type,metric_name
     |;
