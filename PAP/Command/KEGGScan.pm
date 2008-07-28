40e22fc4442d88c8a2a3b85532a31a742c9ccf56
mjohnson <mjohnson@genome.wustl.edu>
2008-07-28 15:56:25 +0000

    Add stubbed in modules for BlastP, PsortB and KEGGScan.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37003 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ PAP/Command/KEGGScan.pm
@@ -0,0 +1,53 @@
+package PAP::Command::KEGGScan;
+
+use strict;
+use warnings;
+
+use Workflow;
+
+use Bio::Seq;
+use Bio::SeqIO;
+
+use English;
+
+
+class PAP::Command::KEGGScan {
+    is => ['PAP::Command'],
+    has => [
+        fasta_file  => { is => 'SCALAR', doc => 'fasta file name' },
+        result_file => { is => 'SCALAR', doc => 'absolute path to the output file from iprscan' },
+    ],
+};
+
+operation PAP::Command::KEGGScan {
+    input  => [ 'fasta_file', 'result_file' ],
+    output => [ ],
+};
+
+sub sub_command_sort_position { 10 }
+
+sub help_brief {
+    "Run iprscan";
+}
+
+sub help_synopsis {
+    return <<"EOS"
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+Need documenation here.
+EOS
+}
+
+sub execute {
+
+    my $self = shift;
+
+
+    my $fasta_file  = $self->fasta_file();
+
+}
+ 
+1;
