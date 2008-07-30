dcacadb5278437757ebb34b6b48e519114863e73
mjohnson <mjohnson@genome.wustl.edu>
2008-07-30 16:03:20 +0000

    Doh!  Forgot to commit this.  It's kinda important.  8)
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37092 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ PAP.pm
@@ -0,0 +1,17 @@
+package PAP;
+
+use warnings;
+use strict;
+
+use UR;
+use Workflow;
+
+use lib '/gscmnt/temp212/info/annotation/bioperl-svn/bioperl-live';
+use lib '/gscmnt/temp212/info/annotation/bioperl-svn/bioperl-run';
+
+class PAP {
+    is => ['UR::Namespace'],
+    type_name => 'pap',
+};
+
+1;
