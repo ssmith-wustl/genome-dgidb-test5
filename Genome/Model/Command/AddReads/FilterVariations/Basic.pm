738f439a544111b183ec8afba9e8a77172d14d56
ssmith <ssmith@genome.wustl.edu>
2008-07-30 04:48:19 +0000

    basic filter (currently does nothing) for cases where there is no decision tree, tuning, etc.  currently does nothing except run in the test case.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37086 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/AddReads/FilterVariations/Basic.pm
@@ -0,0 +1,39 @@
+package Genome::Model::Command::AddReads::FilterVariations::Dtr3e;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Command::AddReads::FilterVariations::Basic {
+    is => ['Genome::Model::Command::AddReads::FilterVariations'],
+    sub_classification_method_name => 'class',
+    has => [ ]
+};
+
+sub sub_command_sort_position { 100 }
+
+sub help_brief {
+    "Create filtered lists of variations."
+}
+
+sub help_synopsis {
+    return <<"EOS"
+    genome-model postprocess-alignments filter-variations none --model-id 5 --ref-seq-id 22 
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+    This is a no-op dummy filter step for use by test suites, etc.  In all "production" cases, you want some sort of filtering.
+EOS
+}
+
+sub execute {
+    my $self=shift;
+    $self->status_message("no filtering");
+    return 1;
+}
+
+1;
+
