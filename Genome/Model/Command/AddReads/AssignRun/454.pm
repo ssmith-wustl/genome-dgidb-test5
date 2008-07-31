ad5c7d7a3dfb6f4ec4e964a9eaf2dbeac4af3bbb
jwalker <jwalker@genome.wustl.edu>
2008-07-31 17:43:13 +0000

    No longer dump all the files.  Simply create the lock for the read set and the data directory, just like our Solexa counterpart.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37135 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/AddReads/AssignRun/454.pm
+++ Genome/Model/Command/AddReads/AssignRun/454.pm
@@ -6,57 +6,10 @@ use warnings;
 use above "Genome";
 use Command;
 use Genome::Model;
-use Genome::Model::Tools::454::SffInfo;
 use File::Path;
-use Data::Dumper;
 
 class Genome::Model::Command::AddReads::AssignRun::454 {
     is => 'Genome::Model::Command::AddReads::AssignRun',
-    has => [ 
-            model_id   => { is => 'Integer', is_optional => 0, doc => 'the genome model on which to operate' },
-            sff_file => {
-                         is => 'string',
-                         doc => 'The path were the sff file will be dumped',
-                         calculate_from => ['read_set_directory','read_set'],
-                         calculate => q|
-                           return $read_set_directory .'/'. $read_set->subset_name .'.sff';
-                       |,
-                     },
-            _sff_path => {
-                          calculate_from => ['read_set'],
-                          calculate => q|
-                              return $read_set->full_path . $read_set->subset_name .'.sff';
-                          |,
-                      },
-            fasta_file => {
-                           is => 'string',
-                           doc => "The path were the fasta file will be dumped",
-                           calculate_from => ['read_set_directory','read_set'],
-                           calculate => q|
-                               return $read_set_directory .'/'. $read_set->subset_name .'.fna';
-                           |,
-                       },
-            _fasta_path => {
-                            calculate_from => ['read_set'],
-                            calculate => q|
-                                return $read_set->full_path . $read_set->subset_name .'.fna';
-                            |,
-                        },
-            qual_file => {
-                          is => 'string',
-                          doc => "The path were the quality file will be dumped",
-                          calculate_from => ['read_set_directory','read_set'],
-                          calculate => q|
-                               return $read_set_directory .'/'. $read_set->subset_name .'.qual';
-                           |,
-                      },
-            _qual_path => {
-                           calculate_from => ['read_set'],
-                           calculate => q|
-                                return $read_set->full_path . $read_set->subset_name .'.qual';
-                            |,
-                       },
-    ]
 };
 
 sub help_brief {
@@ -76,120 +29,75 @@ sub help_detail {
 EOS
 }
 
+sub create {
+    my $class = shift;
+    my $obj = $class->SUPER::create(@_);
+
+    unless ($obj->model_id and $obj->run_id and $obj->event_type) {
+        $class->error_message("This step requires the model and run to be specified at construction time for locking concurrency.");
+        $obj->delete;
+        return;
+    }
+
+    my $model = $obj->model;
+
+    my $resource_id = join(".",$class,'create',$obj->run_id);
+    my $lock = $model->lock_resource(resource_id => $resource_id);
+    unless ($lock) {
+        $class->error_message("Failed to lock $resource_id.");
+        $obj->delete;
+        return;
+    }
+
+    my @prev =
+        grep { $_ ne $obj }
+        $class->load(
+            model_id    => $obj->model_id,
+            run_id      => $obj->run_id,
+            event_type  => $obj->event_type,
+        );
+
+    if (@prev) {
+        $obj->error_message(
+            "This run/lane, " 
+            . $obj->run_name . "/" . $obj->run_subset_name. ' '
+            . '(' . $obj->read_set_id . '),'
+            . ' has already been assigned to this model '
+            . $model->id . ' (' . $model->name . ')'
+            . ' on event '
+            . $prev[0]->genome_model_event_id
+        );
+        $obj->model->unlock_resource(resource_id => $resource_id);
+        $obj->delete;
+        return;
+    }
+
+    my $unlock = sub { $model->unlock_resource(resource_id => $resource_id) };
+    $obj->create_subscription(method => 'commit', callback => $unlock);
+    $obj->create_subscription(method => 'delete', callback => $unlock);
+
+    return $obj;
+}
+
 sub execute {
     my $self = shift;
-
+    $DB::single = $DB::stopper;
     my $model = $self->model;
-    my $read_set = $self->read_set;
-
-    unless ($read_set) {
-        $self->error_message("Did not find read_set info for seq_id ". $self->seq_id);
-        return;
+    unless (-d $model->data_directory) {
+        $self->create_directory($model->data_directory);
     }
-
-    unless (-d $model->model_links_directory) {
-        eval { mkpath $model->model_links_directory };
-        if ($@) {
-            $self->error_message('Could not create read_set directory path '. $model->model_links_directory .": $@");
-            return;
-        }
-        unless(-d $model->model_links_directory) {
-            $self->error_message('Failed to create data parent directory: '. $model->model_links_directory .": $!");
-            return;
-        }
-    }
-
-    my $read_set_dir = $self->read_set_directory;
-    unless (-d $read_set_dir) {
-        eval { mkpath($read_set_dir) };
-        if ($@) {
-            $self->error_message("Couldn't create read_set directory path $read_set_dir: $@");
-            return;
-        }
-    }
-
-    # Create the sample_data directory for this run and seq_id if it doesn't already exist
-    my $sample_data_dir = $self->read_set->full_path;
-    unless (-d $sample_data_dir) {
-        eval { mkpath($sample_data_dir) };
-        if ($@) {
-            $self->error_message("Couldn't create sample_data directory path $sample_data_dir: $@");
-            return;
-        }
-    }
-
-    # Dump the sff file(if it doesn't already exist)
-    unless (-s $self->_sff_path) {
-        unless ($read_set->_run_region_454->dump_sff('filename' => $self->_sff_path)) {
-            $self->error_message('Could not dump sff file to '.  $self->_sff_path
-                                 .' for read set '. $read_set->id);
-            return;
-        }
-    }
-    # the sample_data is not in the correct location for it to be used by other models
-    if (-e $self->sff_file) {
-        $self->error_message('sff file already exists under model_data '. $self->sff_file);
-        return;
-    }
-    # symlink the sample_data to the model_data
-    unless(symlink($self->_sff_path,$self->sff_file)) {
-        $self->error_message('Failed to create symlink '. $self->sff_file
-                             .' => '. $self->_sff_path);
-        return;
-    }
-
-    # Create the fasta file from the sff file (if it doesn't already exist)
-    unless (-s $self->_fasta_path) {
-        my $sffinfo_fasta = Genome::Model::Tools::454::SffInfo->create(
-                                                                       sff_file => $self->_sff_path,
-                                                                       params => '-s',
-                                                                       output_file => $self->_fasta_path,
-                                                                   );
-        unless ($sffinfo_fasta->execute) {
-            $self->error_message('Can not convert sff '. $self->_sff_path .' to fasta '. $self->_fasta_path);
-            return;
-        }
-    }
-    # the sample_data is not in the correct location for it to be used by other models
-    if (-e $self->fasta_file) {
-        $self->error_message('fasta file already exists under model_data '. $self->fasta_file);
-        return;
-    }
-    # symlink the sample_data to the model_data
-    unless(symlink($self->_fasta_path,$self->fasta_file)) {
-        $self->error_message('Failed to create symlink '. $self->fasta_file
-                             .' => '. $self->_fasta_path);
-        return;
-    }
-
-
-    # Create the quality file from the sff file (if it doesn't already exist)
-    unless (-s $self->_qual_path) {
-        my $sffinfo_qual = Genome::Model::Tools::454::SffInfo->create(
-                                                                      sff_file => $self->_sff_path,
-                                                                      params => '-q',
-                                                                      output_file => $self->_qual_path,
-                                                                  );
-        unless ($sffinfo_qual->execute) {
-            $self->error_message('Can not convert sff '. $self->_sff_path .' to quality '. $self->_qual_path);
-            return;
-        }
-    }
-    # the sample_data is not in the correct location for it to be used by other models
-    if (-e $self->qual_file) {
-        $self->error_message('qual file already exists under model_data '. $self->qual_file);
-        return;
-    }
-    # symlink the sample_data to the model_data
-    unless(symlink($self->_qual_path,$self->qual_file)) {
-        $self->error_message('Failed to create symlink '. $self->qual_file
-                             .' => '. $self->_qual_path);
-        return;
-    }
-
     return 1;
 }
 
+sub verify_successful_completion {
+    my $self = shift;
+    my $model = $self->model;
+    unless (-d $model->data_directory) {
+    	$self->error_message("Data parent directory doesnt exist: ".$model->data_directory);
+        return 0;
+    }
+    return 1;
+}
 
 1;
 
