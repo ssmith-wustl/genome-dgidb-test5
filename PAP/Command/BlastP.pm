9ef07f07c8bd718ee1d0bb090c500e7e729da74b
adukes <adukes@genome.wustl.edu>
2008-08-01 18:51:20 +0000

    yup
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37184 127c0c0e-971a-0410-ad48-868c1a942d8d

--- PAP/Command/BlastP.pm
+++ PAP/Command/BlastP.pm
@@ -55,8 +55,38 @@ sub execute {
 
     my $fasta_file  = $self->fasta_file();
 
+    my $version;#=???
+    my $file1;#=???
+    my $dir;#=???
+    my $3;#=???
+    my $blastp_dir = "$dir/Blastp/Version_$version/$file1";
 
-    $self->bio_seq_feature([]);
+    unless (-d  $blastp_dir){
+        $self->error_message("blast_p dir doesn't exist");
+        return 0;
+    }
+    
+    $self->status_message( "Running Blastp" );
+ 
+    chdir $blastp_dir;
+    
+ unless( -e $3){
+  system("ln -s $dir/Gene_merging/Version_$version/$file1/$3 .");
+}
+ my $numTotal =`array_shatter $3`;
+ my $remainder = $numTotal%1000;
+ my $numMax = $numTotal-$remainder;
+ my $num0 = 1;
+ my $num1= $num0+999;
+
+ while ($num1 <= $numMax ){
+   bsub -J 'blastp_array['$num0-$num1']' -q long -n 2 -R 'span[hosts=1]' -o blastp.out -e blastp.err "blastp /gscmnt/temp110/analysis/blast_db/gsc_bacterial/bacterial_nr/bacterial_nr \$LSB_JOBINDEX.fasta -o \$LSB_JOBINDEX.blastp E=1e-10 V=1 B=50"
+  $num0= $num1+1;
+  $num1= $num0+999;
+}
+
+#Tranlate
+$self->bio_seq_feature([]);
 
 }
  
