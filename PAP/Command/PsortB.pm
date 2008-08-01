29d232eedc163d4df4e7ea97519fbfba29ff2ccc
josborne <josborne@genome.wustl.edu>
2008-08-01 19:08:03 +0000

    further worked out initial guts for psortb module
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37185 127c0c0e-971a-0410-ad48-868c1a942d8d

--- PAP/Command/PsortB.pm
+++ PAP/Command/PsortB.pm
@@ -7,6 +7,9 @@ use Workflow;
 
 use Bio::Seq;
 use Bio::SeqIO;
+use File::Temp qw/ tempdir /;
+use IPC::Run;
+use Cwd;
 
 use English;
 
@@ -51,12 +54,56 @@ EOS
 sub execute {
 
     my $self = shift;
-
-
     my $fasta_file  = $self->fasta_file();
+
+    my @psortb_command = (
+                          'psort-b',
+                          '-p',
+                          '-o terse',
+                         );
+
+    # do tmp dir, shatter files,
+    my $current_dir = getcwd;
+    my $tmpdir = tempdir("psortbXXXXXX"); # CLEANUP => 1???
+    $self->working_directory($current_dir);
+    chdir($current_dir . "/" . $tmpdir);
+    my $max_chunks = $self->shatter_fasta();
+    # psort-b each sub-fasta
+    foreach my $i (1..$max_chunks)
+    {
+        my $psortb_file = $i . ".fa";
+        push(@psortb_command,$psortb_file);
+        IPC::Run::run(
+                       \@psortb_command,
+                       \undef,
+                       '>',
+                       \$psortb_out,
+                       '2>',
+                       \$psortb_err,
+                     );
+        pop(@psortb_command);
+    }
+    # parse output
+    # turn that into bioseq features
+    # clean up sub-fastas/temp dir.
+    # fertig!
 
     $self->bio_seq_feature([]);
 
 }
+
+sub shatter_fasta {
+    my $self = shift;
+    my $seqfile = $self->fasta_file();
+    my $s = new Bio::SeqIO(-file => $seqfile, -format => 'fasta');
+    my $idx = 1;
+    while(my $seq = $s->next_seq()) {
+        my $ofile = $idx . ".fa"; 
+        my $so = new Bio::SeqIO(-file => ">$ofile", -format => 'fasta');
+    }
+    return $idx;
+}
  
 1;
+
+# $Id$
