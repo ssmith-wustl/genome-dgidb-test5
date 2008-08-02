fc1e8266d0af67536bc078dc08e7add5e93db443
ssmith <ssmith@genome.wustl.edu>
2008-08-02 19:01:40 +0000

    Removed use
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37191 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome.pm
+++ Genome.pm
@@ -2,8 +2,11 @@ package Genome;
 
 use warnings;
 use strict;
+use UR;
 
-use above "UR";
+# DB::single is set to this value in many places, creating a source-embedded break-point
+# set it to zero in the debugger to turn off the constant stopping...
+$DB::stopper = 1;
 
 UR::Object::Type->define(
     class_name => 'Genome',
