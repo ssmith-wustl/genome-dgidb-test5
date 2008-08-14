f98c2a6ecbeb8653ac921423e78f1b8c3e69c40d
jwalker <jwalker@genome.wustl.edu>
2008-08-14 13:26:05 +0000

    An empty processing profile to get started with Assembly of 454 data DeNovo
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37551 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/Assembly.pm
@@ -0,0 +1,14 @@
+package Genome::ProcessingProfile::Assembly;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::ProcessingProfile::Assembly{
+    is => 'Genome::ProcessingProfile',
+};
+
+
+1;
+
