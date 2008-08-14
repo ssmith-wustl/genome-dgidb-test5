dbf25ab3b4a09ef240fa8ea54bf75042ae713ea6
jwalker <jwalker@genome.wustl.edu>
2008-08-14 13:24:35 +0000

    moved pretty_print_text up to ProcessingProfile.pm since it's the same fo
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37550 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/MicroArray.pm
+++ Genome/ProcessingProfile/MicroArray.pm
@@ -10,53 +10,6 @@ class Genome::ProcessingProfile::MicroArray {
     is => 'Genome::ProcessingProfile::ImportedVariants',
 };
 
-my @printable_property_names;
-sub pretty_print_text {
-    my $self = shift;
-    unless (@printable_property_names) {
-        # do this just once...
-        my $class_meta = $self->get_class_object;
-        for my $name ($class_meta->all_property_names) {
-            next if $name eq 'name';
-            my $property_meta = $class_meta->get_property_meta_by_name($name);
-            unless ($property_meta->is_delegated or $property_meta->is_calculated) {
-                push @printable_property_names, $name;
-            }
-        }
-    }
-    my @out;
-    for my $prop (@printable_property_names) {
-        if (my @values = $self->$prop) {
-            my $value;
-            if (@values > 1) {
-                if (grep { ref($_) } @values) {
-                    next;
-                }
-                $value = join(", ", grep { defined $_ } @values);
-            }
-            else {
-                $value = $values[0];
-            }
-            next if not defined $value;
-            next if ref $value;
-            next if $value eq '';
-            
-            push @out, [
-                Term::ANSIColor::colored($prop, 'red'),
-                Term::ANSIColor::colored($value, "cyan")
-            ]
-        }
-    }
-    
-    Genome::Model::EqualColumnWidthTableizer->new->convert_table_to_equal_column_widths_in_place( \@out );
-
-    my $out;
-    $out .= Term::ANSIColor::colored(sprintf("Processing Profile: %s (ID %s)", $self ->name, $self->id), 'bold magenta') . "\n\n";
-    $out .= Term::ANSIColor::colored("Configured Properties:", 'red'). "\n";    
-    $out .= join("\n", map { " @$_ " } @out);
-    $out .= "\n\n";
-    return $out;
-}
 
 1;
 
