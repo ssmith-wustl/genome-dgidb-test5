285611ec498e2b735dafba2da55019a679c14241
jwalker <jwalker@genome.wustl.edu>
2008-08-14 14:02:41 +0000

    Add the build methods for creating a 454 DeNovo assembly.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37558 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Build/Assembly.pm
@@ -0,0 +1,97 @@
+package Genome::Model::Command::Build::Assembly;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::Model::Command::Build::Assembly {
+    is => 'Genome::Model::Command::Build',
+    has => [
+        ],
+ };
+
+sub sub_command_sort_position { 40 }
+
+sub command_subclassing_model_property {
+    print "hi\n";
+    'foora'
+}
+
+sub help_brief {
+    "assemble a genome"
+}
+
+sub help_synopsis {
+    return <<"EOS"
+genome-model build mymodel 
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+One build of a given assembly model.
+EOS
+}
+
+sub subordinate_job_classes {
+    return (
+            'Genome::Model::Command::Build::Assembly::AssignReadSetToModel',
+            'Genome::Model::Command::Build::Assembly::AddReadSetToProject',
+    );
+}
+
+sub execute {
+    my $self = shift;
+
+    $DB::single = $DB::stopper;
+    my $model = $self->model;
+    
+    my @sub_command_classes = $self->subordinate_job_classes;
+    my $last_event_id;
+    my @available_read_sets = $model->available_read_sets;
+    for my $read_set (@available_read_sets) {
+        my $prior_event_id = undef;
+        foreach my $command_class ( @sub_command_classes ) {
+            my $command = $command_class->create(
+                                                 model_id => $self->model_id,
+                                                 #should be read_set_id but still uses old name
+                                                 run_id => $read_set->id,
+                                                 prior_event_id => $prior_event_id,
+                                                 parent_event_id => $self->id,
+                                             );
+            $command->parent_event_id($self->id);
+            $command->event_status('Scheduled');
+            $command->retry_count(0);
+            $prior_event_id = $command->id;
+        }
+        $last_event_id = $prior_event_id;
+    }
+
+    my $data_directory = $model->data_directory;
+    unless (-e $data_directory) {
+        unless($self->create_directory($data_directory)) {
+            $self->error_message("Failed to create directory '$data_directory'");
+            return;
+        }
+    }
+    my $assembler = Genome::Model::Command::Build::Assembly::Assemble->create(
+                                                                              model_id => $self->model_id,
+                                                                              prior_event_id => $last_event_id,
+                                                                              parent_event_id => $self->id,
+                                                                          );
+    $assembler->parent_event_id($self->id);
+    $assembler->event_status('Scheduled');
+    $assembler->retry_count(0);
+    $last_event_id= $assembler->id;
+
+    return 1;
+}
+
+
+sub _get_sub_command_class_name{
+  return __PACKAGE__; 
+}
+
+1;
+
---
+++ Genome/Model/Command/Build/Assembly/Assemble.pm
@@ -0,0 +1,53 @@
+package Genome::Model::Command::Build::Assembly::Assemble;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::Model::Command::Build::Assembly::Assemble {
+    is => 'Genome::Model::Event',
+};
+
+sub bsub_rusage {
+    return '';
+}
+
+sub sub_command_sort_position { 40 }
+
+sub help_brief {
+    "assemble a genome"
+}
+
+sub help_synopsis {
+    return <<"EOS"
+genome-model build mymodel 
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+One build of a given assembly model.
+EOS
+}
+
+
+sub execute {
+    my $self = shift;
+
+    $DB::single = $DB::stopper;
+
+    my $model = $self->model;
+    unless (Genome::Model::Tools::454::Newbler::RunProject->execute(
+                                                                    test => $model->test,
+                                                                    dir => $model->assembly_directory,
+                                                                )) {
+        $self->error_message('Failed to run assembly project '. $model->assembly_directory);
+        return;
+    }
+    return 1;
+}
+
+
+1;
+
