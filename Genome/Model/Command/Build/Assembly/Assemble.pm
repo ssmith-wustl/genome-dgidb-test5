e0dc50122ca94c4f80e3a7b6c7dee12df6f2953c
jwalker <jwalker@genome.wustl.edu>
2008-08-20 14:38:10 +0000

    create an abstract class for assembling a project with a subclass for the newbler assembler
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37709 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Assembly/Assemble.pm
+++ Genome/Model/Command/Build/Assembly/Assemble.pm
@@ -4,51 +4,39 @@ use strict;
 use warnings;
 
 use above "Genome";
+use Command;
 
 class Genome::Model::Command::Build::Assembly::Assemble {
-    is => 'Genome::Model::Event',
+    is_abstract => 1,
+    is => ['Genome::Model::Event'],
 };
 
-sub bsub_rusage {
-    return '';
-}
-
-sub sub_command_sort_position { 40 }
+sub sub_command_sort_position { 10 }
 
 sub help_brief {
-    "assemble a genome"
+    "assemble the reads in a project"
 }
 
 sub help_synopsis {
     return <<"EOS"
-genome-model build mymodel 
+    genome-model build assembly assemble --model-id 5 --read-set-id 10
 EOS
 }
 
 sub help_detail {
     return <<"EOS"
-One build of a given assembly model.
+This command is launched automatically by build assembly.
+
+It delegates to the appropriate sub-command according to
+the assembler.
 EOS
 }
 
-
-sub execute {
-    my $self = shift;
-
-    $DB::single = $DB::stopper;
-
-    my $model = $self->model;
-    my $run_project = Genome::Model::Tools::454::Newbler::RunProject->create(
-                                                                             test => $model->test,
-                                                                             dir => $model->assembly_directory,
-                                                                         );
-    unless($run_project->execute) {
-        $self->error_message('Failed to run assembly project '. $model->assembly_directory);
-        return;
-    }
-    return 1;
+sub command_subclassing_model_property {
+    return 'assembler';
 }
 
+sub should_bsub { 1;}
 
 1;
 
---
+++ Genome/Model/Command/Build/Assembly/Assemble/Newbler.pm
@@ -0,0 +1,54 @@
+package Genome::Model::Command::Build::Assembly::Assemble::Newbler;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::Model::Command::Build::Assembly::Assemble::Newbler {
+    is => 'Genome::Model::Command::Build::Assembly::Assemble',
+};
+
+sub bsub_rusage {
+    return '';
+}
+
+sub sub_command_sort_position { 40 }
+
+sub help_brief {
+    "assemble a genome"
+}
+
+sub help_synopsis {
+    return <<"EOS"
+genome-model build assembly assemble
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+One build of a given assembly model.
+EOS
+}
+
+
+sub execute {
+    my $self = shift;
+
+    $DB::single = $DB::stopper;
+
+    my $model = $self->model;
+    my $run_project = Genome::Model::Tools::454::Newbler::RunProject->create(
+                                                                             test => $model->test,
+                                                                             dir => $model->assembly_directory,
+                                                                         );
+    unless($run_project->execute) {
+        $self->error_message('Failed to run assembly project '. $model->assembly_directory);
+        return;
+    }
+    return 1;
+}
+
+
+1;
+
