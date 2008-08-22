acc7cf97f7e4497071b62796209b55156d7158b0
dlarson <dlarson@genome.wustl.edu>
2008-08-22 20:17:52 +0000

    Added in validation based sensitivity and sensitivity measurements
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37827 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Snp/Evaluation.pm
+++ Genome/Model/Tools/Snp/Evaluation.pm
@@ -15,7 +15,13 @@ class Genome::Model::Tools::Snp::Evaluation {
     { 
         type => 'String',
         is_optional => 0,
-        doc => "input file of snp in the experimental metrics format",
+        doc => "input file of snps in the experimental metrics format",
+    },
+    original_snp_file => 
+    { 
+        type => 'String',
+        is_optional => 1,
+        doc => "Input file of snps in the experimental metrics format. Used to compare the metrics of a filtered snp file to it's parent. Required for calculation of database specificity and sensitivity",
     },
     gold_snp_file =>
     {
@@ -79,6 +85,42 @@ class Genome::Model::Tools::Snp::Evaluation {
         is_optional => 1,
         default => 0,
     },        
+    total_database_germline =>
+    {
+        type => 'Integer',
+        is_optional => 1,
+        default => 0,
+    },        
+    total_database_wildtype =>
+    {
+        type => 'Integer',
+        is_optional => 1,
+        default => 0,
+    },        
+    total_database_somatic =>
+    {
+        type => 'Integer',
+        is_optional => 1,
+        default => 0,
+    },        
+    total_database_present_wildtype =>
+    {
+        type => 'Integer',
+        is_optional => 1,
+        default => 0,
+    },        
+    total_database_present_germline =>
+    {
+        type => 'Integer',
+        is_optional => 1,
+        default => 0,
+    },        
+    total_database_present_somatic =>
+    {
+        type => 'Integer',
+        is_optional => 1,
+        default => 0,
+    },        
     REFDIR =>
     {
         #This is the path to the reference sequence used for aligning the model
@@ -118,6 +160,10 @@ sub execute {
         $self->error_message("Snps file is not a file: " . $self->snp_file);
         return;
     }
+    if(defined($self->original_snp_file) && !(-f $self->original_snp_file)) {
+        $self->error_message("Original SNP file is not a file: " . $self->original_snp_file);
+        return;
+    }
     unless(-f $self->gold_snp_file) {
         $self->error_message("Gold snp file is not a file: " . $self->gold_snp_file);
         return;
@@ -127,11 +173,24 @@ sub execute {
         return;
     }
 
+    if(defined($self->original_snp_file) && !(-f $self->dbsnp_file)) {
+        $self->error_message("dbSNP file is not a file: " . $self->dbsnp_file);
+        return;
+    }
+
     #Check and open filehandles
     my $snp_fh=IO::File->new($self->snp_file);
     unless($snp_fh) {
         $self->error_message("Failed to open filehandle for: " .  $self->snp_file );
         return;
+    }
+    my $original_fh;
+    if(defined($self->original_snp_file)) {
+        $original_fh=IO::File->new($self->original_snp_file);
+        unless($original_fh) {
+            $self->error_message("Failed to open filehandle for: " .  $self->original_snp_file );
+            return;
+        }
     }
     my $gold_fh=IO::File->new($self->gold_snp_file);
     unless($gold_fh) {
@@ -152,6 +211,7 @@ sub execute {
         return;
     }
 
+
     my ($gold_het_hash_ref, $gold_ref_hash_ref) = $self->create_gold_snp_hashes($gold_fh);
     close($gold_fh);
     
@@ -160,14 +220,90 @@ sub execute {
         return;
     }
 
-    #Do the calculations
+    my ($validation_db_hash_ref) = $self->create_db_validation_hash();
+    #calculate the metrics on the primary file
+
+    #set up temporary values
+    my ($total_snp_positions, 
+        $total_concordant_positions, 
+        $total_gold_het_concordant_snps, 
+        $total_gold_hom_ref_concordant_snps, 
+        $total_database_present_wildtype, 
+        $total_database_present_germline, 
+        $total_database_present_somatic) = 
+    $self->calculate_metrics($snp_fh,$dbsnp_hash_ref,$gold_het_hash_ref,$gold_ref_hash_ref,$validation_db_hash_ref);
+
+    #Set all the class variables for the primary file
+    $self->total_concordant_positions($total_concordant_positions);
+    $self->total_snp_positions($total_snp_positions);
+    $self->total_gold_hom_ref_concordant_snps($total_gold_hom_ref_concordant_snps);
+    $self->total_gold_het_concordant_snps($total_gold_het_concordant_snps);
+
+    $self->total_database_present_somatic($total_database_present_somatic);
+    $self->total_database_present_wildtype($total_database_present_wildtype);
+    $self->total_database_present_germline($total_database_present_germline);
+
+    if($self->print) {
+        printf("Evaluation of the SNP File\n");
+        $self->print_gold_and_dbsnp($total_concordant_positions,$total_snp_positions,$total_gold_het_concordant_snps, $self->total_gold_heterozygote_snps, $total_gold_hom_ref_concordant_snps, $self->total_gold_homozygous_ref_positions);
+        printf("There were %i validated somatic SNVs detected out of %i total validated somatic SNVs in the database\n", $self->total_database_present_somatic,$self->total_database_somatic);
+    }
+    #Calculate metrics on the original file
+    if(defined($original_fh)) {
+
+        my ($org_total_snp_positions, 
+            $org_total_concordant_positions, 
+            $org_total_gold_het_concordant_snps, 
+            $org_total_gold_hom_ref_concordant_snps, 
+            $org_total_database_present_wildtype, 
+            $org_total_database_present_germline, 
+            $org_total_database_present_somatic) = 
+        $self->calculate_metrics($original_fh, $dbsnp_hash_ref,$gold_het_hash_ref,$gold_ref_hash_ref,$validation_db_hash_ref); 
+
+        #set class variables for sensitivity and specificity
+        $self->total_database_somatic( $org_total_database_present_somatic );
+        $self->total_database_germline( $org_total_database_present_germline );
+        $self->total_database_wildtype( $org_total_database_present_wildtype );
+
+
+        if($self->print) {
+            printf("\nEvaluation of the Original SNP File\n");
+            $self->print_gold_and_dbsnp($org_total_concordant_positions,$org_total_snp_positions,$org_total_gold_het_concordant_snps, $self->total_gold_heterozygote_snps, $org_total_gold_hom_ref_concordant_snps, $self->total_gold_homozygous_ref_positions);
+
+            #print sensitivity and specificity
+            printf("\nEvaluation of the SNP File based on database SNPs present in the Original SNP file\n");
+
+            printf("There were %i validated SNVs detected out of %i total validated SNVs in the original file for a sensitivity of %0.2f%%\n", $self->total_database_present_somatic + $self->total_database_present_germline, $org_total_database_present_somatic + $self->total_database_germline,(100 * ($self->total_database_present_somatic+$self->total_database_present_germline) / ($self->total_database_somatic+$self->total_database_germline)));
+
+            printf("There were %i validated false positive SNVs detected out of %i total validated false positive SNVs in the database for a specificity of %0.2f%%\n", $self->total_database_present_wildtype,$self->total_database_wildtype,100 * (1 - $self->total_database_present_wildtype / $self->total_database_wildtype));
+        }
+    }
+
+    return 1;
+}
+
     
+
+
+1;
+
+sub help_detail {
+    "This module calculates the dbSNP concordance and False Negative rate of a snp file. Useful for evaluating decision trees"
+}
+
+sub calculate_metrics {
+    my ($self,$snp_fh,$dbsnp_hash_ref, $gold_het_hash_ref, $gold_ref_hash_ref, $validation_db_hash_ref) = @_;
+
     #set up temporary values
     my $total_gold_het_concordant_snps;
     my $total_snp_positions;
     my $total_gold_hom_ref_concordant_snps;
     my $total_concordant_positions;
-    
+
+    my $total_database_present_germline;
+    my $total_database_present_wildtype;
+    my $total_database_present_somatic;
+
     #skip header
     $snp_fh->getline;
 
@@ -175,11 +311,28 @@ sub execute {
     while(my $line = $snp_fh->getline) {
         next if($line =~ /chromosome/); #Skip experimental metrics headers if included because of say cat
         my ($chr,$pos,$ref,$var) = split /,\s*/, $line; #handle both types of experimental metrics formats
+
         if($self->exclude_y) {
             next if($chr eq 'Y'); #female patient these are BS
         }
 
         next if($ref eq ' ' || $ref eq '' || $ref eq 'N'); #skip 'SNPs' where the reference is N or non-existent
+
+        #Check db status
+        if(exists($validation_db_hash_ref->{$chr}{$pos})) { 
+            if( $validation_db_hash_ref->{$chr}{$pos} eq 'G') {
+                $total_database_present_germline++;
+            }
+            if( $validation_db_hash_ref->{$chr}{$pos} eq 'WT') {
+                $total_database_present_wildtype++;
+            }
+            if( $validation_db_hash_ref->{$chr}{$pos} eq 'S') {
+                $total_database_present_somatic++;
+            }
+
+        }
+
+
         #If there is a het SNP site, it is possible that there are two lines in the metrics file
         #corresponding to that position. We want to check both but only count the position once
         #Check that the genotypes match
@@ -207,32 +360,17 @@ sub execute {
             }
         }
     }
+    return ($total_snp_positions, $total_concordant_positions, $total_gold_het_concordant_snps, $total_gold_hom_ref_concordant_snps, $total_database_present_wildtype, $total_database_present_germline, $total_database_present_somatic);
 
-    #Set all the class variables
-    $self->total_concordant_positions($total_concordant_positions);
-    $self->total_snp_positions($total_snp_positions);
-    $self->total_gold_hom_ref_concordant_snps($total_gold_hom_ref_concordant_snps);
-    $self->total_gold_het_concordant_snps($total_gold_het_concordant_snps);
-
-    if($self->print) {
-        printf("There were %i positions in dbSNP out of %i total SNP positions for a dbSNP concordance of %0.2f%%\n", $self->total_concordant_positions, $self->total_snp_positions, ($self->total_concordant_positions/$self->total_snp_positions)*100);
-
-        printf("There were %i heterozygous Gold SNPs detected out of %i total for a false negative rate of %0.2f%%\n", $self->total_gold_het_concordant_snps, $self->total_gold_heterozygote_snps, (1 - ($self->total_gold_het_concordant_snps/$self->total_gold_heterozygote_snps)) * 100);
-
-        printf("There were %i homozygous reference Gold SNPs detected out of %i total for a false positive rate of %0.2f%%\n", $self->total_gold_hom_ref_concordant_snps, $self->total_gold_homozygous_ref_positions, ($self->total_gold_hom_ref_concordant_snps/$self->total_gold_homozygous_ref_positions) * 100);
-
-    }
-
-    return 1;
 }
 
-    
+sub print_gold_and_dbsnp {
+    my ($self, $concordant_positions, $snp_positions,  $gold_het_concordant, $gold_het_total, $gold_ref_concordant, $gold_ref_total) = @_; 
+    printf("There were %i positions in dbSNP out of %i total SNP positions for a dbSNP concordance of %0.2f%%\n", $concordant_positions, $snp_positions, ($concordant_positions/$snp_positions)*100);
 
+    printf("There were %i heterozygous Gold SNPs detected out of %i total for a false negative rate of %0.2f%%\n", $gold_het_concordant, $gold_het_total, (1 - ($gold_het_concordant/$gold_het_total)) * 100);
 
-1;
-
-sub help_detail {
-    "This module calculates the dbSNP concordance and False Negative rate of a snp file. Useful for evaluating decision trees"
+    printf("There were %i homozygous reference Gold SNPs detected out of %i total for a false positive rate of %0.2f%%\n", $gold_ref_concordant, $gold_ref_total, ($gold_ref_concordant/$gold_ref_total) * 100);
 }
 
 #Functions to create hashes of data
@@ -247,7 +385,7 @@ sub create_dbSNP_hash {
 
     #read in header
     my $header = $dbsnp_fh->getline;
-    
+
     my %dbsnp_at;
     while(my $line = $dbsnp_fh->getline) {
         chomp $line;
@@ -379,4 +517,98 @@ sub create_gold_snp_hashes {
     $self->total_gold_homozygous_ref_positions($total_gold_homozygous_ref_positions);
 
     return (\%heterozygous_snp_at,\%reference_snp_at);
+}
+
+#In my neverending quest to use as many hashes as possible...
+#This creates a hash of the db results
+#This shouldn't suck up too much memory unless we validate a whole lotta crap
+sub create_db_validation_hash {
+    my ($self) = @_;
+    my @variants = Genome::VariantReviewDetail->get(variant_type => 'S');
+    my %validated_status_for;
+
+    my $total_database_wildtype;
+    my $total_database_germline;
+    my $total_database_somatics;
+
+    my %rank = ('S' => 0,
+        'G' => 1,
+        'WT' => 2,
+        'A' => 3,
+        'V' => 3,
+        'O' => 3,
+        'LQ' => 4,
+        'X' => 4,
+        'NC' => 5,
+    );
+
+    foreach my $variant (@variants) {
+        my $chr = $variant->chromosome;
+        my $pos = $variant->begin_position;
+        my $status = $variant->somatic_status;
+        next if(!defined($chr) || !defined($pos));
+
+        next if(!defined($status));
+
+        #The following handles two levels of discrepancy
+        #TODO make it recursive so it can handle arbitrary levels
+        if(!exists($validated_status_for{$chr}{$pos})) {
+            my ($level1_discrep) = $status =~ /^DISCREPANCY\((.*)/;
+            my $level2_discrep;
+            if(defined($level1_discrep)) {
+                ($level2_discrep) = $level1_discrep =~ /^DISCREPANCY\((.*)/;
+            }
+            if(defined($level2_discrep)) {
+                my @status = split /:/, $level2_discrep;
+                map {s/^\s*(\S*)\s*$/$1/} @status;
+                my $new_status = 'NC';
+                foreach my $discr_status (@status) {
+                    if(exists($rank{$discr_status}) && $rank{$discr_status} < $rank{$new_status} ) {
+                        $new_status = $discr_status;
+                    }
+                }
+
+                $validated_status_for{$chr}{$pos} = $new_status;
+
+            }
+            elsif(defined($level1_discrep)) {
+                my @status = split /:/, $level1_discrep;
+                map {s/^\s*(\S*)\s*$/$1/} @status;
+                my $new_status = 'NC';
+                foreach my $discr_status (@status) {
+                    unless(defined($rank{$discr_status})) {
+                        $self->error_message("Unexpected status: '$discr_status'"); #quotes for whitespace visualization
+                    }
+                    if($rank{$discr_status} < $rank{$new_status} ) {
+                        $new_status = $discr_status;
+                    }
+                }
+                $validated_status_for{$chr}{$pos} = $new_status;
+            }
+            else {
+                $validated_status_for{$chr}{$pos} = $status;
+            }
+        }
+        else {
+            #check and see if it is an improvement over NC or LQ
+            if(uc($status) eq 'G' || uc($status) eq 'WT' || uc($status) eq 'S') {
+                $validated_status_for{$chr}{$pos} = $status;
+            }
+        }
+        #At this point, assuming the database has only 1 entry per position, we have the final status
+        if($validated_status_for{$chr}{$pos} eq 'G') {
+            $total_database_germline++;
+        }
+        if($validated_status_for{$chr}{$pos} eq 'S') {
+            $total_database_somatics++;
+        }
+        if($validated_status_for{$chr}{$pos} eq 'WT') {
+            $total_database_wildtype++;
+        }
+    }
+    #Set class variables
+    #$self->total_database_wildtype($total_database_wildtype);
+    $self->total_database_somatic($total_database_somatics);
+    #$self->total_database_germline($total_database_germline);
+    return \%validated_status_for;
 }
