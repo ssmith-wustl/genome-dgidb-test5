063f3cfc256b606bf514c8a847c1ae9d1eddbbda
mjohnson <mjohnson@genome.wustl.edu>
2008-08-27 20:01:13 +0000

    Minor decrufting, changed some references to 'mgap' to 'pap'
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37946 127c0c0e-971a-0410-ad48-868c1a942d8d

--- PAP/Command.pm
+++ PAP/Command.pm
@@ -7,20 +7,28 @@ use warnings;
 use PAP;
 use Command;
 
+use File::Temp;
+
+
 class PAP::Command {
-    is => ['Command'],
+    is           => ['Command'],
     english_name => 'pap command',
 };
 
 sub command_name {
-    my $self = shift;
+
+    my $self  = shift;
     my $class = ref($self) || $self;
-    return 'mgap' if $class eq __PACKAGE__;
+
+
+    return 'pap' if $class eq __PACKAGE__;
     return $self->SUPER::command_name(@_);
+    
 }
 
 sub help_brief {
     "modularized commands for testing Workflow"
 }
+
 
 1;
