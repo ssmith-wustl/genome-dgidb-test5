b494ee22e3d5a970f4538e024734b69ed3271698
mjohnson <mjohnson@genome.wustl.edu>
2008-08-27 19:58:34 +0000

    General cleanup and test enhancement.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37943 127c0c0e-971a-0410-ad48-868c1a942d8d

--- PAP/Command/KEGGScan.pm
+++ PAP/Command/KEGGScan.pm
@@ -10,31 +10,32 @@ use Workflow;
 use Bio::Seq;
 use Bio::SeqIO;
 use Bio::SeqFeature::Generic;
-use File::Slurp;
-use File::Temp qw/ tempdir tempfile /;
-use Cwd;
-use IPC::Run;
 
 use English;
+use File::Basename;
+use File::chdir;
+use File::Temp;
+use IO::File;
+use IPC::Run;
 
 
 class PAP::Command::KEGGScan {
     is  => ['PAP::Command'],
     has => [
-        fasta_file      => { 
-                            is => 'SCALAR', 
-                            doc => 'fasta file name'            
-                           },
-        bio_seq_feature => { 
-                            is          => 'ARRAY',  
-                            is_optional => 1,
-                            doc         => 'array of Bio::Seq::Feature' 
-                           },
-        working_directory => {
-                              is => 'SCALAR',
-                              doc => 'working directory',
-                              is_optional => 1,
+        fasta_file        => { 
+                              is  => 'SCALAR', 
+                              doc => 'fasta file name',            
                              },
+        working_directory => {
+                              is          => 'SCALAR',
+                              is_optional => 1,
+                              doc         => 'analysis program working directory',
+                             },
+        bio_seq_feature   => { 
+                              is          => 'ARRAY',  
+                              is_optional => 1,
+                              doc         => 'array of Bio::Seq::Feature', 
+                           },
     ],
 };
 
@@ -65,32 +66,49 @@ sub execute {
     my $self = shift;
 
 
-    my $fasta_file  = $self->fasta_file();
-    # this incarnation of keggscan should be deployed.
+    ##FIXME:  This should not be hardcoded.  At least not here.  
     my @keggscan_command = (
                             '/gsc/scripts/gsc/annotation/KEGGscan_KO.new.070125',
                            );
-    my ($kegg_stdout, $kegg_stderr) = ("test.out","test.err");
-    # should create tempdir, move into it.
-    my $tmpdir = tempdir("keggscanXXXXXX"); # CLEANUP=>1???
-    my $current_dir = getcwd;
-    $self->working_directory($current_dir);
-    chdir($current_dir . "/" . $tmpdir);
-    $self->create_kscfg();
+    
+    my ($kegg_stdout, $kegg_stderr);
+    
+    $self->working_directory(
+                             File::Temp::tempdir(
+                                                 'PAP_keggscan_XXXXXXXX',
+                                                 DIR     => '/gscmnt/temp212/info/annotation/PAP_tmp',
+                                                 CLEANUP => 1,
+                                             )
+                         );
 
-    IPC::Run::run(
-                   \@keggscan_command,
-                   \undef,
-                   '>',
-                   \$kegg_stdout,
-                   '2>',
-                   \$kegg_stderr,
-                 );
+    my $fasta_file = $self->fasta_file();
 
-    # cleanup, move out of temp dir
-    $self->parse_kegg();
-    $self->cleanup();
-    chdir($current_dir);
+    ## We're about to screw with the current working directory.
+    ## Thusly, we must fixup the fasta_file property if it 
+    ## contains a relative path.  
+    unless ($fasta_file =~ /^\//) {
+        $fasta_file = join('/', $CWD, $fasta_file);
+        $self->fasta_file($fasta_file);
+    }
+    
+    $self->write_config_file();
+
+    {
+    
+        local $CWD = $self->working_directory();
+
+        IPC::Run::run(
+                     \@keggscan_command,
+                     \undef,
+                     '>',
+                     \$kegg_stdout,
+                     '2>',
+                     \$kegg_stderr,
+                     );
+                     
+    }
+    
+    $self->parse_result();
 
     return 1;
 
@@ -110,72 +128,116 @@ KEGGscan needs a configuration file with these items:
 
 =cut
 
-sub create_kscfg
-{
+sub write_config_file {
+
     my $self = shift;
 
-# need to output
+
+    ##FIXME:  This probably should not be undef.  This should probably be an input property.
     my $species = undef;
+
+    ##FIXME:  Hardcoded path.
     my $subjectpath = "/gscmnt/233/analysis/sequence_analysis/species_independant/jmartin/hgm.website/KEGG/KEGG_release_41/genes.v41.faa";
-    my $queryfasta = $self->fasta_file;
-    unless($queryfasta =~ /^\//x)
-    {
-        $queryfasta = $self->working_directory() . "/". $queryfasta;
-    }
-    my $bladeload = 1;
-    my $keggrel = "RELEASE-41";
+    
+    my $query_fasta = $self->fasta_file();
+    my $working_dir = $self->working_directory();
+
+    my $bladeload  = 40;
+    my $keggrel    = "RELEASE-41";
 
     my @config = (
                   qq(SpeciesName\t"default"\n),
-                  qq(QueryFastaPath\t"$queryfasta"\n),
+                  qq(QueryFastaPath\t"$query_fasta"\n),
                   qq(SubjectFastaPath\t"$subjectpath"\n),
                   qq(QuerySeqType\t"CONTIG"\n),
                   qq(Queue\t"long"\n),
                   qq(BladeLoad\t"$bladeload"\n),
                   qq(KeggRelease\t"$keggrel"\n),
                  );
-    write_file('KS.cfg', @config);
+
+    my $cfg_fh = IO::File->new();
+    $cfg_fh->open(">$working_dir/KS.cfg") or die "Can't open '$working_dir/KS.cfg': $OS_ERROR";
+
+    print $cfg_fh @config;
+
+    $cfg_fh->close();
+    
     return;
+    
 }
 
+sub parse_result {
 
-sub cleanup
-{
     my $self = shift;
-    # get rid of crap here.
-    return;
-}
 
-sub parse_kegg
-{
-    my $self = shift;
-    my $keggout;
-    my @lines = read_file($keggout);
-    chomp;
-    my $feat = undef;
-    my $a = Bio::Annotation::Collection->new;
-    foreach my $l (@lines)
-    {
-        my ($junk,$gene,$code, $eVal,$junk2,$desc, $junk3,$ko)
-            = split(/\t/x,$l);
-        $gene =~ s/\(.*//x;
-        unless(defined($feat))
-        {
-            $feat = new Bio::SeqFeature::Generic( -seq_id => $gene );
+
+    my $output_fn = join('.', 'KS-OUTPUT', File::Basename::basename($self->fasta_file()));
+    $output_fn = join('/', $self->working_directory(), $output_fn, 'REPORT-top.ks');
+    
+    my $output_fh = IO::File->new();
+    
+    $output_fh->open("$output_fn") or die "Can't open '$output_fn': $OS_ERROR";
+
+    my @features = ( );
+    
+    while (my $line = <$output_fh>) {
+
+        my @fields = split /\t/, $line;
+        
+        my (
+            $ec_number,
+            $gene_name,
+            $hit_name,
+            $e_value,
+            $description,
+            $orthology
+           ) = @fields[1,2,3,4,6,8];
+
+        ## The values in the third column should be in this form:
+        ## gene_name (N letters; record M)
+        ($gene_name) = split /\s+/, $gene_name; 
+
+        ## Some descriptions have EC numbers embedded in them.
+        ## Prat's original pipeline removed them.
+        ## The present goal is to match the output of that pipeline.
+        ## Thus, remove the EC numbers.
+        $description =~ s/\(EC .+\)//;
+        
+        my $feature = new Bio::SeqFeature::Generic(-display_name => $gene_name);
+
+        $feature->add_tag_value('kegg_evalue', $e_value);
+        $feature->add_tag_value('kegg_description', $description);
+        
+        my $gene_dblink = Bio::Annotation::DBLink->new(
+                                                       -database   => 'KEGG',
+                                                       -primary_id => $hit_name,
+                                                   );
+        
+        $feature->annotation->add_Annotation('dblink', $gene_dblink);
+
+        ## Sometimes there is no orthology identifier (value is literally 'none').
+        ## It is not unforseeable that it might also be blank/undefined.  
+        if (defined($orthology) && ($orthology ne 'none')) {
+        
+            my $orthology_dblink = Bio::Annotation::DBLink->new(
+                                                                -database   => 'KEGG',
+                                                                -primary_id => $orthology,
+                                                            );
+            
+            $feature->annotation->add_Annotation('dblink', $orthology_dblink);
+                
         }
-        $desc =~ s/\(EC .+\)//;  # removing the EC number???
-        if($ko eq "none")
-        {
-            $ko = "";
-        }
-        # create the bio seq annotation/feature object
-        my $keggstring = $code . " " . $eVal . " " . $desc . " " . $ko;
-        $feat->add_Annotation("KEGG", $keggstring);
+        
+        push @features, $feature;
+        
     }
-    # put $feat into $self->bio_seq_feature 
-    $self->bio_seq_feature( [ $feat ] );
+
+    $output_fh->close();
+    
+    $self->bio_seq_feature( \@features );
 
     return;
+    
 }
  
 1;
--- PAP/t/KEGGScan.t
+++ PAP/t/KEGGScan.t
@@ -6,15 +6,16 @@ use Workflow;
 use Bio::Seq;
 use Bio::SeqIO;
 
+use Cwd;
 use File::Temp;
-use Test::More tests => 5;
+use Test::More tests => 287;
 
 BEGIN {
     use_ok('PAP::Command');
     use_ok('PAP::Command::KEGGScan');
 }
 
-my $command = PAP::Command::KEGGScan->create('fasta_file' => 'data/B_coprocola.fasta');
+my $command = PAP::Command::KEGGScan->create('fasta_file' => 'data/B_coprocola.chunk.fasta');
 isa_ok($command, 'PAP::Command::KEGGScan');
 
 ok($command->execute());
@@ -22,3 +23,22 @@ ok($command->execute());
 my $ref = $command->bio_seq_feature();
 
 is(ref($ref), 'ARRAY');
+
+foreach my $feature (@{$ref}) {
+
+    isa_ok($feature, 'Bio::SeqFeature::Generic');
+     
+    ok($feature->has_tag('kegg_evalue'));
+    ok($feature->has_tag('kegg_description'));
+
+    my $annotation_collection = $feature->annotation();
+
+    isa_ok($annotation_collection, 'Bio::Annotation::Collection');
+
+    foreach my $annotation ($annotation_collection->get_Annotations()) {
+
+        isa_ok($annotation, 'Bio::Annotation::DBLink');
+    
+    }
+    
+}
