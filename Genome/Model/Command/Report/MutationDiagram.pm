f3913d4b52eb7266ee644faa1d151ee4917c07f6
ssmith <ssmith@genome.wustl.edu>
2008-08-28 20:27:43 +0000

    perltidy
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@37996 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Report/MutationDiagram.pm
+++ Genome/Model/Command/Report/MutationDiagram.pm
@@ -4,19 +4,17 @@ package Genome::Model::Command::Report::MutationDiagram;
 use strict;
 use warnings;
 
-use above "Genome";
+use Genome;
 use MG::MutationDiagram;
-#use Data::Dumper;
-#$Data::Dumper::Indent = 1;
 
-UR::Object::Type->define(
+class Genome::Model::Command::Report::MutationDiagram {
     class_name => __PACKAGE__,
     is => 'Command',
     has => [
-						'maf'   => { type => 'String',  doc => "MAF file"},
-						'genes'   => { type => 'String',  doc => "comma separated list of (hugo) gene names (uppercase)--default is ALL", is_optional => 1},
+        maf     => { type => 'String',  doc => "MAF file"},
+	genes   => { type => 'String',  doc => "comma separated list of (hugo) gene names (uppercase)--default is ALL", is_optional => 1},
     ],
-);
+};
 
 sub help_brief {
     "report mutations as a (svg) diagram"
@@ -24,8 +22,7 @@ sub help_brief {
 
 sub help_synopsis {
     return <<"EOS"
-genome-model report mutation-diagram  
-                    --maf my.maf
+genome-model report mutation-diagram  --maf my.maf
 EOS
 }
 
@@ -39,15 +36,17 @@ sub execute {
     $DB::single = $DB::stopper;
     my $self = shift;
     my $maf_file = $self->maf;
-		my $maf_obj = new MG::MutationDiagram(maf_file => $maf_file,
-																			hugos => $self->genes
-																		 );
-#		my $data = $maf_obj->Data();
-#		print Dumper($data);
+    my $maf_obj = new MG::MutationDiagram(
+        maf_file => $maf_file,
+        hugos => $self->genes
+    );
+    #use Data::Dumper;
+    #$Data::Dumper::Indent = 1;
+    # my $data = $maf_obj->Data();
+    # print Dumper($data);
     return 1;
 }
 
 
 1;
 
-__END__
