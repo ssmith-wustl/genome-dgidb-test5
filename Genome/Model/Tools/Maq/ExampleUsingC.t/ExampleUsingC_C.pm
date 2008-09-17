939d6f4a892f4e56fbb056da5e23b102eea4bb8d
ssmith <ssmith@genome.wustl.edu>
2008-09-17 05:12:31 +0000

    fix to use the common Inline path
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@38666 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Maq/ExampleUsingC.t/ExampleUsingC_C.pm
+++ Genome/Model/Tools/Maq/ExampleUsingC.t/ExampleUsingC_C.pm
@@ -1,7 +1,7 @@
 package Genome::Model::Tools::Maq::ExampleUsingC_C;
 
 use Genome::Model::Tools::Maq::Map::Utils;
-use Inline 'C' => 'Config' => @Genome::Model::Tools::Maq::MapUtils::CONFIG;
+use Inline 'C' => 'Config' => @Genome::Model::Tools::Maq::MapUtils::CONFIG, DIRECTORY => Genome::Inline::DIRECTORY();
 
 use strict;
 use warnings;
