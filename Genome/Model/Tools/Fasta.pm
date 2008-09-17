e3da8e50d18c9d67f02cbc2877d918b0a3799a31
ebelter <ebelter@genome.wustl.edu>
2008-09-17 17:03:40 +0000

    Fasta.pm - base for fasta tools Fasta/ - dir for fasta tools TrimQuality.pm - trims fasta and quality files
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@38680 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Fasta.pm
@@ -0,0 +1,106 @@
+package Genome::Model::Tools::Fasta;
+
+use strict;
+use warnings;
+
+use above 'Genome';
+
+class Genome::Model::Tools::Fasta {
+    is => 'Command',
+    is_abstract => 1,
+    has => [
+    fasta_file => {
+        type => 'String',
+        is_optional => 0,
+        doc => 'FASTA file. Quality file (if appropriate) will be named <fasta_file>\'.qual\'',
+    },
+    ],
+};
+
+use Data::Dumper;
+
+sub create { 
+    my $class = shift;
+
+    my $self = $class->SUPER::create;
+    $self->{_cwd} = Cwd::getcwd();
+    $self->fasta_file( Cwd::abs_path( $self->fasta_file ) );
+    my ($base, $directory) = File::Basename::fileparse( $self->fasta_file );
+    chdir $directory
+        or ( $self->error_message("Can't access directory ($directory): $!") and return );
+    $self->{_fasta_base} = $base;
+
+    return 1;
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    chdir $self->_cwd;
+    
+    return 1;
+}
+
+sub _cwd {
+    return shift->{_cwd};
+}
+
+sub _fasta_base {
+    return shift->{_fasta_base};
+}
+
+sub qual_base {
+    my $self = shift;
+
+    return sprintf('%s.qual', $self->_fasta_base);
+}
+
+sub qual_file {
+    my $self = shift;
+
+    return sprintf('%s.qual', $self->fasta_file);
+}
+
+1;
+
+=pod
+
+=head1 Name
+
+ModuleTemplate
+
+=head1 Synopsis
+
+=head1 Usage
+
+=head1 Methods
+
+=head2 
+
+=over
+
+=item I<Synopsis>
+
+=item I<Arguments>
+
+=item I<Returns>
+
+=back
+
+=head1 See Also
+
+=head1 Disclaimer
+
+Copyright (C) 2005 - 2008 Washington University Genome Sequencing Center
+
+This module is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY or the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+=head1 Author(s)
+
+B<Eddie Belter> I<ebelter@watson.wustl.edu>
+
+=cut
+
+#$HeadURL$
+#$Id$
+
---
+++ Genome/Model/Tools/Fasta/TrimQuality.pm
@@ -0,0 +1,85 @@
+package Genome::Model::Tools::Fasta::TrimQuality;
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+class Genome::Model::Tools::Fasta::TrimQuality {
+    is  => 'Genome::Model::Tools::Fasta',
+    has => [	 
+    min_trim_quality => {
+        is => 'Int',
+        doc => 'Minimum quality value cutoff (10)',
+        default => 10,
+    },
+    min_trim_length => {
+        is => 'Int',
+        doc => 'Minimum clipped read length (100)',
+        default => 100,
+    },		 
+    ],
+};
+
+use Data::Dumper;
+require File::Copy;
+
+sub help_brief {
+    return 'Trims by FASTA and Quality files by minimum quality and length';
+}
+
+sub help_detail { 
+    return <<EOS 
+EOS
+}
+
+sub execute {
+    my $self = shift;
+    ($self->error_message( 
+		      sprintf('Quality file (%s) does not exist.', $self->qual_file)
+		      )
+     and return)unless -s $self->qual_file;
+
+    my $command = sprintf(
+        'trim3 %s -m %s -q %s',# -x 10',
+        $self->_fasta_base,
+        $self->min_trim_length,
+        $self->min_trim_quality,
+    );
+
+    if ( system $command ) {
+        ($self->error_message("trim3 failed.") and return);
+    }
+
+    # Makes a <fasta>.clip and <fasta>.clip.qua, move to file names
+    # FASTA
+    my $fasta_bak = sprintf('%s.preclip', $self->_fasta_base);
+    File::Copy::copy($self->_fasta_base, $fasta_bak)
+        or ($self->error_message(sprintf('Can\'t copy %s to %s: %s', $self->_fasta_base, $fasta_bak, $!)) 
+	    and return);
+    unlink $self->_fasta_base;
+    my $fasta_clip = sprintf('%s.clip', $self->_fasta_base);
+    File::Copy::copy($fasta_clip, $self->_fasta_base)
+        or ($self->error_message( sprintf('Can\'t copy output file (%s) to %s: %s', $fasta_clip, $self->_fasta_base, $!) )
+	    and return);
+    unlink $fasta_clip;
+
+    # QUAL
+    my $qual_bak = sprintf('%s.preclip', $self->qual_base);
+    File::Copy::copy($self->qual_base, $qual_bak)
+        or ($self->error_message( sprintf('Can\'t copy %s to %s: %s', $self->qual_base, $qual_bak, $!) ) and return);
+    unlink $self->qual_base;
+    my $qual_clip = sprintf('%s.clip.qual', $self->_fasta_base);
+    File::Copy::copy($qual_clip, $self->qual_base)
+        or ($self->error_message( sprintf('Can\'t copy output qual file (%s) to %s: %s', $qual_clip, $self->qual_base, $!) ) and return);
+    unlink $qual_clip;
+
+    $self->warn_msg("No sequences made the quality and length cut.") unless -s $self->_fasta_base;
+
+    return 1;
+}
+
+1;
+
+#$HeadURL$
+#$Id$
