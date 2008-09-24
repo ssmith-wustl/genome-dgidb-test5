7e5c37267da84e11e4017ad33ac570bcf7354c87
jwalker <jwalker@genome.wustl.edu>
2008-09-24 21:34:49 +0000

    An inital method for checking read sets before queueing for genome modeling with processing profile.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39045 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/Assembly.pm
+++ Genome/ProcessingProfile/Assembly.pm
@@ -34,7 +34,23 @@ sub params_for_class {
     return @PARAMS;
 }
 
+sub instrument_data_is_applicable {
+    my $self = shift;
+    my $instrument_data_type = shift;
+    my $instrument_data_id = shift;
+    my $subject_name = shift;
 
+    my $lc_instrument_data_type = lc($instrument_data_type);
+    if ($self->sequencing_platform) {
+        unless ($self->sequencing_platform eq $lc_instrument_data_type) {
+            $self->error_message('The processing profile sequencing platform ('. $self->sequencing_platform
+                                 .') does not match the instrument data type ('. $lc_instrument_data_type);
+            return;
+        }
+    }
+
+    return 1;
+}
 
 1;
 
