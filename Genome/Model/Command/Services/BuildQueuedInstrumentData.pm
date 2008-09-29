eea8a2dc49c71490ce9760b146ec32fb716eb661
jwalker <jwalker@genome.wustl.edu>
2008-09-29 17:14:06 +0000

    This command will be called by a cron to create models when appropriate, add reads to genome model, and initiate the build commands for data in progress for pse 'queue instrument data for genome modeling'
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39159 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Services/BuildQueuedInstrumentData.pm
@@ -0,0 +1,102 @@
+package Genome::Model::Command::Services::BuildQueuedInstrumentData;
+
+use strict;
+use warnings;
+
+use Genome;
+use Genome::RunChunk;
+
+class Genome::Model::Command::Services::BuildQueuedInstrumentData {
+    is => 'Command',
+    has => [
+        ],
+};
+
+
+sub help_brief {
+    'Find all QueueInstrumentDataForGenomeModeling PSEs, create appropriate models, AddReads, and finally trigger Build on the model'
+}
+
+sub help_synopsis {
+    return <<'EOS'
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+EOS
+}
+
+sub execute {
+    my $self = shift;
+
+    $DB::single = $DB::stopper;
+
+    my $ps = GSC::ProcessStep->get(process_to => 'queue instrument data for genome modeling');
+    my @pses = GSC::PSE->get(
+                             ps_id => $ps->ps_id,
+                             pse_status => 'inprogress',
+                         );
+    my %model_ids;
+    foreach my $pse (@pses) {
+        my ($instrument_data_type) = $pse->added_param('instrument_data_type');
+        my ($instrument_data_id) = $pse->added_param('instrument_data_id');
+        my ($subject_type) = $pse->added_param('subject_type');
+        my ($subject_name) = $pse->added_param('subject_name');
+        my ($sample_name) = $pse->added_param('sample_name');
+        my ($library_name) = $pse->added_param('library_name');
+        my ($research_project_name) = $pse->added_param('research_project_name');
+        my ($processing_profile_name) = $pse->added_param('processing_profile_name');
+
+        my $pp = Genome::ProcessingProfile->get(name => $processing_profile_name);
+        unless ($pp) {
+            $self->error_message("Failed to get processing profile '$processing_profile_name' for inprogress pse ". $pse->pse_id);
+            next;
+        }
+        my $model = Genome::Model->get(
+                                        subject_name => $subject_name,
+                                        processing_profile_id => $pp->id,
+                                    );
+        unless ($model) {
+            my $model_name = $subject_name .'.'. $pp->name;
+            my $model_create = Genome::Model::Command::Create::Model->create(
+                                                                             model_name => $model_name,
+                                                                             subject_name => $subject_name,
+                                                                             processing_profile_name => $pp->name,
+                                                                             bare_args => [],
+                                                                         );
+            unless ($model_create->execute) {
+                $self->error_message("Failed to create model '$model_name'");
+                next;
+            }
+        }
+        $model = Genome::Model->get(
+                                    subject_name => $subject_name,
+                                    processing_profile_id => $pp->id,
+                                );
+        $model_ids{$model->id} = 1;
+        my $add_reads = Genome::Model::Command::AddReads->create(
+                                                                 read_set_id => $instrument_data_id,
+                                                                 model_id => $model->id,
+                                                             );
+        unless ($add_reads->execute) {
+            $self->error_message('Failed to execute add reads for model '. $model->id .' and read set '. $instrument_data_id);
+            next;
+        }
+    }
+    #Execute all the builds for models with new data
+    for my $model_id (keys %model_ids) {
+        my $build = Genome::Model::Command::Build->create(
+                                                          model_id => $model_id,
+                                                      );
+        unless ($build->execute) {
+            $self->error_message('Failed to execute build '. $build->id .' for model '. $model_id);
+            next;
+        }
+    }
+    return 1;
+}
+
+
+
+1;
