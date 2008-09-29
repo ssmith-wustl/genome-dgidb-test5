d551cea347ced493e5c34b25007cd92d727cbb48
dlarson <dlarson@genome.wustl.edu>
2008-09-29 14:10:29 +0000

    Added support for tree visualization
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39145 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/SeeFourFive/Tree.pm
+++ Genome/Model/Tools/SeeFourFive/Tree.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Genome;            
 use IO::File;
+use GraphViz;
+
 class Genome::Model::Tools::SeeFourFive::Tree {
     is => 'Command',
     has => [ 
@@ -192,6 +194,117 @@ sub tree_evaluation_function {
     return $src;
 }
 
+
+sub as_graphviz_obj {
+    my ($self,$graph,$root,$identifier,$lines) = @_;
+    unless($lines) {
+        $lines = $self->lines;
+    }  
+    chomp @$lines;
+    unless (@$lines) {
+        return "# no lines $self->{n}?";
+    }
+    my $src;
+    my %metric_names;
+    unless($graph) {
+        $graph = GraphViz->new();
+    }
+    $$identifier ||= 0;
+
+    #this may be ok, but perhaps a hash of each level of tree to figure out if we printed something
+    #at that depth before would allow us to use
+    #if/elsif correctly
+    my $tree_depth_count=0;
+    my @parents;
+    unless($root) {
+        $graph->add_node("ROOT");
+        @parents = ("ROOT");
+    }
+    else {
+        @parents = ($root);
+    }
+
+    for my $line (@$lines) {
+        #use tr to count how deep we are in the tree and remove the | so our previous
+        #regex works
+        my $current_depth= $line =~ tr/|/ /;
+
+        #three cases here.  
+        #Equal to current depth= if
+        #Greater than current depth: if
+        #less than current depth: output difference # of clsoe brackets
+
+        #my ($metric, $op, $value, $decision) = ($line =~ /\s+([\w\-]+)\s+(\S+)\s+(\S+)\s+:\s+(\S+)/);
+        my ($metric, $op, $value, $decision, $total_trials, $errors) = ($line =~ /\s*([\w\-]+)\s+(\S+)\s+([\S\w]+)\s*:\s*([^\s]*)\s*\(?([\d.]*)\/?([\d.]*)\)?/);
+        unless($metric && $op && (defined $value)) {
+            $self->error_message("Parsing failure on line: $line\n");
+            print "Metric: $metric\n";
+            print "OP: $op\n";
+            print "value: $value\n";
+            print "total trials: $total_trials\n";
+            print "ErrorS: $errors\n"; 
+            return;
+        }
+        if(defined $decision) {
+            unless(defined $total_trials && defined $errors) {
+                $self->error_message("a decision does not have metrics about its error rate: $line");
+                return;
+            }
+        }
+        if ($value =~ m/[A|G|C|T]/) {
+            $op = 'eq'; 
+            $value = "'$value'"; 
+        }
+        $op = '==' if $op eq '=';
+
+        ##protect us o lord from the evil aliens
+        if($current_depth >= $tree_depth_count) {
+            #NEW NODE
+            $graph->add_node($$identifier, label => "$metric $op $value");
+            if(@parents) {
+                $graph->add_edge($parents[-1] => $$identifier);
+            }
+            push @parents,$$identifier;
+
+            ++$$identifier;
+
+        }elsif($current_depth < $tree_depth_count) {
+            while ($tree_depth_count > $current_depth) {
+                pop @parents;
+                $tree_depth_count--;
+            }
+
+            $graph->add_node($$identifier, label => "$metric $op $value");
+            if(@parents) {
+                $graph->add_edge($parents[-1] => $$identifier);
+            }
+            push @parents, $$identifier;
+            ++$$identifier;
+        }
+        #having a [ implies a subtree
+        if($decision && ($decision !~ m/\[/)) { 
+            $graph->add_node($$identifier, label => "$decision");
+            if(@parents) {
+                $graph->add_edge($parents[-1] => $$identifier);
+                ++$$identifier;
+            }
+            $tree_depth_count--;
+            pop @parents; #b/c parent is leaf
+        }
+        elsif($decision) {
+            $decision =~ tr/[]//d;
+            ##$self->error_message( "Unsupported creation of graph with subtrees");
+            ##return;
+            #add in subtree
+            my $subtree_lines= $self->subtree_hashref_of_arrayrefs->{$decision};
+            my $temp_g = $self->as_graphviz_obj($graph,$parents[-1],$identifier,$subtree_lines); #should modify by reference
+            pop @parents;
+        }
+        $tree_depth_count=$current_depth;
+    }
+
+    return $graph;
+}
 
 
 
