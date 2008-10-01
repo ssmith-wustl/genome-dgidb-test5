eaa5a8a3a182963bc2d81cd4b463e1598ca3e7bc
jwalker <jwalker@genome.wustl.edu>
2008-10-01 18:55:33 +0000

    models no longer have sample_name, it is now subject_name
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39251 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/List/Models.pm
+++ Genome/Model/Command/List/Models.pm
@@ -14,7 +14,7 @@ class Genome::Model::Command::List::Models {
              is_constant => 1, 
             value => 'Genome::Model' 
         },
-        show => { default_value => 'id,name,sample_name,processing_profile_name' },
+        show => { default_value => 'id,name,subject_name,processing_profile_name' },
     ],
 };
 
