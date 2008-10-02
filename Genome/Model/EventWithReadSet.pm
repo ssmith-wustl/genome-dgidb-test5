17d2ea80f0eb8cb242b8c8acb94983349b3acb62
jwalker <jwalker@genome.wustl.edu>
2008-10-02 19:21:27 +0000

    move log directory into baseclass and switch from run_id to read_set_id.  Use read_set_link which is the new Genome::Model::ReadSet object
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39330 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/EventWithReadSet.pm
+++ Genome/Model/EventWithReadSet.pm
@@ -9,58 +9,57 @@ class Genome::Model::EventWithReadSet {
     is => 'Genome::Model::Event',
     is_abstract => 1,
     has => [
-        read_set            => { is => 'Genome::RunChunk', id_by => 'run_id', is_optional => 0, constraint_name => 'event_run' },
-        read_set_id         => { via => 'read_set', to => 'seq_id' }, # not really the fk currently (run_id), see below...        
-        read_set_name       => { via => 'read_set', to => 'full_name' },
-
-        # use the "read_set" terminology
-        run_id              => { is => 'NUMBER', len => 11, is_optional => 0, doc => 'the genome_model_run on which to operate', is_deprecated => 1 }, # don't use
-        run                 => { is => 'Genome::RunChunk', id_by => 'run_id', is_deprecated => 1 }, # use read_set
-
-        run_name            => { via => 'read_set' },
-        run_short_name      => { via => 'read_set', to => 'short_name' },
-        run_subset_name     => { via => 'read_set', to => 'subset_name' },
-        
-        library_name        => { via => 'read_set' },
-        sample_name         => { via => 'read_set' },
-        
-        alignment_directory => { via => 'model'},
-        read_set_alignment_directory  => {
-                                          calculate_from => ['alignment_directory','read_set'],
+            read_set       => { is => 'Genome::RunChunk', id_by => 'read_set_id', constraint_name => 'event_run' },
+            read_set_link  => { is => 'Genome::Model::ReadSet', id_by => ['model_id','read_set_id'] },
+            read_set_name  => { via => 'read_set', to => 'full_name' },
+            read_set_id    => {
+                               is => 'NUMBER',
+                               len => 11,
+                               doc => 'the id of thegenome_model_run on which to operate',
+                           },
+            run_name            => { via => 'read_set_link' },
+            run_short_name      => { via => 'read_set_link' },
+            run_subset_name     => { via => 'read_set_link', to => 'subset_name' },
+            library_name        => { via => 'read_set_link' },
+            sample_name         => { via => 'read_set_link' },
+            alignment_directory => { via => 'read_set_link'},
+            read_set_alignment_directory  => {
+                                          calculate_from => ['alignment_directory','read_set_link'],
                                           calculate => q|
                                               return sprintf('%s/%s/%s_%s',
                                                              $alignment_directory,
-                                                             $read_set->run_name,
-                                                             $read_set->subset_name,
-                                                             $read_set->seq_id,
+                                                             $read_set_link->run_name,
+                                                             $read_set_link->subset_name,
+                                                             $read_set_link->seq_id,
                                                             );
                                           |,
                             },
             #This is temporary so the 454 test runs without creating a bunch of lengthy blat alignments
             #Please fix once a standard testing scheme is developed
-        new_read_set_alignment_directory => {
-                                             calculate_from => ['alignment_directory','read_set'],
-                                             calculate => q|
+            new_read_set_alignment_directory => {
+                                                 calculate_from => ['alignment_directory','read_set_link'],
+                                                 calculate => q|
                                               return sprintf('%s/%s/%s/%s',
                                                              $alignment_directory,
-                                                             $read_set->sample_name,
-                                                             $read_set->run_name,
-                                                             $read_set->subset_name,
+                                                             $read_set_link->sample_name,
+                                                             $read_set_link->run_name,
+                                                             $read_set_link->subset_name,
                                                             );
                                           |,
-                                         },
+                                             },
 
         # deprecated
         read_set_directory  => {
-                                calculate_from => ['model','read_set'],
+                                calculate_from => ['model','read_set_link'],
                                 calculate => q|
                                     return sprintf('%s/runs/%s/%s',$model->data_directory,
-                                                                   $read_set->sequencing_platform,
-                                                                   $read_set->name);
+                                                                   $read_set_link->sequencing_platform,
+                                                                   $read_set_link->name);
                                 |,
                                 doc => 'Only keep this as long as we are moving old read data',
                                 is_deprecated => 1,
                             },
+        run                 => { is => 'Genome::RunChunk', id_by => 'read_set_id', is_deprecated => 1 }, # use read_set
     ],
     sub_classification_method_name => '_get_sub_command_class_name',
 };
@@ -77,7 +76,7 @@ sub _shell_args_property_meta {
     return grep {
             not (
                 $_->class_name eq __PACKAGE__
-                and $_->property_name !~ /(model_id|run_id)/
+                and $_->property_name !~ /(model_id|read_set_id)/
             )
         } shift->SUPER::_shell_args_property_meta(@_);
 }
@@ -94,14 +93,23 @@ sub invalid {
                                         );
     }
 
-    unless (Genome::RunChunk->get(id => $self->run_id)) {
+    unless (Genome::RunChunk->get(id => $self->read_set_id)) {
         push @tags, UR::Object::Tag->create(
                                             type => 'invalid',
-                                            properties => ['run_id'],
-                                            desc => "There is no genome run with id ". $self->run_id,
+                                            properties => ['read_set_id'],
+                                            desc => "There is no genome run with read_set_id ". $self->read_set_id,
                                         );
     }
     return @tags;
+}
+
+sub resolve_log_directory {
+    my $self = shift;
+    return sprintf('%s/logs/%s/%s',
+                   $self->model->latest_build_directory,
+                   $self->sequencing_platform,
+                   $self->run_name
+               );
 }
 
 1;
