8910308929c0e51c824b831b854d5c3b0e812dc7
ebelter <ebelter@genome.wustl.edu>
2008-10-02 15:46:12 +0000

    represents a model of a meta genomic subject
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39274 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/MetaGenomicComposition.pm
@@ -0,0 +1,47 @@
+package Genome::Model::MetaGenomicComposition;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::MetaGenomicComposition {
+    is => 'Genome::Model',
+    has => [
+    read_set_assignment_events =>{
+        is => 'Genome::Model::Command::Build::Assembly::AssignReadSetToModel',
+        is_many => 1,
+        reverse_id_by => 'model',
+        where => [ "event_type like" => 'genome-model build assembly assign-read-sets%'],
+        doc => 'each case of a read set being assigned to the model',
+    },
+    assembler => {
+        via => 'processing_profile',
+    },
+    read_origin => {
+        via => 'processing_profile',
+    },
+    ],
+};
+
+sub _test {
+    # Hard coded param for now
+    return 1;
+}
+
+sub _build_subclass_name {
+    return 'assembly';
+}
+
+sub _assembly_directory {
+    my $self = shift;
+    return $self->data_directory . '/assembly';
+}
+
+1;
+
+=pod
+=cut
+
+#$HeadURL$
+#$Id$
