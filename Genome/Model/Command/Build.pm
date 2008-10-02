c64d0a00888e5182f480771ca80a598f92247b05
jwalker <jwalker@genome.wustl.edu>
2008-10-02 19:25:10 +0000

    move all the workflow type logic out of the subclasses and the base class now controls the scheduling of jobs, executing of jobs, and the dependendcies for running the next build command
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39331 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build.pm
+++ Genome/Model/Command/Build.pm
@@ -11,16 +11,27 @@ class Genome::Model::Command::Build {
     table_name => 'GENOME_MODEL_BUILD',
     first_sub_classification_method_name => '_resolve_subclass_name',
     id_by => [
-        build_id                 => { is => 'NUMBER', len => 10, constraint_name => 'GMB_GME_FK' },
+        build_id                 => { is => 'NUMBER', len => 10, constraint_name => 'GMB_GME_FK' , is_optional => 1},
     ],
     has => [
         model                    => { is => 'Genome::Model', id_by => 'model_id', constraint_name => 'GMB_GMM_FK' },
         data_directory           => { is => 'VARCHAR2', len => 1000, is_optional => 1 },
+        auto_execute => {
+                         is => 'Boolean',
+                         doc => 'The build will execute genome-model run-jobs before completing',
+                         default_value => 1,
+                         is_transient => 1,
+                     },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
 };
 
+sub stages {
+    my $class = shift;
+    $class = ref($class) if ref($class);
+    die("Please implement stages in class '$class'");
+}
 
 sub command_subclassing_model_property {
     return 'build_subclass_name';
@@ -31,6 +42,191 @@ sub resolve_data_directory {
     my $model = $self->model;
     return $model->data_directory . '/build' . $self->id;
 }
+
+sub build_in_stages {
+    my $self = shift;
+
+    $self->data_directory($self->model->data_directory);
+    my @stages = $self->stages;
+    #Need to know when to return 0,1,2 or should subclass->execute handle this?
+    for my $stage_name (@stages) {
+        my $classes_method_name = $stage_name .'_job_classes';
+        my $objects_method_name = $stage_name .'_objects';
+
+        my @stage_classes = $self->$classes_method_name;
+        $self->_verify_existing_events(\@stage_classes);
+
+        my @objects = $self->$objects_method_name;
+        if (@objects) {
+            my @scheduled_objects = $self->_schedule_stage(\@stage_classes,\@objects);
+            return $self->_run_stage(@scheduled_objects);
+        }
+    }
+    return 1;
+}
+
+sub _verify_existing_events {
+    my $self = shift;
+    my $command_classes_ref = shift;
+
+    my $model = $self->model;
+    for my $command_class (@$command_classes_ref) {
+        if (ref($command_class) eq 'ARRAY') {
+            $self->_verify_existing_events($command_class);
+        } else {
+            my @events = $command_class->get( model_id => $model->id );
+            my @broke_events = grep {$_->event_status =~ /Scheduled|Running|Crashed|Failed/} @events;
+            if( @broke_events ) {
+                $self->fail('Found '. scalar(@broke_events) .' for class '. $command_class);
+            }
+        }
+    }
+    return 1;
+}
+
+sub _schedule_stage {
+    my $self = shift;
+    my $sub_command_classes_ref = shift;
+    my $objects_to_schedule = shift;
+
+    my @scheduled_commands;
+    foreach my $object (@$objects_to_schedule) {
+        my $object_class;
+        my $object_id;
+        if (ref($object)) {
+            $object_class = ref($object);
+            $object_id = $object->id;
+        } else {
+            $object_class = 'reference_sequence';
+            $object_id = $object;
+        }
+        $self->status_message('Scheduling for '. $object_class);
+        push @scheduled_commands, $self->_schedule_command_classes_for_object($object,$sub_command_classes_ref);
+    }
+    return @scheduled_commands;
+}
+
+sub _schedule_command_classes_for_object {
+    my $self = shift;
+    my $object = shift;
+    my $command_classes = shift;
+    my $prior_event_id = shift;
+
+    $DB::single = 1;
+    my @scheduled_commands;
+    for my $command_class (@{$command_classes}) {
+        if (ref($command_class) eq 'ARRAY') {
+            push @scheduled_commands, $self->_schedule_command_classes_for_object($object,$command_class,$prior_event_id);
+        } else {
+            my $command;
+            if ($command_class->isa('Genome::Model::EventWithRefSeq')) {
+                if (ref($object)) {
+                    unless ($object->can('ref_seq_id')) {
+                        $self->fail('No support for the new Genome::Model::RefSeq objects. FIX ME!!!');
+                    }
+                    $self->fail('Expecting non-reference for EventWithRefSeq but got '. ref($object));
+                }
+                $command = $command_class->create(
+                                                  model_id => $self->model_id,
+                                                  ref_seq_id => $object,
+                                              );
+            } elsif ($command_class->isa('Genome::Model::EventWithReadSet')) {
+                unless ($object->isa('Genome::Model::ReadSet')) {
+                    $self->fail('Expecting a Genome::Model::ReadSet object and got '. ref($object));
+                }
+                $command = $command_class->create(
+                                                  read_set_id => $object->read_set_id,
+                                                  model_id => $self->model_id,
+                                              );
+                $object->first_build_id($self->build_id);
+            } elsif ($command_class->isa('Genome::Model::Event')) {
+                $command = $command_class->create(
+                                                  model_id => $self->model_id,
+                                              );
+            }
+            unless ($command) {
+                $self->fail(
+                            'Problem creating subcommand for class '
+                            . ' for object class '. ref($object)
+                            . ' model id '. $self->model_id
+                            . ': '. $command_class->error_message()
+                        );
+            }
+            $command->parent_event_id($self->id);
+            $command->event_status('Scheduled');
+            $command->retry_count(0);
+            $command->prior_event_id($prior_event_id);
+
+            $prior_event_id = $command->id;
+            push @scheduled_commands, $command;
+            my $object_id;
+            if (ref($object)) {
+                $object_id = $object->id;
+            } else {
+                $object_id = $object;
+            }
+            $self->status_message('Scheduled '. $command_class .' for  '. $object_id
+                                  .' event_id '. $command->genome_model_event_id ."\n");
+        }
+    }
+    return @scheduled_commands;
+}
+
+sub _run_stage {
+    my $self = shift;
+    my @scheduled_commands = @_;
+    if ($self->auto_execute && @scheduled_commands) {
+        my @dependency_ids = map {$_->lsf_job_id} @scheduled_commands;
+        unless (Genome::Model::Command::RunJobs->execute(model_id => $self->model_id)) {
+            $self->error_message('Failed to execute run-jobs for model '. $self->model_id);
+            return;
+        }
+        unless($self->execute_with_bsub(dep_type=>'ended', dependency_expression => join(")&&ended(", @dependency_ids) )) {
+            $self->error__message("Hello, I am the build module, and I was unable to schedule myself to run after my peeps.");
+            return;
+        }
+        return 2;
+    }
+    return 1;
+}
+
+
+sub fail {
+    my $self = shift;
+    my $reason = shift;
+    $self->cry_for_help($reason);
+    die($reason);
+}
+
+
+sub cry_for_help {
+    my $self = shift;
+    my $word_that_describes_what_i_failed = shift;
+
+    my $sendmail = "/usr/sbin/sendmail -t";
+    my $from = "From: ssmith\@genome.wustl.edu\n";
+    my $reply_to = "Reply-to: thisisafakeemail\n";
+    my $subject = "Subject: Build failed, you suck\n";
+    my $content = "This is the Build failure email. your build ". $self->id . " failed some portion of the $word_that_describes_what_i_failed phase. \n\n";
+    my $to = "To: " . $self->user_name . '@genome.wustl.edu' . "\n";
+
+    my $helpful_link1= "https://gscweb.gsc.wustl.edu/cgi-bin/solexa/genome-model-stage1.cgi?model-name=" . $self->model->name  .    "&refresh=1\n\n";
+    my $helpful_link2= "https://gscweb.gsc.wustl.edu/cgi-bin/solexa/genome-model-stage2.cgi?model-name=" . $self->model->name  .    "&refresh=1\n";
+
+    $content .= $helpful_link1 . $helpful_link2;
+
+
+
+    open(SENDMAIL, "|$sendmail") or die "Cannot open $sendmail: $!";
+    print SENDMAIL $reply_to;
+    print SENDMAIL $from;
+    print SENDMAIL $subject;
+    print SENDMAIL $to;
+    print SENDMAIL $content;
+    close(SENDMAIL);
+    return 1;
+}
+
 
 1;
 
