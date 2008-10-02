fc4415cce530de340542ede79a76ddd217c89fcf
ebelter <ebelter@genome.wustl.edu>
2008-10-02 18:46:36 +0000

    place holder for dir
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39311 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Build/MetaGenomicComposition.pm
@@ -0,0 +1,23 @@
+package Genome::Model::Command::Build::MetaGenomicComposition;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Command::Build::MetaGenomicComposition {
+    is => 'Genome::Model::Command::Build',
+    #has => [ ],
+};
+
+sub help_brief {
+    return '';
+}
+
+sub help_detail {
+    return <<"EOS"
+EOS
+}
+
+1;
+
