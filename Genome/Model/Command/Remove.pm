41f485c4bbab70c10f2905cdc5ef2f78bc4be8de
jwalker <jwalker@genome.wustl.edu>
2008-10-09 17:04:31 +0000

    use the new delete method on model.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39597 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Remove.pm
+++ Genome/Model/Command/Remove.pm
@@ -10,6 +10,12 @@ use Data::Dumper;
 
 class Genome::Model::Command::Remove {
     is => 'Genome::Model::Command',
+    has => [
+            model_id => {
+                         is => 'Integer',
+                         doc => 'The model_id of the model you wish to remove',
+                     }
+        ]
 };
 
 sub sub_command_sort_position { 2 }
@@ -31,11 +37,16 @@ EOS
 }
 
 sub execute {
-    my $self = shift;    
-    my $model = $self->model;
-    my @events = $model->events;
-    for (@events) { $_->delete }
-    $model->delete;
+    my $self = shift;
+    my $model = Genome::Model->get($self->model_id);
+    unless ($model) {
+        $self->error_message('No model found for model_id '. $self->model_id);
+        return;
+    }
+    unless ($model->delete) {
+        $self->error_message('Failed to delete model '. $model->id);
+        return;
+    }
     return 1;
 }
 
