41edc84c53aa625a7090566fe17f341def15dc33
ssmith <ssmith@genome.wustl.edu>
2008-10-13 15:54:10 +0000

    Made contact data optional, as it actually is optional currently.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39681 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Project.pm
+++ Genome/Project.pm
@@ -25,21 +25,22 @@ class Genome::Project {
         description         => { is => 'Text', len => 256, column_name => 'SETUP_DESCRIPTION' },
         project_type        => { is => 'Text', len => 32 },
         mailing_list        => { is => 'Text', column_name => 'MAILING_LIST' }, 
- 
-        external_contact        => { is => 'Genome::Project::Contact', id_by => 'ext_con_id' },
-        external_contact_name   => { is => 'Text', via => 'external_contact', to => 'name' }, 
-        external_contact_email  => { is => 'Text', via => 'external_contact', to => 'email' }, 
-
-        internal_contact        => { is => 'Genome::Project::Contact', id_by => 'internal_con_id' },
-        internal_contact_name   => { is => 'Text', via => 'internal_contact', to => 'name' }, 
-        internal_contact_email  => { is => 'Text', via => 'internal_contact', to => 'email' }, 
-
+        
         # not used, but available
         acct_id             => { is => 'Number', len => 10 },
         het_testing         => { is => 'Boolean' },
         is_submitted        => { is => 'Boolean' },
         parent_project_id   => { is => 'Number', len => 10 },
         setup_project_id    => { is => 'Number', len => 10 },
+    ],
+    has_optional => [ 
+        external_contact        => { is => 'Genome::Project::Contact', id_by => 'ext_con_id' },
+        external_contact_name   => { is => 'Text', via => 'external_contact', to => 'name' }, 
+        external_contact_email  => { is => 'Text', via => 'external_contact', to => 'email' }, 
+       
+        internal_contact        => { is => 'Genome::Project::Contact', id_by => 'internal_con_id' },
+        internal_contact_name   => { is => 'Text', via => 'internal_contact', to => 'name' }, 
+        internal_contact_email  => { is => 'Text', via => 'internal_contact', to => 'email' }, 
     ],
     has_many => [
         model_assignments   => { is => 'Genome::Model::ProjectAssignment', reverse_id_by => 'project' },
