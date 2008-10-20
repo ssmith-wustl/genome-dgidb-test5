c6c4bca69291e5d9c667c50a439405968eac9d2c
jwalker <jwalker@genome.wustl.edu>
2008-10-20 19:20:02 +0000

    refactor the processing profile to work from a property hash which is used to not only make the class description for this class, but also for a corresponding create command
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@39980 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/Assembly.pm
+++ Genome/ProcessingProfile/Assembly.pm
@@ -5,33 +5,94 @@ use warnings;
 
 use Genome;
 
-my @PARAMS = qw/
-               read_filter
-               read_filter_params
-               read_trimmer
-               read_trimmer_params
-               assembler
-               assembler_params
-               sequencing_platform
-              /;
+my %PROPERTIES = &properties_hash;
 
 class Genome::ProcessingProfile::Assembly{
     is => 'Genome::ProcessingProfile',
     has => [
-            ( map { $_ => {
-                           via => 'params',
-                           to => 'value',
-                           where => [name => $_],
-                           is_mutable => 1
-                       },
-                   } @PARAMS
-         ),
+            map {
+                $_ => {
+                       via => 'params',
+                       to => 'value',
+                       where => [ name => $_ ],
+                       is_optional => (
+                                       ( exists $PROPERTIES{$_}->{is_optional} )
+                                       ? $PROPERTIES{$_}->{is_optional}
+                                       : 0
+                                   ),
+                       is_mutable => 1,
+                       doc => (
+                               ( exists $PROPERTIES{$_}->{valid_valiues} )
+                               ? sprintf('%s Valid values: %s.', $PROPERTIES{$_}->{doc}, join(', ', @{$PROPERTIES{$_}->{valid_values}}))
+                               : $PROPERTIES{$_}->{doc}
+                           ),
+                   },
+               } keys %PROPERTIES
         ],
 };
 
+sub properties_hash {
+    my %properties = (
+                      sequencing_platform => {
+                                              doc => 'The sequencing platform used to produce the read sets to be assembled',
+                                              valid_values => ['454', 'solexa'],
+                                          },
+                      assembler_name => {
+                                         doc => 'The name of the assembler to use when assembling read sets',
+                                         valid_values => ['newbler'],
+                                     },
+                      assembler_params => {
+                                           doc => 'A string of parameters to pass to the assembler',
+                                           is_optional => 1,
+                                       },
+                      read_trimmer_name => {
+                                            doc => 'The name of the software to use when trimming read sets',
+                                            is_optional => 1,
+                                        },
+                      read_trimmer_params => {
+                                              doc => 'A string of parameters to pass to the read_trimmer',
+                                              is_optional => 1,
+                                          },
+                      read_filter_name => {
+                                           doc => 'The name of the software to use when filtering read sets',
+                                           is_optional => 1,
+                                       },
+                      read_filter_params => {
+                                             doc => 'A string of parameters to pass to the read_filter',
+                                             is_optional => 1,
+                                         },
+                  );
+    return %properties
+}
+
 sub params_for_class {
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my %properties = &properties_hash;
+    return keys %properties;
+}
+
+sub create {
     my $class = shift;
-    return @PARAMS;
+    my $self = $class->SUPER::create(@_);
+    my $class_object = $self->get_class_object;
+    for my $property_name ( keys %PROPERTIES ) {
+        next if $class_object->{has}->{$property_name}->{is_optional} && !$self->$property_name;
+        next unless exists $PROPERTIES{$property_name}->{valid_values};
+        unless ( grep { $self->$property_name eq $_ } @{$PROPERTIES{$property_name}->{valid_values}} ) {
+            $self->error_message(
+                sprintf(
+                    'Invalid value (%s) for %s.  Valid values: %s',
+                    $self->$property_name,
+                    $property_name,
+                    join(', ', @{$PROPERTIES{$property_name}->{valid_values}}),
+                )
+            );
+            $self->delete;
+            return;
+        }
+    }
+    return $self;
 }
 
 sub instrument_data_is_applicable {
