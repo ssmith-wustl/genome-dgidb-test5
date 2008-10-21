940b160140b3b9b25cc622889d8c8e5314e6df7e
jwalker <jwalker@genome.wustl.edu>
2008-10-21 19:03:58 +0000

    A base class for blat tools
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40043 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Blat.pm
@@ -0,0 +1,31 @@
+package Genome::Model::Tools::Blat;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::Blat {
+    is => 'Command',
+};
+
+sub sub_command_sort_position { 14 }
+
+sub help_brief {
+    "Tools to run blat or work with its output files.",
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+gt blat ...
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+EOS
+}
+
+1;
+
