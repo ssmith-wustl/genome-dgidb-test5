26cd0591ac9771c41ff46c01cf8c39098e356fd7
jwalker <jwalker@genome.wustl.edu>
2008-10-21 20:22:00 +0000

    workflow implementation of the blat read aligner for 454 reference alignment model.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40063 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Blat.pm
+++ Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Blat.pm
@@ -20,7 +20,7 @@ class Genome::Model::Command::Build::ReferenceAlignment::AlignReads::Blat {
                            doc => 'the file path to the fasta file of reads',
                            calculate_from => ['read_set_link'],
                            calculate => q|
-                               return $read_set_link->read_set_data_directory .'/'. $read_set_link->subset_name .'.fa';
+                               return $read_set_link->full_path .'/'. $read_set_link->subset_name .'.fa';
                            |
                        },
             _alignment_files =>{
@@ -57,11 +57,6 @@ EOS
 
 sub should_bsub { 1;}
 
-
-sub proper_blat_path {
-    my $self = shift;
-    return '/gsc/bin/blat';
-}
 
 sub alignment_file {
     my $self = shift;
@@ -108,7 +103,7 @@ sub read_set_aligner_error_file {
 sub execute {
     my $self = shift;
 
-    my $read_set_data_directory = $self->read_set_link->read_set_data_directory;
+    my $read_set_data_directory = $self->read_set_link->full_path;
     unless (-e $read_set_data_directory) {
         unless ($self->create_directory($read_set_data_directory)) {
             $self->error_message("Failed to created read set data directory '$read_set_data_directory'");
@@ -145,174 +140,21 @@ sub execute {
     my $model = $self->model;
     my @ref_seq_paths = grep {$_ !~ /all_sequences/ } $model->get_subreference_paths(reference_extension => 'fa');
 
-    my $blat_path = $self->proper_blat_path;
     my $blat_params = '-mask=lower -out=pslx -noHead';
     $blat_params .= $model->read_aligner_params || '';
 
-    my %jobs;
-    my @psl_to_cat;
-    my @out_to_cat;
-    my @err_to_cat;
-    for my $ref_seq_path (@ref_seq_paths) {
-        my $ref_seq_name = basename($ref_seq_path);
-        $ref_seq_name =~ s/\.fa//;
-
-        my $psl_file = $self->read_set_alignment_file .'_'. $ref_seq_name;
-        my $out_file = $self->read_set_aligner_output_file .'_'. $ref_seq_name;
-        my $err_file = $self->read_set_aligner_error_file .'_'. $ref_seq_name;
-
-        push @psl_to_cat, $psl_file;
-        push @out_to_cat, $out_file;
-        push @err_to_cat, $err_file;
-
-        my $blat_cmd = $blat_path .' '. $blat_params.' '. $ref_seq_path .' '. $self->fasta_file .' '.
-            $psl_file;
-
-        my $job = PP::LSF->create(
-                                  pp_type => 'lsf',
-                                  q => 'long',
-                                  command => $blat_cmd,
-                                  o => $out_file,
-                                  e => $err_file,
-                              );
-        unless ($job) {
-            $self->error_message("Failed to create job for '$blat_cmd'");
-            return;
-        }
-        my $id = $job->id;
-        $jobs{$id} = $job;
-    }
-
-    for my $job_id (keys %jobs) {
-        my $job = $jobs{$job_id};
-        $self->status_message('starting '. $job->id  .' bsub_cmd:'. $job->bsub_cmd);
-        unless ($job->start) {
-            $self->error_message('failed to start '. $job->id  .' bsub_cmd:'. $job->bsub_cmd);
-            return;
-        }
-    }
-  MONITOR: while ( %jobs ) {
-        sleep 15;
-        for my $job_id ( keys %jobs ) {
-            my $job = $jobs{$job_id};
-            if ( $job->has_ended ) {
-                if ( $job->has_ended ) {
-                    if ( $job->is_successful ) {
-                        $self->status_message("$job_id successful");
-                        delete $jobs{$job_id};
-                    } else {
-                        $self->status_message("$job_id failed\n");
-                        $self->_kill_jobs(values %jobs);
-                        last MONITOR;
-                    }
-                }
-            }
-        }
-    }
-    unless ($self->_cat_alignment_files(@psl_to_cat)) {
-        $self->error_message("Failed to cat psl blat alignments");
+    my $blat_subjects = Genome::Model::Tools::Blat::Subjects->create(
+                                                                     query_file => $self->fasta_file,
+                                                                     subject_files => \@ref_seq_paths,
+                                                                     blat_params => $blat_params,
+                                                                     psl_path => $self->read_set_alignment_file,
+                                                                     blat_output_path => $self->read_set_aligner_output_file
+                                                                 );
+    unless ($blat_subjects->execute) {
+        $self->error_message('Failed to execute '. $blat_subjects->command_name .' for '.
+                             $self->fasta_file ." against:\n". join ("\n",@ref_seq_paths));
         return;
     }
-    unless ($self->_cat_aligner_output_files(@out_to_cat)) {
-        $self->error_message("Failed to cat blat output");
-        return;
-    }
-    unless ($self->_cat_aligner_error_files(@err_to_cat)) {
-        $self->error_message("Failed to cat blat error");
-        return;
-    }
-    my @to_remove;
-    push @to_remove, @psl_to_cat, @out_to_cat, @err_to_cat;
-    for (@to_remove) {
-        unless (unlink($_)) {
-            $self->error_message("Failed to remove file '$_'");
-        }
-    }
-    return 1;
-}
-
-
-sub _kill_jobs {
-    my $self = shift;
-    my @jobs = @_;
-    for my $job ( @jobs ) {
-        next if $job->has_ended;
-        $job->kill;
-    }
-    return 1;
-}
-
-sub _cat_alignment_files {
-    my $self = shift;
-    my @files = @_;
-
-    my $out_file = $self->read_set_alignment_file;
-    if (-s $out_file) {
-        $self->error_message("File already exists '$out_file'");
-        return;
-    }
-    my $writer = Genome::Utility::PSL::Writer->create(
-                                                      file => $out_file,
-                                                  );
-    for my $file (@files) {
-        my $reader = Genome::Utility::PSL::Reader->create(
-                                                          file => $file,
-                                                      );
-        unless ($reader) {
-            $self->error_message("Failed to read file '$file'");
-            return;
-        }
-        while (my $record = $reader->next) {
-            $writer->write_record($record);
-        }
-        $reader->close
-    }
-    $writer->close;
-    return 1;
-}
-
-sub _cat_aligner_output_files {
-    my $self = shift;
-    my @files = @_;
-
-    my $out_file = $self->read_set_aligner_output_file;
-    return $self->_cat_files($out_file,@files);
-}
-
-sub _cat_aligner_error_files {
-    my $self = shift;
-    my @files = @_;
-
-    my $out_file = $self->read_set_aligner_error_file;
-    return $self->_cat_files($out_file,@files);
-}
-
-sub _cat_files {
-    my $self = shift;
-    my $out_file = shift;
-    my @files = @_;
-
-    if (-s $out_file) {
-        $self->error_message("File already exists '$out_file'");
-        return;
-    }
-
-    my $out_fh = IO::File->new($out_file,'w');
-    unless ($out_fh) {
-        $self->error_message("File will not open with write priveleges '$out_file'");
-        return;
-    }
-    for my $in_file (@files) {
-        my $in_fh = IO::File->new($in_file,'r');
-        unless ($in_fh) {
-            $self->error_message("File will not open with read priveleges '$in_file'");
-            return;
-        }
-        while (my $line = $in_fh->getline()) {
-            $out_fh->print($line);
-        }
-    }
-    $out_fh->close();
     return 1;
 }
 
