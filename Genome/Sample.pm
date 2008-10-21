df17d470b61b4385d2539e005d1a136786e8eca3
jwalker <jwalker@genome.wustl.edu>
2008-10-21 19:52:07 +0000

    complicated join to get all dna, organism_sample, and organism_taxon data for all solexa or 454 samples found(sample_name or incoming_dna_name)
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40056 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Sample.pm
+++ Genome/Sample.pm
@@ -13,12 +13,39 @@ use strict;
 use warnings;
 
 class Genome::Sample {
-    table_name => '(select * from organism_sample@dw) sample',
+    table_name => "
+(select d.*, s.*, o.taxon_id
+from (
+	select sample_name
+	from solexa_lane_summary\@dw
+	union
+	select sample_name
+	from run_region_454\@dw
+	union
+	select incoming_dna_name
+	from run_region_454\@dw
+	where sample_name is null
+) a
+join dna\@oltp d on d.dna_name = a.sample_name
+left join organism_sample\@dw s on a.sample_name = s.sample_name
+left join (
+	dna_resource\@oltp dr 
+	join entity_attribute_value\@oltp eav		
+		on eav.entity_id = dr.dr_id
+		and eav.type_name = 'dna'
+		and eav.attribute_name = 'org id'	
+	join organism_taxon\@dw o 
+		on o.legacy_org_id = eav.value
+) on dr.dna_resource_prefix = substr(dna_name,0,4)	
+) sample ",
     id_by => [
-          organism_sample_id            => { is => 'Text', len => 10 },
+            #organism_sample_id            => { is => 'Text', len => 10 },
+            id                          => { is => 'Number', column_name => 'DNA_ID' },
     ],
     has => [
-            name                        => { is => 'Text',     len => 64, column_name => 'SAMPLE_NAME' }, 
+            name                        => { is => 'Text',     len => 64, column_name => 'DNA_NAME' }, # 'SAMPLE_NAME' }, 
+    ],
+    has_optional => [
             source_id                   => { is => 'Number',   len => 10 },
             source_type                 => { is => 'Text',     len => 64 }, 
             source_individual           => { is => 'Genome::Individual', id_by => 'source_id' },
@@ -29,9 +56,12 @@ class Genome::Sample {
                                                 $class->get(id => $source_id, @_) 
                                             | },
             source_name                 => { via => 'source', to => 'name' },
+            # The join over to organism_taxon is not working correctly
+            # the query has a n extra iteration of the above table_name query
+            # thrown in 
+            taxon                       => { is => 'Genome::Taxon', id_by => 'taxon_id' },
+            species_name                => { via => 'taxon' },
             #projects                   => {},
-    ],
-    has_optional => [
             organ_name                  => { is => 'Text', len => 64 }, 
             tissue_name                 => { is => 'Text', len => 64 }, 
             cell_type                   => { is => 'Text', len => 100 }, 
