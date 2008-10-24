d6e9fabf20ae64cd7844ce75287783ef5cea1674
charris <charris@genome.wustl.edu>
2008-10-24 19:36:41 +0000

    first commit of first report in new report system for use by catalyst web project
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40222 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Report.pm
+++ Genome/Model/Command/Report.pm
@@ -8,6 +8,11 @@ use Genome;
 
 class Genome::Model::Command::Report {
     is => 'Genome::Model::Command',
+    has => [ 
+           model => { is => 'Genome::Model', id_by => 'model_id'},
+           #generate_report_brief => { is_abstract => 1 },
+           #generate_report_detail => { is_abstract => 1},
+       ],
 };
 
 sub help_brief {
@@ -15,6 +20,41 @@ sub help_brief {
 }
 
 sub sub_command_sort_position { 12 }
+
+sub generate_report_brief {
+    die "Implement me in the subclass, owangutang";
+}
+
+sub generate_report_detail {
+    die "Implement generate_report_detail in the subclass you're writing, or this will 
+     continue to fail";
+ }
+
+
+
+sub resolve_reports_directory {
+    my $self = shift;
+    my $reports_dir = $self->model->latest_build_directory . "/reports/";
+    unless(-d $reports_dir) {
+        unless(mkdir $reports_dir) {
+            $self->error_message("Directory $reports_dir doesn't exist, can't create");
+            return;
+        }
+        chmod 02775, $reports_dir;
+    }
+   return $reports_dir;
+}
+
+sub report_brief_output_filename {
+    my $self=shift;
+    return $self->resolve_reports_directory . "/brief.html";
+}
+
+sub report_detail_output_filename {
+    my $self=shift;
+    return $self->resolve_reports_directory . "/detail.html";
+}
+
 
 1;
 
