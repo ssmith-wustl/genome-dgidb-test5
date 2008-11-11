56d15b1432854edf28284d900390c88878dc5208
ebelter <ebelter@genome.wustl.edu>
2008-11-11 22:48:58 +0000

    list processing profiles
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40876 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/Command/List.pm
@@ -0,0 +1,24 @@
+package Genome::ProcessingProfile::Command::List;
+
+use strict;
+use warnings;
+
+use Genome;
+use Command; 
+use Data::Dumper;
+
+class Genome::ProcessingProfile::Command::List {
+    is => 'UR::Object::Command::List',
+    has => [
+    subject_class_name  => {
+        is_constant => 1, 
+        value => 'Genome::ProcessingProfile' 
+    },
+    #show => { default_value => 'id,name,subject_name,processing_profile_name' },
+    ],
+};
+
+1;
+
+#$HeadURL$
+#$Id$
