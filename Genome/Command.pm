ad36bb690b4ff03e57098852a45600ce6ef99a1e
ebelter <ebelter@genome.wustl.edu>
2008-11-11 22:11:50 +0000

    removed cruft below __END__; added more command class types
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40862 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Command.pm
+++ Genome/Command.pm
@@ -13,11 +13,17 @@ class Genome::Command {
     english_name => 'genome command',
 };
 
-our %SUB_COMMAND_CLASSES = (
-    model => 'Genome::Model::Command',
-    'processing-profile' => 'Genome::ProcessingProfile::Command',
-    tools => 'Genome::Model::Tools',
+our @SUB_COMMAND_DATA = (
+    #'project'               => 'Genome::Project::Command',
+    #'sample'                => 'Genome::Sample::Command',
+    #'population-group'      => 'Genome::PopulationGroup::Command',
+    #'instrument-data'       => 'Genome::InstrumentData::Command',
+    'processing-profile'    => 'Genome::ProcessingProfile::Command',
+    'model'                 => 'Genome::Model::Command',
+    'tools'                 => 'Genome::Model::Tools',
 );
+our %SUB_COMMAND_CLASSES = @SUB_COMMAND_DATA;
+
 for my $class ( values %SUB_COMMAND_CLASSES ) {
     eval("use $class;");
     die $@ if $@; 
@@ -51,78 +57,6 @@ sub class_for_sub_command {
   
 1;
 
-__END__
-
-sub _genome_command_sub_class_types {
-    return (qw/ model processing_profile /);
-}
-
-sub _genome_command_sub_class_types_camel_cased {
-    return map { join('', map { ucfirst } split(/\_/)) } _genome_command_sub_class_types();
-}
-
-sub _genome_command_sub_class_commands {
-    return map { s#\_#\-#g } _genome_command_sub_class_types();
-}
-
-sub _camel_case_to_command {
-    my ($string) = @_;
-
-    my @words = $string =~ /([A-Z](?:[A-Z]*(?=$|[A-Z][a-z])|[a-z]*))/g;
-    return join('-', map { lc } @words);
-}
-
-sub command_name {
-    my $self = shift;
-    my $class = ref($self) || $self;
-    my ($sub_class) = grep { $class =~ m#$_#i } _genome_command_sub_class_types_camel_cased()
-        or return $self->SUPER::command_name(@_);
-    return sprintf('genome %s', _camel_case_to_command($sub_class));
-}
-
-sub sub_command_dirs {
-    my $class = ref($_[0]) || $_[0];
-
-    my $dir = $class->SUPER::sub_command_dirs;
-    
-    return $dir unless $class eq __PACKAGE__;
-
-    print Dumper({dir=>$dir});
-    $dir =~ s#/Command##;
-    
-    return map { sprintf('%s/%s/Command/', $dir, $_) } _genome_command_sub_class_types_camel_cased();
-}
-
-sub sub_command_classes {
-    my $class = shift;
-
-    return $class->SUPER::sub_command_classes unless $class eq __PACKAGE__;
-    print Dumper([$class, [caller]]);
-
-    return map { sprintf('Genome::%s::Command', $_) } _genome_command_sub_class_types_camel_cased();
-}
-
-sub class_for_sub_command {
-    my $class = ref($_[0]) || $_[0];
-
-    return $_[0]->SUPER::class_for_sub_command(@_) unless $class eq __PACKAGE__;
-
-    my ($type) = grep { $_[1] eq $_ } _genome_command_sub_class_commands;
-    return unless $type;
-
-    return sprintf$_[0]->SUPER::class_for_sub_command(@_) unless $class eq __PACKAGE__;
-    return sprintf('Genome::%s::Command', join('', map { ucfirst } split(/\_/, $type)));
-}
-  
-sub sub_command_names {
-    my $class = shift;
-    print Dumper({$class=>[caller]});
-    my @sub_command_classes = $class->sub_command_classes;
-    my @sub_command_names= map { $_->command_name_brief } @sub_command_classes;
-    return @sub_command_names;
-}
-
-1;
-
 #$HeadURL$
 #$Id$
+
