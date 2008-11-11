5b9294eaff8a23a4e44ba7072207801ad4fb76b4
ssmith <ssmith@genome.wustl.edu>
2008-11-11 22:09:39 +0000

    The logic to connect to GSC/App namespaces is now here. (ebelter)
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40860 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome.pm
+++ Genome.pm
@@ -2,7 +2,12 @@ package Genome;
 
 use warnings;
 use strict;
+
 use UR;
+
+# GSCApp removes our overrides to can/isa for Class::Autoloader.  Tell it to put them back.
+use GSCApp;
+App::Init->_restore_isa_can_hooks();
 
 # DB::single is set to this value in many places, creating a source-embedded break-point
 # set it to zero in the debugger to turn off the constant stopping...
