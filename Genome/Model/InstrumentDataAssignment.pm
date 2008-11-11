b27f01ec89514d35d07a4d5f9feb628c098d3bbf
ebelter <ebelter@genome.wustl.edu>
2008-11-11 22:14:05 +0000

    bridge between models and instrument data
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40865 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/InstrumentDataAssignment.pm
@@ -0,0 +1,23 @@
+package Genome::Model::InstrumentDataAssignment;
+
+use strict;
+use warnings;
+
+use Genome;
+class Genome::Model::InstrumentDataAssignment {
+    table_name => 'MODEL_INSTRUMENT_DATA_ASSGNMNT',
+    id_by => [
+        model_id           => { is => 'NUMBER', len => 10 },
+        instrument_data_id => { is => 'VARCHAR2', len => 1000 },
+    ],
+    has => [
+        first_build_id     => { is => 'NUMBER', len => 10, is_optional => 1 },
+    ],
+    schema_name => 'GMSchema',
+    data_source => 'Genome::DataSource::GMSchema',
+};
+
+1;
+
+#$HeadURL$
+#$Id$
