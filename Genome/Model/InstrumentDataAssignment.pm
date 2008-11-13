f109ca71bbbf8e9edd98e23b7d998ffcdd56634b
ebelter <ebelter@genome.wustl.edu>
2008-11-13 18:16:49 +0000

    changed properties to be objects via their id; added read set properties
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@40962 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/InstrumentDataAssignment.pm
+++ Genome/Model/InstrumentDataAssignment.pm
@@ -5,14 +5,42 @@ use warnings;
 
 use Genome;
 class Genome::Model::InstrumentDataAssignment {
-    type_name => 'model instrument data assgnmnt',
     table_name => 'MODEL_INSTRUMENT_DATA_ASSGNMNT',
     id_by => [
-        model_id           => { is => 'NUMBER', len => 10 },
-        instrument_data_id => { is => 'VARCHAR2', len => 1000 },
+    #model_id           => { is => 'NUMBER', len => 10 },
+    #instrument_data_id => { is => 'VARCHAR2', len => 1000 },
+    model => { 
+        is => 'Genome::Model',
+        id_by => 'model_id',
+    },
+    instrument_data => { 
+        is => 'Genome::InstrumentData',
+        id_by => 'instrument_data_id',
+    },
     ],
     has => [
         first_build_id => { is => 'NUMBER', len => 10, is_optional => 1 },
+        #< Attributes from the model >#
+        alignment_directory => { via => 'model'},
+        #< Attributes from the instrument data >#
+        run_name => { via => 'instrument_data'},
+
+        #< Left over from Genome::Model::ReadSet >#
+        # PICK ONE AND FIX EVERYTHING THAT USES THIS
+        subset_name         => { via => 'instrument_data'},
+        run_subset_name     => { via => 'instrument_data', to => 'subset_name'},
+        # PICK ONE AND FIX EVERYTHING THAT USES THIS
+        short_name          => { via => 'instrument_data' },
+        run_short_name      => { via => 'instrument_data', to => 'short_name' },
+        library_name        => { via => 'instrument_data' },
+        sample_name         => { via => 'instrument_data' },
+        sequencing_platform => { via => 'instrument_data' },
+        full_path           => { via => 'instrument_data' },
+        unique_reads_across_library     => { via => 'instrument_data' },
+        duplicate_reads_across_library  => { via => 'instrument_data' },
+        median_insert_size => {via => 'instrument_data'},
+        sd_above_insert_size => {via => 'instrument_data'},
+        is_paired_end => {via => 'instrument_data' },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
