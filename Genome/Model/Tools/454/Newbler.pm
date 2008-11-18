ed090972f7a924e07ff18d0cecc5b9556b903f96
kkyung <kkyung@genome.wustl.edu>
2008-11-18 19:51:22 +0000

    added ability to build test version of assembler
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41096 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/454/Newbler.pm
+++ Genome/Model/Tools/454/Newbler.pm
@@ -33,13 +33,9 @@ sub newbler_bin {
     my $self = shift;
 
     my $bin_path = $self->bin_path;
-    #if ($self->test) {
-    #    if ($self->arch_os =~ /64/) {
-    #        $bin_path = '/gsc/pkg/bio/454/newbler/applicationsBin64';
-    #    } else {
-    #        $bin_path = '/gsc/pkg/bio/454/newbler/applicationsBin32';
-    #    }
-    #}
+    if ($self->test) {
+	$bin_path = '/gsc/pkg/bio/454/newbler/applicationsBin';
+    }
     return $bin_path;
 }
 
