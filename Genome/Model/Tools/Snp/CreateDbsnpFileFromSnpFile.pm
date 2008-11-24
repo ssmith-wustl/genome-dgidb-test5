4fe93342ed738ae7e13bb1231728a2f2e7f1ca73
dlarson <dlarson@genome.wustl.edu>
2008-11-24 18:14:25 +0000

    updated to calculate dbSNP-129 concordance better
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41288 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Snp/CreateDbsnpFileFromSnpFile.pm
+++ Genome/Model/Tools/Snp/CreateDbsnpFileFromSnpFile.pm
@@ -7,6 +7,9 @@ use Genome;
 use Genome::DB::Schema;
 use Command;
 use IO::File;
+use GSCApp;
+
+App->init;
 #use Genome::DB::Schema;
 
 class Genome::Model::Tools::Snp::CreateDbsnpFileFromSnpFile {
@@ -22,17 +25,23 @@ class Genome::Model::Tools::Snp::CreateDbsnpFileFromSnpFile {
     {
         type => 'String',
         is_optional => 0,
-        doc => "Output file name for dbSNP-127/Watson/Venter intersect file",
+        doc => "Output file name for dbSNP intersect file",
     },        
+    release =>
+    {
+        type => 'Number',
+        is_optional => 1,
+        doc => "Release of dbSNP to use",
+        default => '129',
+    },
     ],
 };
 
 
 sub execute {
     my $self=shift;
-    $DB::single=1; 
     # local $| = 1;
-    
+    my $release = $self->release;
 
     unless(-f $self->snp_file) {
         $self->error_message("Snp file is not a file: " . $self->snp_file);
@@ -52,125 +61,88 @@ sub execute {
     }
 
     #print output header
-    print $output_fh "chromosome\tstart\tend\tdbSNP-127\tWATSON\tVENTER\n";
-
-#    my $snps_at = $self->make_snp_file_hash($snp_fh);
-#    $snp_fh->close;
+    print $output_fh "chromosome\tstart\tend\tdbSNP-129\n";
 
     #make db connection
-    my $schema = Genome::DB::Schema->connect_to_dwrac;
-    $self->error_message("Can't connect to dwrac") and return unless $schema;
 
-    ##########3
-    ############
-    #If iterating through Variation
-    #my $rs = $schema->resultset('Variation');
-    #while ( my $var = $rs->next ) { 
-    #}
-    #assuming we are reasonably sorted
-    my $chrom; #= #$schema->resultset('Chromosome')->find({ name => '1' });
-    my $var_window;# = $chrom->variation_window;
+    my $dw = GSC::Sequence::Item->dbh;
+    my $cur_chr = 0;
+    my $cur_chrom_id;
+    my $chrom_id = $dw->prepare(qq/
+        select seq_id from sequence_item si
+        where sequence_item_type = 'chromosome sequence'
+            and sequence_item_name = ?
+        /);
+    my $variation_exists = $dw->prepare(qq/
+        select seq2_start,variation_type,allele_description from variation_sequence_tag vs
+        join sequence_correspondence scr on scr.scrr_id = vs.vstag_id
+        join sequence_collaborator sc on sc.seq_id = vs.vstag_id
+        where sc.collaborator_name = 'dbSNP'
+            and sc.role_detail = '$release'
+            and seq2_start = seq2_stop
+            and seq2_id = ? 
+        order by seq2_start
+        /);
+
+    #Potential Alternate Query worked out by Scott Smith
+    #/*+ gives Oracle a 'hint'. Ordering has changed according to tora so this may be faster.
+    #
+    #select /*+ ordered */ seq2_start,variation_type,allele_description 
+    #from sequence_collaborator sc 
+    #join sequence_correspondence scr on scr.scrr_id = sc.seq_id 
+    #join variation_sequence_tag vs on  vs.vstag_id = vs.vstag_id
+    #where sc.collaborator_name = 'dbSNP'
+    #and sc.role_detail = '129'
+    #and seq2_start = seq2_stop
+    #and seq2_id = 1676114023
+    #order by seq2_start;
+    #
     
+    my ($variant_position, $variant_allele, $variant_class);
+
+    #assuming we are reasonably sorted
     while ( my $line = $snp_fh->getline) {
-        #    print STDERR "\r",$snp_fh->input_line_number;
         chomp $line;
-        my %snp;
-        @snp{('chrom','position')} = split /\s+/, $line; 
-        if (!defined($chrom) || $snp{chrom} ne $chrom->chromosome_name ) {
-            #get new var window;
-            $chrom = $schema->resultset('Chromosome')->find({ chromosome_name => $snp{chrom}});
-            if(defined($chrom)) {
-                $var_window = $chrom->variation_window;
+        my ($chr,$pos,) = split /\s+/, $line; 
+        
+        if($chr ne $cur_chr) {
+            #retrieve the new chr_id
+            $chrom_id->execute('NCBI-human-build36-chrom' . $chr);
+            ($cur_chrom_id) = $chrom_id->fetchrow_array;
+            next unless(defined($cur_chrom_id));
+            $variation_exists->execute($cur_chrom_id);
+            $variation_exists->bind_columns(\$variant_position,\$variant_class,\$variant_allele);
+            $variation_exists->fetch;
+            $cur_chr = $chr;
+            print STDERR "Annotating $cur_chr\n";
+        }
+        
+        while($variant_position < $pos && $variation_exists->fetch) {};
+        
+        while($variant_position == $pos) {   
+            if($pos == $variant_position && $variant_class eq 'snp') {
+                printf $output_fh "%s\t%d\t%d\t1\n",$chr,$pos,$pos;
+                last;
             }
             else {
-                next;
+                last unless $variation_exists->fetch;
             }
-        }
-
-        my %submitters;
-        for my $var ( grep { $_->start_ == $snp{position} && $_->start_ == $_->end } $var_window->scroll( $snp{position} ) ) {
-            next if $var->allele_string =~ m#\-#;
-            @submitters{ map { $_->variation_source } $var->submitters } = 1; 
-        }
-
-        my $dbsnp_string = join "\t", (map {$_ ||= 0} @submitters{('dbSNP-127','WATSON','VENTER')});
-
-        #only print SNPs that are in dbSNP, Watson or Venter
-        if($dbsnp_string ne "0\t0\t0") { 
-            printf $output_fh "%s\t%d\t%d\t$dbsnp_string\n",$snp{chrom},$snp{position},$snp{position};
         }
     }
 
-
-
-    ###########33
-    #############
-    
-    #grab DBIx class Eddie made
-    #my $counter = 0;
-    #my $page = 1;
-    #my $variations = $schema->resultset('Variation')->search(undef, { rows => 10000, order_by => 'start_'});
-    #while(my $variation_itr = $variations->page($page++)) {
-    #    while(my $variation = $variation_itr->next) {
-    #        #        print "\r",++$counter;
-    #        next if($variation->allele_string =~ /-/); #ignore deletions 
-    #        my $start = $variation->start_;
-    #        next if($start != $variation->end);
-    #        my $chr = $variation->chromosome->chromosome_name;
-    #        $chr = $chr =~ /^\d+/ ? sprintf("%02d",$chr) : $chr;
-    #        next if(!exists($snps_at->{$chr}{$start}));
-    #        my %submitters = map {$_->variation_source => 1} $variation->submitters;
-    #        my $dbsnp_string = join "", (map {$_ ||= 0} @submitters{('dbSNP-127','WATSON','VENTER')});
-    #        $snps_at->{$chr}{$start} = sprintf("%03d",$snps_at->{$chr}{$start} | $dbsnp_string); 
-    #    }
-    #}
-
-    #my $output_fh = IO::File->new($self->output_file, "w");
-    #unless($output_fh) {
-    #    $self->error_message("Failed to open filehandle for: " .  $self->output_file );
-    #    return;
-    #}
-
-    #$self->print_dbsnp_file($output_fh,$snps_at);
     $snp_fh->close; 
     $output_fh->close;
-    
+
     return 1;
 }
 
 1;
 
 sub help_detail {
-    return "This module takes a snp list and creates a file of its intersections with dbSNP-127, Watson, and Venter genomes";
+    return "This module takes a snp list and creates a file of its intersections with dbSNP-129";
 }
 
 sub help_brief {
     return "Create a dbSNP/Watson/Venter file";
 }
 
-sub make_snp_file_hash {
-    my ($self,$handle) = @_;
-    my %snp_at;
-    while(my $line = $handle->getline) {
-        chomp $line;
-        my ($chr, $start,) = split /\s+/, $line;
-        #use Brian's trick of padding the chromosome
-        $chr = $chr =~ /^\d+/ ? sprintf("%02d",$chr) : $chr;
-        $snp_at{$chr}{$start} = '000';
-    }
-    return \%snp_at;
-
-}
-
-sub print_dbsnp_file {
-    my ($self,$handle,$snps) = @_;
-    print $handle "chromosome\tstart\tend\tdbSNP-127\tWATSON\tVENTER\n";
-    foreach my $chr (sort (keys %{$snps})) {
-        my $chromosome = $chr;
-        $chromosome =~ s/^0//;
-        foreach my $pos (sort { $a <=> $b } (keys %{$snps->{$chr}})) {
-            my $dbsnp_string = join "\t", split //, $snps->{$chr}{$pos}; 
-            print $handle "$chromosome\t$pos\t$pos\t$dbsnp_string\n";
-        }
-    }
-}
