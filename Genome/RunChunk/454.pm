92e5886685dca7ba893e4c868c4054623214448f
jwalker <jwalker@genome.wustl.edu>
2008-12-01 17:12:47 +0000

    add methods that will eventually move to instrument data for downloading sff files
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41451 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/RunChunk/454.pm
+++ Genome/RunChunk/454.pm
@@ -9,6 +9,11 @@ use Genome::RunChunk;
 class Genome::RunChunk::454 {
     is  => 'Genome::RunChunk',
     has => [
+            sff_file => {
+                         doc => 'The sff file associated with the 454 run chunk',
+                         calculate_from => [qw/ full_path seq_id /],
+                         calculate => q| return sprintf('%s/%s.sff', $full_path, $seq_id); |,
+                     },
             run_region_454     => {
                                     doc => 'Lane representation from LIMS.  This class should eventually be a base class for data like this.',
                                     is => 'GSC::RunRegion454',
@@ -31,7 +36,7 @@ sub resolve_subset_name {
     return $read_set->region_number;
 }
 
-sub resolve_full_path{
+sub resolve_full_path {
     my $class = shift;
     my $read_set = shift;
 
@@ -45,6 +50,40 @@ sub _desc_dw_obj {
     my $class = shift;
     my $obj = shift;
     return $obj->run_name . "/" . $obj->region_number . " (" . $obj->id . ")";
+}
+
+# Copied from InstrumentData
+sub create_data_directory_and_link {
+    my $self = shift;
+
+    my $data_path = $self->full_path;
+    Genome::Utility::FileSystem->create_directory($data_path)
+          or return;
+    Genome::Utility::FileSystem->create_symlink($data_path, $self->data_link)
+          or return;
+    return $data_path;
+}
+# Copied from InstrumentData
+sub dump_to_file_system {
+    my $self = shift;
+
+    $self->create_data_directory_and_link
+        or return;
+    unless ( -e $self->sff_file ) {
+        unless ( $self->run_region_454->dump_sff(filename => $self->sff_file) ) {
+            $self->error_message('Failed to dump sff_file to '. $self->sff_file);
+            return;
+        }
+    }
+    return 1;
+}
+# Copied from InstrumentData
+sub _links_base_path {
+    return '/gscmnt/839/info/medseq/instrument_data_links/';
+}
+# Copied from InstrumentData
+sub data_link {
+    return sprintf('%s/%s', _links_base_path(), $_[0]->seq_id);
 }
 
 1;
