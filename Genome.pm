8fcb708986fd206397c3c1859633be9552440c15
charris <charris@genome.wustl.edu>
2008-12-01 16:13:37 +0000

    ur and app commit now tied together for nefarious purposes
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41430 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome.pm
+++ Genome.pm
@@ -7,7 +7,20 @@ use UR;
 
 # GSCApp removes our overrides to can/isa for Class::Autoloader.  Tell it to put them back.
 use GSCApp;
+App::DB->db_access_level('rw');
 App::Init->_restore_isa_can_hooks();
+
+
+UR::Context->create_subscription(
+    method => "commit",
+    callback => sub {
+        if (UR::DBI->no_commit) {
+            App::DB->no_commit(1);
+        }
+        (App::DB->sync_database && App::DB->commit)
+            or die "Failed to commit changes to the DW!: " . App::DB->error_message
+    },
+); 
 
 # DB::single is set to this value in many places, creating a source-embedded break-point
 # set it to zero in the debugger to turn off the constant stopping...
