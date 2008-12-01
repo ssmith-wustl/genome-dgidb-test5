1fcb03ce73c2bb5319ce8111aa0b5591bc1e2653
jwalker <jwalker@genome.wustl.edu>
2008-12-01 16:51:13 +0000

    run the installed standalone version of breakPointRead
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41432 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Build/ReferenceAlignment/FindVariations/BreakPointRead.pm
@@ -0,0 +1,56 @@
+package Genome::Model::Command::Build::ReferenceAlignment::FindVariations::BreakPointRead;
+
+use strict;
+use warnings;
+
+use Genome;
+use Command;
+use Genome::Model;
+
+class Genome::Model::Command::Build::ReferenceAlignment::FindVariations::BreakPointRead {
+    is => [
+           'Genome::Model::Command::Build::ReferenceAlignment::FindVariations',
+       ],
+    has => [
+    ],
+
+};
+
+sub help_brief {
+    my $self = shift;
+    return "empty implementation of " . $self->command_name_brief;
+}
+
+sub help_synopsis {
+    return <<"EOS"
+    genome-model postprocess-alignments identify-variation break-point-read-454 --model-id 5 --ref-seq-id 10
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+This command is usually called as part of the postprocess-alignments process
+EOS
+}
+
+sub execute {
+    my $self = shift;
+
+    my $model = $self->model;
+
+    my $cmd = 'bprStandalone --alignments-file '.  $self->parent_event->merged_alignments_file
+        .' --sample-name '. $model->subject_name .' --sff-file '. $self->parent_event->merged_sff_file
+            .' --output-dir '. $self->parent_event->accumulated_alignments_directory
+                .' --fasta-dir '. $self->parent_event->merged_fasta_dir .' --qual-dir '. $self->parent_event->merged_qual_dir;
+    $self->status_message('Running: '. $cmd);
+    my $rv = system($cmd);
+    unless ($rv == 0) {
+        $self->error_message("non-zero exit code '$rv' returned from '$cmd'");
+        return;
+    }
+    return 1;
+}
+
+
+1;
+
