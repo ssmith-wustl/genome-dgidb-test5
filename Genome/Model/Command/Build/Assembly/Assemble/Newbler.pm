b11c9a7796d5f476904f8f90407785d8eca8ecfe
kkyung <kkyung@genome.wustl.edu>
2008-12-09 17:07:21 +0000

    added assembler version to command params
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41662 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Assembly/Assemble/Newbler.pm
+++ Genome/Model/Command/Build/Assembly/Assemble/Newbler.pm
@@ -38,11 +38,15 @@ sub execute {
     $DB::single = $DB::stopper;
 
     my $model = $self->model;
+
+    my $pp = Genome::ProcessingProfile->get( id => $model->processing_profile_id );
+
     my $params = $model->assembler_params || '';
 
     my %run_project_params = (
 			      params => $params,
 			      dir => $model->data_directory,
+			      assembler_version => $pp->assembler_version,
 			      );
 
     my $run_project = Genome::Model::Tools::454::Newbler::RunProject->create( %run_project_params );
@@ -55,9 +59,9 @@ sub execute {
     `chmod -R g+w $assembly_dir`;
     
     $model->last_complete_build_id($self->parent_event_id);
-    if ($model->current_running_build_id eq $self->parent_event_id) {
-        $model->current_running_build_id(undef);
-    }
+#    if ($model->current_running_build_id eq $self->parent_event_id) {
+#        $model->current_running_build_id(undef);
+#    }
     return 1;
 }
 
