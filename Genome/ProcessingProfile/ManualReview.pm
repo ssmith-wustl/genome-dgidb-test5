686fcecda715fa16e8fb3a59ed61049709512697
gsanders <gsanders@genome.wustl.edu>
2008-12-09 20:22:47 +0000

    Added skeleton modules for manual review... including the model, processing profile, and build.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41704 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/ManualReview.pm
@@ -0,0 +1,12 @@
+package Genome::ProcessingProfile::ManualReview;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::ProcessingProfile::ManualReview {
+    is => 'Genome::ProcessingProfile::Composite',
+};
+
+1;
