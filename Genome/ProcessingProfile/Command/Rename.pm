41b9b14cec7b95224d6362ad1b46c4160a1f4b21
ebelter <ebelter@genome.wustl.edu>
2008-12-10 15:53:54 +0000

    renmae a processing profile
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41741 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/Command/Rename.pm
@@ -0,0 +1,98 @@
+package Genome::ProcessingProfile::Command::Rename;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::ProcessingProfile::Command::Rename {
+    is => 'Genome::ProcessingProfile::Command',
+    has => [
+    new_name => {
+        is => 'Text',
+        doc => 'The new name for the processing profile',
+    },
+    ],
+};
+
+sub execute {
+    my $self = shift;
+
+    # Verify processing profile 
+    $self->_verify_processing_profile
+        or return;
+
+    # Verify new name
+    unless ( $self->new_name =~ /\w+/ ) {
+        $self->error_message("Letters are required to be included in the new name");
+        return;
+    }
+    
+    if ( $self->new_name eq $self->processing_profile->name ) {
+        $self->error_message(
+            sprintf('Processing profile (<ID> %s) already is named "%s"', $self->processing_profile->id, $self->new_name)
+        );
+        return;
+    }
+    
+    # Rename
+    $self->processing_profile->name( $self->new_name );
+
+    # Sanity chack
+    unless ( $self->new_name eq $self->processing_profile->name ) {
+        $self->error_message(
+            sprintf(
+                'Could not rename processing profile (<ID> %s) to "%s" for unkown reasons', 
+                $self->processing_profile->id, 
+                $self->new_name,
+            )
+        );
+        return;
+    }
+    
+    return 1;
+}
+
+1;
+
+=pod
+
+=head1 Name
+
+ModuleTemplate
+
+=head1 Synopsis
+
+=head1 Usage
+
+=head1 Methods
+
+=head2 
+
+=over
+
+=item I<Synopsis>
+
+=item I<Arguments>
+
+=item I<Returns>
+
+=back
+
+=head1 See Also
+
+=head1 Disclaimer
+
+Copyright (C) 2005 - 2008 Washington University Genome Sequencing Center
+
+This module is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY or the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+=head1 Author(s)
+
+B<Eddie Belter> I<ebelter@watson.wustl.edu>
+
+=cut
+
+#$HeadURL$
+#$Id$
+
