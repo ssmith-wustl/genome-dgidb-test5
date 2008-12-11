21f863f3de3cf2a5a6b97f3faf5c01bdc3c51ccb
jwalker <jwalker@genome.wustl.edu>
2008-12-11 14:16:34 +0000

    add doc. use sff_file method from read_set and dump data using the read_set. add verify_successful_completion method.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41753 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Assembly/AssignReadSetToModel/454.pm
+++ Genome/Model/Command/Build/Assembly/AssignReadSetToModel/454.pm
@@ -8,76 +8,60 @@ use Genome;
 class Genome::Model::Command::Build::Assembly::AssignReadSetToModel::454 {
     is => 'Genome::Model::Command::Build::Assembly::AssignReadSetToModel',
     has => [
-            read_set_data_directory => {
-                                        calculate_from => ['read_set'],
-                                        calculate => q|
-                                            return $read_set->full_path;
-                                        |,
-                                    },
             sff_file => {
-                         calculate_from => ['read_set_data_directory','read_set'],
+                         calculate_from => ['read_set'],
                          calculate => q|
-                             return $read_set_data_directory .'/'. $read_set->seq_id .'.sff';
+                             return $read_set->sff_file;
                          |,
                      },
         ]
  };
 
-sub bsub_rusage {
-    return '';
-}
-
 sub sub_command_sort_position { 40 }
 
 sub help_brief {
-    "assemble a genome"
+    "Assigns the read set to the model by dumping the read set data to the correct filesystem location."
 }
 
 sub help_synopsis {
     return <<"EOS"
-genome-model build mymodel 
+
 EOS
 }
 
 sub help_detail {
     return <<"EOS"
-One build of a given assembly model.
+Each read set assigned to an assembly model needs an sff file on the filesystem.
+This step dumps the read set data and verifies that the sff file exists and has size.
 EOS
 }
-
 
 sub execute {
     my $self = shift;
 
     $DB::single = $DB::stopper;
-
-    my $read_set_data_directory = $self->read_set_data_directory;
-    unless (-e $read_set_data_directory) {
-        unless($self->create_directory($read_set_data_directory)) {
-            $self->error_message("Failed to create directory '$read_set_data_directory'");
-            return;
-        }
+    unless ($self->read_set->dump_to_file_system) {
+        $self->error_message('Failed to dump read set data to the filesystem');
+        return;
     }
-    unless (-e $self->sff_file) {
-        my $run_region_454 = $self->read_set->run_region_454;
-        # THIS COULD SAVE SPACE BUT NOT SURE IF IT WILL WORK OR HOW TO HANDLE WHEN THE FILE DOESN'T EXIST
-        # The file may already exist on the filesystem.  If so, create a symlink
-        #my $sff_file_location = $run_region_454->sff_filesystem_location;
-        #if (-e $sff_file_location) {
-        #    unless (symlink($sff_file_location,$self->sff_file)) {
-        #        $self->error_message("Failed to create symlink '". $self->sff_file ."' to '$sff_file_location'");
-        #        return;
-        #    }
-        #} else {
-            unless ($run_region_454->dump_sff(filename => $self->sff_file)) {
-                $self->error_message('Failed to dump sff_file to '. $self->sff_file);
-                return;
-            }
-        #}
-    }
-    return 1;
+    return $self->verify_successful_completion;
 }
 
+
+sub verify_successful_completion {
+    my $self = shift;
+
+    unless (-e $self->sff_file) {
+        $self->error_message('Failed to find sff file '. $self->sff_file);
+        return;
+    }
+    unless (-s $self->sff_file) {
+        $self->error_message('Sff file '. $self->sff_file .' is zero size.');
+        return;
+    }
+
+    return 1;
+}
 
 1;
 
