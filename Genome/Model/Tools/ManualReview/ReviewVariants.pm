0f2b51298307f72812d40b9f2ef3a9d303fcdf40
jschindl <jschindl@genome.wustl.edu>
2008-12-12 17:53:54 +0000

    Minor feature updates.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41845 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/ManualReview/ReviewVariants.pm
+++ Genome/Model/Tools/ManualReview/ReviewVariants.pm
@@ -40,9 +40,17 @@ sub execute {
     my $project_file = $self->project_file;
     
     $DB::single = 1;
-    my $mr_dir = `wtf Genome::Model::Tools::ManualReview`;
+    my $mr_dir = 'Genome/Model/Tools/ManualReview';
+    foreach my $path (@INC) {
+        my $fullpath = $path . "/" .$mr_dir;
+        if( -e $fullpath) {
+            $mr_dir = $fullpath;
+            last;
+        }
+    }
+    
     chomp $mr_dir;
-    ($mr_dir) = $mr_dir =~ /(.*)\.pm/;
+    
     $mr_dir .= '/manual_review.glade';
     
     my $glade = new Gtk2::GladeXML($mr_dir,"manual_review");
@@ -54,8 +62,9 @@ sub execute {
     my $treeview = $glade->get_widget("review_list");
     $mr->build_review_tree;
 
-    $mainWin->signal_connect("destroy", sub { Gtk2->main_quit });
-    $mr->open_file($project_file) if($project_file && -e $project_file);;
+    $mainWin->signal_connect("destroy", sub { Gtk2->main_quit; system "killall consed"; });
+    $mr->open_file($project_file) if($project_file && -e $project_file);
+
     Gtk2->main();
     return 1;
 }
