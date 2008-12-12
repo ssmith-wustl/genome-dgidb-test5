5e0dbc92b890ddd4fb19d43f30106c130079ef8b
ssmith <ssmith@genome.wustl.edu>
2008-12-12 23:38:40 +0000

    Commit Eric & Josh's Genome/Report/RefSeqMaq.pm since the last commit does not compile.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41877 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Report/RefSeqMaq.pm
+++ Genome/Model/Report/RefSeqMaq.pm
@@ -83,11 +83,9 @@ sub generate_report_brief
     my ($rpt,$avg);
     foreach my $ref_seq(sort keys %maqs)
     {
-        $rpt = $maps{$ref_seq};
+        $rpt = $maqs{$ref_seq};
         $avg =  $rpt=~m/(.*non-gap regions:\s*)(\d+\.\d+)/ ? $2 : 'Not Available';
-        $brief->print("<li>$ref_seq:<a href=\"" .
-                      $self->reports_dir . '/' .  $ref_seq_name . '_detail.html'; . 
-                      "\">$avg</a></li>");
+        $brief->print("<li>$ref_seq:<a href=\"" .$self->resolve_reports_directory . '/' .  $o->ref_seq_name . '_detail.html' . "\">$avg</a></li>");
     }
     $brief->print("<ul>");
     $brief->close;
@@ -104,32 +102,80 @@ sub get_maq_content
 {
     my $self = shift;
     my $model = $self->model;
-    my $i = $self->get_ref_seq_iterator;  #rely on get_ref_seq_iterator to filter on ref_seq_name, if necessary
+    #my $i = $self->get_ref_seq_iterator;  #rely on get_ref_seq_iterator to filter on ref_seq_name, if necessary
 
-    my ($o, @o);
-    while ($o = $i->next) 
-    {
-        push @o, $o;
-    }
+   # my ($o, @o);
+   # while ($o = $i->next) 
+   # {
+   #    print "Working on ".$o->ref_seq_name."\n";
+   #    push @o, $o;
+   # }
 
-    my ($maq_file, $bfa_file, $cmd, @maq, $fh, $file_name, %output, $rpt);
+    my ($maq_file, $bfa_file, $cmd, @maq, $fh, $file_name, %output, $rpt,$maplist);
     my $reports_dir = $self->model->resolve_reports_directory;
-    foreach $o(@o)
-    {
-        $maq_file = $o->resolve_accumulated_alignments_filename;
-        $bfa_file = $self->bfa_path . $o->ref_seq_name . ".bfa " . $maq_file;
-        $cmd = $self->cmd . " " .$bfa_file; 
-        @maq = `$cmd`;
-        $rpt = join('',@maq);
-        #make detail report
-        $file_name = $reports_dir . '/' .  $o->ref_seq_name . '_detail.html';
-        $fh = IO::File->new(">$file_name");        
-        $fh->print($rpt);
-        $fh->close;
-        #store for brief report
-        $output{$o->ref_seq_name} = $rpt;
+
+    my @all_map_lists;
+    my @map_list;
+    my $c;
+    my @chromosomes = (1..22,'X','Y');
+    foreach $c(@chromosomes) {
+        my $a_ref_seq = Genome::Model::RefSeq->get(model_id=>$model->genome_model_id,ref_seq_name=>$c);
+        @map_list = $a_ref_seq->combine_maplists;
+        push (@all_map_lists, @map_list); 
     }
-    return %output;
+
+   
+    #my $inputs = join("\n", @all_map_lists));
+    my $result_file = '/tmp/mapmerge_file.txt';
+
+    $self->warning_message("Performing a complete mapmerge for $result_file \n"); 
+        #$self->warning_message("on $inputs \n ") 
+        $self->warning_message("Hold on...\n");
+
+        #my $cmd = Genome::Model::Tools::Maq::MapMerge->create(use_version => '0.6.5', output => $result_file, inputs => \@inputs);
+        ($fh,$maplist) = File::Temp::tempfile;
+        print "Printing..." . scalar(@all_map_lists) . "\n";
+        print "Printing..." . join("",@all_map_lists ). "\n";
+        $fh->print(join("\n",@all_map_lists),"\n");
+        $fh->close;
+        print "gt maq vmerge --maplist $maplist --pipe $result_file &";
+        system "gt maq vmerge --maplist $maplist --pipe $result_file &";
+        my $start_time = time;
+        until (-p "$result_file" or ( (time - $start_time) > 100) )  {
+            $self->status_message("Waiting for pipe...");
+            sleep(5);
+        }
+        unless (-p "$result_file") {
+            die "Failed to make pipe? $!";
+        }
+        $self->status_message("Streaming into file $result_file.");
+        #system "cp $result_file.pipe $result_file";
+
+        $self->warning_message("mapmerge complete.  output filename is $result_file");
+        chmod 00664, $result_file;
+        return $result_file;
+
+    #foreach $o(@o)
+    #{
+    #   print "Working on ".$o->ref_seq_name."\n";
+    #   @map_list = $o->combine_map_lists;
+    #   push (@map_list, @all_map_lists); 
+    #}
+    #print "All the maps: ". join("\n",@all_map_lists);
+
+        #$maq_file = $o->resolve_accumulated_alignments_filename;
+        #$bfa_file = $self->bfa_path . $o->ref_seq_name . ".bfa " . $maq_file;
+        #$cmd = $self->cmd . " " .$bfa_file; 
+        #@maq = `$cmd`;
+        #$rpt = join('',@maq);
+        #make detail report
+        #$file_name = $reports_dir . '/' .  $o->ref_seq_name . '_detail.html';
+        #$fh = IO::File->new(">$file_name");        
+        #$fh->print($rpt);
+        #$fh->close;
+        #store for brief report
+        #$output{$o->ref_seq_name} = $rpt;
+    #return %output;
 }
 
 sub get_ref_seq_iterator
