a0e2b34938e8422453250904882ff3754e572e8e
josborne <josborne@genome.wustl.edu>
2008-12-15 19:59:26 +0000

    added bridge module between HGMI pipeline and PAP
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41919 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Hgmi/SendToPap.pm
@@ -0,0 +1,271 @@
+package Genome::Model::Tools::Hgmi::SendToPap;
+
+use strict;
+use warnings;
+
+use Genome;
+use Command;
+use Carp;
+use File::Slurp;
+use File::Temp qw/ tempfile tempdir /;
+use DateTime;
+use List::MoreUtils qw/ uniq /;
+use Bio::SeqIO;
+use Bio::Seq;
+use Bio::DB::BioDB;
+use Bio::DB::Query::BioQuery;
+use IPC::Run;
+use Workflow::Simple;
+
+UR::Object::Type->define(
+    class_name => __PACKAGE__,
+    is         => 'Command',
+    has        => [
+        'locus_tag' => {
+            is  => 'String',
+            doc => "taxonomy name"
+        },
+        'sequence_set_id' => {
+            is  => 'Integer',
+            doc => "Sequence set id in MGAP database",
+        },
+        'taxon_id' => {
+            is  => 'Integer',
+            doc => "NCBI Taxonomy id",
+        },
+        'workflow_xml' => {
+            is => 'String',
+            doc => "Workflow xml file",
+            is_optional => 1,
+        },
+        'keep_pep' => {
+            is          => 'Boolean',
+            doc         => "keep temporary fasta file of gene proteins",
+            is_optional => 1,
+            default     => 0
+        },
+        'pep_file' => {
+            is          => 'String',
+            doc         => "fasta file of gene proteins",
+            is_optional => 1,
+        },
+        'dev' => {
+            is => 'Boolean',
+            doc => "use development databases",
+            is_optional => 1,
+        },
+        'chunk_size' => {
+            is => 'Integer',
+            doc => "Chunk size parameter for PAP (default 10)",
+            is_optional => 1,
+            default => 10,
+        },
+    ]
+);
+
+sub help_brief
+{
+    "Bridges between HGMI tools and PAP";
+}
+
+sub help_synopsis
+{
+    return <<"EOS"
+Bridges between HGMI tools and PAP.  This tool pulls data from biosql,
+then initializes and runs the PAP workflow.
+EOS
+}
+
+sub help_detail
+{
+    return <<"EOS"
+Bridges between HGMI tools and PAP.  This tool loads predictions from mgap to
+biosql, pulls data from biosql, then initializes and runs the PAP workflow.
+EOS
+}
+
+sub execute
+{
+    my $self = shift;
+
+    print STDERR "moving data from mgap to biosql\n";
+    $self->mgap_to_biosql();
+
+    print STDERR "creating peptide file\n";
+    $self->get_gene_peps();
+
+    # interface to workflow to start the PAP.
+    $self->do_pap_workflow();
+
+    return 1;
+}
+
+sub get_gene_peps
+{
+    my $self = shift;
+    # this needs to handle switching to
+    # either dev or prod
+    my $dbadp = Bio::DB::BioDB->new(
+        -database => 'biosql',
+        -user     => 'sg_user',
+        -pass     => 'sgus3r',
+        -dbname   => 'DWDEV',
+        -driver   => 'Oracle'
+    );
+
+    my $cleanup = $self->keep_pep ? 0 : 1;
+    my $tempdir = tempdir( CLEANUP => $cleanup );
+    my ( $fh, $file ) = tempfile(
+        "pap-XXXXXX",
+        DIR    => $tempdir,
+        SUFFIX => '.fa'
+    );
+    #print "tempdir: ", $tempdir, ", tempfile: ", $file, ", cleanup: ",
+    #    $cleanup, "\n";
+    unless(defined($self->pep_file))
+    {
+        $self->pep_file($file);
+    }
+    $file = $self->pep_file();
+    my $seqout = new Bio::SeqIO(
+        -file   => ">$file",
+        -format => "fasta"
+    );
+
+    my $adp = $dbadp->get_object_adaptor("Bio::SeqI");
+
+    my $query = Bio::DB::Query::BioQuery->new();
+    $query->datacollections( [ "Bio::PrimarySeqI s", ] );
+
+    my $locus_tag = $self->locus_tag;
+    $query->where( ["s.display_id like '$locus_tag%'"] );
+    my $res = $adp->find_by_query($query);
+GENE: while ( my $seq = $res->next_object() )
+    {
+        my $gene_name = $seq->display_name();
+
+        #print $gene_name, "\n";
+        my @feat = $seq->get_SeqFeatures();
+        foreach my $f (@feat)
+        {
+            my $display_name = $f->display_name();
+            #print STDERR $display_name," ", $f->primary_tag,"\n";
+            next GENE if $f->primary_tag ne 'gene';
+            my $ss;
+            $ss = $seq->subseq( $f->start, $f->end );
+            my $pep = $self->make_into_pep( $f->strand,
+                #$ss,
+                $seq->subseq($f->start,$f->end),
+                $display_name );
+            $seqout->write_seq($pep);
+            #print STDERR "sequence should be written out\n";
+        }
+    }
+    if(! -f $file )
+    {
+        print STDERR "the fasta file $file doesn't exist!\n";
+        return 0;
+    }
+    unless( -s $file > 0 )
+    {
+        print STDERR "the fasta file $file still empty!\n";
+    }
+
+    return 1;
+}
+
+sub make_into_pep
+{
+    my ( $self, $strand, $subseq, $name ) = @_;
+
+    my $seq = new Bio::Seq(
+        -display_id => $name,
+        -seq        => $subseq
+    );
+    my $newseq;
+    if ( $strand < 0 )
+    {
+        $newseq = $seq->revcom->translate->seq;
+    }
+    else
+    {
+        $newseq = $seq->translate->seq;
+    }
+    my $seqobj = new Bio::Seq(
+        -display_id => $name,
+        -seq        => $newseq
+    );
+    return $seqobj;
+}
+
+sub mgap_to_biosql
+{
+    my $self      = shift;
+    my $locus_tag = $self->locus_tag;
+    my $ssid      = $self->sequence_set_id;
+    my $taxid     = $self->taxon_id;
+    my $testnorun = shift;
+    my @command = (
+        '/gscuser/josborne/src/hgmi_annotation/BAP/bap_load_biosql.pl', '--sequence-set-id', $ssid,
+        '--tax-id',        $taxid,
+    );
+
+    if($self->dev)
+    {
+        push(@command,'--dev');
+        #push(@command,'--dev-biosql');
+    }
+    my ($cmd_out,$cmd_err);
+
+    if($testnorun)
+    {
+        # just for testing.
+        print join(" " , @command),"\n";;
+        return 1;
+    }
+
+    IPC::Run::run(
+        \@command,
+        \undef,
+        '>',
+        \$cmd_out,
+        '2>',
+        \$cmd_err,
+    ) or croak "can't load biosql from mgap!!!\n$cmd_err";
+
+    return 1;
+
+}
+
+## need workflow item here...
+
+sub do_pap_workflow
+{
+    my $self = shift;
+
+    my $xml_file = $self->workflow_xml;
+    my $fasta_file = $self->pep_file;
+    my $chunk_size = $self->chunk_size;
+    #print STDERR "\n",$xml_file,"\n";
+    #print STDERR "\nfasta file is ",$fasta_file,"\n";
+    if(! -f $fasta_file)
+    {
+        print STDERR "\nwhere is the fasta file ", $fasta_file, "?\n";
+        croak "fasta file doesn't exist!";
+    }
+    my $output = run_workflow_lsf(
+                              $xml_file,
+                              'fasta file'       => $fasta_file,
+                              'chunk size'       => 10,
+                              'dev flag'         => 1,
+                              'biosql namespace' => 'MGAP',
+                              'gram stain'       => 'negative',
+                              'report save dir'  => '/gscmnt/temp212/info/annotation/PAP_testing/blast_reports',
+    );
+
+    return 1;
+}
+
+1;
+
+# $Id$
---
+++ Genome/Model/Tools/Hgmi/SendToPap.t
@@ -0,0 +1,53 @@
+#!/gsc/bin/perl
+
+use strict;
+use warnings;
+
+use above "Genome";
+use File::Remove qw/ remove /;
+
+use Test::More tests => 5;
+
+BEGIN {
+        use_ok('Genome::Model::Tools::Hgmi::SendToPap');
+}
+
+
+my $s = Genome::Model::Tools::Hgmi::SendToPap->create(
+          'locus_tag' => "ROSINTL182DFT_test",
+          'sequence_set_id' => 71,
+          'taxon_id' => 166486,
+          'dev' => 1,
+);
+
+my $sequence = "ATGGGGGCCTGACCCGA";
+my $result = $s->make_into_pep("-1", $sequence, "TEST.1");
+is($result->seq, "SGQAP", 'make sequence into peptide');
+
+ok($s->get_gene_peps(), 'run get_gene_peps()');
+my $s1 = Genome::Model::Tools::Hgmi::SendToPap->create(
+          'locus_tag' => "ROSINTL182DFT_test",
+          'sequence_set_id' => 71,
+          'taxon_id' => 166486,
+          'dev' => 1,
+          'keep_pep' => 1,
+);
+
+$s1->get_gene_peps();
+my $file = $s->pep_file();
+ok( -f $file, 'keep-pep option works');
+my $size = -s $file;
+ok($size gt 0, 'file is not empty');
+#my $dir = $file =~ s/\/pap-{[a-zA-Z0-9.]}+.fa$//;
+#print $dir,"\n";
+#print $file,"\n";
+#print $size,"\n";
+
+remove \1, qq{ $file };
+
+#$s->mgap_to_biosql(1);
+
+# todo:
+
+
+# $Id$
