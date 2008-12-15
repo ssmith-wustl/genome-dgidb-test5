9717382b5a45a6674ecc89c405d4475d8e693421
ssmith <ssmith@genome.wustl.edu>
2008-12-15 18:36:42 +0000

    Added a method to return a specific build by the version number given by the external data provider.  i.e.: $m = Genome::Model->get(name => "NCBI-human"); $b = $m->build_by_version("36");
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@41903 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ImportedReferenceSequence.pm
+++ Genome/Model/ImportedReferenceSequence.pm
@@ -10,4 +10,16 @@ class Genome::Model::ImportedReferenceSequence{
     is => 'Genome::Model',
 };
 
+sub build_by_version {
+    my $self = shift;
+    my $version = shift;
+    my $model = $self->model;
+    my @b = $model->bulids("data_directory like" => "v${version}_%");
+    if (@b > 1) {
+        die "Multiple builds for version $version for model " . $self->model_id;
+    }
+    return $b[0];
+}
+
+1;
 
