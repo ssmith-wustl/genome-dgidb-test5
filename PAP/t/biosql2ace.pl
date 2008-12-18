5f7805bac974ac51c4cb1441ab055c586de57d1a
mjohnson <mjohnson@genome.wustl.edu>
2008-12-18 18:47:57 +0000

    Committing the PAP AceDB .ace file dumper used during acceptance testing.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42101 127c0c0e-971a-0410-ad48-868c1a942d8d

--- PAP/t/biosql2ace.pl
+++ PAP/t/biosql2ace.pl
@@ -1,16 +1,42 @@
 #!/gsc/bin/perl
 
-use lib '/gscuser/mjohnson/bioperl-svn/bioperl-live';
-use lib '/gscuser/mjohnson/bioperl-svn/bioperl-db';
-
 use strict;
 use warnings;
 
-use Bio::Seq;
 use Bio::DB::BioDB;
 use Bio::DB::Query::BioQuery;
+use Bio::Seq;
 
 use Data::Dumper;
+use English;
+use IO::File;
+
+
+my $product_fh = IO::File->new();
+my $product_fn = 'productID.ace';
+
+$product_fh->open(">$product_fn") or die "Can't open '$product_fn': $OS_ERROR";
+
+my $psortb_fh = IO::File->new();
+my $psortb_fn = 'psort.ace';
+
+$psortb_fh->open(">$psortb_fn") or die "Can't open '$psortb_fn': $OS_ERROR";
+
+my $blastp_fh = IO::File->new();
+my $blastp_fn = 'briefID.fof.ace';
+
+$blastp_fh->open(">$blastp_fn") or die "Can't open '$blastp_fn': $OS_ERROR";
+
+my $interpro_fh = IO::File->new();
+my $interpro_fn = 'merged.raw.sorted.ace';
+
+$interpro_fh->open(">$interpro_fn") or die "Can't open '$interpro_fn': $OS_ERROR";
+
+my $kegg_fh = IO::File->new();
+my $kegg_fn = 'REPORT-top.ks.ace';
+
+$kegg_fh->open(">$kegg_fn") or die "Can't open '$kegg_fn': $OS_ERROR";
+
 
 my $dbadp = Bio::DB::BioDB->new(
                                 -database => 'biosql',
@@ -34,78 +60,148 @@ my $result = $adp->find_by_query($query);
 
 while (my $seq = $result->next_object()) {
 
-    my $gene_name = $seq->display_name();
-    
     my @features = $seq->get_SeqFeatures();
 
-    #print qq{Sequence : "$gene_name"}, "\n";
+    FEATURE: foreach my $feature (@features) {
 
-    foreach my $feature (@features) {
+        my $display_name     = $feature->display_name();
+        my $new_display_name = $display_name;
 
-        my $display_name = $feature->display_name();
+
+        ## InterPro
+        ##
+        ## This thing is not like the others.  Whereas all
+        ## the other annotation got shoved into the features
+        ## for the predicted genes, due to the need to have
+        ## multiple sets of tags and multiple dblinks and
+        ## have them properly linked, InterPro gets its own
+        ## features. 
+        if ($feature->source_tag eq 'InterPro') {
+
+            $new_display_name =~ s/\.InterPro\.\d+//g;
+            
+            my ($dblink) = grep { $_->database() eq 'InterPro' } $feature->annotation->get_Annotations();
+
+            if (defined($dblink)) {
+
+                my $ipr_number = $dblink->primary_id();
+
+                my ($analysis) = $feature->each_tag_value('interpro_analysis');
+                my ($evalue)   = $feature->each_tag_value('interpro_evalue');
+                my ($desc)     = $feature->each_tag_value('interpro_description');
+
+                print $interpro_fh qq{Sequence $new_display_name}, "\n";
+                print $interpro_fh qq{Interpro   "$analysis : $ipr_number $desc : $evalue}, "\n\n";
+                
+            }
+
+        }
         
+        ## Psort-B
         if (
             $feature->has_tag('psort_localization') &&
             $feature->has_tag('psort_score')
-           ) {
-
-            print qq{Sequence $display_name}, "\n";
-
+        ) {
+            
             my ($psort_localization) = $feature->each_tag_value('psort_localization');
             my ($psort_score)        = $feature->each_tag_value('psort_score');
-            
-            print qq{PSORT_B $psort_localization $psort_score}, "\n";
 
+            print $psortb_fh qq{Sequence $new_display_name}, "\n";
+            print $psortb_fh qq{PSORT_B $psort_localization $psort_score}, "\n\n";
+
+        }
+
+        
+        ## BlastP Product Categorization
+        if ($feature->has_tag('blastp_category')) {
+        
+            my ($product) = $feature->each_tag_value('blastp_category');
+
+            print $product_fh qq{Sequence $new_display_name}, "\n";
+            print $product_fh qq{Product_ID "$product"}, "\n\n";
+            
         }
         
         my $annotation_collection = $feature->annotation();
-
+        
         my @annotations = $annotation_collection->get_Annotations();
+        my @dblinks     = grep { $_->isa('Bio::Annotation::DBLink') } @annotations;
 
-        my @dblinks = grep { $_->isa('Bio::Annotation::DBLink') } @annotations;
- 
- 
-        my @kegg_dblinks = grep { $_->database() eq 'KEGG' } @dblinks;
+        
+        ## BlastP
+        if (@dblinks > 0) {
+            
+            my ($top_hit) = grep { $_->database() eq 'GenBank' } @dblinks;
 
-        my ($gene_dblink)      = grep { $_->primary_id() =~ /\w{3}\:\w+/ } @kegg_dblinks;
-        my ($orthology_dblink) = grep { $_->primary_id() =~ /^K\d+$/   } @kegg_dblinks;
+            if (defined($top_hit)) {
+                
+                my $hit_accession = $top_hit->primary_id();
+                
+                my ($bit_score)  = $feature->each_tag_value('blastp_bit_score');
+                my ($evalue)     = $feature->each_tag_value('blastp_evalue');
+                my ($percent_id) = $feature->each_tag_value('blastp_percent_identical');
+                my ($q_start)    = $feature->each_tag_value('blastp_query_start');
+                my ($q_end)      = $feature->each_tag_value('blastp_query_end');
+                my ($s_start)    = $feature->each_tag_value('blastp_subject_start');
+                my ($s_end)      = $feature->each_tag_value('blastp_subject_end');
+                my ($hit_name)   = $feature->each_tag_value('blastp_hit_name');
+                my ($hit_desc)   = $feature->each_tag_value('blastp_hit_description');
+                
+                my $brief_id = join(
+                                    "\t",
+                                    qq{Brief_identification "Stats: $bit_score},
+                                    $evalue,
+                                    $percent_id,
+                                    $q_start,
+                                    $q_end,
+                                    $s_start,
+                                    $s_end,
+                                    qq{ TopHit: $hit_name $hit_desc "},
+                                );
+                
+                print $blastp_fh qq{Sequence $new_display_name}, "\n";
+                print $blastp_fh qq{$brief_id}, "\n\n";
+                
+            }
+            
+        }
 
-        if (
-            $feature->has_tag('kegg_evalue') &&
-            $feature->has_tag('kegg_description') &&
-            defined($gene_dblink) &&
-            defined($orthology_dblink)
-           ) {
-           
-             my $gene_id            = $gene_dblink->primary_id();
-             my $orthology_id       = $orthology_dblink->primary_id();
-             my ($kegg_evalue)      = $feature->each_tag_value('kegg_evalue');
-             my ($kegg_description) = $feature->each_tag_value('kegg_description');
-             
-             print qq{KEGG   "$gene_id $kegg_evalue $kegg_description $orthology_id"}, "\n";   
 
+        ## KEGG
+        if (@dblinks > 0) {
+
+            my @kegg_dblinks = grep { $_->database() eq 'KEGG' } @dblinks;
+            
+            my ($gene_dblink)      = grep { $_->primary_id() =~ /^\w{3}\:\w+/ } @kegg_dblinks;
+            my ($orthology_dblink) = grep { $_->primary_id() =~ /^K\d+(\:K\d+)*$/   } @kegg_dblinks;
+
+            if (
+                $feature->has_tag('kegg_evalue') &&
+                $feature->has_tag('kegg_description') &&
+                defined($gene_dblink)
+            ) {
+                
+                my $gene_id            = $gene_dblink->primary_id();
+               
+                my ($kegg_evalue)      = $feature->each_tag_value('kegg_evalue');
+                my ($kegg_description) = $feature->each_tag_value('kegg_description');
+
+                my $kegg_ace = qq{KEGG   "$gene_id $kegg_evalue $kegg_description};
+
+                if (defined($orthology_dblink)) {
+                    my $orthology_id       = $orthology_dblink->primary_id();    
+                    $kegg_ace = join('  ', $kegg_ace, $orthology_id);
+                }
+
+                $kegg_ace = join('', $kegg_ace, qq{"});
+                
+                print $kegg_fh qq{Sequence $new_display_name}, "\n";
+                print $kegg_fh $kegg_ace, "\n\n";
+                
+            }
+            
         }
         
-        my @interpro_dblinks = grep { $_->database() eq 'InterPro' } @dblinks;
-        
-        foreach my $dbl (@interpro_dblinks) {
-
-            my $ipr = $dbl->primary_id();
-            
-            print qq{Interpro   "HMMPfam : $ipr"}, "\n";
-
-        }
-
-        #my @genbank_dblinks = grep { $_->database() eq 'GenBank' } @dblinks;
-
-        #foreach my $dbl (@genbank_dblinks) {
-
-        #    my $id      = $dbl->primary_id();
-            
-            #print qq{$id}, "\n";
-
-        #}
-
     }
     
 }
