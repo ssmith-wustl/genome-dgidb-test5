7683cd42c6d709d774665ad2eca447b62bcf492f
jwalker <jwalker@genome.wustl.edu>
2008-12-30 16:12:29 +0000

    remove new_read_set_alignment_directory since nothing is using it.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42235 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/EventWithReadSet.pm
+++ Genome/Model/EventWithReadSet.pm
@@ -38,20 +38,6 @@ class Genome::Model::EventWithReadSet {
                                                             );
                                           |,
                             },
-            #This is temporary so the 454 test runs without creating a bunch of lengthy blat alignments
-            #Please fix once a standard testing scheme is developed
-            new_read_set_alignment_directory => {
-                                                 calculate_from => ['alignment_directory','read_set_link'],
-                                                 calculate => q|
-                                              return sprintf('%s/%s/%s/%s',
-                                                             $alignment_directory,
-                                                             $read_set_link->sample_name,
-                                                             $read_set_link->run_name,
-                                                             $read_set_link->subset_name,
-                                                            );
-                                          |,
-                                             },
-
         # deprecated
         read_set_directory  => {
                                 calculate_from => ['model','read_set_link'],
