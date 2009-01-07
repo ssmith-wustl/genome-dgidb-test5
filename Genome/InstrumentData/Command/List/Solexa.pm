77473785e8832cc8812e11a1c6263d7faf3b72f5
jeldred <jeldred@genome.wustl.edu>
2009-01-07 19:48:08 +0000

    changed default show fields to include sample name
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42423 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Command/List/Solexa.pm
+++ Genome/InstrumentData/Command/List/Solexa.pm
@@ -14,7 +14,7 @@ class Genome::InstrumentData::Command::List::Solexa {
             is_constant => 1, 
             value => 'Genome::InstrumentData::Solexa' 
         },
-        show => { default_value => 'id,flow_cell_id,lane,library_name,read_length,is_paired_end,clusters,median_insert_size,sd_above_insert_size' },
+        show => { default_value => 'id,flow_cell_id,lane,sample_name,library_name,read_length,is_paired_end,clusters,median_insert_size,sd_above_insert_size' },
     ],
     doc => 'list illumina/solexa lanes available for analysis',
 };
