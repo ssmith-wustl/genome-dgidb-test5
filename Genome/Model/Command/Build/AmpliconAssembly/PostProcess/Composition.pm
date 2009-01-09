d035c3c410e069981b350eb9ba582dd364a20247
ebelter <ebelter@genome.wustl.edu>
2009-01-09 14:52:50 +0000

    added rdp classifiaction
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42475 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/PostProcess/Composition.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/PostProcess/Composition.pm
@@ -5,13 +5,30 @@ use warnings;
 
 use Genome;
 
+use Genome::Model::Tools::MetagenomicClassifier::Rdp;
+
 class Genome::Model::Command::Build::AmpliconAssembly::PostProcess::Composition {
     is => 'Genome::Model::Command::Build::AmpliconAssembly::PostProcess',
 };
 
 #< Beef >#
 sub execute {
-    return 1;
+    my $self = shift;
+
+    unlink $self->model->rdp_file if -e $self->model->rdp_file;
+        
+    my $classifier = Genome::Model::Tools::MetagenomicClassifier::Rdp->create(
+        input_file => $self->model->assembly_fasta,
+        output_file => $self->model->rdp_file,
+    )
+        or return;
+    my $classification = $classifier->execute
+        or return;
+
+    return 1 if -s $self->model->rdp_file;
+
+    $self->error_message("RDP classification executed correctly, but a file was not created");
+    return;
 }
 
 1;
