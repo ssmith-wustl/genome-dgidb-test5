fbd0aa09ce3af87b54bcf994025f4189a4ccc7b4
jwalker <jwalker@genome.wustl.edu>
2009-01-12 21:53:34 +0000

    remove the build object attributes from the build event
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42559 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/PolyphredPolyscan.pm
+++ Genome/Model/Command/Build/PolyphredPolyscan.pm
@@ -2,7 +2,9 @@ package Genome::Model::Command::Build::PolyphredPolyscan;
 
 use strict;
 use warnings;
+
 use Genome;
+
 use File::Copy "cp";
 use File::Basename;
 
@@ -46,7 +48,7 @@ sub execute {
         die;
     }
 
-    my $instrument_data_directory = $self->instrument_data_directory;
+    my $instrument_data_directory = $self->build->instrument_data_directory;
 
     if (-d $instrument_data_directory ){
         $self->error_message("new current instrument data dir $instrument_data_directory exists before it should!");
@@ -81,81 +83,14 @@ sub execute {
     $model->last_complete_build_id($self->build_id);
     $model->current_running_build_id(undef);
 
-    $self->complete_queue_pses;
+    $self->build->complete_queue_pses;
     return $model;
 }
-
-# Returns full path to the input data in the current build
-sub instrument_data_directory {
-    my $self = shift;
-    my $build_data_directory = $self->data_directory;
-
-    my $instrument_data_directory = "$build_data_directory/instrument_data/";
-
-    # Remove spaces, replace with underscores
-    $instrument_data_directory =~ s/ /_/;
-    
-    return $instrument_data_directory;
-}
-
-# Returns an array of the files in the current input dir
-sub instrument_data_files {
-    my $self = shift;
-
-    my $instrument_data_directory = $self->instrument_data_directory;
-    my @current_instrument_data_files = `ls $instrument_data_directory`;
-    
-    foreach my $file (@current_instrument_data_files){  #gets rid of the newline from ls, remove this if we switch to IO::Dir
-        $file = $instrument_data_directory . $file;
-        chomp $file;
-    }
-
-    return @current_instrument_data_files;
-}
-
-
 
 sub _get_sub_command_class_name{
   return __PACKAGE__; 
 }
 
-# Sets all of the appropriate queue pse's to complete
-# i.e. queue pse's concerned with the model for this build
-sub complete_queue_pses {
-    my $self = shift;
-    my $model = $self->model;
-    my $processing_profile_id = $model->processing_profile_id;
-    my $subject_name = $model->subject_name;
-
-    my $process_step = GSC::ProcessStep->get(process_to => 'queue instrument data for genome modeling');
-    unless ($process_step) {
-        $self->error_message("Failed to get queue process step in complete_queue_pses");
-        return;
-    }
-
-    my @pses = GSC::PSE->get(
-                                ps_id => $process_step->ps_id,
-                                pse_status => 'inprogress',
-                            );
-    for my $pse (@pses) {
-        # We only care about pse's that have the same processing profile and sample name
-        unless(defined($pse->added_param('subject_name')) && 
-              ($pse->added_param('subject_name') eq $subject_name)) {
-            next;
-        }
-
-        my $processing_profile_id = $pse->added_param('processing_profile');
-        next unless (defined($processing_profile_id)); 
-        my $processing_profile = Genome::ProcessingProfile->get($processing_profile_id);
-        
-        unless($pse->added_param('processing_profile_name') eq $processing_profile->name) {
-            next;
-        }
-
-        # Now that we are sure this is one of ours... complete it
-        $pse->pse_status('complete');
-    }
-}
 
 1;
 
