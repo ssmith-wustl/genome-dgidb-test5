ca04ecb48e673b8ba02cfc62bdae968c4a83d517
jwalker <jwalker@genome.wustl.edu>
2009-01-12 21:06:52 +0000

    separate the de novo assembly build object from the build command
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42534 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Assembly.pm
+++ Genome/Model/Command/Build/Assembly.pm
@@ -11,24 +11,6 @@ class Genome::Model::Command::Build::Assembly {
         ],
  };
 
-sub create {
-    my $class = shift;
-    my $self = $class->SUPER::create(@_);
-
-    my $model = $self->model;
-
-    my @instrument_data = $model->instrument_data;
-
-    unless (scalar(@instrument_data) && ref($instrument_data[0])  &&  $instrument_data[0]->isa('Genome::InstrumentData::454')) {
-        $self->error_message('InstrumentData has not been added to model: '. $model->name);
-        $self->error_message("The following command will add all available InstrumentData:\ngenome model add-reads --model-id=".
-        $model->id .' --all');
-        $self->delete;
-        return;
-    }
-    return $self;
-}
-
 sub sub_command_sort_position { 40 }
 
 sub help_brief {
