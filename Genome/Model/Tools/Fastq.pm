1a8955c8497bd5a3aba602ccbf9537b3e2d48f1a
fdu <fdu@genome.wustl.edu>
2009-01-14 21:34:19 +0000

    rewrite
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42706 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Fastq.pm
+++ Genome/Model/Tools/Fastq.pm
@@ -4,15 +4,15 @@ use strict;
 use warnings;
 
 use Genome;
-use Data::Dumper;
+use Bio::SeqIO;
 
 class Genome::Model::Tools::Fastq {
     is => 'Command',
     has => [
         fastq_file => {
-            type => 'Text',
+            type        => 'Text',
             is_optional => 0,
-            doc => 'FASTA file. Quality file (if appropriate) will be named <fasta_file>\'.qual\'',
+            doc         => 'FASTQ file that contains both sequences and quality values',
         },
     ],
 };
@@ -21,92 +21,90 @@ sub help_brief {
     "tools for working with FASTQ files"
 }
 
+
 sub help_detail {
-    "Tools to work with fastq format sequence/quality files";
+    "Tools to work with fastq format files";
 }
+
 
 sub create { 
     my $class = shift;
 
     my $self = $class->SUPER::create(@_);
     $self->{_cwd} = Cwd::getcwd();
-    $self->fasta_file( Cwd::abs_path( $self->fasta_file ) );
-    my ($base, $directory) = File::Basename::fileparse( $self->fasta_file );
-    chdir $directory
-        or ( $self->error_message("Can't access directory ($directory): $!") and return );
-    $self->{_fasta_base} = $base;
+    $self->fastq_file(Cwd::abs_path($self->fastq_file));
+    my ($basename, $directory) = File::Basename::fileparse( $self->fastq_file );
+    
+    $self->{_fastq_basename}  = $basename;
+    $self->{_fastq_directory} = $directory;
 
     return $self;
 }
 
+
 sub DESTROY {
     my $self = shift;
-
-    chdir $self->_cwd;
-    
+    $self->chdir_cwd;    
     return 1;
 }
 
-sub _cwd {
+
+sub cwd {
     return shift->{_cwd};
 }
 
-sub _fasta_base {
-    return shift->{_fasta_base};
-}
 
-sub qual_base {
+sub chdir_cwd {
     my $self = shift;
 
-    return sprintf('%s.qual', $self->_fasta_base);
+    unless (chdir $self->cwd) {
+        $self->error_message(sprintf('Can\'t access cwd (%s)', $self->cwd));
+        return;
+    }
+    return 1;
 }
 
-sub qual_file {
-    my $self = shift;
 
-    return sprintf('%s.qual', $self->fasta_file);
+sub fastq_basename {
+    return shift->{_fastq_basename};
 }
+
+
+sub fastq_directory {
+    return shift->{_fastq_directory};
+}
+
+
+sub get_fastq_reader {
+    return _get_bioseq_reader(@_, 'fastq');
+}
+
+
+sub _get_bioseq_reader {
+    return _get_bioseq(@_, '<');
+}
+
+
+sub get_fastq_writer {
+    return _get_bioseq_writer(@_, 'fastq');
+}
+
+
+sub _get_bioseq_writer {
+    return _get_bioseq(@_, '>');
+}
+
+
+sub _get_bioseq {
+    my ($self, $file, $format, $rw) = @_;
+
+    return Bio::SeqIO->new(
+        -file   => $rw.' '.$file,
+        -format => $format,
+    );
+}
+    
 
 1;
 
-=pod
-
-=head1 Name
-
-ModuleTemplate
-
-=head1 Synopsis
-
-=head1 Usage
-
-=head1 Methods
-
-=head2 
-
-=over
-
-=item I<Synopsis>
-
-=item I<Arguments>
-
-=item I<Returns>
-
-=back
-
-=head1 See Also
-
-=head1 Disclaimer
-
-Copyright (C) 2005 - 2008 Washington University Genome Sequencing Center
-
-This module is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY or the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-=head1 Author(s)
-
-B<Eddie Belter> I<ebelter@watson.wustl.edu>
-
-=cut
-
-#$HeadURL: /gscpan/perl_modules/trunk/Genome/Model/Tools/Fasta.pm $
-#$Id: /gscpan/perl_modules/trunk/Genome/Model/Tools/Fasta.pm 39201 2008-09-29T18:26:47.471156Z ebelter  $
 
