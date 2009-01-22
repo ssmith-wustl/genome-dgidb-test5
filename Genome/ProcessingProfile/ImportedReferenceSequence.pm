c84f0916f6355ddfc2bd3843e71fea63c137998a
jwalker <jwalker@genome.wustl.edu>
2009-01-22 19:19:45 +0000

    add methods used for building
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42897 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ImportedReferenceSequence.pm
+++ Genome/ProcessingProfile/ImportedReferenceSequence.pm
@@ -10,4 +10,19 @@ class Genome::ProcessingProfile::ImportedReferenceSequence{
     is => 'Genome::ProcessingProfile',
 };
 
+sub stages {
+    return (qw/
+              imported_reference_sequence
+              verify_successful_completion
+            /);
+}
 
+sub imported_reference_sequence_job_classes {
+    return (qw/
+            Genome::Model::Command::Build::ImportedReferenceSequence::Run
+        /);
+}
+
+sub imported_reference_sequence_objects {
+    return 1;
+}
