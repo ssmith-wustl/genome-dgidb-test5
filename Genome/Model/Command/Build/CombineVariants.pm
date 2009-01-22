9f6ce7cf518592ac8b35ad9bb2545d78a9c4bb41
jwalker <jwalker@genome.wustl.edu>
2009-01-22 20:00:44 +0000

    moved execute logic out to event module that will run with build api
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42936 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/CombineVariants.pm
+++ Genome/Model/Command/Build/CombineVariants.pm
@@ -2,9 +2,8 @@ package Genome::Model::Command::Build::CombineVariants;
 
 use strict;
 use warnings;
+
 use Genome;
-use File::Copy "cp";
-use File::Basename;
 
 class Genome::Model::Command::Build::CombineVariants {
     is => 'Genome::Model::Command::Build',
@@ -28,39 +27,5 @@ One build of a given PolyphredPolyscan model.
 EOS
 }
 
-sub execute {
-    my $self = shift;
-
-    my $model = $self->model;
-    unless ($model){
-        $self->error_message("Couldn't find model for id ".$self->model_id);
-        die;
-    }
-    $self->status_message("Found Model: " . $model->name);
-
-    $self->create_directory($self->data_directory);
-    unless (-d $self->data_directory) {
-        $self->error_message("Failed to create new build dir: " . $self->data_directory);
-        die;
-    }
-
-    $self->status_message("Combining variants");
-    $model->combine_variants();
-    
-    $self->status_message("Annotating variants");
-    $model->annotate_variants();
-
-    $self->status_message("Writing maf files");
-    $model->write_post_annotation_maf_files();
-
-    $model->last_complete_build_id($self->build_id);
-
-    return $model;
-}
-
-
-sub _get_sub_command_class_name{
-  return __PACKAGE__; 
-}
 
 1;
