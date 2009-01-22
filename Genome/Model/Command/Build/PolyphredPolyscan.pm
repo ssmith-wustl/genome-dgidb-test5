1feebfcc20e7dc9665dfc3c4145a7cc7ce743deb
jwalker <jwalker@genome.wustl.edu>
2009-01-22 20:01:09 +0000

    moved execute logic out to event module that will run with build api
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42937 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/PolyphredPolyscan.pm
+++ Genome/Model/Command/Build/PolyphredPolyscan.pm
@@ -5,9 +5,6 @@ use warnings;
 
 use Genome;
 
-use File::Copy "cp";
-use File::Basename;
-
 class Genome::Model::Command::Build::PolyphredPolyscan {
     is => 'Genome::Model::Command::Build',
 
@@ -29,66 +26,6 @@ sub help_detail {
     return <<"EOS"
 One build of a given PolyphredPolyscan model.
 EOS
-}   
- 
-sub execute {
-    my $self = shift;
- 
-       
-    my $model = $self->model;
-    unless ($model){
-        $self->error_message("Couldn't find model for id ".$self->model_id);
-        die;
-    }
-    $self->status_message("Found Model: " . $model->name);
-
-    $self->create_directory($self->data_directory);
-    unless (-d $self->data_directory) {
-        $self->error_message("Failed to create new build dir: " . $self->data_directory);
-        die;
-    }
-
-    my $instrument_data_directory = $self->build->instrument_data_directory;
-
-    if (-d $instrument_data_directory ){
-        $self->error_message("new current instrument data dir $instrument_data_directory exists before it should!");
-        die;
-    }
-
-    $self->create_directory($instrument_data_directory);
-
-    unless (-d $instrument_data_directory){
-        $self->error_message("New current instrument data dir $instrument_data_directory does not exist");
-        die;
-    }
-
-    my @pending_instrument_data_files = $model->pending_instrument_data_files;
-    
-    for my $file (@pending_instrument_data_files) {
-        my $link_to = readlink $file;
-        unless ($link_to){
-            $self->error_message("couldn't extract source instrumnent data link from instrument data file $file");
-            die;
-        }
-        my $new_link = basename($file);
-        $new_link = "$instrument_data_directory/$new_link";
-        symlink($link_to, $new_link);
-
-        unless (-e $new_link) {
-            $self->error_message("Failed to link $new_link to $file->$link_to ");
-            die;
-        }
-    }
-    
-    $model->last_complete_build_id($self->build_id);
-    $model->current_running_build_id(undef);
-
-    $self->build->complete_queue_pses;
-    return $model;
-}
-
-sub _get_sub_command_class_name{
-  return __PACKAGE__; 
 }
 
 
