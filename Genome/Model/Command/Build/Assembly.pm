810e593e33f6820d69037b0c3ebed889addfefc9
jwalker <jwalker@genome.wustl.edu>
2009-01-22 19:55:43 +0000

    moved building logic to processing profile
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42929 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Assembly.pm
+++ Genome/Model/Command/Build/Assembly.pm
@@ -29,57 +29,6 @@ One build of a given assembly model.
 EOS
 }
 
-sub stages {
-    my @stages = qw/
-        setup_project
-        assemble
-        verify_successful_completion
-    /;
-    return @stages;
-}
-
-sub setup_project_job_classes {
-    my @classes = qw/
-            Genome::Model::Command::Build::Assembly::AssignReadSetToModel
-            Genome::Model::Command::Build::Assembly::FilterReadSet
-            Genome::Model::Command::Build::Assembly::TrimReadSet
-            Genome::Model::Command::Build::Assembly::AddReadSetToProject
-    /;
-    return @classes;
-}
-
-sub assemble_job_classes {
-    my @classes = qw/
-            Genome::Model::Command::Build::Assembly::Assemble
-    /;
-    return @classes;
-}
-
-sub verify_successful_completion_job_classes {
-    my @classes = qw/
-            Genome::Model::Command::Build::VerifySuccessfulCompletion
-    /;
-    return @classes;
-}
-
-sub setup_project_objects {
-    my $self = shift;
-    return $self->model->unbuilt_instrument_data;
-}
-
-sub assemble_objects {
-    my $self = shift;
-    return 1;
-}
-
-sub verify_successful_completion_objects {
-    my $self = shift;
-    return 1;
-}
-
-sub _get_sub_command_class_name{
-  return __PACKAGE__;
-}
 
 1;
 
