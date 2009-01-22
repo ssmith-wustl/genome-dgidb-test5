9363b56723cd618b718f98b620019b909951bb93
jwalker <jwalker@genome.wustl.edu>
2009-01-22 19:20:29 +0000

    add methods used for building
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42898 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ManualReview.pm
+++ Genome/ProcessingProfile/ManualReview.pm
@@ -9,4 +9,21 @@ class Genome::ProcessingProfile::ManualReview {
     is => 'Genome::ProcessingProfile::Composite',
 };
 
+sub stages {
+    return (qw/
+            manual_review
+            verify_successful_completion
+            /);
+}
+
+sub manual_review_job_classes {
+    return (qw/
+            Genome::Model::Command::Build::ManualReview::Run
+        /);
+}
+
+sub manual_review_objects {
+    return 1;
+}
+
 1;
