8d7840f50d98a37a081c928a15e1320fc3a0b5e5
jwalker <jwalker@genome.wustl.edu>
2009-01-22 19:17:26 +0000

    add methods used for building
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42893 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/MicroArrayIllumina.pm
+++ Genome/ProcessingProfile/MicroArrayIllumina.pm
@@ -14,3 +14,20 @@ sub params_for_class {
     my $class = shift;
     return $class->SUPER::params_for_class;
 }
+
+sub stages {
+    return (qw/
+            micro_array_illumina
+            verify_successful_completion
+            /);
+}
+
+sub micro_array_illumina_job_classes {
+    return (qw/
+            Genome::Model::Command::Build::MicroArrayIllumina::Run
+        /);
+}
+
+sub micro_array_illumina_objects {
+    return 1;
+}
