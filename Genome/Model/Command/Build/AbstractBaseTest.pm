5df8553f2c7b7e5ec62232f3ce6e771e0b392efe
jwalker <jwalker@genome.wustl.edu>
2009-01-22 19:50:24 +0000

    The command build (build event) no longer is responsible for building each subclass. The processing profile has this.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42922 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AbstractBaseTest.pm
+++ Genome/Model/Command/Build/AbstractBaseTest.pm
@@ -9,48 +9,6 @@ class Genome::Model::Command::Build::AbstractBaseTest {
     is => 'Genome::Model::Command::Build',
 };
 
-sub stages {
-    my @stages = qw/
-                    stage1
-                    stage2
-                   /;
-    return @stages;
-}
-
-sub stage1_objects {
-    my $self = shift;
-    my $model = $self->model;
-    my @read_sets = $model->read_sets;
-    return @read_sets;
-}
-
-sub stage2_objects {
-    my $self = shift;
-    my $model = $self->model;
-    my @ref_seqs = $model->ref_seqs;
-    return @ref_seqs;
-}
-
-sub stage1_job_classes {
-    return (
-            'Genome::Model::Command::Build::AbstractBaseTest::StageOneJobOne',
-            'Genome::Model::Command::Build::AbstractBaseTest::StageOneJobTwo',
-            'Genome::Model::Command::Build::AbstractBaseTest::StageOneJobThree',
-        );
-}
-
-sub stage2_job_classes {
-    return (
-            'Genome::Model::Command::Build::AbstractBaseTest::StageTwoJobOne',
-            'Genome::Model::Command::Build::AbstractBaseTest::StageTwoJobTwo',
-        );
-}
-
-sub _get_sub_command_class_name{
-  return __PACKAGE__;
-}
-
-
 package Genome::Model::Command::Build::AbstractBaseTest::StageOneJobOne;
 
 class Genome::Model::Command::Build::AbstractBaseTest::StageOneJobOne {
