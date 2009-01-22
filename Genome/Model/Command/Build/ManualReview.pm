7e272c1de7e1562c9262375a617adacd71290a6a
jwalker <jwalker@genome.wustl.edu>
2009-01-22 19:59:58 +0000

    moved execute logic out to event module that will run with build api
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@42934 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ManualReview.pm
+++ Genome/Model/Command/Build/ManualReview.pm
@@ -26,33 +26,5 @@ One build of a given ManualReview model.
 EOS
 }
 
-sub execute {
-    my $self = shift;
-
-    # Some basic logic... may not need to be here depending on what you are doing I suppose, take it or leave it
-    my $model = $self->model;
-    unless ($model){
-        $self->error_message("Couldn't find model for id ".$self->model_id);
-        die;
-    }
-    $self->status_message("Found Model: " . $model->name);
-
-    $self->create_directory($self->data_directory);
-    unless (-d $self->data_directory) {
-        $self->error_message("Failed to create new build dir: " . $self->data_directory);
-        die;
-    }
-
-    # TODO replace this with good logic
-    $self->status_message("Build logic not implemented for Manual Review models yet... so this isnt really doing anything");
-    return 1;
-    
-    return $model;
-}
-
-
-sub _get_sub_command_class_name{
-  return __PACKAGE__; 
-}
 
 1;
