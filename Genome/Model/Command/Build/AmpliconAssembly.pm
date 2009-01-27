266018e004a2f8ceced89e405c0e277aa001a8c7
ebelter <ebelter@genome.wustl.edu>
2009-01-27 19:34:13 +0000

    removed use statements
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43070 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly.pm
+++ Genome/Model/Command/Build/AmpliconAssembly.pm
@@ -5,16 +5,6 @@ use warnings;
 
 use Genome;
 
-use Genome::Model::Command::Build::AmpliconAssembly::VerifyInstrumentData;
-use Genome::Model::Command::Build::AmpliconAssembly::Assemble;
-use Genome::Model::Command::Build::AmpliconAssembly::Collate;
-use Genome::Model::Command::Build::AmpliconAssembly::Orient;
-#use Genome::Model::Command::Build::AmpliconAssembly::QualityHistogram;
-use Genome::Model::Command::Build::AmpliconAssembly::PostProcess;
-use Genome::Model::Command::Build::AmpliconAssembly::PostProcess::Composition;
-use Genome::Model::Command::Build::AmpliconAssembly::PostProcess::Reference;
-use Genome::Model::Command::Build::AmpliconAssembly::CleanUp;
-;
 class Genome::Model::Command::Build::AmpliconAssembly {
     is => 'Genome::Model::Command::Build',
 };
