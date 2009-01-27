76f3bc0acbbed3aef868ae0e5162491de30be27e
ebelter <ebelter@genome.wustl.edu>
2009-01-27 19:29:58 +0000

    moved dump instr data into own method
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43066 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/VerifyInstrumentData.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/VerifyInstrumentData.pm
@@ -27,33 +27,19 @@ sub execute {
     my $self = shift;
 
     if ( $self->model->sequencing_center eq 'gsc' ) {
-        $self->_dump_instrument_data
+        $self->_link_instrument_data
             or return;
     }
 
     return $self->model->amplicons; # Error msg is on model if no amplicons
 }
 
-sub _dump_instrument_data {
+sub _link_instrument_data {
     my $self = shift;
 
     my $chromat_dir = $self->model->chromat_dir;
     for my $ida ( $self->model->instrument_data_assignments ) {
-        unless ( $ida->first_build_id ) {
-            unless ( $ida->instrument_data->dump_to_file_system ) {
-                $self->error_message(
-                    sprintf(
-                        'Error dumping instrument data (%s <ID: %s) for model (%s <ID %s)',
-                        $ida->instrument_data->run_name,
-                        $ida->instrument_data->id,
-                        $self->model->name,
-                        $self->model->id,
-                    )
-                );
-                return;
-            }
-            $ida->first_build_id( $self->build_id );
-        }
+        $self->_dump_unbuilt_instrument_data($ida) or return;
 
         my $instrument_data_dir = $ida->instrument_data->resolve_full_path;
         my $dh = Genome::Utility::FileSystem->open_directory($instrument_data_dir)
@@ -68,6 +54,27 @@ sub _dump_instrument_data {
         }
     }
 
+    return 1;
+}
+
+sub _dump_unbuilt_instrument_data {
+    my $self = shift;
+    my $ida = shift;
+    unless ( $ida->first_build_id ) {
+        unless ( $ida->instrument_data->dump_to_file_system ) {
+            $self->error_message(
+                sprintf(
+                    'Error dumping instrument data (%s <ID: %s) for model (%s <ID %s)',
+                    $ida->instrument_data->run_name,
+                    $ida->instrument_data->id,
+                    $self->model->name,
+                    $self->model->id,
+                )
+            );
+            return;
+        }
+        $ida->first_build_id( $self->build_id );
+    }
     return 1;
 }
 
