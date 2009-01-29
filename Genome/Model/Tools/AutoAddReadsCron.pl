fc6415ce2f2a4afb326592f51f0e1f1d79488ddc
jeldred <jeldred@genome.wustl.edu>
2009-01-29 19:49:03 +0000

    now with even more hyper-abrummettization of the cron run environment
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43145 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/AutoAddReadsCron.pl
+++ Genome/Model/Tools/AutoAddReadsCron.pl
@@ -6,6 +6,7 @@ use warnings;
 use lib '/gsc/scripts/lib/perl';
 
 my $cmd='/gsc/scripts/bin/gt auto-add-reads';
+$ENV{'PERL5LIB'} = '/gsc/scripts/lib/perl/:' . $ENV{'PERL5LIB'};
 system($cmd);
 
 1;
