decee2ad089737e24f2a34da9539b8658ca656ec
mjohnson <mjohnson@genome.wustl.edu>
2009-02-03 16:49:59 +0000

    Nuked 'use lib' statements pointing to bioperl-svn.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43228 127c0c0e-971a-0410-ad48-868c1a942d8d

--- MGAP.pm
+++ MGAP.pm
@@ -6,9 +6,6 @@ use strict;
 use UR;
 use Workflow;
 
-use lib '/gscmnt/temp212/info/annotation/bioperl-svn/bioperl-live';
-use lib '/gscmnt/temp212/info/annotation/bioperl-svn/bioperl-run';
-
 class MGAP {
     is => ['UR::Namespace'],
     type_name => 'mgap',
