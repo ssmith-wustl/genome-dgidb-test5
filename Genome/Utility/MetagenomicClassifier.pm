4125dac2122b41bf9a3f109e5b8519859b67ca99
ebelter <ebelter@genome.wustl.edu>
2009-02-04 22:13:18 +0000

    some class methods for MG classification
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43283 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Utility/MetagenomicClassifier.pm
@@ -0,0 +1,90 @@
+package Genome::Utility::MetagenomicClassifier;
+
+use strict;
+use warnings;
+
+#use base 'Class::Singleton';
+
+use Bio::Taxon;
+
+my @RANKS = (qw/ domain kingdom phylum class order family genus species /); 
+sub taxonomic_ranks {
+    return @RANKS;
+}
+
+sub taxonomic_rank_at {
+    return $RANKS[ $_[1] ];
+}
+
+my @DOMAINS = (qw/ Archaea Bacteria Eukarya/);
+sub domains {
+    return @DOMAINS;
+}
+
+my @KINGDOMS = (qw/ Animalia Archaea Eubacteria Fungi Plantae Protista /);
+sub kingdoms {
+    return @KINGDOMS;
+}
+
+sub create_taxon {
+    my ($self, %params) = @_;
+
+    my $taxon = Bio::Taxon->new(
+        '-id' => $params{id},
+        '-rank' => $params{rank},
+    );
+
+    if ( $params{tags} ) {
+        for my $key ( keys %{$params{tags}} ) {
+            $taxon->add_tag_value($key, $params{tags}->{$key});
+        }
+    }
+
+    $params{ancestor}->add_Descendent($taxon) if $params{ancestor};
+
+    return $taxon;
+}
+
+1;
+
+=pod
+
+=head1 Name
+
+ModuleTemplate
+
+=head1 Synopsis
+
+=head1 Usage
+
+=head1 Methods
+
+=head2 
+
+=over
+
+=item I<Synopsis>
+
+=item I<Arguments>
+
+=item I<Returns>
+
+=back
+
+=head1 See Also
+
+=head1 Disclaimer
+
+Copyright (C) 2005 - 2008 Washington University Genome Sequencing Center
+
+This module is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY or the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+=head1 Author(s)
+
+B<Eddie Belter> I<ebelter@watson.wustl.edu>
+
+=cut
+
+#$HeadURL$
+#$Id$
+
