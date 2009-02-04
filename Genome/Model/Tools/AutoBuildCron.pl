e1ef4b5142f7adfa93b9e235c5d46620516fcfe1
jeldred <jeldred@genome.wustl.edu>
2009-02-04 21:13:23 +0000

    changed above Genome
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43278 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/AutoBuildCron.pl
+++ Genome/Model/Tools/AutoBuildCron.pl
@@ -3,9 +3,10 @@
 use strict;
 use warnings;
 
-use above 'Genome';
-use lib '/gsc/scripts/lib/perl';
-$ENV{'PERL5LIB'} = '/gsc/scripts/lib/perl/:' . $ENV{'PERL5LIB'};
+use Genome;
+
+#use lib '/gsc/scripts/lib/perl';
+#$ENV{'PERL5LIB'} = '/gsc/scripts/lib/perl/:' . $ENV{'PERL5LIB'};
 
 my $results = Genome::Model::Tools::AutoBuild->execute();
 
