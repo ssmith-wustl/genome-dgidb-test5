58001c21a1a0c41c5096586a6cb091778e8ed2a4
jeldred <jeldred@genome.wustl.edu>
2009-02-04 20:53:32 +0000

    AutoBuild broken out from AutoAddReads
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43276 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/AutoBuildCron.pl
+++ Genome/Model/Tools/AutoBuildCron.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use above 'Genome';
+use lib '/gsc/scripts/lib/perl';
+$ENV{'PERL5LIB'} = '/gsc/scripts/lib/perl/:' . $ENV{'PERL5LIB'};
 
 my $results = Genome::Model::Tools::AutoBuild->execute();
 
