13c5b93d100d29643534789b5a246fbfde8fdd05
dlarson <dlarson@genome.wustl.edu>
2009-02-09 19:01:05 +0000

    Added a bunch of info on number of bases etc
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43388 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Library/GatherLibraryMetrics.pm
+++ Genome/Model/Tools/Library/GatherLibraryMetrics.pm
@@ -7,6 +7,8 @@ use Genome;
 use Genome::RunChunk; # get access to GSC namespace
 use Command;
 use IO::File;
+use GSCApp; 
+App->init;
 
 class Genome::Model::Tools::Library::GatherLibraryMetrics {
     is => 'Command',
@@ -39,54 +41,103 @@ sub execute {
         event_status => 'Succeeded'
     );
     #Convert events to ReadSet objects
-    my @readsets = map {Genome::Model::ReadSet->get(read_set_id => $_->read_set_id)} @events;
+    my @readsets = map {Genome::Model::ReadSet->get(read_set_id => $_->read_set_id, model_id => $model_id)} @events;
 
     my %stats_for;
     my %readset_stats;
 
+        print STDOUT join "\t",("Name","#Reads_Mapped","#Reads_Total","isPaired","#Reads_Mapped_asPaired","Median_Insert_Size","Standard_Deviation_Above_Insert_Size",),"\n";
 #Completely undeprecated loop over the readsets
     foreach my $read_set_link (@readsets) {
         my $library = $read_set_link->library_name;
+        unless(defined($library)) {
+            $self->error_message("No library defined for ".$read_set_link->read_set_id);
+            next;
+        }
         my $hash = $read_set_link->get_alignment_statistics;
         $stats_for{$library}{total_read_sets} += 1;
-
-        next unless defined($hash); #ignore runs where there are no aligner outputs (shouldn't really happen anymore)
+        unless(defined($hash)) {
+            #ignore runs where there are no aligner outputs (shouldn't really happen anymore)
+            $stats_for{$library}{no_aligner_stats} += 1;
+            next;
+        }
         $stats_for{$library}{$hash->{isPE}}{mapped} += $hash->{mapped};
         $stats_for{$library}{$hash->{isPE}}{total} += $hash->{total};
         $stats_for{$library}{$hash->{isPE}}{paired} += $hash->{paired};
         $stats_for{$library}{$hash->{isPE}}{read_sets} += 1;
         my $sls = GSC::RunLaneSolexa->get($read_set_link->read_set_id); 
+        unless(defined($sls)) {
+            $self->error_message("Unable to find RunLaneSolexa object for ".$read_set_link->read_set_id);
+            next;
+        }
         my $median_insert_size = $sls->median_insert_size;
         my $sd_above_insert_size = $sls->sd_above_insert_size;
-        if(defined($median_insert_size)) {
+        my $lane_name = $read_set_link->short_name."_".$read_set_link->subset_name;
+        if(defined($median_insert_size) && $hash->{isPE}) {
             $stats_for{$library}{median_insert_size} += $median_insert_size;
-            $stats_for{$library}{median_insert_size_n} ++;
+            $stats_for{$library}{median_insert_size_n} +=1;
         }
-        if(defined($sd_above_insert_size)) {
+        if(defined($sd_above_insert_size) && $hash->{isPE}) {
             $stats_for{$library}{sd_above_insert_size} += $sd_above_insert_size;
-            $stats_for{$library}{sd_above_insert_size_n} ++;
+            $stats_for{$library}{sd_above_insert_size_n} += 1;
         }
-        #print join "\t",($hash->{mapped},$hash->{total},$hash->{paired},$sls->median_insert_size,$sls->sd_above_insert_size,"\n");
-    }
 
+        #clean things up for printing
+        unless(defined($median_insert_size)) {
+            $median_insert_size = '-';
+        }
+        unless(defined($sd_above_insert_size)) {
+            $sd_above_insert_size = '-';
+        }
+        $readset_stats{$lane_name} = join "\t",($lane_name, $hash->{mapped},$hash->{total},$hash->{isPE}, $hash->{paired},$median_insert_size,$sd_above_insert_size,),"\n";
+    }
+    foreach my $lane (sort keys %readset_stats) {
+        print $readset_stats{$lane};
+    }
+    my $total_lanes = 0;
+    my $total_reads = 0;
+    my $total_mapped_reads = 0;
+    my $total_paired_reads = 0;
+
+    print STDOUT "\n\n",'-' x 5,'Library Averages','-' x 5,"\n";
     foreach my $library (keys %stats_for) {
         print "$library: ",$stats_for{$library}{total_read_sets}, " Total Lanes\n";
-        print "\tPaired Lanes: ", $stats_for{$library}{1}{read_sets},"\n";
-        print "\t\tTotal Reads: ", $stats_for{$library}{1}{total}, "\n";
-        print "\t\tMapped Reads: ", $stats_for{$library}{1}{mapped}, "\n";
-        print "\t\tPaired Reads: ", $stats_for{$library}{1}{paired}, "\n\n";
-        print "\t\tThere were ", $stats_for{$library}{median_insert_size_n}, " out of ", $stats_for{$library}{1}{read_sets}, " lanes where the median insert size was available\n";
-        printf "\t\tFrom these the average median insert size was %0.2f\n\n",$stats_for{$library}{median_insert_size}/$stats_for{$library}{median_insert_size_n};
+        $total_lanes += $stats_for{$library}{total_read_sets};
+        if(exists($stats_for{$library}{1})) {
+            $total_reads += $stats_for{$library}{1}{total};
+            $total_mapped_reads += $stats_for{$library}{1}{mapped};
+            $total_paired_reads += $stats_for{$library}{1}{paired};
+
+            print "\tPaired Lanes: ", $stats_for{$library}{1}{read_sets},"\n";
+            print "\t\tTotal Reads: ", $stats_for{$library}{1}{total}, "\n";
+            print "\t\tMapped Reads: ", $stats_for{$library}{1}{mapped}, "\n";
+            print "\t\tPaired Reads: ", $stats_for{$library}{1}{paired}, "\n";
+            printf "\t\tPaired Rate: %0.02f%%\n", $stats_for{$library}{1}{paired}/$stats_for{$library}{1}{mapped} * 100; 
+            print "\t\tThere were ", $stats_for{$library}{median_insert_size_n}, " out of ", $stats_for{$library}{1}{read_sets}, " lanes where the median insert size was available\n";
+            printf "\t\tFrom these the average median insert size was %0.2f\n\n",$stats_for{$library}{median_insert_size}/$stats_for{$library}{median_insert_size_n};
 
 
-        print "\t\tThere were ", $stats_for{$library}{sd_above_insert_size_n}, " out of ", $stats_for{$library}{1}{read_sets}, " lanes where the sd above the insert size was available\n";
-        printf "\t\tFrom these the average sd above the insert size was %0.2f\n\n",$stats_for{$library}{sd_above_insert_size}/$stats_for{$library}{sd_above_insert_size_n};
+            print "\t\tThere were ", $stats_for{$library}{sd_above_insert_size_n}, " out of ", $stats_for{$library}{1}{read_sets}, " lanes where the sd above the insert size was available\n";
+            printf "\t\tFrom these the average sd above the insert size was %0.2f\n\n",$stats_for{$library}{sd_above_insert_size}/$stats_for{$library}{sd_above_insert_size_n};
+        }
         if(exists($stats_for{$library}{0})) {
+
+            $total_reads += $stats_for{$library}{0}{total};
+            $total_mapped_reads += $stats_for{$library}{0}{mapped};
             print "\tFragment Lanes: ", $stats_for{$library}{0}{read_sets},"\n";
             print "\t\tTotal Reads: ", $stats_for{$library}{0}{total}, "\n";
             print "\t\tMapped Reads: ", $stats_for{$library}{0}{mapped}, "\n";
         }
     }
+
+    #print model totals, this probably shouldn't go in this module but it can sit here for now
+    print STDOUT "\n\n",'-' x 5,'Model Averages','-' x 5,"\n";
+    print "\tTotal Lanes: ", $total_lanes, "\n";
+    printf "\tTotal Runs: %0.02f\n", $total_lanes / 8; 
+    print "\tTotal Reads: ", $total_reads, "\n";
+    print "\tMapped Reads: ", $total_mapped_reads, "\n";
+    print "\tPaired Reads: ", $total_paired_reads, "\n";
+    
     return 1;
 
 }
