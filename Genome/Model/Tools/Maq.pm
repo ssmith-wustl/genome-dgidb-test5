170ed80fe2400bcc599758a4a32f1f3c8956d820
ssmith <ssmith@genome.wustl.edu>
2009-02-16 07:54:40 +0000

    Added maq path translation.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43581 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Maq.pm
+++ Genome/Model/Tools/Maq.pm
@@ -3,8 +3,7 @@ package Genome::Model::Tools::Maq;
 use strict;
 use warnings;
 
-use Genome;                         # >above< ensures YOUR copy is used during development
-
+use Genome;
 use File::Basename;
 
 class Genome::Model::Tools::Maq {
@@ -36,13 +35,13 @@ EOS
 
 sub c_linkage_class {
     my $self = shift;
+    $DB::single = $DB::stopper;
 
-$DB::single = $DB::stopper;
     my $version = $self->use_version;
     $version =~ s/\./_/g;
 
     my $class_to_use = __PACKAGE__ . "::CLinkage$version";
-  
+    
     #eval "use above '$class_to_use';";
     eval "use $class_to_use;";
     if ($@) {
@@ -51,6 +50,51 @@ $DB::single = $DB::stopper;
     }
 
     return $class_to_use;
+}
+
+sub maq_path {
+    my $self = $_[0];
+    return $self->path_for_maq_version($self->use_version);
+}
+
+sub path_for_maq_version {
+    my $class = shift;
+    my $version = shift;
+    if ($version eq '0.6.3') {
+        return '/gsc/pkg/bio/maq/maq-0.6.3_x86_64-linux/maq';
+    } elsif ($version eq '0.6.4') {
+        return '/gsc/pkg/bio/maq/maq-0.6.4_x86_64-linux/maq';
+    } elsif ($version eq '0.6.5') {
+        return '/gsc/pkg/bio/maq/maq-0.6.5_x86_64-linux/maq';
+    } elsif ($version eq '0.6.8') {
+        return '/gsc/pkg/bio/maq/maq-0.6.8_x86_64-linux/maq';
+    } elsif ($version eq '0.7.1') {
+        return '/gsc/pkg/bio/maq/maq-0.7.1-64/bin/maq';     
+    } elsif ($version eq 'maq') {
+        return 'maq';
+    } else {
+        return;
+    }
+}
+
+sub path_for_mapsplit_version {
+    my $class = shift;
+    my $version = shift;
+    if ($version eq '0.6.3') {
+        return '/gscuser/charris/c-src-BLECH/mapsplit/mapsplit';
+    } elsif ($version eq '0.6.4') {
+        return '/gscuser/charris/c-src-BLECH/mapsplit/mapsplit';
+    } elsif ($version eq '0.6.5') {
+        return '/gscuser/charris/c-src-BLECH/mapsplit/mapsplit';
+    } elsif ($version eq '0.6.8') {
+        return '/gscuser/charris/c-src-BLECH/mapsplit/mapsplit';
+    } elsif ($version eq '0.7.1') {
+        return '/gscuser/charris/c-src-BLECH/mapsplit/mapsplit_long';
+    } elsif ($version eq 'maq') {
+        return;
+    } else {
+        return;
+    }
 }
 
 1;
