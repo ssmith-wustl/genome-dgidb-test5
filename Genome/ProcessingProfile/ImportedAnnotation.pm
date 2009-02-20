afa5a345493e7235accf7b24febd673a3fb8bc29
adukes <adukes@genome.wustl.edu>
2009-02-20 23:10:48 +0000

    imported annotation skeleton classes need to be deployed since there is a model in the db already
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43812 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/ImportedAnnotation.pm
@@ -0,0 +1,36 @@
+package Genome::ProcessingProfile::ImportedAnnotation;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::ProcessingProfile::ImportedAnnotation{
+    is => 'Genome::ProcessingProfile',
+    has => [
+        reference_sequence_model_id => {
+            doc => 'What genes the analysis should be limited to. Comma delimited, leave blank for no limitation.',
+            is_optional => 0,
+            is_mutable  => 1,
+            via         => 'params',
+            to          => 'value',
+            where       => [name => 'reference_sequence_model_id'],
+        },
+    ],
+    
+};
+
+sub stages {
+    return (qw/
+              imported_annotation
+              verify_successful_completion
+            /);
+}
+
+sub imported_reference_sequence_job_classes {
+    return (qw/
+        Genome::Model::Command::Build::ImportedAnnotation::Run
+        /);
+}
+
+1;
