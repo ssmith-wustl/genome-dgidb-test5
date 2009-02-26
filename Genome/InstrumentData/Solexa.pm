e2afdd4ec049a0f09d7b7c7694a11f2d0da3d845
jwalker <jwalker@genome.wustl.edu>
2009-02-26 22:10:46 +0000

    methods to resolve bfq files and calculate paired end and read counts correctly
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@43978 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Solexa.pm
+++ Genome/InstrumentData/Solexa.pm
@@ -24,45 +24,41 @@ EOS
         # TODO: fill these, even though this is a non-creatable class
         unique_reads_across_library     => { },
         duplicate_reads_across_library  => { },
-        read_length                     => { }, 
-        clusters                        => { },
+        read_length                     => { },
         run_type                        => { },
         gerald_directory                => { },
         median_insert_size              => { },
         sd_above_insert_size            => { },
-        is_external                     => { },
         flow_cell_id                    => { }, # = short name
         lane                            => { }, # = subset_name
-        
+        is_external                     => { },
+        clusters                        => { via => '_run_lane_solexa', to => 'filt_clusters' },
         short_name => {
             doc => 'The essential portion of the run name which identifies the run.  The rest is redundent information about the instrument, date, etc.',
             is => 'Text', 
             calculate_from => ['run_name'],
             calculate => q|($run_name =~ /_([^_]+)$/)[0]|
         },
-        
-        is_paired_end                   => { 
-                                             calculate_from => ['run_type'],
-                                             calculate => q| if ($run_type eq 'Paired End Read 2') {
-                                                                return 2;
+        is_paired_end                   => {
+                                            calculate_from => ['run_type'],
+                                            calculate => q| if (defined($run_type) and $run_type =~ m/Paired End Read (\d)/) {
+                                                                return $1;
                                                              }
                                                              else {
-                                                                return 0;
-                                                             } |,
-                                             #calc_sql => "(case when run_type = 'Paired End Read 2' then 2 else 0 end) is_paired_end",
-                                           },
-        
+                                                                 return 0;
+                                                             } |
+                                        },
         _run_lane_solexa => {
             doc => 'Solexa Lane Summary from LIMS.',
             is => 'GSC::RunLaneSolexa',
             calculate => q| GSC::RunLaneSolexa->get($id); |,
             calculate_from => ['id']
         },
-         
+
         # deprecated, compatible with Genome::RunChunk::Solexa
         genome_model_run_id => {},
         limit_regions       => {},
-        
+
     ],
 };
 
@@ -110,6 +106,114 @@ sub resolve_full_path {
 sub dump_to_file_system {
     #$self->warning_message("Method 'dump_data_to_file_system' not implemented");
     return 1;
+}
+
+sub bfq_filenames {
+    my $self = shift;
+    my $seq_dedup = shift;
+    my @bfqs;
+    if ($self->is_external) {
+        @bfqs = $self->resolve_external_bfq_filenames;
+    } else {
+        @bfqs = $self->resolve_bfq_filenames;
+    }
+    return @bfqs;
+}
+
+sub desc {
+    my $self = shift;
+    return $self->full_name .'('. $self->id .')';
+}
+
+sub resolve_bfq_filenames {
+    my $self = shift;
+    my $seq_dedup = shift;
+
+    my $lane = $self->subset_name;
+
+    ###################
+    #
+    #srf 2 fastq goes here
+    #if ->srf_path filename is not null and exists, run srf2fastq.
+    #figure out how it will spit out paired data.
+    #dump this crap into temp on the blade, PROBABLY don't run sol2sanger...
+    #still run fastq2bfq, rest of pipeline continues unaltered.
+    #####################
+
+
+
+    my $gerald_directory = $self->gerald_directory;
+    unless ($gerald_directory) {
+        die('No gerald directory in the database for or '. $self->desc);
+    }
+    unless (-d $gerald_directory) {
+        die('No gerald directory on the filesystem for '. $self->desc .' : '. $gerald_directory);
+    }
+
+    # handle fragment or paired-end data
+    my @solexa_output_paths;
+    if ($self->is_paired_end) {
+        if (-e "$gerald_directory/s_${lane}_1_sequence.txt") {
+            push @solexa_output_paths, "$gerald_directory/s_${lane}_1_sequence.txt";
+        }
+        elsif (-e "$gerald_directory/Temp/s_${lane}_1_sequence.txt") {
+            push @solexa_output_paths, "$gerald_directory/Temp/s_${lane}_1_sequence.txt";
+        }
+        else {
+            die "No gerald forward data in directory for lane $lane! $gerald_directory";
+        }
+
+        if (-e "$gerald_directory/s_${lane}_2_sequence.txt") {
+            push @solexa_output_paths, "$gerald_directory/s_${lane}_2_sequence.txt";
+        }
+        elsif (-e "$gerald_directory/Temp/s_${lane}_2_sequence.txt") {
+            push @solexa_output_paths, "$gerald_directory/Temp/s_${lane}_2_sequence.txt";
+        }
+        else {
+            die "No gerald reverse data in directory for lane $lane! $gerald_directory";
+        }
+    }
+    else {
+        if (-e "$gerald_directory/s_${lane}_sequence.txt") {
+            push @solexa_output_paths, "$gerald_directory/s_${lane}_sequence.txt";
+        }
+        elsif (-e "$gerald_directory/Temp/s_${lane}_sequence.txt") {
+            push @solexa_output_paths, "$gerald_directory/Temp/s_${lane}_sequence.txt";
+        }
+        else {
+            die "No gerald data in directory for lane $lane! $gerald_directory";
+        }
+    }
+
+    return @solexa_output_paths;
+}
+
+sub resolve_external_bfq_filenames {
+    my $self = shift;
+    my $seq_dedup = shift;
+
+    my @bfq_pathnames;
+    my $bfq_pathname = $self->create_temp_file_path('bfq');
+    unless ($bfq_pathname) {
+        die "Failed to create temp file for bfq!";
+    }
+    return ($bfq_pathname);
+}
+
+sub _calculate_total_read_count {
+    my $self = shift;
+
+    if($self->is_external) {
+        my $data_path_object = Genome::MiscAttribute->get(entity_id => $self->id, property_name=>'full_path');
+        my $data_path = $data_path_object->value;
+        my $lines = `wc -l $data_path`;
+        return $lines/4;
+    }
+    if ($self->clusters <= 0) {
+        die('Impossible value '. $self->clusters .' for clusters field for solexa lane '. $self->id);
+    }
+
+    return $self->clusters;
 }
 
 1;
