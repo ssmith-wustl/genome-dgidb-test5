fa7c6b48b2a841f49f789023d2eaf41efb8bc0d8
fdu <fdu@genome.wustl.edu>
2009-02-27 19:48:56 +0000

    update
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44006 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Fastq/ToPhdballScf.pm
+++ Genome/Model/Tools/Fastq/ToPhdballScf.pm
@@ -17,8 +17,7 @@ class Genome::Model::Tools::Fastq::ToPhdballScf {
         },
         scf_dir => {
             is  => 'String',
-            doc => 'The path to place scf trace files',
-            default => './chromat_dir',
+            doc => 'The path to place scf trace files. This is also a flag to make scf traces',
         },
         ball_file => {
             is  => 'String',
@@ -75,13 +74,16 @@ sub create {
             return;
         }
     }
-    my $scf_dir = $self->scf_dir;
 
-    unless (-d $scf_dir) {
-        mkdir $scf_dir, 0777;
+    if ($self->scf_dir) {
+        my $scf_dir = $self->scf_dir;
+
         unless (-d $scf_dir) {
-            $self->error_message("Fail to make dir: $scf_dir");
-            return;
+            mkdir $scf_dir, 0777;
+            unless (-d $scf_dir) {
+                $self->error_message("Fail to make dir: $scf_dir");
+                return;
+            }
         }
     }
     return $self;
@@ -102,15 +104,7 @@ sub execute {
         $seq =~ s/[^AaTtCcGg]/A/g if $self->base_fix;
         
         my $id = $id_range ? $new_id : $fq->id;
-         
-        my $scf_file = $self->scf_dir.'/'.$id;
-        my $gz_file  = $scf_file.'.gz';
         
-        if (-s $gz_file) {
-            $self->warning_message("$gz_file already exist, now delete it");
-            unlink $gz_file;
-        }
-                
         my %params = (
             -seq  => $seq,
             -qual => $fq->qual,
@@ -119,19 +113,27 @@ sub execute {
             -trace       => [map{$_*10}(0..$fq->length-1)], 
         );
         
-        my $scf_swq = Bio::Seq::Quality->new(%params);
-        my $phd_swq = Bio::Seq::Quality->new(%params);
-
+        my $phd_swq = Bio::Seq::Quality->new(%params);  
         $phd_swq->chromat_file($id);
         $phd_swq->time($self->time) if $self->time;
+        $ball_io->write_seq($phd_swq);  
         
-        my $scf_io = $self->get_scf_writer($scf_file, 'scf');
+        if ($self->scf_dir) {
+            my $scf_file = $self->scf_dir.'/'.$id;
+            my $gz_file  = $scf_file.'.gz';
         
-        $scf_io->write_seq(-target => Bio::Seq::SequenceTrace->new(-swq => $scf_swq));
-        $ball_io->write_seq($phd_swq);     
-
-        `gzip $scf_file 2>&1`;
-        $new_id++;
+            if (-s $gz_file) {
+                $self->warning_message("$gz_file already exist, now delete it");
+                unlink $gz_file;
+            }
+                
+            my $scf_swq = Bio::Seq::Quality->new(%params);
+            my $scf_io  = $self->get_scf_writer($scf_file, 'scf');
+        
+            $scf_io->write_seq(-target => Bio::Seq::SequenceTrace->new(-swq => $scf_swq));
+            `gzip $scf_file 2>&1`;
+        }
+        $new_id++ if $id_range;
     }
     
     if ($id_range) {
