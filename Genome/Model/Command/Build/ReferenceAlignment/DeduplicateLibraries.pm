f27073a74fa0d91d8169e11d09ceb7831ba16675
jpeck <jpeck@genome.wustl.edu>
2009-03-02 18:10:21 +0000

    Solexa pipeline update implementing library deduplication.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44035 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Build/ReferenceAlignment/DeduplicateLibraries.pm
@@ -0,0 +1,43 @@
+package Genome::Model::Command::Build::ReferenceAlignment::DeduplicateLibraries;
+
+use strict;
+use warnings;
+
+use Genome;
+use Command; 
+
+class Genome::Model::Command::Build::ReferenceAlignment::DeduplicateLibraries {
+    is => ['Genome::Model::EventWithRefSeq'],
+};
+
+sub sub_command_sort_position { 52}
+
+sub help_brief {
+    "Merge any accumulated alignments on a model";
+}
+
+sub help_synopsis {
+    return <<"EOS"
+    genome-model add-reads merge-alignments --model-id 5  --ref-seq-id all_sequences
+EOS
+}
+
+sub help_detail {
+    return <<"EOS"
+This command is launched automatically by "postprocess alignments".  
+
+It delegates to the appropriate sub-command for the aligner
+specified in the model.
+EOS
+}
+
+sub command_subclassing_model_property {
+    return 'read_aligner_name';
+}
+
+sub is_not_to_be_run_by_add_reads {
+    return 1;
+}
+  
+1;
+
