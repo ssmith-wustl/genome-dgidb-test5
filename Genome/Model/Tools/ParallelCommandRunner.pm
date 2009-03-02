a17d58ff86989e2c6596823184394e352a92e50f
jpeck <jpeck@genome.wustl.edu>
2009-03-02 18:13:29 +0000

    Utility for easily parallelizing system commands via workflow.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44037 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/ParallelCommandRunner.pm
+++ Genome/Model/Tools/ParallelCommandRunner.pm
@@ -44,44 +44,77 @@ sub execute {
     	my $self = shift;
         my @commands = @{$self->command_list}; 
         #print("Running commands: ".join(",",@commands)."\n");
-        print('There are '.scalar(@commands).' to run.');
+        #print('There are '.scalar(@commands).' to run.');
     
         Genome::Utility::FileSystem->create_directory($self->log_path);
         unless (-d $self->log_path) {
-	   print("Can't create directory for logging: ".$self->log_path);
+	   #print("Can't create directory for logging: ".$self->log_path);
 	   $self->error_message("Can't create directory for logging: ".$self->log_path);
            die();
         }
 
 	require Workflow::Simple;
         $Workflow::Simple::store_db=0;
+
+    my $m = Workflow::Model->create(
+        name => 'Container',
+        input_properties => ['command_list','log_file','log_path'],
+        output_properties => ['result','output_file']
+    );
         
-	my $op = Workflow::Operation->create(
+	my $op = $m->add_operation(
             name => 'Parallel Command.',
             operation_type => Workflow::OperationType::Command->get('Genome::Model::Tools::ParallelCommand')
-        );
+    );
 
 	$op->parallel_by('command_list');
 
+    foreach my $inprop (@{ $m->operation_type->input_properties }) {
+        $m->add_link(
+            left_operation => $m->get_input_connector,
+            left_property => $inprop,
+            right_operation => $op,
+            right_property => $inprop
+        );
+    }
+
+    foreach my $outprop (@{ $m->operation_type->output_properties }) {
+        $m->add_link(
+            left_operation => $op,
+            left_property => $outprop,
+            right_operation => $m->get_output_connector,
+            right_property => $outprop
+        );
+    }
+
         my $output = Workflow::Simple::run_workflow_lsf(
-            $op,
+            $m,
             'command_list' =>\@commands, 
-            'log_file' =>$self->log_file, 
-            'log_path' =>$self->log_path, 
+            'log_file' =>$self->log_file || '', 
+            'log_path' =>$self->log_path || '', 
         );
  
         #$self->status_message("Output: ".$output);
         #print("Output: ".$output);
 
-        while ( my ($key, $value) = each(%$output) ) {
-                if ( $key eq 'result' ) {	
-                	#print "Output key: $key \n";
-			my @result_array = @{$value};
-                	for my $result_item (@result_array) {
-                    		print "Result: $result_item";
-                	}
-        	}
-	} 
+        if (!defined $output) {
+            #error occurred.
+
+            foreach my $error (@Workflow::Simple::ERROR) {
+                $self->error_message($error->error);
+            }
+            die 'Commands with errors: ' . scalar @Workflow::Simple::ERROR;
+        }
+
+        #while ( my ($key, $value) = each(%$output) ) {
+        #        if ( $key eq 'result' ) {	
+        #        	#print "Output key: $key \n";
+	#		my @result_array = @{$value};
+        #        	for my $result_item (@result_array) {
+        #            		print "Result: $result_item";
+        #        	}
+        #	}
+	#} 
 
 
 	#try printing the output
