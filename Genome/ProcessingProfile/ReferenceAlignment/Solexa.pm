fb961994fea42a9c8da68cad544c989e3832aa68
jpeck <jpeck@genome.wustl.edu>
2009-03-02 18:27:54 +0000

    Updated to facilitate solexa pipeline additions.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44051 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ReferenceAlignment/Solexa.pm
+++ Genome/ProcessingProfile/ReferenceAlignment/Solexa.pm
@@ -8,11 +8,13 @@ use Genome;
 class Genome::ProcessingProfile::ReferenceAlignment::Solexa {
     is => 'Genome::ProcessingProfile::ReferenceAlignment',
 };
-
 sub stages {
     my @stages = qw/
+
         alignment
+        deduplication
         variant_detection
+        generate_reports
         verify_successful_completion
     /;
     return @stages;
@@ -30,7 +32,6 @@ sub alignment_job_classes {
 
 sub variant_detection_job_classes {
     my @steps = (
-                 'Genome::Model::Command::Build::ReferenceAlignment::MergeAlignments',
                  'Genome::Model::Command::Build::ReferenceAlignment::UpdateGenotype',
                  'Genome::Model::Command::Build::ReferenceAlignment::FindVariations',
                  (
@@ -42,11 +43,35 @@ sub variant_detection_job_classes {
     return @steps;
 }
 
+sub deduplication_job_classes {
+    my @steps = ( 
+        'Genome::Model::Command::Build::ReferenceAlignment::DeduplicateLibraries'
+    );
+    return @steps;
+}
+
+sub generate_reports_job_classes {
+    my @steps = (
+        'Genome::Model::Command::Build::ReferenceAlignment::RunReports'
+    );
+    return @steps;
+}
+
+
+sub verify_successful_completion_job_classes {
+    my @sub_command_classes= qw/
+        Genome::Model::Command::Build::VerifySuccessfulCompletion
+    /;
+    return @sub_command_classes;
+}
+
 sub alignment_objects {
     my $self = shift;
     my $model = shift;
     return $model->unbuilt_read_sets;
 }
+
+   
 
 sub variant_detection_objects {
     my $self = shift;
@@ -59,4 +84,27 @@ sub variant_detection_objects {
     return @subreferences_names;
 }
 
+sub deduplication_objects {
+    my $self = shift;
+    my $model = shift;
+    my @subreferences_names = grep {$_ eq "all_sequences" } $model->get_subreference_names(reference_extension=>'bfa');
+   
+   return @subreferences_names;
+}
+
+sub generate_reports_objects {
+    my $self = shift;
+    my $model = shift;
+    my @subreferences_names = grep {$_ eq "all_sequences" } $model->get_subreference_names(reference_extension=>'bfa');
+   
+   return @subreferences_names;
+}
+
+
+sub verify_successful_completion_objects {
+    my $self = shift;
+    return 1;
+    $self->model->current_running_build_id($self->id);
+}
 1;
+
