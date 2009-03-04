61fc4f8adecc3e73de46a8ff2cc8dbb7eaa3f251
jwalker <jwalker@genome.wustl.edu>
2009-03-04 20:08:20 +0000

    switch to instrument data instead of read set, yeah class name needs refactored too
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44175 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/EventWithReadSet.pm
+++ Genome/Model/EventWithReadSet.pm
@@ -11,46 +11,23 @@ class Genome::Model::EventWithReadSet {
     has => [
             instrument_data_assignment => {
                                            is => 'Genome::Model::InstrumentDataAssignment',
-                                           id_by => ['model_id','read_set_id'],
+                                           id_by => ['model_id','instrument_data_id'],
                                        },
-            read_set_name  => { via => 'read_set', to => 'full_name' },
-            read_set_id    => {
+            read_set_name  => { via => 'instrument_data_assignment', to => 'full_name' },
+            instrument_data_id    => {
                                is => 'NUMBER',
                                len => 11,
-                               doc => 'the id of thegenome_model_run on which to operate',
+                               doc => 'the id of the instrument data on which to operate',
                            },
-            run_name            => { via => 'read_set_link' },
-            run_short_name      => { via => 'read_set_link' },
-            run_subset_name     => { via => 'read_set_link', to => 'subset_name' },
-            library_name        => { via => 'read_set_link' },
-            sample_name         => { via => 'read_set_link' },
-            
+            run_name            => { via => 'instrument_data_assignment' },
+            run_short_name      => { via => 'instrument_data_assignment' },
+            run_subset_name     => { via => 'instrument_data_assignment', to => 'subset_name' },
+            library_name        => { via => 'instrument_data_assignment' },
+            sample_name         => { via => 'instrument_data_assignment' },
+
         # deprecated
-        read_set       => { is => 'Genome::RunChunk', id_by => 'read_set_id', constraint_name => 'event_run' },
-        read_set_link  => { is => 'Genome::Model::ReadSet', id_by => ['model_id','read_set_id'] },
-        alignment_directory => { via => 'read_set_link'},
-        read_set_alignment_directory  => {
-                                      calculate_from => ['alignment_directory','read_set_link'],
-                                      calculate => q|
-                                          return sprintf('%s/%s/%s_%s',
-                                                         $alignment_directory,
-                                                         $read_set_link->run_name,
-                                                         $read_set_link->subset_name,
-                                                         $read_set_link->seq_id,
-                                                        );
-                                      |,
-                        },
-        read_set_directory  => {
-                                calculate_from => ['model','read_set_link'],
-                                calculate => q|
-                                    return sprintf('%s/runs/%s/%s',$model->data_directory,
-                                                                   $read_set_link->sequencing_platform,
-                                                                   $read_set_link->name);
-                                |,
-                                doc => 'Only keep this as long as we are moving old read data',
-                                is_deprecated => 1,
-                            },
-        run                 => { is => 'Genome::RunChunk', id_by => 'read_set_id', is_deprecated => 1 }, # use read_set
+        alignment_directory => { via => 'instrument_data_assignment'},
+        read_set_alignment_directory  => { via => 'instrument_data_assignment'} ,
     ],
     sub_classification_method_name => '_get_sub_command_class_name',
 };
@@ -67,7 +44,7 @@ sub _shell_args_property_meta {
     return grep {
             not (
                 $_->class_name eq __PACKAGE__
-                and $_->property_name !~ /(model_id|read_set_id)/
+                and $_->property_name !~ /(model_id|instrument_data_id)/
             )
         } shift->SUPER::_shell_args_property_meta(@_);
 }
@@ -84,11 +61,11 @@ sub invalid {
                                         );
     }
 
-    if (!Genome::RunChunk->get($self->read_set_id) && !Genome::InstrumentData->get($self->read_set_id)) {
+    if (!Genome::InstrumentData->get($self->instrument_data_id)) {
         push @tags, UR::Object::Tag->create(
                                             type => 'invalid',
-                                            properties => ['read_set_id'],
-                                            desc => "There is no genome run with read_set_id ". $self->read_set_id,
+                                            properties => ['instrument_data_id'],
+                                            desc => "There is no instrument data with instrument_data_id ". $self->instrument_data_id,
                                         );
     }
     return @tags;
@@ -98,7 +75,7 @@ sub resolve_log_directory {
     my $self = shift;
     return sprintf('%s/logs/%s/%s',
                    $self->model->latest_build_directory,
-                   $self->read_set_link->sequencing_platform,
+                   $self->instrument_data_assignment->sequencing_platform,
                    $self->run_name
                );
 }
