36b61d904f9ee247507f7ead14568d6aff52e281
fdu <fdu@genome.wustl.edu>
2009-03-10 17:34:10 +0000

    refactoring
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44425 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Velvet.pm
+++ Genome/Model/Tools/Velvet.pm
@@ -3,10 +3,19 @@ package Genome::Model::Tools::Velvet;
 use strict;
 use warnings;
 
+use POSIX;
 use Genome;
 
 class Genome::Model::Tools::Velvet {
-    is => 'Command',
+    is  => 'Command',
+    is_abstract => 1,
+    has => [
+        version => {
+            is   => 'String',
+            doc  => 'velvet version, must be one of old, installed, test. default is installed',
+            default => 'installed',
+        },
+    ],
 };
 
 sub sub_command_sort_position { 14 }
@@ -27,5 +36,26 @@ sub help_detail {
 EOS
 }
 
+
+sub resolve_version {
+    my $self = shift;
+
+    my ($type) = ref($self) =~ /\:\:(\w+)$/;
+    $type = 'velvet'.lc(substr $type, 0, 1);
+
+    my @uname = POSIX::uname();
+    my $ver   = $self->version;
+    $ver .= '-64' if $uname[4] eq 'x86_64';
+    
+    my $exec = "/gsc/pkg/bio/velvet/$ver/$type";
+    unless (-x $exec) {
+        $self->error_message("$exec is not excutable");
+        return;
+    }
+
+    return $exec;
+}
+
+    
 1;
 
