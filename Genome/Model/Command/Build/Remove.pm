6edb244a521ba9f45676164c64e75e9568f7c83b
jwalker <jwalker@genome.wustl.edu>
2009-03-12 18:56:12 +0000

    command to remove a single build from a model
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44588 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Command/Build/Remove.pm
@@ -0,0 +1,48 @@
+package Genome::Model::Command::Build::Remove;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Command::Build::Remove {
+    is => ['Genome::Model::Command'],
+    has => [
+            build_id => {
+                         is => 'Number',
+                         doc => 'The id of the build in which to update status',
+                         is_optional => 1,
+                     },
+            build   => {
+                        is => 'Genome::Model::Build',
+                        id_by => 'build_id',
+                        is_optional => 1,
+                    },
+        ],
+};
+
+sub help_detail {
+    "This command will remove the build and all events that make up the build";
+}
+
+sub execute {
+    my $self = shift;
+    my $model = $self->model;
+    unless ($self->build_id) {
+        $self->build_id($model->current_running_build_id);
+    }
+    my $build = $self->build;
+    unless ($build) {
+        $self->error_message('Build not found for model id '. $self->model_id .' and build id '. $self->build_id);
+        return;
+    }
+    unless ($build->delete) {
+        $self->error_message('Failed to remove build '. $self->build_id);
+        return;
+    }
+    return 1;
+}
+
+
+1;
+
