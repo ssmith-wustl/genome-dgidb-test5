c37237ed0a43c65e99cd93da84ef09d1dfeafabd
ebelter <ebelter@genome.wustl.edu>
2009-03-24 21:37:00 +0000

    moved Generator test pkg to Test.pm; added tests for new methods in Gen.pm
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44985 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Report/Generator.pm
+++ Genome/Report/Generator.pm
@@ -10,7 +10,6 @@ use Data::Dumper 'Dumper';
 
 class Genome::Report::Generator {
     is => 'UR::Object',
-    is_abstract => 1,
     has => [
     name => { 
         is => 'Text',
@@ -67,38 +66,197 @@ sub _get_params_for_generation {
 
     my %params;
     for my $property ( $self->get_class_object->get_all_property_objects ) {
+        my $property_name = $property->property_name;
+        next if $property_name =~ /^_/;
         next if $property->via or $property->id_by;
         next unless $property->class_name->isa('Genome::Report::Generator');
         next if $property->class_name eq 'Genome::Report::Generator';
-        my $property_name = $property->property_name;
         #print Dumper($property_name);
-        $params{$property_name} = $self->$property_name;
+        $params{$property_name} = ( $property->is_many )
+        ? [ $self->$property_name ]
+        : $self->$property_name;
     }
 
     return \%params;
 }
 
 #< Data Generation Helper Methods >#
+sub _generate_csv_string {
+    my ($self, %params) = @_;
 
-################################
-## TEST GENERATOR FOR TESTING ##
-################################
+    $self->_validate_aryref(
+        name => 'headers',
+        value => $params{headers},
+        method => '_generate_csv_string',
+    )
+        or return;
 
-package Genome::Report::Generator::ForTesting;
+    $self->_validate_aryref(
+        name => 'data',
+        value => $params{data},
+        method => '_generate_csv_string',
+    )
+        or return;
 
-use strict;
-use warnings;
+    my $io = IO::String->new();
+    my $svw = Genome::Utility::IO::SeparatedValueWriter->create(
+        output => $io,
+        headers => $params{headers},
+    )
+        or return;
 
-class Genome::Report::Generator::ForTesting {
-    is => 'Genome::Report::Generator',
-};
+    for my $row ( @{$params{data}} ) {
+        my %data;
+        @data{ @{$params{headers}} } = @$row;
+        unless ( $svw->write_one(\%data) ) {
+            $self->error_message("Error writing row");
+            return;
+        }
+    }
 
-sub _generate_data {
-    return {
-        description => 'something about nothing',
-        html => '<html></html>',
-        csv => "c1,c2\nr1,r2\n",
-    };
+    $io->seek(0, 0);
+
+    return join('', $io->getlines);
+}
+
+sub _generate_html_table {
+    my ($self, %params) = @_;
+
+    $self->_validate_aryref(
+        name => 'headers',
+        value => $params{headers},
+        method => '_generate_html_table',
+    )
+        or return;
+
+    $self->_validate_aryref(
+        name => 'data',
+        value => $params{data},
+        method => '_generate_html_table',
+    )
+        or return;
+
+    my $table = $self->_start_hmtl_table(%params)
+        or return;
+
+    my @data = _convert_data_with_undef_and_empty_strings_to_html_spaces(@{$params{data}});
+    my $entry_attrs = ( $params{entry_attrs} ? ' '.$params{entry_attrs} : '' );
+
+    for my $row ( @data ) {
+        $table .= '<tr>';
+        $table .= join('', map { sprintf('<td%s>%s</td>', $entry_attrs, $_) } @$row );
+        $table .= "</tr>\n";
+    }
+
+    $table .= "</table>\n";
+
+    return $table;
+}
+
+sub _generate_vertical_html_table {
+    my ($self, %params) = @_;
+
+    $self->_validate_aryref(
+        name => 'horizontal headers',
+        value => $params{horizontal_headers},
+        method => '_generate_vertical_html_table',
+    )
+        or return;
+
+    $self->_validate_aryref(
+        name => 'data',
+        value => $params{data},
+        method => '_generate_vertical_html_table',
+    )
+        or return;
+
+    my $table = $self->_start_hmtl_table(%params)
+        or return;
+
+    my @data = _convert_data_with_undef_and_empty_strings_to_html_spaces(@{$params{data}});
+    my $entry_attrs = ( $params{entry_attrs} ? ' '.$params{entry_attrs} : '' );
+
+    for ( my $i = 0; $i < @{$params{horizontal_headers}}; $i++ ) {
+        # header
+        $table .= sprintf('<tr><td%s><b>%s</b></td>', $entry_attrs, $params{horizontal_headers}->[$i]);
+        # data
+        for my $data ( @data ) {
+            $table .= join('', map { sprintf('<td%s>%s</td>', $entry_attrs, $_) } $data->[$i]);
+        }
+        $table .= "</tr>\n";
+    }
+
+    $table .= "</table>\n";
+
+    return $table;
+}
+
+sub _html_table_attrs {
+    return (
+        table_attrs => 'style="text-align:left;border:groove;border-width:3"',
+        header_attrs => 'style="border:groove;border-width:1"',
+        entry_attrs => 'style="border:groove;border-width:1"',
+    );
+}
+
+sub _start_hmtl_table {
+    my ($self, %params) = @_;
+
+    my $table;
+    if ( $params{title} ) {
+        $table = '<h2>'.$params{title}.'</h2><br>';
+    }
+
+    $table .= sprintf('<table %s>', ( $params{table_attrs} || '' ));
+
+    my $header_attrs = ( $params{header_attrs} ? ' '.$params{header_attrs} : '');
+    $table .=  join(
+        '', 
+        map { 
+            sprintf(
+                '<th%s>%s</th>',
+                $header_attrs,
+                $_,
+            ) 
+        } map { 
+            defined $_ ? $_ : '&nbsp'
+        } 
+        _convert_undef_and_empty_strings_to_html_spaces(@{$params{headers}})
+    );
+
+    return $table;
+}
+
+sub _validate_aryref { 
+    my ($self, %params) = @_;
+
+    # value => value of attr
+    # name => name of attr
+    # method => caller method
+    
+    unless ( $params{value} ) {
+        $self->error_message(ucfirst($params{name}).' are required for '.$params{method});
+        return;
+    }
+
+    unless ( ref($params{value}) eq 'ARRAY' ) {
+        $self->error_message(ucfirst($params{name}).' are required to be an array reference for '.$params{desc});
+        return;
+    }
+
+    return 1;
+}
+
+sub _convert_data_with_undef_and_empty_strings_to_html_spaces { # takes an aryref of aryrefs in @_ - no self/class
+    my @new_data;
+    for my $aryref ( @_ ) {
+        push @new_data, [ _convert_undef_and_empty_strings_to_html_spaces(@$aryref) ];
+    }
+    return @new_data;
+}
+
+sub _convert_undef_and_empty_strings_to_html_spaces { # straight ary in @_ - no 'self' or class
+    return map { (( not defined($_) or $_ eq "" ) ? "&nbsp" : $_ ) } @_;
 }
 
 1;
@@ -122,7 +280,7 @@ Base class for report generators.  Use this class as a base for yours...then imp
 
  my $report = $generator->generate_report
     or die;
- 
+
  $report->save('some_directory')
     or die;
 
