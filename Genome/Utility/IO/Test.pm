2f8b543f024067b9b08f00afc323fb54ece7964e
ebelter <ebelter@genome.wustl.edu>
2009-03-24 21:34:13 +0000

    added base test class for sep value reader/writer; updated svw; added svr
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@44982 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Utility/IO/Test.pm
+++ Genome/Utility/IO/Test.pm
@@ -1,22 +1,65 @@
-package Genome::Utility::IO::SeparatedValueWriterTest;
+package Genome::Utility::IO::SeparatedValueTestBase;
 
 use strict;
 use warnings;
 
 use base 'Genome::Utility::TestBase';
 
-use Data::Dumper 'Dumper';
-use File::Compare 'compare';
-use Test::More;
 use Storable 'retrieve';
 
-sub svw {
+sub svo {
     return $_[0]->{_object};
 }
 
 sub test_class_sub_dir {
     return 'Genome-Utility-IO';
 }
+
+sub required_attrs {
+    return;
+}
+
+#< >#
+sub _albums_csv {
+    return $_[0]->dir.'/albums.csv';
+}
+
+sub _albums_stor {
+    return $_[0]->dir.'/albums.stor';
+}
+
+sub _albums_no_headers {
+    return $_[0]->dir.'/albums.no_headers.csv';
+}
+
+sub _albums_regexp {
+    return $_[0]->dir.'/albums.test_regexp.csv';
+}
+
+sub _stored_albums {
+    my $self = shift;
+
+    unless ( $self->{_albums} ) {
+        my $data = retrieve($self->_albums_stor)
+            or die "Can't get stored albums\n";
+        $self->{_albums} = $data;
+    }
+
+    return $self->{_albums};
+}
+
+###################################################################
+
+package Genome::Utility::IO::SeparatedValueWriterTest;
+
+use strict;
+use warnings;
+
+use base 'Genome::Utility::IO::SeparatedValueTestBase';
+
+use Data::Dumper 'Dumper';
+use File::Compare 'compare';
+use Test::More;
 
 sub test_class {
     return 'Genome::Utility::IO::SeparatedValueWriter';
@@ -27,34 +70,27 @@ sub params_for_test_class {
     return (
         #output => $self->dir.'/myalbums.csv',
         output => $self->tmp_dir.'/albums.csv',
+        headers => $self->_headers,
     );
-}
-
-sub required_attrs {
-    return;
 }
 
 sub _headers {
     my $self = shift;
 
-    $self->_set_headers_and_rows unless $self->{_headers};
+    $self->_set_headers_and_data unless $self->{_headers};
 
     return $self->{_headers};
 }
 
-sub _rows {
+sub _data {
     my $self = shift;
 
-    $self->_set_headers_and_rows unless $self->{_headers};
+    $self->_set_headers_and_data unless $self->{_headers};
 
-    return $self->{_rows};
+    return $self->{_data};
 }
 
-sub _albums_csv {
-    return $_[0]->dir.'/albums.csv';
-}
-
-sub _set_headers_and_rows {
+sub _set_headers_and_data {
     my $self = shift;
 
     my $fh = Genome::Utility::FileSystem->open_file_for_reading( $self->_albums_csv )
@@ -64,45 +100,132 @@ sub _set_headers_and_rows {
     $self->{_headers} = [ split(',', $header_line) ];
     while ( my $line = $fh->getline ) {
         chomp $line;
-        push @{$self->{_rows}}, [ split(',', $line) ];
+        my %data;
+        @data{ @{$self->{_headers}} } = split(',', $line);
+        push @{$self->{_data}}, \%data;
     }
-    
+
     return 1;
 }
 
-sub test01_write_and_compare : Test(2) {
+sub test01_write_and_compare : Test(1) {
     my $self = shift;
 
-    my $svw = $self->svw;
-    ok($svw->write_one( $self->_headers ), "Wrote headers");
-    for my $row ( @{$self->_rows} ) {
-        $svw->write_one($row);
+    for my $data ( @{$self->_data} ) {
+        $self->svo->write_one($data);
     }
 
-    is(compare($svw->get_original_output, $self->_albums_csv, ), 0, 'Compared generated and files');
+    is(compare($self->svo->get_original_output, $self->_albums_csv, ), 0, 'Compared generated and files');
 
     return 1;
 }
 
-sub test02_write_one_fails : Test(3) {
+sub test02_write_one_fails : Tests {
     my $self = shift;
 
     my %fails = (
         'undef' => undef,
-        'not an aryref' => 'a string',
-        'unequal number of elements' => [qw/ too many elements in this ary ref /],
-        #'' => [qw//],
+        'empty hash ref' => {},
+        'not a hash ref' => 'a string',
+        'different headers' => { different => 'headers' },
     );
     
-    my $svw = $self->svw;
     for my $desc ( keys %fails ) {
-        ok(!$svw->write_one($fails{$desc}), "Failed as expected, tried to 'write one' w/ $desc");
+        ok(!$self->svo->write_one($fails{$desc}), "Failed as expected, tried to 'write one' w/ $desc");
     }
 
     return 1;
 }
 
 #######################################################################
+
+package Genome::Utility::IO::SeparatedValueReaderTest;
+
+use strict;
+use warnings;
+
+use base 'Genome::Utility::IO::SeparatedValueTestBase';
+
+use Data::Dumper 'Dumper';
+use Test::More;
+
+sub test_class {
+    return 'Genome::Utility::IO::SeparatedValueReader';
+}
+
+sub params_for_test_class {
+    my $self = shift;
+    return (
+        input => $self->_albums_csv,
+    );
+}
+
+sub required_attrs {
+    return (qw/ input /);
+}
+
+sub test01_read_and_compare : Test(4) {
+    my $self = shift;
+
+    my $stored_albums = $self->_stored_albums;
+    
+    my @albums;
+    while ( my $album = $self->svo->next ) { push @albums, $album }
+    is_deeply(\@albums, $stored_albums->{albums}, 'Albums from SVR (method: next) match expected albums');
+    is($self->svo->line_number, 5, 'Line number incremented successfully');
+    ok($self->svo->reset, 'reset');
+    is($self->svo->line_number, 1, 'Reset to line 1');
+
+    return 1;
+}
+
+sub test02_headers_not_in_file : Test(2) {
+    my $self = shift;
+
+    my $stored_albums = $self->_stored_albums;
+
+    my $svr = $self->test_class->create(
+        input => $self->_albums_no_headers, # w/o headers
+        headers => $stored_albums->{headers},
+    );
+    ok($svr, 'Created SVR w/o headers');
+    my @albums = $svr->all;
+    is_deeply(\@albums, $stored_albums->{albums}, 'Albums from SVR (method: all) match expected albums');
+    $svr->delete;
+
+    return 1;
+}
+
+sub test03_regexp_separator : Test(2) {
+    my $self = shift;
+
+    my $stored_albums = $self->_stored_albums;
+
+    my $svr = $self->test_class->create(
+        input => $self->_albums_regexp, # w/ headers
+        separator => ',+',
+        is_regex => 1,
+
+    );
+    ok($svr, 'Created SVR');
+    my @albums = $svr->all;
+    is_deeply(\@albums, $stored_albums->{albums}, 'Albums from SVR (regexp) match expected albums');
+
+    return 1;
+}
+
+sub test04_create_with_different_number_of_headers_than_values_in_file : Test(2) {
+    my $self = shift;
+
+    my $svr = $self->test_class->create(
+        input => $self->_albums_no_headers, # w/o headers
+        headers => [qw/ not the right number of headers /],
+    );
+    ok($svr, 'Created SVR to test different header v. value count');
+    ok(!$svr->next, 'Failed as expected - next');
+
+    return 1;
+}
 
 1;
 
