efe4dd953bab13fa97d64c5f0bbebb4f853d22b1
ebelter <ebelter@genome.wustl.edu>
2009-03-25 18:01:06 +0000

    now runs on a build, not model
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45040 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/PostProcess/Composition.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/PostProcess/Composition.pm
@@ -5,8 +5,6 @@ use warnings;
 
 use Genome;
 
-use Genome::Model::Tools::MetagenomicClassifier::Rdp;
-
 class Genome::Model::Command::Build::AmpliconAssembly::PostProcess::Composition {
     is => 'Genome::Model::Command::Build::AmpliconAssembly::PostProcess',
 };
@@ -15,17 +13,33 @@ class Genome::Model::Command::Build::AmpliconAssembly::PostProcess::Composition 
 sub execute {
     my $self = shift;
 
-    unlink $self->model->rdp_file if -e $self->model->rdp_file;
-        
-    my $classifier = Genome::Model::Tools::MetagenomicClassifier::Rdp->create(
-        input_file => $self->model->assembly_fasta,
-        output_file => $self->model->rdp_file,
-    )
-        or return;
-    my $classification = $classifier->execute
-        or return;
+    return 1;
+    unlink $self->build->rdp_file if -e $self->build->rdp_file;
+    
+    my $classifier = Genome::Utility::MetagenomicClassifier::Rdp::Test->create_broad_classifier;
+    my $factory = Genome::Utility::MetagenomicClassifier::PopulationCompositionFactory->instance;
+    my $composition = $factory->get_composition(
+        classifier => Genome::Utility::MetagenomicClassifier::Rdp->new(
+            training_set => 'broad',
+        ),
+        fasta_file => Genome::Utility::MetagenomicClassifier::TestBase->fasta,
+    );
 
-    return 1 if -s $self->model->rdp_file;
+    return 1 if -s $self->build->rdp_file;
+
+    #< Report
+    my $report = Genome::Model::Report->create(
+        composition => $composition,
+    );
+    $report->execute;
+
+    #< RDP Output
+    my $writer = Genome::Utility::MetagenomicClassifier::Rdp::Writer->create(
+        output => ,
+    );
+    for my $classification ( $composition->clasifications ) {
+        $writer->write_classification($classification);
+    }
 
     $self->error_message("RDP classification executed correctly, but a file was not created");
     return;
