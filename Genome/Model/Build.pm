8bf375b2d8e2f81cfd152d8bcb99a1414a49949e
jwalker <jwalker@genome.wustl.edu>
2009-03-25 19:44:54 +0000

    get a disk allocation by creating the object and let the commit happen in a forked process via system call
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45057 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build.pm
+++ Genome/Model/Build.pm
@@ -136,21 +136,15 @@ sub resolve_data_directory {
         $allocation_path .= $build_subdirectory;
         my $kb_requested = $self->calculate_estimated_kb_usage;
         if ($kb_requested) {
-            my $allocate = Genome::Disk::Allocation::Command::Allocate->execute(
-                                                                                disk_group_name => 'info_genome_models',
-                                                                                allocation_path => $allocation_path,
-                                                                                kilobytes_requested => $kb_requested,
-                                                                                owner_class_name => $self->class,
-                                                                                owner_id => $self->id,
-                                                                            );
-            unless ($allocate) {
-                $self->error_message('Failed to allocate '. $kb_requested .'kb for build.');
-                $self->delete;
-                die;
-            }
-            my $disk_allocation = $allocate->disk_allocation;
+            my $disk_allocation = Genome::Disk::Allocation->create(
+                                                            disk_group_name => 'info_genome_models',
+                                                            allocation_path => $allocation_path,
+                                                            kilobytes_requested => $kb_requested,
+                                                            owner_class_name => $self->class,
+                                                            owner_id => $self->id,
+                                                        );
             unless ($disk_allocation) {
-                $self->error_message('Failed to get disk allocation for allocator_id: '. $allocate->allocator_id);
+                $self->error_message('Failed to get disk allocation');
                 $self->delete;
                 die $self->error_message;
             }
