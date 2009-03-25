cdb6dbb58f33a26c94c249b500dcfcff9b544ae7
ebelter <ebelter@genome.wustl.edu>
2009-03-25 17:46:44 +0000

    =update to tests
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45033 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/AmpliconAssembly/Test.pm
+++ Genome/Model/AmpliconAssembly/Test.pm
@@ -3,8 +3,9 @@ package Genome::Model::AmpliconAssembly::Test;
 use strict;
 use warnings;
 
-use base 'Test::Class';
+use base 'Genome::Utility::TestBase';
 
+use Carp 'confess';
 use Data::Dumper 'Dumper';
 use File::Copy 'copy';
 use File::Temp 'tempdir';
@@ -14,106 +15,102 @@ use Genome::ProcessingProfile::AmpliconAssembly::Test;
 use Genome::Utility::FileSystem;
 use Test::More;
 
-#< DIR >#
-sub test_dir {
-    return '/gsc/var/cache/testsuite/data/Genome-Model-AmpliconAssembly';
+
+#< REAL MODEL FOR TESTING >#
+#TODO
+sub test_class {
+    return 'Genome::Model::AmpliconAssembly';
 }
 
+sub params_for_test_class {
+    return (
+        subject_name => 'dna',
+        subject_type => 'dna_resource_item_name',
+    );
+}
+
+sub required_attrs {
+    return (qw/ subject_type /);
+}
+
+#< DIR >#
 sub _test_dir_for_type {
-    die "No type to get test dir\n" if not defined $_[0] or $_[0] eq __PACKAGE__;
-    return test_dir().'/'.$_[0];
+    return $_[0]->dir.'/build-10000/'.$_[1];
 }
 
 #< MOCK ># 
 sub create_mock_model {
-    my $self = shift;
+    my ($self, %params) = @_;
 
     # Processing profile
     my $pp = Genome::ProcessingProfile::AmpliconAssembly::Test->create_mock_processing_profile
         or return;
 
-    #my $data_dir = File::Temp::tempdir(DIR => $self->test_dir, CLEANUP => 0);
-    my $data_dir = File::Temp::tempdir(CLEANUP => 1);
-    die "Can't make temp data directory for mock model\n" unless -d $data_dir;
-
+    my $model_data_dir = ( $params{use_test_dir} ) 
+    ? $self->dir
+    : File::Temp::tempdir(CLEANUP => 1);
+    
     # Model
     my $model = Genome::Model::AmpliconAssembly->create_mock(
         id => -5000,
         genome_model_id => -5000,
         name => 'mr. mock',
+        type_name => 'amplicon assembly',
         subject_name => 'mock_dna',
         subject_type => 'dna_resource_item_name',
         processing_profile_id => $pp->id,
         processing_profile => $pp,
-        data_directory => $data_dir,
+        data_directory => $model_data_dir,
     )
         or die "Can't create mock model for amplicon assembly\n";
     
     for my $pp_param ( Genome::ProcessingProfile::AmpliconAssembly->params_for_class ) {
         $model->set_always($pp_param, $pp->$pp_param);
     }
-    
-    for my $dir_type ( Genome::Consed::Directory->directories ) {
-        my $dir = $data_dir.'/'.$dir_type; 
-        $model->set_always($dir_type, $dir);
-        mkdir $dir
-            or die "Can't make directory ($dir): $!\n";
-    }
-    
-    $model->mock(
-        'amplicons',
-        sub{  
-            my $dh = Genome::Utility::FileSystem->open_directory( $model->chromat_dir )
-                or die;
 
-            my %amplicons;
-            while ( my $scf = $dh->read ) {
-                next if $scf =~ m#^\.#;
-                $scf =~ s#\.gz##;
-                $scf =~ /^(.+)\.[bg]\d+$/
-                    or next;
-                push @{$amplicons{$1}}, $scf;
-            }
-            $dh->close;
-
-            return \%amplicons;
-        },
+    $self->mock_methods(
+        $model,
+        'Genome::Model',
+        (qw/
+            running_builds build_event 
+            /),
     );
 
-    $model->mock(
-        'get_amplicons',
-        sub{
-            my $amplicons = $model->amplicons;
-            my @amplicons;
-            my $edit_dir = $model->edit_dir;
-            for my $name ( keys %$amplicons ) {
-                push @amplicons, Genome::Model::AmpliconAssembly::Amplicon->new(
-                    name => $name,
-                    reads => $amplicons->{$name},
-                    directory => $edit_dir,
-                );
-            }
+    # Build
+    my $build = Genome::Model::Build::AmpliconAssembly->create_mock(
+        id => -10000,
+        build_id => -10000,
+        model_id => $model->id,
+        data_directory => $model->data_directory.'/build-10000',
+    );
+    mkdir $build->data_directory;
+    $build->set_always('model', $model);
+    $model->set_list('builds', $build);
+    $model->set_always('latest_complete_build', $build);
+    $model->mock('current_running_build', sub{ Genome::Model::current_running_build(@_); } );
 
-            return @amplicons;
-        },
+    $self->mock_methods(
+        $build,
+        'Genome::Model::Build::AmpliconAssembly',
+        (qw/
+            consed_directory create_directory_structure
+            edit_dir chromat_dir phd_dir fasta_dir
+            link_instrument_data 
+            get_amplicons _determine_amplicons_in_chromat_dir_gsc 
+            assembly_fasta reads_fasta processed_fasta 
+            metrics_report 
+            /),
+    );
+    $self->mock_accessors(
+        $build,
+        (qw/
+            event_status date_scheduled date_completed
+            /),
     );
 
-    #< FILES >#
-    for my $type (qw/ assembly reads processed assembly.confirmed assembly.unconfirmed /) {
-        my $method = $type.'_fasta';
-        $model->set_always($method, sprintf('%s/%s.%s.fasta', $data_dir, $model->subject_name, $type));
-    }
-    $model->set_always('metrics_file',  sprintf('%s/%s.metrics.txt', $data_dir, $model->subject_name));
+    $build->create_directory_structure;
 
-    return $model;
-}
-
-sub create_mock_model_with_instrument_data {
-    my $self = shift;
-
-    my $model = $self->create_mock_model
-        or return;
-
+    #< INSTR DATA >#
     my $inst_data = Genome::InstrumentData::Sanger::Test->create_mock_instrument_data; # this dies if no workee
     my $ida = Genome::Model::InstrumentDataAssignment->create_mock(
         id => -5000,
@@ -125,7 +122,7 @@ sub create_mock_model_with_instrument_data {
     $ida->set_always('model_id', $model);
     $ida->set_always('instrument_data', $inst_data);
     $ida->mock(
-        'first_build_id', sub { 
+        'first_build_id', sub{ 
             my ($ida, $fbi) = @_;
             $ida->{first_build_id} = $fbi if defined $fbi;
             return $ida->{first_build_id}; 
@@ -138,27 +135,15 @@ sub create_mock_model_with_instrument_data {
 }
 
 #< COPY DATA >#
-sub copy_dirs_to_model {
-    my ($self, $model, @dirs) = @_;
+sub copy_test_dir {
+    my ($self, $subdir, $dest) = @_;
 
-    die "Need dir types to copy from\n" unless @dirs;
-
-    for my $dir ( @dirs ) {
-        my $dest = ( $model->can($dir) )
-        ? $model->$dir
-        : $model->data_directory;
-        $self->_copy_dir(_test_dir_for_type($dir), $dest)
-            or return;
-    }
-
-    return 1;
-}
-
-sub _copy_dir {
-    my ($self, $source, $dest) = @_;
-
+    Genome::Utility::FileSystem->validate_existing_directory($dest)
+        or confess;
+    
+    my $source = $self->_test_dir_for_type($subdir);
     my $dh = Genome::Utility::FileSystem->open_directory($source)
-        or return;
+        or confess;
 
     while ( my $file = $dh->read ) {
         next if $file =~ m#^\.#;
@@ -167,11 +152,9 @@ sub _copy_dir {
     }
 
     return 1;
-
 }
 
-#< REAL MODEL FOR TESTING >#
-#TODO
+######################################################################
 
 package Genome::Model::AmpliconAssembly::AmpliconTest;
 
@@ -216,7 +199,9 @@ sub test01_reads : Test(5) {
     return 1;
 }
 
-package Genome::Model::AmpliconAssembly::Report::AssemblyStatsTest;
+######################################################################
+
+package Genome::Model::AmpliconAssembly::Report::TestBase;
 
 use strict;
 use warnings;
@@ -226,57 +211,89 @@ use base 'Genome::Utility::TestBase';
 use Data::Dumper 'Dumper';
 use Test::More;
 
-sub stats {
+sub generator {
     return $_[0]->{_object};
 }
+
+sub report_name {
+    my $self = shift;
+
+    my ($pkg) = $self->test_class =~ m/Genome::Model::AmpliconAssembly::Report::(\w+)$/;
+
+    return 'Test '.$pkg.' Report',
+}
+
+sub params_for_test_class {
+    my $self = shift;
+
+    return (
+        name => $self->report_name,
+        build_ids => [ $self->mock_model->latest_complete_build->id ],
+    );
+}
+
+sub mock_model {
+    my $self = shift;
+
+    unless ( $self->{_mock_model} ) {
+        $self->{_mock_model} = Genome::Model::AmpliconAssembly::Test->create_mock_model(use_test_dir => 1);
+    }
+    
+    return $self->{_mock_model};
+}
+
+sub test_01_generate_report : Test(2) {
+    my $self = shift;
+
+    can_ok($self->generator, '_generate_data');
+
+    my $report = $self->generator->generate_report;
+    ok($report, 'Generated report');
+    #print Dumper($report);
+
+    return 1;
+}
+
+######################################################################
+
+package Genome::Model::AmpliconAssembly::Report::AssemblyStatsTest;
+
+use strict;
+use warnings;
+
+use base 'Genome::Model::AmpliconAssembly::Report::TestBase';
+
+use Data::Dumper 'Dumper';
+use Test::More;
 
 sub test_class {
     'Genome::Model::AmpliconAssembly::Report::AssemblyStats';
 }
 
-sub test_01_add_amplicon : Tests {
-    my $self = shift;
+######################################################################
 
-    my $stats = $self->stats;
+package Genome::Model::AmpliconAssembly::Report::QualityHistogramTest;
 
-    my $amplicon = Genome::Model::AmpliconAssembly::AmpliconTest->create_valid_object;
-    ok($amplicon, 'Got amplicon');
-    ok($stats->add_amplicon($amplicon), 'Added amplicon');
-    ok($stats->add_amplicon($amplicon), 'Added again');
-    my %totals = $stats->calculate_totals;
-    ok(%totals, 'Got totals');
-    print Dumper(\%totals);
-    
-    return 1;
+use strict;
+use warnings;
+
+use base 'Genome::Model::AmpliconAssembly::Report::TestBase';
+
+use Data::Dumper 'Dumper';
+use Test::More;
+
+sub test_class {
+    'Genome::Model::AmpliconAssembly::Report::QualityHistogram';
+}
+
+
+sub test_01_generate_report : Test(1) {
+    ok(1, 'skip generate');
 }
 
 1;
 
 =pod
-
-=head1 Name
-
-ModuleTemplate
-
-=head1 Synopsis
-
-=head1 Usage
-
-=head1 Methods
-
-=head2 
-
-=over
-
-=item I<Synopsis>
-
-=item I<Arguments>
-
-=item I<Returns>
-
-=back
-
-=head1 See Also
 
 =head1 Disclaimer
 
@@ -292,4 +309,3 @@ B<Eddie Belter> I<ebelter@watson.wustl.edu>
 
 #$HeadURL$
 #$Id$
-
