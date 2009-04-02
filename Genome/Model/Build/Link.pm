b9bff8c1f8408cb4b7e33d764bab4471d420a2f8
adukes <adukes@genome.wustl.edu>
2009-04-02 19:47:20 +0000

    removed duplicate instance of data_source property in class definition
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45343 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build/Link.pm
+++ Genome/Model/Build/Link.pm
@@ -9,14 +9,21 @@ class Genome::Model::Build::Link {
     table_name => 'GENOME_MODEL_BUILD_LINK',
     data_source_id => 'Genome::DataSource::GMSchema',
     id_by => [
-        from_build => { is => 'NUMBER', len => 11 },
-        to_build   => { is => 'NUMBER', len => 11 },
+        from_build_id => { is => 'NUMBER', len => 11, implied_by => 'from_build'},
+        to_build_id   => { is => 'NUMBER', len => 11, implied_by => 'to_build' },
     ],
     has => [
         role => { is => 'VARCHAR2', len => 56 },
+        from_build => { is => 'Genome::Model::Build', 
+                        id_by => 'from_build_id',
+                        constraint_name => 'GMBL_FB_GMB_FK',
+                    },
+        to_build => {   is => 'Genome::Model::Build', 
+                        id_by => 'to_build_id',
+                        constraint_name => 'GMBL_TB_GMB_FK',
+                    },
     ],
     schema_name => 'GMSchema',
-    data_source => 'Genome::DataSource::GMSchema=HASH(0xa333e14)',
 };
 
 1;
