3303af9478ea6e8391ddf57d44286554876729e4
ssmith <ssmith@genome.wustl.edu>
2009-04-04 02:11:17 +0000

    Fixed indentation.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45404 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Report.pm
+++ Genome/Model/Report.pm
@@ -10,26 +10,26 @@ use Data::Dumper 'Dumper';
 class Genome::Model::Report {
     is => 'Genome::Report::Generator',
     has => [
-    build => {
-        is => 'Genome::Model::Build', 
-        id_by => 'build_id'
-    },
-    build_id => {
-        is => 'Integer', 
-        doc=> 'Build id'
-    },
-    model => {
-        is => 'Genome::Model', 
-        via => 'build'
-    },
-    model_id => {
-        via => 'model',
-        to => 'id',
-    },
-    model_name => {
-        via => 'model',
-        to => 'name',
-    },
+        build => {
+            is => 'Genome::Model::Build', 
+            id_by => 'build_id'
+        },
+        build_id => {
+            is => 'Integer', 
+            doc=> 'Build id'
+        },
+        model => {
+            is => 'Genome::Model', 
+            via => 'build'
+        },
+        model_id => {
+            via => 'model',
+            to => 'id',
+        },
+        model_name => {
+            via => 'model',
+            to => 'name',
+        },
     ],
 };
 
