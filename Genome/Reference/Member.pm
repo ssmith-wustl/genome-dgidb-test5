458c7e6f10678093273d78227383d5f82c61e5a9
jwalker <jwalker@genome.wustl.edu>
2009-04-08 20:42:29 +0000

    A view of gsc.reference_sequence_set_member
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45552 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Reference/Member.pm
@@ -0,0 +1,25 @@
+package Genome::Reference::Member;
+
+class Genome::Reference::Member {
+    id_by => ['seq_id','member_seq_id'],
+    table_name => '(select * from reference_sequence_set_member@dw) reference_set_member',
+    has => [
+            reference => {
+                              is => 'Genome::Reference',
+                              id_by => 'seq_id',
+                          },
+            sequence_item => {
+                              calculate_from => 'member_seq_id',
+                              calculate => q|
+                                  return GSC::Sequence::Item->get($member_seq_id);
+                              |,
+                          },
+            sequence_item_name => { via => 'sequence_item' },
+            sequence_item_type => { via => 'sequence_item' },
+            seq_length => { via => 'sequence_item' },
+        ],
+    schema_name => 'GMSchema',
+    data_source => 'Genome::DataSource::GMSchema',
+};
+
+1;
