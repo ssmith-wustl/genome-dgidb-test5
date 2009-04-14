30a547c2139fdca8f27c64eca8f056ffa9aa71f9
jmcmicha <jmcmicha@genome.wustl.edu>
2009-04-14 21:43:58 +0000

    merged all dbSNP, all SNPs, and concordance plots for unfiltered and filtered SNVs all on the same graph, with checkboxes for toggling each plot
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45789 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ReferenceAlignment/Report/DbSnpConcordance.pm
+++ Genome/Model/ReferenceAlignment/Report/DbSnpConcordance.pm
@@ -8,6 +8,7 @@ use Genome;
 use App::Report;
 use CGI;
 use IO::String;
+use Data::Dumper;
 
 class Genome::Model::ReferenceAlignment::Report::DbSnpConcordance {
     is => 'Genome::Model::Report',
@@ -61,50 +62,16 @@ sub generate_report_detail
     my $build_id = $build->id;
     my $model = $build->model;
     
-    my $body = IO::String->new();
-    die unless $body;
-    my $r = new CGI;
-    
     my $module_path = $INC{"Genome/Model/ReferenceAlignment/Report/DbSnpConcordance.pm"};
     die 'failed to find module path!' unless $module_path;
-    
-    my $css_file = "$module_path.html.css";
-    my $css_fh = IO::File->new($css_file);
-    unless ($css_fh) {
-        die "failed to open file $css_file!"; 
-    }
-    my $css_content = join('',$css_fh->getlines);
-    
-    
-    my $title =  'Db Snp for model ' . $model->id . ' ("' . $model->name . '")  build ' . $build_id . ')';
-    
-    $body->print($r->start_html(
-				-title  => $title,
-				-style  => { -code => $css_content },
-				-script => [
-					    { -type => 'text/javascript', -src => 'https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/js/jquery.js'},
-					    { -type => 'text/javascript', -src => 'https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/js/jquery.flot.js'}
-					   ]
-				));
-    
-#    $body->print("<small><a href='../../reports/Summary/report.html'>full report</a></small>");
-    $body->print('<div class="container">');
-    $body->print('<div class="background">');
+
+	my @cqr_unfiltered;
+	my @cqr_filtered;
+
+	my $concordance_report;
 
     for my $list (qw/variant_list_files variant_filtered_list_files/) {
         
-        my $header;
-        if ($list eq 'variant_list_files') {
-            $header = 'dbSNP Concordance for Unfiltered SNVs';
-        }
-        elsif ($list eq 'variant_filtered_list_files') {
-            $header = 'dbSNP Concordance for SNPfilter SNVs'
-        }
-        else {
-            die "unknown SNV list $list!.  cannot resolve report header!";
-        }
-        
-$DB::single = 1;            
         my $snp_file;
         if  (defined $self->override_variant_file) {
             # override SNP list for testing
@@ -115,7 +82,7 @@ $DB::single = 1;
             # get the SNV lists from the build and put them together
             $snp_file = $self->create_temp_file_path($list);
             my @files = $self->$list;
-	    #my $file_list = join(" ", @files);
+			#my $file_list = join(" ", @files);
             #my $cat_cmd = "cat $file_list > $snp_file";
             #switching this back for the time being... 
             #my $cat_rv = Genome::Utility::FileSystem->shellcmd( cmd=>$cat_cmd, input_files=>\@files, output_files=>[$snp_file] ); 
@@ -146,8 +113,8 @@ $DB::single = 1;
             $db_snp_path = $self->build->resolve_reports_directory() ."/". $model->genome_model_id. "$list.dbsnp";
             
             $self->status_message("Generating dbSNP intersection for concordance checking...");
-            
-            unless ($self->test and -e $db_snp_path) {
+			if (! ($self->test || -e $db_snp_path) ) { 
+            # unless ($self->test and -e $db_snp_path) {
                 unless (Genome::Model::Tools::Snp::CreateDbsnpFileFromSnpFile->execute(output_file=>$db_snp_path,snp_file=>$snp_file) ) {
 			die "Could not execute create-db-snp-file-from-snp-file.";		
 		} 
@@ -165,8 +132,6 @@ $DB::single = 1;
         #    "--snp-file $snp_file".
         #    "--output-file $cc_output"; 
         #print("Generating concordance report using cmd: $concordance_cmd");
-        
-        my $concordance_report;
         
         if ($self->test and -e "./concordance-$list") {
             $concordance_report = `cat ./concordance-$list`;
@@ -192,10 +157,6 @@ $DB::single = 1;
         unless ($concordance_report) {
             die "Error generating concordance report!"
         }
-        $body->print("<h1 class=\"section_title\">$header</h1>");
-		$body->print("<div class='content_padding'>");
-        $body->print("<pre>$concordance_report</pre>");
-        $body->print("<p/>");
         
         #my $concordance_quality_cmd = 
         #    "gt snp db-snp-concordance ".
@@ -226,35 +187,231 @@ $DB::single = 1;
             die "Error generating concordance quality report!";
         }
         
-        $body->print("<h3>$header by Quality</h3>");
-        
         $DB::single = 1;
         
         my @concordance_quality_report = split(/\n/,$concordance_quality_report);
 		pop @concordance_quality_report;
 		pop @concordance_quality_report;
-        # my $footer = pop @concordance_quality_report;
-        # $footer = pop(@concordance_quality_report) . "\n" . $footer;
-        
-        my $fabulous_concordance_quality_report = $self->make_fabulous(\@concordance_quality_report);
+                
+        if ($list eq 'variant_list_files') {
+			@cqr_unfiltered = @concordance_quality_report;
+        }
+        elsif ($list eq 'variant_filtered_list_files') {
+			@cqr_filtered = @concordance_quality_report;
+        }
+        else {
+            die "unknown SNV list $list!.  Cannot properly assign graph data strings!";
+        }
 
-        $body->print($fabulous_concordance_quality_report);
-        # $body->print("\n$footer</pre>");
-        $body->print("</div>"); #close content_padding div
-        next;
-        
-        # this now appears in the page source, but not in the page itself
-        $body->print("<!--\n");
-        #$body->print("DbSnp concordance command:  ".$concordance_cmd);
-        #$body->print("\n");
-        #$body->print("DbSnp concordance quality command:  ".$concordance_quality_cmd);
-        $body->print("\n");
-        $body->print("\n-->");
-
-		$body->print("</div>"); #close background div
-		$body->print("</div>"); #close container div
     }
 
+	
+    # let's make sure that the reports made it out of the loop:
+    $DB::single = 1;
+
+    # print "\@cqr_unfiltered:\n";
+    # print Dumper \@cqr_unfiltered;
+
+    # print "\@cqr_filtered:\n";
+    # print Dumper \@cqr_filtered;
+
+
+    #
+    # BUILD HTML REPORT
+    #
+
+    my $body = IO::String->new();
+    die unless $body;
+    my $r = new CGI;
+
+
+    # load page resources
+	
+    my $css_file = "$module_path.html.css";
+    my $css_fh = IO::File->new($css_file);
+    unless ($css_fh) {
+        die "failed to open file $css_file!"; 
+    }
+    my $css_content = join('',$css_fh->getlines);
+    
+    my $title =  'Db Snp for model ' . $model->id . ' ("' . $model->name . '")  build ' . $build_id . ')';
+    
+    $body->print($r->start_html(
+		-title  => $title,
+		-style  => { -code => $css_content },
+		-script => [
+			{ -type => 'text/javascript', -src => 'https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/js/jquery.js'},
+			{ -type => 'text/javascript', -src => 'https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/js/jquery.flot.js'}
+		]
+	));
+    
+    # $body->print("<small><a href='../../reports/Summary/report.html'>full report</a></small>");
+    $body->print('<div class="container">');
+    $body->print('<div class="background">');
+
+
+    my $header = "dbSNP Concordance for SNVs";
+
+    $body->print("<h1 class=\"section_title\">$header</h1>");
+    $body->print("<div class='content_padding'>");
+    $body->print("<pre>$concordance_report</pre>");
+    $body->print("<p/>");
+
+    $body->print("<h3>$header by Quality</h3>");
+
+    #
+    # BUILD GRAPH
+    #
+
+	## TODO: Parse the data and create the graph data strings using a proper function
+	## instead of repeating the same process twice. - jmcmicha
+
+	## begin filtered graph data assembly
+    my $filtered_lines = \@cqr_filtered;
+
+    my @filtered_x_axis;
+    my @filtered_dataset1;
+    my @filtered_dataset2;
+    my @filtered_current_data;
+
+    # Get the body
+    while (my $line = shift @$filtered_lines) {
+        @filtered_current_data = split("\t", $line);
+        push(@filtered_x_axis, $filtered_current_data[0]);
+        push(@filtered_dataset1, $filtered_current_data[1]);
+        push(@filtered_dataset2, $filtered_current_data[2]);
+    }
+
+    # Reverse the arrays since flot is printing this stuff backwards from what we would expect
+    @filtered_x_axis = reverse(@filtered_x_axis);
+    @filtered_dataset1 = reverse(@filtered_dataset1);
+    @filtered_dataset2 = reverse(@filtered_dataset2);
+    
+    my $filtered_db_snp_data = build_coordinate_string(\@filtered_x_axis, \@filtered_dataset1);
+    my $filtered_all_snp_data = build_coordinate_string(\@filtered_x_axis, \@filtered_dataset2);
+    my $filtered_concordance_data = build_concordance_string(\@filtered_x_axis, \@filtered_dataset1, \@filtered_dataset2);
+	## end filtered graph data assembly
+
+	## yeah, this is a horrible kludge.
+	
+	## begin unfiltered graph data assembly
+    my $unfiltered_lines = \@cqr_unfiltered;
+
+    my @unfiltered_x_axis;
+    my @unfiltered_dataset1;
+    my @unfiltered_dataset2;
+    my @unfiltered_current_data;
+
+    # Get the body
+    while (my $line = shift @$unfiltered_lines) {
+        @unfiltered_current_data = split("\t", $line);
+        push(@unfiltered_x_axis, $unfiltered_current_data[0]);
+        push(@unfiltered_dataset1, $unfiltered_current_data[1]);
+        push(@unfiltered_dataset2, $unfiltered_current_data[2]);
+    }
+
+    # Reverse the arrays since flot is printing this stuff backwards from what we would expect
+    @unfiltered_x_axis = reverse(@unfiltered_x_axis);
+    @unfiltered_dataset1 = reverse(@unfiltered_dataset1);
+    @unfiltered_dataset2 = reverse(@unfiltered_dataset2);
+    
+    my $unfiltered_db_snp_data = build_coordinate_string(\@unfiltered_x_axis, \@unfiltered_dataset1);
+    my $unfiltered_all_snp_data = build_coordinate_string(\@unfiltered_x_axis, \@unfiltered_dataset2);
+    my $unfiltered_concordance_data = build_concordance_string(\@unfiltered_x_axis, \@unfiltered_dataset1, \@unfiltered_dataset2);
+	## end unfiltered graph data assembly
+	
+    my $js_datasets = qq|
+    var datasets = {
+        "db SNP filtered": {
+            label: "db SNP filtered",
+            hoverable: true,
+            clickable: true,
+            shadowSize: 0,
+            data: $filtered_db_snp_data
+        },
+
+        "all SNPs filtered": {
+            label: "all SNPs filtered",
+            hoverable: true,
+            clickable: true,
+            shadowSize: 0,
+            data: $filtered_all_snp_data
+        },
+
+        "concordance filtered": {
+            label: "filtered concordance",
+            yaxis: 2,
+            hoverable: true,
+            clickable: true,
+            shadowSize: 0,
+            data: $filtered_concordance_data
+        },
+        "db SNP unfiltered": {
+            label: "db SNP unfiltered",
+            hoverable: true,
+            clickable: true,
+            shadowSize: 0,
+            data: $unfiltered_db_snp_data
+        },
+
+        "all SNPs unfiltered": {
+            label: "all SNPs unfiltered",
+            hoverable: true,
+            clickable: true,
+            shadowSize: 0,
+            data: $unfiltered_all_snp_data
+        },
+
+        "concordance unfiltered": {
+            label: "unfiltered concordance",
+            yaxis: 2,
+            hoverable: true,
+            clickable: true,
+            shadowSize: 0,
+            data: $unfiltered_concordance_data
+        }
+
+    };|;
+
+    my $js_file = "$module_path.html.js";
+    my $js_fh = IO::File->new($js_file);
+    unless ($js_fh) {
+        die "failed to open file $js_file!"; 
+    }
+    my $js_content = join('',$js_fh->getlines);
+	
+	$body->print("<script type='text/javascript'>//<![CDATA[\n\n");
+	$body->print("\$(function() {");
+	$body->print($js_datasets);
+	$body->print($js_content);
+	$body->print("});");
+	$body->print("\n\n//]]></script>");
+	
+    my $flot_graph = qq|
+
+<table width="100%" cellpadding="5" cellspacing="0">
+  <tr>
+   <td valign="middle"><img src="https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/images/axis_label_v_SNPs.png" width="19" height="49"/></td>
+   <td align="center" valign="middle"><div id="placeholder" class="graph_placeholder"/></td>
+   <td valign="middle"><img src="https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/images/axis_label_v_pct_Concordance.png" width="19" height="151"/></td>
+  </tr>
+  <tr>
+   <td>&nbsp;</td>
+   <td align="center"><img src="https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/images/axis_label_h_Quality.png" width="67" height="23"/></td>
+   <td>&nbsp;</td>
+  </tr>
+  <tr>
+   <td colspan="3">
+    <div id="plots"><p><strong>Show:</strong</p></div>
+   </td>
+  </tr>
+</table>
+        |;
+		
+    $body->print($flot_graph);
+    $body->print("</div>"); #close content_padding div
+    $body->print("</div>"); #close background div
+    $body->print("</div>"); #close container div	
     $body->print( $r->end_html );
     $body->seek(0,0);
     
@@ -280,140 +437,6 @@ sub _generate_combined_snp_file
     return $fname;
 }
 
-my $n = 0;
-sub make_fabulous {
-    my $self = shift;
-    my $lines = shift;
-
-    $n++;
-
-$DB::single = 1;
-
-    my @x_axis;
-    my @dataset1;
-    my @dataset2;
-    my @current_data;
-    my $string_of_data;
-    my $line;
-    my $header;
-    my $footer;
-
-    # Get the body
-    while (my $line = shift @$lines) {
-        @current_data = split("\t", $line);
-        push(@x_axis, $current_data[0]);
-        push(@dataset1, $current_data[1]);
-        push(@dataset2, $current_data[2]);
-    }
-
-    # Reverse the arrays since flot is printing this stuff backwards from what we would expect
-    @x_axis = reverse(@x_axis);
-    @dataset1 = reverse(@dataset1);
-    @dataset2 = reverse(@dataset2);
-    
-    my $db_snp_data = build_coordinate_string(\@x_axis, \@dataset1);
-    my $all_snp_data = build_coordinate_string(\@x_axis, \@dataset2);
-    my $concordance_data = build_concordance_string(\@x_axis, \@dataset1, \@dataset2);
-
-    my $db_snp_string = "{ label: \"db snp\", hoverable: true, clickable: true, data: $db_snp_data }";
-    my $total_snp_string = "{ label: \"all snps\", hoverable: true, clickable: true, data: $all_snp_data }";
-    my $concordance_data_string = "{ label: \"concordance\", yaxis: 2,  hoverable: true, clickable: true, data: $concordance_data }";
-
-
-    my $graph_data_string = "[ $db_snp_string, $total_snp_string, $concordance_data_string ]";
-    
-    my $javascript_block=qq|
-        <script id="source" language="javascript" type="text/javascript">
-        \$(function () {
-            var options = {
-                legend: { show: true, position: "ne", margin: 20},
-                lines: { show: true },
-                points: { show: false },
-                xaxis: { ticks: 10 },
-                yaxis: { tickFormatter: function (v, axis) { return addCommas(v) }},
-                y2axis: { ticks: 10, tickFormatter: function (v, axis) { return v + "%"  } },
-                grid: { hoverable: true, clickable: true },
-            };
-        
-            \$.plot(\$("#placeholder$n"), $graph_data_string, options);
-        });
-
-    function addCommas(number) {
-        number = '' + number;
-        if (number.length > 3) {
-        var mod = number.length % 3;
-        var output = (mod > 0 ? (number.substring(0,mod)) : '');
-        for (i=0 ; i < Math.floor(number.length / 3); i++) {
-                if ((mod == 0) && (i == 0))
-                output += number.substring(mod+ 3 * i, mod + 3 * i + 3);
-                else
-                output+= ',' + number.substring(mod + 3 * i, mod + 3 * i + 3);
-        }
-        return (output);
-        }
-        else return number;
-    }
-
-    function showTooltip(x, y, contents) {
-        \$('<div id="tooltip">' + contents + '</div>').css( {
-            position: 'absolute',
-            display: 'none',
-            top: y + 5,
-            left: x + 5,
-            border: '1px solid #fdd',
-            padding: '2px',
-            'background-color': '#fee',
-            opacity: 0.80
-        }).appendTo("body").fadeIn(200);
-    }
-
-    var previousPoint = null;
-    \$("#placeholder$n").bind("plothover", function (event, pos, item) {
-        \$("#x").text(pos.x.toFixed(2));
-        \$("#y").text(pos.y.toFixed(2));
-
-            if (item) {
-                if (previousPoint != item.datapoint) {
-                    previousPoint = item.datapoint;
-                    
-                    \$("#tooltip").remove();
-                    var x = item.datapoint[0].toFixed(2),
-                        y = item.datapoint[1].toFixed(2);
-                    
-                    showTooltip(item.pageX, item.pageY,
-                                item.series.label + " at " + x + " = " + y);
-                }
-            }
-            else {
-                \$("#tooltip").remove();
-                previousPoint = null;            
-            }
-
-    });
-
-        </script> 
-    |;
-    
-    my $output = qq|
-
-<table width="100%" cellpadding="5" cellspacing="0">
-  <tr>
-   <td valign="middle"><img src="https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/images/axis_label_v_SNPs.png" width="19" height="49"/></td>
-   <td align="center" valign="middle"><div id="placeholder$n" class="graph_placeholder"/></td>
-   <td valign="middle"><img src="https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/images/axis_label_v_pct_Concordance.png" width="19" height="151"/></td>
-  </tr>
-  <tr>
-   <td>&nbsp;</td>
-   <td align="center"><img src="https://gscweb.gsc.wustl.edu/report_resources/db_snp_concordance/images/axis_label_h_Quality.png" width="67" height="23"/></td>
-   <td>&nbsp;</td>
-  </tr>
-</table>
-
-        $javascript_block
-        |;
-    return $output;
-}
-
 sub build_coordinate_string {
   my $x_axis_ref = shift;
   my $data_set_ref = shift;
@@ -421,7 +444,8 @@ sub build_coordinate_string {
   my $formatted_return="[ ";
   
   for my $x_point (@{$x_axis_ref}) {
-    $formatted_return .= "[ $x_point , " . $data_set_ref->[$x_point] . "],\n ";
+	  my $v1 = $data_set_ref->[$x_point] || 0;
+	  $formatted_return .= "[ $x_point , " . $v1 . "], ";
   }
   
   $formatted_return .= "] ";
@@ -438,16 +462,16 @@ sub build_concordance_string {
   for my $x_point (@{$x_axis_ref}) {
     my $v1 =  $data_set_ref1->[$x_point];
     my $v2 = $data_set_ref2->[$x_point];
-		my $v3;
-		if (!$v2) {
-			$v3 = 0;
-		} else {
-			$v3 = $v1/$v2;
-		}
+	my $v3;
 
-    $formatted_return .= "[ $x_point , " . sprintf("%.2f", 100 * $v3) . "], /* $v1\t$v2\t$v3 */\n";
+	if (!$v2) {
+	 	$v3 = 0;
+	} else {
+	 	$v3 = $v1/$v2;
+	}
+
+    $formatted_return .= "[ $x_point , " . sprintf("%.2f", 100 * $v3) . "], ";
   }
-  
   
   $formatted_return .= "] ";
   return $formatted_return;
