719e9ed4272f0baac5b3f903c86d0a85a8308783
jmcmicha <jmcmicha@genome.wustl.edu>
2009-04-15 21:35:40 +0000

    % concordance axis now is forced to display full 0-100% range
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45864 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ReferenceAlignment/Report/DbSnpConcordance.pm.html.js
+++ Genome/Model/ReferenceAlignment/Report/DbSnpConcordance.pm.html.js
@@ -28,8 +28,7 @@ function addCommas(number) {
 			}
         }
         return (output);
-    }
-    else { return number };
+    } else { return number; };
 }
 
 function showTooltip(x, y, contents) {
@@ -49,45 +48,45 @@ var previousPoint = null;
 $("#placeholder").bind("plothover", function (event, pos, item) {
     $("#x").text(pos.x.toFixed(2));
     $("#y").text(pos.y.toFixed(2));
-	
+
     if (item) {
         if (previousPoint != item.datapoint) {
             previousPoint = item.datapoint;
-            
+
             $("#tooltip").remove();
             var x = item.datapoint[0].toFixed(2),
             y = item.datapoint[1].toFixed(2);
-            
+
             showTooltip(item.pageX, item.pageY, item.series.label + " at " + x + " = " + y);
         }
     }
     else {
         $("#tooltip").remove();
-        previousPoint = null;            
+        previousPoint = null;
     }
 });
 
 function plotAccordingToChoices() {
 	var data = [];
-	
+
 	choiceContainer.find("input:checked").each(function () {
 		var key = $(this).attr("name");
 		if (key && datasets[key]) {
 			data.push(datasets[key]);
 		}
 	});
-	
+
 	if (data.length > 0) {
 		$.plot($("#placeholder"), data, {
-			legend: { show: true, position: "ne", margin: 20},
-			lines: { show: true },
-			points: { show: false },
-			xaxis: { ticks: 10 },
-			yaxis: { tickFormatter: function (v, axis) { return addCommas(v); }},
-			y2axis: { ticks: 10, tickFormatter: function (v, axis) { return v + "%";  } },
-			grid: { hoverable: true, clickable: true }
+				 legend: { show: true, position: "ne", margin: 20},
+				 lines: { show: true },
+				 points: { show: false },
+				 xaxis: { ticks: 10 },
+				 yaxis: { tickFormatter: function (v, axis) { return addCommas(v); }},
+				 y2axis: { ticks: 10, min: 0, max: 100, tickFormatter: function (v, axis) { return v + "%";  } },
+				 grid: { hoverable: true, clickable: true },
+				 markings: [ { y2axis: { from:0, to: 100 } } ]
 		});
 	}
 }
 plotAccordingToChoices();
-
