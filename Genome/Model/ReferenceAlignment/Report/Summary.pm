a87a8ce202d083a4be807e5aae4a4af096423873
ssmith <ssmith@genome.wustl.edu>
2009-04-15 03:27:03 +0000

    Switched the methods on the build on which this runs to be the new ones which return combined files.  Improved the line counter to handle the single-item lists from wc.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45809 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ReferenceAlignment/Report/Summary.pm
+++ Genome/Model/ReferenceAlignment/Report/Summary.pm
@@ -255,14 +255,14 @@ $DB::single = 1;
     # processing profile
     my $pp = $model->processing_profile;
 
-    my @unfiltered_files = $build->_variant_list_files;
+    my @unfiltered_files = $build->_snv_file_unfiltered;
     my $unfiltered_snp_calls = `wc -l @unfiltered_files | tail -n 1`;
-    $unfiltered_snp_calls =~ s/total//i;
+    $unfiltered_snp_calls =~ s/\s\S+\s*$//i;
     $unfiltered_snp_calls =~ s/\s//g;
     
-    my @filtered_files = $build->_variant_filtered_list_files;
+    my @filtered_files = $build->_snv_file_filtered;
     my $filtered_snp_calls = `wc -l @filtered_files | tail -n 1`;
-    $filtered_snp_calls =~ s/total//i;
+    $filtered_snp_calls =~ s/\s\S+\s*$//i;
     $filtered_snp_calls =~ s/\s//g;
 
     my @stat = stat($filtered_files[-1]); 
