48e69ba1ad6f8a55a95891d5a98d402578879b59
ssmith <ssmith@genome.wustl.edu>
2009-04-19 15:03:52 +0000

    Added method to resolve adaptor file (jason).
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45942 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Solexa.pm
+++ Genome/InstrumentData/Solexa.pm
@@ -305,6 +305,32 @@ sub resolve_quality_converter {
     return $analysis_software_versions{$analysis_software_version};
 }
 
+sub resolve_adaptor_file {
+    my $self = shift;
+
+    #these are constants and should probably be defined in class properties...TODO
+    my $dna_primer_file = '/gscmnt/sata114/info/medseq/adaptor_sequences/solexa_adaptor_pcr_primer';
+    my $rna_primer_file = '/gscmnt/sata114/info/medseq/adaptor_sequences/solexa_adaptor_pcr_primer_SMART';
+
+    if ( defined($self->sample_type) ){
+        my $adaptor_file;
+        if ( $self->sample_type eq 'dna' ) {
+            $adaptor_file = $dna_primer_file;
+        } elsif ( $self->sample_type eq 'rna' ) {
+            $adaptor_file = $rna_primer_file;
+        }
+        if ($adaptor_file) {
+            unless (-f $adaptor_file) {
+                $self->error_message('Specified adaptor file'. $adaptor_file .' does not exist.');
+                die($self->error_message);
+            }
+        }
+    }
+    return;
+}
+
+
+
 1;
 
 #$HeaderURL$
