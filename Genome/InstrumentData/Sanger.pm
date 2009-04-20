cae9a528008ff4e3703121589d647a0db42c7927
mjohnson <mjohnson@genome.wustl.edu>
2009-04-20 17:10:46 +0000

    Added library_name and sample_name properties to Genome::InstrumentData::Sanger.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45984 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Sanger.pm
+++ Genome/InstrumentData/Sanger.pm
@@ -10,12 +10,32 @@ class Genome::InstrumentData::Sanger {
     has => [
     #< Run from OLTP Attrs >#
     _gsc_run => {
-        doc => 'GSC Run from LIMS',
-        is => 'GSC::Run',
-        calculate_from => [qw/ id /],
-        calculate => q| GSC::Run->get($id); |,
+                 doc => 'GSC Run from LIMS',
+                 is => 'GSC::Run',
+                 calculate_from => [qw/ id /],
+                 calculate => q| GSC::Run->get($id); |,
     },
-    ]
+    sample_name => {
+                    via   => 'attributes',
+                    to    => 'value',
+                    where => [
+                              entity_class_name => 'Genome::InstrumentData::Sanger',
+                              property_name     => 'sample_name',
+                          ],
+                    is_optional => 1,
+                    is_mutable  => 1,
+                },     
+    library_name => {
+                     via   => 'attributes',
+                     to    => 'value',
+                     where => [
+                               entity_class_name => 'Genome::InstrumentData::Sanger',
+                               property_name     => 'library_name',
+                           ],
+                     is_optional => 1,
+                     is_mutable  => 1,
+                 },
+    ],
 };
 
 sub _data_base_path {
@@ -73,6 +93,41 @@ sub _get_read_iterator {
     }
 
     return $reads;
+}
+
+sub get_library_summary {
+    my $self = shift;
+    return GSC::LibrarySummary->get(full_name => $self->library_name);
+}
+
+sub get_source_sample {
+    my $self = shift;
+
+    my $library_summary = $self->get_library_summary;
+    unless ($library_summary) {
+        return;
+    }
+    return $library_summary->get_source_sample;
+}
+
+sub get_population {
+    my $self = shift;
+
+    my $library_summary = $self->get_library_summary;
+    unless ($library_summary) {
+        return;
+    }
+    return $library_summary->get_population;
+}
+
+sub get_organism_taxon {
+    my $self = shift;
+
+    my $library_summary = $self->get_library_summary;
+    unless ($library_summary) {
+        return;
+    }
+    return $library_summary->get_organism_taxon;
 }
 
 1;
