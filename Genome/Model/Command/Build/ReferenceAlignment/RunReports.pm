c526f8e34ba5cc1b7600f2285df0d2c2864acce5
jwalker <jwalker@genome.wustl.edu>
2009-04-20 15:39:52 +0000

    remove all code that was partially responsible for generating reports, now simply loop over a list of report names and run/generate them in order.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45969 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ReferenceAlignment/RunReports.pm
+++ Genome/Model/Command/Build/ReferenceAlignment/RunReports.pm
@@ -3,25 +3,16 @@ package Genome::Model::Command::Build::ReferenceAlignment::RunReports;
 use strict;
 use warnings;
 
-use above "Genome";
-
 use Genome;
-#use Data::Dumper;
-#use Genome::Model::Command::Report::VariationsBatchToLsf;
-#use Genome::Model::EventWithRefSeq;
-#use Genome::Utility::Parser;
-use IO::File;
 
 class Genome::Model::Command::Build::ReferenceAlignment::RunReports {
-    is => [ 'Genome::Model::EventWithRefSeq' ],
-    #is => [ 'Genome::Model::Event' ],
+    is => [ 'Genome::Model::Event' ],
     has => [
     ],
 };
 
 #########################################################
 
-#sub sub_command_sort_position { 90 } # TODO needed?
 
 # TODO Add doc
 sub help_brief {
@@ -38,254 +29,100 @@ Generates GoldSnp report, etc
 EOS
 }
 
-#sub bsub_rusage { return "-R 'span[hosts=1]'"; } 
+my @REPORT_TYPES = qw/
+        Mapcheck
+        DbSnpConcordance
+        GoldSnpConcordance
+    /;
 
-#- EXECUTE -#
 sub execute {
     my $self = shift;
-    
-    my $ts = time();
+
     my $build = $self->build;
-    my $build_id = $self->build_id;
-    my $model = $self->model;
-    
-    my $id = $model->genome_model_id;
-    
-    unless ($self->create_directory($self->build->resolve_reports_directory) ) {
-	die "Could not create reports directory at: ".$self->build->resolve_reports_directory;
-    } 
- 
-    my $maq_snp_dir = $self->build->maq_snp_related_metric_directory;
-    $self->status_message('Maq snp related dir: '.$maq_snp_dir);
-    
-    ###############################################
-    ###Generate the big snp file
-
-    my @snp_list = $self->build->_variant_list_files;
-    my $file_list = join(" ", @snp_list);
-    $self->status_message("Variant list files: $file_list");
-    my $snp_file = "$maq_snp_dir/snpfile_$id"."_$ts.snp";
-    my $snp_file_cmd = "cat $file_list > $snp_file";
-    $self->status_message('Snp file concatenation command: '.$snp_file_cmd);
-    my $snp_file_result = `$snp_file_cmd`;
-   
-    #Note:  maq_snp_related_metric_directory in Solex.pm
-     
-    ###SNP Filter############################################
-    $self->status_message('Starting SNP Filter report.');
-    #maq.pl SNPfilter [snpfile] > [outputfile]
-    my $snp_filter_path = "$maq_snp_dir/snpfilter_report_$id"."_$ts.out";
-    $self->status_message('Dumping SNPfilter output to: '.$snp_filter_path);
-    my $snp_filter_cmd = "maq.pl SNPfilter $snp_file > $snp_filter_path";
-    $self->status_message("SNPfilter command: $snp_filter_cmd");
-    my $snp_filter_result = `$snp_filter_cmd`;
-    $self->status_message('Completed SNP Filter report.');
-    
-
-    my $accumulated_alignments_file;
-
-    ###############################################
-    $self->status_message('Starting MapCheck report.');
-    my $MapCheck_report_name = 'RefSeqMaq';
-    #model id for previous test:  2733662090
-    my $MapCheck_report = Genome::Model::ReferenceAlignment::Report::RefSeqMaq->create(
-                                                                                       build_id => $build_id,
-                                                                                       name => $MapCheck_report_name,
-                                                                                       version => $self->model->read_aligner_version
-                                                                                   );
-    #my $MapCheck_report = Genome::Model::Report::RefSeqMaq->create(build_id =>$build_id, name=>$MapCheck_report_name, version=>$self->model->read_aligner_version);
-    $accumulated_alignments_file = $self->accumulate_maps();
-    unless ($accumulated_alignments_file) {
-        $self->error_message('Failed to get accumulated maps file');
-        return;
-    }
-    $self->status_message('The accumulated alignments file is: '.$accumulated_alignments_file);
-    $MapCheck_report->accumulated_alignments_file($accumulated_alignments_file);
-    #$MapCheck_report->generate_report_detail();
-    $self->build->add_report( $MapCheck_report->generate_report );
-    $self->status_message('Finished MapCheck report.');
-  
-    ###############################################
-    #cleaning up accumulated file
-    my $rm_cmd = "rm $accumulated_alignments_file";
-    $self->status_message("Removing accumlated file with command: $rm_cmd");
-    my $rv = `$rm_cmd`;
-    $self->status_message("Result of remove: $rv");
-   
-        
-    #Indelpe##############################################
-    #cd /gscmnt/sata810/info/medseq/GBM_71_maps/
-    #maq indelpe /gscmnt/839/info/medseq/reference_sequences/NCBI-human-build36/all_sequences.bfa bigmap.map > normal.indelpe
-    $self->status_message('Starting Indelpe report.');
-    my $aligner_path = $self->aligner_path('read_aligner_version'); 
-    my $ref_seq = $model->reference_sequence_path."/all_sequences.bfa"; 
-    my $indelpe_output_path = "$maq_snp_dir/indelpe_report_$id"."_$ts.out";
-$DB::single = 1;
-    $accumulated_alignments_file = $self->accumulate_maps();
-
-    unless ($accumulated_alignments_file) {
-        $self->error_message('Failed to get accumulated map file');
-        return;
+    unless ($self->create_directory($build->resolve_reports_directory) ) {
+	die('Could not create reports directory at: '. $build->resolve_reports_directory);
     }
 
-    $self->status_message('Acc. alignments file: '.$accumulated_alignments_file);
-    $self->status_message('Ref Seq file: '.$ref_seq.' has a size of: '.-s $ref_seq);
-    $self->status_message('The accumulated alignments file is: '.$accumulated_alignments_file);
-    my $indelpe_cmd = "$aligner_path indelpe $ref_seq $accumulated_alignments_file > $indelpe_output_path";
-    my $indelpe_result = $self->shellcmd(
-        cmd => $indelpe_cmd,
-        input_files => [$aligner_path, $ref_seq, $accumulated_alignments_file],
-        #TODO: add flag to allow for null/zero size outputs
-        #output_files => [$indelpe_output_path],
-    );
-    unless (-s $indelpe_output_path) {
-	$self->status_message('Zero size or non-existent indelpe output file '. $indelpe_output_path); 
-    }
-    $self->status_message('Indelpe result: '.$indelpe_result);
-    $self->status_message('Indelpe output file:'.$indelpe_output_path.' has a size of: '.-s $indelpe_output_path);
-    $self->status_message('Completed Indelpe report.');
-   
-    ###############################################
-    # TODO: get the rest of them to fit here, 
-    # then put the list in the processing profile,
-    # then allow it to be extended arbitrarily with model hang-offs.
-    my $failures = 0;
-    for my $report_type (qw/
-        DbSnpConcordance 
-        GoldSnpConcordance
-    /) {
+    for my $report_type (@REPORT_TYPES) {
         my $report_class = 'Genome::Model::ReferenceAlignment::Report::' . $report_type;
         my $report_name = 'unknown';
-        eval {
-            
-            $self->status_message("Starting $report_type report.");
-            
-            my $report_def = $report_class->create(build_id =>$build_id);
-            unless ($report_def) {
-                $self->error_message("Error creating report $report_class!: " . $report_class->error_message());
-                $failures++;
-                next;
-            }
-            $report_name = $report_def->name;
-            $self->status_message("Defined report with name $report_name");
-            
-            my $report = $report_def->generate_report;
-            unless ($report) {
-                $self->error_message("Error generating $report_name ($report_class)!: " . $report_class->error_message());
-                $failures++;
-                $report_def->delete;
-                next;
-            } else {
-		$self->status_message("Successfully generated report: $report_name");
-            }
- 
-            $self->status_message("About to add report: $report_name to build: ".$self->build->id);
-            if ($self->build->add_report($report)) {
-            	$self->status_message('Saved report: '.$report);
-            } else {
-                $self->error_message('Error saving '.$report.'. Error: '. $self->build->error_message);
-	    }
-        };
-        if ($@) {
-            $self->error_message("Error generationg report named '$report_name' (class: $report_class):\n$@");
-            $failures++;
-            next;
+        $self->status_message("Starting $report_type report.");
+
+        my $report_def = $report_class->create(build_id => $build->id);
+        unless ($report_def) {
+            $self->error_message("Error creating report $report_class!: " . $report_class->error_message());
+            die($self->error_message);
+        }
+        $report_name = $report_def->name;
+        $self->status_message("Defined report with name $report_name");
+        my $report = $report_def->generate_report;
+        unless ($report) {
+            $self->error_message("Error generating $report_name ($report_class)!: " . $report_class->error_message());
+            $report_def->delete;
+            die($self->error_message);
+        } else {
+            $self->status_message("Successfully generated report: $report_name");
+        }
+        $self->status_message("About to add report: $report_name to build: ".$self->build->id);
+        if ($build->add_report($report)) {
+            $self->status_message('Saved report: '.$report);
+        } else {
+            $self->error_message('Error saving '.$report.'. Error: '. $self->build->error_message);
+            die($self->error_message);
         }
     }
 
     ##############################################
+    #Summary Report
+
     $self->status_message('Starting report summary.');
-    my $r = Genome::Model::ReferenceAlignment::Report::Summary->create( build_id => $self->build_id );
+    my $r = Genome::Model::ReferenceAlignment::Report::Summary->create( build_id => $build->id );
 
     my @templates = $r->report_templates;
-    $self->status_message("Using report templates: ".join(",",@templates));  
+    $self->status_message("Using report templates: ". join(",",@templates));
 
-    my $generated_report = $r->generate_report;
-
-    my $result = $generated_report->save($self->build->resolve_reports_directory);
-
-    $self->status_message("Save result return value: $result. Saved report: ".$generated_report->name. " to ". $self->build->resolve_reports_directory);
- 
+    my $summary_report = $r->generate_report;
+    unless ($summary_report->save($build->resolve_reports_directory)) {
+        $self->error_message("Failed to save report: ". $summary_report->name .' to '. $build->resolve_reports_directory);
+        return;
+    }
     $self->status_message('Report summary complete.');
 
-    ################################################### 
-    my $mail_dest = ($build_id < 0 ? Genome::Config->user_email() . ',charris@genome.wustl.edu' : 'apipe@genome.wustl.edu');
+    ###################################################
+    #Send user email
+
+    my $mail_dest = ($build->id < 0 ? Genome::Config->user_email() . ',charris@genome.wustl.edu' : 'apipe@genome.wustl.edu');
     $self->status_message('Sending summary e-mail to ' . $mail_dest);
     my $mail_rv = Genome::Model::Command::Report::Mail->execute(
-        model => $build->model,
+        model => $self->model,
         build => $build,
         report_name => "Summary",
         to => $mail_dest,
     );
     $self->status_message("E-mail command executed.  Return value: $mail_rv");
-   
-    ############################################### 
-    #PFAM Reports
-    #$self->status_message("Starting Pfam report for build id: $build_id");
-    #my $p = Genome::Model::ReferenceAlignment::Report::Pfam->create(
-    #                                     build_id     => $build_id,
-    #                                      name         => 'Pfam',
-    #                                    );
-    #$p->generate_report_detail(report_detail => "full_report_test.csv");
-    #$self->status_message('Completed Pfam report.');
-    ############################################### 
-    
+
     ###############################################
-    $rm_cmd = "rm $accumulated_alignments_file";
-    $self->status_message("Removing accumlated file with command: $rm_cmd");
-    $rv = `$rm_cmd`;
-    $self->status_message("Result of remove: $rv");
-    
-    my $success = $self->verify_successful_completion();
-        
-    if ( $success )
-    { 
-        $self->event_status("Succeeded");
-    }
-    else 
-    {
-        $self->event_status("Failed");
-    }
 
-    $self->date_completed( UR::Time->now );
-
-    return $success;
+    return $self->verify_successful_completion;
 }
 
 sub verify_successful_completion {
-
     my $self = shift;
 
-    my $return_value = 1;
     my $build = $self->build;
-
-    if ( defined($build) ) {
-            my $report_dir = $self->build->resolve_reports_directory;
-            $self->status_message('Report dir: '.$report_dir);
-            my @report_dirs = glob($report_dir."/*");
-            $self->status_message('Contents of report dir: '.join(",",@report_dirs) );
-	    my $dir_count = 0;
-	    for my $each_dir (@report_dirs) {
-		if (-d $each_dir) {
-			$dir_count = $dir_count + 1;
- 	        }	
-	    }
-
-$DB::single = 1;            
- 	    unless ( $dir_count == 4 ) {
-                $self->error_message("Can't verify successful completeion of RunReports step.  Expecting 4 report directories.  Got: $dir_count");
-                $self->error_message('Report dir: '.$report_dir);
-                $self->error_message('Contents of report dir: '.join(",",@report_dirs) );
-                return 0;
-            }
- 
-    } else {
-        $self->error_message("Can't verify successful completion of RunReports step. Build is undefined.");
-        return 0;
+    unless ($build) {
+        $self->error_message('Failed verify_successful_completion of RunReports step. Build is undefined.');
+        return;
     }
+    my $report_dir = $build->resolve_reports_directory;
 
-    return $return_value;
-
+    for my $sub_directory ( 'Mapcheck', 'dbSNP_Concordance', 'Gold_SNP_Concordance', 'Summary') {
+        unless (-d $report_dir .'/'. $sub_directory) {
+            $self->error_message('Failed verify_successful_completeion of RunReports step.  Failed to find directory: '. $report_dir .'/'. $sub_directory);
+            return;
+        }
+    }
+    return 1;
 }
 
 1;
