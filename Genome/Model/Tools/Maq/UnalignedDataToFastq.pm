d0f368b248684d4dd9ca1f2ba279cd600b73340f
ssmith <ssmith@genome.wustl.edu>
2009-04-20 04:33:49 +0000

    Added a test case, expanded functionality to handle making a pair of fastqs for paired-end data.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@45954 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Maq/UnalignedDataToFastq.pm
+++ Genome/Model/Tools/Maq/UnalignedDataToFastq.pm
@@ -8,18 +8,27 @@ use Genome;
 class Genome::Model::Tools::Maq::UnalignedDataToFastq {
     is => 'Command',
     has => [
-        in => { is => 'String', doc => "Pathname to a file generated from maq map's -u option" },
-        fastq => { is => 'String', doc => "pathname of the fastq file to write" },
+        in              => { is => 'Text', 
+                            doc => "Pathname to a file generated from maq map's -u option", 
+                            shell_args_position => 1 },
     ],
-};
+    has_optional => [
+        fastq           => { is => 'Text', 
+                            doc => 'the output pathname for "forward" reads (or all reads on a fragment run)', 
+                            shell_args_position => 2 },
 
-sub help_brief {
-    "Create a new fastq-format file containing reads that aligned poorly in the prior align-reads step";
-}
+        reverse_fastq   => { is => 'Text', 
+                            doc => 'the output pathname for "reverse" for paired-end data', 
+                            shell_args_position => 3 },
+    ],
+    doc => "Create a new fastq-format file containing reads that aligned poorly in the prior align-reads step"
+};
 
 sub help_synopsis {
     return <<"EOS"
-    gt maq unaligned-data-to-fastq --in /path/to/inputpathname.unaligned --fastq /path/to/data.fastq
+    gt maq unaligned-data-to-fastq in.maq-unaligned out.fwd.fastq out.rev.fastq
+    
+    gt maq unaligned-data-to-fastq -i /path/to/inputpathname.unaligned -f /path/to/forward.fastq -r /path/to/reverse.fastq
 EOS
 }
 
@@ -27,11 +36,13 @@ sub help_detail {
     return <<EOS 
 As part of the aligmnent process, the -u option to maq will create a file containing 
 reads that had low alignment quailty, or did not align at all.  This command
-will take that file and create a fastq-formatted file containing those reads
+will take that file and create a fastq-formatted file containing those reads.
+
+For paired-end data, this tool will make 2 files, and expect 2 outputs to be specified.
+
+It will correct the error common in some maq unaligned files that read #2 is misnamed with the name of its mate.
 EOS
 }
-
-
 
 sub execute {
     my $self = shift;
@@ -45,66 +56,68 @@ $DB::single = $DB::stopper;
         return;
     }
 
-    my $unaligned_fastq_file = $self->fastq();
-    my $fastq = IO::File->new(">$unaligned_fastq_file");
-    unless ($fastq) {
-        $self->error_message("Unable to open $unaligned_fastq_file for writing: $!");
+    my $unaligned_fastq_file1 = $self->fastq();
+    my $fastq1 = IO::File->new(">$unaligned_fastq_file1");
+    unless ($fastq1) {
+        $self->error_message("Unable to open $unaligned_fastq_file1 for writing: $!");
         return;
     }
   
-    while(<$unaligned>) {
-        chomp;
-        my($read_name,$alignment_quality,$sequence,$read_quality) = split;
-        $fastq->print("\@$read_name\n$sequence\n\+\n$read_quality\n");
+    my $unaligned_fastq_file2 = $self->reverse_fastq();
+    my $fastq2;
+
+    if ($unaligned_fastq_file2) {
+        $fastq2 = IO::File->new(">$unaligned_fastq_file2");
+        unless ($fastq2) {
+            $self->error_message("Unable to open $unaligned_fastq_file2 for writing: $!");
+            return;
+        }
+    }
+
+    my ($read_name,$alignment_quality,$sequence,$read_quality);
+    my $last_read_name;
+    my $warned;
+    if ($fastq2) {
+        while(<$unaligned>) {
+            chomp;
+            ($read_name,$alignment_quality,$sequence,$read_quality) = split;
+            if ($read_name !~ /\/1\s*$/) {
+                die "bad read name $read_name.  expected forward read to end in /1";
+            }
+            $fastq1->print("\@$read_name\n$sequence\n\+\n$read_quality\n");
+
+            $last_read_name = $read_name;
+
+            my $rev = <$unaligned>;
+            chomp $rev;
+            ($read_name,$alignment_quality,$sequence,$read_quality) = split(/\s+/,$rev);
+            if ($read_name eq $last_read_name) {
+                substr($read_name,length($read_name)-1,1) = '2';
+                unless ($warned) {
+                    $self->status_message("repairing the reverse read names in the unaligned reads file when producing a new fastq ($read_name)...");
+                    $warned = 1;
+                } 
+            }
+            elsif ($read_name !~ /\/2\s*$/) {
+                die "bad read name $read_name.  expected reverse read to end in /2 (or to have a name matching its mate we can repair)";
+            }
+            $fastq2->print("\@$read_name\n$sequence\n\+\n$read_quality\n");
+        }
+        
+    }
+    else {
+        while(<$unaligned>) {
+            chomp;
+            my($read_name,$alignment_quality,$sequence,$read_quality) = split;
+            $fastq1->print("\@$read_name\n$sequence\n\+\n$read_quality\n");
+        }
     }
 
     $unaligned->close();
-    $fastq->close();
-
+    $fastq1->close();
+    $fastq2->close() if $fastq2;
     return 1;
 }
-
-
-
-# The sequence/quality in the unaligned data file is exactly the same
-# as in the original fastq file, so we can skip looking up the data in
-# the original file
-#
-# The filehandle passed in here must be from a sorted fastq file,
-# and you must ask for read records that are sorted in the same order
-sub _get_original_data_for_read_name {
-    my($self,$read_name,$fastq_fh) = @_;
-
-    $read_name = "\@$read_name";   # The fastq records still have the @ in the read name (for now)
-
-    while(my $record = $self->get_next_fastq_record($fastq_fh) ) {
-        return $record if ($record->{'read_name'} eq $read_name);
-    }
-
-    return;
-}
-
-
-# FIXME This should go in a better location later
-# returns the next record of data from a fastq filehandle
-sub get_next_fastq_record {
-    my($self,$fh) = @_;
-
-    my %node;
-    my $read_name = $fh->getline;
-    return unless $read_name;
-
-    chomp($node{'read_name'} = $read_name);;
-
-    chomp($node{'sequence'} = $fh->getline);
-    $fh->getline;  # This should be the read name again, or just a '+'
-    chomp($node{'quality'} = $fh->getline);
-
-    return \%node;
-}
-
-    
-
 
 1;
 
