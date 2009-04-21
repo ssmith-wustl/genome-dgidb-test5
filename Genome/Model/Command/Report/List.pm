3bfad1140a06d4e485f7566327768a948fc68fce
ebelter <ebelter@genome.wustl.edu>
2009-04-21 16:21:10 +0000

    added listing of builds reports
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@46043 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Report/List.pm
+++ Genome/Model/Command/Report/List.pm
@@ -5,17 +5,29 @@ use warnings;
 
 use Genome;
 
+use Carp 'confess';
 use Data::Dumper 'Dumper';
 require Term::ANSIColor;
 require Text::Wrap;
 
 class Genome::Model::Command::Report::List {
     is => 'Command',
+    #is => 'Genome::Model::Command::Report',
     has_optional => [
-        model_sub_type => {
+        type_name => {
             is => 'Text', 
             is_input => 1,
             doc => 'List reports for this model sub type only' 
+        },
+        build => { 
+            is => 'Genome::Model::Build', 
+            id_by => 'build_id',
+            doc => 'The build of a genome model to check reports',
+        },
+        build_id => {
+            is => 'Integer',
+            is_input => 1,
+            doc => 'The build id of a genome model to check reports',
         },
     ],
 };
@@ -23,24 +35,25 @@ class Genome::Model::Command::Report::List {
 ###################################################
 
 sub help_brief {
-    return 'Lists a models reports';
+    return 'Lists reports for builds or model types';
 }
 
 sub help_synopsis {
     return <<"EOS"
-List all models' retpots
+List all models' available reports
  genome model report list
 
-List reports for reference alignment
- genome model report list --model-sub-type 'reference alignment'
+List reports for reference alignment models
+ genome model report list --type_name 'reference alignment'
+
+List reports for a build
+ genome model report list --build-id 96426120
 
 EOS
 }
 
 sub help_detail {
-    return <<"EOS"
-    Lists all models (or, if specified, a particular sub type) reports.
-EOS
+    return help_brief();
 }
 
 ###################################################
@@ -48,62 +61,230 @@ EOS
 sub execute {
     my $self = shift;
 
-    # Get perl dir we are using - then get the model dir
-    my $module = _class_to_module(__PACKAGE__);
-    my $directory = $INC{$module};
-    $directory =~ s/$module//;
-    my $model_directory = "$directory/Genome/Model";
-
-    # Get a list of models sub types or use the one indicated
-    my @model_sub_classes;
-    if ( $self->model_sub_type ) {
-        # TODO validate sub type?
-        @model_sub_classes = join('', map { ucfirst } split(' ', $self->model_sub_type));
+    my $method;
+    if ( $self->type_name ) {
+        $method = '_list_reports_for_type_name';
+    }
+    elsif ( $self->build_id ) {
+        $method = '_list_reports_for_build';
     }
     else {
-        @model_sub_classes = glob("$model_directory/*pm");
-    }
-
-    # Get reports for models
-    my %reports;
-    for my $model_sub_class ( @model_sub_classes ) {
-        $model_sub_class =~ s#$model_directory/##;
-        $model_sub_class =~ s/\.pm//;
-
-        # skip unless isa model
-        my $model_class = 'Genome::Model::' . $model_sub_class;
-        next unless $model_class->isa('Genome::Model');
-
-        # skip if no reports dir
-        my $report_directory = "$model_directory/$model_sub_class/Report";
-        next unless -d $report_directory;
-
-        # get model sub type
-        my $model_sub_type = _camel_case_to_string($model_sub_class, ' ');
-        
-        for my $report_module ( glob("$report_directory/*pm") ) {
-            $report_module =~ s#$directory/##;
-            my $report_class = _module_to_class($report_module);
-            #print $report_class,"\n";
-            next unless $report_class->isa('Genome::Report::Generator');
-            my ($report_subclass) = $report_class =~ m#::([\w\d]+)$#;
-            my $report_name = _camel_case_to_string($report_subclass);
-            push @{$reports{$model_sub_type}}, $report_name;
-        }
-    }
-
-    unless ( %reports ) {
-        if ( $self->model_sub_type ) { # this is ok
-            printf("No reports found for '%s'\n", $self->model_sub_type);
-            return 1;
-        }
-        else { # not ok
-            $self->error_message("No reports found for any models!");
-            return;
-        }
+        $method = '_list_reports_for_all_type_names';
     }
     
-    return $self->_print_models_and_reports(\%reports)
+    return $self->$method;
+}
+
+sub _list_reports_for_type_name {
+    my $self = shift;
+
+    $self->_print_header_for_type_names
+        or return;
+
+    return $self->_print_reports_for_type_name($self->type_name);
+}
+
+sub _list_reports_for_all_type_names {
+    my $self = shift;
+
+    my @type_names = $self->get_model_type_names
+        or return; # this errors if none found
+
+    $self->_print_header_for_type_names
+        or return;
+    
+    for my $type_name ( @type_names ) {
+        $self->_print_reports_for_type_name($type_name)
+            or return;
+    }
+
+    return 1;
+}
+
+sub _print_header_for_type_names {
+    my $self = shift;
+
+    return print Term::ANSIColor::colored('Reports for Model Type Names', 'bold')."\n";
+}
+
+sub _print_reports_for_type_name {
+    my ($self, $type_name) = @_;
+
+    my @report_names = $self->get_report_names_for_type_name($type_name);
+
+    return print Text::Wrap::wrap(
+        ' ',
+        '  ',
+        Term::ANSIColor::colored($type_name, 'red'), 
+        "\n",
+        ( @report_names ? join("\n", @report_names) : 'none' ),
+    )."\n";
+}
+
+#< Build's Reports >#
+sub _list_reports_for_build {
+    my $self = shift;
+    
+    my $type_name = $self->build->model->type_name;
+    my @availble_report_generators = $self->get_report_names_for_type_name($type_name);
+
+    unless ( @availble_report_generators ) { # ok
+        print "Model type ($type_name) does not have any reports.\n";
+        return 1;
+    }
+    
+    my %reports = map { $_ => [] } @availble_report_generators;
+    for my $report ( $self->build->reports ) {
+        my ($report_subclass) = $report->get_generator =~ m#::([\w\d]+)$#;
+        my @words = $report_subclass =~ /([A-Z](?:[A-Z]*(?=$|[A-Z][a-z])|[a-z]*))/g;
+        my $report_generator = join(' ', map { lc } @words);
+        push @{$reports{$report_generator}}, $report;
+    }
+
+    #print Dumper([keys %reports]); print $self->build->resolve_reports_directory,"\n";
+
+    $self->_print_header_for_build
+        or return;
+
+    for my $report_name ( @availble_report_generators ) {
+        $self->_print_reports($report_name, $reports{$report_name})
+            or return;
+    }
+
+    return 1;
+}
+
+sub _print_header_for_build {
+    my $self = shift;
+
+    return print(
+        Term::ANSIColor::colored(
+            sprintf('Reports for %s Build (<Id> %s)', 
+                join('', map { ucfirst } split(/\s+/, $self->build->model->type_name)),
+                $self->build->id
+            ), 
+            'bold'
+        ),
+        "\n",
+    );
+}
+
+sub _print_reports {
+    my ($self, $report_name, $reports) = @_;
+
+    my @strings = Term::ANSIColor::colored(sprintf('%s (%s)', $report_name, scalar(@$reports)), 'red');
+    
+    if ( @$reports ) {
+        for my $report ( @$reports ) {
+            push @strings, sprintf("%s (%s)", $report->name, $report->get_date);
+        }
+    }
+    else {
+        push @strings, "None";
+    }
+
+    return print Text::Wrap::wrap(' ', '  ', join("\n", @strings))."\n";
+}
+
+#< Public Methods for Getting Models and Reports Subclasses and Such >#
+sub get_inc_directory_for_class {
+    my ($self, $class) = @_;
+
+    my $module = _class_to_module($class);
+    my $directory = $INC{$module};
+    $directory =~ s/$module//;
+
+    return $directory;
+}
+
+sub get_model_subclasses {
+    my $self = shift;
+
+    # return if no models dir
+    my $directory = $self->get_inc_directory_for_class(__PACKAGE__);
+    my $model_directory = "$directory/Genome/Model";
+    unless ( -d $model_directory ) { # not ok
+        confess "Model directory ($model_directory) does not exist!\n";
+    }
+
+    my @model_subclasses;
+    for my $model_module ( glob("$model_directory/*pm") ) {
+        $model_module =~ s#$directory/##;
+        my $model_class = _module_to_class($model_module);
+        #print $model_class,"\n";
+        next unless $model_class->isa('Genome::Model');
+        my ($model_subclass) = $model_class =~ m#::([\w\d]+)$#;
+        push @model_subclasses, $model_subclass;
+    }
+
+    unless ( @model_subclasses ) {
+        $self->error_message("No model type_names found in directory ($model_directory)");
+        return;
+    }
+
+    return @model_subclasses;
+}
+
+sub get_model_type_names {
+    my $self = shift;
+
+    my @subclasses = $self->get_model_subclasses
+        or return;
+
+    return map { _camel_case_to_string($_, ' ') } @subclasses;
+}
+
+sub get_report_classes_for_type_name {
+    my ($self, $type_name) = @_;
+
+    unless ( $type_name ) {
+        # die or err?
+        die "No model sub type given\n"; 
+        return;
+    }
+
+    # return if no reports dir
+    my $directory = $self->get_inc_directory_for_class(__PACKAGE__);
+    my $model_subclass = _string_to_camel_case($type_name);
+    my $report_directory = "$directory/Genome/Model/$model_subclass/Report";
+    #print $report_directory."\n";
+    unless ( -d $report_directory ) {
+        # "No report directory for model type_name ($type_name)"
+        return;
+    }
+
+    my @report_classes;
+    for my $report_module ( glob("$report_directory/*pm") ) {
+        $report_module =~ s#$directory/##;
+        my $report_class = _module_to_class($report_module);
+        next unless $report_class->isa('Genome::Report::Generator');
+        push @report_classes, $report_class;
+    }
+
+    return @report_classes;
+}
+
+sub get_report_subclasses_for_type_name {
+    my ($self, $type_name) = @_;
+
+    my @classes = $self->get_report_classes_for_type_name($type_name)
+        or return;
+
+    return map { $_ =~ m#::([\w\d]+)$# } @classes;
+}
+
+sub get_report_names_for_type_name {
+    my ($self, $type_name) = @_;
+
+    my @subclasses = $self->get_report_subclasses_for_type_name($type_name)
+        or return;
+
+    return map { _camel_case_to_string($_, ' ') } @subclasses;
+}
+
+#< Converter Methods >#
+sub _string_to_camel_case {
+    return join('', map { ucfirst } split(/[\s_]+/, $_[0]));
 }
 
 sub _camel_case_to_string {
@@ -127,24 +308,6 @@ sub _module_to_class {
     $module =~ s#\.pm##;
     $module =~ s#/#::#g;
     return $module;
-}
-
-sub _print_models_and_reports {
-    my ($self, $models_reports) = @_;
-
-    my $string;
-    for my $model ( sort { $a cmp $b } keys %$models_reports ) {
-        $string .= Text::Wrap::wrap(
-            '',
-            '   ',
-            Term::ANSIColor::colored('Reports for', 'bold'),
-            Term::ANSIColor::colored($model, 'red'),
-            "\n",
-            join("\n", @{$models_reports->{$model}}),
-        );
-    }
-
-    return print "$string\n";
 }
 
 1;
