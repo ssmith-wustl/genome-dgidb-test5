47a6d1e259d38b93044cc6972063d9b0017f74cb
ssmith <ssmith@genome.wustl.edu>
2009-04-28 00:22:00 +0000

    Changed column order.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@46299 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Sample/Command/List.pm
+++ Genome/Sample/Command/List.pm
@@ -12,7 +12,7 @@ class Genome::Sample::Command::List {
             is_constant => 1, 
             value => 'Genome::Sample' 
         },
-        show => { default_value => 'id,name,species_name,tissue_label,extraction_label,extraction_type,tissue_desc,extraction_desc,source_name' },
+        show => { default_value => 'id,name,species_name,source_name,tissue_label,tissue_desc,extraction_type,extraction_label,extraction_desc' },
     ],
 };
 
