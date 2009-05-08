1d5c03578db70a1770ce62cb9f5496303b4fd914
ssmith <ssmith@genome.wustl.edu>
2009-05-08 06:25:49 +0000

    removed test script
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@46684 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Vending/try.pl
+++
@@ -1,8 +0,0 @@
-use above 'Vending';
-#print "\n\n*** Get Cookie type!\n";
-#my $c = Vending::Product->get(name => 'Cookie');
-
-print "\n\n*** Get Cookies items\n";
-my @c = Vending::VendItem->get(slot_name => { operator => 'like', value => 'chan%' });
-
-1;
