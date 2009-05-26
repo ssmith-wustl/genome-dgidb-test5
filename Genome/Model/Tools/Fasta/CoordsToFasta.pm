b927054381dd1e76591e6523bef42bb5e89b2d91
rmeyer <rmeyer@genome.wustl.edu>
2009-05-26 16:14:55 +0000

    made modifications for mitochondrial sequence
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@47205 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Fasta/CoordsToFasta.pm
+++ Genome/Model/Tools/Fasta/CoordsToFasta.pm
@@ -22,7 +22,7 @@ class Genome::Model::Tools::Fasta::CoordsToFasta {
 	     },
 	     chromosome => {
 		 type  =>  'String',
-		 doc   =>  "chromosome ie {1,2,...,22,X,Y}",
+		 doc   =>  "chromosome ie {1,2,...,22,X,Y,M}",
 		 is_optional  => 1,
 	     },
 	     start => {
@@ -51,6 +51,12 @@ class Genome::Model::Tools::Fasta::CoordsToFasta {
 		 doc   =>  "use this option if you want softmasked sequence rather than the default softmasked sequence",
 		 is_optional  => 1,
 	     },
+	     reverse_complement => {
+		 type  =>  'Boolean',
+		 doc   =>  "use this option if you reverse completementd sequence rather than the default positively oriented sequence",
+		 is_optional  => 1,
+	     },
+
 	     ],
 	
     
@@ -65,7 +71,7 @@ sub help_brief {
 sub help_synopsis {
     return <<EOS
 
-running...
+running with optional minimum input...
 
 gt fasta coords-to-fasta --list 
  
@@ -80,6 +86,64 @@ EOS
 sub help_detail {
     return <<EOS 
 
+If you would like a fasta file for one genomic range you may find it simplest to get in this fashion
+
+ gt fasta coords-to-fasta --chromosome chr3 --start 103057567 --stop 103057936
+
+ your output will be printed to your screen like this
+
+>chr3:103057567:103057936
+TGGGTGGTTAAGAAGCCCAGAAttttttttttttttgagacagagtctcactgtgtcgcccaggctggaatgcagtggtgcgatcttggctcactgcaacctccgactccctggttcaagcgattctcctgcctcagcctgcccagtagctgggactacaggtgcctaccaccacacccagctaattttttgtatttttagtagagacggggtttcaccatgttagccaggatggtctcgatctcctgacctcgtgatctgcccgcctcggcctcccgaagtgctgggattacaggcgtgagccaccgcgcccggccAAGAAGCCCAGATTTTAACAGATCATTTCATGTGTTTTCTTGATTTGCTTTAA
+
+
+as you can see this is the soft masked sequence by default use the --unmasked option if you want all caps in your sequence
+
+ gt fasta coords-to-fasta --chromosome chr3 --start 103057567 --stop 103057936 --unmasked
+>chr3:103057567:103057936
+TGGGTGGTTAAGAAGCCCAGAATTTTTTTTTTTTTTGAGACAGAGTCTCACTGTGTCGCCCAGGCTGGAATGCAGTGGTGCGATCTTGGCTCACTGCAACCTCCGACTCCCTGGTTCAAGCGATTCTCCTGCCTCAGCCTGCCCAGTAGCTGGGACTACAGGTGCCTACCACCACACCCAGCTAATTTTTTGTATTTTTAGTAGAGACGGGGTTTCACCATGTTAGCCAGGATGGTCTCGATCTCCTGACCTCGTGATCTGCCCGCCTCGGCCTCCCGAAGTGCTGGGATTACAGGCGTGAGCCACCGCGCCCGGCCAAGAAGCCCAGATTTTAACAGATCATTTCATGTGTTTTCTTGATTTGCTTTAA
+
+use the masked option if you want masked sequence. The masked sequence comes from our database and will take longer to obtain than sequence from the other two option which come from an indexed sequence of NCBI Build36
+
+ gt fasta coords-to-fasta --chromosome chr3 --start 103057567 --stop 103057936 --masked
+
+>chr3:103057567:103057936
+TGGGTGGTTAAGAAGCCCAGAANNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNAAGAAGCCCANATTTTAACAGATCATTTCATGTGTTTTCTTGATTTGCTTTAA
+
+if you would like to write a more discriptive refseq header use the --name option
+
+ gt fasta coords-to-fasta --chromosome chr3 --start 103057567 --stop 103057936 --name "chr3:103057567:103057936.refseq.fasta Chr:3, Coords 103057567-103057936, Ori (+), comment"
+
+
+>chr3:103057567:103057936.refseq.fasta Chr:3, Coords 103057567-103057936, Ori (+), comment
+TGGGTGGTTAAGAAGCCCAGAAttttttttttttttgagacagagtctcactgtgtcgcccaggctggaatgcagtggtgcgatcttggctcactgcaacctccgactccctggttcaagcgattctcctgcctcagcctgcccagtagctgggactacaggtgcctaccaccacacccagctaattttttgtatttttagtagagacggggtttcaccatgttagccaggatggtctcgatctcctgacctcgtgatctgcccgcctcggcctcccgaagtgctgggattacaggcgtgagccaccgcgcccggccAAGAAGCCCAGATTTTAACAGATCATTTCATGTGTTTTCTTGATTTGCTTTAA
+
+a refseq header of this sort will work in junction with some of the other tools such as convert-consed-coords
+
+if you would like to write your output to a file use the --out option
+
+ gt fasta coords-to-fasta --chromosome chr3 --start 103057567 --stop 103057936 --name "chr3:103057567:103057936.refseq.fasta Chr:3, Coords 103057567-103057936, Ori (+), comment" --out chr3:103057567:103057936.refseq.fasta
+
+will produce the file chr3:103057567:103057936.refseq.fasta with the same content as the above example
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+If you would like to produce a fasta of fastas you can use the --list option 
+
+    The list should be a (space or tab) delimited file in this order
+          chromosome start stop name
+	  the name is optional in the list. Other options and defaults are the same as above 
+
+	  see this list file for some acceptable list line configurations, and run this as an example 
+
+  gt fasta coords-to-fasta --list /gsc/var/cache/testsuite/data/Genome-Model-Tools-Fasta-CoordsToFasta/CoordsToFasta.test.list
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+If you are retrieving mitochondrial sequence please note that masked sequence is not available
 
 
 EOS
@@ -90,32 +154,49 @@ sub execute {
 
     my $self = shift;
 
-    my $genome = GSC::Sequence::Genome->get(sequence_item_name => 'NCBI-human-build36');
+    my $genome;
+    if ($self->masked) {$genome = GSC::Sequence::Genome->get(sequence_item_name => 'NCBI-human-build36');}
 
     my $out = $self->out;
     if ($out) {open(OUT,">$out");}
     
     my $list = $self->list;
+
     if ($list) {
 	open(LIST,"$list");
 	while (<LIST>) {
 	    chomp;
 	    my $line = $_;
 	    my ($chr,$start,$stop,$name) = split(/[\s]+/,$line);
+	    if ($line =~ /\"([\S\s]+)\"/) {
+		$name = $1;
+	    }
+
+	    unless ($chr && $start && $stop) { $self->error_message("chromosome start and stop was not found in $line. This line will be skipped"); next;}
+	    if (($start =~ /\D/) || ($stop =~ /\D/)) { $self->error_message("chromosome start and stop was not found in $line. This line will be skipped"); next;}
+
+
+	    unless($name) {$name = "$chr\:$start\:$stop";}
 	    $chr =~ s/chr([\S]+)/\U$1/;
 
+	    if ($chr =~ /M/ || $chr =~ /m/) { $chr == "MT"; }
+
 	    my $seq = &get_ref_base($chr,$start,$stop,$self);
+
+	    unless ($seq) {$self->error_message("the sequence was not found from the info $line. This line will be skipped"); next;}
+
 	    $chr =~ s/chr//;
-	    my $chromosome = $genome->get_chromosome($chr);
 
 	    if ($self->masked) {
-		
-		my $masked_seq = $chromosome->mask_snps_and_repeats(begin_position       => $start, 
-								    end_position         => $stop,
-								    sequence_base_string => $seq);
-		$seq = $masked_seq;
-	    } 
-	    
+		unless ($chr eq "MT") {
+		    my $chromosome = $genome->get_chromosome($chr);
+		    my $masked_seq = $chromosome->mask_snps_and_repeats(begin_position       => $start, 
+									end_position         => $stop,
+									sequence_base_string => $seq);
+		    $seq = $masked_seq;
+		} 
+	    }
+	    if ($self->reverse_complement) { my $rev_seq = &reverse_complement_allele($seq); $seq = $rev_seq;}
 	    if ($out) {
 		print OUT qq(\>$name\n$seq\n);
 	    } else {
@@ -124,26 +205,39 @@ sub execute {
 	} close (LIST);
 	close (OUT);
     } else {
+
 	my $chr = $self->chromosome;
-	my $chromosome = $genome->get_chromosome($chr);
-	
-	
 	my $start = $self->start;
-	unless ($start =~ /^[\d]+$/) {$self->error_message("please provide the Build 36 start coordinate"); return 0; }
 	my $stop = $self->stop;
+
+	unless ($chr && $start && $stop) { system qq(gt fasta coords-to-fasta --help); return 0;}
+
+	unless ($start =~ /^[\d]+$/) {$self->error_message("please provide the Build 36 start coordinate"); return 0; }
 	unless ($stop =~ /^[\d]+$/) {$self->error_message("please provide the Build 36 start coordinate"); return 0; }
-	
-	my $seq = &get_ref_base($chromosome,$start,$stop,$self);
-	
+
+	if ($chr =~ /M/ || $chr =~ /m/) { $chr == "MT"; }
+
+	my $seq = &get_ref_base($chr,$start,$stop,$self);
+
+	$chr =~ s/chr//;
+
 	if ($self->masked) {
-	    
-	    my $masked_seq = $chromosome->mask_snps_and_repeats(begin_position       => $start, 
-								end_position         => $stop,
-								sequence_base_string => $seq);
-	    $seq = $masked_seq;
-	} 
+	    unless ($chr eq "MT") {
+		
+#if($chr eq M || MT there isn't any masked mit chr here so step around
+		
+		my $chromosome = $genome->get_chromosome($chr);
+		my $masked_seq = $chromosome->mask_snps_and_repeats(begin_position       => $start, 
+								    end_position         => $stop,
+								    sequence_base_string => $seq);
+		$seq = $masked_seq;
+	    } 
+	}
+	if ($self->reverse_complement) { my $rev_seq = &reverse_complement_allele($seq); $seq = $rev_seq;}
+
 	my $name = $self->name;
 	unless ($name) { $name = "chr$chr:$start:$stop"; }
+
 	if ($out) {
 	    print OUT qq(\>$name\n$seq\n);
 	} else {
@@ -174,9 +268,15 @@ sub get_ref_base {
     my $refdb = Bio::DB::Fasta->new($RefDir);
 
     my ($chr_name,$chr_start,$chr_stop,$self) = @_;
-    my $seq = $refdb->seq($chr_name, $chr_start => $chr_stop);
+    my $chr = $chr_name;
 
+    unless ($chr_name =~ /chr/) {$chr = "chr$chr_name";}
 
+    $chr =~ s/chr([\S]+)/\U$1/;
+
+    my $seq = $refdb->seq($chr, $chr_start => $chr_stop);
+
+    unless ($seq) {return(0);}
 
     if ($self->unmasked) {
 	$seq =~ s/([\S]+)/\U$1/;
