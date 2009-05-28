f2ca4634dd4ef79795b6569058092501ab82134a
jwalker <jwalker@genome.wustl.edu>
2009-05-28 16:08:42 +0000

    genome tool for creating coverage bins based on breadth and depth of coverage from ref-cov output
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@47314 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/RefCov/CoverageBins.pm
@@ -0,0 +1,99 @@
+package Genome::Model::Tools::RefCov::CoverageBins;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::RefCov::CoverageBins {
+    is => ['Command'],
+    has_input => [
+                  stats_file => {
+                                  is => 'Text',
+                                  doc => 'The stats tsv file produed by ref-cov.',
+                              },
+                  output_file => {
+                                  is => 'Text',
+                                  doc => 'The output file path to dump the results',
+                                  is_optional => 1,
+                              },
+              ],
+};
+
+sub execute {
+    my $self = shift;
+
+    my $oldout;
+    if ($self->output_file) {
+        open $oldout, ">&STDOUT"     or die "Can't dup STDOUT: $!";
+        my $output_fh = Genome::Utility::FileSystem->open_file_for_writing($self->output_file);
+        unless ($output_fh) {
+            $self->error_message('Failed to open output file '. $self->output_file .' for writing!');
+            return;
+        }
+        STDOUT->fdopen($output_fh,'w');
+    }
+
+    # Data structures for holding main information.
+    my %ref;
+
+    # Reading in the STATS.tsv file information.
+    my $stats_fh = Genome::Utility::FileSystem->open_file_for_reading($self->stats_file);
+    unless ($stats_fh) {
+        $self->error_message('Failed to open stats file '. $self->stats_file ." for reading:  $!");
+        return;
+    }
+    while (my $line = $stats_fh->getline) {
+        # [0]  Reference Name
+        # [1]  Percent of Reference Bases Covered
+        # [2]  Total Number of Reference Bases
+        # [3]  Total Number of Covered Bases
+        # [4]  Number of Missing Bases
+        # [5]  Average Coverage Depth
+        # [6]  Standard Deviation Average Coverage Depth
+        # [7]  Median Coverage Depth
+        # [8]  Number of Gaps
+        # [9]  Average Gap Length
+        # [10] Standard Deviation Average Gap Length
+        # [11] Median Gap Length
+        # [12] Min. Depth Filter
+        # [13] Discarded Bases (Min. Depth Filter)
+        chomp($line);
+        my @fields = split (/\t/, $line);
+        my $ref             = $fields[0];
+        $ref{$ref}->{cov}   = $fields[1];
+        $ref{$ref}->{size}  = $fields[2];
+        $ref{$ref}->{depth} = $fields[5];
+    }
+    $stats_fh->close;
+
+    # TODO: make this an input param
+    my %bins;
+    foreach my $ref (keys %ref) {
+        my $size = $ref{$ref}->{size};
+        my $cov    = $ref{$ref}->{cov};
+        if (($size >= 100) && ($size <= 2_999) && ($cov >= 90)) {
+            $bins{SMALL}++;
+        }
+        elsif (($size >= 3_000) && ($size <= 6_999) && ($cov >= 50)) {
+            $bins{MEDIUM}++;
+        }
+        elsif (($size >= 7_000) && ($cov >= 30)) {
+            $bins{LARGE}++;
+        }
+    }
+    my %desc = (
+                SMALL => 'SMALL(>=100,<=2999,1X,90%)',
+                MEDIUM => 'MEDIUM(>=3000,<=6999,1X,50%)',
+                LARGE => 'LARGE(>=7000,1X,30%)',
+            );
+    for my $key ('SMALL', 'MEDIUM', 'LARGE') {
+        print $desc{$key} .":\t". $bins{$key} ."\n";
+    }
+    if ($oldout) {
+        open STDOUT, ">&", $oldout or die "Can't dup \$oldout: $!";
+    }
+    return 1;
+}
+
+__END__
