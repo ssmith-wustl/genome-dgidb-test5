3208b09b6d8b757d96ea65e672b1998aa01f07ea
charris <charris@genome.wustl.edu>
2009-06-08 18:45:13 +0000

    model search string is untested. model id works
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@47697 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Somatic/AddVariants.pm
@@ -0,0 +1,124 @@
+package Genome::Model::Tools::Somatic::AddVariants;
+
+use strict;
+use warnings;
+use Genome;
+use Genome::Utility::HugoGene::HugoGeneMethods;
+use Carp;
+
+class Genome::Model::Tools::Somatic::AddVariants{
+    is => 'Command',
+    has => [
+    input_file => {
+        is  => 'String',
+        doc => 'The input file of variants to be stored',
+    },
+    model_search_string => {
+        is_optional=>1,
+        doc => 'The string to try to resolve a model id from',
+    },
+    model_id => {
+        is_optional=>1,
+        doc =>"The model you want to add variants to",
+    },
+    build_id => {
+        is_optional=>1,
+        doc=>"the build id to add variants to supply only one of the above",
+    }
+
+
+    ],
+};
+
+sub help_brief {
+    "runs ucsc annotation on some variants",
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+    genome-model tools somatic ucsc-annotator...    
+EOS
+}
+
+sub help_detail {                           
+    return <<EOS 
+    runs ucsc annotation on some variants
+EOS
+}
+
+sub execute {
+    my $self = shift;
+    my $build_id = $self->resolve_build_id;
+    unless($build_id) {
+        $self->error_message("Unable to resolve build id!");
+        return;
+    }
+    my $ifh=IO::File->new($self->input_file);
+
+    my $annotator = Genome::Model::Tools::Annotate::TranscriptVariantsStream->create(annotation_filter=>'top');
+
+    while (my $line=$ifh->getline) {
+        #assume  annotation input;
+        chomp $line;
+        my ($chr,
+        $start_pos,
+        $stop_pos,
+        $ref,
+        $var) = split /\t/, $line;
+
+        $DB::single=1;
+        my @transcripts = $annotator->annotate($chr,$start_pos,$stop_pos,$ref,$var);
+        my $gene_name = $transcripts[0]->{gene_name};
+        Genome::VariantOccurrence->create(
+            build_id=> $build_id,
+            chromosome=> $chr,
+            start_pos => $start_pos,
+            stop_pos => $stop_pos,
+            reference_allele=>$ref,
+            variant_allele=>$var,
+            gene=> $gene_name,
+            type => 'S',);
+        }
+
+    }
+
+    sub resolve_build_id {
+        my $self=shift;
+        if($self->build_id) { 
+            if(my $build = Genome::Model::Build->get($self->build_id)) {
+                return $self->build_id;
+            } else {return;}
+        }elsif($self->model_id) {
+            if(my $model=Genome::Model->get($self->model_id)) {
+                if($model->last_complete_build_id) {
+                    return $model->last_complete_build_id;
+                }
+                else{ return;}
+            }
+        }elsif($self->model_search_string) {
+            my @models = Genome::Model->get(operator=>'like', value=>$self->model_search_string);
+            if(scalar(@models) > 1) {
+                $self->error_message("Search String inadequate: multiple models found");
+                my @model_names = map { $_->name } @models;
+                $self->error_message( join "\n", @model_names);
+                return;
+            }
+            if(my $model=Genome::Model->get($models[0]->id)) {
+                if($model->last_complete_build_id) {
+                    return $model->last_complete_build_id;
+                }
+                else {
+                    $self->error_message("couldn't resolve model from earch string"); 
+                    return;
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+
