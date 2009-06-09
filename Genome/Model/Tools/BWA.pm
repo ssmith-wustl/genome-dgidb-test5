b78b381a4f7c360948a0faecc9f9a7751400cdf8
boberkfe <boberkfe@genome.wustl.edu>
2009-06-09 21:54:38 +0000

    New tools module for running alignments with the BWA aligner
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@47755 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/BWA.pm
@@ -0,0 +1,69 @@
+package Genome::Model::Tools::BWA;
+
+use strict;
+use warnings;
+
+use Genome;
+use File::Basename;
+
+class Genome::Model::Tools::BWA {
+    is => 'Command',
+    has => [
+        use_version => { is => 'Version', is_optional => 1, default_value => '0.4.9', doc => "Version of bwa to use" },
+        arch_os => {
+                    calculate => q|
+                            my $arch_os = `uname -m`;
+                            chomp($arch_os);
+                            return $arch_os;
+                        |
+                },
+    ],
+};
+
+sub sub_command_sort_position { 12 }
+
+sub help_brief {
+    "Tools to run BWA or work with its output files.",
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+genome-model tools bwa ...    
+EOS
+}
+
+sub help_detail {                           
+    return <<EOS 
+More information about the BWA suite of tools can be found at http://bwa.sourceforege.net.
+EOS
+}
+
+sub bwa_path {
+    my $self = $_[0];
+    return $self->path_for_bwa_version($self->use_version);
+}
+my %BWA_VERSIONS = (
+		    '0.4.2' => '/gsc/pkg/bio/bwa/bwa-0.4.2-64/bwa',
+		    '0.4.9' => '/gsc/pkg/bio/bwa/bwa-0.4.9-64/bwa',
+                    'bwa'   => 'bwa',
+                );
+
+sub available_bwa_versions {
+    my $self = shift;
+    return keys %BWA_VERSIONS;
+}
+
+sub path_for_bwa_version {
+    my $class = shift;
+    my $version = shift;
+
+    if (defined $BWA_VERSIONS{$version}) {
+        return $BWA_VERSIONS{$version};
+    }
+    die('No path for bwa version '. $version);
+}
+
+
+1;
+
