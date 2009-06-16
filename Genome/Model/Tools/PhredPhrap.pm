9d1aaaa3a4d1c18c4d4d737ab745f87acdbd7108
ebelter <ebelter@genome.wustl.edu>
2009-06-16 14:50:41 +0000

    added options trim_qual and vector_bound; moved attrs to has_optional, removed is_optional from attrs
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@47907 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/PhredPhrap.pm
+++ Genome/Model/Tools/PhredPhrap.pm
@@ -8,54 +8,56 @@ use above 'Genome';
 class Genome::Model::Tools::PhredPhrap {
     is => 'Command',
     is_abstract => 1,
-    has => [
+    has_optional => [
     version  => {
         type => 'String',
-        is_optional => 1,
         default => __PACKAGE__->default_version,#=> $versions[0],
         doc => "Version to use: " . join(', ', __PACKAGE__->versions)
     },
     forcelevel  => {
         type => 'int non_neg',
-        is_optional => 1,
         default => 1,
-        doc => 'Relaxes stringency to varying degree during final contig merge pass.  Allowed values are integers from 0 (most stringent, to 10 (least stringent)',
+        doc => 'Relaxes stringency to varying degree during final contig merge pass.  Allowed values are integers from 0 (most stringent, to 10 (least stringent). (1)',
     },
     minmatch  => {
         type => 'int non_neg',
-        is_optional => 1,
         default => 17,
-        doc => 'Minimum length of matching word to nucleate SWAT comparison. if minmatch = 0, a full (non-banded, comparison is done [N.B. NOT PERMITTED IN CURRENT VERSION]. Increasing -minmatch can dramatically decrease the time required for the pairwise sequence comparisons; in phrap, it also tends to have the effect of increasing assembly stringency. However it may cause some significant matches to be missed, and it may increase the risk of incorrect joins in phrap in certain situations (by causing implied overlaps between reads with high-quality discrepancies to be missed).',
+        doc => 'Minimum length of matching word to nucleate SWAT comparison. if minmatch = 0, a full (non-banded, comparison is done [N.B. NOT PERMITTED IN CURRENT VERSION]. Increasing -minmatch can dramatically decrease the time required for the pairwise sequence comparisons; in phrap, it also tends to have the effect of increasing assembly stringency. However it may cause some significant matches to be missed, and it may increase the risk of incorrect joins in phrap in certain situations (by causing implied overlaps between reads with high-quality discrepancies to be missed). (17)',
     },
     minscore  => {
         type => 'int non_neg',
-        is_optional => 1,
         default => 30,
-        doc => 'Minimum alignment score.',
+        doc => 'Minimum alignment score. (30)',
     },
     revise_greedy  => {
         type => 'Boolean',
-        is_optional => 1,
         default => 0,
         doc => 'Splits initial greedy assembly into pieces at "weak joins", and then tries to reattach them to give higher overall score.  Use of this option should correct some types of missassembly.',
     },
     shatter_greedy  => {
         type => 'Boolean',
-        is_optional => 1,
         default => 0,
         doc => 'Breaks assembly at weak joins (as with revise-greedy, but does not try to reattach pieces.',
     },
     view  => {
         type => 'Boolean',
-        is_optional => 1,
-        default => 1,
-        doc => 'Create ".view" file suitable for input to phrapview.',
+        default => 0,
+        doc => 'Create ".view" file suitable for input to phrapview. (0)',
     },
     new_ace  => {
-       type => 'Boolean',
-       is_optional => 1,
-       default => 1,
-       doc => 'Create ".ace" file for viewing in consed. Default is to create an acefile.',
+        type => 'Boolean',
+        default => 1,
+        doc => 'Create ".ace" file for viewing in consed. Default is to create an acefile. (1)',
+    },
+    trim_qual => {
+        type => 'Integer',
+        default => 13,
+        doc => 'Quality value used in to define the "high-quality" part of a read, (the part which should overlap; this is used to adjust qualities at ends of reads. (13)'
+    },
+    vector_bound => {
+        type => 'Integer',
+        default => 80,
+        doc => 'Number of potential vector bases at beginning of each read.  Matches that lie entirely within this region are assumed to represent vector matches and are ignored. (80)',
     },
     ],
 };
