5dc28a09519e4e4d1b63f86e9374e1869ed7d4f0
jlolofie <jlolofie@genome.wustl.edu>
2009-06-18 19:10:45 +0000

    compares snp output file from somatic sniper against snps from maq
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48018 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Somatic/SnpFilter.pm
@@ -0,0 +1,101 @@
+package Genome::Model::Tools::Somatic::SnpFilter;
+
+use warnings;
+use strict;
+
+use Genome;
+use Workflow;
+use Carp;
+use FileHandle;
+use Data::Dumper;
+use List::Util qw( max );
+
+class Genome::Model::Tools::Somatic::SnpFilter {
+
+    is  => ['Command'],
+    has => [
+        tumor_model => {
+            is          => 'Genome::Model',
+            id_by       => 'tumor_model_id',
+            is_optional => 1,
+        },
+        tumor_model_id => { 
+            is_input    => 1, 
+            is          => 'integer', 
+            is_optional => 0 
+        },
+        tumor_snp_file => {
+            calculate_from => 'tumor_model',
+            calculate      => q{
+                my $build = $tumor_model->last_complete_build || die 'no completed build';
+                return $build->filtered_snp_file();
+            },
+            is_optional => 1,
+            is_input    => '1',
+            doc         => 'The snp filter output file from maq.',
+        },
+        sniper_snp_file => {
+            is       => 'String',
+            is_input => '1',
+            doc      => 'The snp output file from somatic sniper.',
+        },
+        output_file => {
+            is        => 'String',
+            is_input  => '1',
+            is_output => '1',
+            doc       => 'The somatic sniper output file.',
+        },
+    ],
+};
+
+sub help_brief {
+    return "Gets intersection of SNPs from somatic sniper and maq";
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+    gmt somatic snpfilter --sniper_snp_file=[pathname] --output_file=[pathname]
+EOS
+}
+
+sub help_detail {                           
+    return <<EOS 
+    Calls `gmt snp intersect` on the tumor snp file from the last maq build and the snp file from somatic sniper.
+    (Outputs lines from the somatic sniper file.)
+EOS
+}
+
+sub execute {
+
+    my ($self) = @_;
+
+    my $tumor_snp_file = $self->tumor_snp_file();
+    if ( ! Genome::Utility::FileSystem->validate_file_for_reading($tumor_snp_file) ) {
+        die 'cant read from: ' . $tumor_snp_file;
+    }
+
+    my $sniper_snp_file = $self->sniper_snp_file();
+    if ( ! Genome::Utility::FileSystem->validate_file_for_reading($sniper_snp_file) ) {
+        die 'cant read from: ' . $sniper_snp_file;
+    }
+
+    my $output_file = $self->output_file();
+    if ( ! Genome::Utility::FileSystem->validate_file_for_writing($output_file) ) {
+        die 'cant write to: ' . $sniper_snp_file;
+    }
+
+
+    # passing sniper snp file in first makes it the default output
+    my $cmd = join(' ', 'gmt snp intersect', $sniper_snp_file, $tumor_snp_file);
+    my $result = Genome::Utility::FileSystem->shellcmd(
+        cmd          => $cmd,
+        input_files  => [ $sniper_snp_file, $tumor_snp_file ],
+        output_files => [ $output_file ],
+        skip_if_output_is_present => 0
+    );
+
+    return $result;
+}
+
+1;
