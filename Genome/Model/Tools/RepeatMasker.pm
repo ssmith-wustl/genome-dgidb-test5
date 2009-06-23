69956dec8942bca9c1f5219ef87bf0b844f147cd
jwalker <jwalker@genome.wustl.edu>
2009-06-23 20:36:24 +0000

    now that we have a parallel RepeatMasker, merge the results and create a summary stats table
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48191 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/RepeatMasker.pm
+++ Genome/Model/Tools/RepeatMasker.pm
@@ -18,6 +18,23 @@ class Genome::Model::Tools::RepeatMasker {
             is => 'Text',
             doc => 'The output directory where files are written',
         },
+        sensitivity => {
+            is => 'Text',
+            default_value => '',
+            doc => 'the sensitivity level to use which effects speed',
+            valid_values => ['-s','-q','-qq',''],
+        },
+        species => {
+            is => 'Text',
+            doc => 'the species specific library to use',
+            default_value => 'human',
+        },
+        mask => {
+            is => 'Text',
+            doc => 'the type of masking to perform on the masked file, RepeatMasker default is Ns',
+            default_value => '-n',
+            valid_values => ['-small','-xsmall','-x','-n'],
+        },
     ],
     has => [
             output_directory => {
