9b91501bfae645025317564fcddd3873a7818f8d
kkyung <kkyung@genome.wustl.edu>
2009-06-23 20:02:16 +0000

    Code release for velvet pipeline
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48177 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/DeNovoAssembly.pm
@@ -0,0 +1,29 @@
+package Genome::Model::DeNovoAssembly;
+
+use strict;
+use warnings;
+
+use Genome;
+use Genome::ProcessingProfile::DeNovoAssembly;
+
+
+class Genome::Model::DeNovoAssembly {
+    is => 'Genome::Model',
+    has => [
+	    map({
+		$_ => {
+		    via => 'processing_profile',
+		}
+	    } Genome::ProcessingProfile::DeNovoAssembly->params_for_class
+	 ),
+      ],
+};
+
+sub build_subclass_name {
+    return 'de-novo-assembly';
+}
+
+1;
+
+#$HeadURL$
+#$Id$
