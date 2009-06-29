5aa4c7b3e991024baca1e0db32098d5122626db0
ebelter <ebelter@genome.wustl.edu>
2009-06-29 19:09:31 +0000

    updates to pipeline
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48362 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/DeNovoAssembly/PrepareInstrumentData/Velvet.pm
+++ Genome/Model/Command/Build/DeNovoAssembly/PrepareInstrumentData/Velvet.pm
@@ -34,45 +34,70 @@ sub _prepare_solexa_data {
 
     my @instrument_data = $self->model->instrument_data;
     unless (@instrument_data) {
-	$self->error_message("");
-	return;
+        $self->error_message(sprintf('No instrument data assigned to model (<Name> %s <Id> %s)', $self->model_name, $self->model_id));
+        return
     }
-
     if (@instrument_data > 1) {
-	$self->error_message("");
-	return;
+        $self->error_message(sprintf('Muliple instrument data was assigned to model (<Name> %s <Id> %s).  This is not yet supported for velvet de novo assemblies.', $self->model_name, $self->model_id));
+        return;
     }
 
     my @fastqs = $instrument_data[0]->fastq_filenames;
-
-    if (@fastqs > 2) {
-	$self->error_message("");
-	return;
+    unless (@fastqs) {
+        $self->error_message(sprintf('No fastqs found for model', $self->model_name, $self->model_id));
+        return;
+    }
+    unless (@fastqs == 2) {
+        $self->error_message(sprintf('Velvet de novo assemblies currently require exactly two fastq files, but found %s', $self->model_name, $self->model_id, scalar @fastqs));
+        return;
     }
 
-    my $fq1 = Bio::SeqIO::fastq->new(-file => $fastqs[0]);
-    my $fq2 = Bio::SeqIO::fastq->new(-file => $fastqs[1]);
+    my $fq1;
+    eval { 
+        $fq1 = Bio::SeqIO::fastq->new(-file => $fastqs[0]);
+    };
+    unless ( $fq1 ) {
+        $self->error_message("Can't open fastq input $fastqs[0]: $!");
+        return;
+    }
+    my $fq2;
+    eval {
+        $fq2 = Bio::SeqIO::fastq->new(-file => $fastqs[1]);
+    };
+    unless ( $fq2 ) {
+        $self->error_message("Can't open fastq input $fastqs[1]: $!");
+        return;
+    }
     my $fq_out = Bio::SeqIO::fastq->new(-file => '>'.$self->build->velvet_fastq_file);
-
-    my $limit = $self->get_read_limit_count();
-    my $cnt = 0;
-
-    while ( my $seq1 = $fq1->next_seq ) {
-	last if ++$cnt > $limit;
-	my $seq2 = $fq2->next_seq;
-	unless ( $seq2 ) {
-	    $self->error_message("Odd number of fastqs in files: ".join(', ', @fastqs));
-	    return;
-	}
-	my $pcap_id = $self->_convert_to_pcap_id($seq1->id);
-	$seq1->id($pcap_id);
-	$fq_out->write_fastq($seq1);
-
-	$pcap_id = $self->_convert_to_pcap_id($seq2->id);
-	$seq2->id($pcap_id);
-	$fq_out->write_fastq($seq2);
+    unless ( $fq_out ) {
+        $self->error_message("Can't open fastq output: ".$self->build->velvet_fastq_file.": $!");
+        return;
     }
 
+    my $limit = $self->get_read_limit_count;
+    my $cnt = 0;
+    while ( my $seq1 = $fq1->next_seq ) {
+        last if ++$cnt > $limit;
+        my $seq2 = $fq2->next_seq;
+        unless ( $seq2 ) {
+            $self->error_message("Odd number of fastqs in files: ".join(', ', @fastqs));
+            # TODO unlink fastq output file?
+            return;
+        }
+        my $pcap_id = $self->_convert_to_pcap_id($seq1->id);
+        $seq1->id($pcap_id);
+        $fq_out->write_fastq($seq1);
+
+        $pcap_id = $self->_convert_to_pcap_id($seq2->id);
+        $seq2->id($pcap_id);
+        $fq_out->write_fastq($seq2);
+    }
+
+    unless ( -s $self->build->velvet_fastq_file ) {
+        $self->error_message("Did not write any fastqs (unknown reason)");
+        return;
+    }
+    
     return 1;
 }
 
@@ -85,16 +110,26 @@ sub _convert_to_pcap_id {
 
 sub get_read_limit_count {
     my $self = shift;
-    my $string = $self->model->processing_profile->{_prepare_instrument_data_params};
-    my ($limit) = $string =~ /cutoff\s+(\d+)/;
-    return $limit;
+
+    my $prepare_instrument_data_params = $self->model->processing_profile->get_prepare_instrument_data_params;
+    unless ( $prepare_instrument_data_params ) {
+        $self->error_message("Problem getting prepare instrument data params");
+        return;
+    }
+
+    unless ( exists $prepare_instrument_data_params->{reads_cutoff} ) {
+        $self->error_message("No reads cutoff found in prepare instrument data params");
+        return;
+    }
+
+    return $prepare_instrument_data_params->{reads_cutoff};
 }
 
 sub valid_params {
     return {
-	reads_cutoff => {
-	    is => 'Number',
-	},
+        reads_cutoff => {
+            is => 'Number',
+        },
     };
 }
 
