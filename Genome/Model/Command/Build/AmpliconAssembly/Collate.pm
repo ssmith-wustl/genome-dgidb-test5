85e0a29fec1ee8423eb32852b89dedb9793c39c2
ebelter <ebelter@genome.wustl.edu>
2009-06-30 17:21:15 +0000

    now using the G:M:T packages
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48415 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/Collate.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/Collate.pm
@@ -5,8 +5,7 @@ use warnings;
 
 use Genome;
 
-use Bio::SeqIO;
-use Data::Dumper 'Dumper';
+require Genome::Model::Tools::AmpliconAssembly::Collate;
 
 class Genome::Model::Command::Build::AmpliconAssembly::Collate {
     is => 'Genome::Model::Event',
@@ -15,84 +14,16 @@ class Genome::Model::Command::Build::AmpliconAssembly::Collate {
 sub execute {
     my $self = shift;
 
-    my $amplicons = $self->build->get_amplicons
+    my $collate = Genome::Model::Tools::AmpliconAssembly::Collate->create(
+        directory => $self->build->data_directory,
+        sequencing_center => $self->model->sequencing_center,
+    )
         or return;
-    
-    my @amplicon_fasta_types = $self->build->amplicon_fasta_types;
-
-    $self->_open_fasta_and_qual_writers(@amplicon_fasta_types)
+    $collate->execute
         or return;
-
-    for my $amplicon ( @$amplicons ) {
-        for my $type ( @amplicon_fasta_types ) {
-            $self->_collate_amplicon_fasta_and_qual($amplicon, $type);
-        }
-    }
-
-    #print $self->build->data_directory,"\n"; <STDIN>;
 
     return 1;
 }
-
-#< FHs >#
-sub _key_for_fasta_writer {
-    return '_'.$_[0].'_fasta_writer';
-}
-
-sub _key_for_qual_writer {
-    return '_'.$_[0].'_qual_writer';
-}
-
-sub _open_fasta_and_qual_writers {
-    my ($self, @types) = @_;
-
-    for my $type ( @types ) {
-        my $fasta_file = $self->build->fasta_file_for_type($type);
-        unlink $fasta_file if -e $fasta_file;
-        $self->{ _key_for_fasta_writer($type) } = Bio::SeqIO->new(
-            '-file' => '>'.$fasta_file,
-            '-format' => 'fasta',
-        )
-            or return;
-
-        my $qual_file = $self->build->qual_file_for_type($type);
-        unlink $qual_file if -e $qual_file;
-        $self->{ _key_for_qual_writer($type) } = Bio::SeqIO->new(
-            '-file' => '>'.$qual_file,
-            '-format' => 'qual',
-        )
-            or return;
-    }
-
-    return 1;
-}
-
-#< Collating the Amplicon Fastas >#
-sub _collate_amplicon_fasta_and_qual {
-    my ($self, $amplicon, $type) = @_;
-
-    my $method = $self->build->amplicon_bioseq_method_for_type($type);
-    unless ( $method ) {
-        $self->error_message("Can't determine method for getting bioseqs for type ($type)");
-        return;
-    }
-    
-    my @bioseqs = $amplicon->$method
-        or return;
-
-    for my $bioseq ( @bioseqs ) {
-        $self->{ _key_for_fasta_writer($type) }->write_seq($bioseq);
-        $self->{ _key_for_qual_writer($type) }->write_seq($bioseq);
-    }
-
-    return 1;
-}
-
-sub _verify_bioseq {
-    
-}
-
-1;
 
 #$HeadURL$
 #$Id$
