eb0d959e46f51ba39ce166331810e54958ba5692
ebelter <ebelter@genome.wustl.edu>
2009-06-30 17:21:25 +0000

    now using the G:M:T packages
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48416 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/Orient.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/Orient.pm
@@ -12,73 +12,9 @@ class Genome::Model::Command::Build::AmpliconAssembly::Orient {
 sub execute {
     my $self = shift;
 
-    my $amplicons = $self->build->get_amplicons
-        or return;
-
-    for my $amplicon ( @$amplicons ) {
-        my $bioseq = $amplicon->get_bioseq;
-        next unless $bioseq; # ok - not all will have a bioseq
-
-        my $classification = $amplicon->get_classification;
-        unless ( $classification ) {
-            $self->error_message(
-                sprintf(
-                    'Can\'t get classification for amplicon (<Amplicon %s> <Build Id %s>)', 
-                    $amplicon->get_name,
-                    $self->build->id,
-                )
-            );
-            next;
-        }
-
-        $amplicon->confirm_orientation( $classification->is_complemented )
-            or return;
-    }
-    
-    #print $self->build->data_directory."\n"; <STDIN>;
-    
-    return 1;
-}
-
-sub _execute {
-    my $self = shift;
-
-    unless ( -s $self->build->assembly_fasta ) {
-        $self->error_message(
-            sprintf(
-                "The assembly fasta file for model's (<id> %s <name> %s) build (<ID> %s) does not exist.  Please collate it first.",
-                $self->model->id,
-                $self->model->name,
-                $self->build->id,
-            )
-        );
-        return;
-    }
-
-    my %primer_fastas;
-    for my $type (qw/ sense anti_sense /) {
-        my $method = sprintf('%s_primer_fasta', $type);
-        my $fasta = $self->model->processing_profile->$method;
-        next unless -s $fasta;
-        $primer_fastas{ sprintf('%s_fasta_file', $type) } = $fasta;
-    }
-
-    unless ( %primer_fastas ) { # No primers fastas exist
-        $self->error_message( 
-            sprintf(
-                'No primer fasta files found for model\'s (<id> %s <name> %s) processing profile (<id> %s <name> %s)',
-                $self->model->processing_profile->id, 
-                $self->model->processing_profile->id, 
-                $self->model->id, 
-                $self->model->name
-            ) 
-        );
-        return;
-    }
-
-    my $orient = Genome::Model::Tools::Fasta::Orient->create(
-        fasta_file => $self->build->assembly_fasta,
-        %primer_fastas,
+    my $orient = Genome::Model::Tools::AmpliconAssembly::Orient->create(
+        directory => $self->build->data_directory,
+        sequencing_center => $self->model->sequencing_center,
     )
         or return;
     $orient->execute
