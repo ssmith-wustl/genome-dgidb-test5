c230a1c0a233879cb7f06eaa9a9edc490d0de68c
ebelter <ebelter@genome.wustl.edu>
2009-06-30 17:21:04 +0000

    now using the G:M:T packages
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48414 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/Assemble.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/Assemble.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Genome;
 
-require Genome::Model::Tools::PhredPhrap::ScfFile;
+require Genome::Model::Tools::AmpliconAssembly::Assemble;
 
 class Genome::Model::Command::Build::AmpliconAssembly::Assemble{
     is => 'Genome::Model::Event',
@@ -14,39 +14,13 @@ class Genome::Model::Command::Build::AmpliconAssembly::Assemble{
 sub execute {
     my $self = shift;
 
-    my $amplicons = $self->build->get_amplicons
-        or return;
-
-    for my $amplicon ( @$amplicons ) {
-        $self->_assemble_amplicon($amplicon)
-            or return;
-    }
-
-    #print $self->build->data_directory,"\n"; <STDIN>;
-
-    return 1;
-}
-
-sub _assemble_amplicon {
-    my ($self, $amplicon) = @_;
-
-    # Create SCF file
-    my $scf_file = $amplicon->create_scfs_file;
-    unless ( $scf_file ) {
-        $self->error_message("Error creating SCF file ($scf_file)");
-        return;
-    }
-
-    # Create and run the Command
-    my $command = Genome::Model::Tools::PhredPhrap::ScfFile->create(
+    my $assemble = Genome::Model::Tools::AmpliconAssembly::Assemble->create(
         directory => $self->build->data_directory,
-        assembly_name => $amplicon->get_name,
-        scf_file => $scf_file,
-    );
-    #eval{ # if this fatals, we still want to go on
-    $command->execute;
-    #};
-    #TODO write file for failed assemblies
+        sequencing_center => $self->model->sequencing_center,
+    )
+        or return;
+    $assemble->execute
+        or return;
 
     return 1;
 }
