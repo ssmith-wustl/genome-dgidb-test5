bace7b26472c47147e56da3c5f0b8e5d92852af6
ebelter <ebelter@genome.wustl.edu>
2009-07-01 14:42:37 +0000

    updated for new xml reports
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48434 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Report.pm
+++ Genome/Model/Command/Report.pm
@@ -1,4 +1,3 @@
-
 package Genome::Model::Command::Report;
 
 use strict;
@@ -6,58 +5,125 @@ use warnings;
 
 use Genome;
 
+require Carp;
+
 class Genome::Model::Command::Report {
-    is => 'Genome::Model::Command',
+    is => 'Command',
     has => [ 
-           model => { is => 'Genome::Model', id_by => 'model_id'},
-           #generate_report_brief => { is_abstract => 1 },
-           #generate_report_detail => { is_abstract => 1},
-       ],
+    # report
+    report_name => { 
+        is => 'Text', 
+        doc => 'Report name',
+    },
+    report => {
+        is => 'Genome::Report',
+        calculate_from => [qw/ report_name build /],
+        calculate => q|
+            return $build->get_report($report_name);
+        |,
+        doc => 'The report from the build with report name',
+    },
+    # build and derivatives
+    model => { 
+        is => 'Genome::Model', 
+        via => 'build',
+        to => 'model',
+    },
+    build => { 
+        is => 'Genome::Model::Build', 
+        id_by => 'build_id',
+        doc => 'Build',
+    },
+    build_id => {
+        doc => 'Build id',
+    },
+    ],
 };
 
+#< Helps >#
 sub help_brief {
-    "generate reports for a given model"
+    return "Operate on a build's reports"
 }
 
 sub sub_command_sort_position { 12 }
 
-sub generate_report_brief {
-    die "Implement me in the subclass, owangutang";
-}
+#< Command >#
+sub create {
+    my $class = shift;
 
-sub generate_report_detail {
-    die "Implement generate_report_detail in the subclass you're writing, or this will 
-     continue to fail";
- }
+    my $self = $class->SUPER::create(@_)
+        or return;
 
-
-
-sub resolve_reports_directory {
-    my $self = shift;
-    my $reports_dir = $self->model->resolve_reports_directory;
-    unless(-d $reports_dir) {
-        unless(mkdir $reports_dir) {
-            $self->error_message("Directory $reports_dir doesn't exist, can't create");
-            return;
-        }
-        chmod 02775, $reports_dir;
+    unless ( $self->report_name ) {
+        $self->error_message("Report name is required to create");
+        $self->delete;
+        return;
     }
-   return $reports_dir;
+    
+    $self->_validate_build_id
+        or return;
+
+    return $self;
 }
 
+#< Report >#
+sub report {
+    my $self = shift;
 
-##these can be moved down to subclass or subclasses can supply a type argument to support
-##files other than HTML...don't freak out, you danglers
-sub report_brief_output_filename {
-    my $self=shift;
-    return $self->resolve_reports_directory . "/brief.html";
+    unless ( $self->{_report} ) { 
+        $self->{_report} = $self->build->get_report($self->report_name);
+    }
+
+    return $self->{_report};
 }
 
-sub report_detail_output_filename {
-    my $self=shift;
-    return $self->resolve_reports_directory . "/detail.html";
+#< Validations >#
+sub _validate_build_id {
+    my $self = shift;
+
+    unless ( $self->build_id ) {
+        $self->error_message("No build id given");
+        return;
+    }
+    
+    unless ( $self->build ) {
+        $self->error_message("Can't get build for id: ". $self->build_id);
+        return;
+    }
+
+    return 1;
 }
 
+sub _validate_report_name_for_build {
+    my ($self, $build, $report_name) = @_;
+
+    unless ( $build ) {
+        $self->error_message("No build given to validate report name");
+        return;
+    }
+
+    unless ( $report_name ) {
+        $self->error_message("No report name given to validate report name");
+        return;
+    }
+    
+    my $type_name = $build->model->type_name;
+    my @report_names = Genome::Model::Report::get_report_names_for_type_name($type_name);
+
+    unless ( @report_names ) {
+        $self->error_message('No report names for model type name: '.$build->model->type_name);
+        return;
+    }
+
+    unless ( grep { $report_name eq $_ } @report_names ) {
+        $self->error_message("Report name ($report_name) was not found for model type name ($type_name): ". join(', ', @report_names));
+        return;
+    }
+    
+    return 1;
+}
 
 1;
 
+#$HeadURL$
+#$Id$
