d49554bf13a8a5a28431a4955444166c6aae8303
ebelter <ebelter@genome.wustl.edu>
2009-07-01 14:45:32 +0000

    moved some methods to G:M:Report
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48438 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Report/List.pm
+++ Genome/Model/Command/Report/List.pm
@@ -9,10 +9,10 @@ use Carp 'confess';
 use Data::Dumper 'Dumper';
 require Term::ANSIColor;
 require Text::Wrap;
+require Genome::Model::Report;
 
 class Genome::Model::Command::Report::List {
     is => 'Command',
-    #is => 'Genome::Model::Command::Report',
     has_optional => [
         type_name => {
             is => 'Text', 
@@ -35,7 +35,7 @@ class Genome::Model::Command::Report::List {
 ###################################################
 
 sub help_brief {
-    return 'Lists reports for builds or model types';
+    return 'Lists reports for model type names';
 }
 
 sub help_synopsis {
@@ -87,7 +87,7 @@ sub _list_reports_for_type_name {
 sub _list_reports_for_all_type_names {
     my $self = shift;
 
-    my @type_names = $self->get_model_type_names
+    my @type_names = Genome::Model::Command::get_model_type_names()
         or return; # this errors if none found
 
     $self->_print_header_for_type_names
@@ -110,7 +110,9 @@ sub _print_header_for_type_names {
 sub _print_reports_for_type_name {
     my ($self, $type_name) = @_;
 
-    my @report_names = $self->get_report_names_for_type_name($type_name);
+    my @report_names = Genome::Model::Report::get_report_names_for_type_name(
+        $type_name
+    );
 
     return print Text::Wrap::wrap(
         ' ',
@@ -126,7 +128,7 @@ sub _list_reports_for_build {
     my $self = shift;
     
     my $type_name = $self->build->model->type_name;
-    my @availble_report_generators = $self->get_report_names_for_type_name($type_name);
+    my @availble_report_generators = Genome::Model::Report::get_report_names_for_type_name($type_name);
 
     unless ( @availble_report_generators ) { # ok
         print "Model type ($type_name) does not have any reports.\n";
@@ -136,8 +138,7 @@ sub _list_reports_for_build {
     my %reports = map { $_ => [] } @availble_report_generators;
     for my $report ( $self->build->reports ) {
         my ($report_subclass) = $report->get_generator =~ m#::([\w\d]+)$#;
-        my @words = $report_subclass =~ /([A-Z](?:[A-Z]*(?=$|[A-Z][a-z])|[a-z]*))/g;
-        my $report_generator = join(' ', map { lc } @words);
+        my $report_generator = Genome::Utility::Text::camel_case_to_string($report_subclass);
         push @{$reports{$report_generator}}, $report;
     }
 
@@ -184,130 +185,6 @@ sub _print_reports {
     }
 
     return print Text::Wrap::wrap(' ', '  ', join("\n", @strings))."\n";
-}
-
-#< Public Methods for Getting Models and Reports Subclasses and Such >#
-sub get_inc_directory_for_class {
-    my ($self, $class) = @_;
-
-    my $module = _class_to_module($class);
-    my $directory = $INC{$module};
-    $directory =~ s/$module//;
-
-    return $directory;
-}
-
-sub get_model_subclasses {
-    my $self = shift;
-
-    # return if no models dir
-    my $directory = $self->get_inc_directory_for_class(__PACKAGE__);
-    my $model_directory = "$directory/Genome/Model";
-    unless ( -d $model_directory ) { # not ok
-        confess "Model directory ($model_directory) does not exist!\n";
-    }
-
-    my @model_subclasses;
-    for my $model_module ( glob("$model_directory/*pm") ) {
-        $model_module =~ s#$directory/##;
-        my $model_class = _module_to_class($model_module);
-        #print $model_class,"\n";
-        next unless $model_class->isa('Genome::Model');
-        my ($model_subclass) = $model_class =~ m#::([\w\d]+)$#;
-        push @model_subclasses, $model_subclass;
-    }
-
-    unless ( @model_subclasses ) {
-        $self->error_message("No model type_names found in directory ($model_directory)");
-        return;
-    }
-
-    return @model_subclasses;
-}
-
-sub get_model_type_names {
-    my $self = shift;
-
-    my @subclasses = $self->get_model_subclasses
-        or return;
-
-    return map { _camel_case_to_string($_, ' ') } @subclasses;
-}
-
-sub get_report_classes_for_type_name {
-    my ($self, $type_name) = @_;
-
-    unless ( $type_name ) {
-        # die or err?
-        die "No model sub type given\n"; 
-        return;
-    }
-
-    # return if no reports dir
-    my $directory = $self->get_inc_directory_for_class(__PACKAGE__);
-    my $model_subclass = _string_to_camel_case($type_name);
-    my $report_directory = "$directory/Genome/Model/$model_subclass/Report";
-    #print $report_directory."\n";
-    unless ( -d $report_directory ) {
-        # "No report directory for model type_name ($type_name)"
-        return;
-    }
-
-    my @report_classes;
-    for my $report_module ( glob("$report_directory/*pm") ) {
-        $report_module =~ s#$directory/##;
-        my $report_class = _module_to_class($report_module);
-        next unless $report_class->isa('Genome::Report::Generator');
-        push @report_classes, $report_class;
-    }
-
-    return @report_classes;
-}
-
-sub get_report_subclasses_for_type_name {
-    my ($self, $type_name) = @_;
-
-    my @classes = $self->get_report_classes_for_type_name($type_name)
-        or return;
-
-    return map { $_ =~ m#::([\w\d]+)$# } @classes;
-}
-
-sub get_report_names_for_type_name {
-    my ($self, $type_name) = @_;
-
-    my @subclasses = $self->get_report_subclasses_for_type_name($type_name)
-        or return;
-
-    return map { _camel_case_to_string($_, ' ') } @subclasses;
-}
-
-#< Converter Methods >#
-sub _string_to_camel_case {
-    return join('', map { ucfirst } split(/[\s_]+/, $_[0]));
-}
-
-sub _camel_case_to_string {
-    my $camel_case = shift;
-    my $join = ( @_ )
-    ? $_[0]
-    : ' '; 
-    my @words = $camel_case =~ /([A-Z](?:[A-Z]*(?=$|[A-Z][a-z])|[a-z]*))/g;
-    return join($join, map { lc } @words);
-}
-
-sub _class_to_module {
-    my $module = shift;
-    $module =~ s/::/\//g;
-    $module .= '.pm';
-    return $module;
-}
-
-sub _module_to_class {
-    my $module = shift;
-    $module =~ s#\.pm##;
-    $module =~ s#/#::#g;
-    return $module;
 }
 
 1;
