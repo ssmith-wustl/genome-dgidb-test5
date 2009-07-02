60c9ed64ff82e2f117d6591e960bdaa94e12caba
jpeck <jpeck@genome.wustl.edu>
2009-07-02 21:15:54 +0000

    A perl interface to the Samtools Java API.  Allows a user to add or edit Sam file header information.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48521 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Sam/SamHeaderEditor.pm
@@ -0,0 +1,143 @@
+package Genome::Model::Tools::Sam::SamHeaderEditor;
+
+use strict;
+use warnings;
+
+use Genome;
+use File::Copy;
+use File::Basename;
+
+class Genome::Model::Tools::Sam::SamHeaderEditor {
+    is  => 'Genome::Model::Tools::Sam',
+    has => [ 
+        input_sam_file    => { 
+            is  => 'String',      
+            doc => 'name of sam input file',
+        },
+        output_sam_file    => { 
+            is  => 'String',      
+            doc => 'name of output same file',
+        },
+    ],
+    has_optional => [
+        bam_version_field => {
+            is => 'String',
+            doc => 'bam version used to generate the file',
+        },
+        sort_order_field => {
+            is => 'String',
+            doc => 'sort order of the bam file',
+        },
+        assembly_field => {
+            is => 'String',
+            doc => 'the assembly used to generate the bam file e.g. HG18',
+        },
+        species_field => {
+            is => 'String',
+            doc => 'species',
+        },
+        refseq_path_field => {
+            is => 'String',
+            doc => 'the path to the reference sequence',
+        },
+        sample_name_field => {
+            is => 'String',
+            doc => 'the sample name',
+        },
+        platform_field => {
+            is => 'String',
+            doc => 'sequencing platform e.g. illumina',
+        },
+        platform_unit_field => {
+            is => 'String',
+            doc => 'a run identifier',
+        },
+        library_field => {
+            is => 'String',
+            doc => 'the library',
+        },
+        date_run_field => {
+            is => 'String',
+            doc => 'a run identifier',
+        },
+        genome_center_field => {
+            is => 'String',
+            doc => 'the genome center identifier e.g. gc-wustl',
+        },
+        aligner_command_field => {
+            is => 'String',
+            doc => 'the aligner command',
+        },
+        aligner_version_field => {
+            is => 'String',
+            doc => 'the aligner version',
+        },
+
+    ],
+};
+
+
+sub help_brief {
+    "Edits the given sam file header to contain the provided name value pairs.";
+}
+
+
+sub help_detail {
+    return <<EOS 
+Edits the given sam file header to contain the provided name value pairs.
+EOS
+}
+
+
+sub create {
+    my $class = shift;
+    my $self = $class->SUPER::create(@_);
+
+    #find the location of the required jar files based on path of the class            
+    my $base_dir = $class->base_dir; 
+    my $cp = $base_dir."/"."sam-1.0.jar".":".$base_dir."/"."GCSam.jar";
+            
+    #eval the 'use inline' command to inject the correct classpath
+    #note:  the "PACKAGE=>'main'" entry allows you to replace "Genome::Model::Tools::Sam::SamHeaderEditor" with "main" when subsequently calling the Java class.  See in execute. 
+    my $inline_string =  "use Inline ( Java => 'STUDY', CLASSPATH => qw($cp), STUDY => ['edu.wustl.genome.samtools.SamHeaderEditor'], AUTOSTUDY => 1, PACKAGE => 'main', );";
+    eval "$inline_string";
+    unless ( defined $@ ) {
+        $self->error_message("There was a problem evaluating the 'use Inline' code for the required Java libraries: $inline_string.  The classpath was set to: $cp");
+        die;
+    } 
+        
+    #test to see if the input sam file exists. 
+    $self->error_message('Sam input file does not exist.') and return unless -s $self->input_sam_file;
+
+    return $self;
+}
+
+sub execute {
+
+   my $self = shift;
+ 
+   #see note in create() for explanation of 'main' package definition.
+   my $she = new main::edu::wustl::genome::samtools::SamHeaderEditor();
+
+   #The order of the following parameters is important.  Do not change unless you change the corresponding Java class!
+   my $result = $she->addAttributes(  $self->input_sam_file, 
+                         $self->output_sam_file, 
+                         $self->bam_version_field, 
+                         $self->sort_order_field, 
+                         $self->assembly_field, 
+                         $self->species_field,
+                         $self->refseq_path_field, 
+                         $self->sample_name_field, 
+                         $self->platform_field, 
+                         $self->platform_unit_field, 
+                         $self->library_field, 
+                         $self->date_run_field,
+                         $self->genome_center_field, 
+                         $self->aligner_command_field,
+                         $self->aligner_version_field
+                      );
+    
+    return $result;
+}
+
+1;
