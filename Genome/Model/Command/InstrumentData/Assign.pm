d8eedf2ec3397e431b895d9aecd86f68b150702d
ebelter <ebelter@genome.wustl.edu>
2009-07-08 16:13:26 +0000

    added support for multiple instr data
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48661 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/InstrumentData/Assign.pm
+++ Genome/Model/Command/InstrumentData/Assign.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use Genome;
 
+use Data::Dumper 'Dumper';
+
 class Genome::Model::Command::InstrumentData::Assign {
     is => 'Genome::Model::Event',
     has => [
@@ -16,7 +18,11 @@ class Genome::Model::Command::InstrumentData::Assign {
     has_optional => [
         instrument_data_id => {
             is => 'Number',
-            doc => 'The unique ID of the instrument data to assign.'
+            doc => 'The unique ID of the instrument data to assign.  To assign multiple instrument data, enclose this param in quotes(\'), and separate the IDs by a space.'
+        },
+        instrument_data_ids => {
+            is => 'Number',
+            doc => 'The unique IDs of the instrument data to assign, enclosed in quotes(\'\'), and separated by a space.'
         },
         all => {
             is => 'Boolean',
@@ -38,70 +44,44 @@ sub help_detail {
 
 #########################################################
 
+sub create { 
+    my ($class, %params) = @_;
+
+    my $self = $class->SUPER::create(%params)
+        or return;
+
+    my @requested_actions = grep { 
+        $self->$_ 
+    } (qw/ instrument_data_id instrument_data_ids all /);
+    if ( @requested_actions > 1 ) {
+        $self->error_message('Multiple actions requested: '.join(', ', @requested_actions));
+        $self->delete;
+        return;
+    }
+    
+    $self->_verify_model
+        or  return;
+
+    return $self;
+}
+
 sub execute {
     my $self = shift;
 
-    $self->_verify_model
-        or return;
-    
-    # We got an id, assign this one only
-    if ( defined $self->instrument_data_id ) {
-        my $instrument_data = Genome::InstrumentData->get( $self->instrument_data_id );
-        unless ( $instrument_data ) {
-            $self->error_message( sprintf('Failed to find specified instrument data for id (%s)', $self->instrument_data_id) );
-            return;
-        }
-        return $self->_assign_instrument_data($instrument_data);
-    } 
-
-    # Assign all unassigned if requested 
-    if ( $self->all ) {
-        $self->status_message("Attempting to assign all available instrument data");
-        my @unassigned_instrument_data = $self->model->unassigned_instrument_data;
-        unless ( @unassigned_instrument_data ){
-            $self->error_message("Attempted to assign all instrument data that was unassigned for model, but found none");
-            return;
-        }
-        for my $id ( @unassigned_instrument_data ) { 
-            $self->_assign_instrument_data($id)
-                or return;
-        }
-
-        return 1;
+    if ( $self->instrument_data_id ) { # assign this
+        return $self->_assign_by_instrument_data_id;
+    }
+    elsif ( $self->instrument_data_ids ) { # assign these
+        return $self->_assign_by_instrument_data_ids;
+    }
+    elsif ( $self->all ) { # assign all
+        return $self->_assign_all_instrument_data;
     }
 
-    if ( 1 ) { #$self->list_available ) {
-        # List available
-        my @compatible_instrument_data = $self->model->compatible_instrument_data;
-        my @assigned_instrument_data = $self->model->assigned_instrument_data;
-        my @unassigned_instrument_data = $self->model->unassigned_instrument_data;
-
-        $self->status_message(
-            sprintf(
-                'Model (<name> %s <subject_name> %s): %s assigned and %s unassigned of %s compatible instrument data',
-                $self->model->name,
-                $self->model->subject_name,
-                scalar @assigned_instrument_data,
-                scalar @unassigned_instrument_data,
-                scalar @compatible_instrument_data
-            )
-        );
-
-        if (@unassigned_instrument_data) {
-            my $lister = Genome::Model::Command::InstrumentData::List->create(
-                unassigned=>1,
-                model_id => $self->model->id
-            );
-
-            return $lister->execute;
-        }
-
-        return 1;
-    }
-
-    return 1;
+    return $self->_list_compatible_instrument_data; # list compatable
 }
 
+#< Assign Instrument Data >#
 sub _assign_instrument_data {
     my ($self, $instrument_data) = @_;
 
@@ -151,6 +131,109 @@ sub _assign_instrument_data {
             $self->model->name,
         )
     );
+
+    return 1;
+}
+
+sub _get_instrument_data_for_id {
+    my ($self, $id) = @_;
+
+    my $instrument_data = Genome::InstrumentData->get($id);
+    unless ( $instrument_data ) {
+        $self->error_message( 
+            sprintf('Failed to find specified instrument data for id (%s)', $id) 
+        );
+        return;
+    }
+
+    return $instrument_data;
+}
+
+#< Methods Run Based on Inputs >#
+sub _assign_by_instrument_data_id {
+    my $self = shift;
+
+    # Get it 
+    my $instrument_data = $self->_get_instrument_data_for_id( $self->instrument_data_id );
+
+    # Assign it
+    return $self->_assign_instrument_data($instrument_data)
+}
+
+sub _assign_by_instrument_data_ids {
+    my $self = shift;
+
+    # Get the ids
+    my @ids = split(/\s+/, $self->instrument_data_ids);
+    unless ( @ids ) {
+        $self->error_message("No instrument data ids found in instrument_data_id input: ".$self->instrument_data_id);
+        return;
+    }
+
+    # Get the instrument data
+    my @instrument_data;
+    for my $id ( @ids ) {
+        unless ( push @instrument_data, Genome::InstrumentData->get($id) ) {
+            $self->error_message( 
+                sprintf('Failed to find specified instrument data for id (%s)', $id) 
+            );
+            return;
+        }
+    }
+
+    # Assign 'em
+    for my $instrument_data ( @instrument_data ) {
+        $self->_assign_instrument_data($instrument_data)
+            or return;
+    }
+
+    return 1;
+}
+
+sub _assign_all_instrument_data {
+    my $self = shift;
+
+    # Assign all unassigned if requested 
+    $self->status_message("Attempting to assign all available instrument data");
+    my @unassigned_instrument_data = $self->model->unassigned_instrument_data;
+    unless ( @unassigned_instrument_data ){
+        $self->error_message("Attempted to assign all instrument data that was unassigned for model, but found none");
+        return;
+    }
+    for my $id ( @unassigned_instrument_data ) { 
+        $self->_assign_instrument_data($id)
+            or return;
+    }
+
+    return 1;
+}
+
+sub _list_compatible_instrument_data {
+    my $self = shift;
+
+    my @compatible_instrument_data = $self->model->compatible_instrument_data;
+    my @assigned_instrument_data = $self->model->assigned_instrument_data;
+    my @unassigned_instrument_data = $self->model->unassigned_instrument_data;
+
+    $self->status_message(
+        sprintf(
+            'Model (<name> %s <subject_name> %s): %s assigned and %s unassigned of %s compatible instrument data',
+            $self->model->name,
+            $self->model->subject_name,
+            scalar @assigned_instrument_data,
+            scalar @unassigned_instrument_data,
+            scalar @compatible_instrument_data
+        )
+    );
+
+    if (@unassigned_instrument_data) {
+        my $lister = Genome::Model::Command::InstrumentData::List->create(
+            unassigned=>1,
+            model_id => $self->model->id
+        );
+
+        return $lister->execute;
+    }
 
     return 1;
 }
