fa794628bcac6372db8ddb0e073d3eb4efd7d037
josborne <josborne@genome.wustl.edu>
2009-07-14 13:36:59 +0000

    fixed has-a relation ships...
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48857 127c0c0e-971a-0410-ad48-868c1a942d8d

--- BAP/DB/Tag.pm
+++ BAP/DB/Tag.pm
@@ -6,7 +6,7 @@ use DBD::Oracle qw(:ora_types);
 __PACKAGE__->table('tag');
 __PACKAGE__->columns( All => qw/tag_id tag_name tag_value/ );
 
-__PACKAGE__->has_a('tag_name' => BAP::DB::TagNames);
-__PACKAGE__->has_a('tag_value' => BAP::DB::TagValues);
+__PACKAGE__->has_a('tag_name' => BAP::DB::TagName);
+__PACKAGE__->has_a('tag_value' => BAP::DB::TagValue);
 
 1;
