7746a4386266d66b1289e1773ad9fd69f47aeca4
boberkfe <boberkfe@genome.wustl.edu>
2009-07-15 22:45:37 +0000

    assume that bwa wants 4 CPUs until i get around to making it smarter and autodetect from the processing profile.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48949 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Bwa.pm
+++ Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Bwa.pm
@@ -41,7 +41,7 @@ EOS
 }
 
 sub bsub_rusage {
-    return "-R 'select[model!=Opteron250 && type==LINUX64] span[hosts=1] rusage[mem=12000]' -M 1610612736";
+    return "-R 'select[model!=Opteron250 && type==LINUX64] span[hosts=1] rusage[mem=12000]' -M 12000000 -n 4";
 }
 
 
