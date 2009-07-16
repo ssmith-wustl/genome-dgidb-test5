d42c6ee3ef58a66c31fb56f8e2bcfd07a8b2a926
gsanders <gsanders@genome.wustl.edu>
2009-07-16 21:21:29 +0000

    Processing profile for somatic models
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@48989 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/Somatic.pm
@@ -0,0 +1,14 @@
+
+package Genome::ProcessingProfile::Somatic;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::ProcessingProfile::Somatic{
+    is => 'Genome::ProcessingProfile',
+};
+
+1;
+
