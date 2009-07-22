6b6ffaa07d7cc5a47bf51535af9af64053dd913b
adukes <adukes@genome.wustl.edu>
2009-07-22 22:47:12 +0000

    Genome/TranscriptSubStructure.pm - data directory instead of build, length sub; Genome/Model/ImportedAnnotation.pm - added species name property; Genome/Model/Build/ImportedAnnotation.pm - added species name property, transcript-iterator uses annotation_data_directory instead of build to create iterator; Genome/Model/Tools/Annotate/TranscriptVariantsStream.pm, Genome/Model/Tools/Annotate/TransAndNovel.pm - add version argument to Genome::Transcript::variant annotator create(); Genome/Model/Tools/Annotate/TranscriptVariants.pm -- add version argument to Genome::Transcript::variant annotator create(), set default annotation to human combined-annotation v0; Genome/Model/Tools/ImportAnnotation/Ensembl.pm, Genome/Model/Tools/ImportAnnotation/Genbank.pm - major refactoring, imports mouse and human; Genome/Model/Tools/ImportAnnotation.pm - parent class, creates introns and flank sub structures, assigns ordinality and phase to sub structures; Genome/Model/Command/Build/ImportedAnnotation/Run.pm - updated logic when building a combined-annotation model; Genome/ExternalGeneId.pm, Genome/Transcript.pm, Genome/Variation.pm, Genome/Gene.pm, Genome/Protein.pm  - has data directory instead of build; Genome/DataSource/TranscriptSubStructures.pm, Genome/DataSource/Transcripts.pm, Genome/DataSource/ExternalGeneIds.pm, Genome/DataSource/Genes.pm, Genome/DataSource/Proteins.pm - use data_directory instead of buiild in file-resolver, use comma as delimiter; Genome/Transcript.t - remove testing for expression data, grab iterator from build; Genome/Transcript/VariantAnnotator.pm - version arg is passed in, since transcript-iterators now have data_directories instead of builds; GSC/ImportExport/Ensembl.pm - use ensembl objects to get chrom number, rather than rely on stuff in the genbank branch of import export; GSC/ImportExport/GenBank/CDS.pm - search in another possible place in hash for cds data; GSC/ImportExport/GenBank/Gene.pm - add support for mouse; GSC/ImportExport/Ensembl/Gene.pm - removed an unused expected argument causing method failure;
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@49196 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/TranscriptSubStructure.pm
+++ Genome/TranscriptSubStructure.pm
@@ -9,26 +9,75 @@ class Genome::TranscriptSubStructure {
     type_name => 'genome transcript sub structure',
     table_name => 'TRANSCRIPT_SUB_STRUCTURE',
     id_by => [
-        transcript_structure_id => { is => 'NUMBER', len => 10 },
+        transcript_structure_id => { 
+            is => 'NUMBER', 
+            len => 10 
+        },
     ],
     has => [
-        transcript_id => { is => 'NUMBER', len => 10 },
-        structure_type => { is => 'VARCHAR', len => 10, is_optional => 1 },
-        structure_start => { is => 'NUMBER', len => 10, is_optional => 1 },
-        structure_stop => { is => 'NUMBER', len => 10, is_optional => 1 },
-        ordinal => { is => 'NUMBER', len => 10, is_optional => 1},
-        phase => { is => 'NUMBER', len => 7, is_optional => 1},
-        nucleotide_seq => { is => 'CLOB', is_optional => 1},
-
-        transcript => { is => 'Genome::Transcript', id_by => 'transcript_id' },
-        build => {
-                    is => "Genome::Model::Build",
-                    id_by => 'build_id',
-                    },
- 
+        transcript_id => {  #TODO changed to text extended length
+            is => 'Text', 
+            len => 35 
+        },
+        structure_type => { 
+            is => 'VARCHAR',    
+            len => 10, 
+            is_optional => 1 
+        },
+        structure_start => { 
+            is => 'NUMBER', 
+            len => 10, 
+            is_optional => 1 
+        },
+        structure_stop => { 
+            is => 'NUMBER', 
+            len => 10, 
+            is_optional => 1 
+        },
+        ordinal => { 
+            is => 'NUMBER', 
+            len => 10, 
+            is_optional => 1
+        },
+        phase => { 
+            is => 'NUMBER', 
+            len => 7, 
+            is_optional => 1
+        },
+        nucleotide_seq => { 
+            is => 'CLOB', 
+            is_optional => 1
+        },
+        transcript => { #TODO, straighten out ID stuff w/ Tony
+            is => 'Genome::Transcript', 
+            id_by => 'transcript_id' 
+        },
+        data_directory => {
+            is => "Path",
+        },
     ],
+    schema_name => 'files',
     data_source => 'Genome::DataSource::TranscriptSubStructures',
 };
 
+sub rename_to_errors_later{  #TODO not sure what the new valid sub name is
+    my $self = shift;
+    return if grep  { ! defined $self->$_ } qw/structure_type structure_start structure_stop ordinal/;
+    return unless $self->structure_stop >= $self->structure_start;
+    unless ($self->structure_type =~ /intron/){
+        return unless defined $self->nucleotide_seq;
+    }
+    return 1;
+}
+
+sub length{
+    my $self = shift;
+    my $length = $self->structure_stop - $self->structure_start + 1;
+    return $length;
+}
 
 1;
+
+#TODO
+=pod
+=cut
