d3b3aab526d6c454abc1c214f7570a6ccc642400
fdu <fdu@genome.wustl.edu>
2009-07-23 21:57:49 +0000

    for samtools varFilter
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@49237 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Sam/VarFilter.pm
@@ -0,0 +1,159 @@
+package Genome::Model::Tools::Sam::VarFilter;
+
+use strict;
+use warnings;
+
+use Genome;
+use IO::File;
+
+use File::Temp;
+use File::Basename;
+
+
+my %options = (
+    min_map_qual_snp => {
+        is  => 'Integer',
+        doc => '-Q minimum RMS mapping quality for SNPs, default 25',
+        default => 25,
+    },
+    min_map_qual_gap => {
+        is  => 'Integer',
+        doc => '-q minimum RMS mapping quality for gaps, default 10',
+        default => 10,
+    },
+    min_read_depth => {
+        is  => 'Integer',
+        doc => '-d minimum read depth, default 3',
+        default => 3,
+    },
+    max_read_depth => {
+        is  => 'Integer',
+        doc => '-D maximum read depth, default 100',
+        default => 100,
+    },
+    snp_win_size => {
+        is  => 'Integer',
+        doc => '-W window size for filtering dense SNPs, default 10',
+        default => 10,
+    },
+    gap_win_size => {
+        is  => 'Integer',
+        doc => '-l window size for filtering adjacent gaps, default 30',
+        default => 30,
+    },
+    gap_nearby_size => {
+        is  => 'Integer',
+        doc => '-w SNP within INT bp around a gap to be filtered, default 10',
+        default => 10,
+    },
+    max_snp_per_win => {
+        is  => 'Integer',
+        doc => '-N maximum number of SNPs in a sized window',
+        default => 2,
+    },
+    min_indel_score => {
+        is  => 'Integer',
+        doc => '-G minimum indel score for nearby SNP filtering, default is 25',
+        default => 25,
+    },
+    filtered_snp_out_file => {
+        is  => 'String',
+        doc => 'snp output file after filter',
+        default => 'snp.varfilter',
+    },
+    filtered_indel_out_file => {
+        is  => 'String',
+        doc => 'snp output file after filter',
+        default => 'indel.varfilter',
+    },
+);
+
+
+class Genome::Model::Tools::Sam::VarFilter {
+    is  => 'Genome::Model::Tools::Sam',
+    has => [
+        bam_file => {
+            is  => 'String',
+            doc => 'The input bam file',
+        },
+        ref_seq_file => {
+            is  => 'String',
+            doc => 'reference sequence file used for samtools pileup',
+        },
+    ],
+    has_optional => [%options],
+};
+
+
+sub help_brief {
+    'Filter samtools-pileup snp indel output. This uses samtools pileup and samtools varFilter pipe together';
+}
+
+sub help_detail {
+    return <<EOS
+    Filter samtools-pileup snp indel output.
+EOS
+}
+
+
+sub execute {
+    my $self = shift;
+    my $bam_file = $self->bam_file;
+    my $ref_seq  = $self->ref_seq_file;
+    my $sam_path = $self->samtools_path;
+    
+    unless (-s $bam_file and -s $ref_seq) {
+        $self->error_message("Can not find valid bam file: $bam_file or valid ref seq: $ref_seq");
+        return;
+    }
+    
+    my ($tmp_fh, $tmp_out) = File::Temp::tempfile(
+        'varFilterXXXXXX', 
+        UNLINK => 1,
+        TMPDIR => 1,
+    );
+    
+    my $pileup_cmd = sprintf('%s pileup -f %s -q 1 -c %s', $sam_path, $ref_seq, $bam_file);
+    
+    my $filter_cmd = $self->samtools_pl_path . ' varFilter';
+
+    for my $option (keys %options) {
+        next if $option =~ /^filtered/;
+        if (defined $self->$option) {
+            my ($opt) = $options{$option}->{doc} =~ /^(\-\S)\s/;
+            $filter_cmd .= " $opt " . $self->$option;
+        }
+    }
+
+    $filter_cmd .= " - > $tmp_out";
+
+    my $cmd = $pileup_cmd . ' | '. $filter_cmd;
+    
+    my $rv = Genome::Utility::FileSystem->shellcmd(
+        cmd => $cmd,
+        output_files => [$tmp_out],
+        skip_if_output_is_present => 0,
+    );
+        
+    my $snp_out_fh   = Genome::Utility::FileSystem->open_file_for_writing($self->filtered_snp_out_file) or return;
+    my $indel_out_fh = Genome::Utility::FileSystem->open_file_for_writing($self->filtered_indel_out_file) or return;
+
+    while (my $line = $tmp_fh->getline) {
+        my ($id) = $line =~ /^\S+\s+\S+\s+(\S+)\s+/;
+        if ($id eq '*') {
+            $indel_out_fh->print($line);
+        }
+        else {
+            $snp_out_fh->print($line);
+        }
+    }
+    
+    $tmp_fh->close;
+    $snp_out_fh->close;
+    $indel_out_fh->close;
+    
+    return 1;
+}
+    
+
+1;
---
+++ Genome/Model/Tools/Sam/VarFilter.t
@@ -0,0 +1,52 @@
+#!/gsc/bin/perl
+
+use strict;
+use warnings;
+
+use above "Genome";
+
+use Test::More;
+use File::Temp;
+use File::Compare;
+
+BEGIN {
+    if (`uname -a` =~ /x86_64/){
+        plan tests => 5;
+    }
+    else{
+        plan skip_all => 'Must run on a 64 bit machine';
+    }
+
+    use_ok('Genome::Model::Tools::Sam::VarFilter');
+}
+
+my $root_dir = '/gsc/var/cache/testsuite/data/Genome-Model-Tools-Sam/VarFilter';
+
+my $tmp_dir  = File::Temp::tempdir(
+    "VarFilter_XXXXXX", 
+    DIR     => $root_dir,
+    CLEANUP => 1,
+);
+
+my $bam_file  = "$root_dir/test.bam";
+my $ref_seq   = '/gscmnt/839/info/medseq/reference_sequences/NCBI-human-build36/all_sequences.fasta';
+my $ori_snp   = "$root_dir/test.bam.snp.varfilter";
+my $ori_indel = "$root_dir/test.bam.indel.varfilter";
+my $snp_out   = "$tmp_dir/snp.varfilter";
+my $indel_out = "$tmp_dir/indel.varfilter";
+
+my $filter = Genome::Model::Tools::Sam::VarFilter->create(
+    bam_file     => $bam_file,                                                      
+    ref_seq_file => $ref_seq,
+    filtered_snp_out_file   => $snp_out,
+    filtered_indel_out_file => $indel_out,
+);
+
+isa_ok($filter,'Genome::Model::Tools::Sam::VarFilter');
+ok($filter->execute,'executed ok');
+
+is(compare($ori_snp, $snp_out), 0, 'filtered SNP output is generated as expected');
+is(compare($ori_indel, $indel_out), 0, 'filtered indel output is generated as expected');
+
+exit;
+
