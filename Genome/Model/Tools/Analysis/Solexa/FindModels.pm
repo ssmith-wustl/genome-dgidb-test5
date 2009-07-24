9b8ae867851a835b3c2f914cd766569763821afd
dkoboldt <dkoboldt@genome.wustl.edu>
2009-07-24 16:52:24 +0000

    Search tool for genome models
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@49250 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Analysis/Solexa/FindModels.pm
@@ -0,0 +1,96 @@
+
+package Genome::Model::Tools::Analysis::Solexa::FindModels;     # rename this when you give the module file a different name <--
+
+#####################################################################################################################################
+# FindModels - Search for specific genome models and check on their status
+#					
+#	AUTHOR:		Dan Koboldt (dkoboldt@watson.wustl.edu)
+#
+#	CREATED:	07/24/2009 by D.K.
+#	MODIFIED:	07/24/2009 by D.K.
+#
+#	NOTES:	
+#			
+#####################################################################################################################################
+
+use strict;
+use warnings;
+
+use FileHandle;
+
+use Genome;                                 # using the namespace authorizes Class::Autouse to lazy-load modules under it
+
+class Genome::Model::Tools::Analysis::Solexa::FindModels {
+	is => 'Command',                       
+	
+	has => [                                # specify the command's single-value properties (parameters) <--- 
+		sample_list	=> { is => 'Text', doc => "Accept a list of sample names as input", is_optional => 1 },
+		sample_name	=> { is => 'Text', doc => "Search by sample name", is_optional => 1 },
+		print_location	=> { is => 'Text', doc => "If set to 1, prints data location" , is_optional => 1},
+	],
+};
+
+sub sub_command_sort_position { 12 }
+
+sub help_brief {                            # keep this to just a few words <---
+    "Searches for existing genome models and checks their status"                 
+}
+
+sub help_synopsis {
+    return <<EOS
+This command searches for Illumina/Solexa data using the database
+EXAMPLE:	gt analysis solexa search-runs --flowcell_id 302RT
+EOS
+}
+
+sub help_detail {                           # this is what the user will see with the longer version of help. <---
+    return <<EOS 
+
+EOS
+}
+
+
+################################################################################################
+# Execute - the main program logic
+#
+################################################################################################
+
+sub execute {                               # replace with real execution logic.
+	my $self = shift;
+
+	## Get required parameters ##
+	my $sample_name = $self->sample_name;
+	my $sample_list = $self->sample_list;
+
+	my $print_location;
+	$print_location = $self->print_location if($self->print_location);
+
+	my $model_list = "";
+
+	if($sample_name)
+	{
+		$model_list = `genome model list --filter=subject_name=$sample_name`;
+		chomp($model_list);
+	}
+
+	my @lines = split(/\n/, $model_list);
+	
+	foreach my $line (@lines)
+	{
+		print "$line\n";
+	}
+
+	
+	return 1;                               # exits 0 for true, exits 1 for false (retval/exit code mapping is overridable)
+}
+
+sub commify
+{
+	local($_) = shift;
+	1 while s/^(-?\d+)(\d{3})/$1,$2/;
+	return $_;
+}
+
+
+1;
+
