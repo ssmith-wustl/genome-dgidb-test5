d8ee04956dd35acfe3a59aab80e7023b406ac298
ebelter <ebelter@genome.wustl.edu>
2009-07-30 16:18:07 +0000

    now saving datasets as csv
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@49405 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/Reports.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/Reports.pm
@@ -66,6 +66,47 @@ sub _generate_and_save_report {
         );
     }
 
+    my @dataset_names = $report->get_dataset_names;
+    unless ( @dataset_names ) { # not ok
+        $self->error_message(
+            sprintf(
+                'No datasets in %s report (MODEL <Name:%s Id:%s> BUILD <Id:%s>)', 
+                $type,
+                $self->model->name,
+                $self->model->id,
+                $self->build->id,
+            )
+        );
+        return;
+    }
+    my $file_base = sprintf(
+        '%s/%s',
+        $self->build->reports_directory,
+        $report->name_to_subdirectory($report->name),
+    );
+    for my $dataset_name ( @dataset_names ) {
+        my $file = $file_base."/$dataset_name.csv";
+        unlink $file if -e $file;
+        my $fh = Genome::Utility::FileSystem->open_file_for_writing($file)
+            or return; # not ok
+        my ($svs) = $report->get_datasets_by_name_as_separated_value_string($dataset_name, ',');
+        unless ( $svs ) { # not ok
+            $self->error_message(
+                sprintf(
+                    'Could not get dataset (%s) for %s report (MODEL <Name:%s Id:%s> BUILD <Id:%s>)', 
+                    $dataset_name,
+                    $type,
+                    $self->model->name,
+                    $self->model->id,
+                    $self->build->id,
+                )
+            );
+            return;
+        }
+        $fh->print($svs);
+        $fh->close;
+    }
+
     return $report;
 }
 
