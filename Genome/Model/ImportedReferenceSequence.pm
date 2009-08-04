472294964b96cc88cd3267372216bb23f31a80a9
adukes <adukes@genome.wustl.edu>
2009-08-04 21:27:46 +0000

    added species_name attribute via subject, and species subroutine that grabs models subject name
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@49547 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ImportedReferenceSequence.pm
+++ Genome/Model/ImportedReferenceSequence.pm
@@ -8,12 +8,18 @@ use Genome;
 
 class Genome::Model::ImportedReferenceSequence{
     is => 'Genome::Model',
+    has => [
+        species_name => {
+            via => 'subject',
+            to => 'species_name',
+        },
+    ]
 };
 
 sub build_by_version {
     my $self = shift;
     my $version = shift;
-    my @b = $self->builds("data_directory like" => "%/v${version}_%");
+    my @b = $self->builds("data_directory like" => "%/v${version}-%");
     if (@b > 1) {
         die "Multiple builds for version $version for model " . $self->model_id;
     }
@@ -32,6 +38,11 @@ sub get_bases_file
     my $self = shift;
     my $build = $self->last_complete_build();
     return $build->get_bases_file(@_);
+}
+
+sub species{
+    my $self = shift;
+    return $self->subject_name;
 }
 
 1;
