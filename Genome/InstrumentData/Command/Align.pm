88fd83bf69c8ad32e39bce71f042e4ecf7e72995
jwalker <jwalker@genome.wustl.edu>
2009-08-06 15:45:23 +0000

    pass through params for read trimming.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@49640 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Command/Align.pm
+++ Genome/InstrumentData/Command/Align.pm
@@ -38,6 +38,18 @@ class Genome::InstrumentData::Command::Align {
                                             is => 'Text', default_value => '', 
                                             doc => 'any additional params for the aligner in a single string'
                                         },
+        trimmer_name                    => {
+                                            is => 'Text',
+                                            doc => 'the read trimming algorithm to use',
+        },
+        trimmer_version                 => {
+                                            is => 'Text',
+                                            doc => 'the verstion of the read trimming algorithm to use',
+        },
+        trimmer_params                  => {
+                                            is => 'Text',
+                                            doc => 'the params to pass to the read trimming algorithm',
+        },
         force_fragment                  => {
                                             is => 'Boolean', default_value => 0,
                                             doc => 'force paired end instrument data to align as fragment',
@@ -72,19 +84,29 @@ sub execute {
     my $self = shift;
 
     my $alignment;
+    my %alignment_params = (
+        instrument_data_id => $self->instrument_data_id,
+        reference_name => $self->reference_name,
+        aligner_name => $self->aligner_name,
+        aligner_version => $self->version,
+        aligner_params => $self->params,
+        force_fragment => $self->force_fragment,
+    );
+    if ($self->trimmer_name) {
+        $alignment_params{trimmer_name} = $self->trimmer_name;
+        if ($self->trimmer_version) {
+            $alignment_params{trimmer_version} = $self->trimmer_version;
+        }
+        if ($self->trimmer_params) {
+            $alignment_params{trimmer_params} = $self->trimmer_params;
+        }
+    }
     eval {
-       $alignment = Genome::InstrumentData::Alignment->create(
-                                                  instrument_data_id => $self->instrument_data_id,
-                                                  reference_name => $self->reference_name,
-                                                  aligner_name => $self->aligner_name,
-                                                  aligner_version => $self->version,
-                                                  aligner_params => $self->params,
-                                                  force_fragment => $self->force_fragment,
-                                              );
+        $alignment = Genome::InstrumentData::Alignment->create(%alignment_params);
     };
     if (!$alignment || $@) {
         $self->error_message($@);
-        $self->error_message('Failed to create an alignment object');
+        $self->error_message('Failed to create an alignment object with params: '. Data::Dumper::Dumper(%alignment_params) );
         return;
     }
     unless ($alignment->find_or_generate_alignment_data) {
