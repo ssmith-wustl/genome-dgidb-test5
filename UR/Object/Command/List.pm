a004927f9cd22bc731c1f1c1c6f9bb487baccff1
abrummet <abrummet@genome.wustl.edu>
2009-08-21 21:52:38 +0000

    Wrap an eval{} around calling the iterator's next().  That way if there's a problem processing one of the items in the result set (say, there's a problem subclassing it), you'll get to see all the other results, and not just a die message.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@50225 127c0c0e-971a-0410-ad48-868c1a942d8d

--- UR/Object/Command/List.pm
+++ UR/Object/Command/List.pm
@@ -167,6 +167,21 @@ sub new{
     return bless(\%args, $class);
 }
 
+sub _get_next_object_from_iterator {
+    my $self = shift;
+
+    my $obj;
+    for (1) {
+        $obj = eval { $self->{'iterator'}->next };
+        if ($@) {
+            UR::Object::Command::List->warning_message($@);
+            redo;
+        }
+    }
+    return $obj;
+}
+        
+
 sub _object_properties_to_string {
     my ($self, $o, $char) = @_;
     my @v;
@@ -202,7 +217,7 @@ sub format_and_print{
     }
 
     my $count = 0;
-    while (my $object = $self->{iterator}->next) {
+    while (my $object = $self->_get_next_object_from_iterator()) {
         $self->{output}->print($self->_get_object_string($object), "\n");
         $count++;
     }
@@ -239,7 +254,7 @@ sub format_and_print{
     }
 
     my $count = 0;
-    while (my $object = $self->{iterator}->next) {
+    while (my $object = $self->_get_next_object_from_iterator()) {
         $self->{output}->print($self->_get_object_string($object));
         $count++;
     }
@@ -299,7 +314,7 @@ sub format_and_print{
 	$doc->setDocumentElement($results_node);
 
 	my $count = 0;
-    while (my $object = $self->{iterator}->next) {
+    while (my $object = $self->_get_next_object_from_iterator()) {
 		my $object_node = $results_node->addChild( $doc->createElement("object") );
 
 		my $object_reftype = ref $object;
@@ -359,7 +374,7 @@ sub format_and_print{
     }
 
     my $count = 0;
-    while (my $object = $self->{iterator}->next) {
+    while (my $object = $self->_get_next_object_from_iterator()) {
         $tab_delimited .= $self->_get_object_string($object)."\n";
         $count++;
     }
