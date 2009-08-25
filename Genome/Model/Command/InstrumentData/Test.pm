bb43c79f7270b2b6fc580b948fa960dfadffa2c7
ebelter <ebelter@genome.wustl.edu>
2009-08-25 21:01:20 +0000

    updated to use new mock model system
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@50352 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/InstrumentData/Test.pm
+++ Genome/Model/Command/InstrumentData/Test.pm
@@ -18,7 +18,7 @@ use warnings;
 
 use base 'Genome::Utility::TestBase';
 
-require Genome::Model::AmpliconAssembly::Test;
+require Genome::Model::Test;
 use Test::More;
 
 sub test_class {
@@ -43,35 +43,12 @@ sub create_mock_model_and_instrument_data : Test(startup => 2) {
     my $self = shift;
 
     # model
-    my $model = Genome::Model::AmpliconAssembly::Test->create_basic_mock_model;
-    ok($model, 'Created mock model');
-    $self->mock_methods(
-        $model,
-        'Genome::Model',
-        (qw/
-            assigned_instrument_data unassigned_instrument_data
-            instrument_data_assignments 
-            /),
-    );
-
+    my $model = Genome::Model::Test->create_basic_mock_model(type_name => 'tester');
+    ok($model, 'Created mock model') or die;
     $self->{_model} = $model;
 
     # instrument data
-    my @instrument_data;
-    for my $i (1..4) {
-        my $run_name = '0'.$i.'jan00.101amaa';
-        push @instrument_data, Genome::InstrumentData::Sanger->create_mock(
-            id => $run_name,
-            run_name => $run_name,
-            sequencing_platform => 'sanger',
-            seq_id => $run_name,
-            sample_name => 'unknown',
-            subset_name => 1,
-            library_name => 'unknown',
-        )
-            or die "Can't create mock sanger instrument data";
-    }
-
+    my @instrument_data = Genome::Model::Test->create_mock_sanger_instrument_data(4);
     is(scalar(@instrument_data), 4, 'Created 4 instrument data');
     $self->{_instrument_data} = \@instrument_data;
 
@@ -91,7 +68,7 @@ sub test_01_list : Tests {
 sub test_02_assign_single_id : Tests {
     my $self = shift;
 
-    my $assign = $self->_create_object(
+    my $assign = $self->test_class->create(
         $self->params_for_test_class,
         instrument_data_id => $self->instrument_data->[0]->id,
     );
@@ -103,7 +80,7 @@ sub test_02_assign_single_id : Tests {
 sub test_03_assign_multiple_id : Tests {
     my $self = shift;
 
-    my $assign = $self->_create_object(
+    my $assign = $self->test_class->create(
         $self->params_for_test_class,
         instrument_data_ids => join(
             ' ',
@@ -119,7 +96,7 @@ sub test_03_assign_multiple_id : Tests {
 sub test_04_assign_all : Tests {
     my $self = shift;
 
-    my $assign = $self->_create_object(
+    my $assign = $self->test_class->create(
         $self->params_for_test_class,
         all => 1,
     );
@@ -131,7 +108,7 @@ sub test_04_assign_all : Tests {
 sub test_05_multiple_actions : Tests {
     my $self = shift;
 
-    my $assign = $self->_create_object(
+    my $assign = $self->test_class->create(
         all => 1,
         instrument_data_ids => '44 44',
     );
