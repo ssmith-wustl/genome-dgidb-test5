cc925e29eed652eede3738b920e50d22a1a1e754
ebelter <ebelter@genome.wustl.edu>
2009-08-28 18:31:44 +0000

    xslt report command
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@50498 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Report/Command.pm
@@ -0,0 +1,45 @@
+package Genome::Report::Command;
+
+use strict;
+use warnings;
+
+use Genome;
+      
+class Genome::Report::Command {
+    is => 'Command',
+    #is_abstract => 1,
+    #english_name => 'genome report command',
+    doc => 'Work with reports.',
+};
+
+############################################
+
+sub help_brief {
+    my $class = ref($_[0]) || $_[0];
+    return $class->get_class_object->doc if not $class or $class eq __PACKAGE__;
+    my ($func) = $class =~ /::(\w+)$/;
+    return sprintf('%s a report', ucfirst($func));
+}
+
+sub help_detail {
+    return help_brief(@_);
+}
+
+sub command_name {
+    my $class = ref($_[0]) || $_[0];
+    return $class->SUPER::command_name unless $class eq __PACKAGE__;
+    return 'genome report';
+}
+
+sub command_name_brief {
+    my $class = ref($_[0]) || $_[0];
+    return $class->SUPER::command_name_brief unless $class eq __PACKAGE__;
+    return 'report';
+}
+
+############################################
+
+1;
+
+#$HeadURL$
+#$Id$
