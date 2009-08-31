9907d12d374d87275178eef522952bc32629d543
josborne <josborne@genome.wustl.edu>
2009-08-31 13:13:53 +0000

    this is ready and seems to work
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@50560 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/ProcessingProfile/GenotypeMicroarray.pm
@@ -0,0 +1,23 @@
+package Genome::ProcessingProfile::GenotypeMicroarray;
+
+
+class Genome::ProcessingProfile::GenotypeMicroarray {
+    is => 'Genome::ProcessingProfile',
+    has => [ ],
+};
+
+sub input_format
+{
+    my $self = shift;
+    
+    return;
+}
+
+sub instrument_type
+{
+    my $self = shift;
+
+    return;
+}
+
+1;
