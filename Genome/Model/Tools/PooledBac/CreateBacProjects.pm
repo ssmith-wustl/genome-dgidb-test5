bbcbb0d177603dafb55dc7998645c78b63ed64cf
jschindl <jschindl@genome.wustl.edu>
2009-09-02 19:36:13 +0000

    Fix a few bugs with Pooled Bac pipeline step that runs newbler assemblies.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@50648 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/PooledBac/CreateBacProjects.pm
+++ Genome/Model/Tools/PooledBac/CreateBacProjects.pm
@@ -39,6 +39,8 @@ sub dump_sff_read_names
     my ($self, $input_fasta) = @_;
     my $input_fasta_fh = IO::File->new("$input_fasta");
     my $read_names_fh = IO::File->new(">read_names");
+    #print `pwd`,"\n";
+    #print $input_fasta,"\n";
     while(<$input_fasta_fh>) {
         next unless />/;
         chomp;
@@ -53,8 +55,11 @@ sub execute {
     my $project_dir = $self->project_dir;
     $DB::single = 1;
     chdir($project_dir);
-    my @sff_files = ('/gscmnt/sata601/production/96211313/R_2009_03_17_15_50_42_FLX03080333_adminrig_96211313/D_2009_03_18_04_10_38_blade9-2-6_fullProcessing/sff/FSP3MSF02.sff',
-                             '/gscmnt/sata601/production/96211313/R_2009_03_17_15_50_42_FLX03080333_adminrig_96211313/D_2009_03_18_04_10_23_blade9-4-10_fullProcessing/sff/FSP3MSF01.sff');
+    my @sff_files = ('/gscmnt/232/finishing/projects/Fosmid_two_pooled_Combined/Fosmid_two_pooled70_combined_trim-1.0_090417.newb/Fosmid_two_pooled70_combined_Data/input_output_data/FSP3MSF01.sff',
+                     '/gscmnt/232/finishing/projects/Fosmid_two_pooled_Combined/Fosmid_two_pooled70_combined_trim-1.0_090417.newb/Fosmid_two_pooled70_combined_Data/input_output_data/FSP3MSF02.sff');
+    
+    #('/gscmnt/sata601/production/96211313/R_2009_03_17_15_50_42_FLX03080333_adminrig_96211313/D_2009_03_18_04_10_38_blade9-2-6_fullProcessing/sff/FSP3MSF02.sff',
+                     #        '/gscmnt/sata601/production/96211313/R_2009_03_17_15_50_42_FLX03080333_adminrig_96211313/D_2009_03_18_04_10_23_blade9-4-10_fullProcessing/sff/FSP3MSF01.sff');
     my $sff_string = join ' ',@sff_files;
     
     my $seqio = Bio::SeqIO->new(-format => 'fasta', -file => 'ref_seq.fasta');
@@ -64,7 +69,11 @@ sub execute {
         my $name = $seq->display_id;
         my $date = `date +%m%d%y`;
         chomp $date;
+        next unless ((-e "$project_dir/$name") && (-d "$project_dir/$name"));
         chdir($project_dir."/$name");
+        next unless (-e 'core'||!(-e 'newbler_assembly/consed'));
+        `/bin/rm core*` if -e 'core';
+        print $project_dir."/$name","\n";
         
         $self->dump_sff_read_names("pooledreads.fasta");
         my $create_sff = "sfffile -o pooledreads.sff -i read_names $sff_string";
@@ -73,7 +82,7 @@ sub execute {
         print $command_fh "$create_sff\n$run_newbler\n";
         my %job_params = (
             pp_type => 'lsf',
-            q => 'short',
+            q => 'bigmem',
             command => 'source ./command.sh',
             o => "newbler_bsub.log",
             M => 16000000,
@@ -97,7 +106,8 @@ sub execute {
         {
             if(defined $_ && $_->has_ended){
                 if($_->is_successful) {$_ = undef;}
-                else { print Dumper $_; print "\n"; die "Job failed.\n"}                    
+                else { print "Job failed\n"; $_ = undef;#print Dumper $_; print "\n"; die "Job failed.\n"
+                }                    
             }
         }
         foreach(@jobs)
