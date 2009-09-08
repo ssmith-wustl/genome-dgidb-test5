e89261ea9469f2c1eddc51c8220e742afd4049e3
ebelter <ebelter@genome.wustl.edu>
2009-09-08 21:38:08 +0000

    added tests for  ability to exclude contaminated amplicons and use most recent reds only
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@50811 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/AmpliconAssembly/Test.pm
+++ Genome/AmpliconAssembly/Test.pm
@@ -36,21 +36,232 @@ sub invalid_params_for_test_class {
     );
 }
 
-sub test_01_amplicons_gsc_sanger : Tests {
+sub test01_invalid_creates : Tests(1) {
+    my $self = shift;
+
+    ok(!$self->test_class->create(sequencing_center => 'broad', exclude_contaminated_amplicons => 1), 'Failed as expected - create w/ unsupported attrs for broad');
+    
+    return 1;
+}
+
+sub test02_amplicons_gsc_sanger : Tests() {
     my $self = shift;
     
     # amplicons
     my $amplicon_assembly = $self->amplicon_assembly;
     my $amplicons = $amplicon_assembly->get_amplicons;
-    is(@$amplicons, 5, 'got 5 amplicons');
-
+    is_deeply(
+        [ map { $_->name } @$amplicons ],
+        [qw/ HMPB-aad13a05 HMPB-aad13e12 HMPB-aad15e03 HMPB-aad16a01 HMPB-aad16c10 /],
+        'Got 5 amplicons',
+    );
     # reads for amplicon
     my @reads = $self->amplicon_assembly->get_all_amplicons_reads_for_read_name(
         ($amplicons->[0]->reads)[0],
     );
     is_deeply(\@reads, $amplicons->[0]->get_reads, 'Got all amplicons reads for read name');
     
+    # get amplicons excluding contaminated and using only new recent read
+    my %mock_reads = $self->_create_mock_gsc_sequence_reads or die;
+    no warnings 'redefine';
+    local *Genome::AmpliconAssembly::_get_gsc_sequence_read = sub{ 
+        die "No mock read for ".$_[1] unless exists $mock_reads{$_[1]};
+        return $mock_reads{$_[1]};
+    };
+    $amplicon_assembly->exclude_contaminated_amplicons(1);
+    my $uncontaminated_amplicons = $amplicon_assembly->get_amplicons;
+    is_deeply(
+        [ map { $_->name } @$uncontaminated_amplicons ],
+        [qw/ HMPB-aad13a05 HMPB-aad13e12 HMPB-aad16a01 HMPB-aad16c10 /],
+        'Got 4 uncontaminated amplicons using all read iterations',
+    );
+    $amplicon_assembly->only_use_latest_iteration_of_reads(1);
+    my $only_latest_reads_amplicons = $amplicon_assembly->get_amplicons;
+    is_deeply(
+        [ map { $_->name } @$only_latest_reads_amplicons ],
+        # we get all 5 amplicons here because the read that is contaminated is older
+        [qw/ HMPB-aad13a05 HMPB-aad13e12 HMPB-aad15e03 HMPB-aad16a01 HMPB-aad16c10 /],
+        'Got 5 uncontaminated amplicons using only latest read iterations',
+    );
+    is_deeply(
+        $only_latest_reads_amplicons->[0]->get_reads,
+        [qw/ 
+        HMPB-aad13a05.b3
+        HMPB-aad13a05.b4
+        HMPB-aad13a05.g1
+        /],
+        'Got latest iterations for reads'
+    );
+    
     return 1;
+}
+
+sub _create_mock_gsc_sequence_reads {
+    my $self = shift;
+
+    my %reads;
+    my %read_params = (
+        'HMPB-aad13a05.b1' => {
+            'run_date' => '16-MAY-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13a05.b2' => {
+            'run_date' => '17-MAY-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13a05.b3' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13a05.b4' => {
+            'run_date' => '02-OCT-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13a05.g1' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13a05.g2' => {
+            'run_date' => '20-MAY-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13e12.b1' => {
+            'run_date' => '16-MAY-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13e12.b2' => {
+            'run_date' => '17-MAY-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13e12.b3' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13e12.b4' => {
+            'run_date' => '02-OCT-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13e12.g1' => {
+            'run_date' => '20-MAY-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad13e12.g2' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad15e03.b1' => {
+            'run_date' => '16-MAY-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad15e03.b2' => {
+            'run_date' => '17-MAY-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad15e03.b3' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad15e03.b4' => {
+            'run_date' => '02-OCT-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad15e03.g1' => {
+            'run_date' => '20-MAY-2008',
+            'primer_code' => '-28RPpOT',
+            # CONTAMINATED READ #
+            'contamination_status' => 'FOUND',
+        },
+        'HMPB-aad15e03.g2' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16a01.b1' => {
+            'run_date' => '16-MAY-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16a01.b2' => {
+            'run_date' => '17-MAY-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16a01.b3' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16a01.b4' => {
+            'run_date' => '02-OCT-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16a01.g1' => {
+            'run_date' => '20-MAY-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16a01.g2' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16c10.b1' => {
+            'run_date' => '16-MAY-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16c10.b2' => {
+            'run_date' => '17-MAY-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16c10.b3' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-21UPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16c10.b4' => {
+            'run_date' => '02-OCT-2008',
+            'primer_code' => '907R',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16c10.g1' => {
+            'run_date' => '20-MAY-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+        'HMPB-aad16c10.g2' => {
+            'run_date' => '01-OCT-2008',
+            'primer_code' => '-28RPpOT',
+            'contamination_status' => 'NOT FOUND',
+        },
+    );
+    for my $read_name ( keys %read_params ) {
+        $reads{$read_name} = Test::MockObject->new();
+        $reads{$read_name}->set_always('trace_name', $read_name);
+        for my $attr ( keys %{$read_params{$read_name}} ) {
+        $reads{$read_name}->set_always($attr, $read_params{$read_name}->{$attr});
+    }
+    }
+
+    return %reads;
 }
 
 # TODO sub test_02_amplicons_broad_sanger : Tests {
