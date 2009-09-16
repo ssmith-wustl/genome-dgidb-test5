37906c550c7f26dd15a1ff3a7c436cc33dca2e0c
josborne <josborne@genome.wustl.edu>
2009-09-16 20:30:41 +0000

    Removed accessors which don't map to columns.  Removed old boilerplate code.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51047 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/GenotypeMicroarray.pm
+++ Genome/Model/GenotypeMicroarray.pm
@@ -9,35 +9,12 @@ use Sort::Naturally;
 use IO::File;
 
 class Genome::Model::GenotypeMicroarray{
-    #is => 'Genome::Model::ImportedVariants',
     is => 'Genome::Model',
     has => [
-        filename => { is => 'String',
-                      doc => "",
-                    },
-        source => {
-                    is => 'String',
-                    doc => "",
-        },
         input_format    => { via => 'processing_profile' },
         instrument_type => { via => 'processing_profile' },
     ],
 };
-
-
-sub create
-{
-    my $class = shift;
-    my $self = $class->SUPER::create(@_);
-    return unless $self;
-
-    return $self;
-}
-
-# Hack for now to get genome-model list models to not break
-sub reference_sequence_name {
-    return 'N/A';
-}
 
 1;
 
