91a741fbcdd6e4fe2057001b615e703ddff3054a
jwalker <jwalker@genome.wustl.edu>
2009-09-17 12:56:30 +0000

    modifications to check for successful completion.  Also convert the sam to bam and cleanup all intermediate files.  10x the original allocation is necessary for processing alignments, but most intermediate files are removed upon completion.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51071 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Alignment/Tophat.pm
+++ Genome/InstrumentData/Alignment/Tophat.pm
@@ -4,11 +4,12 @@ use strict;
 use warnings;
 
 use Genome;
+use File::Path;
 
 class Genome::InstrumentData::Alignment::Tophat {
     is => ['Genome::InstrumentData::Alignment'],
     has_constant => [
-        aligner_name => { value => 'Tophat' },
+        aligner_name => { value => 'tophat' },
     ],
 };
 
@@ -33,9 +34,14 @@ sub aligner_output_file {
     return $self->alignment_directory .'/tophat.aligner_output';
 }
 
-sub alignment_file {
+sub sam_file {
     my $self = shift;
     return $self->alignment_directory .'/accepted_hits.sam';
+}
+
+sub bam_file {
+    my $self = shift;
+    return $self->alignment_directory .'/accepted_hits.bam';
 }
 
 sub coverage_file {
@@ -48,13 +54,28 @@ sub junctions_file {
     return $self->alignment_directory .'/junctions.bed';
 }
 
+sub tmp_tophat_directory {
+    my $self = shift;
+    return $self->alignment_directory .'/tmp';
+}
+
 sub output_files {
     my $self = shift;
     my @output_files;
-    for my $method (qw/aligner_output_file alignment_file coverage_file junctions_file/) {
+    for my $method (qw/aligner_output_file bam_file coverage_file junctions_file/) {
         push @output_files, $self->$method;
     }
     return @output_files;
+}
+
+sub estimated_kb_usage {
+    my $self = shift;
+    my $instrument_data = $self->instrument_data;
+    unless ($instrument_data->calculate_alignment_estimated_kb_usage) {
+        return;
+    } else {
+        return ($instrument_data->calculate_alignment_estimated_kb_usage * 10);
+    }
 }
 
 sub find_or_generate_alignment_data {
@@ -117,7 +138,6 @@ sub _run_aligner {
     my $instrument_data = $self->instrument_data;
     my $reference_build = $self->reference_build;
     my $alignment_directory = $self->alignment_directory;
-
     $self->status_message("OUTPUT PATH: $alignment_directory\n");
 
 
@@ -131,6 +151,7 @@ sub _run_aligner {
         $self->error_message('Failed to find full consensus path for reference build '. $reference_build->name);
         die($self->error_message);
     }
+    #TODO:  A check for bowtie index files could be added to the reference placeholder...
     my $ref_seq_index_file =  $ref_seq_file .'.1.ebwt';
     unless (-e $ref_seq_index_file) {
         $self->error_message("Reference build index path '$ref_seq_index_file' does not exist.");
@@ -149,7 +170,6 @@ sub _run_aligner {
         $is_paired_end = 0;
     }
 
-
     # these are general params not infered from the above
     my $aligner_output_file = $self->aligner_output_file;
     my $aligner_params = $self->aligner_params;
@@ -165,6 +185,7 @@ sub _run_aligner {
 
     # RESOLVE A STRING OF ALIGNMENT PARAMETERS
     if ($is_paired_end && $insert_size && $insert_sd) {
+        #The standard deviation is the above instert size deviation, does below insert size stdev matter?
         $aligner_params .= ' --mate-inner-dist '. $insert_size .' --mate-std-dev '. $insert_sd;
     }
 
@@ -175,23 +196,46 @@ sub _run_aligner {
               $alignment_directory,
               $aligner_params,
               $ref_seq_file,
-              $files_to_align) . $aligner_output_file .' 2>&1';
+              $files_to_align) .' > '. $aligner_output_file .' 2>&1';
     my @input_files = ($ref_seq_file, @input_pathnames);
 
     $self->status_message("COMMAND: $cmdline\n");
-
-    my @output_files = $self->output_files;
     Genome::Utility::FileSystem->shellcmd(
                                           cmd                         => $cmdline,
                                           input_files                 => \@input_files,
-                                          output_files                => \@output_files,
+                                          output_files                => [$self->sam_file],
+                                          allow_zero_size_output_files => 1,
                                           skip_if_output_is_present   => 1,
                                       );
-    unless ($self->verify_aligner_successful_completion) {
-        $self->error_message('Failed to verify Tophat successful completion from output files: '. join("\n",@output_files) ."\n");
+
+    my $sam_to_bam = Genome::Model::Tools::Sam::SamToBam->execute(
+        sam_file    => $self->sam_file,
+        bam_file    => $self->bam_file,
+        ref_list    => $reference_build->full_consensus_sam_index_path,
+        is_sorted   => 0,
+        index_bam   => 1,
+        fix_mate    => 0,
+        keep_sam    => 0,
+    );
+    unless ($sam_to_bam) {
+        $self->error_message('Error converting SAM file: '. $self->sam_file .' to BAM file '. $self->bam_file);
         die($self->error_message);
     }
-    #TODO: convert SAM format output to BAM file, then sort
+    unless ($self->verify_aligner_successful_completion) {
+        $self->error_message('Failed to verify Tophat successful completion!');
+        die($self->error_message);
+    }
+    unless (rmtree($self->tmp_tophat_directory)) {
+        $self->error_message('Failed to remove tmp tophat directory '. $self->tmp_tophat_directory .":  $!");
+        die($self->error_message);
+    }
+    my @intermediate_files = glob($self->alignment_directory .'/*.fq*');
+    for my $intermediate_file (@intermediate_files) {
+        unless (unlink($intermediate_file)) {
+            $self->error_message('Failed to remove intermediate tophat file '. $intermediate_file .":  $!");
+            die($self->error_message);
+        }
+    }
     return 1;
 }
 
@@ -205,22 +249,13 @@ sub verify_aligner_successful_completion {
 
     my @output_files = $self->output_files;
     for my $output_file (@output_files) {
-        unless (-s $output_file) {
-            $self->error_message("Alignment output file '$output_file' not found or zero size.");
+        unless (-e $output_file) {
+            $self->error_message("Alignment output file '$output_file' not found.");
             return;
         }
-        my $output_fh = Genome::Utility::FileSystem->open_file_for_reading($output_file);
-        unless ($output_fh) {
-            $self->error_message("Can't open alignment output file $output_file: $!");
-            return;
-        }
-        $output_fh->close;
     }
-    return 1;
-
     #TODO: Find a line in the aligner output that
     # 1.) denotes paired end alignment
-    # 2.) equates to successful completion
     #Otherwise the file existence check is already occuring in shellcmd
     
     #my $instrument_data = $self->instrument_data;
@@ -241,13 +276,19 @@ sub verify_aligner_successful_completion {
             #}
         #}
     #}
-    #while(<$aligner_output_fh>) {
-    #    if (m/^# Done.$/) {
-    #        $aligner_output_fh->close();
-    #        return 1;
-    #    }
-    #}
-    #return;
+    my $aligner_output_fh = Genome::Utility::FileSystem->open_file_for_reading($self->aligner_output_file);
+    unless ($aligner_output_fh) {
+        $self->error_message('Failed to open aligner output file '. $self->aligner_output_file .":  $!");
+        return;
+    }
+    while(<$aligner_output_fh>) {
+        if (m/^Run complete/) {
+            $aligner_output_fh->close();
+            return 1;
+        }
+    }
+    $aligner_output_fh->close();
+    return;
 }
 
 sub get_alignment_statistics {
