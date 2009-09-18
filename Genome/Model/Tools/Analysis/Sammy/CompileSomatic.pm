b9bf9db1d7b69c6f863762f8ffe2fe5ecb3da344
dkoboldt <dkoboldt@genome.wustl.edu>
2009-09-18 21:25:46 +0000

    New LOH Functions
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51158 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Analysis/Sammy/CompileSomatic.pm
+++ Genome/Model/Tools/Analysis/Sammy/CompileSomatic.pm
@@ -29,6 +29,8 @@ class Genome::Model::Tools::Analysis::Sammy::CompileSomatic {
 		somatic_file	=> { is => 'Text', doc => "File name of Sammy-somatic calls", is_optional => 1 },
 		dbsnp_file	=> { is => 'Text', doc => "Tab-delimited file containing known dbSNPs", is_optional => 1 },
 		p_value	        => { is => 'Text', doc => "P-value threshold for somatic variants [1.0E-06]", is_optional => 1 },
+		include_dbsnp   => { is => 'Text', doc => "When set to 1, dbSNP variants are included in annotation/tier1", is_optional => 1 },		
+		include_loh   => { is => 'Text', doc => "When set to 1, LOH variants are included in annotation/tier1", is_optional => 1 },		
 	],
 };
 
@@ -68,6 +70,19 @@ sub execute {                               # replace with real execution logic.
 	
 	my $p_value = 1.0E-06;
 	$p_value = $self->p_value if($self->p_value);
+
+	## Determine dbSNP file ##
+	
+	my $dbsnp_file = "";
+	
+	if($self->dbsnp_file)
+	{
+		$dbsnp_file = $self->dbsnp_file;
+	}
+	else
+	{
+		$dbsnp_file = "/gscuser/dkoboldt/SNPseek/SNPseek2/ucsc/snp130.txt.nochrPositions";
+	}
 	
 	## Determine somatic status file ##
 
@@ -84,50 +99,96 @@ sub execute {                               # replace with real execution logic.
 
 	my $somatic_snp_file = $self->output_dir . "/" . $self->sample_name . ".somatic";
 	my $num_somatic = parse_somatic_variants($self, $somatic_status_file, $somatic_snp_file);
-
 	print "$num_somatic Somatic variants\n";
 
 	if($num_somatic > 0)
-	{
-		## Determine dbSNP file ##
-		
-		my $dbsnp_file = "";
-		
-		if($self->dbsnp_file)
-		{
-			$dbsnp_file = $self->dbsnp_file;
-		}
-		else
-		{
-			$dbsnp_file = "/gscuser/dkoboldt/SNPseek/SNPseek2/ucsc/snp130.txt.nochrPositions";
-		}
-	
+	{	
 		## Remove dbSNPs ##
 		
 		my $somatic_non_dbsnp = $self->output_dir . "/" . $self->sample_name . ".somatic.not-dbSNP";
-		
-		system("gt bowtie limit-snps --positions-file $dbsnp_file --variants-file $somatic_snp_file --not-file $somatic_non_dbsnp");
-
-
-		## Format SNPs for annotation ##
 		my $formatted_file = $self->output_dir . "/" . $self->sample_name . ".somatic.not-dbSNP.formatted";
+		
+		if($self->include_dbsnp)
+		{
+			print "dbSNP variants will NOT be excluded prior to annotation\n";
+			$somatic_non_dbsnp = $self->output_dir . "/" . $self->sample_name . ".somatic";
+			$formatted_file = $self->output_dir . "/" . $self->sample_name . ".somatic.formatted";
+		}
+		else
+		{
+			system("gt bowtie limit-snps --positions-file $dbsnp_file --variants-file $somatic_snp_file --not-file $somatic_non_dbsnp");
+		}
+
+		my $annotated_file = $formatted_file . ".annotations";
+		my $merged_file = $somatic_non_dbsnp . ".annotated";
+		
+		## Format SNPs for annotation ##
+
 		system("perl ~dkoboldt/src/mptrunk/trunk/Auto454/format_snps_for_annotation.pl $somatic_non_dbsnp $formatted_file");
 
 		my $num_non_dbsnp = `cat $formatted_file | wc -l`;
 		chomp($num_non_dbsnp);
 		
 		print "$num_non_dbsnp Somatic non-dbSNP variants formatted for annotation\n";
-
 		print "Running annotation...\n";
 		
-		my $annotated_file = $formatted_file . ".annotations";
+
 		system("gt annotate transcript-variants --variant-file $formatted_file --output-file $annotated_file");
 		
 		print "Merging annotations with SNP calls...\n";
-		my $merged_file = $somatic_non_dbsnp . ".annotated";
+
 		system("perl ~dkoboldt/src/mptrunk/trunk/Auto454/format_snps_with_annotation.pl $somatic_non_dbsnp $annotated_file $merged_file");
 
 	}
+
+
+	if($self->include_loh)
+	{
+		my $loh_snp_file = $self->output_dir . "/" . $self->sample_name . ".loh";
+		my $num_loh = parse_loh_variants($self, $somatic_status_file, $loh_snp_file);		
+		print "$num_loh LOH variants\n";
+	
+		if($num_loh > 0)
+		{	
+			## Remove dbSNPs ##
+			
+			my $loh_non_dbsnp = $self->output_dir . "/" . $self->sample_name . ".loh.not-dbSNP";
+			my $formatted_file = $self->output_dir . "/" . $self->sample_name . ".loh.not-dbSNP.formatted";
+			
+			if($self->include_dbsnp)
+			{
+				print "dbSNP variants will NOT be excluded prior to annotation\n";
+				$loh_non_dbsnp = $self->output_dir . "/" . $self->sample_name . ".loh";
+				$formatted_file = $self->output_dir . "/" . $self->sample_name . ".loh.formatted";
+			}
+			else
+			{
+				system("gt bowtie limit-snps --positions-file $dbsnp_file --variants-file $loh_snp_file --not-file $loh_non_dbsnp");
+			}
+	
+			my $annotated_file = $formatted_file . ".annotations";
+			my $merged_file = $loh_non_dbsnp . ".annotated";
+			
+			## Format SNPs for annotation ##
+	
+			system("perl ~dkoboldt/src/mptrunk/trunk/Auto454/format_snps_for_annotation.pl $loh_non_dbsnp $formatted_file");
+	
+			my $num_non_dbsnp = `cat $formatted_file | wc -l`;
+			chomp($num_non_dbsnp);
+			
+			print "$num_non_dbsnp Somatic non-dbSNP variants formatted for annotation\n";
+			print "Running annotation...\n";
+			
+	
+			system("gt annotate transcript-variants --variant-file $formatted_file --output-file $annotated_file");
+			
+			print "Merging annotations with SNP calls...\n";
+	
+			system("perl ~dkoboldt/src/mptrunk/trunk/Auto454/format_snps_with_annotation.pl $loh_non_dbsnp $annotated_file $merged_file");
+		}
+	}
+
+
 	
 	return 1;                               # exits 0 for true, exits 1 for false (retval/exit code mapping is overridable)
 }
@@ -193,6 +254,64 @@ sub parse_somatic_variants
 }
 
 
+
+###############################################################################################
+# parse_somatic_variants - parse out the Somatic variants from the status file
+#
+###############################################################################################
+
+sub parse_loh_variants
+{
+	(my $self, my $infile, my $outfile) = @_;
+
+	my $p_value_threshold = 1.0E-06;
+	$p_value_threshold = $self->p_value if($self->p_value);
+	
+	print "Infile: $infile\nOutfile: $outfile P-value $p_value_threshold\n";
+
+	my $num_somatic = 0;
+	
+	open(OUTFILE, ">$outfile") or die "Can't open outfile $outfile: $!\n";
+	
+	my $input = new FileHandle ($infile);
+	my $lineCounter = 0;
+	
+	while (<$input>)
+	{
+		chomp;
+		my $line = $_;
+		$lineCounter++;		
+
+		if($line)
+		{
+			my @lineContents = split(/\t/, $line);
+			my $chrom = $lineContents[0];
+			my $position = $lineContents[1];
+			my $allele1 = $lineContents[2];
+			my $allele2 = $lineContents[3];
+			my $status = $lineContents[12];
+			my $p_value = $lineContents[13];
+
+			if($status && $status eq "LOH" && $p_value <= $p_value_threshold)
+			{
+				print OUTFILE "$line\n";
+				print "$chrom\t$position\t$allele1\t$allele2\t$status\t$p_value\n";
+				
+				$num_somatic++;
+			}
+
+		}
+		
+#		return(0) if($lineCounter > 40);
+	}
+
+	
+	close($input);
+	
+	close(OUTFILE);
+	
+	return($num_somatic);
+}
 
 
 sub call_sammy
