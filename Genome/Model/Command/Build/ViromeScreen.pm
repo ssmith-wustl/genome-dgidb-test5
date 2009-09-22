ab7f04a233c2fb62b29aa509c7b5a6de98045c60
kkyung <kkyung@genome.wustl.edu>
2009-09-22 14:56:41 +0000

    updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51193 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ViromeScreen.pm
+++ Genome/Model/Command/Build/ViromeScreen.pm
@@ -7,7 +7,36 @@ use Genome;
 
 class Genome::Model::Command::Build::ViromeScreen {
     is => 'Genome::Model::Command::Build',
- };
+    has => [
+	barcode_file => {
+	    doc => 'Barcode file that contains sequences to filter reads by',
+	    is => 'String',
+	    is_optional => 1,
+	},
+	log_file => {
+	    doc => 'Log file to keep track of virome screen run ',
+	    is => 'String',
+	    is_optional => 1,
+	},
+    ]
+};
+
+sub create {
+    my $class = shift;
+
+    my $self = $class->SUPER::create(@_);
+
+    #SHOULD BE DONE IN BUILD
+    unless (-s $self->barcode_file) {
+	$self->error_message("Error: Uable to open file: ".$self->barcode_file);
+	return;
+    }
+
+    $self->build->barcode_file($self->barcode_file);
+    $self->build->log_file($self->log_file);
+
+    return $self;
+}
 
 sub help_brief {
     "Run virome screening on a 454 run"
