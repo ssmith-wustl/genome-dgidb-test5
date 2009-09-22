5796fdfa7deed9edbc201b85b4f90630e568bcc9
kkyung <kkyung@genome.wustl.edu>
2009-09-22 14:57:29 +0000

    Updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51194 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ViromeScreen/Screen.pm
+++ Genome/Model/Command/Build/ViromeScreen/Screen.pm
@@ -4,40 +4,45 @@ use strict;
 use warnings;
 
 use Genome;
+use IO::File;
+use Data::Dumper;
 
 class Genome::Model::Command::Build::ViromeScreen::Screen {
-    is => 'Genome::Model::Command::Build::ViromeScreen',
+    is => 'Genome::Model::Event',
 };
 
 sub execute {
     my $self = shift;
 
-    unless (-d $self->build->dir) {
+    unless (-d $self->build->data_directory) {
 	$self->error_message("Run directory does not exist");
 	return;
     }
 
-    unless (-s $self->build->fasta_file) {
-	$self->error_message("Run fasta file does not exist");
+    my $fasta_name = $self->model->subject_name.'.fna';
+    my $screen_dir = $self->build->data_directory.'/virome_screen';
+
+    unless (-s $screen_dir.'/'.$fasta_name) {
+	$self->error_message("Error: Can not find file: $fasta_file");
 	return;
     }
 
-    unless (-s $self->build->barcode_file) {
-	$self->error_message("Run barcode file does not exist");
-	return;
-    }
+    print $self->build->log_file."LogFile\n";
 
     my $run = Genome::Model::Tools::ViromeScreening->create (
-	fasta_file   => $self->build->fasta_file,
+	fasta_file   => $screen_dir.'/'.$fasta_name,
 	barcode_file => $self->build->barcode_file,
-	dir          => $self->build->dir,
-	logfile      => $self->build->logfile, #THIS IS NOT AN INPUT FILE??
+	dir          => $self->build->data_directory,
+	logfile      => $screen_dir.'/'.$self->build->log_file, #THIS IS NOT AN INPUT FILE??
     );
 
     unless ($run) {
 	$self->error_message("Failed to create virome screen run");
 	return;
     }
+
+    print Dumper $run;
+    return 1;
 
     unless ($run->execute) {
 	$self->error_message("Failed virome screen run");
