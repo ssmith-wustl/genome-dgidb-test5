926fd3729b2adb67258ac4fe1c622eec4d33d9b5
ebelter <ebelter@genome.wustl.edu>
2009-09-28 16:20:48 +0000

    updates to aa asembly tools
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51400 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/AmpliconAssembly/TrimAndScreen.pm
+++ Genome/Model/Command/Build/AmpliconAssembly/TrimAndScreen.pm
@@ -14,8 +14,6 @@ sub execute {
 
     my $trim_and_screener = Genome::Model::Tools::AmpliconAssembly::TrimAndScreen->create(
         directory => $self->build->data_directory,
-        sequencing_center => $self->model->sequencing_center,
-        sequencing_platform => $self->model->sequencing_platform,
         trimmer_and_screener => 'trim3_and_crossmatch',
         #trimmer_and_screener => $self->model->trimmer_and_screener,
         #trimmer_and_screener_params => $self->model->trimmer_and_screener_params,
