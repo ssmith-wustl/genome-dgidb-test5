e628cbb5f9b5ba04a8d2186b3711e21d226fdc6f
ebelter <ebelter@genome.wustl.edu>
2009-09-28 16:12:57 +0000

    base class for aa reports
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51391 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/AmpliconAssembly/Report.pm
@@ -0,0 +1,40 @@
+package Genome::AmpliconAssembly::Report;
+
+use strict;
+use warnings;
+
+use Genome;
+
+use Data::Dumper 'Dumper';
+use Regexp::Common;
+
+class Genome::AmpliconAssembly::Report {
+    is => 'Genome::Report::Generator',
+    has => [
+    amplicon_assemblies => {
+        is => 'ARRAY',
+        doc => 'Amplicon assemblies to generate stats.',
+    },
+    ],
+};
+
+#< Generator >#
+sub create {
+    my $class = shift;
+
+    my $self = $class->SUPER::create(@_)
+        or return;
+
+    unless ( $self->amplicon_assemblies ) {
+        $self->error_message('No amplicon assemblies given to generate stats');
+        $self->delete;
+        return;
+    }
+
+    return $self;
+}
+
+1;
+
+#$HeadURL$
+#$Id$
