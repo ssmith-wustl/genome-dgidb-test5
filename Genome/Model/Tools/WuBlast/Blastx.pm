d3d6064a981a46377f1674f34d6b41c092711c78
jwalker <jwalker@genome.wustl.edu>
2009-10-01 16:50:38 +0000

    a version of blastx with a copy of the query-chunk command
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51531 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/WuBlast/Blastx.pm
@@ -0,0 +1,14 @@
+package Genome::Model::Tools::WuBlast::Blastx;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::WuBlast::Blastx {
+    is => 'Genome::Model::Tools::WuBlast',
+};
+
+
+1;
+
