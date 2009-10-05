54498850ccf8967d5f4d8636223213c67f92b2bb
jpeck <jpeck@genome.wustl.edu>
2009-10-05 20:36:33 +0000

    Changes for TCGA support and integration.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51623 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ReferenceAlignment/Test.pm
+++ Genome/Model/Command/Build/ReferenceAlignment/Test.pm
@@ -48,7 +48,7 @@ sub new {
         $self->add_directory_to_remove($self->data_dir);
     }
 
-    my $tmp_dir = File::Temp::tempdir('ReferenceAlignmentTestXXXXX', DIR => '/gsc/var/cache/testsuite/running_testsuites', CLEANUP => 0);
+    my $tmp_dir = File::Temp::tempdir('ReferenceAlignmentTestXXXXX', DIR => '/gsc/var/cache/testsuite/running_testsuites', CLEANUP => 1);
     $ENV{GENOME_MODEL_ROOT} = $tmp_dir;
     $ENV{GENOME_MODEL_DATA} = $tmp_dir;
     if ($args{messages}) {
