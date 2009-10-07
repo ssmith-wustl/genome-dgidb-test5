5efc9954d67a138afeaf2793be2cdbabb13f5f0e
ebelter <ebelter@genome.wustl.edu>
2009-10-07 21:30:29 +0000

    added inputs to models and builds; updated Input class def
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51732 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Build/Input.pm
@@ -0,0 +1,35 @@
+package Genome::Model::Build::Input;
+
+use strict;
+use warnings;
+
+use Genome;
+class Genome::Model::BuildInput {
+    type_name => 'genome model build input',
+    table_name => 'GENOME_MODEL_BUILD_INPUT',
+    subclassify_by => 'input_class_name',
+    id_by => [
+        build_id         => { is => 'NUMBER', len => 11 },
+        input_class_name => { is => 'VARCHAR2', len => 255 },
+        input_id         => { is => 'VARCHAR2', len => 1000 },
+        name             => { is => 'VARCHAR2', len => 255, },
+    ],
+    has => [
+    model => {
+        is => 'Genome::Model',
+        via => 'build',
+    },
+    build => { 
+        is => 'Genome::Model::Build', 
+        id_by => 'build_id',
+        constraint_name => 'GMBI_GMB_FK',
+    },
+    ],
+    schema_name => 'GMSchema',
+    data_source => 'Genome::DataSource::GMSchema',
+};
+
+1;
+
+#$HeadURL$
+#$Id$
