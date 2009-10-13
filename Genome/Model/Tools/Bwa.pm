faeceee1cf29421430c8f156fe9829ed40839122
boberkfe <boberkfe@genome.wustl.edu>
2009-10-13 17:52:41 +0000

    add bwa 0.5.4
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51857 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Bwa.pm
+++ Genome/Model/Tools/Bwa.pm
@@ -6,7 +6,7 @@ use warnings;
 use Genome;
 use File::Basename;
 
-my $DEFAULT = '0.5.3';
+my $DEFAULT = '0.5.4';
 
 class Genome::Model::Tools::Bwa {
     is => 'Command',
@@ -49,6 +49,7 @@ my %BWA_VERSIONS = (
 	'0.5.1' => '/gsc/pkg/bio/bwa/bwa-0.5.1-64/bwa',
     '0.5.2' => '/gsc/pkg/bio/bwa/bwa-0.5.2-64/bwa',
     '0.5.3' => '/gsc/pkg/bio/bwa/bwa-0.5.3-64/bwa',
+    '0.5.4' => '/gsc/pkg/bio/bwa/bwa-0.5.4-64/bwa',
     'bwa'   => 'bwa',
 );
 
