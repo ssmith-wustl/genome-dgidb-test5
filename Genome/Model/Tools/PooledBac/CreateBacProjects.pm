cd75b7289659227b7be117b6e8ef4e7a66789454
jschindl <jschindl@genome.wustl.edu>
2009-10-14 16:59:38 +0000

    Added some code to better handle newbler crashes.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@51895 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/PooledBac/CreateBacProjects.pm
+++ Genome/Model/Tools/PooledBac/CreateBacProjects.pm
@@ -17,7 +17,26 @@ class Genome::Model::Tools::PooledBac::CreateBacProjects {
             type => 'String',
             is_optional => 0,
             doc => "output dir for separate pooled bac projects"        
-        } 
+        },
+        queue_type => 
+        {
+            type => 'String',
+            is_optional => 1,
+            doc => "can be either short, big_mem, or long, default is long",     
+            valid_values => ['long','bigmem','short']   
+        },
+        retry_count =>
+        {
+            type => 'String',
+            is_optional => 1,
+            doc => "This is the number of retries for a failed job.  The default is 1.",        
+        },
+        sff_files =>
+        {
+            type => 'String',
+            is_optional => 1,
+            doc => "This is the location of the pooled bac Sff Files",        
+        }             
     ]
 };
 
@@ -102,16 +121,16 @@ sub execute {
     my ($self) = @_;
     print "Creating Bac Projects...\n";
     my $project_dir = $self->project_dir;
+    my $retry_count = $self->retry_count || 3;
+        
     $DB::single = 1;
     chdir($project_dir);
     #my @sff_files = ('/gscmnt/232/finishing/projects/Fosmid_two_pooled_Combined/Fosmid_two_pooled70_combined_trim-1.0_090417.newb/Fosmid_two_pooled70_combined_Data/input_output_data/FSP3MSF01.sff',
     #                 '/gscmnt/232/finishing/projects/Fosmid_two_pooled_Combined/Fosmid_two_pooled70_combined_trim-1.0_090417.newb/Fosmid_two_pooled70_combined_Data/input_output_data/FSP3MSF02.sff');
-    my @sff_files = ('/gscmnt/274/finishing/projects/Human_pool_1_combined_3730_454-1.0_090516.newb/sff/FR3TRLO01.sff',
-                            '/gscmnt/274/finishing/projects/Human_pool_1_combined_3730_454-1.0_090516.newb/sff/FR3TRLO02.sff',
-                            '/gscmnt/274/finishing/projects/Human_pool_1_combined_3730_454-1.0_090516.newb/sff/FUJGK1Y01.sff',
-                            '/gscmnt/274/finishing/projects/Human_pool_1_combined_3730_454-1.0_090516.newb/sff/FUJGK1Y02.sff');
-    my $sff_string = join ' ',@sff_files;
-    
+    my $sff_string = $self->sff_files;
+    $sff_string =~ tr/,/ / if defined $sff_string;
+    print "sff string is $sff_string\n" if defined $sff_string;
+
     my $seqio = Bio::SeqIO->new(-format => 'fasta', -file => 'ref_seq.fasta');
     $self->error_message("Failed to open $project_dir/ref_seq.fasta") unless defined $seqio;
     my @jobs;
@@ -123,12 +142,22 @@ sub execute {
         
         chdir($project_dir."/$name");        
         `/bin/rm core*` if -e 'core';
-        print $project_dir."/$name","\n";
-        $self->dump_sff_read_names("pooledreads.fasta");
-        `sfffile -o pooledreads.sff -i read_names $sff_string`;
-        $self->add_3730_reads;
-        my $run_newbler = "mapasm runAssembly -o newbler_assembly -consed -rip -cpu 7 -vt /gscmnt/233/analysis/sequence_analysis/databases/genomic_contaminant.db.081104.fna reference_reads.fasta 3730_reads.fasta pooledreads.sff";
+        print "submitting job for clone ",$project_dir."/$name","\n";
+        my $run_newbler;
+        if(defined $sff_string)
+        {
+            print "sff string is $sff_string\n";
+            $self->dump_sff_read_names("pooledreads.fasta"); 
+            `sfffile -o pooledreads.sff -i read_names $sff_string`;
+            $self->add_3730_reads;
+            $run_newbler = "mapasm runAssembly -o newbler_assembly -consed -rip -cpu 7 -vt /gscmnt/233/analysis/sequence_analysis/databases/genomic_contaminant.db.081104.fna reference_reads.fasta 3730_reads.fasta pooledreads.sff";
+        }
+        else
+        {
+            $run_newbler = "mapasm runAssembly -o newbler_assembly -consed -rip -cpu 7 -vt /gscmnt/233/analysis/sequence_analysis/databases/genomic_contaminant.db.081104.fna reference_reads.fasta pooledreads.fasta";
+        }        
         my $command_fh = IO::File->new(">command.sh");
+        $self->error_message("Failed to create file handle for $project_dir/$name/command.sh\n") unless defined $command_fh;
         print $command_fh "$run_newbler\n";
         system ("chmod 755 ./command.sh");
         my %job_params = (
@@ -140,30 +169,50 @@ sub execute {
             rusage => ['mem=16000'],
             select => ['type==LINUX64','mem>16000']
         );
-        my $job = PP::LSF->create(%job_params);
+        my $job = {params => \%job_params, job => PP::LSF->create(%job_params), try => 0, dir => "$project_dir/$name"};
         $self->error_message("Can't create job: $!")
-            and return unless $job;
+            and return unless $job->{job};
         push @jobs, $job;            
         chdir($project_dir);
     }
     foreach(@jobs)
     {
-        $_->start;
+        $_->{job}->start;
     }
     print "Running jobs";
     while(1)
     {
-        foreach(@jobs)
+        foreach my $job (@jobs)
         {
-            if(defined $_ && $_->has_ended){
-                if($_->is_successful) {$_ = undef;}
-                else { $self->warning_message( "Job failed"); $_ = undef;#print Dumper $_; print "\n"; die "Job failed.\n"
+            if(defined $job->{job} && $job->{job}->has_ended){
+                if($job->{job}->is_successful) {$job->{job} = undef;}
+                else {                    
+                    if($job->{try}<$retry_count)
+                    {
+                        chdir ($job->{dir});
+                        $self->warning_message( "Job failed\n");
+                        print "Restarting job\n";
+                        print "Resubmitting command $job->{params}{command}\n";
+                        $job->{try}++;
+                        $job->{job} = PP::LSF->create(%{$job->{params}});
+                        $self->error_message("Can't create job: $!")
+                            and return unless $job->{job};
+
+                        $job->{job}->start;
+                        chdir($project_dir);
+                    
+                    }
+                    else
+                    {
+                        print "Job failed after $retry_count tries\n";
+                        print "Command for failed job is located at $job->{params}{command}\n";
+                    }
                 }                    
             }
         }
-        foreach(@jobs)
+        foreach my $job (@jobs)
         {
-            if(defined $_) { goto SLEEP;}
+            if(defined $job->{job}) { goto SLEEP;}
         }
         last; #if we're here then we're done
 SLEEP:  
@@ -171,6 +220,7 @@ SLEEP:
         sleep 30;
     }
     print "\nJobs finished\n";
+    system "chmod 777 -R $project_dir";
     return 1;
 }
 
