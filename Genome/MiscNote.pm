efe2408428f41f5b47c5e263cb8fa73ddb0680d0
ssmith <ssmith@genome.wustl.edu>
2009-10-19 18:46:33 +0000

    Updated to use the id_class_by logic to get its subject.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@52002 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/MiscNote.pm
+++ Genome/MiscNote.pm
@@ -11,15 +11,34 @@ class Genome::MiscNote {
         id => { is => 'NUMBER', len => 10 },
     ],
     has => [
-        body_text          => { is => 'VARCHAR2', len => 1000 },
+        subject_class_name => { is => 'VARCHAR2', len => 255 },
+        subject_id         => { is => 'VARCHAR2', len => 255 },
+        subject            => { is => 'UR::Object', id_class_by => 'subject_class_name', id_by => 'subject_id' },
         editor_id          => { is => 'VARCHAR2', len => 200 },
         entry_date         => { is => 'DATE' },
         header_text        => { is => 'VARCHAR2', len => 200 },
-        subject_class_name => { is => 'VARCHAR2', len => 255 },
-        subject_id         => { is => 'VARCHAR2', len => 255 },
+    ],
+    has_optional => [
+        body_text          => { is => 'VARCHAR2', len => 1000 },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
 };
+
+sub create {
+    my $class = shift;
+    my $self = $class->SUPER::create(@_);
+    return unless $self;
+        
+    unless ($self->entry_date) {
+        $self->entry_date(UR::Time->now);
+    }
+
+    unless ($self->editor_id) {
+        $self->editor_id(Genome::Config->current_user_id);
+    }
+
+    return $self;
+}
 
 1;
