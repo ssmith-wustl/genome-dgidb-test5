c16b5e50bfc168e7233ba460311a1bab4a4481c2
ebelter <ebelter@genome.wustl.edu>
2009-10-28 19:40:17 +0000

    model input command line interface
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@52389 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Input.pm
+++ Genome/Model/Input.pm
@@ -8,10 +8,9 @@ use Genome;
 class Genome::Model::Input {
     type_name => 'genome model input',
     table_name => 'GENOME_MODEL_INPUT',
-    subclassify_by => 'input_class_name',
     id_by => [
-    input_class_name => { is => 'VARCHAR2', len => 255 },
-    input_id         => { is => 'VARCHAR2', len => 1000 },
+    value_class_name => { is => 'VARCHAR2', len => 255, column_name => 'INPUT_CLASS_NAME', },
+    value_id         => { is => 'VARCHAR2', len => 1000, column_name => 'INPUT_ID' },
     model_id         => { is => 'NUMBER', len => 11 },
     name             => { is => 'VARCHAR2', len => 255, },
     ],
@@ -21,10 +20,38 @@ class Genome::Model::Input {
         id_by => 'model_id',
         constraint_name => 'GMI_GM_FK',
     },
+    model_name => {
+        via => 'model',
+        to => 'name',
+    },
+    value => {
+        is => 'UR::Object',
+        id_by => 'value_id',
+        id_class_by => 'value_class_name',
+    },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
 };
+
+sub __create {
+    my ($class, %params) = @_;
+
+    #FIXME Can't get to work in UR...auto set the class name when using primitive 
+    
+    unless ( $params{value} ) {
+        unless ( defined $params{value_id} ) {
+            $class->error_message("No value or value id given.");
+            return;
+        }
+
+        unless ( $params{value_class_name} ) {
+            $params{value_class_name} = 'UR::Value';
+        }
+    }
+    
+    return $class->SUPER::create(%params);
+}
 
 1;
 
