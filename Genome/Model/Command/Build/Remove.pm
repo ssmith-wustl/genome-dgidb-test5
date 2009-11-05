eafb812890b5b9f6ec12a39c883cc13ab000d3f8
josborne <josborne@genome.wustl.edu>
2009-11-05 21:08:15 +0000

    genome model build remove now tries to kill running builds that you remove
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@52691 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Remove.pm
+++ Genome/Model/Command/Build/Remove.pm
@@ -39,6 +39,25 @@ sub execute {
         $self->error_message('Build not found for model id '. $self->model_id .' and build id '. $self->build_id);
         return;
     }
+
+    # cribbed from genome model build abandon - kills running jobs
+    # if any events are running
+    my @events = $build->build_events;
+    foreach my $event (@events) {
+        if($event->event_status =~ /Running/)
+        {
+            my $rv = eval { $build->abandon; };
+            unless ($rv and not $@) {
+                $self->error_message(
+                    'Failed to abandon build '
+                    . $self->build_id 
+                    . ($@ ? " ERRORS: $@" : "")
+                    );
+                return;
+            }
+            last;
+        }
+    }
     unless ($build->delete) {
         $self->error_message('Failed to remove build '. $self->build_id);
         return;
