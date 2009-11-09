71dc12403f58d56d18b3b20d13b690ae48dd6843
gsanders <gsanders@genome.wustl.edu>
2009-11-09 22:39:24 +0000

    first draft tool to upload validation for variants from the somatic pipeline.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@52748 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Somatic/UploadVariantValidation.pm
@@ -0,0 +1,99 @@
+package Genome::Model::Tools::Somatic::UploadVariantValidation;
+
+use strict;
+use warnings;
+use Genome;
+use Genome::Info::IUB;
+
+class Genome::Model::Tools::Somatic::UploadVariantValidation{
+    is => 'Command',
+    has => [
+    variant_file => {
+        is  => 'String',
+        doc => 'The file of somatic pipeline results to be uploaded. This will usually be a high confidence tier 1 or 2 snp file, or a tier 1 indel file from the somatic pipeline.',
+    },
+    model_id => {
+        is => 'Number',
+        doc => 'The model id that should be linked to the variant validation. This is manual for now and required.',
+    },
+    
+    ],
+};
+
+sub help_brief {
+    "Adds validation results to the variations from the somatic pipeline.",
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+    genome-model tools somatic upload-variant-validation --variant-file high_confidence_file.out --model-id 12345
+EOS
+}
+
+sub help_detail {                           
+    return <<EOS 
+Adds validation results to the variations from the somatic pipeline.
+EOS
+}
+
+sub execute {
+    my $self = shift;
+      
+    my $variant_fh = IO::File->new($self->variant_file);
+    unless ($variant_fh) {
+        $self->error_message("Could not open variant file: " . $self->variant_file . " for reading. $!");
+        die;
+    }
+
+    my $model = Genome::Model->get($self->model_id);
+    unless ($model) {
+        $self->error_message("Model does not exist for " . $self->model_id . " please use a valid model.");
+        die;
+    }
+
+    # Go through each line in the variant file and get each annotation line that matches from the annotation file
+    # For each line, print it to the output file and upload it to the database
+    while (my $line = $variant_fh->getline) {
+        my ($chr, $start, $stop, $reference, $variant, $result) = split "\t", $line;
+        
+
+        my $variant_already_exists = Genome::Model::Variant->get(
+            chromosome       => $chr,
+            start_pos        => $start,
+            stop_pos         => $stop,
+            reference_allele => $reference,
+            variant_allele   => $variant
+        );
+        unless ($variant_already_exists) {
+            $self->error_message("Variant does not exist for $chr $start $stop $reference $variant... variants should be already uploaded for validation data.");
+            die;
+        }
+
+        # Get the existing "Official" call and change it to the result we have from this validation type
+        # TODO: In the future... we might have multiple validation types so we may not always want to replace the official call without further investigation
+        my $overall_validation = Genome::Model::VariantValidation->get_or_create(
+            variant           => $variant_already_exists,
+            validation_type   => 'Official',
+            model_id          => $model,
+        );
+        $overall_validation->validation_result($result);
+
+        # Replace any old validation for this type
+        my $type_validation = Genome::Model::VariantValidation->get_or_create(
+            variant           => $variant_already_exists,
+            validation_type   => $self->validation_type,
+            model_id          => $model,
+        );
+        $type_validation->validation_result($result);
+
+        unless($overall_validation && $type_validation) {
+            $self->error_message("Unable to create overall validation OR type validation");
+            $self->error_message("Problem line: $line");
+            die;
+        }
+    }
+
+    return 1;
+
+}
