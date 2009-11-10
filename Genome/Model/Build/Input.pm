ea7b8e524f77252226243580accb270eea0f037e
ebelter <ebelter@genome.wustl.edu>
2009-11-10 22:29:51 +0000

    added inputs to models and builds; also copying inst data assgns as inputs
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@52815 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build/Input.pm
+++ Genome/Model/Build/Input.pm
@@ -4,26 +4,23 @@ use strict;
 use warnings;
 
 use Genome;
-class Genome::Model::BuildInput {
+class Genome::Model::Build::Input {
     type_name => 'genome model build input',
     table_name => 'GENOME_MODEL_BUILD_INPUT',
-    subclassify_by => 'input_class_name',
     id_by => [
-        build_id         => { is => 'NUMBER', len => 11 },
+        build_id         => { is => 'NUMBER', len => 11, implied_by => 'build' },
         value_class_name => { is => 'VARCHAR2', len => 255 },
-        value_id         => { is => 'VARCHAR2', len => 1000 },
-        name             => { is => 'VARCHAR2', len => 255, },
+        value_id         => { is => 'VARCHAR2', len => 1000, implied_by => 'value' },
+        name             => { is => 'VARCHAR2', len => 255 },
     ],
     has => [
-    model => {
-        is => 'Genome::Model',
-        via => 'build',
-    },
-    build => { 
-        is => 'Genome::Model::Build', 
-        id_by => 'build_id',
-        constraint_name => 'GMBI_GMB_FK',
-    },
+        model      => { is => 'Genome::Model', via => 'build' },
+        model_name => { via => 'model', to => 'name' },
+        build      => { is => 'Genome::Model::Build', id_by => 'build_id', constraint_name => 'GMBI_GMB_FK' },
+        value      => { is => 'UR::Object', id_class_by => 'value_class_name', id_by => 'value_id' },
+    ],
+    unique_constraints => [
+        { properties => [qw/build_id name value_class_name value_id/], sql => 'GMBI_PK' },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
