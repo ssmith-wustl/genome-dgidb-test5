0191e0095a0b86aad930e9aa613ba342008d48a5
ebelter <ebelter@genome.wustl.edu>
2009-11-10 22:30:17 +0000

    added inputs to models and builds; also copying inst data assgns as inputs
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@52819 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Input.pm
+++ Genome/Model/Input.pm
@@ -9,49 +9,19 @@ class Genome::Model::Input {
     type_name => 'genome model input',
     table_name => 'GENOME_MODEL_INPUT',
     id_by => [
-    value_class_name => { is => 'VARCHAR2', len => 255, },
-    value_id         => { is => 'VARCHAR2', len => 1000, },
-    model_id         => { is => 'NUMBER', len => 11, },
-    name             => { is => 'VARCHAR2', len => 255, },
+        value_class_name => { is => 'VARCHAR2', len => 255 },
+        value_id         => { is => 'VARCHAR2', len => 1000, implied_by => 'value' },
+        model_id         => { is => 'NUMBER', len => 11, implied_by => 'model' },
+        name             => { is => 'VARCHAR2', len => 255 },
     ],
     has => [
-    model => { 
-        is => 'Genome::Model',
-        id_by => 'model_id',
-        constraint_name => 'GMI_GM_FK',
-    },
-    model_name => {
-        via => 'model',
-        to => 'name',
-    },
-    value => {
-        is => 'UR::Object',
-        id_by => 'value_id',
-        id_class_by => 'value_class_name',
-    },
+        model      => { is => 'Genome::Model', id_by => 'model_id', constraint_name => 'GMI_GM_FK' },
+        model_name => { via => 'model', to => 'name' },
+        value      => { is => 'UR::Object', id_class_by => 'value_class_name', id_by => 'value_id' },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
 };
-
-sub __create {
-    my ($class, %params) = @_;
-
-    #FIXME Can't get to work in UR...auto set the class name when using primitive 
-    
-    unless ( $params{value} ) {
-        unless ( defined $params{value_id} ) {
-            $class->error_message("No value or value id given.");
-            return;
-        }
-
-        unless ( $params{value_class_name} ) {
-            $params{value_class_name} = 'UR::Value';
-        }
-    }
-    
-    return $class->SUPER::create(%params);
-}
 
 1;
 
