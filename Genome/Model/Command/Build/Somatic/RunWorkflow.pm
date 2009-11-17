62ecd2df41693bca2e0901f4fcb3e197b1fdb050
gsanders <gsanders@genome.wustl.edu>
2009-11-17 21:10:00 +0000

    added review comments
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53082 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Somatic/RunWorkflow.pm
+++ Genome/Model/Command/Build/Somatic/RunWorkflow.pm
@@ -1,3 +1,9 @@
+# Review gsanders jlolofie
+# The main think of here is adding some progrommatic way to pass every processing profile attribute to the workflow.
+# Currently when a param is added to the workflow, I need to go add that as a class property to the processing profile,
+# and then add it to this module to pass it to the workflow.
+# Also should we check the workflow->execute return code at the end? I think we should. Lets talk to eclark about this.
+
 package Genome::Model::Command::Build::Somatic::RunWorkflow;
 
 use strict;
