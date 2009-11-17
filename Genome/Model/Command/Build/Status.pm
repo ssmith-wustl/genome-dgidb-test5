f3dee4d28197c7e21e0af8f0a49183c4a2d77111
boberkfe <boberkfe@genome.wustl.edu>
2009-11-17 17:09:52 +0000

    review comments
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53039 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Status.pm
+++ Genome/Model/Command/Build/Status.pm
@@ -1,3 +1,7 @@
+#:boberkfe this looks like a good place to use memcache to cache up some build status.
+#:boberkfe when build events update, stuff their status into memcache.  gathering info otherwise
+#:boberkfe can get reaaaaal slow.
+
 package Genome::Model::Command::Build::Status;
 
 use strict;
