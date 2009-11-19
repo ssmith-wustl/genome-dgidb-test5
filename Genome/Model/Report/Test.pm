08f652d4bb4f0421160bdabed6eaa4539bbb2f0d
ebelter <ebelter@genome.wustl.edu>
2009-11-19 20:46:15 +0000

    moved verify successful completion code to build; added more robust error checking to Run; create build error to handle wf errors and directly created errors; updated failed report to use the build errors
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53238 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Report/Test.pm
+++ Genome/Model/Report/Test.pm
@@ -69,7 +69,7 @@ sub test01_generate_report : Tests() {
     my $report = $generator->generate_report;
     ok($report, 'Generated report');
 
-    if ( 0 ) { # Email/save yourself the report if ya wanna
+    if ( 1 ) { # Email/save yourself the report if ya wanna
         #$report->save('/gscuser/ebelter', 1);
         my $email = Genome::Report::Email->send_report(
             report => $report,
@@ -185,113 +185,24 @@ sub _additional_params_for_test_class {
 sub _error_objects {
     my $self = shift;
     
-    my $error1 = Test::MockObject->new();
-    $error1->set_isa('Workflow::Operation::InstanceExecution::Error');
-    $self->_set_mock_attrs( 
-        $error1, {
-            # BUILD_EVEVNT_ID STAGE
-            name => '97901036 assemble',
-            # BUILD_EVEVNT_ID STAGE
-            path_name => '97901036 all stages',
-            _build_path_string => '97901036 all stages',
-            # LSF_JOB_ID maybe undef
-            dispatch_identifier => undef,
-            # ERROR_STRING
-            error => <<EOS,
-Execution halted due to unresolvable dependencies or crashed children.  Status and incomplete inputs:
-input connector <591875> (done)
-output connector <591876> (new)
-  result
-verify-instrument-data 97901037 <591877> (done)
-prepare-instrument-data 97901038 <591878> (done)
-trim-and-screen 97901039 <591879> (crashed)
-assemble 97901040 <591880> (new)
-  prior_result
-classify 97901041 <591881> (new)
-  prior_result
-orient 97901042 <591882> (new)
-  prior_result
-collate 97901043 <591883> (new)
-  prior_result
-clean-up 97901044 <591884> (new)
-  prior_result
-reports 97901045 <591885> (new)
-  prior_result
-merge results <591886> (new)
-  result_1
-EOS
-            ,
-            start_time => UR::Time->now,
-            end_time => UR::Time->now,
-        },
-    );
-
-    my $error2 = Test::MockObject->new();
-    #$error2->set_isa('Workflow::Operation::InstanceExecution::Error');
-    $self->_set_mock_attrs( 
-        $error2, {
-            # STEP EVENT_ID  
-            name => 'trim-and-screen 97901039',
-            # BUILD_EVEVNT_ID STAGE/STEP EVENT_ID  
-            path_name => '97901036 all stages/97901036 assemble',
-            _build_path_string=> '97901036 all stages/97901036 assemble',
-            # LSF_JOB_ID maybe undef
-            dispatch_identifier => 1000001,
-            # ERROR_STRING
-            error => 'Command module returned undef',
-            start_time => UR::Time->now,
-            end_time => UR::Time->now,
-        },
-    );
-
-    my $error3 = Test::MockObject->new();
-    #$error3->set_isa('Workflow::Operation::InstanceExecution::Error');
-    $self->_set_mock_attrs( 
-        $error3, {
-            # STEP EVENT_ID  
-            name => 'trim-and-screen 97901040',
-            # BUILD_EVEVNT_ID STAGE/STEP EVENT_ID  
-            path_name => '97901036 all stages/97901036 assemble/trim-and-screen 97901040',
-            _build_path_string=> '97901036 all stages/97901036 assemble/trim-and-screen 97901040',
-            # LSF_JOB_ID maybe undef
-            dispatch_identifier => 1000002,
-            # ERROR_STRING
-            error => 'A really long error message to see if wrapping the text of this error looks good in the report that is generated for users to see why their build failed and what happened to cause it to fail.',
-            start_time => UR::Time->now,
-            end_time => UR::Time->now,
-        },
-    );
-
-    my $error4 = Test::MockObject->new();
-    #$error4->set_isa('Workflow::Operation::InstanceExecution::Error');
-    $self->_set_mock_attrs( 
-        $error4, {
-            # STEP EVENT_ID  
-            name => 'trim-and-screen 97901041',
-            # BUILD_EVEVNT_ID STAGE/STEP EVENT_ID  
-            path_name => '97901036 all stages/97901036 assemble/trim-and-screen 97901041',
-            _build_path_string=> '97901036 all stages/97901036 assemble/trim-and-screen 97901041',
-            # LSF_JOB_ID maybe undef
-            dispatch_identifier => 1000003,
-            # ERROR_STRING
-            error => 'Can\'t call method "run" on undefined value',
-            start_time => UR::Time->now,
-            end_time => UR::Time->now,
-        },
-    );
-
-    return [ $error1, $error2, $error3, $error4 ];
-}
-
-sub _set_mock_attrs {
-    my ($self, $obj, $params) = @_;
-
-    for my $attr ( keys %$params ) {
-        #$obj->set_always($attr, $params->{$attr});
-        $obj->mock($attr, sub { return $params->{$attr}; });
+    # Doing 2 errors to see in report
+    my @errors;
+    for (1..2) {
+        my $error = $self->create_mock_object(
+            class => 'Genome::Model::Build::Error',
+            'build_event_id' => '97901036',
+            'stage_event_id' => '97901036',
+            'stage' => 'assemble',
+            'step' => 'trim-and-screen',
+            'step_event_id' => '97901040',
+            'error' => 'A really long error message to see if wrapping the text of this error looks good in the report that is generated for users to see why their build failed and what happened to cause it to fail.',
+        );
+        $self->mock_methods($error, 'error_wrapped');
+        $error->set_always('error_log', '/somewhere/a_log/file.err');
+        push @errors, $error;
     }
 
-    return 1;
+    return \@errors;
 }
 
 #############################################################
