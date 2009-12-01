961241a8b514a2304e9c2aff1f511700e23c02c0
ndees <ndees@genome.wustl.edu>
2009-12-01 19:23:22 +0000

    First iteration of germline tool to find snps from filtered.indelpe.snps which are not in dbsnp.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53469 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Germline/PrivateSnps.pm
+++ Genome/Model/Tools/Germline/PrivateSnps.pm
@@ -40,12 +40,17 @@ sub execute {
         $self->error_message("Unable to resolve a build object for build id: " . $self->build_id);
         return 0;
     }
-    my $db_snp_path = $self->build->resolve_reports_directory() ."/". $self->build->model_id . "variant_filtered_list_files.dbsnp";
+    my $db_snp_file = $self->build->resolve_reports_directory() ."/". $self->build->model_id . "variant_filtered_list_files.dbsnp";
 
     my $filtered_snp_file = $self->build->filtered_snp_file;
 
-    print $filtered_snp_file . "\n";
-    print $db_snp_path . "\n";
+    #print $filtered_snp_file . "\n";
+    #print $db_snp_file . "\n";
+
+    my $output_file = $self->output_file;
+    my $intersect = Genome::Model::Tools::Snp::IntersectChromPos -> create( file1=>$filtered_snp_file, file2=>$db_snp_file, f1_only_output=>$output_file, f2_only_output=>"/dev/null", intersect_output=>"/dev/null", headers2=>1 );
+    $intersect->execute;
+        
 }
     
         
