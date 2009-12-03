00ccbdbb0ffcde49197c60246eb76593c6281fe9
ssmith <ssmith@genome.wustl.edu>
2009-12-03 01:58:01 +0000

    Updated docs.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53546 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build/Command/List.pm
+++ Genome/Model/Build/Command/List.pm
@@ -18,5 +18,29 @@ class Genome::Model::Build::Command::List {
 
 sub sub_command_sort_position { 1 }
 
+sub help_synopsis {
+    my $self = shift;
+    my $syn = $self->SUPER::help_synopsis(@_);
+    $syn .= <<EOS;
+  # given model 123 named "mymodel" has builds 456 and 789:
+
+  # list the first build
+  genome model build list 456
+
+  # list the second build
+  genome model build list 789
+
+  # list all builds for the model by model ID
+  genome model build list 123
+
+  # list all builds for the model by model name
+  genome model build list mymodel
+
+  # or use standard filters
+  genome model build list --filter status=Abandoned,data_directory~/gscmnt/839% --show id,subject_name,data_directory
+EOS
+    return $syn;
+}
+
 1;
 
