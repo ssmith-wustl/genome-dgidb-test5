2b3a581b2fdb6082317a3b0630c7c1d6bfd84ea5
tmooney <tmooney@genome.wustl.edu>
2009-12-08 21:57:54 +0000

    Change how statistics are calculated to (1) take into account potentially differing forward and reverse read_length and (2) find values by directly examining BAM files rather than using maths.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53656 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Maq.pm
+++ Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Maq.pm
@@ -53,18 +53,71 @@ sub metrics_for_class {
 
     my @metric_names = qw(
                           total_read_count
+                          fwd_reads_passed_quality_filter_count
+                          rev_reads_passed_quality_filter_count
                           total_reads_passed_quality_filter_count
                           total_bases_passed_quality_filter_count
+                          fwd_poorly_aligned_read_count
+                          rev_poorly_aligned_read_count
                           poorly_aligned_read_count
                           contaminated_read_count
+                          fwd_aligned_read_count
+                          rev_aligned_read_count
                           aligned_read_count
                           aligned_base_pair_count
+                          fwd_unaligned_read_count
+                          rev_unaligned_read_count
                           unaligned_read_count
                           unaligned_base_pair_count
                           total_base_pair_count
     );
 
     return @metric_names;
+}
+
+sub fwd_reads_passed_quality_filter_count {
+    my $self = shift;
+    return $self->get_metric_value('fwd_reads_passed_quality_filter_count');
+}
+
+sub _calculate_fwd_reads_passed_quality_filter_count {
+    my $self = shift;
+    my $fwd_reads_passed_quality_filter_count;
+    
+    unless($self->instrument_data->is_paired_end) {
+        $fwd_reads_passed_quality_filter_count = 0;
+    } else {
+        my ($fwd_fastq) = $self->instrument_data->fastq_filenames(paired_end_as_fragment => 1);
+        
+        unless(-f $fwd_fastq) {
+            $self->error_message("Problem calculating metric (didn't find forward FASTQ)...this doesn't mean the step failed");
+            return 0;
+        }
+        $fwd_reads_passed_quality_filter_count = $self->_count_reads_in_fastq_files($fwd_fastq);
+    }
+    return $fwd_reads_passed_quality_filter_count;
+}
+
+sub rev_reads_passed_quality_filter_count {
+    my $self = shift;
+    return $self->get_metric_value('rev_reads_passed_quality_filter_count');
+}
+
+sub _calculate_rev_reads_passed_quality_filter_count {
+    my $self = shift;
+    my $rev_reads_passed_quality_filter_count;
+    
+    unless($self->instrument_data->is_paired_end) {
+        $rev_reads_passed_quality_filter_count = 0;
+    } else {
+        my ($rev_fastq) = $self->instrument_data->fastq_filenames(paired_end_as_fragment => 2);
+        unless(-f $rev_fastq) {
+            $self->error_message("Problem calculating metric (didn't find reverse FASTQ)...this doesn't mean the step failed");
+            return 0;
+        }
+        $rev_reads_passed_quality_filter_count = $self->_count_reads_in_fastq_files($rev_fastq);
+    }
+    return $rev_reads_passed_quality_filter_count;
 }
 
 sub total_reads_passed_quality_filter_count {
@@ -75,24 +128,18 @@ sub total_reads_passed_quality_filter_count {
 sub _calculate_total_reads_passed_quality_filter_count {
     my $self = shift;
     my $total_reads_passed_quality_filter_count;
-    do {
-        no warnings;
 
-        unless ($total_reads_passed_quality_filter_count) {
-            my @f = grep {-f $_ } $self->instrument_data->fastq_filenames;
-            unless (@f) {
-                $self->error_message("Problem calculating metric...this doesn't mean the step failed");
-                return;
-            }
-            my ($wc) = grep { /total/ } `wc -l @f`;
-            $wc =~ s/total//;
-            $wc =~ s/\s//g;
-            if ($wc % 4) {
-                warn "run $a->{id} has a line count of $wc, which is not divisible by four!"
-            }
-            $total_reads_passed_quality_filter_count = $wc/4;
+    if($self->instrument_data->is_paired_end) {
+        $total_reads_passed_quality_filter_count = $self->fwd_reads_passed_quality_filter_count + $self->rev_reads_passed_quality_filter_count;
+    } else {
+        my @f = grep {-f $_ } $self->instrument_data->fastq_filenames;
+        unless (@f) {
+            $self->error_message("Problem calculating metric (didn't find FASTQ(s))...this doesn't mean the step failed");
+            return 0;
         }
-    };
+        $total_reads_passed_quality_filter_count = $self->_count_reads_in_fastq_files(@f);
+    }
+
     return $total_reads_passed_quality_filter_count;
 }
 
@@ -104,10 +151,58 @@ sub total_bases_passed_quality_filter_count {
 sub _calculate_total_bases_passed_quality_filter_count {
     my $self = shift;
 
-    # total_reads_passed_quality_filter_count might return "Not Found"
-    no warnings 'numeric';
-    my $total_bases_passed_quality_filter_count = $self->total_reads_passed_quality_filter_count * $self->instrument_data_assignment->read_length;
+    my $total_bases_passed_quality_filter_count;
+    if($self->instrument_data->is_paired_end) {
+        $total_bases_passed_quality_filter_count =
+            $self->fwd_reads_passed_quality_filter_count * $self->instrument_data->fwd_read_length +
+            $self->rev_reads_passed_quality_filter_count * $self->instrument_data->rev_read_length;
+        
+    } else {
+        $total_bases_passed_quality_filter_count = $self->total_reads_passed_quality_filter_count * $self->instrument_data->read_length;
+    }
     return $total_bases_passed_quality_filter_count;
+}
+
+sub fwd_poorly_aligned_read_count {
+    my $self = shift();
+    
+    return $self->get_metric_value('fwd_poorly_aligned_read_count');
+}
+
+sub _calculate_fwd_poorly_aligned_read_count {
+    my $self = shift();
+    my $fwd_poorly_aligned_read_count;
+    
+    unless($self->instrument_data->is_paired_end) {
+        $fwd_poorly_aligned_read_count = 0;
+    } else {
+        my $include_flags = 0x40 | 0x4; #is first read & unmapped read (note: also use 0x8 if need to check corresponding rev read is unmapped)
+        
+        $fwd_poorly_aligned_read_count = $self->_count_lines_in_bam_file(include_flags => $include_flags);
+    }
+    
+    return $fwd_poorly_aligned_read_count;
+}
+
+sub rev_poorly_aligned_read_count {
+    my $self = shift();
+    
+    return $self->get_metric_value('rev_poorly_aligned_read_count');
+}
+
+sub _calculate_rev_poorly_aligned_read_count {
+    my $self = shift();
+    my $rev_poorly_aligned_read_count;
+    
+    unless($self->instrument_data->is_paired_end) {
+        $rev_poorly_aligned_read_count = 0;
+    } else {
+        my $include_flags = 0x80 | 0x4; #is second read & unmapped read (note: also use 0x8 if need to check corresponding fwd read is unmapped)
+        
+        $rev_poorly_aligned_read_count = $self->_count_lines_in_bam_file(include_flags => $include_flags);
+    }
+    
+    return $rev_poorly_aligned_read_count;
 }
 
 sub poorly_aligned_read_count {
@@ -118,21 +213,16 @@ sub poorly_aligned_read_count {
 
 sub _calculate_poorly_aligned_read_count {
     my $self = shift;
-
-    #unless ($self->should_calculate) {
-    #    return 0;
-    #}
-    my $instrument_data_assignment = $self->instrument_data_assignment;
-    my $alignment = $instrument_data_assignment->alignment;
-    my $total = 0;
-    for my $f ($alignment->unaligned_reads_list_paths) {
-        my $fh = IO::File->new($f);
-        $fh or die "Failed to open $f to read.  Error returning value for poorly_aligned_read_count.\n";
-        while (my $row = $fh->getline) {
-            $total++
-        }
+    
+    my $poorly_aligned_read_count;
+    if($self->instrument_data->is_paired_end) {
+        $poorly_aligned_read_count = $self->fwd_poorly_aligned_read_count + $self->rev_poorly_aligned_read_count;
+    } else {
+        my $include_flags = 0x4; #is unmapped read
+        
+        $poorly_aligned_read_count = $self->_count_lines_in_bam_file(include_flags => $include_flags);
     }
-    return $total;
+    return $poorly_aligned_read_count;
 }
 
 sub contaminated_read_count {
@@ -142,11 +232,13 @@ sub contaminated_read_count {
 
 sub _calculate_contaminated_read_count {
     my $self = shift;
-
+ 
     my $instrument_data_assignment = $self->instrument_data_assignment;
-    my $alignment = $instrument_data_assignment->alignment;
+    my $alignment = $instrument_data_assignment->alignment_set;
     my @f = $alignment->aligner_output_file_paths;
-    my $total = 0;
+    @f = grep($_ !~ 'sanitized', @f);
+    
+    my $contaminated_read_count = 0;
     for my $f (@f) {
         my $fh = IO::File->new($f);
         $fh or die "Failed to open $f to read.  Error returning value for contaminated_read_count.\n";
@@ -161,9 +253,48 @@ sub _calculate_contaminated_read_count {
             #$self->warning_message("No adaptor information found in $f!");
             next;
         }
-        $total += $n;
+        $contaminated_read_count += $n;
     }
-    return $total;
+    
+    return $contaminated_read_count;
+}
+
+sub fwd_aligned_read_count {
+    my $self = shift;
+    return $self->get_metric_value('fwd_aligned_read_count');
+}
+
+sub _calculate_fwd_aligned_read_count {
+    my $self = shift;
+    my $fwd_aligned_read_count;
+    unless($self->instrument_data->is_paired_end) {
+        $fwd_aligned_read_count = 0;
+    } else {
+        my $exclude_flags = 0x4; #unmapped read
+        my $include_flags = 0x40; #is first read
+        
+        $fwd_aligned_read_count = $self->_count_lines_in_bam_file(include_flags => $include_flags, exclude_flags => $exclude_flags);
+    }
+    return $fwd_aligned_read_count;
+}
+
+sub rev_aligned_read_count {
+    my $self = shift;
+    return $self->get_metric_value('rev_aligned_read_count');
+}
+
+sub _calculate_rev_aligned_read_count {
+    my $self = shift;
+    my $rev_aligned_read_count;
+    unless($self->instrument_data->is_paired_end) {
+        $rev_aligned_read_count = 0;
+    } else {
+        my $exclude_flags = 0x4; #unmapped read
+        my $include_flags = 0x80; #is second read
+        
+        $rev_aligned_read_count = $self->_count_lines_in_bam_file(include_flags => $include_flags, exclude_flags => $exclude_flags); 
+    }
+    return $rev_aligned_read_count;
 }
 
 sub aligned_read_count {
@@ -173,9 +304,15 @@ sub aligned_read_count {
 
 sub _calculate_aligned_read_count {
     my $self = shift;
-    no warnings 'numeric';
-    # total_reads_passed_quality_filter_count might return "Not Found"
-    my $aligned_read_count = $self->total_reads_passed_quality_filter_count - $self->poorly_aligned_read_count - $self->contaminated_read_count;
+
+    my $aligned_read_count;
+    if($self->instrument_data->is_paired_end) {
+        $aligned_read_count = $self->fwd_aligned_read_count + $self->rev_aligned_read_count;
+    } else {
+        my $exclude_flags = 0x4; #unmapped read
+        
+        $aligned_read_count = $self->_count_lines_in_bam_file(exclude_flags => $exclude_flags);
+    }
     return $aligned_read_count;
 }
 
@@ -187,9 +324,39 @@ sub aligned_base_pair_count {
 sub _calculate_aligned_base_pair_count {
     my $self = shift;
 
-    my $aligned_base_pair_count = $self->aligned_read_count * $self->instrument_data_assignment->read_length;
+    my $aligned_base_pair_count;
+    if($self->instrument_data->is_paired_end) {
+        $aligned_base_pair_count =
+            $self->fwd_aligned_read_count * $self->instrument_data->fwd_read_length +
+            $self->rev_aligned_read_count * $self->instrument_data->rev_read_length;
+    } else {
+        $aligned_base_pair_count = $self->aligned_read_count * $self->instrument_data->read_length;
+    }
     return $aligned_base_pair_count;
 }
+
+sub fwd_unaligned_read_count {
+    my $self = shift;
+    return $self->get_metric_value('fwd_unaligned_read_count');
+}
+
+sub _calculate_fwd_unaligned_read_count {
+    my $self = shift;
+    my $fwd_unaligned_read_count = $self->fwd_poorly_aligned_read_count;
+    return $fwd_unaligned_read_count;
+}
+
+sub rev_unaligned_read_count {
+    my $self = shift;
+    return $self->get_metric_value('rev_unaligned_read_count');
+}
+
+sub _calculate_rev_unaligned_read_count {
+    my $self = shift;
+    my $rev_unaligned_read_count = $self->rev_poorly_aligned_read_count;
+    return $rev_unaligned_read_count;
+}
+
 sub unaligned_read_count {
     my $self = shift;
     return $self->get_metric_value('unaligned_read_count');
@@ -197,7 +364,12 @@ sub unaligned_read_count {
 
 sub _calculate_unaligned_read_count {
     my $self = shift;
-    my $unaligned_read_count = $self->poorly_aligned_read_count + $self->contaminated_read_count;
+    my $unaligned_read_count;
+    if($self->instrument_data->is_paired_end) {
+        $unaligned_read_count = $self->fwd_unaligned_read_count + $self->rev_unaligned_read_count;
+    } else {
+        $unaligned_read_count = $self->poorly_aligned_read_count;
+    }
     return $unaligned_read_count;
 }
 
@@ -208,7 +380,16 @@ sub unaligned_base_pair_count {
 
 sub _calculate_unaligned_base_pair_count {
     my $self = shift;
-    my $unaligned_base_pair_count = $self->unaligned_read_count * $self->instrument_data_assignment->read_length;
+    
+    my $unaligned_base_pair_count;
+    if($self->instrument_data->is_paired_end) {        
+        $unaligned_base_pair_count =
+            $self->fwd_unaligned_read_count * $self->instrument_data->fwd_read_length +
+            $self->rev_unaligned_read_count * $self->instrument_data->rev_read_length;
+    } else {    
+        $unaligned_base_pair_count = $self->unaligned_read_count * $self->instrument_data->read_length;
+    }
+    
     return $unaligned_base_pair_count;
 }
 
@@ -220,8 +401,66 @@ sub total_base_pair_count {
 sub _calculate_total_base_pair_count {
     my $self = shift;
 
-    my $total_base_pair_count = $self->total_read_count * $self->instrument_data_assignment->read_length;
+    my $total_base_pair_count = $self->instrument_data->total_bases_read;
     return $total_base_pair_count;
+}
+
+sub _count_reads_in_fastq_files {
+    my $self = shift;
+    my @fastq_files = @_;
+    
+    my @wcs = `wc -l @fastq_files`;
+    
+    unless(@wcs) {
+        $self->error_message('Unable to count reads in FASTQ files (No output from `wc` returned.): ' . join(', ', @fastq_files));
+        return;
+    }
+
+    my $wc;
+    if(scalar @wcs > 1) {
+        ($wc) = grep { /total/ } @wcs;
+        $wc =~ s/total//;
+        $wc =~ s/\s//g;
+    } else {
+        ($wc) = $wcs[0] =~ /^(\d+)/; #no total printed on one input, just grab count
+    }
+
+    if ($wc % 4) {
+        $self->warning_message("run has a line count of $wc, which is not divisible by four!");
+    }
+    my $total_reads_in_fastq_files = $wc/4; #four lines per read in FASTQ file format
+    
+    return $total_reads_in_fastq_files;
+}
+
+sub _count_lines_in_bam_file {
+    my $self = shift;
+    my %options = @_;
+    
+    my $alignment = $self->instrument_data_assignment->alignment_set;
+    
+    my $cmd = Genome::Model::Tools::Sam->path_for_samtools_version($alignment->samtools_version);
+    $cmd .= ' view ';
+    
+    if($options{include_flags}) {
+        $cmd .= '-f ' . $options{include_flags} . ' ';
+    }
+    if($options{exclude_flags}) {
+        $cmd .= '-F ' . $options{exclude_flags} . ' ';
+    }
+    
+    $cmd .= $alignment->alignment_file;
+    $cmd .= ' | wc -l';
+    
+    my $result = `$cmd`;
+    my ($wc) = $result =~ /^(\d+)/;
+    
+    unless($wc) {
+        $self->error_message('Unable to count reads in BAM file (No output from `wc` returned.)');
+        return;
+    }
+    
+    return $wc;
 }
 
 sub execute {
@@ -264,8 +503,6 @@ sub execute {
     return 1;
 }
 
-
-
 sub verify_successful_completion {
     my $self = shift;
 
@@ -290,6 +527,4 @@ sub verify_successful_completion {
     return 1;
 }
 
-
 1;
-
--- Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Maq.t
+++ Genome/Model/Command/Build/ReferenceAlignment/AlignReads/Maq.t
@@ -10,7 +10,20 @@ my $event = Genome::Model::Event->get($event_id);
 ok($event, "got an event");
 
 #this is time consuming. comment out for autorun.
-#my @m = $event->generate_metrics();
-#is(scalar(@m), 6, "got metrics");
-
-
+#my @metrics = $event->generate_metric();
+#is(scalar(@metrics), 18, "got metrics");
+#
+#my %metrics;
+#for my $metric (@metrics) {
+#    $metrics{$metric->name} = $metric;
+#}
+#
+#{
+#    skip "won't test fwd/rev counts using fragment data", 2 unless $event->instrument_data->is_paired_end;
+#
+#    is($metrics{'fwd_aligned_read_count'} + $metrics{'rev_aligned_read_count'}, $metrics{'aligned_read_count'}, 'fwd/rev aligned read counts sum to overall aligned');
+#    is($metrics{'fwd_unaligned_read_count'} + $metrics{'rev_unaligned_read_count'}, $metrics{'unaligned_read_count'}, 'fwd/rev unaligned read counts sum to overall unaligned');
+#    is($metrics{'fwd_reads_passed_quality_filter_count'} + $metrics{'rev_reads_passed_quality_filter_count'}, $metrics{'total_reads_passed_quality_filter_count'}, 'fwd/rev read passed filter counts sum to total reads passed filter');
+#}
+#
+#is($metrics{'unaligned_read_count'} + $metrics{'aligned_read_count'}, $metrics{'total_reads_passed_quality_filter_count'}, 'unaligned and aligned read counts sum to total reads passed filter');
