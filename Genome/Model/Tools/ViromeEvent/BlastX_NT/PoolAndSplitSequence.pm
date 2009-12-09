dafb9166ebc401ce92c4774ee62902a5e43f9e44
kkyung <kkyung@genome.wustl.edu>
2009-12-09 20:42:37 +0000

    Re-writes and updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53736 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/ViromeEvent/BlastX_NT/PoolAndSplitSequence.pm
+++ Genome/Model/Tools/ViromeEvent/BlastX_NT/PoolAndSplitSequence.pm
@@ -7,6 +7,8 @@ use warnings;
 use Genome;
 use Workflow;
 use IO::File;
+use Bio::SeqIO;
+use File::Basename;
 
 class Genome::Model::Tools::ViromeEvent::BlastX_NT::PoolAndSplitSequence{
     is => 'Genome::Model::Tools::ViromeEvent',
@@ -46,183 +48,69 @@ sub create {
 
 }
 
-sub execute
-{
+sub execute {
     my $self = shift;
-    $self->log_event("Blast X NT entered");
+
     my $dir = $self->dir;
+    my $sample_name = basename($dir);
 
+    $self->log_event("Pooling data to run NT blastX for $sample_name");
 
-
-    $self->pool_BNfiltered_sequence( );
-
-    opendir(DH, $dir) or die "Can not open dir $dir!\n";
-    my $matches = 0;
-    foreach my $file (readdir DH) 
-    {
-	if ($file =~ /\.BNFiltered\.fa$/) 
-        { 
-                $self->log_event("$file did MATCH BNFiltered");
-
-		# directory for splited files
-		my $out_dir = $file;
-		$out_dir =~ s/\.BNFiltered\.fa/.BNFiltered_TBLASTX_nt/;
-		$out_dir = $dir."/".$out_dir;
-
-                $matches++;
-			
-		$self->splitGivenNumberSequence($file, $out_dir);
-	}
-        else
-        {
-            $self->log_event("$file did NOT match BNFiltered");
-        }
+    #FIND NT BLASTN DATA DIR (PREVIOUS BLAST RUN)
+    my $nt_blastn_dir = $dir.'/'.$sample_name.'.HGfiltered_BLASTN';
+    unless (-d $nt_blastn_dir) {
+	$self->log_event("Failed to find NT blastN data dir for $sample_name");
+	return;
     }
-    $self->log_event("Blast X NT completed with $matches matches");
-    return 1;
-}
-
-sub pool_BNfiltered_sequence 
-{
-	my ($self) = @_;
-        my $dir = $self->dir;
-        my $matches = 0;
-
-	opendir(DH, $dir) or die "Can not open dir $dir!\n";
-	foreach my $name (readdir DH) 
-        {
-		if ($name =~ /HGfiltered_BLASTN$/) 
-                {
-                        $matches++;
-                        $self->log_event("$name did MATCH HGfiltered_BLASTN");
-			my $temp = $name;
-			my @temp = split(/\./, $temp);
-			$temp = shift @temp;
-			my $full_path = $dir."/".$name;
-			my $out = $dir."/".$temp.".BNFiltered.fa";
-			if (-e $out) {
-                                $self->log_event("$out exists, exiting");
-				return 0;
-			}
-			my @files = ();
-			opendir(SubDH, $full_path) or die "can not open dir $full_path!\n";
-			foreach my $name2 (readdir SubDH) { 
-				if ($name2 =~ /BNfiltered\.fa$/) {
-					push @files, $full_path."/".$name2;
-				}
-			}
-			closedir( SubDH );
-
-			my $com = "";
-			foreach my $file (@files) {
-				$com = "cat $file >> $out ";
-                                $self->log_event("calling $com");
-				system( $com );
-			}
-		}
-	}
-
-	closedir( DH );
-        $self->log_event("$matches pooled matches");
-}
-
-
-sub splitGivenNumberSequence {
-	my ($self, $inFile_name, $outDir ) = @_;
-        my $dir = $self->dir;				
-        my $numSeq = 100; # number of sequences in each file
-	# read in sequences
-	my $inFile = $dir."/".$inFile_name;
-	my %seq = $self->read_FASTA_data($inFile);
-	my $num_seq_total = keys %seq;
-	my $num_seq_left = $num_seq_total;
-        $self->log_event("splitGivenNumberSequence entered with $outDir");	
-	# check there are sequences in the file
-	my $n = keys %seq;
-	if (!$n) {
-		$self->log_event("in splitGivenNumberSequence ", $inFile_name, " does not have any sequences!");
-		return 0;
-	}
-
-	# make directory for splited files
-	if (-e $outDir) {
-                $self->log_event("in splitGivenNumberSequence, $outDir exists, returning");
-		return 0;
-	}
-	else {
-                $self->log_event("in splitGivenNumberSequence, creating $outDir");
-		my $com = "mkdir $outDir";
-		system ($com);
-	}
-        $self->log_event("beginning splitting...");	
-	# start spliting
-	my $geneCount = 0;
-	my $fileCount = 0;
-	my $outFile = $outDir."/".$inFile_name."_file".$fileCount.".fa";
-	open (OUT, ">$outFile") or die "can not open file $outFile!\n";
-	foreach my $read (keys %seq) {
-                $self->log_event("printing $read to $outFile");
-		print OUT ">$read\n";
-		print OUT $seq{$read}, "\n";
-		$geneCount++;
-
-		if (!($geneCount%$numSeq)) {
-			close OUT;
-			$fileCount++;
-			$num_seq_left = $num_seq_total - $geneCount;
-			if ($num_seq_left) {
-				$outFile = $outDir."/".$inFile_name."_file".$fileCount.".fa";
-				open (OUT, ">$outFile") or die "can not open file $outFile!\n";
-			}
-			else {
-				;
-			}
-		}
-	}
-	close OUT;
-        $self->log_event("splitGivenNumberSequence complete");
-}
-
-sub read_FASTA_data () {
-    my ($self,$fastaFile) = @_;
-    $self->log_event("read_FASTA_data entered");
-    #keep old read seperator and set new read seperator to ">"
-    my $oldseperator = $/;
-    $/ = ">";
-	 
-    my %fastaSeq;	 
-    open (FastaFile, $fastaFile) or die "Can't Open FASTA file: $fastaFile";
-
-    while (my $line = <FastaFile>){
-		# Discard blank lines
-        if ($line =~ /^\s*$/) {
-		    next;
-		}	
-		# discard comment lines
-		elsif ($line =~ /^\s*#/) {
-	       next;
-		}
-		# discard the first line which only has ">", keep the rest
-		elsif ($line ne ">") {
-			chomp $line;
-			my @rows = ();
-			@rows = split (/\n/, $line);	
-			my $contigName = shift @rows;
-			my $contigSeq = join("", @rows);
-			$contigSeq =~ s/\s//g; #remove white space
-			$fastaSeq{$contigName} = $contigSeq;
-		}
+    #FIND FILES THAT CONTAIN BLASTN FILTERED READS
+    my @filtered_files = glob("$nt_blastn_dir/*BNfiltered.fa");
+    unless (scalar @filtered_files > 0) {
+	$self->log_event("Failed to find any NT blastN filtered data for $sample_name");
+	return;
     }
 
-    # execute right
+    #DEFINE POOLED OUT FILE NAME POOL FILTER READS TO THAT FILE
+    my $pooled_file = $dir.'/'.$sample_name.'.BNFiltered.fa';
+    my $out = Bio::SeqIO->new(-format => 'fasta', -file => ">$pooled_file");
+    foreach my $file (@filtered_files) {
+	my $in = Bio::SeqIO->new(-format => 'fasta', -file => $file);
+	while (my $seq = $in->next_seq) {
+	    $out->write_seq($seq);
+	}
+    }
 
-    #reset the read seperator
-    $/ = $oldseperator;
-   close FastaFile; 
-    $self->log_event("read_FASTA_data completed");
-    return %fastaSeq;
+    #CHECK TO MAKE SURE VALID POOLED FILE HAS BEEN MADE
+    unless (-s $pooled_file) {
+	$self->log_event("Failed to create pooled file of NT blastN filtered reads");
+	return;
+    }
+
+    #SPLIT POOLED FILE TO RUN NT BLAST X
+    my $blast_dir = $dir.'/'.$sample_name.'.BNFiltered_TBLASTX_nt';
+    system ("mkdir $blast_dir");
+    unless (-d $blast_dir) {
+	$self->log_event("Failed to create dir ".basename($blast_dir));
+	return;
+    }
+
+    my $c = 0; my $n = 0; my $limit = 250;
+    my $in = Bio::SeqIO->new(-format => 'fasta', -file => $pooled_file);
+    my $split_file = $blast_dir.'/'.$sample_name.'.BNFiltered.fa_file'.$n.'.fa';
+    my $split_out = Bio::SeqIO->new(-format => 'fasta', -file => ">$split_file");
+    while (my $seq = $in->next_seq) {
+	$c++;
+	$split_out->write_seq($seq);
+	if ($c == $limit) {
+	    $c = 0;
+	    $split_file = $blast_dir.'/'.$sample_name.'.BNFiltered.fa_file'.++$n.'.fa';
+	    $split_out = Bio::SeqIO->new(-format => 'fasta', -file => ">$split_file");
+	}
+    }
+
+    $self->log_event("Pooled data to run NT blast X completed for $sample_name");
+
+    return;
 }
-
 
 1;
 
