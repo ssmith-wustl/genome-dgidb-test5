673c4c587e104db467c91727c41a1d7c9a6c604d
kkyung <kkyung@genome.wustl.edu>
2009-12-09 20:39:32 +0000

    Re-writes and updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53724 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/ViromeEvent/BlastHumanGenome/SplitGivenNumberReads.pm
+++ Genome/Model/Tools/ViromeEvent/BlastHumanGenome/SplitGivenNumberReads.pm
@@ -7,6 +7,7 @@ use warnings;
 use Genome;
 use Workflow;
 use IO::File;
+use File::Basename;
 
 class Genome::Model::Tools::ViromeEvent::BlastHumanGenome::SplitGivenNumberReads{
     is => 'Genome::Model::Tools::ViromeEvent',
@@ -39,134 +40,66 @@ sub create {
     my $class = shift;
     my $self = $class->SUPER::create(@_);
     return $self;
-
 }
 
 sub execute
 {
     my $self = shift;
     my $dir = $self->dir;
+    my $sample_name = basename ($dir);
 
-    $self->log_event("Blast Human Genome entered");
+    $self->log_event("Split reads executing for $sample_name");
 
-    opendir(DH, $dir) or die "Can not open dir $dir!\n";
-    foreach my $file (readdir DH) {
-        $self->log_event("step 0:  $file");
-        	if ($file =~ /\.masked\.goodSeq$/) { 
-                    $self->log_event("step 1:  found $file for splitting");
-	        	# directory for splited files
-		    my $out_dir = $file;
-        		$out_dir = $dir."/".$out_dir."_HGblast";
-		    $self->splitGivenNumberSequence($dir, $file, $out_dir);
-	        }							
-    }   
+    #FILE TO SPLIT
+    my $good_seq_file = $dir.'/'.$sample_name.'.fa.cdhit_out.masked.goodSeq';
+    unless (-s $good_seq_file) {
+	$self->log_event("Failed to find repeat masker good seq file");
+	return;
+    }
 
-    $self->log_event("Blast Human Genome completed");
+    #DIRECTORY TO PUT SPLIT FILES INTO
+    my $output_dir = $dir.'/'.$sample_name.'.fa.cdhit_out.masked.goodSeq_HGblast';
+    system ("mkdir $output_dir");
+    unless (-d $output_dir) {
+	$self->log_event("Failed to create human blast dir for $sample_name");
+	return;
+    }
+
+    my $in = Bio::SeqIO->new(-format => 'fasta', -file => $good_seq_file);
+    unless ($in) {
+	$self->log_event("Failed to create Bio::SeqIO for human blast");
+	return;
+    }
+
+    my $c = 0; my $n = 0; my $max = 500;
+
+    my $out_file = $output_dir.'/'.$sample_name.'.fa.cdhit_out.masked.goodSeq_file'.$n.'.fa';
+
+    my $out_io = Bio::SeqIO->new(-format => 'fasta', -file => ">$out_file");
+    unless (defined $out_io) {
+	$self->log_event("Failed to create Bio SeqIO for HG blast splitting");
+	return;
+    }
+
+    while (my $seq = $in->next_seq) {
+	$c++;
+	$out_io->write_seq($seq);
+	if ($c == $max) {
+	    $c = 0;
+	    $out_file = $output_dir.'/'.$sample_name.'.fa.cdhit_out.masked.goodSeq_file'.++$n.'.fa';
+	    $out_io = Bio::SeqIO->new(-format => 'fasta', -file => ">$out_file");
+	}
+    }
+
+    unless (-s $out_file) {
+	$self->log_event("Failed to create HG blastN input file or no valid data available");
+	return;
+    }
+
+    $self->log_event("Split reads for HG blast completed for $sample_name");
 
     return 1;
 }
-
-sub splitGivenNumberSequence {
-	my ($self, $inFile_dir, $inFile_name, $outDir ) = @_;
-        my $numSeq = 200; # number of sequences in each file
-        $self->log_event("Blast Human Genome splitGivenNumberSequence entered");
-				
-	# read in sequences
-	my $inFile = $inFile_dir."/".$inFile_name;
-	my %seq = $self->read_FASTA_data($inFile);
-	my $num_seq_total = keys %seq;
-	my $num_seq_left = $num_seq_total;
-					
-	# check there are sequences in the file
-	my $n = keys %seq;
-	if (!$n) {
-                $self->log_event("step 2, $inFile_name does not have any sequences, returning");
-		print $inFile_name, " does not have any sequences!\n\n";
-		return 0;
-	}
-
-	# make directory for splited files
-	if (-e $outDir) {
-                $self->log_event("step 2, $outDir exists, returning");
-		return 0;
-	}
-	else {
-		my $com = "mkdir $outDir";
-                $self->log_event("step 2, mkdir $outDir");
-		system ($com);
-	}
-	
-	# start spliting
-	my $geneCount = 0;
-	my $fileCount = 0;
-	my $outFile = $outDir."/".$inFile_name."_file".$fileCount.".fa";
-	open (OUT, ">$outFile") or die "can not open file $outFile!\n";
-        $self->log_event("step 3, $outFile created");
-	foreach my $read (keys %seq) {
-		print OUT ">$read\n";
-		print OUT $seq{$read}, "\n";
-		$geneCount++;
-
-		if (!($geneCount%$numSeq)) {
-			close OUT;
-			$fileCount++;
-			$num_seq_left = $num_seq_total - $geneCount;
-			if ($num_seq_left) {
-				$outFile = $outDir."/".$inFile_name."_file".$fileCount.".fa";
-				open (OUT, ">$outFile") or die "can not open file $outFile!\n";
-			}
-			else {
-				return 0;
-			}
-		}
-	}
-	close OUT;
-        $self->log_event("Blast Human Genome splitGivenNumberSequence completed");
-}
-
-
-sub read_FASTA_data () {
-    my ($self,$fastaFile) = @_;
-    $self->log_event("Blast Human Genome read_FASTA_data entered");
-
-    #keep old read seperator and set new read seperator to ">"
-    my $oldseperator = $/;
-    $/ = ">";
-	 
-    my %fastaSeq;	 
-    open (IN, $fastaFile) or die "Can't Open FASTA file: $fastaFile";
-
-    $self->log_event("$fastaFile opened");
-    while (my $line = <IN>){
-		# Discard blank lines
-        if ($line =~ /^\s*$/) {
-		    next;
-		}	
-		# discard comment lines
-		elsif ($line =~ /^\s*#/) {
-	       next;
-		}
-		# discard the first line which only has ">", keep the rest
-		elsif ($line ne ">") {
-			chomp $line;
-			my @rows = ();
-			@rows = split (/\n/, $line);	
-			my $contigName = shift @rows;
-			my $contigSeq = join("", @rows);
-			$contigSeq =~ s/\s//g; #remove white space
-			$fastaSeq{$contigName} = $contigSeq;
-		}
-    }
-
-    #reset the read seperator
-    $/ = $oldseperator;
-	close IN;
-    
-    $self->log_event("Blast Human Genome read_FASTA_data completed");
-    return %fastaSeq;
-}
-
-
 
 1;
 
