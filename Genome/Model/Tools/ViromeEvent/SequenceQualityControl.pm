c1ac6b4392317120c1ce4aed5122627e7864f227
kkyung <kkyung@genome.wustl.edu>
2009-12-09 20:41:31 +0000

    Re-writes and updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53732 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/ViromeEvent/SequenceQualityControl.pm
+++ Genome/Model/Tools/ViromeEvent/SequenceQualityControl.pm
@@ -7,6 +7,8 @@ use warnings;
 use Genome;
 use Workflow;
 use IO::File;
+use Bio::SeqIO;
+use File::Basename;
 
 class Genome::Model::Tools::ViromeEvent::SequenceQualityControl{
     is => 'Genome::Model::Tools::ViromeEvent',
@@ -48,127 +50,52 @@ sub create {
 sub execute
 {
     my $self = shift;
-    $self->log_event("Sequence Quality Control entered");
+
     my $dir = $self->dir;
-    my @fields = split(/\//, $dir);
-    my $libName = $fields[$#fields];
+    my $sample_name = basename ($dir);
 
-    my $total_seq = 0;
-    my $good_seq = 0;
-    my $bad_seq = 0;
-    my $OutFile1 = $dir."/".$libName.".fa.cdhit_out.masked.goodSeq";
-    my $OutFile2 = $dir."/".$libName.".fa.cdhit_out.masked.badSeq";
+    $self->log_event("Running repeat masker qual control for sample: $sample_name");
 
-    # if already has the files, exit
-    if (-s $OutFile1) 
-    {
-	return 0;
+    my $good_seq_file = $dir.'/'.$sample_name.'.fa.cdhit_out.masked.goodSeq';
+    my $bad_seq_file  = $dir.'/'.$sample_name.'.fa.cdhit_out.masked.badSeq';
+
+    my $good_io = Bio::SeqIO->new(-format => 'fasta', -file => ">$good_seq_file");
+    my $bad_io  = Bio::SeqIO->new(-format => 'fasta', -file => ">$bad_seq_file");
+
+    my $repeat_masker_dir = $dir.'/'.$sample_name.'.fa.cdhit_out_RepeatMasker';
+    unless (-d $repeat_masker_dir) {
+        $self->log_event("Failed to find repeat masker dir for sample: $sample_name");
+        return;
     }
 
-    open (OUT1, ">$OutFile1") or die "can not open $OutFile1\n";
-    open (OUT2, ">$OutFile2") or die "can not open $OutFile2\n";
-	
-    opendir(DH, $dir) or die "Can not open dir $dir!\n";
-    $self->log_event("step 0 opening $dir");
-    foreach my $name (readdir DH) 
-    {
-        $self->log_event("step 1 sequencing $name");
-        if ($name =~ /.cdhit_out_RepeatMasker$/) 
-        { # RepeatMasker directory
-            $self->log_event("step 2 sequencing $name");
-	    my $full_path = $dir."/".$name;
-	    opendir(SubDH, $full_path) or die "can not open dir $full_path!\n";
-            $self->log_event("step 3 opened $full_path");
-	    foreach my $file (readdir SubDH) 
-            {
-                $self->log_event("step 4 opened $file");
-	        if ($file =~ /\.masked$/) 
-                { # masked sequence
-                    $self->log_event("step 5 opened $file");
-		    my $maskedFile = $full_path."/".$file;
-		    my %seq = ();
-		    $self->read_FASTA_data($maskedFile, \%seq);
+    my @masked_files = glob ("$repeat_masker_dir/$sample_name*.fa.masked");
+    unless (scalar @masked_files > 0) {
+        $self->log_event("No masked fastas for repeat masker run found for sample: $sample_name");
+        return;
+    }
 
-        		# check for contiguous bases >= 50 bp (non-Ns) 
-	        	foreach my $read_id (keys %seq) 
-                        {
-        		    $total_seq++;
-	        	    my $seq_temp = $seq{$read_id};
-		            my $goodQuality=$seq_temp=~/[ACTG]{50,}/; 
-        		    if($goodQuality) 
-                            {
-	        		print OUT1 ">$read_id\n";
-		        	print OUT1 $seq{$read_id}, "\n";
-			        $good_seq++;
-        		    }
-	        	    else 
-                            {
-            			print OUT2 ">$read_id\n";
-	        		print  OUT2 "$seq{$read_id}\n";
-        			$bad_seq++;
-		            }
-		        }
-	        }
-                else
-                {
-                    $self->log_event("step 5 $file doesn't match mask");
-                }
+    foreach my $file (@masked_files) {
+	my $io = Bio::SeqIO->new(-format => 'fasta', -file => $file);
+	while (my $seq = $io->next_seq) {
+	    #LOOK FOR 50+ CONTIGUIOUS BASES(NONE-NS)
+	    my $ge_50 = 0;
+	    my @strings = split (/[n|x]/i, $seq->seq);
+	    foreach my $string (@strings) {
+		$ge_50 = 1 if (length $string >= 50);
 	    }
-        }
+	    if ($ge_50 == 1) {
+		$good_io->write_seq($seq);
+	    }
+	    else {
+		$bad_io->write_seq($seq);
+	    }
+	}
     }
 
-    print OUT2 "total seq = $total_seq\n";
-    $self->log_event("total seq = $total_seq");
-    if ($total_seq)
-    {
-        print OUT2 "good seq = $good_seq % = ", $good_seq/$total_seq, "\n";
-        print OUT2 "bad seq = $bad_seq % = ", $bad_seq/$total_seq, "\n";
-        $self->log_event("good seq = $good_seq % = ", $good_seq/$total_seq);
-        $self->log_event("bad seq = $bad_seq % = ", $bad_seq/$total_seq);
-    }
-    close(OUT1);
-    close(OUT2);
-    $self->log_event("Sequence Quality Control completed");
+    $self->log_event("Repeat masker Seq qual control completed for sample: $sample_name");
     return 1;
 }
 
-sub read_FASTA_data () 
-{
-    my ($self,$fastaFile, $hash_ref) = @_;
-
-    #keep old read seperator and set new read seperator to ">"
-    my $oldseperator = $/;
-    $/ = ">";
-	 
-    open (FastaFile, $fastaFile) or die "Can't Open FASTA file: $fastaFile";
-    while (my $line = <FastaFile>){
-	# Discard blank lines
-        if ($line =~ /^\s*$/) {
-	    next;
-	}
-	# discard comment lines
-	elsif ($line =~ /#/) {
-	    next;
-	}
-	# discard the first line which only has ">", keep the rest
-	elsif ($line ne ">") {
-	    chomp $line;
-	    my @rows = ();
-	    @rows = split (/\n/m, $line);	
-	    my $seqName = shift @rows;
-	    my @temp = split (/\s/, $seqName);
-	    $seqName = shift @temp;
-	    my $Seq = join("", @rows);
-	    $Seq =~ s/\s//g; #remove white space
-	    $hash_ref->{$seqName} = $Seq;
-	}
-    }
-    
-    close FastaFile;
-    #reset the read seperator
-    $/ = $oldseperator;
-}
-
-
 1;
+
 
