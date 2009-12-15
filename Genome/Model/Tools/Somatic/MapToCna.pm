75e942bb77cb3178e8ca048264bdea747b6a2c7b
gsanders <gsanders@genome.wustl.edu>
2009-12-15 22:16:11 +0000

    request 64 bit blades... not sure how this slipped by unnoticed but it did by chance for awhile
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53900 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Somatic/MapToCna.pm
+++ Genome/Model/Tools/Somatic/MapToCna.pm
@@ -79,6 +79,15 @@ class Genome::Model::Tools::Somatic::MapToCna {
         default => 0,
         doc => 'enable this flag to shortcut through annotation if the output_file is already present. Useful for pipelines.',
     },
+    # Make workflow choose 64 bit blades
+    lsf_resource => {
+        is_param => 1,
+        default_value => 'rusage[mem=4000] select[type==LINUX64] span[hosts=1]',
+    },
+    lsf_queue => {
+        is_param => 1,
+        default_value => 'long',
+    },
     ]
 };
 
