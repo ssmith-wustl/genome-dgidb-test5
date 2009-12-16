fb36154ba7201cd75cd3a0ff64b798920c508a4d
jwalker <jwalker@genome.wustl.edu>
2009-12-16 18:03:25 +0000

    more or less a re-write to grab sequences randomly from mutliple files.  DOES NOT WORK FOR PE FILES
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53947 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Fastq/RandomSubset.pm
+++ Genome/Model/Tools/Fastq/RandomSubset.pm
@@ -8,23 +8,40 @@ use Math::Random;
 class Genome::Model::Tools::Fastq::RandomSubset {
     is => 'Command',
     has => [
-        input_fastq_file => {
+        input_fastq_files => {
             is => 'Text',
+            is_many => 1,
         },
         output_fastq_file => {
             is => 'Text',
         },
-        subset_size => {
-            is => 'Integer',
-        },
     ],
     has_optional => [
         seed_phrase => { is => 'Text' },
+        reads => {
+            is =>  'Integer',
+        },
+        base_pair => {
+            is => 'Integer',
+        },
         _index => { },
-        _fh => { },
+        _fhs => { },
+        _shortest_seq => { },
     ],
 };
 
+
+sub create {
+    my $class = shift;
+    my $self = $class->SUPER::create(@_);
+    unless ($self->reads || $self->base_pair) {
+        die('Failed to define either reads or base_pair');
+    }
+    if ($self->reads && $self->base_pair) {
+        die('Either reads or base_pair can be defined but not both');
+    }
+    return $self;
+}
 
 sub execute {
     my $self = shift;
@@ -36,16 +53,38 @@ sub execute {
     my $out = Genome::Utility::FileSystem->open_file_for_writing($self->output_fastq_file);
     my @index = @{$self->_index};
     my $n;
-    if (scalar(@index) <= $self->subset_size) {
-        $n = scalar(@index);
-    } else {
-        $n = $self->subset_size;
+    if ($self->reads){ 
+        if (scalar(@index) <= $self->reads) {
+            die('The number of fastq entries '. scalar(@index) .' is less than subset size '. $self->reads);
+            #$n = scalar(@index);
+        } else {
+            $n = $self->reads;
+        }
+    } elsif ($self->base_pair) {
+        $n = int($self->base_pair / $self->_shortest_seq);
     }
+    my @fhs = @{$self->_fhs};
+    my $total_seq;
     foreach my $i (random_uniform_integer($n, 0, scalar(@index))) {
-        my $begin = $index[$i];
-        my @fastq_lines = $self->get_fastq_lines($begin);
+        my $index_array_ref = $index[$i];
+
+        my $fh_id = $index_array_ref->[0];
+        my $seq_length = $index_array_ref->[1];
+        my $begin = $index_array_ref->[2];
+
+        my $fh = $fhs[$fh_id];
+        #set read pos
+        $fh->seek($begin,0);
+        my @fastq_lines;
+        for (1 .. 4) {
+            push @fastq_lines, $fh->getline;
+        }
         for my $line (@fastq_lines) {
             print $out $line;
+        }
+        $total_seq += $seq_length;
+        if ($self->base_pair && $total_seq >= $self->base_pair) {
+            last;
         }
     }
     $out->close;
@@ -55,31 +94,35 @@ sub execute {
 
 sub _create_index {
     my $self = shift;
-    my $begin = 0;
-    my $fastq_file = $self->input_fastq_file;
-    my $fh = Genome::Utility::FileSystem->open_file_for_reading($fastq_file);
+    my @fastq_files = $self->input_fastq_files;
+    my @fhs;
     my @index;
-    while (<$fh>) {
-        if (/^@/) {
-            # $begin is the position of the first character after the '@'
-            my $begin = tell($fh) - length( $_ );
-            push @index, $begin;
+    for (my $file_id = 0; $file_id < scalar(@fastq_files); $file_id++) {
+        my $fastq_file = $fastq_files[$file_id];
+        my $fh = Genome::Utility::FileSystem->open_file_for_reading($fastq_file);
+        my $seq_length;
+        while (<$fh>) {
+            if (/^@/) {
+                # $begin is the position of the first character after the '@'
+                my $begin = tell($fh) - length( $_ );
+                # assumes equal sequence length within a fastq file
+                unless ($seq_length) {
+                    my $seq = $fh->getline;
+                    chomp($seq);
+                    $seq_length = length($seq);
+                    if (defined($self->_shortest_seq)) {
+                        if ($seq_length < $self->_shortest_seq) {
+                            $self->_shortest_seq($seq_length);
+                        }
+                    } else { $self->_shortest_seq($seq_length); }
+                }
+                push @index, [$file_id,$seq_length,$begin];
+            }
         }
+        push @fhs, $fh;
     }
-    $self->_fh($fh);
+    $self->_fhs(\@fhs);
     $self->_index(\@index);
-}
-
-sub get_fastq_lines {
-    my $self = shift;
-    my $begin = shift;
-    my $fh = $self->_fh;
-    #set read pos
-    $fh->seek($begin,0);
-    my @fastq_lines;
-    for (1 .. 4) {
-        push @fastq_lines, $fh->getline;
-    }
-    return @fastq_lines;
+    return 1;
 }
 
