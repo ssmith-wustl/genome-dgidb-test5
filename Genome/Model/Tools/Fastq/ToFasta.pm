10cb0fde6ecb2958179bd893189993991e758579
jwalker <jwalker@genome.wustl.edu>
2009-12-16 18:02:12 +0000

    implement the conversion of fastq to both fasta sequence and fasta quality
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@53946 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Fastq/ToFasta.pm
+++ Genome/Model/Tools/Fastq/ToFasta.pm
@@ -23,11 +23,11 @@ class Genome::Model::Tools::Fastq::ToFasta {
 sub execute {
     my $self = shift;
 
-    unless ($self->quality_file) {
+    #unless ($self->quality_file) {
         $self->fastQ2A;
-    } else {
-        $self->bio_convert;
-    }
+    #}# else {
+     #   $self->bio_convert;
+    #}
     return 1;
 }
 
@@ -36,6 +36,10 @@ sub fastQ2A {
     local $/ = "\n@";
     my $in_fh = Genome::Utility::FileSystem->open_file_for_reading($self->fastq_file);
     my $out_fh = Genome::Utility::FileSystem->open_file_for_writing($self->fasta_file);
+    my $qual_fh;
+    if ($self->quality_file) {
+        $qual_fh = Genome::Utility::FileSystem->open_file_for_writing($self->quality_file);
+    }
     while (my $line = $in_fh->getline) {
         # @HWI-EAS75:1:2:0:345#0/1
         # NCCGCGAGATCGGAAGAGCGGTTCAGCAGGAATGC
@@ -47,6 +51,10 @@ sub fastQ2A {
         if ($id1 && $nt) {
             print $out_fh '>' . $id1 . "\n";
             print $out_fh $nt . "\n";
+        }
+        if ($qual_fh && $id1 && $qual) {
+            print $qual_fh '>' . $id1 . "\n";
+            print $qual_fh $qual . "\n";
         }
     }
     $in_fh->close;
