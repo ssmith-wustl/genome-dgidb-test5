be25fead5b0e935b7c26830533f076b233bc7c63
dkoboldt <dkoboldt@genome.wustl.edu>
2010-01-08 19:35:39 +0000

    Changes to somatic tool to remove bsub and enable pipeline
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54359 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Varscan/Somatic.pm
+++ Genome/Model/Tools/Varscan/Somatic.pm
@@ -26,7 +26,9 @@ class Genome::Model::Tools::Varscan::Somatic {
 	has => [                                # specify the command's single-value properties (parameters) <--- 
 		normal_bam	=> { is => 'Text', doc => "Path to Normal BAM file", is_optional => 0 },
 		tumor_bam	=> { is => 'Text', doc => "Path to Tumor BAM file", is_optional => 0 },
-		output	=> { is => 'Text', doc => "Basename for output, eg. varscan_out/varscan.status" , is_optional => 0},
+		output	=> { is => 'Text', doc => "Path to Tumor BAM file", is_optional => 1 },
+		output_snp	=> { is => 'Text', doc => "Basename for SNP output, eg. varscan_out/varscan.status.snp" , is_optional => 1},
+		output_indel	=> { is => 'Text', doc => "Basename for indel output, eg. varscan_out/varscan.status.snp" , is_optional => 1},
 		reference	=> { is => 'Text', doc => "Reference FASTA file for BAMs (default= genome model)" , is_optional => 1},
 		heap_space	=> { is => 'Text', doc => "Megabytes to reserve for java heap [1000]" , is_optional => 1},
 		varscan_params	=> { is => 'Text', doc => "Parameters to pass to VarScan [--min-coverage 8 --min-var-freq 0.10 --p-value 0.10 --somatic-p-value 1.0e-02]" , is_optional => 1},
@@ -64,11 +66,31 @@ sub execute {                               # replace with real execution logic.
 	## Get required parameters ##
 	my $normal_bam = $self->normal_bam;
 	my $tumor_bam = $self->tumor_bam;
-	my $output = $self->output;
+
+	## Get output directive ##
+	my $output = my $output_snp = my $output_indel = "";
+
+	if($self->output)
+	{
+		$output = $self->output;
+		$output_snp = $output . ".snp";
+		$output_indel = $output . ".indel";		
+	}
+	elsif($self->output_snp && $self->output_indel)
+	{
+		$output_snp = $self->output_snp;
+		$output = $output_snp;
+		$output_indel = $self->output_indel;
+	}
+	else
+	{
+		die "Please provide an output basename (--output) or output files for SNPs (--output-snp) and indels (--output-indels)\n";
+	}
+
+
 	my $reference = "/gscmnt/839/info/medseq/reference_sequences/NCBI-human-build36/all_sequences.fa";
 	$reference = $self->reference if($self->reference);
-	my $varscan_params = "--min-coverage 8 --min-var-freq 0.10 --p-value 0.10 --somatic-p-value 0.01 --verbose 1";
-
+	my $varscan_params = "--min-var-freq 0.10 --p-value 0.10 --somatic-p-value 0.01"; #--min-coverage 8 --verbose 1
 
 	if(-e $normal_bam && -e $tumor_bam)
 	{
@@ -77,7 +99,7 @@ sub execute {                               # replace with real execution logic.
 		my $normal_pileup = "samtools pileup -f $reference $normal_bam";
 		my $tumor_pileup = "samtools pileup -f $reference $tumor_bam";
 		
-		open(SCRIPT, ">$output.sh") or die "Can't open output file!\n";
+		open(SCRIPT, ">$output_snp.sh") or die "Can't open output file!\n";
 		print SCRIPT "#!/gsc/bin/bash\n";
 		## Run VarScan ##
 		if($self->heap_space)
@@ -86,12 +108,13 @@ sub execute {                               # replace with real execution logic.
 		}
 		else
 		{
-			print SCRIPT "java -classpath ~dkoboldt/Software/VarScan net.sf.varscan.VarScan somatic <\($normal_pileup\) <\($tumor_pileup\) $output $varscan_params\n";
+			print SCRIPT "java -classpath ~dkoboldt/Software/VarScan net.sf.varscan.VarScan somatic <\($normal_pileup\) <\($tumor_pileup\) --output-snp $output_snp --output-indel $output_indel $varscan_params\n";
 #			system("echo  \<\($normal_pileup\) \<\($tumor_pileup\) $output $varscan_params");
 		}
 		close(SCRIPT);
 		system("chmod 755 $output.sh");
-		system("bsub -q long -R\"select[type==LINUX64 && model != Opteron250 && mem>4000] rusage[mem=4000]\" \"bash $output.sh\"");
+#		system("bsub -q long -R\"select[type==LINUX64 && model != Opteron250 && mem>4000] rusage[mem=4000]\" \"bash $output_snp.sh\"");
+		system("bash $output_snp.sh");
 	}
 	else
 	{
