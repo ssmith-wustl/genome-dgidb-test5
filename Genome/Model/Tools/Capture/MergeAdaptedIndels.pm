2cc10e1809cd919271c288cac94095ee475cc793
dkoboldt <dkoboldt@genome.wustl.edu>
2010-01-14 23:15:26 +0000

    Several enhancements to indel merging/tracking
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54531 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Capture/MergeAdaptedIndels.pm
+++ Genome/Model/Tools/Capture/MergeAdaptedIndels.pm
@@ -60,55 +60,141 @@ sub execute {                               # replace with real execution logic.
 	my $self = shift;
 
 	## Get required parameters ##
-	my $glfSomaticindels = $self->glf_file;
-	my $varScanindels = $self->varscan_file;
-	my $out_file = $self->output_file;
+	my $glf_file = $self->glf_file;
+	my $varscan_file = $self->varscan_file;
+	my $output_file = $self->output_file;
 
-	my $glfinput = new FileHandle ($glfSomaticindels);
-	my $varscaninput = new FileHandle ($varScanindels);
+	my %stats = ();
 
-unless (open(OUT_FILE,">$out_file")) {
-   die "Could not open input file '$out_file' for writing";
-  }
+	## Load the indel sets ##
+	
+	my %glf_indels = load_indels($glf_file);
+	my %varscan_indels = load_indels($varscan_file);
 
-my %columns;
-my %columns_vs;
-while( my $additions = <$varscaninput> ) {
-   my ($Chromosome,$Start_position,$End_position,$Reference_Allele,$Tumor_Seq_Allele1,@other) = split(/\t/, $additions);
-   if ($Chromosome ne 'Chrom') {
-	next;
-   }
-   my $string = join("\t", @other);
-#   $string =~ s/\\t0\\t/\\t-\\t/g;
-   my $merger = "$Chromosome\t$Start_position\t$End_position\t$Reference_Allele\t$Tumor_Seq_Allele1";
-   $columns_vs{$merger} = "varscan\t$string";
-}
+	## Build a list of all unique indel keys ##
 
-close $varscaninput;
+	my %indel_keys = ();
 
-%columns = %columns_vs;
+	foreach my $key (keys %glf_indels)
+	{
+		$indel_keys{$key} = "sniper-only";
+	}
 
-my %columns_glf;
-while( my $additions = <$glfinput> ) {
-   my ($Chromosome,$Start_position,$End_position,$Reference_Allele,$Tumor_Seq_Allele1,@other) = split(/\t/, $additions);
-   my $string = join("\t", @other);
-#   $string =~ s/\\t0\\t/\\t-\\t/g;
-   my $merger = "$Chromosome\t$Start_position\t$End_position\t$Reference_Allele\t$Tumor_Seq_Allele1";
-   $columns_glf{$merger} = "SomaticSniper\t$string";
-   if (exists($columns_vs{$merger})) {
-	$columns{$merger} = "$columns_vs{$merger}\t$columns_glf{$merger}";
-   }
-   else {
-	$columns{$merger} = "$columns_glf{$merger}";
-   }
-}
+	foreach my $key (keys %varscan_indels)
+	{
+		if($indel_keys{$key})
+		{
+			$indel_keys{$key} = "shared";
+		}
+		else
+		{
+			$indel_keys{$key} = "varscan-only";
+		}
+	}
 
-close $glfinput;
+	## Open output files ##
 
-foreach my $keys (sort keys %columns) {
-   print OUT_FILE "$keys\t$columns{$keys}";
-}	
+	open(OUTFILE, ">$output_file") or die "Can't open outfile: $!\n";
+	open(SHARED, ">$output_file.shared") or die "Can't open outfile: $!\n";
+	open(SNIPER, ">$output_file.sniper-only") or die "Can't open outfile: $!\n";
+	open(VARSCAN, ">$output_file.varscan-only") or die "Can't open outfile: $!\n";
+
+
+	## Go thru all unique keys and handle them ##
+
+	foreach my $indel_key (keys %indel_keys)
+	{
+		$stats{'total'}++;
+		
+		if($indel_keys{$indel_key} eq "shared")
+		{
+			$stats{'shared'}++;
+			print SHARED $indel_key . "\t" . $varscan_indels{$indel_key} . "\t" . $glf_indels{$indel_key} . "\n";
+			print OUTFILE $indel_key . "\t" . $varscan_indels{$indel_key} . "\t" . $glf_indels{$indel_key} . "\n";
+		}
+		elsif($indel_keys{$indel_key} eq "varscan-only")
+		{
+			$stats{'varscan-only'}++;
+			print VARSCAN $indel_key . "\t" . $varscan_indels{$indel_key} . "\n";
+			print OUTFILE $indel_key . "\t" . $varscan_indels{$indel_key} . "\n";
+		}
+		elsif($indel_keys{$indel_key} eq "sniper-only")
+		{
+			$stats{'sniper-only'}++;
+			print SNIPER $indel_key . "\t" . $glf_indels{$indel_key} . "\n";
+			print OUTFILE $indel_key . "\t" . $glf_indels{$indel_key} . "\n";
+		}
+		else
+		{
+			## Where did this come from !? ##
+		}
+	}
+	
+	close(OUTFILE);
+	close(SHARED);
+	close(SNIPER);
+	close(VARSCAN);
+
+	print $stats{'total'} . " unique indels\n";
+	print $stats{'varscan-only'} . " VarScan-only\n";
+	print $stats{'sniper-only'} . " Sniper-only\n";
+	print $stats{'shared'} . " shared\n";
+
 	return 1;                               # exits 0 for true, exits 1 for false (retval/exit code mapping is overridable)
+}
+
+
+
+
+################################################################################################
+# Execute - the main program logic
+#
+################################################################################################
+
+sub load_indels
+{
+	my $infile = shift(@_);
+	
+	my %indels = ();
+
+	my $input = new FileHandle ($infile);
+	my $lineCounter = 0;
+	
+	while (<$input>)
+	{
+		chomp;
+		my $line = $_;
+		$lineCounter++;
+
+		(my $chrom, my $chr_start, my $chr_stop, my $allele1, my $allele2) = split(/\t/, $line);
+		
+		if(lc($chrom) ne "chrom")	## Skip header lines ##
+		{
+			$allele1 = "-" if($allele1 eq "0");
+			$allele2 = "-" if($allele2 eq "0");
+			
+			my $indel_key = "$chrom\t$chr_start\t$chr_stop\t$allele1\t$allele2";
+			
+			my @lineContents = split(/\t/, $line);
+			my $numContents = @lineContents;
+			
+			## Build rest of line ##
+			my $rest_of_line = "";
+			for(my $colCounter = 5; $colCounter < $numContents; $colCounter++)
+			{
+				$rest_of_line .= "\t" if($rest_of_line);
+				$rest_of_line .= $lineContents[$colCounter];
+			}
+			
+			## Save indel ##
+			
+			$indels{$indel_key} = $rest_of_line;
+		}
+	}
+	
+	close($input);
+	
+	return(%indels);
 }
 
 
