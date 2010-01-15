fb74dbb6645bbae5692325d984b4d46f6183cef0
ebelter <ebelter@genome.wustl.edu>
2010-01-15 19:48:12 +0000

    abandon works on build or model identifier
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54550 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Abandon.pm
+++ Genome/Model/Command/Build/Abandon.pm
@@ -6,56 +6,32 @@ use warnings;
 use Genome;
 
 class Genome::Model::Command::Build::Abandon {
-    is => ['Genome::Model::Command'],
-    has => [
-            build_id => {
-                         is => 'Number',
-                         doc => 'The id of the build which should be abandoned.',
-                         is_optional => 1,
-                     },
-            model_id => {
-                         is => 'Number',
-                         doc => 'The id of the model whose current running build should be abandoned.',
-                         is_optional => 1,
-                     },
-            build   => {
-                        is => 'Genome::Model::Build',
-                        id_by => 'build_id',
-                        is_optional => 1,
-                    },
-        ],
+    is => 'Genome::Model::Command::Build::Base',
 };
 
 sub sub_command_sort_position { 5 }
 
 sub help_detail {
-    "This command will abandon the build and all events that make up the build";
+    "Abandon a build and it's events";
 }
 
 sub execute {
     my $self = shift;
-    unless ($self->build_id) {
-        my $model = $self->model;
-        $self->build_id($model->current_running_build_id);
-    }
-    my $build = $self->build;
-    unless ($build) {
-        $self->error_message('Build not found for model id '. $self->model_id .' and build id '. $self->build_id);
+
+    # Get build
+    my $build = $self->_resolve_build
+        or return;
+
+    # Abandon
+    unless ( $build->abandon ) {
+        $self->error_message("Failed to abandon build. See above errors.");
         return;
     }
-    my $rv = eval { $build->abandon; };
-    unless ($rv and not $@) {
-        $self->error_message(
-            'Failed to abandon build '
-            . $self->build_id 
-            . ($@ ? " ERRORS: $@" : "")
-        );
-        return;
-    }
-     
+
     return 1;
 }
 
-
 1;
 
+#$HeadURL$
+#$Id$
