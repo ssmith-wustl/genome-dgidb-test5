5cd1df3c201f0c115e5b725906164339de229f14
ebelter <ebelter@genome.wustl.edu>
2010-01-15 19:49:57 +0000

    restored old version temporarily
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54551 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Remove.pm
+++ Genome/Model/Command/Build/Remove.pm
@@ -6,47 +6,70 @@ use warnings;
 use Genome;
 
 class Genome::Model::Command::Build::Remove {
-    is => 'Genome::Model::Command::Build::Base',
+    is => ['Genome::Model::Command'],
+    has => [
+            build_id => {
+                         is => 'Number',
+                         doc => 'The id of the build in which to update status',
+                         is_optional => 1,
+                     },
+            build   => {
+                        is => 'Genome::Model::Build',
+                        id_by => 'build_id',
+                        is_optional => 1,
+                    },
+    ],
     doc => 'delete a build and all of its data from the system'
 };
 
-#< Command >#
 sub sub_command_sort_position { 7 }
-
-sub help_brief {
-    return 'Remove a build and all of its data';
-}
 
 sub help_detail {
     "This command will remove the build and all events that make up the build";
 }
 
-#< Execute >#
 sub execute {
     my $self = shift;
-
-    # Get build
-    my $build = $self->_resolve_build
-        or return;
-
-    # Abandon
-    unless ( $build->abandon ) {
-        $self->error_message(
-            "Unable to remove build (".$self->build_id.") because the build could not be abandoned"
-        );
-        return;
+    my $model = $self->model;
+    unless ($self->build_id) {
+        $self->build_id($model->current_running_build_id);
     }
-    
-    # Delete
-    unless ( $build->delete ) {
-        $self->error_message('Failed to remove build ('.$build->id.')');
+
+    my $build = $self->build;
+    unless ($build) {
+        $self->error_message('Build not found for model id '. $self->model_id .' and build id '. $self->build_id);
         return;
     }
 
+    # cribbed from genome model build abandon - kills running jobs
+    # if any events are running
+    my @events = $build->build_events;
+    foreach my $event (@events) {
+        if($event->event_status =~ /Running/)
+        {
+            my $rv = eval { $build->abandon; };
+            unless ($rv and not $@) {
+                $self->error_message(
+                    'Failed to abandon build '
+                    . $self->build_id 
+                    . ($@ ? " ERRORS: $@" : "")
+                    );
+                return;
+            }
+            last;
+        }
+    }
+
+    my @build_links = $build->to_build_links();  # The links that I'm the from_build
+    $_->delete foreach @build_links;
+
+    unless ($build->delete) {
+        $self->error_message('Failed to remove build '. $self->build_id);
+        return;
+    }
     return 1;
 }
 
+
 1;
 
-#$HeadURL$
-#$Id$
