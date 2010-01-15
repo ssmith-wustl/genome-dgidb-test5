dbbf6cd186511898c8d7860989f785da3564e08b
dlarson <dlarson@genome.wustl.edu>
2010-01-15 15:18:39 +0000

    Better file parsing and error handling
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54536 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Sv/Peruse.pm
+++ Genome/Model/Tools/Sv/Peruse.pm
@@ -6,6 +6,7 @@ use warnings;
 use Genome;
 use Command;
 use IO::File;
+use Text::CSV_XS;
 
 class Genome::Model::Tools::Sv::Peruse {
     is => 'Command',
@@ -84,42 +85,35 @@ sub execute {
 
     my $viewer = $self->viewer_program;
     my $start = $self->start_from;
-    if($start) {
-        my ($start_chr1, $start_chr1_pos, $start_chr2,$start_chr2_pos,$start_type) = split /\_/, $start;
-        while(my $line = $indel_fh->getline) {
-            chomp $line;
-            my ($chr1,
-                $chr1_pos,
-                $orientation1,
-                $chr2,
-                $chr2_pos,
-                $orientation2,
-                $type,
-                $size,
-            ) = split /\s+/, $line; 
-            if($chr1 eq $start_chr1 && $chr2 eq $start_chr2 && $start_chr1_pos eq $chr1_pos && $start_chr2_pos eq $chr2_pos && $start_type eq $type) {
 
-                my $name = "$dir/${chr1}_${chr1_pos}_${chr2}_${chr2_pos}_*_${type}*.png";
-                print "Opening files for ${chr1}_${chr1_pos}_${chr2}_${chr2_pos}_${type}\n";
-                system("$viewer $name &");
-                my $next = <STDIN>;
-                chomp $next;
-                if($next eq 'q') {
-                    exit 0;
-                }
-                else {
-                    last;
-                }
-            }
-        }
+    my $csv = Text::CSV_XS->new({ sep_char => "\t"});   #set up CSV parser to use tabs. This way quotes are allowed and well handled
+    unless($csv) {
+        $self->error_message("Couldn't create CSV file parser");
+        return;
     }
 
 
-
-    #assuming we are reasonably sorted
-    while ( my $line = $indel_fh->getline) {
+    my ($start_chr1, $start_chr1_pos, $start_chr2,$start_chr2_pos,$start_type);
+    if($start) {
+        ($start_chr1, $start_chr1_pos, $start_chr2,$start_chr2_pos,$start_type) = split /\_/, $start;
+        unless(defined $start_chr1 && defined $start_chr1_pos && defined $start_chr2_pos && defined $start_type) {
+            $self->error_message("Invalid --start-from string $start. Must contain both start and stop chromosomes and positions as well as a type.");
+            return;
+        }
+        unless(exists($types{$start_type})) {
+            $self->error_message("You requested an invalid type $start_type in the --start-from argument. Please choose a valid type");
+            $self->error_message("Current valid types are: " . join(",",sort keys %types));
+            return;
+        }
+    }
+    SV:
+    while(my $line = $indel_fh->getline) {
         chomp $line;
-        #$self->status_message("(SEARCHING FOR: $line)");
+        unless($csv->parse($line)) {
+            $self->error_message("Failed to parse line. Parser returned: " . $self->error_message);
+            return;
+        }
+
         my ($chr1,
             $chr1_pos,
             $orientation1,
@@ -128,24 +122,31 @@ sub execute {
             $orientation2,
             $type,
             $size,
-        ) = split /\s+/, $line; 
+        ) = $csv->fields(); 
         if(exists($types{$type})) {
-            #then we should graph it
-            #submit the job
-            #Doing this based on chromosomes in case types ever change
+            if($start) {
+                #handle the initial search operation if requested
+                if($chr1 eq $start_chr1 && $chr2 eq $start_chr2 && $start_chr1_pos eq $chr1_pos && $start_chr2_pos eq $chr2_pos && $start_type eq $type) {
+                    $start = 0;
+                }
+                else {
+                    next SV;
+                }
+            }
+
             my $name = "$dir/${chr1}_${chr1_pos}_${chr2}_${chr2_pos}_*_${type}*.png";
             print "Opening files for ${chr1}_${chr1_pos}_${chr2}_${chr2_pos}_${type}\n";
             system("$viewer $name &");
             my $next = <STDIN>;
             chomp $next;
             if($next eq 'q') {
+                exit 0;
+            }
+            else {
                 last;
             }
-
         }
-
     }
-
     $indel_fh->close; 
 
     return 1;
