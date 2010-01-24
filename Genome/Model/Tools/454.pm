6a4a365ee9d926be5130a8f198729256904b8c60
ssmith <ssmith@genome.wustl.edu>
2010-01-24 20:57:24 +0000

    did perltidy on Genome/Model/Tools/454.pm since indentations was missing in code which emits an error.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54740 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/454.pm
+++ Genome/Model/Tools/454.pm
@@ -8,41 +8,41 @@ use Data::Dumper;
 use File::Temp;
 
 class Genome::Model::Tools::454 {
-    is => ['Command'],
+    is  => ['Command'],
     has => [
-            arch_os => {
-                        calculate => q|
+        arch_os => {
+            calculate => q|
                             my $arch_os = `uname -m`;
                             chomp($arch_os);
                             return $arch_os;
                         |
-                    },
-        ],
+        },
+    ],
     has_optional => [
-                     version => {
-                                 is    => 'string',
-                                 doc   => 'version of 454 application to use',
-                             },
-                     _tmp_dir => {
-                                  is => 'string',
-                                  doc => 'a temporary directory for storing files',
-                              },
-		     version_subdirectory => {
-			                      is => 'string',
-					      doc => '454 subdirectory name',
-					  },
-		     test_link => {
-			            is => 'string',
-				    doc => 'link names to default 454 versions',
-		     },
-                 ]
+        version => {
+            is  => 'string',
+            doc => 'version of 454 application to use',
+        },
+        _tmp_dir => {
+            is  => 'string',
+            doc => 'a temporary directory for storing files',
+        },
+        version_subdirectory => {
+            is  => 'string',
+            doc => '454 subdirectory name',
+        },
+        test_link => {
+            is  => 'string',
+            doc => 'link names to default 454 versions',
+        },
+    ]
 };
 
 sub help_brief {
-    "tools to work with 454 reads"
+    "tools to work with 454 reads";
 }
 
-sub help_detail {  
+sub help_detail {
     return <<EOS
 
 EOS
@@ -50,60 +50,71 @@ EOS
 
 sub create {
     my $class = shift;
-    my $self = $class->SUPER::create(@_);
+    my $self  = $class->SUPER::create(@_);
 
     my $link;
+
     #FOR TEST DATA .. GETS EITHER installed or newbler
-    if ($self->test_link) {
-	$link = $self->test_link;
-    }
-    #FOR ACTUAL RUNS .. GETS EITHER offIns or mapasm
-    elsif ($self->version_subdirectory) {
-	$link = $self->version_subdirectory;
-    }
-    #FOR REFERENCE ALIGNEMNT TESTS
-    else {
-	$link = 'installed';
+    if ( $self->test_link ) {
+        $link = $self->test_link;
     }
 
-    unless ($self->arch_os =~ /64/) {
-        $self->error_message('All 454 tools must be run from 64-bit architecture');
+    #FOR ACTUAL RUNS .. GETS EITHER offIns or mapasm
+    elsif ( $self->version_subdirectory ) {
+        $link = $self->version_subdirectory;
+    }
+
+    #FOR REFERENCE ALIGNEMNT TESTS
+    else {
+        $link = 'installed';
+    }
+
+    unless ( $self->arch_os =~ /64/ ) {
+        $self->error_message(
+            'All 454 tools must be run from 64-bit architecture');
         return;
     }
 
-    my $tempdir = File::Temp::tempdir(CLEANUP => 1);
+    my $tempdir = File::Temp::tempdir( CLEANUP => 1 );
     $self->_tmp_dir($tempdir);
-    unless ($self->version) {
-#	my $base_path = $self->resolve_454_path .'installed';
-	my $base_path = $self->resolve_454_path . $link;
-	if (-l $base_path) {
-	    my $link_path = readlink($base_path);
-	    if ($link_path =~ /^offInstrumentApps/){
-		unless ($link_path =~ /(offInstrumentApps)-(\d\.\d\.\d{2}\.\d{2})/) {
-		    $self->error_message('Link to 454 tools was malformed: '. $link_path);
-		    return;
-		}
-		$self->version($2);
-		$self->version_subdirectory($1);
-	    }
-	    elsif ($link_path =~ /^mapasm454_source/) {
-		unless ($link_path =~ /(mapasm454_source)_(\d{8})/) {
-		    $self->error_message('Link to 454 tools was malformed: '.$link_path);
-		    return;
-		}
-		$self->version($2);
-		$self->version_subdirectory($1);
-	    }
-	    else {
-		return;
-	    }
-	} else {
-	    $self->error_message('Expected symlink to installed software');
-	    return;
-	}
+    unless ( $self->version ) {
+
+        #	my $base_path = $self->resolve_454_path .'installed';
+        my $base_path = $self->resolve_454_path . $link;
+        if ( -l $base_path ) {
+            my $link_path = readlink($base_path);
+            if ( $link_path =~ /^offInstrumentApps/ ) {
+                unless (
+                    $link_path =~ /(offInstrumentApps)-(\d\.\d\.\d{2}\.\d{2})/ )
+                {
+                    $self->error_message(
+                        'Link to 454 tools was malformed: ' . $link_path );
+                    return;
+                }
+                $self->version($2);
+                $self->version_subdirectory($1);
+            }
+            elsif ( $link_path =~ /^mapasm454_source/ ) {
+                unless ( $link_path =~ /(mapasm454_source)_(\d{8})/ ) {
+                    $self->error_message(
+                        'Link to 454 tools was malformed: ' . $link_path );
+                    return;
+                }
+                $self->version($2);
+                $self->version_subdirectory($1);
+            }
+            else {
+                return;
+            }
+        }
+        else {
+            $self->error_message('Expected symlink to installed software');
+            return;
+        }
     }
-    unless ($self->version) {
-        $self->error_message('Failed to resolve version number of 454 applications');
+    unless ( $self->version ) {
+        $self->error_message(
+            'Failed to resolve version number of 454 applications');
         return;
     }
     return $self;
@@ -116,22 +127,31 @@ sub resolve_454_path {
 sub resolve_app_bin_name {
     my $self = shift;
     my $app_bin_name;
-    $app_bin_name = 'bin' if
-	$self->version_subdirectory eq 'offInstrumentApps';
-    $app_bin_name =  'applicationsBin' if
-	$self->version_subdirectory eq 'mapasm454_source';
+    $app_bin_name = 'bin'
+      if $self->version_subdirectory eq 'offInstrumentApps';
+    $app_bin_name = 'applicationsBin'
+      if $self->version_subdirectory eq 'mapasm454_source';
     return $app_bin_name;
 }
 
 sub bin_path {
     my $self = shift;
     my $bin_path;
+
     #difference is - vs _;
-    if ($self->version_subdirectory eq 'offInstrumentApps') {
-	$bin_path = $self->resolve_454_path.$self->version_subdirectory.'-'.$self->version.'/'.$self->resolve_app_bin_name;
+    if ( $self->version_subdirectory eq 'offInstrumentApps' ) {
+        $bin_path =
+            $self->resolve_454_path
+          . $self->version_subdirectory . '-'
+          . $self->version . '/'
+          . $self->resolve_app_bin_name;
     }
-    if ($self->version_subdirectory eq 'mapasm454_source') {
-	$bin_path = $self->resolve_454_path.$self->version_subdirectory.'_'.$self->version.'/'.$self->resolve_app_bin_name;
+    if ( $self->version_subdirectory eq 'mapasm454_source' ) {
+        $bin_path =
+            $self->resolve_454_path
+          . $self->version_subdirectory . '_'
+          . $self->version . '/'
+          . $self->resolve_app_bin_name;
     }
     return $bin_path;
 }
