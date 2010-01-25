be8fb3791a6f5bbf00903ee1cf7d8c767f75a1e9
ebelter <ebelter@genome.wustl.edu>
2010-01-25 22:29:31 +0000

    delete in build now abandons events - moved from remove; added G:M:C:B:Base for build commands; G:M:C:B:Remove inherits from Base;
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54777 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Command/Build/Remove.pm
+++ Genome/Model/Command/Build/Remove.pm
@@ -6,70 +6,39 @@ use warnings;
 use Genome;
 
 class Genome::Model::Command::Build::Remove {
-    is => ['Genome::Model::Command'],
-    has => [
-            build_id => {
-                         is => 'Number',
-                         doc => 'The id of the build in which to update status',
-                         is_optional => 1,
-                     },
-            build   => {
-                        is => 'Genome::Model::Build',
-                        id_by => 'build_id',
-                        is_optional => 1,
-                    },
-    ],
+    is => 'Genome::Model::Command::Build::Base',
     doc => 'delete a build and all of its data from the system'
 };
 
+#< Command >#
 sub sub_command_sort_position { 7 }
+
+sub help_brief {
+    return 'Remove a build and all of its data';
+}
 
 sub help_detail {
     "This command will remove the build and all events that make up the build";
 }
 
+#< Execute >#
 sub execute {
     my $self = shift;
-    my $model = $self->model;
-    unless ($self->build_id) {
-        $self->build_id($model->current_running_build_id);
-    }
 
-    my $build = $self->build;
-    unless ($build) {
-        $self->error_message('Build not found for model id '. $self->model_id .' and build id '. $self->build_id);
+    # Get build
+    my $build = $self->_resolve_build
+        or return;
+    
+    # Delete - also abandons
+    unless ( $build->delete ) {
+        $self->error_message('Failed to remove build ('.$build->id.')');
         return;
     }
 
-    # cribbed from genome model build abandon - kills running jobs
-    # if any events are running
-    my @events = $build->build_events;
-    foreach my $event (@events) {
-        if($event->event_status =~ /Running/)
-        {
-            my $rv = eval { $build->abandon; };
-            unless ($rv and not $@) {
-                $self->error_message(
-                    'Failed to abandon build '
-                    . $self->build_id 
-                    . ($@ ? " ERRORS: $@" : "")
-                    );
-                return;
-            }
-            last;
-        }
-    }
-
-    my @build_links = $build->to_build_links();  # The links that I'm the from_build
-    $_->delete foreach @build_links;
-
-    unless ($build->delete) {
-        $self->error_message('Failed to remove build '. $self->build_id);
-        return;
-    }
     return 1;
 }
 
-
 1;
 
+#$HeadURL$
+#$Id$
