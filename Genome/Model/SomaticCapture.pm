4ad1ed172308f7f442fd77224fbcae8c1013a05b
dkoboldt <dkoboldt@genome.wustl.edu>
2010-01-27 17:21:25 +0000

    Somatic capture
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54838 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/SomaticCapture.pm
@@ -0,0 +1,38 @@
+package Genome::Model::SomaticCapture;
+#:adukes short term, keep_n_most_recent_builds shouldn't have to be overridden like this here.  If this kind of default behavior is acceptable, it belongs in the base class
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::SomaticCapture {
+    is  => 'Genome::Model',
+    has => [
+       only_tier_1 => { via => 'processing_profile'},
+       min_mapping_quality => { via => 'processing_profile'},
+       min_somatic_quality => { via => 'processing_profile'},
+       skip_sv => { via => 'processing_profile'},
+    ],
+    has_optional => [
+         tumor_model_links                  => { is => 'Genome::Model::Link', reverse_as => 'to_model', where => [ role => 'tumor'], is_many => 1,
+                                               doc => '' },
+         tumor_model                     => { is => 'Genome::Model', via => 'tumor_model_links', to => 'from_model', 
+                                               doc => '' },
+         normal_model_links                  => { is => 'Genome::Model::Link', reverse_as => 'to_model', where => [ role => 'normal'], is_many => 1,
+                                               doc => '' },
+         normal_model                     => { is => 'Genome::Model', via => 'normal_model_links', to => 'from_model', 
+                                               doc => '' },
+    ],
+};
+
+
+# we get a failure during verify successful completion
+# if we don't have this...
+sub keep_n_most_recent_builds
+{
+    my $self = shift;
+    return;
+}
+
+1;
