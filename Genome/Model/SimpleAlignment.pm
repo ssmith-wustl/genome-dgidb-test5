1c1ea5457f79cffbb8f7dfd87f054b63ef090624
jpeck <jpeck@genome.wustl.edu>
2010-01-29 16:58:03 +0000

    First draft of model.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@54918 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/SimpleAlignment.pm
@@ -0,0 +1,27 @@
+package Genome::Model::SimpleAlignment;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::SimpleAlignment {
+    is  => 'Genome::Model',
+    has => [
+       reference_sequence_name => { via => 'processing_profile'},
+    ],
+    
+};
+
+
+# we get a failure during verify successful completion
+# if we don't have this...
+sub keep_n_most_recent_builds
+{
+    my $self = shift;
+    return;
+}
+
+
+1
+;
