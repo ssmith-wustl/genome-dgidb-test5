5d01cc71ade62b5dc2f6d6653d27d3e13c292686
abrummet <abrummet@genome.wustl.edu>
2010-02-05 23:07:52 +0000

    Change obsolete call from alignments to alignment_sets
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55134 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ReferenceAlignment/Report/ReferenceCoverage.pm
+++ Genome/Model/ReferenceAlignment/Report/ReferenceCoverage.pm
@@ -87,7 +87,7 @@ sub _generate_summary {
     my $aligned_reads = 0;
     my @idas = $self->build->instrument_data_assignments;
     for my $ida (@idas) {
-        my @alignments = $ida->alignments;
+        my @alignments = $ida->alignment_sets;
         for my $alignment (@alignments) {
             my $stats = $alignment->get_alignment_statistics;
             $total_reads += $stats->{total};
