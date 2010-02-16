014f96af998829a9dd52b23e93716c89e5a10b09
jwalker <jwalker@genome.wustl.edu>
2010-02-16 18:05:20 +0000

    A base class/command for all tools that use cufflinks and it's output.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55445 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Cufflinks.pm
@@ -0,0 +1,85 @@
+package Genome::Model::Tools::Cufflinks;
+
+use strict;
+use warnings;
+
+use Genome;
+use File::Basename;
+
+my $DEFAULT = '0.8.0';
+
+class Genome::Model::Tools::Cufflinks {
+    is => 'Command',
+    has => [
+        use_version => { is => 'Version', is_optional => 1, default_value => $DEFAULT, doc => "Version of cufflinks to use, default is $DEFAULT" },
+        arch_os => {
+                    calculate => q|
+                            my $arch_os = `uname -m`;
+                            chomp($arch_os);
+                            return $arch_os;
+                        |
+                },
+    ],
+};
+
+
+sub help_brief {
+    "Tools to run Cufflinks or work with its output files.",
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+genome-model tools cufflinks ...    
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+More information about the Cufflinks aligner can be found at http://cufflinks.cbcb.umd.edu/.
+EOS
+}
+
+
+my %CUFFLINKS_VERSIONS = (
+    '0.7.0'  => '/gsc/pkg/bio/cufflinks/cufflinks-0.7.0.Linux_x86_64',
+    '0.8.0'  => '/gsc/pkg/bio/cufflinks/cufflinks-0.8.0.Linux_x86_64',
+);
+
+sub cuffcompare_path {
+    my $self = $_[0];
+    return $self->path_for_cufflinks_version($self->use_version) .'/cuffcompare';
+}
+
+sub cuffdiff_path {
+    my $self = $_[0];
+    return $self->path_for_cufflinks_version($self->use_version) .'/cuffdiff';
+}
+
+sub cufflinks_path {
+    my $self = $_[0];
+    return $self->path_for_cufflinks_version($self->use_version) .'/cufflinks';
+}
+
+sub available_cufflinks_versions {
+    my $self = shift;
+    return keys %CUFFLINKS_VERSIONS;
+}
+
+sub path_for_cufflinks_version {
+    my $class = shift;
+    my $version = shift;
+
+    if (defined $CUFFLINKS_VERSIONS{$version}) {
+        return $CUFFLINKS_VERSIONS{$version};
+    }
+    die('No path for cufflinks version '. $version);
+}
+
+sub default_cufflinks_version {
+    die "default cufflinks version: $DEFAULT is not valid" unless $CUFFLINKS_VERSIONS{$DEFAULT};
+    return $DEFAULT;
+}
+
+1;
+
