321d10355ee9ad4941dc8d310231f41c33aa3520
ssmith <ssmith@genome.wustl.edu>
2010-02-18 22:13:56 +0000

    Boilerplate to prevent errors with test pipeline processing profile until other new code is committed
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55597 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Build/TestPipeline.pm
@@ -0,0 +1,9 @@
+package Genome::Model::Build::TestPipeline;
+use strict;
+use warnings;
+use Genome;
+class Genome::Model::Build::TestPipeline {
+    is => 'Genome::Model::Build'
+};
+1;
+
---
+++ Genome/Model/TestPipeline.pm
@@ -0,0 +1,9 @@
+package Genome::Model::TestPipeline;
+use strict;
+use warnings;
+use Genome;
+class Genome::Model::TestPipeline {
+    is => 'Genome::Model'
+};
+1;
+
