82d353a653dcf11215c864ae2669a57a58510674
jwalker <jwalker@genome.wustl.edu>
2010-02-23 18:53:28 +0000

    updated the documentation extensively and forced check of output file or directory
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55767 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/BioSamtools/RefCov.pm
+++ Genome/Model/Tools/BioSamtools/RefCov.pm
@@ -12,7 +12,7 @@ class Genome::Model::Tools::BioSamtools::RefCov {
             doc => 'A BAM file, sorted and indexed, containing alignment/read data',
         },
         bed_file => {
-            doc => 'The BED format (chr\tstart\tend\tname) file containing annotation or regions of interest.',
+            doc => 'The BED format file (tab delimited: chr,start,end,name) file containing annotation or regions of interest.',
         },
         output_directory => {
             doc => 'When run in parallel, this directory will contain all output and intermediate STATS files. Do not define if stats_file is defined.',
@@ -49,11 +49,49 @@ class Genome::Model::Tools::BioSamtools::RefCov {
     ],
 };
 
+sub help_detail {
+'
+These commands are setup to run perl v5.10.0 scripts that use Bio-Samtools and require bioperl v1.6.0.  They all require 64-bit architecture.
+
+Output file format(stats_file):
+[1] Region Name (column 4 of BED file)
+[2] Percent of Reference Bases Covered
+[3] Total Number of Reference Bases
+[4] Total Number of Covered Bases
+[5] Number of Missing Bases
+[6] Average Coverage Depth
+[7] Standard Deviation Average Coverage Depth
+[8] Median Coverage Depth
+[9] Number of Gaps
+[10] Average Gap Length
+[11] Standard Deviation Average Gap Length
+[12] Median Gap Length
+[13] Min. Depth Filter
+[14] Discarded Bases (Min. Depth Filter)
+[15] Percent Discarded Bases (Min. Depth Filter)
+';
+}
+
+sub create {
+    my $class = shift;
+    my $self = $class->SUPER::create(@_);
+    if ($self->output_directory) {
+        if ($self->stats_file) {
+            die('No need to define stats_file when output_directory is defined');
+        }
+    } elsif (!defined($self->stats_file)) {
+        die ('Must define stats_file or output_directory');
+    }
+    return $self;
+}
+
 sub execute {
     my $self = shift;
 
-    unless (-e $self->output_directory){
-        Genome::Utility::FileSystem->create_directory($self->output_directory);
+    if ($self->output_directory) {
+        unless (-e $self->output_directory){
+            Genome::Utility::FileSystem->create_directory($self->output_directory);
+        }
     }
     unless ($self->stats_file) {
         my ($bam_basename,$bam_dirname,$bam_suffix) = File::Basename::fileparse($self->bam_file,qw/.bam/);
