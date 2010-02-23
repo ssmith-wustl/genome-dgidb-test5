95deb95db839dab785f712ffefa76f86ad7338bb
ssmith <ssmith@genome.wustl.edu>
2010-02-23 02:52:25 +0000

    removed empty model/build subclasses which go with Genome::ProcessingProfile::TestPipeline
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55731 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build/TestPipeline.pm
+++
@@ -1,9 +0,0 @@
-package Genome::Model::Build::TestPipeline;
-use strict;
-use warnings;
-use Genome;
-class Genome::Model::Build::TestPipeline {
-    is => 'Genome::Model::Build'
-};
-1;
-
--- Genome/Model/TestPipeline.pm
+++
@@ -1,9 +0,0 @@
-package Genome::Model::TestPipeline;
-use strict;
-use warnings;
-use Genome;
-class Genome::Model::TestPipeline {
-    is => 'Genome::Model'
-};
-1;
-
