f82137335529e27e7f4ca9a55d0ea867f7f9d446
kkyung <kkyung@genome.wustl.edu>
2010-02-24 19:29:41 +0000

    updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55808 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/ImportedAssembly.pm
+++ Genome/Model/ImportedAssembly.pm
@@ -5,6 +5,9 @@ use warnings;
 
 use Genome;
 
+#CREATING MODEL SOLELY FOR TRACKING IT'S LOCATION
+#VIA $model->data_directory
+
 class Genome::Model::ImportedAssembly {
     is => 'Genome::Model',
 };
