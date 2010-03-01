77649f3f496988dbdadfdf3f35de222529d0f160
ssmith <ssmith@genome.wustl.edu>
2010-03-01 04:50:22 +0000

    message updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@55913 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/TestPipeline.pm
+++ Genome/ProcessingProfile/TestPipeline.pm
@@ -20,13 +20,13 @@ class Genome::ProcessingProfile::TestPipeline {
 
 sub _initialize_model {
     my ($self,$model) = @_;
-    warn "defining new model for " . $self->__display_name__ . ':' .  $model->__display_name__ . "\n";
+    warn "defining new model " . $model->__display_name__ . " for profile " . $self->__display_name__ . "\n";
     return 1;
 }
 
 sub _initialize_build {
     my ($self,$build) = @_;
-    warn "defining new build for " . $self->__display_name__ . ':' .  $build->__display_name__ . "\n";
+    warn "defining new build " . $build->__display_name__ . " for profile " . $self->__display_name__ . "\n";
     return 1;
 }
 
