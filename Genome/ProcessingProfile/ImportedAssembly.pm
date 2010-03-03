8baee744c9f74cd70f06a8ca3864abe1ca64bf64
kkyung <kkyung@genome.wustl.edu>
2010-03-03 20:12:14 +0000

    Updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56064 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ImportedAssembly.pm
+++ Genome/ProcessingProfile/ImportedAssembly.pm
@@ -21,59 +21,6 @@ class Genome::ProcessingProfile::ImportedAssembly {
     doc => "Processing profile to track manually created assemblies",
 };
 
-sub _initialize_model {
-    my ($self,$model) = @_;
-    warn "defining new model " . $model->__display_name__ . " for profile " . $self->__display_name__ . "\n";
-    return 1;
-}
-
-sub _initialize_build {
-    my ($self,$build) = @_;
-    warn "defining new build " . $build->__display_name__ . " for profile " . $self->__display_name__ . "\n";
-    return 1;
-}
-
-sub _execute_build {
-    my ($self,$build) = @_;
-    warn "executing build logic for " . $self->__display_name__ . ':' .  $build->__display_name__ . "\n";
-
-    my $cmd = $self->command_name;
-    my $args = $self->args;
-
-    my $dir = $build->data_directory;
-
-    my $exit_code = system "$cmd $args >$dir/output 2>$dir/errors";
-    $exit_code /= 256;
-    if ($exit_code != 0) {
-        $self->error_message("Failed to run $cmd with args $args!  Exit code: $exit_code.");
-        return;
-    }
-
-    return 1;
-}
-
-sub _validate_build {
-    my $self = shift;
-    my $dir = $self->data_directory;
-    
-    my @errors;
-    unless (-e "$dir/output") {
-        my $e = $self->error_message("No output file $dir/output found!");
-        push @errors, $e;
-    }
-    unless (-e "$dir/errors") {
-        my $e = $self->error_message("No output file $dir/errors found!");
-        push @errors, $e;
-    }
-
-    if (@errors) {
-        return;
-    }
-    else {
-        return 1;
-    }
-}
-
 sub stages {
     return (qw/ ImportedAssembly /);
 }
