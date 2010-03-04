f65103adf0f3ec42a376f2490fa48458ea9cb1d7
kkyung <kkyung@genome.wustl.edu>
2010-03-04 19:19:55 +0000

    updates
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56145 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ImportedAssembly.pm
+++ Genome/ProcessingProfile/ImportedAssembly.pm
@@ -2,24 +2,35 @@ package Genome::ProcessingProfile::ImportedAssembly;
 
 use strict;
 use warnings;
+
 use Genome;
 
 class Genome::ProcessingProfile::ImportedAssembly {
     is => 'Genome::ProcessingProfile',
     has_param => [
-        command_name => {
-            doc => 'the name of a single command to run',
-        },
-        args => {
-            is_optional => 1,
-            doc => 'the arguments to use',
-        },
+	assembler_name => {
+	    doc => 'Assembler used to generate the assembly',
+	},
+	assembler_version => {
+	    is_optional => 1,
+	    doc => 'Version of the assembler used to generate the assembly',
+	},
 	assembly_description => {
-	    doc => 'description of the assembly',
-	}
+	    is_optional => 1,
+	    doc => 'Special description of the assembly',
+	},
     ],
     doc => "Processing profile to track manually created assemblies",
 };
+
+#MORE LATER IF NECESSARY
+sub _validate_assembler {
+    return 1;
+}
+#MORE LATER IF NECESSARY
+sub _validate_assembler_version {
+    return 1;
+}
 
 sub stages {
     return (qw/ ImportedAssembly /);
