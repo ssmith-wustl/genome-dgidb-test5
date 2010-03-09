6ffa83e6dee4863ef2f248f8f474da43e777b1d0
ssmith <ssmith@genome.wustl.edu>
2010-03-09 17:08:03 +0000

    fixed whitespace
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56318 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ImportedAssembly.pm
+++ Genome/ProcessingProfile/ImportedAssembly.pm
@@ -8,17 +8,17 @@ use Genome;
 class Genome::ProcessingProfile::ImportedAssembly {
     is => 'Genome::ProcessingProfile',
     has_param => [
-	assembler_name => {
-	    doc => 'Assembler used to generate the assembly',
-	},
-	assembler_version => {
-	    is_optional => 1,
-	    doc => 'Version of the assembler used to generate the assembly',
-	},
-	assembly_description => {
-	    is_optional => 1,
-	    doc => 'Special description of the assembly',
-	},
+        assembler_name => {
+            doc => 'Assembler used to generate the assembly',
+        },
+        assembler_version => {
+            is_optional => 1,
+            doc => 'Version of the assembler used to generate the assembly',
+        },
+        assembly_description => {
+            is_optional => 1,
+            doc => 'Special description of the assembly',
+        },
     ],
     doc => "Processing profile to track manually created assemblies",
 };
