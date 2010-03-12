d58eb225cd63eb938dac948526a4e07a70e49039
jwalker <jwalker@genome.wustl.edu>
2010-03-12 14:56:18 +0000

    Add include and exclude flags to the tool and create a test case to cover each.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56472 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Sam/BamToFastq.pm
+++ Genome/Model/Tools/Sam/BamToFastq.pm
@@ -12,14 +12,35 @@ class Genome::Model::Tools::Sam::BamToFastq {
     is  => 'Genome::Model::Tools::Sam',
     has => [
 	bam_file => {
-	    is  => 'String',
-	    doc => 'Input File',
+	    is  => 'Text',
+	    doc => 'Input BAM File',
 	},
 	fastq_file => {
-	    is => 'Boolean',
+	    is => 'Text',
 	    is_optional => 1,
-	    doc => 'Output File',
+	    doc => 'Output FASTQ File',
 	},
+        include_flag => {
+            is => 'Text',
+            is_optional => 1,
+            doc => 'A bit-wise flag used to include specific alignments/reads:
+               0x0001 => the read is paired in sequencing,
+               0x0002 => the read is mapped in a proper pair,
+               0x0004 => the query sequence itself is unmapped,
+               0x0008 => the mate is unmapped,
+               0x0010 => strand of query (0 for forward; 1 for reverse strand),
+               0x0020 => strand of mate,
+               0x0040 => the read is the first read in a pair,
+               0x0080 => the read is the second read in a pair,
+               0x0100 => the alignment is not primary,
+               0x0200 => the read failes platform/vendor quality checks,
+               0x0400 => the read is either a PCR duplicate or an optical duplicate',
+        },
+        exclude_flag => {
+            is => 'Text',
+            is_optional => 1,
+            doc => 'A bit-wise flag use to exclude specific alignments/reads:  see options for include_flag',
+        },
     ],
 };
 
@@ -35,7 +56,14 @@ sub execute {
     my ($output_basename,$output_dirname,$output_suffix) = File::Basename::fileparse($self->fastq_file,qw/txt fastq fq/);
     my $tmp_dir = File::Temp::tempdir( DIR => $output_dirname, CLEANUP => 1 );
     my $tmp_sam_file = $tmp_dir .'/'. $input_basename .'.sam';
-    my $cmd = $self->samtools_path .' view -o '. $tmp_sam_file .' '. $self->bam_file;
+    my $params = ' -o '. $tmp_sam_file;
+    if ($self->include_flag) {
+        $params .= ' -f '. $self->include_flag;
+    }
+    if ($self->exclude_flag) {
+        $params .= ' -F '. $self->exclude_flag;
+    }
+    my $cmd = $self->samtools_path .' view '. $params .' '. $self->bam_file;
     Genome::Utility::FileSystem->shellcmd(
         cmd => $cmd,
         input_files => [$self->bam_file],
