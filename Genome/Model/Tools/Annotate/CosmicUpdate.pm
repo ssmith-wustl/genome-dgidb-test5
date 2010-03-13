ebf4139373803867d7d78442c3f1599d67839c43
wschierd <wschierd@genome.wustl.edu>
2010-03-13 00:18:05 +0000

    defaults set properly
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56495 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Annotate/CosmicUpdate.pm
+++ Genome/Model/Tools/Annotate/CosmicUpdate.pm
@@ -23,9 +23,9 @@ class Genome::Model::Tools::Annotate::CosmicUpdate {
 	is => 'Command',                       
 	
 	has => [                                # specify the command's single-value properties (parameters) <--- 
-		cosmic_folder	=> { is => 'Text', doc => "Path to the current local COSMIC files", is_optional => 1 },
-		cosmic_url	=> { is => 'Text', doc => "URL to the online COSMIC repository", is_optional => 1 },
-		output_file	=> { is => 'Text', doc => "Output file name for flatfile of amino acid changes" , is_optional => 1},
+		cosmic_folder	=> { is => 'Text', doc => "Path to the current local COSMIC files", is_optional => 1 , default => '/gscmnt/sata180/info/medseq/biodb/shared/cosmic/cosmic_will/' },
+		cosmic_url	=> { is => 'Text', doc => "URL to the online COSMIC repository", is_optional => 1 , default => 'ftp://ftp.sanger.ac.uk/pub/CGP/cosmic/data_export/genes/' },
+		output_file	=> { is => 'Text', doc => "Output file name for flatfile of amino acid changes" , is_optional => 1 , default => 'Cosmic_Database.tsv' },
 	],
 };
 
@@ -56,16 +56,8 @@ sub execute {                               # replace with real execution logic.
 
 my $self = shift;
 my $URL = $self->cosmic_url;
-my $COSMICpath = $self->cosmic_folder;
+my $cosmicdir = $self->cosmic_folder;
 my $cosmicdb = $self->output_file;
-
-my $cosmicdir;
-if (defined $COSMICpath) {
-	$cosmicdir = $COSMICpath;
-}
-else {
-	$cosmicdir = '/gscmnt/sata180/info/medseq/biodb/shared/cosmic/cosmic_will/';
-}
 
 chdir ($cosmicdir);
 my $dir = getcwd;
@@ -74,7 +66,6 @@ print "Working Directory: $dir";
 print "Retrieving File(s) from COSMIC\n";
 
 #directory on COSMIC server with all GENE folders
-$URL ||= 'ftp://ftp.sanger.ac.uk/pub/CGP/cosmic/data_export/genes/';
 system ( wget, "\-r" , "\-A.csv" , "\-nd" , "\-N" , "\-l2" , $URL);
 
 print "Finished Downloading COSMIC File(s), Moving on to Writing COSMIC Flatfile\n";
@@ -93,7 +84,7 @@ foreach my $file (@cosmicdirfiles) {
   }
 }
 
-$cosmicdb ||= 'Cosmic_Database.tsv';
+
 unless (open(COSMIC_DB,">$cosmicdb")) {
     die "Could not open output file '$cosmicdb' for writing";
 }
