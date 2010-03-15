0662a71a817245f0ef7fac5df1b886d221dd1936
josborne <josborne@genome.wustl.edu>
2010-03-15 14:29:54 +0000

    pulled out workflow based system for now.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56504 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Ber/BerRunBlastphmmpfam.pm
+++ Genome/Model/Tools/Ber/BerRunBlastphmmpfam.pm
@@ -13,129 +13,165 @@ use Bio::Seq;
 use Bio::DB::BioDB;
 use Bio::DB::Query::BioQuery;
 
-use Workflow;
-use Workflow::Simple;
 use Data::Dumper;
 use IO::File;
 use IPC::Run qw/ run timeout /;
 use Time::HiRes qw(sleep);
-use File::Slurp;
-use File::Temp;
 
 use Cwd;
 
-## FIXME: locus_tag isn't used at all here.
-
 UR::Object::Type->define(
-    class_name => __PACKAGE__,
-    is         => 'Command',
-    has        => [
-        'locus_tag' => {
-            is  => 'String',
-            doc => "Locus tag for project, containing DFT/FNL postpended",
-        },
-        'proteinnamefile' => {
-            is  => 'String',
-            doc => "Protein name file for all genes dumped from BioSQL",
-        },
-        'fastadirpath' => {
-            is  => 'String',
-            doc => "fasta directory path",
-        },
-        'berdirpath' => {
-            is  => 'String',
-            doc => "blastp directory path",
-        },
-        'hmmdirpath' => {
-            is  => 'String',
-            doc => "hmmpfam directory path",
-        },
-        'bsubfiledirpath' => {
-            is  => 'String',
-            doc => "bsub error files directory path",
-        },
-        'blpqueryfile' => {
-            is  => 'String',
-            doc => "blastp query file",
-        },
-        'hmmdatabase' => {
-            is  => 'String',
-            doc => "hmmpfam query file",
-        },
-    ]
-);
+			 class_name => __PACKAGE__,
+			 is         => 'Command',
+			 has        => [
+					'locus_tag'       => {
+							      is  => 'String',
+							      doc => "Locus tag for project, containing DFT/FNL postpended",
+							     },
+					'proteinnamefile' => {
+							      is  => 'String',
+							      doc => "Protein name file for all genes dumped from BioSQL",
+							     },
+					'fastadirpath'    => {
+							      is  => 'String',
+							      doc => "fasta directory path",
+							     },
+					'berdirpath'      => {
+							       is  => 'String',
+							       doc => "blastp directory path",
+							     } ,
+					'hmmdirpath'      => {
+					                       is  => 'String',
+							       doc => "hmmpfam directory path",
+                                                             },
+					'bsubfiledirpath' => {
+							      is  => 'String',
+							      doc => "bsub error files directory path",
+							     } ,
+					'blpqueryfile'    => {
+                                                              is => 'String',
+							      doc => "blastp query file",
+                                                             } ,
+					'hmmdatabase'     => {
+					                      is => 'String',
+							      doc => "hmmpfam query file",
+					                      } ,
+				       ]
+			);
 
 sub help_brief
-{
+  {
     "Tool for Running Blastp and hmmpfam for BER product naming pipeline";
-}
+  }
 
 sub help_synopsis
-{
+  {
     return <<"EOS"
       Tool for Running Blastp and hmmpfam for BER product naming pipeline.
 EOS
-}
+  }
 
 sub help_detail
-{
+  {
     return <<"EOS"
 Tool for Running Blastp and hmmpfam for BER product naming pipeline.
 EOS
-}
+  }
+
 
 sub execute
-{
+  {
     my $self = shift;
 
-#    my @berrunblastphmmpfam = $self->gather_details();
-    my @sequence_names = $self->get_sequence_names();
+    my @berrunblastphmmpfam = $self->gather_details();
+
     my $i = 0;
-#    print STDERR
-#                   "berdirpath ", $self->berdirpath,"\n",
-#                   "hmmdirpath ", $self->hmmdirpath,"\n",
-#                   "blastp query file " , $self->blpqueryfile,"\n",
-#                   "hmm database " , $self->hmmdatabase,"\n",
-#                   "fasta dir " , $self->fastadirpath , "\n",
-#                   "seq names " , "[ ", join(',',@sequence_names)," ]\n";
+    foreach my $runs (@berrunblastphmmpfam) {
+	$i++;
+	#print qq{\n\n@$runs\n\n};
 
-    # FIXME: need to check existance on the files and paths
-    my ($wfxml_fh,$wfxml) = File::Temp::tempfile("ber-blast-hmmpfam-XXXXXX", DIR => $self->berdirpath , SUFFIX => '.xml');
-    $self->status_message("starting blastp/hmmpfam jobs");
-    write_file($wfxml,$self->workflowxml() );
-    my $result = run_workflow_lsf( $wfxml,
-                   berdirpath => $self->berdirpath,
-                   hmmdirpath => $self->hmmdirpath,
-                   'blastp query file' => $self->blpqueryfile,
-                   'hmm database' => $self->hmmdatabase,
-                   'fasta dir' => $self->fastadirpath ,
-                   'seq names' => \@sequence_names,
-                     );
+	#print Dumper(@$runs);
 
-    unless(defined($result))
-    {
-        foreach my $error (@Workflow::Simple::ERROR) {
-            $self->error_message( join("\t",
-                                       $error->dispatch_identifier(),
-                                       $error->name(),
-                                       $error->start_time(),
-                                       $error->end_time(),
-                                       $error->exit_code(),
-                                      )
-                                );
-            $self->error_message( $error->stdout() );
-            $self->error_message( $error->stderr() );
-        }
-        croak;
+	#print qq{\n\n$i\n\n};
+	IPC::Run::run(@$runs) || croak "\n\nCan't run BerRunBlastphmmpfam.pm from BerRunBlastphmmpfam.pm: $OS_ERROR\n\n";
+
+    }
+
+    print qq{\n\nTotal Jobs for Blastp/Hmmpfam:\t$i\n\n};
+
+    while ( 1 ) {
+
+	my $berdirpath = $self->berdirpath;
+	my $hmmdirpath = $self->hmmdirpath;
+
+	opendir (BERDIR, "$berdirpath")or die "Can't open '$berdirpath' ...  from BerRunBlastphmmpfam.pm : $OS_ERROR\n";
+	my @berfile = readdir (BERDIR);
+	closedir (BERDIR);
+
+	my @storageber = ( );
+	foreach my $bername (@berfile) {
+	    next if $bername =~ m/^\.\.?$/;
+	    my $nr           = qq{.nr};
+
+	    if ( $bername =~ /$nr$/ ) {
+		my $bernamecheck = qq{$berdirpath/$bername};
+		if ( (-e $bernamecheck) && (! -z $bernamecheck)) {
+		    my $size = -s $bernamecheck;
+		    if ( $size > 550 ) {
+			push (@storageber, $bername);
+		    }
+		}
+	    }
+	}
+
+	my $bercount = $#storageber + 1;
+
+	opendir (HMMDIR, "$hmmdirpath")or die "Can't open '$hmmdirpath' ...  from BerRunBlastphmmpfam.pm : $OS_ERROR\n";
+	my @hmmfile = readdir (HMMDIR);
+	closedir (HMMDIR);
+
+	my @storagehmm = ( );
+	foreach my $hmmname (@hmmfile) {
+	    next if $hmmname =~ m/^\.\.?$/;
+	    my $hmmpfam      = qq{.hmmpfam};
+	    if  ($hmmname =~ /$hmmpfam$/) {
+		my $hmmnamecheck = qq{$hmmdirpath/$hmmname};
+		if (( -e $hmmnamecheck) && ( ! -z $hmmnamecheck )){
+		    my $size = -s $hmmnamecheck;
+		    if ( $size > 550 ){
+			push (@storagehmm, $hmmname);
+		    }
+
+            # need to check if there is an error here, then try to resubmit or something???
+            my $chkcmd = "grep -H \"Exited with exit code\" $hmmnamecheck";
+            my $rc = system($chkcmd);
+            if($rc == 0) {
+                warn "errors in $hmmnamecheck - this portion may need to be rerun";
+            }
+		}
+	    }
+	}
+
+	my $hmmcount = $#storagehmm + 1;
+
+	my $totalcount = $bercount + $hmmcount;
+	unless ( $i == $totalcount ) {
+	    sleep 900;
+	    next;
+	}
+	else {
+	    sleep 120;
+	    last;
+	}
     }
 
     return 1;
 
-}
+  }
 
 sub gather_details
-{
-    my $self            = shift;
+  {
+    my $self = shift;
     my $locus_tag       = $self->locus_tag;
     my $proteinnamefile = $self->proteinnamefile;
     my $fastadirpath    = $self->fastadirpath;
@@ -147,178 +183,106 @@ sub gather_details
 
     my @allipc;
     my $cwd = getcwd();
-    unless ( $cwd eq $fastadirpath )
-    {
-        chdir($fastadirpath)
-            or croak
-            "Failed to change to '$fastadirpath', from BerRunBlastphmmpfam.pm: $OS_ERROR";
+    unless ( $cwd eq $fastadirpath ) {
+	chdir($fastadirpath)  or croak "Failed to change to '$fastadirpath', from BerRunBlastphmmpfam.pm: $OS_ERROR";
     }
 
     $proteinnamefile = qq{$fastadirpath/$proteinnamefile};
-    unless ( -e $proteinnamefile )
-    {
-        croak
-            qq{\n\n NO, $proteinnamefile file found, from BerRunBlastphmmpfam.pm : $OS_ERROR \n\n};
+    unless ( -e $proteinnamefile ) {
+      croak qq{\n\n NO, $proteinnamefile file found, from BerRunBlastphmmpfam.pm : $OS_ERROR \n\n};
     }
     my $proteinnamefile_fh = IO::File->new();
     $proteinnamefile_fh->open("<$proteinnamefile")
-        or croak
-        "Can't open '$proteinnamefile' from BerRunBlastphmmpfam.pm : $OS_ERROR";
+      or croak "Can't open '$proteinnamefile' from BerRunBlastphmmpfam.pm : $OS_ERROR";;
 
     my $blpcount  = 0;
     my $hmpfcount = 0;
 
-    # FIXME: this so needs to be turned into a workflow.
-    while ( my $line = <$proteinnamefile_fh> )
-    {
+    while (my $line = <$proteinnamefile_fh>) {
 
-        chomp($line);
-        my $file = qq{$fastadirpath/$line};
+      chomp($line);
+      my $file = qq{$fastadirpath/$line};
 
-        unless ( -e $file )
-        {
-            croak
-                qq{\n\n NO, $file, found for blastp from BerRunBlastphmmpfam.pm : $OS_ERROR \n\n };
-        }
+      unless ( -e $file ) {
+	  croak qq{\n\n NO, $file, found for blastp from BerRunBlastphmmpfam.pm : $OS_ERROR \n\n };
+      }
 
-        #blastp send to lsf
+      #blastp send to lsf
 
-        my $blpout = qq{$berdirpath/$line.nr};
-        my $blperr = qq{$bsubfiledirpath/bsub.err.blp.$line};
+      my $blpout        = qq{$berdirpath/$line.nr};
+      my $blperr        = qq{$bsubfiledirpath/bsub.err.blp.$line};
+     
+      my @blastpcmd = (
+		       'blastp',
+		       $blpqueryfile,
+		       $file,
+		      );
 
-        my @blastpcmd = ( 'blastp', $blpqueryfile, $file, );
+      my $Rbp    = qq {rusage[mem=4096]};
 
-        my $Rbp = qq {rusage[mem=4096]};
+      my @bsubcmdbp = (
+		       'bsub',
+		       '-o',
+		       $blpout,
+		       '-e',
+		       $blperr,
+		       '-q',
+		       'long',
+		       '-n',
+		       '1',
+		       '-R',
+		       $Rbp,
+		       @blastpcmd,
+		      );
 
-        my @bsubcmdbp = (
-            'bsub', '-o', $blpout, '-e', $blperr, '-q',
-            'long', '-n', '1',     '-R', $Rbp,    @blastpcmd,
-        );
+      $blpcount++;
 
-        $blpcount++;
+      my @bpipc = (
+	          \@bsubcmdbp,
+	          \undef,
+	          '2>&1',
+	          );
 
-        my @bpipc = ( \@bsubcmdbp, \undef, '2>&1', );
+      push(@allipc, \@bpipc);
 
-        push( @allipc, \@bpipc );
+      #hmmpfam send to lsf
 
-        #hmmpfam send to lsf
+      my $hmpfmout  = qq{$hmmdirpath/$line.hmmpfam};
+      my $hmpfmerr  = qq{$bsubfiledirpath/bsub.err.hmm.$line};
+     
+      my @hmmpfcmd = (
+	             'hmmpfam',
+                 '--cpu', '1',
+		     $hmmdatabase,
+		     $file,
+		     );
 
-        my $hmpfmout = qq{$hmmdirpath/$line.hmmpfam};
-        my $hmpfmerr = qq{$bsubfiledirpath/bsub.err.hmm.$line};
+      my @bsubcmdhf = (
+	              'bsub',
+		      '-o',
+		      $hmpfmout,
+		      '-e',
+		      $hmpfmerr,
+		      '-q',
+		      'long',
+		      '-n',
+		      '1',
+		      '-R',
+		      $Rbp,
+		      @hmmpfcmd,
+		      );
 
-        my @hmmpfcmd = ( 'hmmpfam', '--cpu', '1', $hmmdatabase, $file, );
+      my @hmmpfipc = (
+	           \@bsubcmdhf,
+		   \undef,
+		   '2>&1',
+                   );
 
-        my @bsubcmdhf = (
-            'bsub',    '-o', $hmpfmout, '-e',
-            $hmpfmerr, '-q', 'long',    '-n',
-            '1',       '-R', $Rbp,      @hmmpfcmd,
-        );
+      push(@allipc, \@hmmpfipc );
 
-        my @hmmpfipc = ( \@bsubcmdhf, \undef, '2>&1', );
-
-        push( @allipc, \@hmmpfipc );
-
-    }
+  }
 
     return @allipc;
 
 }
 
-sub get_sequence_names
-{
-    my $self = shift;
-    my $protnamefile = $self->proteinnamefile;
-    # FIXME: check existance of $self->proteinnamefile.  It should be the full path,
-    # and not just some filename....
-    my @names = read_file($protnamefile) or croak "can't open $protnamefile : $OS_ERROR";
-    chomp @names;
-    return @names;
-}
-
-sub workflowxml
-{
-    my $self = shift;
-    return qq(<?xml version='1.0' standalone='yes'?>
-<workflow name="BER blastp and hmmpfam">
-
-  <link fromOperation="input connector" fromProperty="berdirpath" toOperation="BER Blastp" toProperty="berdirpath" />
-  <link fromOperation="input connector" fromProperty="hmmdirpath" toOperation="BER Hmmpfam" toProperty="hmmdirpath" />
-
-  <link fromOperation="input connector" fromProperty="blastp query file" toOperation="BER Blastp" toProperty="blastp_query" />
-  <link fromOperation="input connector" fromProperty="hmm database" toOperation="BER Hmmpfam" toProperty="hmm_database" />
-  <link fromOperation="input connector" fromProperty="fasta dir" toOperation="BER Hmmpfam" toProperty="fasta_dir" />
-  <link fromOperation="input connector" fromProperty="fasta dir" toOperation="BER Blastp" toProperty="fastadir" />
-  <link fromOperation="input connector" fromProperty="seq names" toOperation="BER Hmmpfam" toProperty="sequence_names" />
-  <link fromOperation="input connector" fromProperty="seq names" toOperation="BER Blastp" toProperty="sequence_names" />
-  <link fromOperation="BER Blastp" fromProperty="success" toOperation="output connector" toProperty="blastp_success" />
-  <link fromOperation="BER Hmmpfam" fromProperty="success" toOperation="output connector" toProperty="hmmpfam_success" />
-
-  <operation name="BER Hmmpfam">
-     <operationtype commandClass="PAP::Command::Hmmpfam" typeClass="Workflow::OperationType::Command" />
-  </operation>
-
-  <operation name="BER Blastp">
-     <operationtype commandClass="PAP::Command::BerBlastp" typeClass="Workflow::OperationType::Command" />
-  </operation>
-
-  <operationtype typeClass="Workflow::OperationType::Model">
-    <inputproperty>berdirpath</inputproperty>
-    <inputproperty>hmmdirpath</inputproperty>
-    <inputproperty>blastp query file</inputproperty>
-    <inputproperty>hmm database</inputproperty>
-    <inputproperty>seq names</inputproperty>
-    <inputproperty>fasta dir</inputproperty>
-
-    <outputproperty>blastp_success</outputproperty>
-    <outputproperty>hmmpfam_success</outputproperty>
-    <outputproperty>result</outputproperty>
-  </operationtype>
-
-
-</workflow>
-
-    );
-
-}
-
-1;
-
-__DATA__
-<?xml version='1.0' standalone='yes'?>
-<workflow name="BER blastp and hmmpfam">
-
-  <link fromOperation="input connector" fromProperty="berdirpath" toOperation="BER Blastp" toProperty="berdirpath" />
-  <link fromOperation="input connector" fromProperty="hmmdirpath" toOperation="BER Hmmpfam" toProperty="hmmdirpath" />
-
-  <link fromOperation="input connector" fromProperty="blastp query file" toOperation="BER Blastp" toProperty="blastp_query" />
-  <link fromOperation="input connector" fromProperty="hmm database" toOperation="BER Hmmpfam" toProperty="hmm_database" />
-  <link fromOperation="input connector" fromProperty="fasta dir" toOperation="BER Hmmpfam" toProperty="fasta_dir" />
-  <link fromOperation="input connector" fromProperty="fasta dir" toOperation="BER Blastp" toProperty="fastadir" />
-  <link fromOperation="input connector" fromProperty="seq names" toOperation="BER Hmmpfam" toProperty="sequence_names" />
-  <link fromOperation="input connector" fromProperty="seq names" toOperation="BER Blastp" toProperty="sequence_names" />
-  <link fromOperation="BER Blastp" fromProperty="success" toOperation="output connector" toProperty="blastp_success" />
-  <link fromOperation="BER Hmmpfam" fromProperty="success" toOperation="output connector" toProperty="hmmpfam_success" />
-
-  <operation name="BER Hmmpfam">
-     <operationtype commandClass="PAP::Command::Hmmpfam" typeClass="Workflow::OperationType::Command" />
-  </operation>
-
-  <operation name="BER Blastp">
-     <operationtype commandClass="PAP::Command::BerBlastp" typeClass="Workflow::OperationType::Command" />
-  </operation>
-
-  <operationtype typeClass="Workflow::OperationType::Model">
-    <inputproperty>berdirpath</inputproperty>
-    <inputproperty>hmmdirpath</inputproperty>
-    <inputproperty>blastp query file</inputproperty>
-    <inputproperty>hmm database</inputproperty>
-    <inputproperty>seq names</inputproperty>
-    <inputproperty>fasta dir</inputproperty>
-
-    <outputproperty>blastp_success</outputproperty>
-    <outputproperty>hmmpfam_success</outputproperty>
-    <outputproperty>result</outputproperty>
-  </operationtype>
-
-
-</workflow>
