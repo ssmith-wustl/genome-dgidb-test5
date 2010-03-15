3c6b0c9f4c967c3fee4926847a329b262a475f54
adukes <adukes@genome.wustl.edu>
2010-03-15 23:34:05 +0000

    gmt metagenomic-composition-shotgun
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56537 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/MetagenomicCompositionShotgun.pm
@@ -0,0 +1,12 @@
+package Genome::Model::Tools::MetagenomicCompositionShotgun;
+
+use strict;
+use warnings;
+
+use Genome; 
+
+class Genome::Model::Tools::MetagenomicCompositionShotgun {
+    is  => 'Command',
+};
+
+1;
