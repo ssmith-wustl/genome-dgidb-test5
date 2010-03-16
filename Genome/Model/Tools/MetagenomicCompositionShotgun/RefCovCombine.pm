53c2f6ed005ba198c4aacbae3bb4f94ae9e44133
adukes <adukes@genome.wustl.edu>
2010-03-16 21:42:29 +0000

    skeleton genome tool for Sahars ref cov combine script
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56590 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/MetagenomicCompositionShotgun/RefCovCombine.pm
@@ -0,0 +1,40 @@
+package Genome::Model::Tools::MetagenomicCompositionShotgun::RefCovCombine;
+
+use strict;
+use warnings;
+use Genome;
+
+class Genome::Model::Tools::MetagenomicCompositionShotgun::RefCovCombine{
+    is => 'Genome::Model::Tools::MetagenomicCompositionShotgun::RefCovCombine',
+    has =>[
+        refcov_output_file => {
+            is => 'Text',
+            doc => 'output from refcov to be organized by species',
+        },
+        taxonomy => {
+            is => 'Text',
+            doc => 'taxonomy file linking reference ids to species names',
+        },
+        viral_taxonomy => {
+            is => 'Text',
+            doc => 'viral taxonomy file linking reference ids to species names',
+        },
+        reference_counts_file => {
+            is => 'Text',
+            doc => 'File containing reference names and read counts'
+        },
+        output => {
+            is => 'Text',
+            doc => 'output file',
+        },
+    ]
+};
+
+sub execute{
+    my $self = shift;
+    my $refcov_output_file = $self->refcov_output_file;
+
+    return 1;
+}
+
+1;
