fc22300fa8548272fcb30da82b582e6c39d9817b
adukes <adukes@genome.wustl.edu>
2010-03-22 19:56:37 +0000

    combined unaligned output now only includes reads in both unaligned files
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56790 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/MetagenomicCompositionShotgun/MergeAlignments.pm
+++ Genome/Model/Tools/MetagenomicCompositionShotgun/MergeAlignments.pm
@@ -85,39 +85,98 @@ sub execute {
     if ($rv_check) {
     	$self->status_message("Output files exists.  Skipping the generation of the unaligned reads files and aligned merged unsorted file.  If you would like to regenerate these files, remove them and rerun.");  	
     } else {
-    	my $rv_cat = Genome::Utility::FileSystem->cat(input_files=>\@unaligned_files,output_file=>$unaligned_combined);
+        
+    	my $rv_cat = Genome::Utility::FileSystem->cat(input_files=>\@unaligned_files,output_file=>"$unaligned_combined.cat");
     	if ($rv_cat) {
     		Genome::Utility::FileSystem->mark_file_ok($unaligned_combined);
     	} else {
     		$self->error_message("There was a problem generating the combined unaligned file: $unaligned_combined");
     		#may want to return here.
-                return;
+            return;
     	}
 
+        my $unaligned_1 = $unaligned_files[0];
+        my $unaligned_1_sorted = $unaligned_1.".sorted";
+        my $unaligned_2 = $unaligned_files[1];
+        my $unaligned_2_sorted = $unaligned_2.".sorted";
+        
+        my $sort_1_rv = Genome::Utility::FileSystem->shellcmd(
+            cmd => "sort $unaligned_1 > $unaligned_1_sorted",
+            input_files => [$unaligned_1],
+            output_files => [$unaligned_1_sorted],
+        );
+
+        unless ($sort_1_rv){
+            $self->error_message("Failed to sort unaligned file $unaligned_1");
+            return;
+        }
+        
+        my $sort_2_rv = Genome::Utility::FileSystem->shellcmd(
+            cmd => "sort $unaligned_2 > $unaligned_2_sorted",
+            input_files => [$unaligned_2],
+            output_files => [$unaligned_2_sorted],
+        );
+        
+        unless ($sort_2_rv){
+            $self->error_message("Failed to sort unaligned file $unaligned_1");
+            return;
+        }
+
+        my $unaligned_ofh;
+        if (Genome::Utility::FileSystem->validate_file_for_writing($unaligned_combined)){
+            $unaligned_ofh = IO::File->new(">$unaligned_combined");
+        }else{
+            print "ERROR\n";   
+        }
+        my $unaligned_fh_1 = Genome::Utility::FileSystem->open_file_for_reading($unaligned_1_sorted);
+        my $unaligned_fh_2 = Genome::Utility::FileSystem->open_file_for_reading($unaligned_2_sorted);
+        my @last_set;
+        my $last_read_name;
+        @last_set = ($unaligned_fh_2->getline, $unaligned_fh_2->getline);
+        ($last_read_name) = split(/\t/, $last_set[0]);
+
+        while ($_ = $unaligned_fh_1->getline and defined $_ and defined $last_read_name){
+            my $line = $_;
+            my $line_pair = $unaligned_fh_1->getline;
+            my ($read_name) = split(/\t/, $line);
+            while (defined $last_read_name and $last_read_name lt $read_name){
+                @last_set = ($unaligned_fh_2->getline, $unaligned_fh_2->getline);
+                if (defined $last_set[0]){
+                    ($last_read_name) = split(/\t/, $last_set[0]);
+                }else{
+                    $last_read_name = undef;
+                }
+            }
+            if (defined $last_read_name  and $last_read_name eq $read_name){
+                $unaligned_ofh->print($line);
+                $unaligned_ofh->print($line_pair);
+            }
+        }
+
         if (scalar(@alignment_files) < 2) {
-             $self->error_message("*** Invalid number of files to merge: ".scalar(@alignment_files).". Must have 2 or more.  Quitting.");
-             return;
+            $self->error_message("*** Invalid number of files to merge: ".scalar(@alignment_files).". Must have 2 or more.  Quitting.");
+            return;
         } else {
-    	    my $rv_merge = Genome::Utility::FileSystem->cat(input_files=>\@alignment_files,output_file=>$merged_alignment_unsorted);
+            my $rv_merge = Genome::Utility::FileSystem->cat(input_files=>\@alignment_files,output_file=>$merged_alignment_unsorted);
             if ($rv_merge != 1) {
-                    $self->error_message("<<<Failed MergeAlignments on cat merge.  Return value: $rv_merge");
-                    return;
+                $self->error_message("<<<Failed MergeAlignments on cat merge.  Return value: $rv_merge");
+                return;
             }
             $self->status_message("Merge complete.");
-   	    Genome::Utility::FileSystem->mark_file_ok($merged_alignment_unsorted);
+            Genome::Utility::FileSystem->mark_file_ok($merged_alignment_unsorted);
         }
 
     }
-   
+
     #sort alignment file 
 
 
     my $merged_alignment_sorted = $working_directory."/aligned_merged_sorted.sam";
-    
+
     my @expected_sorted_output_files = ( $merged_alignment_sorted );
     $self->status_message("Starting sort step.  Checking on existence of $merged_alignment_sorted.");
     my $rv_sort_check = Genome::Utility::FileSystem->are_files_ok(input_files=>\@expected_sorted_output_files);
-    
+
     if ($rv_sort_check == 1) {
         #shortcut this step, all the required files exist.  Quit.
         $self->merged_aligned_file($merged_alignment_sorted);
@@ -145,15 +204,15 @@ sub execute {
             return 1;
         }
     }    
-    
+
     return; 
 }
 
 sub resolve_name_sorted_file_name {
-	my $self = shift;
-	my $refseq_name = shift;
-	my $extension = shift;
-	return $self->_working_directory."/".$refseq_name."_name_sorted.".$extension;
+    my $self = shift;
+    my $refseq_name = shift;
+    my $extension = shift;
+    return $self->_working_directory."/".$refseq_name."_name_sorted.".$extension;
 }
 
 1;
