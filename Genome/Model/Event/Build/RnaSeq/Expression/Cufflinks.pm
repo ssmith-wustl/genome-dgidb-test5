582cc3bd1a4938d862a976d29ae8b067226e7adb
jwalker <jwalker@genome.wustl.edu>
2010-03-23 19:58:03 +0000

    Store expression data in an output directory.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56849 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Event/Build/RnaSeq/Expression/Cufflinks.pm
+++ Genome/Model/Event/Build/RnaSeq/Expression/Cufflinks.pm
@@ -7,7 +7,7 @@ use Genome;
 
 class Genome::Model::Event::Build::RnaSeq::Expression::Cufflinks {
     is => ['Genome::Model::Event::Build::RnaSeq::Expression'],
-	has => [
+    has => [
     ],
 };
 
@@ -17,16 +17,21 @@ sub bsub_rusage {
 
 sub execute {
     my $self = shift;
-
+    my $expression_directory = $self->build->accumulated_expression_directory;
+    unless (-d $expression_directory) {
+        Genome::Utility::FileSystem->create_directory($expression_directory);
+    }
     my $align_reads = Genome::Model::Event::Build::RnaSeq::AlignReads->get(
         model_id => $self->model_id,
         build_id => $self->build_id,
     );
     my $aligner = $align_reads->create_aligner_tool;
     my $sam_file = $aligner->sam_file;
+    my $params = $self->model->expression_params || '';
     unless (Genome::Model::Tools::Cufflinks::Assemble->execute(
         sam_file => $aligner->sam_file,
-        params => $self->model->expression_params,
+        params => $params,
+        output_directory => $expression_directory,
         use_version => $self->model->expression_version,
     )) {
         $self->error_message('Failed to execute cufflinks');
