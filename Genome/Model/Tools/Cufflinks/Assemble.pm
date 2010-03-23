ef69d15aa521e3a1e583a0e480a89d608dbf93ca
jwalker <jwalker@genome.wustl.edu>
2010-03-23 20:00:42 +0000

    Add properties missing and redirect the stdout/stderr to a file within the output directory.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56853 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Cufflinks/Assemble.pm
+++ Genome/Model/Tools/Cufflinks/Assemble.pm
@@ -1,5 +1,11 @@
 package Genome::Model::Tools::Cufflinks::Assemble;
 
+use strict;
+use warnings;
+
+use Genome;
+use Cwd;
+
 class Genome::Model::Tools::Cufflinks::Assemble {
     is => 'Genome::Model::Tools::Cufflinks',
     has_input => [
@@ -10,9 +16,16 @@ class Genome::Model::Tools::Cufflinks::Assemble {
             doc => 'Any additional parameters to pass to cufflinks',
             is_optional => 1,
         },
+        output_directory => { doc => 'The directory to write all output files to', is => 'Text',},
     ],
     has_output => [
         transcripts_file => {
+            is_optional => 1,
+        },
+        transcript_expression_file => {
+            is_optional => 1,
+        },
+        gene_expression_file => {
             is_optional => 1,
         },
     ],
@@ -22,17 +35,27 @@ class Genome::Model::Tools::Cufflinks::Assemble {
 sub execute {
     my $self = shift;
 
-    #TODO: These files should be limited to an output directory
-    $self->transcripts_file('transcripts.gtf');
-    $self->transcript_expression_file('transcripts.expr');
-    $self->gene_expression_file('genes.expr');
-
+    my $cwd = getcwd;
+    my $output_directory = $self->output_directory;
+    unless (chdir($output_directory)) {
+        $self->error_message('Failed to change cwd to '. $output_directory);
+        die($self->error_message);
+    }
+    $self->transcripts_file($output_directory .'/transcripts.gtf');
+    $self->transcript_expression_file($output_directory .'/transcripts.expr');
+    $self->gene_expression_file($output_directory .'/genes.expr');
+    $self->assembler_output_file($output_directory .'/cufflinks.out');
+    
     my $params = $self->params || '';
-    my $cmd = $self->cufflinks_path .' '. $params .' '. $self->sam_file;
+    my $cmd = $self->cufflinks_path .' '. $params .' '. $self->sam_file .' > '. $self->assembler_output_file .' 2>&1';;
     Genome::Utility::FileSystem->shellcmd(
         cmd => $cmd,
         input_files => [$self->sam_file],
-        output_files => [$self->transcripts_file,$self->transcript_expression_file,$self->gene_expression_file],
+        output_files => [$self->transcripts_file,$self->transcript_expression_file,$self->gene_expression_file,$self->assembler_output_file],
     );
+    unless (chdir($cwd)) {
+        $self->error_message('Failed to change directory to '. $cwd);
+        die($self->error_message);
+    }
     return 1;
 }
