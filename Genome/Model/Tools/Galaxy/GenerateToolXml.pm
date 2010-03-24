994ae1e4fb3d62a70ce61b5f338f4c19287b3cd7
eclark <eclark@genome.wustl.edu>
2010-03-24 02:39:05 +0000

    now generates working xml for Tail.pm
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56872 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Galaxy/GenerateToolXml.pm
+++ Genome/Model/Tools/Galaxy/GenerateToolXml.pm
@@ -6,10 +6,10 @@ use warnings;
 use Genome;
 
 class Genome::Model::Tools::Galaxy::GenerateToolXml {
-    is => 'Command',
+    is  => 'Command',
     has => [
         class_name => {
-            is => 'String',
+            is  => 'String',
             doc => 'Input class name'
         }
     ]
@@ -18,7 +18,9 @@ class Genome::Model::Tools::Galaxy::GenerateToolXml {
 sub execute {
     my $self = shift;
 
-    # Old-style definations call a function after setting up the class
+    $DB::single=1;
+
+    my $class_name = $self->class_name;
     {
         eval "use " . $self->class_name;
         if ($@) {
@@ -26,38 +28,150 @@ sub execute {
         }
     }
 
-    my $self = $class->SUPER::create(@_);
-    my $command = $self->command_class_name;
+    my $class_meta = $class_name->get_class_object;
+    if ( !$class_meta ) {
+        $self->error_message("Invalid command class: $class_name");
+        return 0;
+    }
 
+    my $help_brief  = $class_name->help_brief;
+    my $help_detail = $class_name->help_detail;
 
-    my $class_meta = $command->get_class_object;
-    die 'invalid command class' unless $class_meta;
-
+    my $tool_id = 'dummyval'; #TODO
+    my $tool_name = $class_name->command_name;
+    my $command_line = $tool_name;
+    my $input_params = '';
+    my $output_data = '';
+    
     my @property_meta = $class_meta->all_property_metas();
 
-    foreach my $type (qw/input output/) {
-        my $my_method = $type . '_properties';
-        unless ($self->$my_method) {
-            my @props = map {
-                $_->property_name
-            } grep {
-                defined $_->{'is_' . $type} && $_->{'is_' . $type}
-            } @property_meta;
+    my %categories = bin_properties(
+        \@property_meta,
+        required_input => sub {
+            !$_[0]->is_optional
+            && (defined $_[0]->{'is_input'}
+                && $_[0]->{'is_input'})
+            && !(defined $_[0]->{'is_output'}
+                && $_[0]->{'is_output'});
+        },
+        optional_input => sub {
+            $_[0]->is_optional
+            && (defined $_[0]->{'is_input'}
+                && $_[0]->{'is_input'})
+            && !(defined $_[0]->{'is_output'}
+                && $_[0]->{'is_output'});
+        },
+        required_both => sub {
+            !$_[0]->is_optional
+            && (defined $_[0]->{'is_input'}
+                && $_[0]->{'is_input'})
+            && (defined $_[0]->{'is_output'}
+                && $_[0]->{'is_output'});
+        },
+        optional_both => sub {
+            $_[0]->is_optional
+            && (defined $_[0]->{'is_input'}
+                && $_[0]->{'is_input'})
+            && (defined $_[0]->{'is_output'}
+                && $_[0]->{'is_output'});
+        },
+        output => sub {
+            !(defined $_[0]->{'is_input'}
+                && $_[0]->{'is_input'})
+            && (defined $_[0]->{'is_output'}
+                && $_[0]->{'is_output'});
+        }
+    );
 
-            if ($type eq 'input') {
-                my @opt_input = map {
-                    $_->property_name
-                } grep {
-                    $_->is_optional &&
-                    defined $_->{'is_input'} && $_->{'is_input'}
-                } @property_meta;
+    my @args = ();
 
-                $self->optional_input_properties(\@opt_input);
+    foreach my $p (@{ $categories{required_input} }) {
+#        print Data::Dumper->new([$p])->Dump;
+
+        my $line = '<param';
+
+        $line .= ' name="' . $p->property_name . '"';
+
+        if ($p->data_type eq 'file_path') {
+            $line .= ' type="data"';
+            $line .= ' format="' . $p->{'file_format'} . '"';
+        } else {
+            $line .= ' type="' . $p->data_type . '"';
+
+            if ($p->data_length) {
+                $line .= ' size="' . $p->data_length . '"';
             }
+            if ($p->default_value) {
+                $line .= ' value="' . $p->default_value . '"';
+            }
+        }        
 
-            $self->$my_method(\@props);
+
+        $line .= '/>';
+        
+        $input_params .= "\n    $line";
+
+        push @args, $p->property_name;
+    }
+    
+    foreach my $p (@{ $categories{required_both} }) {
+        if ($p->data_type eq 'file_path') {
+            my $line = '<data';
+        
+            $line .= ' name="' . $p->property_name . '"';
+            if ($p->{'same_as'}) {
+                $line .= ' format="input"';
+                $line .= ' metadata_source="' . $p->{'same_as'} . '"';
+            } else {
+                $line .= ' format="' . $p->{'file_format'} . '"';
+            }
+        
+            $line .= '/>';
+        
+            $output_data .= "\n    $line";
+
+            push @args, $p->property_name;
         }
     }
 
+    foreach my $pn (@args) {
+        my $option = $pn;
+        $option =~ s/_/-/g;
+
+        $command_line .= ' --' . $option . '=$' . $pn;
+    }
+
+    my $xml = <<"    XML";
+<tool id="$tool_id" name="$tool_name">
+  <description>$help_brief</description>
+  <command>$command_line</command>
+  <inputs>$input_params
+  </inputs>
+  <outputs>$output_data
+  </outputs>
+  <help>
+$help_detail
+  </help>
+</tool>
+    XML
+
+    print $xml;
+}
+
+sub bin_properties {
+    my ($properties, %spec) = @_;
+    
+    my %output = map { $_ => [] } keys %spec;
+    foreach my $p (@$properties) {
+        my @matched = grep {
+            $spec{$_}->($p)
+        } keys %spec;
+        
+        for (@matched) {
+            push @{ $output{$_} }, $p;
+        }
+    }
+
+    return %output;
 }
 
