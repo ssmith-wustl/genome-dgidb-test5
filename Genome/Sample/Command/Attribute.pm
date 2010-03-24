cf5fe9880f058df90a62729cf2b5151343d14042
ssmith <ssmith@genome.wustl.edu>
2010-03-24 16:43:15 +0000

    Added API for clinical data.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@56891 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Sample/Command/Attribute.pm
@@ -0,0 +1,9 @@
+package Genome::Sample::Command::Attribute;
+
+class Genome::Sample::Command::Attribute {
+    is => 'Command',
+    doc => 'commands to work with clinical data',
+};
+
+1;
+
---
+++ Genome/Sample/Command/Attribute/List.pm
@@ -0,0 +1,8 @@
+package Genome::Sample::Command::Attribute::List;
+
+class Genome::Sample::Command::Attribute::List {
+    is => 'Genome::Sample::Attribute::Command::List',
+};
+
+1;
+
