270cb49c8a2cf1fb1432cf4a318387dd4b5e443d
rmeyer <rmeyer@genome.wustl.edu>
2010-03-31 22:20:30 +0000

    refactored and added abuility to start with a gene name
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@57194 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Annotate/GeneRegions.pm
+++ Genome/Model/Tools/Annotate/GeneRegions.pm
@@ -11,12 +11,14 @@ class Genome::Model::Tools::Annotate::GeneRegions {
 	    type  =>  'String',
 	    doc   =>  "provide the organism either mouse or human; default is human",
 	    is_optional  => 1,
+	    valid_values => ['human','mouse'],
 	    default => 'human',
 	},
 	version => {
 	    type  =>  'String',
 	    doc   =>  "provide the imported annotation version; default for human is 54_36p and for mouse is 54_37g",
 	    is_optional  => 1,
+	    default => '54_36p',
 	},
 	   output => {
 	    type  =>  'String',
@@ -25,7 +27,12 @@ class Genome::Model::Tools::Annotate::GeneRegions {
 	},
 	list => {
 	    type  =>  'String',
-	    doc   =>  "input list tab/space delimited with chromosome ie {1,2,...,22,X,Y,M}, Start coordinate, Stop coordinate",
+	    doc   =>  "input list tab/space delimited with chromosome ie {1,2,...,22,X,Y,M}, Start coordinate, Stop coordinate. Or a hugo gene name",
+	},
+	mode => {
+	    type  =>  'String',
+	    doc   =>  "enter the source for your search",
+	    valid_values => ['coordinates','gene'],
 	},
     ], 
 };
@@ -42,169 +49,249 @@ EOS
 sub help_detail {
     return <<EOS 
 
-	will find all genes and transcripts along with region types that coincides with the coordinates on your list
+	This tool will find all genes and transcripts along with region types that coincides with the coordinates or gene names in your list.
+
+	gmt annotate gene-region -mode coordinates -list some_file
+	
+	gmt annotate gene-region -mode gene -list some_file
+	gmt annotate gene-region -mode gene -list EGFR
+	gmt annotate gene-region -mode gene -list EGFR,KRAS
+
 EOS
 }
 
 sub execute {
 
     my $self = shift;
+    my $output = $self->output;
+    if ($output) {
+	open(OUT,">$output") || App->error_message("\n\nCouldn't open the file $output for writting\n\n.") && return;
+    }
+
+    my $gene_list = &get_gene_list($self);
+    unless ($gene_list) {App->error_message( "\n\nCouldn't generate a gene list\n\n.");return;}
 
     my $organism = $self->organism;
     my $version = $self->version;
-    unless ($version) {	
-	if ($organism eq "mouse") {
-	    $version = "54_37g" ;
-	} elsif ($organism eq "human") { 
-	    $version = "54_36p" ;
-	} else { 
-	    die "organism is restricted to mouse or human\n";
-	}
+
+    if ($organism eq "mouse") {
+	if ($version eq "54_36p") {$version = "54_37g";}
     }
-    my $output = $self->output;
-    
-    my $list;
-    my $targets;
-    my $file = $self->list;
-    
-    unless (-f $file) {system qq(gmt annotate gene-regions --help);print qq(Your list was not found.\t\n\tPlease check that your list is in place and try again.\n\n\n);return 0;}
-    open(LIST,"$file") || die "\nCould not open $file\n";
-    while (<LIST>) {
-	chomp;
-	my $line = $_;
-	my ($chr,$start,$stop) = split(/[\s]+/,$line);
+    my $results = 0;
 
-	my $target = "$chr:$start:$stop";
-	$targets->{$target}->{id}=$line;
-
-	unless ($chr =~ /^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|X|Y|M)$/ && $start =~ /^[\d]+$/ && $stop =~ /^[\d]+$/) {print qq($line does not represent valid coordinates\n);}
-	next unless $chr =~ /^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|X|Y|M)$/ && $start =~ /^[\d]+$/ && $stop =~ /^[\d]+$/;
-
-	for my $n ($start..$stop) {
-	    my $p1 = $list->{$chr}->{$n};
-	    if ($p1) {
-		unless ($p1 =~ /$target/) {
-		    $list->{$chr}->{$n}="$p1\|\|$target";
+    foreach my $hugo_name (sort keys %{$gene_list}) {
+	foreach my $line (sort keys %{$gene_list->{$hugo_name}}) {
+	    unless ($hugo_name eq $line) {
+		if ($output) {
+		    print OUT qq($line\n);
+		} else {
+		    print qq($line\n);
 		}
-	    } else {
-		$list->{$chr}->{$n}="$target";
 	    }
 	}
-    } close(LIST);
-    
-    my ($ncbi_reference) = $version =~ /\_([\d]+)/;
-    my $eianame = "NCBI-" . $organism . ".ensembl";
-    my $gianame = "NCBI-" . $organism . ".genbank";
-    my $build_source = "$organism build $ncbi_reference version $version";
-    
-    my $ensembl_build = Genome::Model::ImportedAnnotation->get(name => $eianame)->build_by_version($version);
-    unless ($ensembl_build) { die qq(Couldn't get ensembl build info for $build_source\n);}
-    
-    my $ensembl_data_directory = $ensembl_build->annotation_data_directory;
-    
-    my $genbank_build = Genome::Model::ImportedAnnotation->get(name => $gianame)->build_by_version($version);
-    my $genbank_data_directory = $genbank_build->annotation_data_directory;
-    
-    my (@et) = Genome::Transcript->get(data_directory => $ensembl_data_directory);
-    my (@gt) = Genome::Transcript->get(data_directory => $genbank_data_directory);
-    
-    my @join_array = (@et,@gt);
-    
-    my $transcript_number = 0;
-    
-    for my $t (@join_array) {
+
+	my $transcripts = &get_transcripts($hugo_name);
+	unless ($transcripts) {print qq(no transcripts found for $hugo_name\n);next;}
+	my @transcripts = split(/\,/,$transcripts);
 	
-	my $chrom_name = $t->chrom_name;
-	next unless $list->{$chrom_name};
-	
-	my $g_id = $t->gene_id;
-	my @gid = split(/[\s]+/,$g_id);
-	my ($gene_id) = $gid[0];
-	
-	my $source = $t->source;
-	my $transcript_status = $t->transcript_status;
-	my $strand = $t->strand;
-	my $transcript_name = $t->transcript_name;
-	my $transcript_id = $t->transcript_id;
-	my $transcript_start = $t->transcript_start;
-	my $transcript_stop = $t->transcript_stop;
-	
-	for my $n ($transcript_start..$transcript_stop) {
+	for my $transcript (@transcripts) {
 	    
-	    my $target_region = $list->{$chrom_name}->{$n};
-
-	    if ($target_region) {
+	    my ($info) = Genome::Model::Tools::Annotate::TranscriptSequence->execute(transcript => $transcript, no_stdout => '1',version => $version, organism => $organism);
+	    unless ($info) {print qq(no info found for $hugo_name $transcript\n); next;}
+	    my $transcript_info = $info->{result};
+	    unless ($transcript_info) {print qq(no transcript info found for $hugo_name $transcript\n); next;}
+	    
+	    my $source_line = $transcript_info->{$transcript}->{'-1'}->{source_line};
+	    my $strand = $transcript_info->{$transcript}->{'-1'}->{strand};
+	    my $transcript_start = $transcript_info->{$transcript}->{'-1'}->{first_base};
+	    my $transcript_stop = $transcript_info->{$transcript}->{'-1'}->{last_base};
+	    my $g_id = $transcript_info->{$transcript}->{'-1'}->{gene_id};
+	    my $chromosome = $transcript_info->{$transcript}->{'-1'}->{chromosome};
+	    my $hugo_gene_name = $transcript_info->{$transcript}->{'-1'}->{hugo_gene_name};
+	    
+	    my $cds;
+	    foreach my $pos (sort {$a<=>$b} keys %{$transcript_info->{$transcript}}) {
+		unless ($pos == -1) {
+		    my $codon = $transcript_info->{$transcript}->{$pos}->{aa_n};
+		    my $base = $transcript_info->{$transcript}->{$pos}->{base};
+		    my $exon = $transcript_info->{$transcript}->{$pos}->{exon};
+		    my $frame = $transcript_info->{$transcript}->{$pos}->{frame};
+		    my $range = $transcript_info->{$transcript}->{$pos}->{range};
+		    my ($exon_number,$exon_type) = split(/\,/,$exon);
+		    $cds->{$exon_number}->{$exon_type}=$range;
+		}
+	    }
+	    
+	    my $threeprimeendutr; #5'to3'
+	    foreach my $exon_number (sort {$a<=>$b} keys %{$cds}) {
+		my $utr_range = $cds->{$exon_number}->{utr_exon};
+		my $cds_range = $cds->{$exon_number}->{cds_exon};
 		
-		my $gene = $t->gene;
-		my $hugo_gene_name = $gene->hugo_gene_name;
-		unless ($hugo_gene_name) {$hugo_gene_name = "unknown";}
-		my @substructures = $t->ordered_sub_structures;
-		my $total_substructures = @substructures;
-		my $t_n = 0; #substructure counter
-		$transcript_number++;
-		my $n_ss = $total_substructures - 2;#subtracting out the flanking regions
-		
-		my $ss_n = 0;
-		if (@substructures) {
-		    
-		    while ($t_n < $total_substructures) {
-			my $t_region = $substructures[$t_n];
-			$t_n++;
-			
-			my $structure_type = $t_region->{structure_type};
-			unless ($structure_type eq "flank") {
-			    $ss_n++;
-			    my $tr_start = $t_region->{structure_start};
-			    my $tr_stop = $t_region->{structure_stop};
-			    
-			    for my $sn ($tr_start..$tr_stop) {
-				my $targeted = $list->{$chrom_name}->{$sn};
-				next unless $targeted;
-
-				my @targeteds = split(/\|\|/,$targeted);
-				for my $target (@targeteds) {
-				    
-				    if ($target) {
-
-					my $i1 = $targets->{$target}->{hugo_gene_name};
-					my $i2 = $targets->{$target}->{transcript_name};
-					my $i3 = $targets->{$target}->{structure_type};
-					
-					if ($i1 && $i2 && $i3) {
-					    unless ($i1 =~ /$hugo_gene_name/) {$targets->{$target}->{hugo_gene_name}="$i1,$hugo_gene_name";}
-					    unless ($i2 =~/$transcript_name/) {$targets->{$target}->{transcript_name}="$i2,$transcript_name";}
-					    unless ($i3 =~ /$structure_type/) {$targets->{$target}->{structure_type}="$i3,$structure_type";}
-					} else {
-					    $targets->{$target}->{hugo_gene_name} = $hugo_gene_name;
-					    $targets->{$target}->{transcript_name} = $transcript_name;
-					    $targets->{$target}->{structure_type} = $structure_type;
-					}
-				    }
-				}
-			    }
+		my $fiveprimeendutr;
+		if ($utr_range) {
+		    unless ($threeprimeendutr) {
+			$utr_range =~ s/\-/\t/;
+			$fiveprimeendutr = 1;
+			$results =1;
+			if ($output) {
+			    print OUT qq($hugo_name\t$strand\t$chromosome\t$transcript\t$exon_number\tutr_exon\t$utr_range\n);
+			} else {
+			    print qq($hugo_name\t$strand\t$chromosome\t$transcript\t$exon_number\tutr_exon\t$utr_range\n);
+			} 
+		    }
+		}
+		if ($cds_range) {
+		    $threeprimeendutr = 1;
+		    $cds_range =~ s/\-/\t/;
+		    $results =1;
+		    if ($output) {
+			print OUT qq($hugo_name\t$strand\t$chromosome\t$transcript\t$exon_number\tcds_exon\t$cds_range\n);
+		    } else {
+			print qq($hugo_name\t$strand\t$chromosome\t$transcript\t$exon_number\tcds_exon\t$cds_range\n);
+		    } 
+		}
+		if ($utr_range && $threeprimeendutr) {
+		    unless ($fiveprimeendutr) {
+			$utr_range =~ s/\-/\t/;
+			$results =1;
+			if ($output) {
+			    print OUT qq($hugo_name\t$strand\t$chromosome\t$transcript\t$exon_number\tutr_exon\t$utr_range\n);
+			} else {
+			    print qq($hugo_name\t$strand\t$chromosome\t$transcript\t$exon_number\tutr_exon\t$utr_range\n);
 			}
 		    }
 		}
 	    }
 	}
     }
-    
-    if ($output) {open(COV,">$output") || die "couldn't open the output file $output\n";}
+    return $results;
+}
 
-    foreach my $target (sort keys %{$targets}) {
-	#unless ($bases_covered) {$bases_covered = 0;}
-	my $i1 = $targets->{$target}->{hugo_gene_name};
-	my $i2 = $targets->{$target}->{transcript_name};
-	my $i3 = $targets->{$target}->{structure_type};
-	#my $line = $targets->{$target}->{id};
-	unless ($i1) {$i1 = "not_found";}
-	unless ($i2) {$i2 = "not_found";}
-	unless ($i3) {$i3 = "not_found";}
-	
-	my ($chr,$start,$stop) = split(/\:/,$target);
-	if ($output) {print COV qq($chr\t$start\t$stop\t$i1\t$i2\t$i3\n);}else{print qq($chr\t$start\t$stop\t$i1\t$i2\t$i3\n);}
+
+sub get_transcripts {
+
+    my ($gene) = @_;
+
+    my $GTDir = "/gscmnt/200/medseq/biodb/shared/misc/annotation/54_36p";
+
+    my $gtdb = Bio::DB::Fasta->new($GTDir);
+    my $transcripts = $gtdb->seq($gene, 1 => 1000);
+
+    return unless $transcripts;
+    return $transcripts;
+
+}
+
+sub get_gene_list {
+
+    my ($self) = @_;
+    my $mode = $self->mode;
+
+    my $gene_list;
+    if ($mode eq "coordinates") {
+	$gene_list = &parse_coordinates($self);
+    } elsif ($mode eq "gene") {
+	$gene_list = &parse_genes($self);
     }
-    if ($output) {close (COV);print qq(see your results in $output\n);}
+    return unless $gene_list;
+    return ($gene_list);
+}
+
+sub parse_genes {
+    
+    my $gene_list;
+    my ($self) = @_;
+    my $list = $self->list;
+    
+    if (-f $list) {
+	open (LIST,"$list") || App->error_message( "\n\nCouldn't open your list $list\n\n.") && return;
+	while (<LIST>) {
+	    chomp;
+	    my $gene = $_;
+	    $gene_list->{$gene}->{$gene}=1;
+	} 
+	close LIST;
+	
+    } else {
+	my @genes = split(/\,/,$list);
+	for my $gene (@genes) {
+	    $gene_list->{$gene}->{$gene}=1;
+	}
+    }
+    
+    return unless ($gene_list);
+    return ($gene_list);
+
+}
+
+sub parse_coordinates {
+    
+    my ($self) = @_;
+    my $organism = $self->organism;
+    my $version = $self->version;
+
+    if ($organism eq "mouse") {
+	if ($version eq "54_36p") {$version = "54_37g";}
+    }
+    my $list = $self->list;
+    my $anno_db = "NCBI-$organism.combined-annotation";
+
+    unless (-f $list) {
+	system qq(gmt annotate gene-regions --help);
+	App->error_message("\nYour file $list was not found.\t\n\tPlease check that your list is in place and try again.\n\n\n");
+	return;
+    }
+    
+    my @genes;
+    open(LIST,"$list") || App->error_message( "\n\nCouldn't open your list $list\n\n.") && return;
+    my $gene_list;
+
+    my $coords;
+    while (<LIST>) {
+	chomp;
+	my $line = $_;
+	my ($chr,$start,$stop) = (split(/[\s]+/,$line))[0,1,2];
+
+	unless ($chr =~ /^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|X|Y|M)$/ && $start =~ /^[\d]+$/ && $stop =~ /^[\d]+$/) {
+	    App->error_message("$line does not represent valid coordinates\n");
+	    next;
+	}
+	$coords->{$chr}->{$start}->{$stop}=$line;
+    }
+    close LIST;
+
+    foreach my $chr (sort keys %{$coords}) {
+	my $ti = Genome::Model->get(name => $anno_db)->build_by_version($version)->transcript_iterator(chrom_name => $chr);
+	unless ($ti) {App->error_message("\ncouldn't get a transcript iterator for $chr\n\n");next;}
+	my $transcript_window =  Genome::Utility::Window::Transcript->create(iterator => $ti);
+	unless ($transcript_window) {App->error_message("\ncouldn't get a transcript window for $chr\n\n");next;}
+	
+	foreach my $start (sort {$a<=>$b} keys %{$coords->{$chr}}) {
+	    foreach my $stop (sort {$a<=>$b} keys %{$coords->{$chr}->{$start}}) {
+		my $line = $coords->{$chr}->{$start}->{$stop};
+		for my $t ($transcript_window->scroll($start,$stop)) {
+		    my $gene = $t->gene;
+		    my $gene_name;
+		    if ($gene) {
+			my @g_id = $t->gene_id;
+			my ($gene_id) = $g_id[0];
+			
+			$gene_name = $gene->name || "unknown\-$gene_id";
+		    }
+		    
+		    $gene_list->{$gene_name}->{$line}=1;
+		}
+	    }
+	}
+	
+	undef($ti);
+	undef($transcript_window);
+	
+    }
+    
+    return unless ($gene_list);
+    return ($gene_list);
+    
 }
 
 1;
