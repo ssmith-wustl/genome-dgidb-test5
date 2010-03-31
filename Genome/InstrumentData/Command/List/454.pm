858876a7e5d9d1023c0af39010755758017b93d5
ssmith <ssmith@genome.wustl.edu>
2010-03-31 14:57:17 +0000

    Updated the lister for 454 data to show index sequence.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@57145 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/Command/List/454.pm
+++ Genome/InstrumentData/Command/List/454.pm
@@ -18,7 +18,7 @@ class Genome::InstrumentData::Command::List::454 {
             is_constant => 1, 
             value => 'Genome::InstrumentData::454' 
         },
-        #show => { default_value => 'id,name,subject_name,processing_profile_name' },
+        show => { default_value => 'id,run_name,region_number,index_sequence,sample_name' }
     ],
     doc => 'list 454 regions available for analysis',
 };
