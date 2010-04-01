c9be26949d1db743ce07c132da8c65064ff7fadf
rlong <rlong@genome.wustl.edu>
2010-04-01 21:37:41 +0000

    added events to support importation, model definition, and building of microarray data.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@57232 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Event/Build/GenotypeMicroarray.pm
@@ -0,0 +1,12 @@
+package Genome::Model::Event::Build::GenotypeMicroarray;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Event::Build::GenotypeMicroarray {
+    is => [ 'Genome::Model::Event' ],
+ };
+
+1;
