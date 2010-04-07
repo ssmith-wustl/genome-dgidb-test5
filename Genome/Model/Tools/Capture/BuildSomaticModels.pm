c17b817c33f9f8df5e33b9cbb557edff4e706dfc
dkoboldt <dkoboldt@genome.wustl.edu>
2010-04-07 19:25:02 +0000

    New module for running somatic capture pipeline
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@57374 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Capture/BuildSomaticModels.pm
@@ -0,0 +1,180 @@
+
+package Genome::Model::Tools::Capture::BuildSomaticModels;     # rename this when you give the module file a different name <--
+
+#####################################################################################################################################
+# BuildSomaticModels - Compare tumor versus normal models to find somatic events
+#					
+#	AUTHOR:		Dan Koboldt (dkoboldt@genome.wustl.edu)
+#
+#	CREATED:	12/09/2009 by D.K.
+#	MODIFIED:	12/09/2009 by D.K.
+#
+#	NOTES:	
+#			
+#####################################################################################################################################
+
+use strict;
+use warnings;
+
+use FileHandle;
+
+use Genome;                                 # using the namespace authorizes Class::Autouse to lazy-load modules under it
+
+## Declare global statistics hash ##
+
+my %stats = ();
+
+class Genome::Model::Tools::Capture::BuildSomaticModels {
+	is => 'Command',                       
+	
+	has => [                                # specify the command's single-value properties (parameters) <--- 
+		processing_profile	=> { is => 'Text', doc => "Processing profile to use [Somatic-Capture-NoSV-AllTiers-Map40-Score40]", is_optional => 1 },
+		data_dir	=> { is => 'Text', doc => "Output directory for comparison files" , is_optional => 0},
+		sample_list	=> { is => 'Text', doc => "Text file of sample, normal-model-id, tumor-model-id" , is_optional => 0},
+		subject_type	=> { is => 'Text', doc => "Subject type, e.g. sample_name, library_name [library_name]" , is_optional => 1},
+		model_basename	=> { is => 'Text', doc => "String to use for naming models; sample will be appended" , is_optional => 0},
+		report_only	=> { is => 'Text', doc => "Flag to skip actual execution" , is_optional => 1},
+	],
+};
+
+sub sub_command_sort_position { 12 }
+
+sub help_brief {                            # keep this to just a few words <---
+    "Define and build somatic-capture pipeline models for tumor-normal pairs"                 
+}
+
+sub help_synopsis {
+    return <<EOS
+Define and build somatic-capture pipeline models for tumor-normal pairs
+EXAMPLE:	gt capture compare-models ...
+EOS
+}
+
+sub help_detail {                           # this is what the user will see with the longer version of help. <---
+    return <<EOS 
+
+EOS
+}
+
+
+################################################################################################
+# Execute - the main program logic
+#
+################################################################################################
+
+sub execute {                               # replace with real execution logic.
+	my $self = shift;
+
+	## Get required parameters ##
+	my $processing_profile = "Somatic-Capture-NoSV-AllTiers-Map40-Score40";
+	$processing_profile = $self->processing_profile if($self->processing_profile);
+
+	my $sample_list = $self->sample_list;
+	my $subject_type = "library_name";
+	$subject_type = $self->subject_type if($self->subject_type);
+
+	my $model_basename = $self->model_basename;
+
+	my $data_dir = "./";
+	$data_dir = $self->data_dir if($self->data_dir);
+
+	if(!$self->report_only)
+	{
+		mkdir($data_dir) if (!(-d $data_dir));
+	}
+
+	## Reset statistics ##
+
+	my $input = new FileHandle ($sample_list);
+	my $lineCounter = 0;
+	
+	while (<$input>)
+	{
+		chomp;
+		my $line = $_;
+		$lineCounter++;
+		
+		(my $sample_name, my $normal_model_id, my $tumor_model_id) = split(/\t/, $line);
+		$stats{'num_pairs'}++;
+
+		my $model_name = $model_basename . "-" . $sample_name;
+		my $model_id = get_model_id($model_name);
+
+		## Build the somatic model ##
+		if(!$model_id)
+		{
+			my $cmd = "genome model define somatic-capture --processing-profile-name \"$processing_profile\" --data-directory $data_dir --model-name \"$model_name\" --subject-name \"$sample_name\" --subject_type \"$subject_type\" --normal-model-id $normal_model_id --tumor-model-id $tumor_model_id";
+			print "$sample_name\t$model_name\n";
+			print "$cmd\n";
+		
+			$model_id = get_model_id($model_name);
+		}
+
+	}
+
+	close($input);
+	
+	return 1;                               # exits 0 for true, exits 1 for false (retval/exit code mapping is overridable)
+}
+
+
+
+
+#############################################################
+# ParseFile - takes input file and parses it
+#
+#############################################################
+
+sub get_model_id
+{
+	my $model_name = shift(@_);
+	my $model_id = 0;
+
+	my $model_output = `genome model list --filter=name=\'$model_name\' --show=id 2>/dev/null`;
+	chomp($model_output);
+	my @output_lines = split(/\n/, $model_output);
+	
+	foreach my $line (@output_lines)
+	{
+		$line =~ s/[^0-9]//g;
+		if($line)
+		{
+			$model_id = $line;
+		}
+	}
+	
+	return($model_id);
+}
+
+
+
+#############################################################
+# ParseFile - takes input file and parses it
+#
+#############################################################
+
+sub get_model_id
+{
+	my $model_name = shift(@_);
+	my $model_id = 0;
+
+	my $model_output = `genome model list --filter=name=\'$model_name\' --show=id 2>/dev/null`;
+	chomp($model_output);
+	my @output_lines = split(/\n/, $model_output);
+	
+	foreach my $line (@output_lines)
+	{
+		$line =~ s/[^0-9]//g;
+		if($line)
+		{
+			$model_id = $line;
+		}
+	}
+	
+	return($model_id);
+}
+
+
+
+1;
+
