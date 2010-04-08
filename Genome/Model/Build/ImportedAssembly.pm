8a73c6fa6a4d9ec74ccfc29e9cf01a5ca75f433d
kkyung <kkyung@genome.wustl.edu>
2010-04-08 20:44:03 +0000

    imported assembly build module to add some checking functions
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@57426 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Build/ImportedAssembly.pm
@@ -0,0 +1,35 @@
+package Genome::Model::Build::ImportedAssembly;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Build::ImportedAssembly {
+    is => 'Genome::Model::Build',
+};
+
+sub create {
+    my $class = shift;
+    my $self = $class->SUPER::create(@_)
+	or return;
+
+    unless (-d $self->data_directory) {
+	$self->error_message("Assembly data directory does not exist:\n\t".$self->data_directory);
+	return;
+    }
+
+    #CHECK OF ALREADY EXISTING BUILDS WITH SAME ASSEMBLY DIRECTORY
+    foreach my $build ($self->model->builds) {
+	if ($build->data_directory eq $self->data_directory) {
+	    $self->error_message("A build with data directory: ".$self->data_directory." already exists\n\tBUILD ID: ".
+				 $build->build_id."\n\tDIR: ".$build->data_directory);
+	    return;
+	}
+    }
+
+    return $self;
+}
+
+
+
