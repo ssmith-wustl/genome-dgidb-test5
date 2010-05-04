7e21322bf92e88b8055ca3e8e8197224a3869c47
kkyung <kkyung@genome.wustl.edu>
2010-05-04 21:17:40 +0000

    updates to validate build data directory
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58239 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build/ImportedAssembly.pm
+++ Genome/Model/Build/ImportedAssembly.pm
@@ -14,18 +14,20 @@ sub create {
     
     my $self = $class->SUPER::create(@_)
 	or return;
-
+    #CHECK TO MAKE SURE DATA DIRECTORY EXISTS
     unless (-d $self->data_directory) {
 	    $self->error_message("Assembly data directory does not exist:\n\t".$self->data_directory);
+	    $self->delete;
 	    return;
     }
-
     #CHECK OF ALREADY EXISTING BUILDS WITH SAME ASSEMBLY DIRECTORY
+    #THERE SHOULDN'T BE MULTIPLE BUILDS WITH SAME DATA DIRECTORIES
     foreach my $build ($self->model->builds) {
         next if $build->build_id == $self->build_id;
 	    if ($build->data_directory eq $self->data_directory) {
 	        $self->error_message("A build with data directory: ".$self->data_directory." already exists\n\tBUILD ID: ".
 				     $build->build_id."\n\tDIR: ".$build->data_directory);
+		$self->delete;
 	        return;
 	    }
     }
