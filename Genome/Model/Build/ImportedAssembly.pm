455b8a0e2771690740a4580b603445ff79bf201b
jschindl <jschindl@genome.wustl.edu>
2010-05-04 17:52:29 +0000

    Added a check to make sure that we don't error out when the current build's data directory is the same as user specified data directory.  Before adding this check Imported Assembly would never run without this error message.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58224 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Build/ImportedAssembly.pm
+++ Genome/Model/Build/ImportedAssembly.pm
@@ -11,21 +11,23 @@ class Genome::Model::Build::ImportedAssembly {
 
 sub create {
     my $class = shift;
+    
     my $self = $class->SUPER::create(@_)
 	or return;
 
     unless (-d $self->data_directory) {
-	$self->error_message("Assembly data directory does not exist:\n\t".$self->data_directory);
-	return;
+	    $self->error_message("Assembly data directory does not exist:\n\t".$self->data_directory);
+	    return;
     }
 
     #CHECK OF ALREADY EXISTING BUILDS WITH SAME ASSEMBLY DIRECTORY
     foreach my $build ($self->model->builds) {
-	if ($build->data_directory eq $self->data_directory) {
-	    $self->error_message("A build with data directory: ".$self->data_directory." already exists\n\tBUILD ID: ".
-				 $build->build_id."\n\tDIR: ".$build->data_directory);
-	    return;
-	}
+        next if $build->build_id == $self->build_id;
+	    if ($build->data_directory eq $self->data_directory) {
+	        $self->error_message("A build with data directory: ".$self->data_directory." already exists\n\tBUILD ID: ".
+				     $build->build_id."\n\tDIR: ".$build->data_directory);
+	        return;
+	    }
     }
 
     return $self;
