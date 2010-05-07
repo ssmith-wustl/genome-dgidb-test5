7f4bf3feacb6e83c06098286092fb257e1913420
fdu <fdu@genome.wustl.edu>
2010-05-07 20:45:52 +0000

    skip GoldSnpConcordance report if gold_snp_path file is not valid
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58417 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Event/Build/ReferenceAlignment/RunReports.pm
+++ Genome/Model/Event/Build/ReferenceAlignment/RunReports.pm
@@ -17,7 +17,7 @@ my %REPORT_TYPES = (
     Mapcheck           => 'Mapcheck',
     DbSnpConcordance   => 'dbSNP_Concordance',
     GoldSnpConcordance => 'Gold_SNP_Concordance',
-    InputBaseCounts   => 'Input_Base_Counts'
+    InputBaseCounts    => 'Input_Base_Counts'
 );
 
 
@@ -43,14 +43,15 @@ sub execute {
         delete $REPORT_TYPES{DbSnpConcordance};
     }
 
-    my $gold_snp_path = $build->gold_snp_path;
-    unless ($gold_snp_path and -s $gold_snp_path) {
-        $self->status_message("No gold_snp_path provided for the build, skip its report");
+    unless ($self->validate_gold_snp_path) {
+        $self->status_message("No valid gold_snp_path for the build, skip GoldSnpConcordance report");
         delete $REPORT_TYPES{GoldSnpConcordance};
     }
+    
     unless ( ($model->dna_type eq 'cdna' || $model->dna_type eq 'rna') && $model->reference_sequence_name =~ /^XStrans_adapt_smallRNA_ribo/ ) {
         delete $REPORT_TYPES{ReferenceCoverage};
     }
+    
     unless ($self->create_directory($build->resolve_reports_directory) ) {
 	    die('Could not create reports directory at: '. $build->resolve_reports_directory);
     }
@@ -67,18 +68,22 @@ sub execute {
         }
         $report_name = $report_def->name;
         $self->status_message("Defined report with name $report_name");
+        
         my $report = $report_def->generate_report;
         unless ($report) {
             $self->error_message("Error generating $report_name ($report_class)!: " . $report_class->error_message());
             $report_def->delete;
             die($self->error_message);
-        } else {
+        } 
+        else {
             $self->status_message("Successfully generated report: $report_name");
         }
         $self->status_message("About to add report: $report_name to build: ".$self->build->id);
+        
         if ($build->add_report($report)) {
             $self->status_message('Saved report: '.$report);
-        } else {
+        } 
+        else {
             $self->error_message('Error saving '.$report.'. Error: '. $self->build->error_message);
             die($self->error_message);
         }
@@ -132,6 +137,27 @@ sub execute {
 
     return $self->verify_successful_completion;
 }
+
+
+sub validate_gold_snp_path {
+    my $self = shift;
+
+    my $gold_snp_path = $self->build->gold_snp_path;
+    unless ($gold_snp_path and -s $gold_snp_path) {
+        $self->status_message('No gold_snp_path provided for the build or it is empty');
+        return;
+    }
+
+    my $head    = `head -1 $gold_snp_path`;
+    my @columns = split /\s+/, $head;
+    
+    unless (@columns and @columns == 9) {
+        $self->status_message("Gold snp file: $gold_snp_path is not 9-column format");
+        return;
+    }
+    return 1;
+}
+    
 
 sub verify_successful_completion {
     my $self = shift;
