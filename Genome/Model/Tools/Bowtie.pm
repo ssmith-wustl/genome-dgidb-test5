bc568cd4d1f79a2820212bee72e10c63438e8048
jwalker <jwalker@genome.wustl.edu>
2010-05-11 16:23:23 +0000

    Add methods to resolve correct versions and meet the API that all other alignment tools use
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58555 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Bowtie.pm
+++ Genome/Model/Tools/Bowtie.pm
@@ -7,6 +7,8 @@ use Genome;
 use Data::Dumper;
 use File::Temp;
 
+my $BOWTIE_DEFAULT = '0.12.5';
+
 class Genome::Model::Tools::Bowtie {
     is => ['Command'],
     has => [
@@ -19,9 +21,10 @@ class Genome::Model::Tools::Bowtie {
                     },
         ],
     has_optional => [
-                     version => {
+                     use_version => {
                                  is    => 'string',
                                  doc   => 'version of Bowtie application to use',
+                                 default_value => $BOWTIE_DEFAULT
                              },
                      _tmp_dir => {
                                   is => 'string',
@@ -40,21 +43,48 @@ sub help_detail {                           # This is what the user will see wit
 EOS
 }
 
+my %BOWTIE_VERSIONS = (
+    '0.12.5' => '/gsc/pkg/bio/bowtie/bowtie-0.12.5',
+    '0.12.1' => '/gsc/pkg/bio/bowtie/bowtie-0.12.1',
+    '0.10.0.2' => '/gsc/pkg/bio/bowtie/bowtie-0.10.0.2',
+    '0.9.9.2' => '/gsc/pkg/bio/bowtie/bowtie-0.9.9.2',
+    '0.9.8' => '/gsc/pkg/bio/bowtie/bowtie-0.9.8',
+    '0.9.4' => '/gsc/pkg/bio/bowtie/bowtie-0.9.4',
+);
+
+sub path_for_bowtie_version {
+    my ($class, $version) = @_;
+    $version ||= $BOWTIE_DEFAULT;
+    my $path = $BOWTIE_VERSIONS{$version};
+    if (defined($path)) {
+        if ($self->arch_os =~ /64/) {
+            $path .= '-64';
+        }
+        return $path;
+    }
+    die 'No path found for bowtie version: '.$version;
+}
+
+sub default_bowtie_version {
+    die "default bowtie version: $BOWTIE_DEFAULT is not valid" unless $BOWTIE_VERSIONS{$BOWTIE_DEFAULT};
+    return $BOWTIE_DEFAULT;
+}
+
 sub create {
     my $class = shift;
-
     my $self = $class->SUPER::create(@_);
-
     unless ($self->arch_os =~ /64/) {
         $self->error_message('Most Bowtie tools must be run from 64-bit architecture');
         return;
     }
-    my $tempdir = File::Temp::tempdir(CLEANUP => 1);
-    $self->_tmp_dir($tempdir);
-
+    unless ($self->temp_directory) {
+        my $base_temp_directory = Genome::Utility::FileSystem->base_temp_directory;
+        my $temp_dir = File::Temp::tempdir($base_temp_directory .'/Bowtie-XXXX', CLEANUP => 1);
+        Genome::Utility::FileSystem->create_directory($temp_dir);
+        $self->_tmp_dir($temp_dir);
+    }
     return $self;
 }
-
 
 
 1;
