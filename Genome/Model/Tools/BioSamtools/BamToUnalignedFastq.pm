8810196603dd6ba78615e5e4f68136cf77a98ca9
jwalker <jwalker@genome.wustl.edu>
2010-05-11 20:26:03 +0000

    Improve documentation and remove warning about name sort since requirement has been removed from script.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58573 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/BioSamtools/BamToUnalignedFastq.pm
+++ Genome/Model/Tools/BioSamtools/BamToUnalignedFastq.pm
@@ -10,11 +10,11 @@ class Genome::Model::Tools::BioSamtools::BamToUnalignedFastq {
     has_input => [
         bam_file => {
             is => 'Text',
-            doc => 'A BAM format file of alignment data(WARNING: needs to be name sorted)'
+            doc => 'A BAM format file of alignment data.'
         },
         output_directory => {
             is => 'Text',
-            doc => 'A directory to output s_*_*_sequence.txt files',
+            doc => 'A directory to output s_*_*_sequence.txt files, two for unmapped pairs and one for unmapped mates',
         },
     ],
 };
@@ -22,7 +22,6 @@ class Genome::Model::Tools::BioSamtools::BamToUnalignedFastq {
 sub execute {
     my $self = shift;
 
-    #TODO: Add check to see if BAM is namesorted, if not name sort the BAM first
     my $cmd = $self->execute_path .'/bamToUnalignedFastq.pl '. $self->bam_file .' '. $self->output_directory;
     Genome::Utility::FileSystem->shellcmd(
         cmd => $cmd,
