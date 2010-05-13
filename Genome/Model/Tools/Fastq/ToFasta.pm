f7ea54d1644bfdf0675f13b060fa967530f37475
jwalker <jwalker@genome.wustl.edu>
2010-05-13 21:09:06 +0000

    A fix to deprecate the quality file and also remove the splitting on the @ character(@is a character in the quality string).
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58735 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Fastq/ToFasta.pm
+++ Genome/Model/Tools/Fastq/ToFasta.pm
@@ -8,66 +8,48 @@ use Genome;
 class Genome::Model::Tools::Fastq::ToFasta {
     is => 'Genome::Model::Tools::Fastq',
     has_input => [
-            fasta_file => {
-                           is => 'Text',
-                           doc => 'the output fasta format sequence file',
-                       },
-            quality_file => {
-                             is => 'Text',
-                             is_optional => 1,
-                             doc => 'the output fasta format quality file',
-                         },
-        ],
+        fasta_file => {
+            is => 'Text',
+            doc => 'the output fasta format sequence file',
+        },
+        quality_file => {
+            is => 'Text',
+            is_optional => 1,
+            is_deprecated => 1,
+            doc => 'the output fasta format quality file.  Deprecated since quality conversion not implemented.',
+        },
+    ],
 };
-
-sub create {
-    my $class = shift;
-    
-    my $self = $class->SUPER::create(@_);
-
-    return $self;
-}
 
 sub execute {
     my $self = shift;
-
-    #unless ($self->quality_file) {
+    unless ($self->quality_file) {
         $self->fastQ2A;
-    #}# else {
-     #   $self->bio_convert;
-    #}
+    } else {
+        $self->bio_convert;
+    }
     return 1;
 }
 
 sub fastQ2A {
     my $self = shift;
-    #This is bad, what if the first base has a quality of '@'....
-    local $/ = "\n@";
+
     my $in_fh = Genome::Utility::FileSystem->open_file_for_reading($self->fastq_file);
     my $out_fh = Genome::Utility::FileSystem->open_file_for_writing($self->fasta_file);
-    my $qual_fh;
-    if ($self->quality_file) {
-        $qual_fh = Genome::Utility::FileSystem->open_file_for_writing($self->quality_file);
-    }
-    while (my $line = $in_fh->getline) {
+    while (my $desc_line = $in_fh->getline) {
         # @HWI-EAS75:1:2:0:345#0/1
         # NCCGCGAGATCGGAAGAGCGGTTCAGCAGGAATGC
         # +HWI-EAS75:1:2:0:345#0/1
         # ENUUUXVUVUUTPTXTSTTQQQVVTTPQVVVVUTQ
-        chomp($line);
-        if ($line =~ /^\@/) { $line =~ s/\@//g }  # first-in
-        my ($id1, $nt, $id2, $qual) = split (/\n/, $line);
-        if ($id1 && $nt) {
-            print $out_fh '>' . $id1 . "\n";
-            print $out_fh $nt . "\n";
-        }
-        if ($qual_fh && $id1 && $qual) {
-            print $qual_fh '>' . $id1 . "\n";
-            print $qual_fh $qual . "\n";
-        }
+        $desc_line =~ s/^\@/\>/;
+        my $seq_line = $in_fh->getline;
+        my $opt_line = $in_fh->getline;
+        my $qual_line = $in_fh->getline;
+        print $out_fh $desc_line;
+        print $out_fh $seq_line;
     }
+    $out_fh->close;
     $in_fh->close;
-    local $/ = "\n";
     return 1;
 }
 
