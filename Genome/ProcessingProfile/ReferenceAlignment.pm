7886caf8653e1e6b82ed94c92200b4b69ebdcb6f
eclark <eclark@genome.wustl.edu>
2010-05-20 15:55:50 +0000

    Comment out extra Dumper call thats messing up views.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58962 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ReferenceAlignment.pm
+++ Genome/ProcessingProfile/ReferenceAlignment.pm
@@ -229,7 +229,7 @@ sub params_for_alignment {
                     filter_name => $assignment->filter_desc || undef
                 );
 
-    print Data::Dumper::Dumper(\%params);
+    #print Data::Dumper::Dumper(\%params);
 
     my @param_set = (\%params);
     return @param_set;
@@ -272,7 +272,7 @@ sub _resolve_subclass_name {
         my @seq_plat_param = grep { $_->name eq 'sequencing_platform' } @params;
         if (scalar(@seq_plat_param) == 1) {
             $sequencing_platform = $seq_plat_param[0]->value;
-        }
+        
     }  else {
         my %params = @_;
         $sequencing_platform = $params{sequencing_platform};
