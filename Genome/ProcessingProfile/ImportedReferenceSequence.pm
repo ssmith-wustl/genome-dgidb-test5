aaafa7eb773509a33c9209b8b128e86e30b3df60
ehvatum <ehvatum@genome.wustl.edu>
2010-05-20 17:37:21 +0000

    Conform to GC Perl coding standard.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58968 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ImportedReferenceSequence.pm
+++ Genome/ProcessingProfile/ImportedReferenceSequence.pm
@@ -20,20 +20,17 @@ sub _execute_build {
 
     my $model = $build->model;
 
-    if(!$model)
-    {
+    if(!$model) {
         $self->error_message("Couldn't find model for build id " . $build->build_id . ".");
         return;
     }
 
     my $fastaSize = -s $build->fasta_file;
-    unless(-e $build->fasta_file && $fastaSize > 0)
-    {
+    unless(-e $build->fasta_file && $fastaSize > 0) {
         $self->status_message("Reference sequence fasta file \"" . $build->fasta_file . "\" is either inaccessible, empty, or non-existent.");
         return;
     }
-    if($fastaSize >= $num4GiB)
-    {
+    if($fastaSize >= $num4GiB) {
         $self->status_message("Reference sequence fasta file \"". $build->fasta_file . "\" is larger than 4GiB.  In order to accommodate " .
                               "BWA, reference sequence fasta files > 4GiB are not supported.  Such sequences must be broken up and each chunk must " .
                               "have its own build and model(s).  Support for associating multiple fastas with a single reference model is " .
@@ -44,39 +41,35 @@ sub _execute_build {
     $self->status_message("Copying fasta");
     my $outDir = $build->data_directory;
     my $fasta = File::Spec->catfile($outDir, 'all_sequences.fa');
-    unless(copy($build->fasta_file, $fasta))
-    {
+    unless (copy($build->fasta_file, $fasta)) {
         $self->error_message("Failed to copy \"" . $build->fasta_file . "\" to \"$fasta\": $!.");
         return;
     }
 
     $self->status_message("Making bases files from fasta.");
 
+
     $self->status_message("Doing bwa indexing.");
     my $bwaIdxAlg = ($fastaSize < 11000000) ? "is" : "bwtsw";
-    if(system("bwa index -a $bwaIdxAlg $fasta") != 0)
-    {
+    if (system("bwa index -a $bwaIdxAlg $fasta") != 0) {
         $self->error_message("bwa indexing failed.");
     }
 
     $self->status_message("Doing maq fasta2bfa.");
     my $bfa = File::Spec->catfile($outDir, 'all_sequences.bfa');
-    if(system("maq fasta2bfa $fasta $bfa") != 0)
-    {
+    if (system("maq fasta2bfa $fasta $bfa") != 0) {
         $self->error_message("maq fasta2bfa failed.");
     }
 
     $self->status_message("Doing samtools faidx.");
-    if(system("samtools faidx $fasta") != 0)
-    {
+    if (system("samtools faidx $fasta") != 0) {
         $self->error_message("samtools faidx failed.");
     }
 
     # Reallocate to amount of space actually consumed if the build has an associated allocation and that allocation
     # has an absolute path the same as this build's data_path
     $self->status_message("Reallocating.");
-    if(defined($build->disk_allocation) && $outDir eq $build->disk_allocation->absolute_path)
-    {
+    if (defined($build->disk_allocation) && $outDir eq $build->disk_allocation->absolute_path) {
         my $duOut = `du -s -k $outDir`;
 		if ( $? == 0
 		     && $duOut =~ /^(\d+)\s/
@@ -84,8 +77,7 @@ sub _execute_build {
         {
             $build->disk_allocation->reallocate('kilobytes_requested' => $1);
         }
-        else
-        {
+        else {
             $self->error_message("Failed to determine the amount of space actually consumed by \"$outDir\" and therefore can not reallocate.");
             return;
         }
@@ -98,7 +90,5 @@ sub _execute_build {
 sub _resolve_disk_group_name_for_build {
     return 'info_apipe_ref';
 }
-
-
 
 1;
