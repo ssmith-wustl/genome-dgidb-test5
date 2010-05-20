d86b84af20c32e85faaa0e963106afa90b632fe0
ssmith <ssmith@genome.wustl.edu>
2010-05-20 20:37:06 +0000

    Added details from old pipeline so that nothing aligner-specific in the primary workflow is required
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@58998 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/AlignmentSet/Maq.pm
+++ Genome/InstrumentData/AlignmentSet/Maq.pm
@@ -6,8 +6,22 @@ use warnings;
 use Genome;
 
 class Genome::InstrumentData::AlignmentSet::Maq {
-    is => ['Genome::InstrumentData::AlignmentSet'],
+    is => 'Genome::InstrumentData::AlignmentSet',
     has_constant => [
-                     aligner_name => { value => 'maq' },
+        aligner_name => { value => 'maq' },
     ],
 };
+
+sub required_arch_os { 
+    'x86_64' 
+}
+
+sub required_rusage { 
+    "-R 'select[model!=Opteron250 && type==LINUX64] span[hosts=1] rusage[tmp=50000:mem=12000]' -M 1610612736";
+}
+
+sub extra_metrics {
+    'contaminated_read_count'
+}
+
+1;
