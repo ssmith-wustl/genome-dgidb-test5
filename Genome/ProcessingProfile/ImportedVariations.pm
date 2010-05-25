1b201fe7712c49717239217179ffe26d36363406
rlong <rlong@genome.wustl.edu>
2010-05-25 16:21:07 +0000

    Removed commented out portions of code which are not needed (they were copied over from the file that this was based on).
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59100 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/ProcessingProfile/ImportedVariations.pm
+++ Genome/ProcessingProfile/ImportedVariations.pm
@@ -47,78 +47,12 @@ sub _execute_build {
         return;
     }
 
-
-
     my $variationSize = -s $build->variation_file;
     unless(-e $build->variation_file && $variationSize > 0)
     {
         $self->status_message("Imported Variation file \"" . $build->variation_file . "\" is either inaccessible, empty, or non-existent.");
         return;
     }
-
-#    $build->
-
-
-=cut
-
-    if($fastaSize >= $num4GiB)
-    {
-        $self->status_message("Reference sequence fasta file \"". $build->fasta_file . "\" is larger than 4GiB.  In order to accommodate " .
-                              "BWA, reference sequence fasta files > 4GiB are not supported.  Such sequences must be broken up and each chunk must " .
-                              "have its own build and model(s).  Support for associating multiple fastas with a single reference model is " .
-                              "desired but will require modifying the alignment code.");
-        return;
-    }
-
-    # Copy the original fasta
-    my $outDir = $build->data_directory;
-    my $fasta = File::Spec->catfile($outDir, 'all_sequences.fasta');
-    unless(copy($build->fasta_file, $fasta))
-    {
-        $self->error_message("Failed to copy \"" . $build->fasta_file . "\" to \"$fasta\": $!.");
-        return;
-    }
-
-    $self->status_message("Doing bwa indexing.");
-    my $bwaIdxAlg = ($fastaSize < 11000000) ? "is" : "bwtsw";
-    if(system("bwa index -a $bwaIdxAlg $fasta") != 0)
-    {
-        $self->error_message("bwa indexing failed.");
-    }
-
-    $self->status_message("Doing maq fasta2bfa.");
-    my $bfa = File::Spec->catfile($outDir, 'all_sequences.bfa');
-    if(system("maq fasta2bfa $fasta $bfa") != 0)
-    {
-        $self->error_message("maq fasta2bfa failed.");
-    }
-
-    $self->status_message("Doing samtools faidx.");
-    if(system("samtools faidx $fasta") != 0)
-    {
-        $self->error_message("samtools faidx failed.");
-    }
-
-    # Reallocate to amount of space actually consumed if the build has an associated allocation and that allocation
-    # has an absolute path the same as this build's data_path
-    $self->status_message("Reallocating.");
-    if(defined($build->disk_allocation) && $outDir eq $build->disk_allocation->absolute_path)
-    {
-        my $duOut = `du -s -k $outDir`;
-		if ( $? == 0
-		     && $duOut =~ /^(\d+)\s/
-			 && $1 > 0 )
-        {
-            $build->disk_allocation->reallocate('kilobytes_requested' => $1);
-        }
-        else
-        {
-            $self->error_message("Failed to determine the amount of space actually consumed by \"$outDir\" and therefore can not reallocate.");
-            return;
-        }
-    }
-
-=cut
 
     $self->status_message("Done.");
     return 1;
