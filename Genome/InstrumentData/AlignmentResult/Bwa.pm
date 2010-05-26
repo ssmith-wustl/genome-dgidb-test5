7a45518eaa7de76ac0584e52c751daaab6d2a9f9
boberkfe <boberkfe@genome.wustl.edu>
2010-05-26 22:23:57 +0000

    search in /opt/fscache for refseq files first
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59219 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/AlignmentResult/Bwa.pm
+++ Genome/InstrumentData/AlignmentResult/Bwa.pm
@@ -33,6 +33,10 @@ sub _run_aligner {
     my $ref_basename = File::Basename::fileparse($reference_build->full_consensus_path('fa'));
     my $reference_fasta_path = sprintf("%s/%s", $reference_build->data_directory, $ref_basename);
 
+    if (-e "/opt/fscache/" . $reference_fasta_path) {
+        $reference_fasta_path = "/opt/fscache/" . $reference_fasta_path
+    }
+
     # decompose aligner params for each stage of bwa alignment
     my %aligner_params = $self->decomposed_aligner_params;
 
