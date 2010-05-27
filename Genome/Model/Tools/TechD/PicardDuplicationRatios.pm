376eff356cf32574420dd6ba4fd068d083b1e02c
jwalker <jwalker@genome.wustl.edu>
2010-05-27 15:00:51 +0000

    improve documentation
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59258 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/TechD/PicardDuplicationRatios.pm
+++ Genome/Model/Tools/TechD/PicardDuplicationRatios.pm
@@ -8,9 +8,25 @@ use Genome;
 class Genome::Model::Tools::TechD::PicardDuplicationRatios{
     is => ['Command'],
     has => {
-        build_id => { },
+        build_id => {
+            is => 'Number',
+            doc => 'The build id used to resolve the path to the Picard MarkDuplicates metrics file of interest.',
+        },
     },
 };
+
+sub help_brief {
+    'A temporary tool to print Picard MarkDuplicates duplication metrics for a build'
+}
+sub help_synopsis {
+    'Print build duplication metrics to terminal'
+}
+sub help_detail{
+    return <<"EOS"
+All builds currently use Picard MarkDuplicates to flag PCR duplicates after merging the individual instrument data alignments.
+One output from Picard MarkDuplicates is a metrics file that provides a per library breakdown of PCR and optical duplicate reads.  This tool simply parses the metrics file and dumps a hash reference of the per library breakdown.
+EOS
+}
 
 sub execute {
     my $self = shift;
