819e74f49acdfedca2ec88ff715173c3b0817c1f
jwalker <jwalker@genome.wustl.edu>
2010-05-27 14:59:58 +0000

    improve documentation
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59257 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/TechD/CoverageStatsSummary.pm
+++ Genome/Model/Tools/TechD/CoverageStatsSummary.pm
@@ -9,9 +9,25 @@ use Genome;
 class Genome::Model::Tools::TechD::CoverageStatsSummary{
     is => ['Command'],
     has => {
-        build_id => { },
+        build_id => { 
+            is => 'Number',
+            doc => 'The build id used to resolve the path to the RefCov stats summary file of interest.',},
     },
 };
+
+sub help_brief {
+    'A temporary tool to print a RefCov stats summary for a build'
+}
+sub help_synopsis {
+    'Print RefCov stats summary to terminal'
+}
+sub help_detail{
+    return <<"EOS"
+All capture builds currently use RefCov to evaluate coverage metrics including depth, breadth, contiguity, etc. of eacth region of interest.
+RefCov will output a STATS file that contains one line per region of interest and minimum depth filter.
+Typically coverage of a region is assessed at 1X, 5X, 10X, etc. minimum depth filters.  A summary file with means, medians and variance are produced as well.  This tool simply parses the summary file and dumps a hash reference of metrics to the terminal.
+EOS
+}
 
 sub execute {
     my $self = shift;
