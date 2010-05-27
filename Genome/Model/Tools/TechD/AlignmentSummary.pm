31824a182893bf28bcab23688c06851c0774cf80
jwalker <jwalker@genome.wustl.edu>
2010-05-27 14:58:34 +0000

    improve documenation
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59256 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/TechD/AlignmentSummary.pm
+++ Genome/Model/Tools/TechD/AlignmentSummary.pm
@@ -5,13 +5,30 @@ use warnings;
 
 use Genome;
 
-
 class Genome::Model::Tools::TechD::AlignmentSummary {
     is => ['Command'],
     has => {
-        build_id => { },
+        build_id => {
+            is => 'Number',
+            doc => 'The build id used to resolve the path to the alignment metrics file of interest.',
+        },
     },
 };
+
+sub help_brief {
+    'A temporary tool to print alignment metrics for a build'
+}
+sub help_synopsis {
+    'Print build alignment metrics to terminal'
+}
+sub help_detail{
+    return <<"EOS"
+Each capture build generates an alignment summary file per wingspan value.
+The alignment summary file contains basic alignment metrics as well as on/off target alignments.
+Duplication rates and paired-end metrics are also included.  This tool simply parses the tsv file
+and dumps the metrics to the a terminal.
+EOS
+}
 
 sub execute {
     my $self = shift;
