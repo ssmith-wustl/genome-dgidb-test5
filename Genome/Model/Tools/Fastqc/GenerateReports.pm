3ec9d525286b79f92468758a763c92bbfddb1f46
jwalker <jwalker@genome.wustl.edu>
2010-05-28 18:20:07 +0000

    A tool to run FastQC.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59346 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Fastqc/GenerateReports.pm
@@ -0,0 +1,32 @@
+package Genome::Model::Tools::Fastqc::GenerateReports;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::Fastqc::GenerateReports {
+    is => 'Genome::Model::Tools::Fastqc',
+    has => [
+        fastq_files => {
+            doc => 'The fastq files to generate quality reports for.',
+            is => 'Text',
+            is_many => 1,
+        },
+        report_directory => {
+            is => 'Text',
+            doc => 'A directory where all output report will be written',
+        }
+    ],
+};
+
+sub execute {
+    my $self = shift;
+    my @fastq_files = $self->fastq_files;
+    my $cmd = $self->fastqc_path .' -Dfastqc.output_dir='. $self->report_directory .' uk.ac.bbsrc.babraham.FastQC.FastQCApplication '. join(' ',@fastq_files) ;
+    $self->run_java_vm(
+        cmd => $cmd,
+        input_files => \@fastq_files,
+    );
+    return 1;
+}
