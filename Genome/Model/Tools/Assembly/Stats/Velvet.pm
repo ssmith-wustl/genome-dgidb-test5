3957ca22b2253b43895358e02b786e3a0ce6b78c
kkyung <kkyung@genome.wustl.edu>
2010-06-02 16:56:33 +0000

    made print to screen of stats optional
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59480 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Assembly/Stats/Velvet.pm
+++ Genome/Model/Tools/Assembly/Stats/Velvet.pm
@@ -10,32 +10,38 @@ use Data::Dumper;
 class Genome::Model::Tools::Assembly::Stats::Velvet {
     is => ['Genome::Model::Tools::Assembly::Stats'],
     has => [
-	    first_tier => {
-		type => 'int non_neg',
-		is_optional => 1,
-		doc => "first tier value",
-	        },
-	    second_tier => {
-		type => 'int non_neg',
-		is_optional => 1,
-		doc => "second tier value",
-	        },
-	    assembly_directory => {
-		type => 'String',
-		is_optional => 1,
-		doc => "path to assembly",
-		},
-	    major_contig_length => {
-		type => 'int non_neg',
-		is_optional => 1,
-		doc => "Major contig length cutoff",
-	        },
-	    out_file => {
-		type => 'String',
-		is_optional => 1,
-		doc => "Stats output file name",
-	        },
-	    ],
+	first_tier => {
+	    type => 'int non_neg',
+	    is_optional => 1,
+	    doc => "first tier value",
+	},
+	second_tier => {
+	    type => 'int non_neg',
+	    is_optional => 1,
+	    doc => "second tier value",
+	},
+	assembly_directory => {
+	    type => 'Text',
+	    is_optional => 1,
+	    doc => "path to assembly",
+	},
+	major_contig_length => {
+	    type => 'int non_neg',
+	    is_optional => 1,
+	    doc => "Major contig length cutoff",
+	},
+	out_file => { #TODO - rename this output_file
+	    type => 'Text',
+	    is_optional => 1,
+	    doc => "Stats output file name",
+	},
+	no_print_to_screen => {
+	    is => 'Boolean',
+	    is_optional => 1,
+	    default_value => 0,
+	    doc => 'Prevent printing of stats to screen',
+	},
+    ],
 };
 
 sub help_brief {
@@ -51,6 +57,12 @@ EOS
 sub execute {
     my ($self) = @_;
     my $stats;
+
+    #TODO - just have one print and one print to stats file statement
+
+    #TODO - fix so this doesn't run in edit_dir .. tests can not clean up
+    my $dir = cwd();
+
     unless ($self->resolve_data_directory()) {
 	$self->error_message("Unable to resolve data directory");
 	return;
@@ -59,41 +71,41 @@ sub execute {
     #SIMPLE READ STATS
     my ($s_stats, $five_k_stats, $content_stats) = $self->get_simple_read_stats();
     $stats .= $s_stats;
-    print $s_stats;
+    print $s_stats unless $self->no_print_to_screen;
 
     #CONTIGUITY STATS
     my $contiguity_stats = $self->get_contiguity_stats;
     $stats .= $contiguity_stats;
-    print $contiguity_stats;
+    print $contiguity_stats unless $self->no_print_to_screen;
 
     #CONSTRAINT STATS
     my $constraint_stats = $self->get_constraint_stats();
     $stats .= $constraint_stats;
-    print $constraint_stats;
+    print $constraint_stats unless $self->no_print_to_screen;
 
     #GENOME CONTENTS
     $stats .= $content_stats;
-    print $content_stats;
+    print $content_stats unless $self->no_print_to_screen;
 
     #GENE CORE SURVEY STATS
     #my $core_survey = $self->get_core_gene_survey_results();
     #$stats .= $core_survey;
-    #print $core_survey;
+    #print $core_survey unless $self->no_print_to_screen;
 
     #READ DEPTH STATS
     my $ace = `ls -t velvet_asm.ace* | grep -v base_depth | head -1`;
     chomp $ace;
     unless ($ace) {
-	$self->error_message("Can not file any velvet_asm.ace ace files");
+	$self->error_message("Can not find any velvet_asm.ace ace files");
 	return;
     }
     my $depth_stats = $self->get_read_depth_stats($ace);
     $stats .= $depth_stats;
-    print $depth_stats;
+    print $depth_stats unless $self->no_print_to_screen;
 
     #FIVE KB CONTIG STATS
     $stats .= $five_k_stats;
-    print $five_k_stats;
+    print $five_k_stats unless $self->no_print_to_screen;
 
     if ($self->out_file) {
 	my $out_file = $self->out_file;
@@ -102,7 +114,10 @@ sub execute {
 	$fh->close;
     }
 
-    print "############\n##  DONE  ##\n############\n";
+    print "############\n##  DONE  ##\n############\n" unless $self->no_print_to_screen;
+
+    #returning to original dir so tests can clean up
+    chdir $dir;
 
     return 1;
 }
