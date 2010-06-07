c6f58c5eeda6de61647086b7c5b7a62e562958bb
boberkfe <boberkfe@genome.wustl.edu>
2010-06-07 18:52:00 +0000

    ld library path for boost
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59655 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/InstrumentData/AlignmentResult.pm
+++ Genome/InstrumentData/AlignmentResult.pm
@@ -449,7 +449,7 @@ sub create_BAM_in_staging_directory {
 sub _compute_alignment_metrics {
     my $self = shift;
     my $bam = $self->temp_staging_directory . "/all_sequences.bam";
-    my $out = `bash -c "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/gscuser/ehvatum/repo/alignment_summary_cpp/yaml-cpp/build /gsc/var/tmp/alignment_summary_cpp_v1.2.1 --bam=\"$bam\""`;
+    my $out = `bash -c "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/gsc/pkg/boost/boost_1_42_0/lib:/gscuser/ehvatum/repo/alignment_summary_cpp/yaml-cpp/build /gsc/var/tmp/alignment_summary_cpp_v1.2.1 --bam=\"$bam\""`;
     unless ($? == 0) {
         $self->error_message("Failed to compute alignment metrics.");
         die $self->error_message;
