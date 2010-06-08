19af0012b5d3cbb467cadbfa1923c46b11a98ec3
jlolofie <jlolofie@genome.wustl.edu>
2010-06-08 20:45:54 +0000

    blurb for search result
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59725 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/WorkOrder/View/SearchResult/Xml.pm
+++ Genome/WorkOrder/View/SearchResult/Xml.pm
@@ -12,6 +12,11 @@ class Genome::WorkOrder::View::SearchResult::Xml {
             is => 'ARRAY',
             default => [
                 'id',
+                'barcode',
+                'pipeline',
+                'project_name',
+                'setup_name',
+                'setup_description',
                 {
                     name => 'project',
                     aspects => ['id', 'name'],
@@ -22,5 +27,11 @@ class Genome::WorkOrder::View::SearchResult::Xml {
         }
     ]
 };
+
+
+# GSC::Setup::WorkOrder
+#    my $name_string =$self->id . "-" .$name ."-" . $dna_count .$type .  "-" .$self->get_creation_event->quick_report_date;
+#
+
 
 1;
