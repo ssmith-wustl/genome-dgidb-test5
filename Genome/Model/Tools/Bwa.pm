489fab0f0ca922b800c8a9f24f12fd8d03320c78
boberkfe <boberkfe@genome.wustl.edu>
2010-06-14 18:41:58 +0000

    bwa0.5.8 is bad! don't use it! use 0.5.8a instead.  make 0.5.8 an invalid version.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@59929 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Bwa.pm
+++ Genome/Model/Tools/Bwa.pm
@@ -6,7 +6,7 @@ use warnings;
 use Genome;
 use File::Basename;
 
-my $DEFAULT = '0.5.8';
+my $DEFAULT = '0.5.8a';
 
 class Genome::Model::Tools::Bwa {
     is => 'Command',
@@ -53,7 +53,7 @@ my %BWA_VERSIONS = (
     '0.5.5' => '/gsc/pkg/bio/bwa/bwa-0.5.5-64/bwa',
     '0.5.6' => '/gsc/pkg/bio/bwa/bwa-0.5.6-64/bwa',
     '0.5.7' => '/gsc/pkg/bio/bwa/bwa-0.5.7-64/bwa',
-    '0.5.8' => '/gsc/pkg/bio/bwa/bwa-0.5.8-64/bwa',
+    '0.5.8a' => '/gsc/pkg/bio/bwa/bwa-0.5.8a-64/bwa',
     'bwa'   => 'bwa',
 );
 
