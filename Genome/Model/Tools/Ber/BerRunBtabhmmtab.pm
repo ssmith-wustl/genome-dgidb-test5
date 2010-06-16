c6a779e73cae76a76196d2cd9dd2327df6ffaa82
josborne <josborne@genome.wustl.edu>
2010-06-16 14:06:21 +0000

    fixup to handle newer hmmtohtab script in BER
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@60019 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Ber/BerRunBtabhmmtab.pm
+++ Genome/Model/Tools/Ber/BerRunBtabhmmtab.pm
@@ -10,10 +10,11 @@ use Carp;
 use Data::Dumper;
 use English;
 
-use File::Find;
+use IO::File;
+use IPC::Run;
+use File::Copy;
+use Time::HiRes qw(sleep);
 use File::Slurp;
-use File::Temp;
-use Workflow::Simple;
 
 use Cwd;
 
@@ -21,7 +22,7 @@ UR::Object::Type->define(
     class_name => __PACKAGE__,
     is         => 'Command',
     has        => [
-        'locus_tag' => {    # is locus_tag used at all here???
+        'locus_tag' => {
             is  => 'String',
             doc => "Locus tag for project, containing DFT/FNL postpended",
         },
@@ -71,163 +72,389 @@ sub execute
 {
     my $self = shift;
 
-    my @sequence_names = $self->get_sequence_names();
+    my @berrunbtabhmmtab = $self->gather_details();
 
-    # FIXME: use tempfile
-    my ( $btab_fh, $btabxml ) = File::Temp::tempfile(
-        "ber-btab-XXXXXX",
-        SUFFIX => '.xml',
-        DIR    => $self->berdirpath
-    );
-    write_file( $btabxml, $self->workflowxml() );
-
-   #    # this way is causing problems.... ?
-   #    my $w = Workflow::Model->create_from_xml(\*DATA);
-   #    my $result = $w->execute( 'locus tag'       => $self->locus_tag,
-   #                              'fastadir'        => $self->fastadirpath,
-   #                              'berdirpath'      => $self->berdirpath,
-   #                              'hmmdirpath'      => $self->hmmdirpath,
-   #                              'srcdirpath'      => $self->srcdirpath,
-   #                              'bsubfiledirpath' => $self->bsubfiledirpath,
-   #                              'seq names'       => \@sequence_names, );
-
-    $self->status_message("starting btab/htab jobs...");
-    my $result = run_workflow_lsf(
-        $btabxml,
-        'locus tag'       => $self->locus_tag,
-        'fastadir'        => $self->fastadirpath,
-        'berdirpath'      => $self->berdirpath,
-        'hmmdirpath'      => $self->hmmdirpath,
-        'srcdirpath'      => $self->srcdirpath,
-        'bsubfiledirpath' => $self->bsubfiledirpath,
-        'seq names'       => \@sequence_names,
-    );
-
-    unless ( defined($result) )
-    {
-        foreach my $error (@Workflow::Simple::ERROR)
-        {
-            $self->error_message(
-                join( "\t",
-                    $error->dispatch_identifier(), $error->name(),
-                    $error->start_time(),          $error->end_time(),
-                    $error->exit_code(), )
-            );
-
-            $self->error_message( $error->stdout() );
-            $self->error_message( $error->stderr() );
-        }
-        croak;
-    }
-
+    write_file( 'btabdump' . $self->locus_tag, Dumper( \@berrunbtabhmmtab ) );
     my $i = 0;
+    foreach my $runs (@berrunbtabhmmtab)
+    {
+        $i++;
 
-    # do a File::Find type deal here and grab the existing btab
-    # and htab files for the $i count (and prehaps rename $i to
-    # something more descriptive).
+        #print Dumper($runs);
 
-    # ??? do we need to worry about this???
+        #print qq{\n\n@$runs\n\n};
 
-    # in case we want to count the numbers of each type of file.
-    my @storageber = ();
-    my @storagehmm = ();
-    find(
-        sub {
-            if ( $_ =~ /\.btab$/ ) { push( @storageber, $_ ); }
-        },
-        $self->berdirpath
-    );
-    find(
-        sub {
-            if ( $_ =~ /\.htab$/ ) { push( @storagehmm, $_ ); }
-        },
-        $self->hmmdirpath
-    );
-    $i = scalar(@storageber) + scalar(@storagehmm);
+#print qq{\n\n$i\n\n};
+#IPC::Run::run(@$runs) || croak "\n\nCan't run BerRunBtabhmmtab.pm from BerRunBtabhmmtab.pm: $CHILD_ERROR : $OS_ERROR\n\n" . join(' ', @$runs). "\n$i";
+        my $retval = IPC::Run::run(@$runs);
+        unless ($retval)
+        {
+            carp
+                "\n\nCan't run BerRunBtabhmmtab.pm from BerRunBtabhmmtab.pm: $CHILD_ERROR : $OS_ERROR\n\n"
+                . join( ' ', @$runs )
+                . "\n$i running again...";
+            sleep(1);
+            print Dumper($runs), "\n";
+            IPC::Run::run(@$runs)
+                || croak
+                "\n\nCan't run BerRunBtabhmmtab.pm from BerRunBtabhmmtab.pm: $CHILD_ERROR : $OS_ERROR\n\n"
+                . join( ' ', @$runs ) . "\n$i "
+                . join( ' ', $runs->[0] ) . "\n";
+        }
 
-    # are the extra newlines needed????
-    print "\n\nTotal for Btab/Htab: ", $i, "\n\n";
+    }
+    print qq{\n\nTotal for Btab/Htab:\t$i\n\n};
+
+    while (1)
+    {
+
+        my $berdirpath = $self->berdirpath;
+        my $hmmdirpath = $self->hmmdirpath;
+
+        opendir( BERDIR, "$berdirpath" )
+            or die
+            "Can't open '$berdirpath' ...  from BerRunBtabhmmtab.pm : $OS_ERROR\n";
+        my @berfile = readdir(BERDIR);
+        closedir(BERDIR);
+
+        my @storageber = ();
+        foreach my $bername (@berfile)
+        {
+            next if $bername =~ m/^\.\.?$/;
+            my $btab = qq{.btab};
+
+            if ( $bername =~ /$btab$/ )
+            {
+                my $bernamecheck = qq{$berdirpath/$bername};
+
+                #if ( (-e $bernamecheck) && (! -z $bernamecheck)) {
+                if ( -e $bernamecheck )
+                {
+                    push( @storageber, $bername );
+                }
+            }
+        }
+
+        my $bercount = $#storageber + 1;
+
+        opendir( HMMDIR, "$hmmdirpath" )
+            or die
+            "Can't open '$hmmdirpath' ...  from BerRunBtabhmmtab.pm : $OS_ERROR\n";
+        my @hmmfile = readdir(HMMDIR);
+        closedir(HMMDIR);
+
+        my @storagehmm = ();
+        foreach my $hmmname (@hmmfile)
+        {
+            next if $hmmname =~ m/^\.\.?$/;
+            my $htab = qq{.htab};
+            if ( $hmmname =~ /$htab$/ )
+            {
+                my $hmmnamecheck = qq{$hmmdirpath/$hmmname};
+                if ( ( -e $hmmnamecheck ) && ( !-z $hmmnamecheck ) )
+                {
+                    push( @storagehmm, $hmmname );
+                }
+            }
+        }
+
+        my $hmmcount = $#storagehmm + 1;
+
+        my $totalcount = $bercount + $hmmcount;
+
+        unless ( $i == $totalcount )
+        {
+            sleep 300;
+            next;
+        }
+        else
+        {
+            sleep 120;
+            last;
+        }
+    }
 
     return 1;
 }
 
-# not sure if I need this or not yet.
-sub get_sequence_names
+sub gather_details
 {
-    my $self     = shift;
-    my $fastadir = $self->fastadirpath;
-    my @sequence_names;
-    find(
-        sub {
-            if ( -f $_ ) { push( @sequence_names, $_ ); }
-        },
-        $fastadir
+    my $self            = shift;
+    my $locus_tag       = $self->locus_tag;
+    my $fastadirpath    = $self->fastadirpath;
+    my $berdirpath      = $self->berdirpath;
+    my $hmmdirpath      = $self->hmmdirpath;
+    my $bsubfiledirpath = $self->bsubfiledirpath;
+    my $srcdirpath      = $self->srcdirpath;
+
+    my @allipc;
+    my $cwd = getcwd();
+    unless ( $cwd eq $srcdirpath )
+    {
+        chdir($srcdirpath)
+            or die
+            "Failed to change to '$srcdirpath'...  from BerRunBtabhmmtab.pm: $OS_ERROR\n\n";
+    }
+    opendir( FASTADIR, "$fastadirpath" )
+        or die
+        "Can't open '$fastadirpath' ...  from BerRunBtabhmmtab.pm: $OS_ERROR\n\n";
+
+    my @fastafile = readdir(FASTADIR);
+
+    closedir(FASTADIR);
+    my @fasta_store = ();
+    foreach my $fastaname (@fastafile)
+    {
+
+        my $fof = qq{fof};
+
+        next if $fastaname =~ m/^\.\.?$/;
+
+        if ( $fastaname =~ /^$locus_tag/ )
+        {
+
+            next if $fastaname =~ /$fof$/;
+
+            my $fastanamedir = qq{$fastadirpath/$fastaname};
+
+            if ( -e $fastanamedir )
+            {
+
+                push( @fasta_store, $fastaname );
+
+            }
+        }
+    }
+
+    opendir( BERDIR, "$berdirpath" )
+        or die
+        "Can't open '$berdirpath' ...  from BerRunBtabhmmtab.pm : $OS_ERROR\n";
+
+    my @berfile = readdir(BERDIR);
+
+    closedir(BERDIR);
+
+    my @ber_store = ();
+
+    foreach my $bername (@berfile)
+    {
+
+        next if $bername =~ m/^\.\.?$/;
+        my $nr = qq{.nr};
+
+        if ( $bername =~ /$nr$/ )
+        {
+
+            my $bernamecheck = qq{$berdirpath/$bername};
+
+            if ( ( -e $bernamecheck ) && ( !-z $bernamecheck ) )
+            {
+
+                push( @ber_store, $bername );
+            }
+        }
+    }
+
+    opendir( HMMDIR, "$hmmdirpath" )
+        or die
+        "Can't open '$hmmdirpath' ...  from BerRunBtabhmmtab.pm : $OS_ERROR\n";
+
+    my @hmmfile = readdir(HMMDIR);
+
+    closedir(HMMDIR);
+
+    my @hmm_store = ();
+
+    foreach my $hmmname (@hmmfile)
+    {
+
+        next if $hmmname =~ m/^\.\.?$/;
+        my $hmm = qq{.hmmpfam};
+
+        if ( ( $hmmname =~ /$hmm$/ ) )
+        {
+
+            my $hmmnamecheck = qq{$hmmdirpath/$hmmname};
+            if ( ( -e $hmmnamecheck ) && ( !-z $hmmnamecheck ) )
+            {
+
+                push( @hmm_store, $hmmname );
+
+            }
+        }
+    }
+
+    my $fastacount = 0;
+    my $bercount   = 0;
+    my $hmmcount   = 0;
+
+    $fastacount = scalar(@fasta_store);
+    $bercount   = scalar(@ber_store);
+    $hmmcount   = scalar(@hmm_store);
+
+    if ( $fastacount > $bercount )
+    {
+
+        die
+            qq{\nWARNING, THE PROGRAM HAS STOPPED and Btab run has not started, since your Fasta counts are greater than Ber (Blastp) counts!($fastacount\t$bercount)\n\n};
+    }
+    elsif ( $fastacount < $bercount )
+    {
+
+        die
+            qq{\nWARNING, THE PROGRAM  HAS STOPPED and Btab run has not started, since your Ber (Blastp) counts are greater than Fasta counts!($fastacount\t$bercount) \n\n};
+
+    }
+    elsif ( $fastacount == $bercount )
+    {
+
+        warn
+            qq{\nFasta and Ber (Blastp) counts are equal ($fastacount\t$bercount), the program will continue to submit the Btab run, :) \n\n};
+
+    }
+    else
+    {
+
+        die
+            qq{WARNING, THE PROGRAM has STOPPED! PROBLEMS with the bercount.  Btab and Htab have not run\n\n};
+
+    }
+
+    if ( $fastacount > $hmmcount )
+    {
+
+        die
+            qq{\nWARNING, THE PROGRAM WILL STOP and Htab run has not started, since your Fasta counts are greater than hmmpfam counts!($fastacount\t$hmmcount)\n\n};
+    }
+    elsif ( $fastacount < $hmmcount )
+    {
+
+        die
+            qq{\n WARNING, THE PROGRAM WILL STOP and Btab run has not started, since your hmmpfam counts are greater than Fasta counts!($fastacount\t$hmmcount) \n\n};
+
+    }
+    elsif ( $fastacount == $hmmcount )
+    {
+
+        warn
+            qq{\nFasta and hmmpfam counts are equal ($fastacount\t$hmmcount), the program will continue to submit the Htab run, :)\n\n};
+
+    }
+    else
+    {
+
+        die
+            qq{WARNING, THE PROGRAM has STOPPED!  PROBLEMS in the hmmcount.  Btab and Htab have not run\n\n};
+    }
+    #################################################
+    #################################################
+    my $btabcount = 0;
+    my $htabcount = 0;
+    my $datecode  = join( '.', time, $PID );
+    my $Rbp       = qq{rusage[mem=4096]};
+
+    #btab send to lsf
+    foreach my $btabfile (@ber_store)
+    {
+
+        my $btabout = qq{$berdirpath/$btabfile.btab};
+        my $btabin  = qq{$berdirpath/$btabfile};
+        my $btablog = qq{$srcdirpath/$locus_tag.btab.$datecode.log};
+        my $btab    = qq{$srcdirpath/wu-blast2btab.pl};
+        my $perlI   = qq{$srcdirpath/lib};
+
+#my $btabcmd     = qq{$perlib $perlI $btab $input $btabin $output $btabout $log $btablog};
+        my $bsubbtaberr = qq{$bsubfiledirpath/bsub.err.btab.$btabfile.btab};
+        my $bsubbtabout = qq{$bsubfiledirpath/bsub.out.btab.$btabfile.btab};
+
+        my @btabcmd = (
+            'perl',    '-I',    $perlI,     $btab,
+            '--input', $btabin, '--output', $btabout,
+            '--log',   $btablog,
+        );
+
+        #my $bsubcmdbb = qq{$bsubbb $obb $ebb $qbb $nbb $Rbb $btabcmd};
+
+        my @bsubcmdb = (
+            'bsub',       '-o', $bsubbtabout, '-e',
+            $bsubbtaberr, '-q', 'short',      '-n',
+            '1',          '-R', $Rbp,         @btabcmd,
+        );
+
+        $btabcount++;
+
+        my @btabipc = (
+
+            #\@bsubcmdb,
+            \@btabcmd,
+            \undef,
+            '2>&1',
+        );
+
+        push( @allipc, \@btabipc );
+    }
+
+    #htab send to lsf
+    my $htabinfo = qq{$srcdirpath/hmm_info.txt};
+    unless ( -e $htabinfo )
+    {
+        die
+            qq{PROBLEMS with hmm_info.txt at '$srcdirpath'...  from BerRunBtabhmmtab.pm : $OS_ERROR\n\n};
+    }
+
+    my @hmmfiles;
+HTAB: foreach my $htabfile (@hmm_store)
+    {
+
+        my $htabout = qq{$hmmdirpath/$htabfile.htab};
+
+        # create a temp file or something, fill it with a list of files and
+        # feed that to hmmToHtab.pl via the -f option.
+        # when do we want to enable this?
+        #      if(-f $htabout )
+        #      {
+        #          next HTAB;
+        #      }
+        push( @hmmfiles, $hmmdirpath . "/" . $htabfile . "\n" );
+
+    }
+    write_file( $hmmdirpath . "/" . $locus_tag . ".tablist", @hmmfiles );
+    my $htabin  = qq{$hmmdirpath/$locus_tag.htablist};
+    my $htablog = qq{$srcdirpath/$locus_tag.htab.$datecode.log};
+    my $htab    = qq{$srcdirpath/hmmToHtab.pl};
+
+    #my $htabcmd     = qq{$htab $htabh $htabinfo $htabf $htabin};
+    #my $bsubhtaberr = qq{$bsubfiledirpath/bsub.err.htab.$htabfile.htab};
+    #my $bsubhtabout = qq{$bsubfiledirpath/bsub.out.htab.$htabfile.htab};
+    my $bsubhtaberr = qq{$bsubfiledirpath/bsub.err.htab.$locus_tag.htab};
+    my $bsubhtabout = qq{$bsubfiledirpath/bsub.out.htab.$locus_tag.htab};
+
+    my @htabcmd = ( $htab, '-H', $htabinfo, '-f', $htabin, );
+
+    #my $bsubcmdhb = qq{$bsubhb $ohb $ehb $qhb $nhb $Rhb $htabcmd};
+    my @bsubcmdh = (
+        'bsub',       '-o', $bsubhtabout, '-e',
+        $bsubhtaberr, '-q', 'short',      '-n',
+        '1',          '-R', $Rbp,         @htabcmd,
     );
-    return @sequence_names;
+
+    my @htabipc = (
+
+        #\@bsubcmdh,
+        \@htabcmd,
+        \undef,
+        '2>&1',
+    );
+    push( @allipc, \@htabipc );
+
+    $htabcount++;
+
+    #}
+
+    print
+        qq{\n\nSent $btabcount\t wu-blast2btab.pl jobs to lsf from BerRunBtabhmmtab.pm!\n\n};
+    print
+        qq{\n\nSent $htabcount\thmmToHtab.pl jobs to lsf from BerRunBtabhmmtab.pm!\n\n};
+
+    return @allipc;
 }
-
-sub workflowxml
-{
-    my $self = shift;
-
-    return qq(<?xml version='1.0' standalone='yes'?>
-<workflow name="BER btab and htab" >
-  <link fromOperation="input connector" fromProperty="locus tag" toOperation="BER btabhmmtab" toProperty="locus_tag" />
-  <link fromOperation="input connector" fromProperty="fastadir" toOperation="BER btabhmmtab" toProperty="fastadir" />
-  <link fromOperation="input connector" fromProperty="berdirpath" toOperation="BER btabhmmtab" toProperty="berdirpath" />
-  <link fromOperation="input connector" fromProperty="hmmdirpath" toOperation="BER btabhmmtab" toProperty="hmmdirpath" />
-  <link fromOperation="input connector" fromProperty="srcdirpath" toOperation="BER btabhmmtab" toProperty="srcdirpath" />
-  <link fromOperation="input connector" fromProperty="bsubfiledirpath" toOperation="BER btabhmmtab" toProperty="bsubfiledirpath" />
-  <link fromOperation="input connector" fromProperty="seq names" toOperation="BER btabhmmtab" toProperty="sequence_names" />
-  <link fromOperation="BER btabhmmtab" fromProperty="success" toOperation="output connector" toProperty="success" />
-
-  <operation name="BER btabhmmtab">
-    <operationtype commandClass="PAP::Command::BtabHmmtab" typeClass="Workflow::OperationType::Command" />
-  </operation>
-
-  <operationtype typeClass="Workflow::OperationType::Model">
-    <inputproperty>locus tag</inputproperty>
-    <inputproperty>fastadir</inputproperty>
-    <inputproperty>berdirpath</inputproperty>
-    <inputproperty>hmmdirpath</inputproperty>
-    <inputproperty>srcdirpath</inputproperty>
-    <inputproperty>bsubfiledirpath</inputproperty>
-    <inputproperty>seq names</inputproperty>
-    <outputproperty>success</outputproperty>
-  </operationtype>
-
-</workflow>
-);
-
-}
-
-1;
-
-__DATA__
-<?xml version='1.0' standalone='yes'?>
-<workflow name="BER btab and htab" >
-  <link fromOperation="input connector" fromProperty="locus tag" toOperation="BER btabhmmtab" toProperty="locus_tag" />
-  <link fromOperation="input connector" fromProperty="fastadir" toOperation="BER btabhmmtab" toProperty="fastadir" />
-  <link fromOperation="input connector" fromProperty="berdirpath" toOperation="BER btabhmmtab" toProperty="berdirpath" />
-  <link fromOperation="input connector" fromProperty="hmmdirpath" toOperation="BER btabhmmtab" toProperty="hmmdirpath" />
-  <link fromOperation="input connector" fromProperty="srcdirpath" toOperation="BER btabhmmtab" toProperty="srcdirpath" />
-  <link fromOperation="input connector" fromProperty="bsubfiledirpath" toOperation="BER btabhmmtab" toProperty="bsubfiledirpath" />
-  <link fromOperation="input connector" fromProperty="seq names" toOperation="BER btabhmmtab" toProperty="sequence_names" />
-  <link fromOperation="BER btabhmmtab" fromProperty="success" toOperation="output connector" toProperty="success" />
-
-  <operation name="BER btabhmmtab">
-    <operationtype commandClass="PAP::Command::BtabHmmtab" typeClass="Workflow::OperationType::Command" />
-  </operation>
-
-  <operationtype typeClass="Workflow::OperationType::Model">
-    <inputproperty>locus tag</inputproperty>
-    <inputproperty>fastadir</inputproperty>
-    <inputproperty>berdirpath</inputproperty>
-    <inputproperty>hmmdirpath</inputproperty>
-    <inputproperty>srcdirpath</inputproperty>
-    <inputproperty>bsubfiledirpath</inputproperty>
-    <inputproperty>seq names</inputproperty>
-    <outputproperty>success</outputproperty>
-  </operationtype>
-
-</workflow>
