f589c621cc2d5c80293d8dc2af01620fa17437bc
ssmith <ssmith@genome.wustl.edu>
2010-06-19 04:14:25 +0000

    New base class for command to remove items.  New build removal command.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@60101 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Command/Remove.pm
@@ -0,0 +1,46 @@
+package Genome::Command::Remove;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Command::Remove {
+    is => 'Genome::Command::OO',
+    #has_input => [
+    #    items => { 
+    #        is => 'Genome::XXXX',
+    #        shell_args_position => 1,
+    #        is_many => 1,
+    #        doc => 'items to remove, specified by id or expression'
+    #    },
+    #],
+    has_optional_transient => [
+        _deletion_params => { is_many => 1 },
+    ],
+    doc => 'delete selected items from the system',
+};
+
+sub execute {
+    my $self = shift;
+    
+    my @i = $self->items();
+    my @deletion_params = $self->_deletion_params();
+
+    $self->status_message("Removing " . scalar(@i) . " " . $i[0]->__label_name__ . " entries...");
+    sleep 5;
+    
+    for my $i (@i) {
+        $self->status_message("deleting " . $i->__display_name__ . "...");
+        $i->delete(@deletion_params);
+        print "$i\n";
+    }
+
+    $self->status_message("deletion complete.");
+    return 1;
+}
+
+1;
+
+#$HeadURL: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Genome/InstrumentData/Command/Remove.pm $
+#$Id: Remove.pm 53285 2009-11-20 21:28:55Z fdu $
--- Genome/Model/Build/Command/Remove.pm
+++ Genome/Model/Build/Command/Remove.pm
@@ -2,52 +2,37 @@ package Genome::Model::Build::Command::Remove;
 
 use strict;
 use warnings;
-
 use Genome;
 
 class Genome::Model::Build::Command::Remove {
-    is => 'Genome::Model::Build::Command::Base',
-    doc => 'delete a build and all of its data from the system',
-    has_optional =>
-    [
-        keep_build_directory => 
-        {
+    is => 'Genome::Command::Remove',
+    has_input => [
+        items => { 
+            is => 'Genome::Model::Build',
+            shell_args_position => 1,
+            is_many => 1,
+            doc => 'builds to remove, specified by id or expression'
+        },
+        keep_build_directory => {
             is => 'Boolean',
             default_value => 0,
+            is_optional => 1,
             doc => 'A boolean flag to allow the retention of the model directory after the model is purged from the database.(default_value=0)',
         }    
     ],
+    doc => 'delete a build and all of its data from the system',
 };
 
-#< Command >#
 sub sub_command_sort_position { 7 }
 
-sub help_brief {
-    return 'Remove a build and all of its data';
-}
-
 sub help_detail {
-    "This command will remove the build and all events that make up the build";
+    "This command will remove a build from the system.  The rest of the model remains the same, as does independent data like alignments.";
 }
 
-#< Execute >#
 sub execute {
-    my $self = shift;    
-
-    # Get build
-    my $build = $self->_resolve_build
-        or return;
-    
-    # Delete - also abandons
-    unless ( $build->delete(keep_build_directory => $self->keep_build_directory)) {
-        $self->error_message('Failed to remove build ('.$build->id.')');
-        return;
-    }
-
-    return 1;
+    my $self = shift;
+    $self->_deletion_params([keep_build_directory => $self->keep_build_directory]);
+    return $self->SUPER::_execute_body();
 }
 
 1;
-
-#$HeadURL$
-#$Id$
