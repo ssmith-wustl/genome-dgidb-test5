8a77ee8c4cbe376029147687843ed608fa17a3f7
ssmith <ssmith@genome.wustl.edu>
2010-06-19 22:38:46 +0000

    Made the method wrapper take an optional argument for the method name.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@60129 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Event/Build/ProcessingProfileMethodWrapper.pm
+++ Genome/Model/Event/Build/ProcessingProfileMethodWrapper.pm
@@ -2,22 +2,26 @@ package Genome::Model::Event::Build::ProcessingProfileMethodWrapper;
 
 use strict;
 use warnings;
-
 use Genome;
 
 # this command is not intended to be executed by users
+# the only purpose of this command is to call _execute_build, or some other method, on the processing_profile
 
 class Genome::Model::Event::Build::ProcessingProfileMethodWrapper {
     is  => 'Command',
     has_input => [
         build_id => {
             is  => 'Number',
-            doc => 'Get build by id'
+            doc => 'specify the build by id'
         },
+        method_name => {
+            is => 'Text',
+            is_optional => 1,
+            default_value => '_execute_build',
+            doc => 'the method on the processing profile to call, passing a build (defaults to _execute_build())',
+        }
     ]
 };
-
-# the only purpose of this command is to call _execute_build on the processing_profile
 
 sub execute {
     my $self = shift;
@@ -27,16 +31,16 @@ sub execute {
     my $build = Genome::Model::Build->get($self->build_id) 
       or die 'cannot load build object for ' . $self->build_id;
 
+    my $method_name = $self->method_name;
+
     my $pp = $build->processing_profile;
 
-    my $rv = $pp->_execute_build($build);
-    die '_execute_build returned undef' if !defined $rv;
-    die '_execute_build returned false' if !$rv;
+    my $rv = $pp->$method_name($build);
+    die $method_name . ' returned undef' if !defined $rv;
+    die $method_name . ' returned false' if !$rv;
 
     return $rv;
 }
 
 1;
 
-#$HeadURL$
-#$Id$
