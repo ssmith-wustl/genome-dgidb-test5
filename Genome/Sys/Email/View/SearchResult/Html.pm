e2a9aa459cd127ce53f010901cc0f3d7fdbde8db
jlolofie <jlolofie@genome.wustl.edu>
2010-06-25 18:49:09 +0000

    dont need this stuff anymore because clark fixed Genome::Sys::Email
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@60364 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Sys/Email/View/SearchResult/Html.pm
+++ Genome/Sys/Email/View/SearchResult/Html.pm
@@ -9,18 +9,5 @@ class Genome::Sys::Email::View::SearchResult::Html {
     is => 'Genome::View::SearchResult::Html',
 };
 
-sub _update_view_from_subject {
-    my $self = shift;
-    
-    my $solr_doc = $self->solr_doc;
-    if($solr_doc) {
-        my $subject = $self->subject;
-        if($subject and not $subject->is_initialized) {
-            $subject->initialize($solr_doc);
-        }
-    }
-    
-    return $self->SUPER::_update_view_from_subject(@_);
-}
 
 1;
--- Genome/Sys/Email/View/SearchResult/Xml.pm
+++ Genome/Sys/Email/View/SearchResult/Xml.pm
@@ -25,18 +25,5 @@ class Genome::Sys::Email::View::SearchResult::Xml {
     ]
 };
 
-sub _update_view_from_subject {
-    my $self = shift;
-    
-    my $solr_doc = $self->solr_doc;
-    if($solr_doc) {
-        my $subject = $self->subject;
-        if($subject and not $subject->is_initialized) {
-            $subject->initialize($solr_doc);
-        }
-    }
-    
-    return $self->SUPER::_update_view_from_subject(@_);
-}
 
 1;
