b69cda631fe3ae794a77b20be5f4eee17797d587
jlolofie <jlolofie@genome.wustl.edu>
2010-06-28 18:57:36 +0000

    separating out caching stuff
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@60418 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Memcache.pm
@@ -0,0 +1,45 @@
+
+package Genome::Memcache;
+
+use strict;
+use warnings;
+
+
+class Genome::Memcache {
+     is => 'UR::Singleton',
+    doc => 'methods for accessing memcache',
+    has => {
+        memcache_server_location => {
+            is => 'Text',
+            default_value => Genome::Config::dev_mode()
+                            ? 'imp:11211'
+                            : 'aims-dev:11211',
+        },
+        _memcache_server => {
+            is => 'Cache::memcache',
+            is_transient => 1,
+        },
+        memcache_server => {
+            calculate_from => ['_memcache_server', 'memcache_server_location'],
+            calculate => q{ return $_memcache_server || new Cache::Memcached {'servers' => [$memcache_server_location], 'debug' => 0, 'compress_threshold' => 10_000,} }
+        },
+    }
+};
+
+
+
+
+sub server {
+
+    my ($class) = @_;
+
+    my $server = $class->_singleton_object->memcache_server();
+    return $server;
+}
+
+
+1;
+
+
+
+
