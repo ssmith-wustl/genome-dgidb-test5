18c51181723398fc1d04a5aceaa76bfd6f945646
adukes <adukes@genome.wustl.edu>
2010-06-30 16:37:51 +0000

    fix indentation
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@60517 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Genome/Model/Tools/Sam/SortBam.pm
+++ Genome/Model/Tools/Sam/SortBam.pm
@@ -20,11 +20,11 @@ class Genome::Model::Tools::Sam::SortBam {
 	    is_optional => 1,
 	    doc => 'Sort by read names instead of chrom coordinates'
 	},
-        output_file => {
-            is  => 'String',
-            doc => 'Output file, defaults to file_name.sorted.bam',
-	    is_optional => 1,
-        },
+    output_file => {
+        is  => 'String',
+        doc => 'Output file, defaults to file_name.sorted.bam',
+        is_optional => 1,
+    },
     ],
 };
 
@@ -46,28 +46,28 @@ sub execute {
     #samtools sort does dumb things with the file extension.  must snap off the output filename.
 
     my $real_output_file; 
-  
+
     if ($self->output_file) {
-	$real_output_file = $self->output_file;
-    	$real_output_file =~ s/\.bam$//;
+        $real_output_file = $self->output_file;
+        $real_output_file =~ s/\.bam$//;
     } else {
-	$real_output_file = $self->file_name;
-    	$real_output_file =~ s/\.bam$//;
-	$real_output_file .= ".sorted";	
+        $real_output_file = $self->file_name;
+        $real_output_file =~ s/\.bam$//;
+        $real_output_file .= ".sorted";	
     }
 
     $self->status_message(sprintf("attempting to sort %s into file with prefix %s", $self->file_name, $real_output_file));
-   
+
     my $sort_params = ($self->name_sort ? " -n " : "");
     $sort_params .= ' -m '. $self->maximum_memory;
-   
+
     my $sam_sort_cmd = sprintf("%s sort %s %s %s", $self->samtools_path, $sort_params, $self->file_name,  $real_output_file);
     my $sam_sort_rv = Genome::Utility::FileSystem->shellcmd(cmd=>$sam_sort_cmd, input_files=>[$self->file_name], output_files=>[$real_output_file.".bam"], skip_if_output_is_present=>0);
     if ($sam_sort_rv != 1) {
-	$self->error_message("Bam sort error.  Return value $sam_sort_rv");
-	return;
+        $self->error_message("Bam sort error.  Return value $sam_sort_rv");
+        return;
     }
- 
+
     $DB::single = 1;
     return 1;
 }
