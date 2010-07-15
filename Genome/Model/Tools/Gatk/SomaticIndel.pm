5106175737b75c833c40d4bc0ddac3d3998e2581
dkoboldt <dkoboldt@genome.wustl.edu>
2010-07-15 16:43:43 +0000

    New tools for running the GATK indel caller
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk@61008 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Genome/Model/Tools/Gatk/SomaticIndel.pm
@@ -0,0 +1,157 @@
+
+package Genome::Model::Tools::Gatk::SomaticIndel;     # rename this when you give the module file a different name <--
+
+#####################################################################################################################################
+# SomaticIndel - Call the GATK somatic indel detection pipeline
+#					
+#	AUTHOR:		Dan Koboldt (dkoboldt@watson.wustl.edu)
+#
+#	CREATED:	15-Jul-2010 by D.K.
+#	MODIFIED:	15-Jul-2010 by D.K.
+#
+#	NOTES:	
+#			
+#####################################################################################################################################
+
+use strict;
+use warnings;
+use FileHandle;
+use Genome;                                 # using the namespace authorizes Class::Autouse to lazy-load modules under it
+
+class Genome::Model::Tools::Gatk::SomaticIndel {
+	is => 'Command',                       
+	
+	has => [                                # specify the command's single-value properties (parameters) <--- 
+		normal_bam	=> { is => 'Text', doc => "BAM File for Normal Sample", is_optional => 0, is_input => 1 },
+		tumor_bam	=> { is => 'Text', doc => "BAM File for Tumor Sample", is_optional => 0, is_input => 1 },
+		output_file     => { is => 'Text', doc => "Output file to receive formatted lines", is_optional => 0, is_input => 1, is_output => 1 },
+		bed_output_file => { is => 'Text', doc => "Optional abbreviated output in BED format", is_optional => 1, is_input => 1, is_output => 1 },
+		formatted_file => { is => 'Text', doc => "Optional output file of indels in annotation format", is_optional => 1, is_input => 1, is_output => 1 },
+		somatic_file => { is => 'Text', doc => "Optional output file for Somatic indels parsed from formatted file", is_optional => 1, is_input => 1, is_output => 1 },
+		gatk_params => { is => 'Text', doc => "Parameters for GATK", is_optional => 1, is_input => 1, is_output => 1, default => "-R /gscmnt/839/info/medseq/reference_sequences/NCBI-human-build36/all_sequences.fa -T IndelGenotyperV2 --verbose --somatic --window_size 300" },
+		path_to_gatk => { is => 'Text', doc => "Path to GATK command", is_optional => 1, is_input => 1, is_output => 1, default => "java -jar /gscuser/dshen/scripts/gatk/dist/GenomeAnalysisTK.jar" },
+	],
+};
+
+sub sub_command_sort_position { 12 }
+
+sub help_brief {                            # keep this to just a few words <---
+    "Runs the GATK somatic indel detection pipeline"                 
+}
+
+sub help_synopsis {
+    return <<EOS
+This command runs the GATK somatic indel detection pipeline
+EXAMPLE:	gmt gatk somatic-indel --normal-bam Normal.bam --tumor-bam Tumor.bam --output-file GATK.indel --bed-output GATK.indel.bed
+EOS
+}
+
+sub help_detail {                           # this is what the user will see with the longer version of help. <---
+    return <<EOS 
+
+EOS
+}
+
+
+################################################################################################
+# Execute - the main program logic
+#
+################################################################################################
+
+sub execute {                               # replace with real execution logic.
+	my $self = shift;
+
+	## Run GATK ##
+	my $path_to_gatk = $self->path_to_gatk;
+	my $gatk_params = $self->gatk_params;
+	#-I /gscmnt/sata905/info/model_data/2858219475/build103084961/alignments/103084961_merged_rmdup.bam
+	#-I /gscmnt/sata871/info/model_data/2858334303/build103084933/alignments/103084933_merged_rmdup.bam
+	#-O gatk_testing/indels.GATK.H_GP-13-0890-01A-01-1.tsv -o gatk_testing/indels.GATK.H_GP-13-0890-01A-01-1.out 
+	
+	my $output_file = $self->output_file;
+	my $cmd = join(" ", $path_to_gatk, $gatk_params, "-I", $self->normal_bam, "-I", $self->tumor_bam, "-O", $output_file);
+
+	if($self->bed_output_file)
+	{
+		my $bed_output_file = $self->bed_output_file;
+		$cmd .= " -o $bed_output_file";
+	}
+
+	## Run GATK Command ##
+
+	print "$cmd\n";
+
+	if($self->formatted_file)
+	{
+		print "Formatting indels for annotation...\n";
+		
+		my $formatted_output_file = $self->formatted_file;
+		## Format GATK Indels ##
+	
+		my $cmd_obj = Genome::Model::Tools::Gatk::FormatIndels->create(
+		    variants_file => $output_file,
+		    output_file => $formatted_output_file,
+		);
+
+		$cmd_obj->execute;			
+
+		if($self->somatic_file)
+		{
+			print "Parsing out Somatic indels...\n";
+			## Parse the results to the somatic output file ##
+			parse_somatic($formatted_output_file, $self->somatic_file);			
+		}
+
+	}
+	
+
+}
+
+
+
+################################################################################################
+# Parse_Somatic - isolate somatic indels 
+#
+################################################################################################
+
+sub parse_somatic
+{
+	my $FileName = shift(@_);
+	my $OutFileName = shift(@_);
+
+	open(OUTFILE, ">$OutFileName") or die "Can't open outfile: $!\n";
+	
+	## Parse the variants file ##
+	
+	my $input = new FileHandle ($FileName);
+	my $lineCounter = 0;
+
+	while (<$input>)
+	{
+		chomp;
+		my $line = $_;
+		$lineCounter++;		
+
+		my @lineContents = split(/\t/, $line);
+#		my $somatic_status = $lineContents[17];
+		
+		if(($lineContents[16] && $lineContents[16] =~ 'SOMATIC') || ($lineContents[17] && $lineContents[17] =~ 'SOMATIC'))
+		{
+			print OUTFILE "$line\n";
+		}
+		else
+		{
+
+		}
+	}
+	
+	close($input);
+	
+	
+	close(OUTFILE);
+
+
+}
+
+1;
+
