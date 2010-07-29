8d5ba7adbdac0937c408402ebf0de53176eba132
Brian Derickson <bdericks@genome.wustl.edu>
2010-07-29 14:28:30 -0500

    Gene prediction model can now create an assembly model and build it, if the necessary flags are set when running the model define command.

--- lib/perl/Genome/Model/Command/Define/GenePrediction.pm
+++ lib/perl/Genome/Model/Command/Define/GenePrediction.pm
@@ -9,10 +9,20 @@ use Carp;
 class Genome::Model::Command::Define::GenePrediction {
     is => 'Genome::Model::Command::Define',
     has_optional => [
-        assemble => {
+        start_assembly_build => {
             is => 'Boolean',
-            default => 1,
+            default => 0,
             doc => 'If set, an assembly build is started if a completed build is not found on the assembly model',
+        },
+        create_assembly_model => {
+            is => 'Boolean',
+            default => 0,
+            doc => 'If set, an assembly model is created if one cannot be found with the supplied taxon',
+        },
+        assembly_processing_profile_name => {
+            is => 'Number',
+            default => 'Velvet Solexa BWA Qual 10 Filter Length 35',
+            doc => 'The processing profile used to create assembly models, if necessary',
         },
         subject_name => {
             is => 'Text',
@@ -90,18 +100,19 @@ EOS
 
 sub help_detail {
     return <<"EOS"
-There are lots of ways to define a gene prediction model. If given just a taxon ID, this
-command looks for DeNovoAssembly and ImportedAssembly models that have that taxon as 
-their subject. The most recent model is used, with DeNovoAssembly models taking precedence
-over ImportedAssembly. If no model is found, then print out the commands the user would have
-to execute to create the assembly model and assign instrument data to it.
+There are lots of ways to define a gene prediction model. A processing profile name is always
+required. Beyond that, if given just a taxon ID, this command looks for DeNovoAssembly and
+ImportedAssembly models that have that taxon as their subject. The most recent model is 
+used, with DeNovoAssembly models taking precedence over ImportedAssembly. If no model is
+found, then a model is either created for the user if the --create-assembly-model flag is
+set, otherwise the necessary commands are printed for the user.
 
 If given an assembly model ID, get that model and get its taxon. If given both an assembly
 model and a taxon ID, get the model and make sure that its taxon matches the supplied one.
 
 Once an assembly model has been found and selected, look for a successful build. If one is not
-found, kick off a build if the --assemble option is set. If that option is not set, output the
-commands the user would have to execute to get a build started.
+found, kick off a build if the --start-assembly-build option is set. If that option is not set, 
+output the commands the user would have to execute to get a build started.
 
 With a successful assembly build, all the information needed to create the new gene prediction
 model is available. Once the model is created, set up model links so all further builds of the
@@ -113,7 +124,8 @@ sub execute {
     my $self = shift;
 
     # Need to find an assembly model that has this taxon as its subject. If one can't be found, then
-    # tell the user how to make one and how to rerun this tool appropriately
+    # either make one and assign instrument data to it (if the --create-assembly-model flag is set) 
+    # or tell the user how to do it
     if (defined $self->taxon and not defined $self->assembly_model) {
         my @denovo = Genome::Model::DeNovoAssembly->get(
             subject_name => $self->taxon->name,
@@ -132,17 +144,65 @@ sub execute {
             $assembly_model = $self->get_most_recent_model(\@imported);
         }
         else {
-            $self->status_message(
-                "Could not find any assembly models with the taxon you provided. If you would like to create an " .
-                "assembly model for use with this gene prediction model, run the following command\n" .
-                "genome model define de-novo-assembly --processing-profile-name ??? --subject-name " . $self->taxon->name . "\n\n" .
-                "That command should give you a model ID. Use it to assign instrument data to the assembly model:\n" .
-                "genome instrument-data assign --model-id <MODEL_ID> --all\n\n" .
-                "Now you have an assembly model with instrument data! Rerun this define command with the " .
-                "--assemble option, which will start a build of that assembly model and kick off gene prediction " .
-                "once that build has completed!"
-            );
-            die "Could not find any assemblies for taxon " . $self->taxon_id;
+            if ($self->create_assembly_model) {
+                $self->status_message("Create assembly model flag is set, creating a new assembly model " .
+                    "using processing profile with name " . $self->assembly_processing_profile_name);
+
+                my $assembly_define_obj = Genome::Model::Command::Define::DeNovoAssembly->create(
+                    processing_profile_name => $self->assembly_processing_profile_name,
+                    subject_name => $self->taxon->name,
+                );
+                unless ($assembly_define_obj) {
+                    $self->error_message("Could not create assembly model define object!");
+                    croak;
+                }
+
+                my $define_rv = $assembly_define_obj->execute;
+                unless (defined $define_rv and $define_rv == 1) {
+                    $self->error_message("Trouble while attempting to define new assembly model!");
+                    croak;
+                }
+
+                my $model_id = $assembly_define_obj->result_model_id;
+                $assembly_model = Genome::Model::DeNovoAssembly->get($model_id);
+                unless ($assembly_model) {
+                    $self->error_message("Could not get newly created assembly model with ID $model_id!");
+                    croak;
+                }
+
+                $self->status_message("Successfully created assembly model with ID $model_id, now assigning data!");
+
+                my $assembly_assign_obj = Genome::Model::Command::InstrumentData::Assign->create(
+                    model_id => $model_id,
+                    all => 1,
+                );
+                unless ($assembly_assign_obj) {
+                    $self->error_message("Could not create instrument data assignment object!");
+                    croak;
+                }
+
+                my $assign_rv = $assembly_assign_obj->execute;
+                unless (defined $assign_rv and $assign_rv == 1) {
+                    $self->error_message("Trouble while attempting to assign instrument data to model!");
+                    croak;
+                }
+
+                $self->status_message("Instrument data has been successfully assigned to model!");
+            }
+            else {
+                $self->status_message(
+                    "Could not find any assembly models with the taxon you provided. If you would like to create an " .
+                    "assembly model for use with this gene prediction model, run the following command\n" .
+                    "genome model define de-novo-assembly --processing-profile-name " . $self->assembly_processing_profile_name . 
+                    " --subject-name " . $self->taxon->name . "\n\n" .
+                    "That command should give you a model ID. Use it to assign instrument data to the assembly model:\n" .
+                    "genome instrument-data assign --model-id <MODEL_ID> --all\n\n" .
+                    "Now you have an assembly model with instrument data! Rerun this define command with the " .
+                    "--assemble option, which will start a build of that assembly model and kick off gene prediction " .
+                    "once that build has completed!"
+                );
+                die "Could not find any assemblies for taxon " . $self->taxon_id;
+            }
         }
 
         $self->assembly_model_id($assembly_model->genome_model_id);
@@ -157,7 +217,7 @@ sub execute {
     # Alright, if we've reached this point we have an assembly model (though it may not have a successful build)
     # First, get that model's taxon and, if this command was given a taxon ID, compare. If the given taxon does not
     # match the one on the assembly model, emit an error message and exit since its not clear if the model ID
-    # or the taxon ID is in error
+    # or the taxon ID is in error.
     my $taxon = $self->assembly_model->subject;
     unless ($taxon->isa('Genome::Taxon')) {
         $self->error_message("Assembly model does not have a taxon as its subject!");
@@ -174,7 +234,7 @@ sub execute {
     }
 
     # Now check for a successful build of the assembly model. If one does not exist, kick off a build if the --assemble
-    # flag has been set, otherwise tell the user how to kick off a build and exit
+    # flag has been set, otherwise tell the user how to kick off a build and exit.
     my $build = $self->assembly_model->last_succeeded_build;
     $build = $self->assembly_model->current_running_build unless $build;
     unless ($build) {
@@ -207,6 +267,7 @@ sub execute {
         }
     }
 
+    # Now create the gene prediction model!
     my $rv = $self->SUPER::_execute_body(@_);
     unless ($rv) {
         $self->error_message("Could not create new gene prediction model!");
@@ -214,8 +275,13 @@ sub execute {
     }
 
     my $model = Genome::Model->get($self->result_model_id);
+    unless ($model) {
+        $self->error_message("Could not get newly created gene prediction model with ID " . $self->result_model_id);
+        croak;
+    }
 
-    # Make a link from the assembly model to this model and vice versa
+    # Make a link from the assembly model to this model and vice versa. This link will be used to kick off a new build
+    # of the gene prediction model every time its linked assembly model completes a build.
     my $to_rv = $self->assembly_model->add_to_model(
         to_model => $model,
         role => 'gene_prediction_model',
