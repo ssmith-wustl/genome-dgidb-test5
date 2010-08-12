703075c9b3801fef578a827cd8ea7ca4a158d2ee
John Martin <jmartin@genome.wustl.edu>
2010-08-12 12:44:08 -0500

    code to download the SRAindex in place and working.

--- lib/perl/Genome/InstrumentData/Command/Import/HmpSra.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/HmpSra.pm
@@ -3,6 +3,7 @@ package Genome::InstrumentData::Command::Import::HmpSra;
 use strict;
 use warnings;
 use Genome;
+use Cwd;
 
 class Genome::InstrumentData::Command::Import::HmpSra {
     is  => 'Command',
@@ -22,6 +23,21 @@ sub execute {
     my $scripts_dir = __FILE__;
     $scripts_dir =~ s/.pm//;
     $self->status_message("Scripts are in: $scripts_dir");    
+
+    my $outfile;
+    my $errfile;
+    my $cmd;
+
+    $outfile = $tmp . '/SRA-index.txt';
+    $errfile = $outfile . '.err';
+    $cmd = "cd $tmp; $scripts_dir/build_public_SRA_run_index.pl --reuse_files "
+        . ' > ' . $outfile 
+        . ' 2> ' . $errfile;        
+    Genome::Utility::FileSystem->shellcmd(
+        cmd => $cmd,
+        output_files => [$outfile,$errfile],
+        skip_if_output_is_present => 1,
+    );
 
     return 1;
 }
