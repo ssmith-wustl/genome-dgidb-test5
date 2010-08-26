5232b17f158399106c740ebd97e7a49809ca09d0
Kim Kyung <kkyung@linus86.gsc.wustl.edu>
2010-08-25 23:56:34 -0500

    for soap assembler

---
+++ lib/perl/Genome/Model/Build/DeNovoAssembly/Soap.pm
@@ -0,0 +1,12 @@
+package Genome::Model::Build::DeNovoAssembly::Soap;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Build::DeNovoAssembly::Soap {
+    is => 'Genome::Model::Build::DeNovoAssembly',
+};
+
+1;
