516b885a23ff20012708c822f0c47542e6da2125
Ben Oberkfell <boberkfe@genome.wustl.edu>
2010-09-02 13:13:59 -0500

    if we need this much RAM, we may as well use 8 cores worth of CPU too

--- lib/perl/Genome/InstrumentData/AlignmentResult/RtgMapX.pm
+++ lib/perl/Genome/InstrumentData/AlignmentResult/RtgMapX.pm
@@ -22,7 +22,7 @@ class Genome::InstrumentData::AlignmentResult::RtgMapX{
 sub required_arch_os { 'x86_64' }
 
 sub required_rusage { 
-    "-R 'select[model!=Opteron250 && type==LINUX64 && tmp>90000 && mem>25000] span[hosts=1] rusage[tmp=90000, mem=25000]' -M 25000000 -n 4";
+    "-R 'select[model!=Opteron250 && type==LINUX64 && tmp>90000 && mem>25000] span[hosts=1] rusage[tmp=90000, mem=25000]' -M 25000000 -n 8";
 }
 
 sub _decomposed_aligner_params {
