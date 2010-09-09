cb81fa8f48e7eb853d464f9a44be537aaa4454ef
Dave Larson <dlarson@genome.wustl.edu>
2010-09-09 17:02:25 -0500

    Added log likelihood calling

--- lib/perl/Genome/Model/Tools/Analysis/Indels/CompileSomatic.pm
+++ lib/perl/Genome/Model/Tools/Analysis/Indels/CompileSomatic.pm
@@ -94,36 +94,84 @@ sub execute {
         my $entry = $indels{$indel_name};
         my ($ref, $var) = @fields[-3,-2];
 
-		my $p_value = VarScan::FisherTest::calculate_p_value($entry->{normal_ref}, $entry->{normal_var}, $ref, $var, 0);
+        my $p_value = VarScan::FisherTest::calculate_p_value($entry->{normal_ref}, $entry->{normal_var}, $ref, $var, 0);
         
         my $llr = 0;
+        my $max_llr;
+        my $max_call;
+        my $max2_llr;
+        my $max2_call;
+        my $marginal_probability = -1_000_000;
 
         if(($entry->{normal_ref}+$entry->{normal_var}) > 0 && ($ref+$var) > 0) {
             #calculate LLR
-            my $somatic_normal_expect = $entry->{normal_var}/($entry->{normal_ref}+$entry->{normal_var});
-            my $not_somatic_normal_expect = 1 - $somatic_normal_expect;
-            my $somatic_tumor_expect = $var/($ref+$var);
-            my $not_somatic_tumor_expect = 1 - $somatic_tumor_expect;
-            my $reference_expect = ($var+$entry->{normal_var})/($entry->{normal_ref}+$entry->{normal_var}+$ref+$var);
-            my $not_reference_expect = 1- $reference_expect;
-            $somatic_normal_expect ||= 0.001;
-            $not_somatic_normal_expect ||= 0.001;
-            $somatic_tumor_expect ||= 0.001;
-            $not_somatic_tumor_expect ||= 0.001;
-            $reference_expect ||= 0.001;
-            $not_reference_expect ||= 0.001;
+            #previous attempt was a miserable failure due to degrees of freedom
+            #use binomial model where variant supporting read is success!
+            #take error rate as 1/1000 just for kicks and so we can actually calculate the results. Other numbers may be more appropriate
+            #Want to test several models and pick the most likely one
+            #Reference: normal expectation is 0.001 and tumor expectation is 0.01
+            #Germline Het: normal expectation is 0.5 and tumor expectation is 0.5
+            #Somatic Het: normal expectation is 0.001 and tumor expectation in 0.5
+            #Germline Homozygote: normal expectation is 0.999 and tumor expectation is 0.999
+            #LOH (variant): germline is 0.5 and tumor is 0.999
+            #LOH (ref): germline is 0.5 and tumor is 0.001
+            my $error_rate = 0.001;
+            my $homozygous_expect = 1 - $error_rate;
+            my $heterozygous_expect = 0.5;
 
-            my $somatic_LLR = $entry->{normal_var}*log($somatic_normal_expect) + $entry->{normal_ref}*log($not_somatic_normal_expect) + $var*log($somatic_tumor_expect) + $ref*log($not_somatic_tumor_expect);
-            my $nonsomatic_LLR = $entry->{normal_var}*log($reference_expect) + $entry->{normal_ref}*log($not_reference_expect) + $var*log($reference_expect) + $ref*log($not_reference_expect);
+            my %calls = (   Reference       => [$error_rate, $error_rate],
+                            Germline_het    => [$heterozygous_expect, $heterozygous_expect],
+                            Germline_hom    => [$homozygous_expect, $homozygous_expect],
+                            Somatic_het     => [$error_rate, $heterozygous_expect],
+                            Somatic_hom     => [$error_rate, $homozygous_expect],
+                            LOH_variant     => [$heterozygous_expect, $homozygous_expect],
+                            LOH_ref         => [$heterozygous_expect, $error_rate],
+                        );
 
-            $llr = $somatic_LLR-$nonsomatic_LLR;
+            my $llr_calculator = $self->generate_llr_calculator($entry->{normal_ref},$entry->{normal_var},$ref,$var);
+
+            for my $call (keys %calls) {
+                my $llr = $llr_calculator->(@{$calls{$call}});
+                #add to marginal
+                if(abs($marginal_probability) >= abs($llr)) {
+                    $marginal_probability = $marginal_probability + log(1 + $llr/$marginal_probability); 
+                }
+                else {
+                    $marginal_probability = $llr + log(1 + $marginal_probability/$llr);
+                }
+
+                unless(defined $max_llr && $llr < $max_llr) {
+                    $max2_call = $max_call;
+                    $max2_llr = $max_llr;
+                    $max_llr = $llr;
+                    $max_call = $call;
+                    
+                }
+                else {
+                    unless(defined $max2_llr && $llr < $max2_llr) {
+                        $max2_call = $call;
+                        $max2_llr = $llr;
+                    }
+                }
+            }
+
         }
-        print STDOUT $entry->{line},"\t",join("\t",@fields[-4,-3,-2,-1],$p_value,$llr),"\n";
+        print STDOUT $entry->{line},"\t",join("\t",@fields[-4,-3,-2,-1],$p_value,defined $max_llr ? $max_llr/$marginal_probability : '-',defined $max_call ? $max_call : '-',defined $max_llr && defined $max2_llr ? $max_llr-$max2_llr : '-',defined $max2_llr ? $max2_llr/$marginal_probability : '-',defined $max2_call ? $max2_call : '-'),"\n";
         delete $indels{$indel_name};
     }
 
     return 1;
 
+}
+
+sub generate_llr_calculator {
+    my ($self,$normal_ref_reads,$normal_var_reads,$tumor_ref_reads,$tumor_var_reads) = @_;
+    return sub {
+        my ($normal_expect, $tumor_expect) = @_;
+        my $not_normal_expect = 1 - $normal_expect;
+        my $not_tumor_expect = 1 - $tumor_expect;
+        return ($normal_ref_reads * log($not_normal_expect) + $normal_var_reads * log($normal_expect) + $tumor_ref_reads*log($not_tumor_expect) + $tumor_var_reads*log($tumor_expect));
+    }
 }
 
 1;
