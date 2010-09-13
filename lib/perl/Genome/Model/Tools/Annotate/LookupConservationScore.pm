b6fb9ef8458545049c7e29a8354c39df73a78ee3
Anthony Brummett <abrummet@watson.wustl.edu>
2010-09-13 13:27:46 -0500

    Refactored to allow the most important part to be called as a regular function, but still can be called as a Command object/class

--- lib/perl/Genome/Model/Tools/Annotate/LookupConservationScore.pm
+++ lib/perl/Genome/Model/Tools/Annotate/LookupConservationScore.pm
@@ -55,55 +55,78 @@ my \$results_array_ref = \$lookup->conservation_scores_results;
 EOS
 }
 
-sub execute{
+# For speed.  slightly unsafe, but there shouldn't ever be any problems, right?
+sub __errors__() {
+    return;
+}
+
+sub execute {
 
     my $self = shift; 
-    my ($chromosome, $coordinates_ref);
-    $chromosome = $self->chromosome;
 
-    if(ref($self->coordinates) eq 'ARRAY'){
-        $coordinates_ref = $self->coordinates;
-    }else{
-        $self->error_message("Malformed coordinates.  Coordinates should be specified as an array ref");
+    my $results = 
+        eval { lookup_conservation_score(
+                   chromosome => $self->chromosome,
+                   coordinates => $self->coordinates,
+                   species => $self->species,
+                   version => $self->version,
+                   reference_transcripts => $self->reference_transcripts)
+        };
+    if ($@) {
+        $self->error_message($@);
+        return 0;
+    } else {
+        $self->conservation_scores_results($results);
+        return 1;
+    }
+}
+
+
+sub lookup_conservation_score {
+    my %args = @_;
+    my($chromosome, $coordinates, $species, $version, $reference_transcripts) = @args{'chromosome','coordinates','species','version','reference_transcripts'};
+
+    unless(ref($coordinates) eq 'ARRAY'){
+        Carp::croak("Malformed coordinates.  Coordinates should be specified as an array ref");
         return 0;
     }
     
-    my ($model_name, $version, $build_version);
-    if($self->reference_transcripts and $self->species and $self->version){
-        $self->error_message("species and version OR reference_transcripts must be specified");
+    my ($model_name, $build_version);
+    if($reference_transcripts and $species and $version){
+        Carp::croak("species and version OR reference_transcripts must be specified");
         return 0;
-    }elsif($self->reference_transcripts){
-        ($model_name, $build_version) = split("/", $self->reference_transcripts); 
+    } elsif($reference_transcripts){
+        ($model_name, $build_version) = split("/", $reference_transcripts); 
         $build_version =~ m/[\d]+_([\d]+)[a-zA-Z]/ or die "Malformed reference_transcripts: $model_name/$build_version";
         $version = $1;
-    }elsif($self->species and $self->version){
-        $model_name = "NCBI-" . $self->species . ".combined-annotation";
-        if($self->version =~ m/[\d]+_([\d]+)[a-zA-Z]/){
+    } elsif($species and $version){
+        $model_name = "NCBI-" . $species . ".combined-annotation";
+        if($version =~ m/[\d]+_([\d]+)[a-zA-Z]/){
             $version = $1; 
-            $build_version = $self->version;
+            $build_version = $version;
         }else{
-            die "Malformed version: " . $self->version;
+            Carp::croak("Malformed version: " . $version);
         }
 
     }else{
-        $self->error_message("species and version OR reference_transcripts must be specified");
+        Carp::croak("species and version OR reference_transcripts must be specified");
         return 0;
     }
 
     #TODO: Remove these restrictions as soon as possible
     unless($model_name eq 'NCBI-human.combined-annotation'){
-        $self->error_message("Cannot look up conservation scores for model: $model_name.  This module currently supports the following models: NCBI-human.combined-annotation"); 
+        Carp::croak("Cannot look up conservation scores for model: $model_name.  This module currently supports the following models: NCBI-human.combined-annotation"); 
         return 0;
     }
     unless($version == 36 or $version == 37){
-        $self->error_message("Cannot look up conservation scores for model $model_name version $version.  This module currently supports the following versions for $model_name: 36, 37"); 
+        Carp::croak("Cannot look up conservation scores for model $model_name version $version.  This module currently supports the following versions for $model_name: 36, 37"); 
         return 0;
     }
 
-    my $model = Genome::Model->get(name => $model_name); 
-    die "Could not get model $model_name: $!" unless $model;
-    my $build = $model->build_by_version($build_version);
-    die "Could not get $model_name build for version $build_version: $!" unless $build;
+    #my $model = Genome::Model->get(name => $model_name); 
+    #die "Could not get model $model_name: $!" unless $model;
+    #my $build = $model->build_by_version($build_version);
+    #die "Could not get $model_name build for version $build_version: $!" unless $build;
 
     #TODO: This will need to change to support conservation dirs that have
     #been sym linked to the build dirs
@@ -112,21 +135,20 @@ sub execute{
 
     #TODO: we're copying directly from MG/ConsScore.pm from this point on.  It probably sucks and
     #needs to be refactored
-    my @results;
-    my $file = $location . "/chr".$chromosome."-rec";
     # open file, 
-    my $fh = new IO::File;
-    if(!exists($self->{_file_handles}->{$chromosome} ) )
-    {
-        $self->{_file_handles}->{$chromosome} = $fh;
-        $fh->open($file) or die "can't open $file : $!";
+    our %file_handles;
+    my $file = $location . "/chr".$chromosome."-rec";
+    unless (exists($file_handles{$file} ) ) {
+        $file_handles{$file} = IO::File->new($file, 'r');
+        unless ($file_handles{$file}) {
+            die "can't open $file : $!";
+        }
     }
-    else
-    {
-        $fh = $self->{_file_handles}->{$chromosome};
-    }
-    @$coordinates_ref = sort { $a <=> $b } @$coordinates_ref;
-    foreach my $pos (@{$coordinates_ref})
+    my $fh = $file_handles{$file};
+
+    my @results;
+    @$coordinates = sort { $a <=> $b } @$coordinates;
+    foreach my $pos (@{$coordinates})
     {
         # start seeking until the first position
         $fh->seek(($pos -1)*2,0);
@@ -137,9 +159,8 @@ sub execute{
         push(@results, [ $pos, $score ]);
     }
     # return array ref of array refs of positions, scores?
-    $fh->close;
-    $self->conservation_scores_results(\@results);
-    return  1;
+    #$fh->close;
+    return \@results;
 }
 
 1;
