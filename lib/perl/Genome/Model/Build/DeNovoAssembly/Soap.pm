4398c0c7923e202687ecd7c53a2bfc881dc99860
Eddie Belter <ebelter@genome.wuslt.edu>
2010-09-17 11:45:14 -0500

    De Novo
    Updated to PGA consortium reqs. Made getting example files easier. Get cpus from LSF for running soap.

--- lib/perl/Genome/Model/Build/DeNovoAssembly/Soap.pm
+++ lib/perl/Genome/Model/Build/DeNovoAssembly/Soap.pm
@@ -10,33 +10,36 @@ class Genome::Model::Build::DeNovoAssembly::Soap {
 };
 
 #< Files >#
+sub soap_output_dir_and_file_prefix {
+    return $_[0]->data_directory.'/'.$_[0]->file_prefix;
+}
+
+sub file_prefix {
+    return $_[0]->model->subject_name.'_'.$_[0]->center_name;
+}
+
 sub assembler_input_files {
     return map { $_[0]->$_ } (qw/ end_one_fastq_file end_two_fastq_file /);
 }
 
-#sub file_prefix {
-#    return $_[0]->instrument_data->sample_name.'_'.$_[0]->center_name;
-#}
-
 sub end_one_fastq_file {
-    #return $_[0]->data_directory.'/'.$_[0]->file_prefix.'.1_fastq';
-    return $_[0]->data_directory.'/'.$_[0]->instrument_data->sample_name.'_'.$_[0]->center_name.'.1_fastq';
+    return $_[0]->data_directory.'/'.$_[0]->file_prefix.'.input_1.fastq';
 }
 
 sub end_two_fastq_file {
-    return $_[0]->data_directory.'/'.$_[0]->instrument_data->sample_name.'_'.$_[0]->center_name.'.2_fastq';
+    return $_[0]->data_directory.'/'.$_[0]->file_prefix.'.input_2.fastq';
 }
 
 sub soap_config_file {
     return $_[0]->data_directory.'/config_file';
 }
 
-sub soap_output_dir_and_file_prefix {
-    return $_[0]->data_directory.'/'.$_[0]->instrument_data->sample_name.'_'.$_[0]->center_name;
+sub soap_scaffold_sequence_file {
+    return $_[0]->data_directory.'/'.$_[0]->file_prefix.'.scafSeq';
 }
 
-sub soap_scaffold_sequence_file {
-    return $_[0]->data_directory.'/'.$_[0]->instrument_data->sample_name.'_'.$_[0]->center_name.'.scafSeq';
+sub soap_output_file_for_ext {
+    return $_[0]->soap_output_dir_and_file_prefix.'.'.$_[1];
 }
 
 1;
