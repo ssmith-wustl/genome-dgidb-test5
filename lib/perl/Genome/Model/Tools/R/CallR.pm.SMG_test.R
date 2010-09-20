ad97af34315ac1800182fd3f4710b6395c4755a2
Cyriac Kandoth <ckandoth@wustl.edu>
2010-09-20 16:57:34 -0500

    General cleanup and reorganizing of files in gmt bmr Also cleaned up a bit of R code called by SmgTest.pm

--- lib/perl/Genome/Model/Tools/R/CallR.pm.SMG_test.R
+++ lib/perl/Genome/Model/Tools/R/CallR.pm.SMG_test.R
@@ -1,33 +1,35 @@
-#######SMG Test Library###############################
+####### SMG Test Library #############################
 
-smg_test=function(in.file,test.file,fdr.file)
+smg_test=function(in.file,test.file)
 {
-source("/gscuser/qzhang/gstat/stat.lib");
+  source("/gscuser/qzhang/gstat/mut_class_test/lib.R");
 
-#in.file="/gscuser/qzhang/gstat/example_data/wu_ov94_capture_bmr.lis_genes.csv"
-#out.file=paste(in.file,'.smgtest.csv',sep='');
+  read.table(in.file,header=T,sep="\t")->mut
+  mut$BMR=as.numeric(as.character(mut$BMR))
 
-read.table(in.file,header=T,sep="\t")->mut
-mut$BMR=as.numeric(as.character(mut$BMR))
-#select the rows with BMR data
-mut=mut[mut$BMR>0 & !is.na(mut$BMR) & mut$Bases>0,]
-tt=NULL
-for (gene in unique(as.character(mut$Gene)))
+  #select the rows with BMR data
+  mut=mut[mut$BMR>0 & !is.na(mut$BMR) & mut$Bases>0,]
+  tt=NULL
+  for (gene in unique(as.character(mut$Gene)))
+  {
+    mutgi=mut[mut$Gene==gene,]
+    mut_class_test(mutgi[,3:5],hmax=25,bin=0.001)->z
+    tt=rbind(tt,cbind(mutgi,z$x[,-(1:3)]))
+  }
+  write.table(tt,file=test.file,quote=FALSE,row.names=F,sep="\t")
+}
+
+smg_fdr=function(in.file,fdr.file)
 {
-mutgi=mut[mut$Gene==gene,]
-mut_class_test(mutgi[,3:5])->z
-tt=rbind(tt,cbind(mutgi,z$x[,-(1:3)]))
-}
-write.table(tt,file=test.file,quote=FALSE,row.names=F,sep="\t")
+  read.table(in.file,header=T,sep="\t")->x
+  x=unique(x)
 
-#Calculate FDR measure and write FDR output
-x=tt[,c(1,11:13)]
-x=unique(x)
-p.adjust(x[,2],method="BH")->fdr.fisher
-p.adjust(x[,3],method="BH")->fdr.lr
-p.adjust(x[,4],method="BH")->fdr.convol
-x=cbind(x,fdr.fisher,fdr.lr,fdr.convol)
-write.table(x,file=fdr.file,sep="\t",quote=FALSE,row.names=F)
-
+  #Calculate FDR measure and write FDR output
+  p.adjust(x[,2],method="BH")->fdr.fisher
+  p.adjust(x[,3],method="BH")->fdr.lr
+  p.adjust(x[,4],method="BH")->fdr.convol
+  x=cbind(x,fdr.fisher,fdr.lr,fdr.convol)
+  write.table(x,file=fdr.file,sep="\t",quote=FALSE,row.names=F)
 }
+
 ######################################################
