540285db1c70ce32d7d5f65bc336a276faf7b348
Thomas Mooney <tmooney@genome.wustl.edu>
2010-10-12 12:19:44 -0500

    Remove redundant build subs; make date_scheduled and date_completed be properties.
    Now can get() builds based on their date_scheduled and date_completed.  build_status
    has been removed as redundant to the status property.

--- lib/perl/Genome/Model/Command/Services/Build/Run.pm
+++ lib/perl/Genome/Model/Command/Services/Build/Run.pm
@@ -289,7 +289,7 @@ sub _eviscerate_old_builds_for_this_model {
     # The calling build will not have been marked as succeeded yet, so allow an "or" here to catch our own build
     my @builds = sort {$a->build_id <=> $b->build_id}
     grep {$_->can('eviscerate')}
-    grep {$_->build_status eq "Succeeded" || $_->id == $self->build->id} 
+    grep {$_->status eq "Succeeded" || $_->id == $self->build->id}
     $model->builds;
 
     my @builds_to_eviscerate = splice @builds, 0, scalar @builds - $recent_keep_count;
