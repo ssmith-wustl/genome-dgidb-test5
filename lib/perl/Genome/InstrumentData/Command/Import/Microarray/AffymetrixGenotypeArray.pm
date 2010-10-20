76e0db1c81efefb32b06c0e1e3bfdd0f4c4273e0
Thomas Mooney <tmooney@genome.wustl.edu>
2010-10-20 13:31:49 -0500

    Include newline at end of print statement.
    This should un-confuse the test runner.

--- lib/perl/Genome/InstrumentData/Command/Import/Microarray/AffymetrixGenotypeArray.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/Microarray/AffymetrixGenotypeArray.pm
@@ -113,7 +113,7 @@ sub process_imported_files {
         print $self->status_message."\n";
     }
     $self->status_message("finished creating genotype file, importing genotype and defining model.");
-    print $self->status_message;
+    print $self->status_message."\n";
 
     unless(Genome::InstrumentData::Command::Import::Genotype->create(
         source_data_file => $genotype_path_and_file,
