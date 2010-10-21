30f07b03e684db38cada6d718095a6867902b353
Nathan Nutter <nnutter@genome.wustl.edu>
2010-10-21 15:48:28 -0500

    genome model build create was not correctly undone during a rollback, added removal of build directory and allocation to an observer

--- lib/perl/Genome/Model/Build.pm
+++ lib/perl/Genome/Model/Build.pm
@@ -238,6 +238,37 @@ sub create {
         return;
     }
 
+    my $context = UR::Context->current();
+    $context->add_observer(
+        aspect => 'rollback',
+        callback => sub {
+            if ($self->data_directory && -e $self->data_directory) {
+                unless (rmtree($self->data_directory, { error => \my $remove_errors })) {
+                    if (@$remove_errors) {
+                        my $error_summary;
+                        for my $error (@$remove_errors) {
+                            my ($file, $error_message) = %$error;
+                            if ($file eq '') {
+                                $error_summary .= "General error removing build directory: $error_message\n";
+                            }
+                            else {
+                                $error_summary .= "Error removing file $file : $error_message\n";
+                            }
+                        }
+                        $self->error_message($error_summary);
+                    }
+
+                    confess "Failed to remove build directory tree at " . $self->data_directory . ", cannot remove build!";
+                }
+            }
+            my $disk_allocation = $self->disk_allocation;
+            if ($disk_allocation) {
+                unless ($disk_allocation->deallocate) {
+                    $self->warning_message('Failed to deallocate disk space.');
+                }
+            }   
+        },
+    );
     return $self;
 }
 
