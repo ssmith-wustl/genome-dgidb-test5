f5cd9191eb5a7129aa83179b148527a48cf58eaf
Nathan Nutter <nnutter@genome.wustl.edu>
2010-11-02 14:26:14 -0500

    update-tab-completion now updated both 'genome' and 'gmt'

--- lib/perl/Genome/Model/Tools/UpdateTabCompletion.pm
+++ lib/perl/Genome/Model/Tools/UpdateTabCompletion.pm
@@ -11,20 +11,14 @@ class Genome::Model::Tools::UpdateTabCompletion {
 sub execute {
     my $self = shift;
 
-    my $classname;
-    $classname = 'Genome::Model::Tools' if ($self->command_name =~ /^gmt/);
-    $classname = 'Genome::Command' if ($self->command_name =~ /^genome/);
-
-    unless ($classname) {
-        $self->error_message("Unable to determine class to generate .opts file for.");
-        return;
-    }
-
-    my $genome_completion = UR::Namespace::Command::CreateCompletionSpecFile->create(
-        classname => $classname,
-    );
-    unless ($genome_completion->execute) {
-        $self->error_message("Updating the genome command spec file did not complete succesfully!");
+    my @command_classes = ('Genome::Model::Tools', 'Genome::Command');
+    for my $classname (@command_classes) {
+        my $genome_completion = UR::Namespace::Command::CreateCompletionSpecFile->create(
+            classname => $classname,
+        );
+        unless ($genome_completion->execute) {
+            $self->error_message("Updating the $classname spec file did not complete succesfully!");
+        }
     }
 
     $self->status_message("Remember to commit the updated .opts file(s)!");
