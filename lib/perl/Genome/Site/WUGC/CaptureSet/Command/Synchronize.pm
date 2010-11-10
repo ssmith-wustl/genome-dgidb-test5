6f0d9652fe2e14b755d6b6444025a2ba53867efb
Thomas Mooney <tmooney@genome.wustl.edu>
2010-11-10 14:50:06 -0600

    Refactor to move property determination to the CaptureSet object itself.
    The synchronize tool now just expects the parameters for the create command
    to all be present as properties on the source object.

--- lib/perl/Genome/Site/WUGC/CaptureSet/Command/Synchronize.pm
+++ lib/perl/Genome/Site/WUGC/CaptureSet/Command/Synchronize.pm
@@ -127,42 +127,21 @@ sub create_feature_list_for_capture_set {
     my $self = shift;
     my $capture_set = shift;
 
-    my $found = 0;
-
-    my $lims_cs = $capture_set->_capture_set;
-
-    unless($lims_cs->file_storage_id) {
-        my $temp_bed_file = Genome::Utility::FileSystem->create_temp_file_path;
-        my $ok = $self->load_bed_file($capture_set, $temp_bed_file);
-
-        if($ok) {
-            #is true bed, since that's what this script outputs
-            my $fl = $self->create_feature_list(
-                name => $capture_set->name,
-                file_path => $temp_bed_file,
-                format => 'true-BED',
-            );
-            $found = $fl;
-        } else  {
-            my $name = $capture_set->name;
-            $self->error_message("Capture set $name has neither associated file storage nor support from capture_file_dumper.");
-            return;
-        }
+    unless($capture_set->file_storage_id) {
+        $self->warning_message('capture set ' . $capture_set->id . ' has no file_storage_id and thus cannot be synchronized.');
+        return;
     }
 
-    $found ||= $self->guess_agilent($capture_set);
-    $found ||= $self->guess_nimblegen($capture_set);
-    $found ||= $self->guess_true_bed($capture_set);
+    my @params;
 
-    #Can't automatically deduce format--will require manual intervention before pipeline use!
-    $found ||= $self->create_unknown($capture_set);
+    my $meta = Genome::FeatureList::Command::Create->__meta__;
+    my @property_meta = $meta->all_property_metas;
 
-    return $found;
-}
-
-sub create_feature_list {
-    my $self = shift;
-    my @params = @_;
+    my @param_names = map { $_->property_name } grep { defined $_->{'is_input'} && $_->{'is_input'} } @property_meta;
+    for my $param_name (@param_names) {
+        push @params,
+            $param_name => $capture_set->$param_name;
+    }
 
     my $fl_command = Genome::FeatureList::Command::Create->create(@params);
     my $fl = $fl_command->execute;
@@ -173,164 +152,6 @@ sub create_feature_list {
     }
 
     return $fl;
-}
-
-
-sub guess_nimblegen {
-    my $self = shift;
-    my $capture_set = shift;
-
-    my $bed_file_content = $capture_set->_capture_set->get_file_storage->content;
-
-    unless($bed_file_content) {
-        $self->error_message('Could not find BED file for capture set ' . $capture_set->name . '.');
-        die($self->error_message);
-    }
-
-    my $temp_bed_file = Genome::Utility::FileSystem->create_temp_file_path;
-    Genome::Utility::FileSystem->write_file($temp_bed_file, $bed_file_content);
-
-    my %params = (
-        name => $capture_set->name,
-        source => 'nimblegen',
-        file_path => $temp_bed_file
-    );
-
-    if($bed_file_content =~ /track name=/ims) {
-        my $fl = $self->create_feature_list(
-            %params,
-            format => 'multi-tracked 1-based',
-        );
-        return $fl;
-    }
-
-    if($capture_set->name =~ /nimblegen/i) {
-        my $fl = $self->create_feature_list(
-            %params,
-            format => '1-based',
-        );
-        return $fl;
-    }
-
-    return;
-}
-
-sub guess_agilent {
-    my $self = shift;
-    my $capture_set = shift;
-
-    return unless $capture_set->name =~ /agilent/i;
-
-    my $bed_file_content = $capture_set->_capture_set->get_file_storage->content;
-
-    unless($bed_file_content) {
-        $self->error_message('Could not find BED file for capture set ' . $capture_set->name . '.');
-        die($self->error_message);
-    }
-
-    my $temp_bed_file = Genome::Utility::FileSystem->create_temp_file_path;
-    Genome::Utility::FileSystem->write_file($temp_bed_file, $bed_file_content);
-
-    my %params = (
-        name => $capture_set->name,
-        source => 'agilent',
-        file_path => $temp_bed_file,
-        format => '1-based',
-    );
-
-    my $fl = $self->create_feature_list(
-        %params,
-    );
-
-    return $fl;
-}
-
-sub guess_true_bed {
-    my $self = shift;
-    my $capture_set = shift;
-
-    my $bed_file_content = $capture_set->_capture_set->get_file_storage->content;
-
-    unless($bed_file_content) {
-        $self->error_message('Could not find BED file for capture set ' . $capture_set->name . '.');
-        die($self->error_message);
-    }
-
-    my $temp_bed_file = Genome::Utility::FileSystem->create_temp_file_path;
-    Genome::Utility::FileSystem->write_file($temp_bed_file, $bed_file_content);
-
-    my %params = (
-        name => $capture_set->name,
-        format => 'true-BED',
-        file_path => $temp_bed_file,
-    );
-
-    my @lines = split("\n",$bed_file_content);
-    for my $line (@lines) {
-        my @fields = split("\t", $line);
-        if($fields[1] == 0) {
-            my $fl = $self->create_feature_list(
-                %params,
-            );
-            return $fl;
-        }
-    }
-
-    return;
-}
-
-sub create_unknown {
-    my $self = shift;
-    my $capture_set = shift;
-
-    my $bed_file_content = $capture_set->_capture_set->get_file_storage->content;
-
-    unless($bed_file_content) {
-        $self->error_message('Could not find BED file for capture set ' . $capture_set->name . '.');
-        die($self->error_message);
-    }
-
-    my $temp_bed_file = Genome::Utility::FileSystem->create_temp_file_path;
-    Genome::Utility::FileSystem->write_file($temp_bed_file, $bed_file_content);
-
-    my %params = (
-        name => $capture_set->name,
-        format => 'unknown',
-        file_path => $temp_bed_file,
-    );
-
-    my $fl = $self->create_feature_list(
-        %params,
-    );
-    return $fl;
-}
-
-sub load_bed_file {
-    my $self = shift;
-    my $capture_set = shift;
-    my $temp_file_path = shift;
-
-    my @barcodes = $capture_set->barcodes;
-    unless(scalar @barcodes) {
-        warn "no barcodes";
-        return;
-    }
-
-    my $cmd = '/gsc/scripts/bin/capture_file_dumper --barcode='. $barcodes[0] .' --output-type=region-bed --output-file='. $temp_file_path;
-
-    eval {
-        Genome::Utility::FileSystem->shellcmd(
-            cmd => $cmd,
-            output_files => [$temp_file_path],
-        );
-    };
-
-    if($@) {
-        $self->error_message('Could not dump BED file: ' . $@);
-        return;
-    }
-
-    return 1;
 }
 
 #Display a list of all those feature-lists that cannot be used in pipelines because they are missing important values.
