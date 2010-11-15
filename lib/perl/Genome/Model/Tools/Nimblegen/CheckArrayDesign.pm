00923573cb1ea1a36b92fae2c31e6e6c1341c2ad
Charles Lu <clu@genome.wustl.edu>
2010-11-15 13:59:12 -0600

    added few options

--- lib/perl/Genome/Model/Tools/Nimblegen/CheckArrayDesign.pm
+++ lib/perl/Genome/Model/Tools/Nimblegen/CheckArrayDesign.pm
@@ -16,13 +16,18 @@ class Genome::Model::Tools::Nimblegen::CheckArrayDesign {
     },
     design_files => {
         type => 'Comma-delimited String',
-        is_optional => 0,
+        is_optional => 1,
         doc => 'A comma-delimited list of files that went into the array design',
+    },
+    design_file_list => {
+	type => 'String',
+	is_optional => 1,
+        doc => 'Absolute path to a file containing list of all the files (1 per line) that went into the array design. If specified, will override design-files option',
     },
     design_summary_outfile => {
         type => 'String',
-        is_optional => 0,
-        doc => 'An output summary file giving the number of input sites that came from each array design input file as compared to the number of these respective sites covered in the array design.',
+        is_optional => 1,
+        doc => 'An output summary file giving the number of input sites that came from each array design input file as compared to the number of these respective sites covered in the array design. If this option is not defined, STDOUT will be used.',
     },
     ]
 };
@@ -33,8 +38,16 @@ sub execute {
 
     #parse inputs
     my $probe_bed = $self->nimblegen_probe_bedfile;
-    my @design_files = split /,/,$self->design_files;
-    my $summary_file = $self->design_summary_outfile;
+    my $summary_file = $self->design_summary_outfile;    
+    
+    my @design_files;
+    if($self->design_file_list) {
+	@design_files = process_design_file_list($self->design_file_list);
+	my $x = 1;
+    }else {
+	@design_files = split(/,/,$self->design_files);
+    }
+    
 
     #put the tiled regions from the probe set into a hash
     my %probes;
@@ -57,7 +70,22 @@ sub execute {
     $probe_fh->close;
 
     #set up summary filehandle and header
-    my $sum_fh = new IO::File $summary_file,"w";
+    my $sum_fh;
+    if(defined $summary_file) {
+        $sum_fh = IO::File->new($summary_file,"w");
+        unless($sum_fh) {
+            $self->error_message("Unable to open file " . $summary_file . " for writing.");
+            return;
+        }
+    }
+    else {
+        $sum_fh = IO::File->new_from_fd(fileno(STDOUT),"w");
+        unless($sum_fh) {
+            $self->error_message("Unable to open STDOUT for writing.");
+            return;
+        }
+    }
+    #my $sum_fh = new IO::File $summary_file,"w";
     print $sum_fh "Design_File\t#_Sites\t#_Tiled_Sites\t%_Tiled_Sites\n";
 
 
@@ -77,6 +105,9 @@ sub execute {
             $sites++;
             chomp $line;
             my ($chr,$start,$stop) = split /\t/,$line;
+	    if($chr !~ /^chr/){ #in case the chromosome number is formatted differently
+		$chr = "chr".$chr;
+	    }
 
             #find out if site was tiled
             my $site_is_tiled = 0;
@@ -103,6 +134,32 @@ sub execute {
         print $sum_fh "$file\t$sites\t$covered_sites\t$percent\n";
     }
     return 1;
+}
+
+
+sub process_design_file_list {
+#return a list of files from a file to process;
+#returns reference to array of files
+
+    my $file = shift;
+    my $fh = IO::File->new($file,"r");
+    my @list=();
+    while(my $line = $fh->getline) {
+	chomp $line;
+	next if($line =~ /^\s+$/ || $line =~ /^\#/); #ignore comment and blank lines
+	push(@list,$line);
+    }
+    $fh->close;
+    
+    #check to see if files exists and readable
+    my @bad_files = grep {!-e $_ } @list;
+    my @good_files = grep {-e $_ } @list;
+    print STDERR "$_ NOT FOUND\n" for(@bad_files);
+
+    return (@good_files);
+    
+
+
 }
 
 sub help_brief {
