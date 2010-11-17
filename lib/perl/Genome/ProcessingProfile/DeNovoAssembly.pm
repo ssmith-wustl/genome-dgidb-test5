21e705631921239efcc841427a55f034519355db
Kim Kyung <kkyung@linus86.gsc.wustl.edu>
2010-11-17 17:35:49 -0600

    Updates for making event build denovo post assemble generic

--- lib/perl/Genome/ProcessingProfile/DeNovoAssembly.pm
+++ lib/perl/Genome/ProcessingProfile/DeNovoAssembly.pm
@@ -37,7 +37,12 @@ class Genome::ProcessingProfile::DeNovoAssembly{
 	    is_optional => 1,
 	    doc => "String of read trimmers, filters and sorters to use. Find processors in 'gmt fast-qual.' List each porocessor in order of execution as they would be run on the command line. Do not include 'gmt fast-qual', as this is assumed. List params starting w/ a dash (-), followed by the value. Separate processors by a pipe w/ a space on each side ( | ). The read processors will be validated. Ex:\n\ttrimmer bwa-style --trim-qual-length | filter by-length filter-length 70",
 	},
-   ],
+	#post assemble tools to run
+	post_assemble => {
+	    is_optional => 1,
+	    doc => 'String of things to run in post assembly stage .. by default already run WU post assemble process .. more later',
+	},
+    ],
 };
 
 sub create {
@@ -76,6 +81,15 @@ sub create {
         return;
     }
 
+    #validate post assemble steps 
+    if ( $self->post_assemble ) {
+	unless ( $self->_validate_post_assemble_steps ) {
+	    $self->status_message("Failed to validate post assemble steps");
+	    $self->delete;
+	    return;
+	}
+    }
+
     return $self;
 }
 
@@ -93,6 +107,7 @@ my %supported_assemblers = (
 	class => 'Genome::Model::Tools::Soap::DeNovoAssemble',
     },
 );
+
 sub supported_sequencing_platforms_for_assembler {
     my $self = shift;
 
@@ -251,10 +266,17 @@ sub assemble_job_classes {
     my $assembler_subclass = Genome::Utility::Text::string_to_camel_case($self->assembler_name);
 
     my @classes = 'Genome::Model::Event::Build::DeNovoAssembly::PrepareInstrumentData';
+
     push @classes, map { $_.'::'.$assembler_subclass } (qw/
         Genome::Model::Event::Build::DeNovoAssembly::Assemble
-        Genome::Model::Event::Build::DeNovoAssembly::PostAssemble
         /);
+
+    if ( $self->post_assemble ) {
+	push @classes, 'Genome::Model::Event::Build::DeNovoAssembly::PostAssemble';
+    }
+
+    #TODO - report needs some of the post-assemble outputs and these
+    #will be blank if post-assemble is not run
     push @classes, 'Genome::Model::Event::Build::DeNovoAssembly::Report';
 
     return @classes;
@@ -262,6 +284,125 @@ sub assemble_job_classes {
 
 sub assemble_objects {
     return 1;
+}
+
+#< post assemble steps >#
+
+sub _validate_post_assemble_steps {
+    my $self = shift;
+
+    $self->status_message("Validating post assemble steps");
+
+    foreach my $post_assemble_part ( $self->post_assemble_parts ) {
+	my ($tool_name) = $post_assemble_part =~ /^(\S+)/;
+	my ($param_string) = $post_assemble_part =~ /\S+\s+(.*)/;
+
+	$tool_name =~ s/-/ /g;
+	my $class_name = Genome::Utility::Text::string_to_camel_case( $tool_name );
+
+	my $class = 'Genome::Model::Tools::' . ucfirst $self->assembler_name . '::' . $class_name;
+
+	my $class_meta;
+	eval { $class_meta = $class->get_class_object; };
+	unless ( $class_meta ) {
+	    $self->error_message("Can't validate tool: $class_name, this tool does not exist: $class");
+	    return;
+	}
+
+	my %params;
+	if ( $param_string ) {
+	    %params = Genome::Utility::Text::param_string_to_hash( $param_string );
+	}
+
+	unless ( $self->validate_post_assemble_class_params( $class_meta, %params ) ) {
+	    $self->error_message("Failed to validate params for class: $class");
+	    return;
+	}
+    }
+
+    $self->status_message("Validated post assemble steps");
+
+    return 1;
+}
+
+sub validate_post_assemble_class_params {
+    my ($self, $class_meta, %params) = @_;
+
+    foreach my $key ( keys %params ) {
+        my $property = $class_meta->property_meta_for_name( $key );
+        unless ( $property ) {
+            $self->error_message("Failed to validate param, $key in class, " . $class_meta->class_name);
+            return;
+        }
+
+	print Dumper \%params;
+
+        my $value = $params{$key};
+        #check value against list of valid values
+        if ( $property->valid_values ) {
+            unless ( grep (/^$value$/, @{$property->valid_values}) ) {
+		my $valid_values;
+		for my $valid_value ( @{$property->valid_values} ) {
+		    $valid_values .= "$valid_value ";
+		}
+                $self->error_message("Failed to find param $key value $value in the list of valid values: $valid_values");
+                return;
+            }
+        }
+        if ( $property->data_type eq 'Integer' ) {
+            unless ( $value =~ /^$RE{num}{int}$/ ) {
+                $self->error_message("Expected property data type of Integer for param, $key but got $value");
+                return;
+            }
+        }
+        elsif ( $property->data_type eq 'Boolean' ) {
+            unless ( $value == 1 or $value == 0 ) { #not sure if this is the best way to check
+                $self->error_message("Expected property data type of Boolean for param, $key, but got $value");
+                return;
+            }
+        }
+        elsif ( $property->data_type eq 'Number' ) {
+            unless ( $value =~ /^$RE{num}{real}$/ ) {
+                $self->error_message("Expected property data type of Number of param, $key, but got $value");
+                return;
+            }
+        }
+        #else is text or string.. need to check
+    }
+
+    #check for missing required param
+    for my $property ( $class_meta->properties ) {
+        if ( not $property->is_optional ) {
+
+            my $property_name = $property->property_name;
+
+	    #exceptions .. since we don't know at this point where the assembly will end up
+	    if ( $property_name eq 'assembly_directory' ) {
+		$self->status_message("assembly_directory is a required param for tool,". $class_meta->class_name .", it will be assigned build data_directory");
+		next;
+	    }
+	    #if other required param is missing, quit
+            if ( not exists $params{$property_name} ) {
+                $self->error_message("Failed to get required param: $property_name for class, ".$class_meta->class_name);
+                return;
+            }
+        }
+    }
+
+    return 1
+}
+
+sub post_assemble_parts {
+    my $self = shift;
+
+    my @post_assemble_parts = split (/\;\s+?/, $self->post_assemble);
+
+    unless ( @post_assemble_parts ) {
+	$self->error_message("Could not find any parts to run in string: ".$self->post_assemble);
+	$self->delete;
+    }
+
+    return @post_assemble_parts;
 }
 
 1;
