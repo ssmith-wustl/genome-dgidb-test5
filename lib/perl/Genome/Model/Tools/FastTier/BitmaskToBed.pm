12b672acbb4a1fb0c5936cc3146b1d040b678e14
Rob Long <rlong@genome.wustl.edu>
2010-12-13 17:23:49 -0600

    Added gmt fast-tier and some tools used to create bed files for tiering
    Squashed commit of the following:
    
    commit 2d359d55e866af783865aed41f2df2ab6095daa6
    Author: Rob Long <rlong@genome.wustl.edu>
    Date:   Mon Dec 13 16:38:32 2010 -0600
    
        gmt fast-tier another small patch to non-indel logic
    
    commit f6bf2767e62b8b3b5bfdf261a4e535e6f8162c1a
    Author: Rob Long <rlong@genome.wustl.edu>
    Date:   Mon Dec 13 16:05:43 2010 -0600
    
        gmt fast-tier adjusted permissions to match genome standards
    
    commit e2680c178cc73969787f5f09cde4a95ef7598f28
    Author: Rob Long <rlong@genome.wustl.edu>
    Date:   Mon Dec 13 15:59:15 2010 -0600
    
        gmt fast-tier fast-tier plugged a hole in logic that caused tier4 non-indel stuff to disappear without being copied in from its temp file
    
    commit 4bca5052edb786ad3b7117cb3859e5696b7bf51d
    Author: Rob Long <rlong@genome.wustl.edu>
    Date:   Mon Dec 13 10:54:44 2010 -0600
    
        gmt fast-tier bitmask-to-bed changed to use above genome
    
    commit c07c6db75f4f7a027ceb45c805ace8e5d5e1623c
    Author: Rob Long <rlong@genome.wustl.edu>
    Date:   Sat Dec 11 00:33:33 2010 -0600
    
        Fixed MakeTierBitmasks and added BitmaskToBed
    
    commit 8a1637570e34fabbf48dae94517702d4b8a7a3e0
    Author: Rob Long <rlong@genome.wustl.edu>
    Date:   Fri Dec 10 11:53:22 2010 -0600
    
        added gmt fast-tier and subdir, this is where fast-tier and it's support scripts will live.

---
+++ lib/perl/Genome/Model/Tools/FastTier/BitmaskToBed.pm
@@ -0,0 +1,106 @@
+package Genome::Model::Tools::FastTier::BitmaskToBed;
+
+use strict;
+use warnings;
+
+use above 'Genome';
+use IO::File;
+use Bit::Vector;
+
+class Genome::Model::Tools::FastTier::BitmaskToBed {
+    is => 'Command',
+    has => [
+        output_file => {
+            type => 'Text',
+            is_output => 1,
+            doc => 'Output file for the bed tier',
+        },
+        bitmask => {
+            type => 'Text',
+            is_input => 1,
+            doc => 'The input bitmask',
+        },
+    ],
+};
+
+
+sub help_brief {
+    "Used to turn bitmask files into bed files"
+}
+
+sub help_synopsis {
+    my $self = shift;
+    return <<"EOS"
+genome-model tools fast-tier bitmask-to-bed ...    
+EOS
+}
+
+sub execute {
+    my $self = shift;
+
+    my $output = $self->output_file;
+    my $bitmask = $self->bitmask;
+    my $out_fh = Genome::Utility::FileSystem->open_file_for_writing($output);
+
+
+
+
+    unless($bitmask) {
+        die("nobitmask");
+    }
+    #do some stuff to read this from a file without making it suck
+    my $in_fh = IO::File->new($bitmask,"<:raw");
+    unless($in_fh) {
+        die("cant.read.from.".$self->bitmask);
+    }
+    my $read_string;
+    sysread $in_fh, $read_string, 4;
+    my $header_length = unpack "N", $read_string;
+    sysread $in_fh, $read_string, $header_length;
+    my $header_string = unpack "a*",$read_string;
+    my %genome = split /\t/, $header_string; #each key is the name, each value is the size in bits
+
+    #now read in each one
+
+    for my $chr (sort keys %genome) {
+        my $index=0;
+        $genome{$chr} = Bit::Vector->new($genome{$chr}); #this throws an exception if it fails. Probably should be trapped at some point in the future
+        sysread $in_fh, $read_string, 4;
+        my $chr_byte_length = unpack "N", $read_string;
+        my $chr_read_string;
+        sysread $in_fh, $chr_read_string, $chr_byte_length;
+        my $chr_length = $genome{$chr}->Size();
+        my $start=0;
+        my $chr_length2=  $chr_byte_length*8;
+        $genome{$chr}->Block_Store($chr_read_string);
+        while(1) {
+            my $bit = $genome{$chr}->bit_test($index);
+            if($bit && $start) {
+                $DB::single=1 if ($start==67167422); 
+            }
+            elsif($bit) {
+                $start=$index+1;
+            }
+            elsif(!$bit && $start) {
+                my $end=$index;
+                $start = $start -1;
+                $out_fh->print("$chr\t$start\t$end\n");
+                $start = 0;
+            }
+            if(($index+1 >= $chr_length) && $start) {
+                $out_fh->print("$chr\t$start\t$chr_length\n");
+                $start = 0;
+                $index=0;
+                last;
+            }
+            elsif($index+1 >= $chr_length) {
+                $index=0;
+                last;
+            }
+            $index++;
+        }
+    }
+    $in_fh->close;
+
+    return 1;
+}
