87d412a45ccf5671d2dd5b08562cfd8ab0f801d1
Jason Walker <jwalker@genome.wustl.edu>
2010-12-22 12:06:35 -0600

    Further refactor the subclasses of the RefCov tool, moving all code up to baseclass.  The default parameters are different per subclass

--- lib/perl/Genome/Model/Tools/RefCov/WholeGenome.pm
+++ lib/perl/Genome/Model/Tools/RefCov/WholeGenome.pm
@@ -9,46 +9,45 @@ class Genome::Model::Tools::RefCov::WholeGenome {
     is => ['Genome::Model::Tools::RefCov'],
     has_input => [
         merged_stats_file => {
-            is => 'Text',
-            doc => 'The final merged stats file',
+            is_optional => 0,
         },
         merge_by => {
-            is => 'Text',
             is_optional => 1,
             default_value => 'transcript',
-            valid_values => ['exome','gene','transcript'],
         },
+        evaluate_gc_coverage => {
+            default_value => 1,
+            is_optional => 1,
+        },
+        roi_normalized_coverage => {
+            default_value => 1,
+            is_optional => 1,
+        },
+        genome_normalized_coverage => {
+            default_value => 1,
+            is_optional => 1,
+        },
+        min_depth_filter => {
+            default_value => '30',
+            is_optional => 1,
+        },
+        print_headers => {
+            default_value => 1,
+            is_optional => 1,
+        },
+
     ],
 };
 
-sub help_detail {
-    '
-These commands are setup to run perl v5.10.0 scripts that use Bio-Samtools and require bioperl v1.6.0.  They all require 64-bit architecture.
-';
+sub help_brief {
+    "The default for running RefCov on WholeGenome BAMs with 30x depth.  Includes merged results by transcript and normalized coverage by genome and ROI.",
 }
 
 sub execute {
     my $self = shift;
-    unless ($] > 5.012) {
-        die "Bio::DB::Sam requires perl 5.12!";
+    unless ($self->print_roi_coverage) {
+        die('Failed to print ROI coverage!');
     }
-    require Bio::DB::Sam;
-    # This is only necessary when run in parallel
-    $self->resolve_final_directory();
-
-    # This is only necessary when run in parallel or only an output_directory is
-    # defined in params(ie. no stats_file)
-    $self->resolve_stats_file();
-
-    unless ($self->print_standard_roi_coverage()) {
-        die( 'Failed to print stats to file '. $self->stats_file() );
-    }
-
-    # WholeGenome assumes we will be evaluating gene (exon) annotation ROI in
-    # context of whole genome alignments (e.g., BWA produced BAMs). We will be
-    # stitching the exons back together on a gene-by-gene basis.
-    $self->merge_stats_by($self->merge_by,$self->merged_stats_file);
-
     return 1;
 }
 
