fba0fd16ebe36ead1274fc6d21bcb68697b0074d
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-01-21 16:19:22 -0600

    Modified WUGC to use Genome::Site::WUGC::Environment.
    Added Environment.pm to include genome center specific environment variables.
    See that module for more information.

---
+++ lib/perl/Genome/Site/WUGC/Environment.pm
@@ -0,0 +1,11 @@
+#
+# Set WUGC specific environment variables here. 
+# In addition, note the fallback path you use for that environment varible
+# If it doesn't exist.
+# e.g. 
+# my $db_path = $ENV{GC_GMT_MUSIC_DBFILES};
+# $db_path ||= "/usr/local/share/genome/music/dbfiles/";
+#
+
+$ENV{GC_GMT_MUSIC_DBFILES} = "/gscmnt/sata893/info/medseq/ndees/pfam/dbfiles/";
+# /usr/local/share/genome/music/dbfiles/
