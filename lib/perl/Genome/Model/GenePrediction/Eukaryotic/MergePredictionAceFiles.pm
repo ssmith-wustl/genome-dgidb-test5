f17127e32b79e4a3c6da7399d209fd3c8e93f130
Brian Derickson <bdericks@genome.wustl.edu>
2011-01-26 14:38:46 -0600

    egap merge prediction ace step now checks for file size before concatenating

--- lib/perl/Genome/Model/GenePrediction/Eukaryotic/MergePredictionAceFiles.pm
+++ lib/perl/Genome/Model/GenePrediction/Eukaryotic/MergePredictionAceFiles.pm
@@ -25,7 +25,7 @@ class Genome::Model::GenePrediction::Eukaryotic::MergePredictionAceFiles {
 sub execute {
     my $self = shift;
 
-    my @ace_file_chunks = $self->ace_file_chunks;
+    my @ace_file_chunks = @{$self->ace_file_chunks};
     unless (@ace_file_chunks) {
         $self->warning_message("No ace file chunks given, exiting");
         return 1;
@@ -37,10 +37,15 @@ sub execute {
         unlink $ace_file;
     }
 
-    $self->status_message("Merging below files into $ace_file:\n" . join("\n", @ace_file_chunks));
+    my @chunks_with_output;
+    for my $chunk (@ace_file_chunks) {
+        push @chunks_with_output, $chunk if -s $chunk;
+    }
+
+    $self->status_message("Concatenating " . scalar @chunks_with_output . " ace file chunks into $ace_file");
 
     my $rv = Genome::Utility::FileSystem->cat(
-        input_files => @ace_file_chunks,
+        input_files => \@chunks_with_output,
         output_file => $ace_file,
     );
     confess "Could not concatenate ace files!" unless defined $rv and $rv;
