4593e8958454b5e30da61a5e2522ec57b298322a
Nathan Nutter <nnutter@genome.wustl.edu>
2011-01-26 15:33:04 -0600

    use Genome::Sys to get dbpath instead of having it hardcoded

--- lib/perl/Genome/Model/Tools/Music/Pfam.pm
+++ lib/perl/Genome/Model/Tools/Music/Pfam.pm
@@ -122,10 +122,9 @@ sub execute {
         my $stop = $fields[$maf_columns{'End_position'}];
         #my $ref = $fields[$maf_columns{'Reference_Allele'}];
         # use environment variable but fall back to reasonable default
-        my $db_path = $ENV{GC_GMT_MUSIC_DBFILES};
-        $db_path ||= '/usr/local/share/genome/music/dbfiles';
+        my $db_path = Genome::Sys->dbpath('pfam', 'latest') or die "Cannot find the pfam db path.";
         my $tabix = can_run('tabix') or die "Cannot find the tabix command. It can be obtained from http://sourceforge.net/projects/samtools/files/tabix";
-        my $tabix_cmd = "$tabix $db_path/pfam/pfam.annotation.gz $chr:$start-$stop - |";
+        my $tabix_cmd = "$tabix $db_path/pfam.annotation.gz $chr:$start-$stop - |";
         my %domains;
         open(TABIX,$tabix_cmd) or die "Cannot open() the tabix command. Please check it is in your PATH. It can be installed from the samtools project. $!";
         while (my $tabline = <TABIX>) {
