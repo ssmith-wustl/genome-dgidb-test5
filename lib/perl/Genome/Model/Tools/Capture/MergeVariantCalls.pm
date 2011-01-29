bf16ea1fdcd255dc9c165b3b080380de2a1c9035
William Schierding <wschierd@genome.wustl.edu>
2011-01-28 19:35:09 -0600

    Fix to correct ssmith breakage of this tool

--- lib/perl/Genome/Model/Tools/Capture/MergeVariantCalls.pm
+++ lib/perl/Genome/Model/Tools/Capture/MergeVariantCalls.pm
@@ -77,9 +77,9 @@ sub execute {                               # replace with real execution logic.
 
 	## Run the merge using Varscan ##
 	
-	my $cmd = "java -Xms3000m -Xmx3000m -classpath ~dkoboldt/Software/Varscan net.sf.varscan.Varscan compare $varscan_file $glf_file merge $output_file.unsorted";
+	my $cmd = "java -Xms3000m -Xmx3000m -jar /gsc/scripts/lib/java/VarScan/VarScan.jar compare $varscan_file $glf_file merge $output_file.unsorted";
 	system($cmd);
-	
+
 	$cmd = "gmt capture sort-by-chr-pos --input-file $output_file.unsorted --output-file $output_file";
 	system($cmd);
 	
