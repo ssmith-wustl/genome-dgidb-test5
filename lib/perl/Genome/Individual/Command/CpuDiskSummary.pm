41c60b472c781a90134fd19d637fddec0ef7504c
Scott Smith <ssmith@genome.wustl.edu>
2011-01-30 19:58:48 -0600

    renamed compute-summary to cpu-disk-summary

--- lib/perl/Genome/Individual/Command/CpuDiskSummary.pm
+++ lib/perl/Genome/Individual/Command/CpuDiskSummary.pm
@@ -1,11 +1,11 @@
-package Genome::Individual::Command::ComputeSummary;
+package Genome::Individual::Command::CpuDiskSummary;
 
 use strict;
 use warnings;
 
 use Genome;
 
-class Genome::Individual::Command::ComputeSummary {
+class Genome::Individual::Command::CpuDiskSummary {
     is => 'Genome::Command::Base',
     has => [
         patients    => { is => 'Genome::Individual', 
@@ -53,13 +53,12 @@ sub execute {
 
     my @row_names = (
         'Individual',
-        '---',
         'Tumor Gbases', 
         'Normal Gbases',
-        'Tumor RefAlign GB', 
-        'Normal RefAlign GB',
+        'Tumor RefAlign (GB)', 
+        'Normal RefAlign (GB)',
         'Tumor RefAlign (slot*hr)', 
-        'Normal RefAlign (slot * hr)',
+        'Normal RefAlign (slot*hr)',
         'Somatic (GB)', 
         'Somatic (slot*hr)',
         '---',
@@ -84,7 +83,7 @@ sub execute {
             next;
         }
 
-        my $best_build;
+        my $somatic_build;
         for my $model (reverse @somatic_models) {
             my @builds = $model->builds(
                 (@force_build_ids ? (id => \@force_build_ids) : (status => 'Succeeded')) 
@@ -93,23 +92,23 @@ sub execute {
                 #$self->warning_message("no successful builds for model " . $model->__display_name__);
                 next;
             }
-            $best_build = $builds[-1];
+            $somatic_build = $builds[-1];
         }
 
-        unless ($best_build) {
+        unless ($somatic_build) {
             $self->error_message("no builds on somatic models for patient " . $patient->__display_name__);
             next;
         }
 
         #$self->status_message(
-        #    "patient " . $patient->__display_name__ . ' using build ' . $best_build->__display_name__ . "\n"
+        #    "patient " . $patient->__display_name__ . ' using build ' . $somatic_build->__display_name__ . "\n"
         #);
         
 
-        $f{somatic_build} = $best_build->__display_name__;
+        $f{somatic_build} = $somatic_build->__display_name__;
 
-        my $tumor_model = $best_build->model->tumor_model;
-        my $normal_model = $best_build->model->normal_model;
+        my $tumor_model = $somatic_build->model->tumor_model;
+        my $normal_model = $somatic_build->model->normal_model;
         
         my $tumor_build = $tumor_model->last_complete_build;
         my $normal_build = $normal_model->last_complete_build;
@@ -117,9 +116,19 @@ sub execute {
         $f{tumor_build} = $tumor_build->__display_name__;
         $f{normal_build} = $normal_build->__display_name__;
 
+        #my $tumor_wfie = $tumor_build->workflow_instance->current;
+        #my $normal_wfie = $normal_build->workflow_instance->current;
+        #my $somatic_wfie = $somatic_build->workflow_instance->current;
+        
+        $f{"Somatic (slot*hr)"} = $somatic_build->cpu_slot_hours;
+        $f{"Tumor RefAlign (slot*hr)"} = $tumor_build->cpu_slot_hours("event_type not like" => '%align-reads%');
+        $f{"Normal RefAlign (slot*hr)"} = $normal_build->cpu_slot_hours("event_type not like" => '%align-reads%');
+
         my $tumor_alloc1 = $tumor_build->disk_allocation;
         my $normal_alloc1 = $normal_build->disk_allocation;
+        my $somatic_alloc1 = $somatic_build->disk_allocation;
 
+        $f{"Somatic (GB)"} = $somatic_alloc1->kilobytes_requested / (1024**2);
         $f{"Tumor RefAlign GB regular"} = $tumor_alloc1->kilobytes_requested / (1024**2);
         $f{"Normal RefAlign GB regular"} = $normal_alloc1->kilobytes_requested / (1024**2);
        
@@ -143,8 +152,8 @@ sub execute {
         $f{"Tumor RefAlign GB bam"} = $tumor_bam_size; 
         $f{"Normal RefAlign GB bam"} = $normal_bam_size; 
 
-        $f{"Tumor RefAlign GB"} = $f{"Tumor RefAlign GB regular"} + $f{"Tumor RefAlign GB bam"};
-        $f{"Normal RefAlign GB"} = $f{"Normal RefAlign GB regular"} + $f{"Normal RefAlign GB bam"};
+        $f{"Tumor RefAlign (GB)"} = $f{"Tumor RefAlign GB regular"} + $f{"Tumor RefAlign GB bam"};
+        $f{"Normal RefAlign (GB)"} = $f{"Normal RefAlign GB regular"} + $f{"Normal RefAlign GB bam"};
 
         #$tumor_metric  = $tumor_build->metric(name => 'instrument_data_total kb');
         #$normal_metric = $normal_build->metric(name => 'instrument_data_total kb');
