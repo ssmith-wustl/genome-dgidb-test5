b450f5db6ec01bf615226f356f2f7c5fd0679c04
Brian Derickson <bdericks@genome.wustl.edu>
2011-02-21 11:50:53 -0600

    removed assignment date from assignments, required PSE query
    fixed assignment lister to no longer default to show assignment_date

--- lib/perl/Genome/Disk/Assignment.pm
+++ lib/perl/Genome/Disk/Assignment.pm
@@ -32,14 +32,8 @@ class Genome::Disk::Assignment {
         mount_path => { via => 'volume' },
         total_kb   => { via => 'volume' },
         unallocated_kb => { via => 'volume' },
-	    percent_full => {
-		    calculate_from => ['absolute_path'],
-			calculate => q|
-			    my @pct_full = `df -h $absolute_path`;
-				my @split_pct_full = split(/%/,$pct_full[-1]);
-				@split_pct_full = split (/ /,$split_pct_full[0]);
-				return $split_pct_full[-1]; |,
-		},
+        percent_allocated => { via => 'volume' },
+        percent_used => { via => 'volume' },
         absolute_path => {
             calculate_from => ['mount_path','subdirectory'],
             calculate => q| return $mount_path .'/'. $subdirectory; |,
