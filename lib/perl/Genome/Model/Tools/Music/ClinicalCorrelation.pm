1eaecea715013dd726b9129396c633e9f87145c7
Nathan Nutter <nnutter@genome.wustl.edu>
2011-02-23 14:24:26 -0600

    clinical-correlation shouldn't use Genome::Command::Base

--- lib/perl/Genome/Model/Tools/Music/ClinicalCorrelation.pm
+++ lib/perl/Genome/Model/Tools/Music/ClinicalCorrelation.pm
@@ -22,7 +22,7 @@ Version 1.01
 #our $VERSION = '1.01';
 
 class Genome::Model::Tools::Music::ClinicalCorrelation {
-    is => 'Genome::Command::Base',                       
+    is => 'Command',                       
     has => [ 
     output_file => {
         is => 'Text',
