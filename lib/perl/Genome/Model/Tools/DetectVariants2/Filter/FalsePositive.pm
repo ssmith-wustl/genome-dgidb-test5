f96d9d5f8d614c58c4ba3ec1f88529ad25032b84
Rob Long <rlong@genome.wustl.edu>
2011-02-25 10:28:10 -0600

    Added DetVar2 Filter FalsePositive module

--- lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalsePositive.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalsePositive.pm
@@ -11,8 +11,26 @@ class Genome::Model::Tools::DetectVariants2::Filter::FalsePositive {
 
 sub _filter_variants {
     my $self = shift;
+    
+    my ($tumor_bam,$variant_file,$output_file,$filtered_file,$refseq);
 
-    die "not yet implemented\n";
+    $tumor_bam = $self->aligned_reads_input;
+    $variant_file = $self->input_directory."/snvs.hq.bed";
+    $output_file = $self->_temp_staging_directory."/snvs.hq.bed";
+    $filtered_file = $self->_temp_staging_directory."/snvs.lq.bed";
+    $refseq = $self->reference_sequence_input;
+
+    my $ff_cmd = Genome::Model::Tools::Somatic::FilterFalsePositives->create(
+            bam_file => $tumor_bam,
+            variant_file => $variant_file,
+            output_file => $output_file,
+            filtered_file => $filtered_file,
+            reference => $refseq,
+    );
+
+    unless( $ff_cmd->execute ) {
+        die $self->error_message("Failed to execute FilterFalsePositive command." );
+    }
 
     return 1;
 }
