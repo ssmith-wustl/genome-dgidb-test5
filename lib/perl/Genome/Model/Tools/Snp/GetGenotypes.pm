7bc1400a86b4bebe3163b8769488832750699d95
William Schierding <wschierd@genome.wustl.edu>
2011-03-09 13:20:41 -0600

    Simplified Code and Options to be easier to read

--- lib/perl/Genome/Model/Tools/Snp/GetGenotypes.pm
+++ lib/perl/Genome/Model/Tools/Snp/GetGenotypes.pm
@@ -28,8 +28,8 @@ class Genome::Model::Tools::Snp::GetGenotypes {
 	has => [                                # specify the command's single-value properties (parameters) <--- 
 		sample_list	=> { is => 'Text', doc => "List of samples as input" , is_optional => 0},
 		data_directory	=> { is => 'Text', doc => "Output data directory" , is_optional => 0},
-		reference	=> { is => 'Text', doc => "reference (build36) or GRCh37 (build37)" , is_optional => 0, default => 'GRCh37'},
-		dbsnp_build	=> { is => 'Text', doc => "130 or 132" , is_optional => 0, default => '132'},
+		reference	=> { is => 'Text', doc => "Current options: reference (for build36) or GRCh37 (for build37)" , is_optional => 0, default => 'GRCh37'},
+		dbsnp_build	=> { is => 'Text', doc => "Version of dbSNP to use, be careful to match with reference" , is_optional => 0, default => '132'},
 		data_source	=> { is => 'Text', doc => "iscan or external" , is_optional => 0, default => 'iscan'},
 		cn_file		=> { is => 'Text', doc => "Do you also want to print out a cn file?" , is_optional => 1, default => 0},
 	],
@@ -86,89 +86,55 @@ sub execute {                               # replace with real execution logic.
 			print "Exporting for $sample\n";
 			
 			# Get all external genotypes for this sample
+			my @genotypes;
 			if ($self->data_source eq 'iscan') {
-				foreach my $genotype ( $organism_sample->get_genotype ) {
-					#LSF: For Affymetrix, this will be the birdseed file.
-					my $ab_file = $genotype->get_genotype_file_ab;
-					
-					# Get the data adapter (DataAdapter::GSGMFinalReport class object)
-					my $filter       = DataAdapter::Result::Filter::Nathan->new();
-					my $data_adapter = $genotype->get_genotype_data_adapter(
-						genome_build => $db_snp_info->genome_build,
-						snp_db_build => $db_snp_info->snp_db_build,
-						filter       => $filter,
-						( $reference ? ( type => $reference ) : () ),
-					);
-					
-					# Next if there is no genotype data.
-					next unless($data_adapter);
-			
-					## Open the output file ##
-					my $outfile = "$data_directory/$sample.genotypes";
-					open(OUTFILE, ">$outfile") or die "Can't open output file: $!\n";
-				
-					my $cn_outfile;
-					if ($self->cn_file) {
-						$cn_outfile = "$data_directory/$sample.cn";
-						open(OUTFILE2, ">$cn_outfile") or die "Can't open output file: $!\n";
-					}
-					# Loop through the result row (DataAdapter::Result::GSGMFinalReport class object)
-					while ( my $result = $data_adapter->next_result )
-					{
-						print OUTFILE join "\t", ( $result->chromosome, $result->position, $result->alleles );
-						#$result->snp_name
-						print OUTFILE "\n";
-						if ($self->cn_file) {
-							print OUTFILE2 join "\t", ( $result->chromosome, $result->position, $result->log_r_ratio );
-						}
-					}
-					close(OUTFILE);
-					if ($self->cn_file) {
-						close(OUTFILE2);
-					}
-				}
+				@genotypes = $organism_sample->get_genotype;
 			}
 			elsif ($self->data_source eq 'external') {
-				foreach my $genotype ( $organism_sample->get_external_genotype ) {
-					#LSF: For Affymetrix, this will be the birdseed file.
-					my $ab_file = $genotype->get_genotype_file_ab;
-					
-					# Get the data adapter (DataAdapter::GSGMFinalReport class object)
-					my $filter       = DataAdapter::Result::Filter::Nathan->new();
-					my $data_adapter = $genotype->get_genotype_data_adapter(
-						genome_build => $db_snp_info->genome_build,
-						snp_db_build => $db_snp_info->snp_db_build,
-						filter       => $filter,
-						( $reference ? ( type => $reference ) : () ),
-					);
-					
-					# Next if there is no genotype data.
-					next unless($data_adapter);
+				@genotypes = $organism_sample->get_external_genotype;
+			}
+			else {
+				die "Must choose either 'iscan' or 'external' for data_source";
+			}
+
+			foreach my $genotype ( @genotypes ) {
+				#LSF: For Affymetrix, this will be the birdseed file.
+				my $ab_file = $genotype->get_genotype_file_ab;
+				
+				# Get the data adapter (DataAdapter::GSGMFinalReport class object)
+				my $filter       = DataAdapter::Result::Filter::Nathan->new();
+				my $data_adapter = $genotype->get_genotype_data_adapter(
+					genome_build => $db_snp_info->genome_build,
+					snp_db_build => $db_snp_info->snp_db_build,
+					filter       => $filter,
+					( $reference ? ( type => $reference ) : () ),
+				);
+				
+				# Next if there is no genotype data.
+				next unless($data_adapter);
 			
-					## Open the output file ##
-					my $outfile = "$data_directory/$sample.genotypes";
-					open(OUTFILE, ">$outfile") or die "Can't open output file: $!\n";
-				
-					my $cn_outfile;
+				## Open the output file ##
+				my $outfile = "$data_directory/$sample.genotypes";
+				open(OUTFILE, ">$outfile") or die "Can't open output file: $!\n";
+			
+				my $cn_outfile;
+				if ($self->cn_file) {
+					$cn_outfile = "$data_directory/$sample.cn";
+					open(OUTFILE2, ">$cn_outfile") or die "Can't open output file: $!\n";
+				}
+				# Loop through the result row (DataAdapter::Result::GSGMFinalReport class object)
+				while ( my $result = $data_adapter->next_result )
+				{
+					print OUTFILE join "\t", ( $result->chromosome, $result->position, $result->alleles );
+					#$result->snp_name
+					print OUTFILE "\n";
 					if ($self->cn_file) {
-						$cn_outfile = "$data_directory/$sample.cn";
-						open(OUTFILE2, ">$cn_outfile") or die "Can't open output file: $!\n";
+						print OUTFILE2 join "\t", ( $result->chromosome, $result->position, $result->log_r_ratio );
 					}
-					# Loop through the result row (DataAdapter::Result::GSGMFinalReport class object)
-					while ( my $result = $data_adapter->next_result )
-					{
-						print OUTFILE join "\t", ( $result->chromosome, $result->position, $result->alleles );
-						#$result->snp_name
-						print OUTFILE "\n";
-						if ($self->cn_file) {
-							print OUTFILE2 join "\t", ( $result->chromosome, $result->position, $result->log_r_ratio );
-						}
-					}
-				
-					close(OUTFILE);
-					if ($self->cn_file) {
-						close(OUTFILE2);
-					}
+				}
+				close(OUTFILE);
+				if ($self->cn_file) {
+					close(OUTFILE2);
 				}
 			}
 		}
