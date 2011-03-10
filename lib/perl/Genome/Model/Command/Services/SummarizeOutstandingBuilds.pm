efa468dc46db943b75df69b74e30756aef078951
Jim Weible <jweible@genome.wustl.edu>
2011-03-10 10:46:58 -0600

    Cleanup

--- lib/perl/Genome/Model/Command/Services/SummarizeOutstandingBuilds.pm
+++ lib/perl/Genome/Model/Command/Services/SummarizeOutstandingBuilds.pm
@@ -42,16 +42,16 @@ sub execute {
         return;
     }
 
-    $self->initialize_buckets;
+    $self->initialize_categories;
 
     my @models = $self->get_models;
     $self->status_message("Done getting models");
     for my $model (@models){
-        $self->separate_models_for_processing($model);
+        $self->categorize_models_for_processing($model);
     }
     $self->status_message("Done separating models");
 
-    $self->process_buckets;
+    $self->process_categories;
 
     Genome::Sys->unlock_resource(resource_lock =>$lock);
  
@@ -94,61 +94,61 @@ sub get_models_with_no_builds {
     return values %models_without_builds;
 }
 
-sub process_buckets {
+sub process_categories {
     my $self = shift;
-    my @buckets = $self->_bucket_names;
-    for my $bucket (@buckets){
-        my $method_name = "_process" . $bucket;
+    my @categories = $self->_category_names;
+    for my $category (@categories){
+        my $method_name = "_process" . $category;
         $self->$method_name;
     }
 }
 
-sub separate_models_for_processing {
+sub categorize_models_for_processing {
     my ($self, $model) = @_;
     my $latest_build = $model->latest_build;
     if (not $latest_build){
-        $self->separate_model_with_no_builds($model);            
+        $self->categorize_model_with_no_builds($model);            
     }elsif($latest_build->status eq 'Scheduled'){
-        $self->separate_model_with_scheduled_build($model, $latest_build);
+        $self->categorize_model_with_scheduled_build($model, $latest_build);
     }elsif($latest_build->status eq 'Running'){
-        $self->separate_model_with_running_build($model, $latest_build);
+        $self->categorize_model_with_running_build($model, $latest_build);
     }elsif($latest_build->status eq 'Succeeded'){
-        $self->separate_model_with_succeeded_build($model, $latest_build);
+        $self->categorize_model_with_succeeded_build($model, $latest_build);
     }elsif($latest_build->status eq 'Abandoned'){
-        $self->separate_model_with_abandoned_build($model, $latest_build);
+        $self->categorize_model_with_abandoned_build($model, $latest_build);
     }elsif($latest_build->status eq 'Failed' or $latest_build->status eq 'Crashed'){
-        $self->separate_model_with_failed_build($model, $latest_build);
+        $self->categorize_model_with_failed_build($model, $latest_build);
     }elsif($latest_build->status eq 'Preserved'){
-        $self->separate_model_with_preserved_build($model, $latest_build);
+        $self->categorize_model_with_preserved_build($model, $latest_build);
     }else{
         $self->warning_message("I dunno what to do with model " . $model->id);
     }
 }
 
-sub separate_model_with_no_builds{
+sub categorize_model_with_no_builds{
     my $self = shift;
     my $model = shift;
     my $model_age = $self->_age_in_days($model->creation_date);
     if($model_age > 7){
-        push(@{$self->_none_kill_and_email_user}, $model);
+        push(@{$self->_category_hash->{_none_kill_and_email_user}}, $model);
     }elsif($model_age > 3){
-        push(@{$self->_none_email_user}, $model);
+        push(@{$self->_category_hash->{_none_email_user}}, $model);
     }
 }
 
-sub separate_model_with_scheduled_build{
+sub categorize_model_with_scheduled_build{
     my ($self, $model, $latest_build) = @_;
     # my $latest_build_id = $latest_build->id;
     # my $lsf_job_id = $latest_build->the_master_event->lsf_job_id;
     
     # my $bjobs_output = `bjobs $lsf_job_id`;
     # if(not $bjobs_output or not ($bjobs_output =~ m/$latest_build_id/)){
-        # push(@{$self->_scheduled_incorrect_state}, $latest_build); 
+        # push(@{$self->_category_hash->{_scheduled_incorrect_state}}, $latest_build);
     # }
 #TODO: replace this with something that works.  lsf_job_id isn't useful if the build is scheduled
 }
 
-sub separate_model_with_running_build{
+sub categorize_model_with_running_build{
     my ($self, $model, $latest_build) = @_;
 
     my $latest_build_id = $latest_build->id;
@@ -158,74 +158,77 @@ sub separate_model_with_running_build{
     $job_id = $latest_build->the_master_event->lsf_job_id unless ($job_id);
     my $bjobs_output = `bjobs $job_id`;
     if(not $bjobs_output or not ($bjobs_output =~ m/$latest_build_id/)){
-        push(@{$self->_running_incorrect_state}, $latest_build); 
+        push(@{$self->_category_hash->{_running_incorrect_state}}, $latest_build);
         return;
     }
 
     my ($model_age) = $self->_age_in_days($latest_build->date_scheduled);
     if($model_age >=10){
-        push(@{$self->_running_kill_and_email_user}, $latest_build);
+        push(@{$self->_category_hash->{_running_kill_and_email_user}}, $latest_build);
     }elsif($model_age >= 3){
-        push(@{$self->_running_email_user}, $latest_build);
+        push(@{$self->_category_hash->{_running_email_user}}, $latest_build);
     }
 }
 
-sub separate_model_with_succeeded_build{
+sub categorize_model_with_succeeded_build{
     my ($self, $model, $latest_build) = @_;
     my @builds = $model->builds;
     for my $build (@builds){
         next if $build == $latest_build;
         if ($build->status eq 'Failed'){
-            push (@{$self->_succeeded_abandon_builds}, $build);
+            push(@{$self->_category_hash->{_succeeded_abandon_builds}}, $build);
         }elsif ($build->status eq 'Succeeded'){
-            push (@{$self->_succeeded_eviscerate_builds}, $build);
+            push(@{$self->_category_hash->{_succeeded_eviscerate_builds}}, $build);
         }elsif ($build->status eq 'Running' and $self->_age_in_days($latest_build->date_completed) >= 1){
-            push(@{$self->_succeeded_kill_running_builds}, $build) if $self->_age_in_days($build->date_scheduled);
+            push(@{$self->_category_hash->{_succeeded_kill_running_builds}}, $build) if $self->_age_in_days($build->date_scheduled);
         }
     }
 }
 
-sub separate_model_with_abandoned_build{
+sub categorize_model_with_abandoned_build{
     my ($self, $model, $latest_build) = @_;
-    push(@{$self->_abandoned_start_new_builds}, $model);
+    push(@{$self->_category_hash->{_abandoned_start_new_builds}}, $model);
 }
 
-sub separate_model_with_failed_build{
+sub categorize_model_with_failed_build{
     my ($self, $model, $latest_build) = @_;
     my @builds = $model->builds;
 
     if(scalar(@builds) > 2 and ($builds[-2]->status eq 'Failed' or $builds[-2]->status eq 'Crashed')){
         #flag model for manual attention if the last two builds are failures
-        push(@{$self->_failed_flag_for_manual_attention}, $model);
+        push(@{$self->_category_hash->{_failed_flag_for_manual_attention}}, $model);
     }
     else{
-        push(@{$self->_failed_start_new_builds}, $model);
+        push(@{$self->_category_hash->{_failed_start_new_builds}}, $model);
     }
 }
 
-sub separate_model_with_preserved_build{
+sub categorize_model_with_preserved_build{
     my ($self, $model, $latest_build) = @_;
     #TODO: what do we do?  
-    push(@{$self->_preserved}, $model);
+    push(@{$self->_category_hash->{_preserved}}, $model);
 }
 
-sub initialize_buckets {
+sub initialize_categories {
     my $self = shift;
-    for my $bucket ($self->_bucket_names){
-        $self->$bucket([]);
+    my %category_hash;
+    $DB::single = 1;
+    for my $category ($self->_category_names){
+        $category_hash{$category} = [];
     }
     
+    $self->_category_hash(\%category_hash);
     return 1;
 }
 
 sub _process_none_email_user{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "E-MAIL FOR MODEL WITH NO BUILDS\n";
-    for my $model (@{$self->_none_email_user}){
+    for my $model (@{$self->_category_hash->{_none_email_user}}){
         print join("\t", $model->id, $model->creation_date), "\n"; 
     }
 }
@@ -233,11 +236,11 @@ sub _process_none_email_user{
 sub _process_none_kill_and_email_user{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "KILL AND E-MAIL FOR MODEL WITH NO BUILDS\n";
-    for my $model (@{$self->_none_kill_and_email_user}){
+    for my $model (@{$self->_category_hash->{_none_kill_and_email_user}}){
         print join("\t", $model->id, $model->creation_date), "\n"; 
     }
 }
@@ -245,11 +248,11 @@ sub _process_none_kill_and_email_user{
 sub _process_scheduled_incorrect_state {
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "SCHEDULED BUILDS IN INCORRECT STATE\n";
-    for my $build (@{$self->_scheduled_incorrect_state}){
+    for my $build (@{$self->_category_hash->{_scheduled_incorrect_state}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -257,11 +260,11 @@ sub _process_scheduled_incorrect_state {
 sub _process_running_incorrect_state{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "RUNNING BUILDS IN INCORRECT STATE\n";
-    for my $build (@{$self->_running_incorrect_state}){
+    for my $build (@{$self->_category_hash->{_running_incorrect_state}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -269,11 +272,11 @@ sub _process_running_incorrect_state{
 sub _process_running_email_user{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "EMAIL ABOUT RUNNING BUILDS 3-9 DAYS OLD\n";
-    for my $build (@{$self->_running_email_user}){
+    for my $build (@{$self->_category_hash->{_running_email_user}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -281,11 +284,11 @@ sub _process_running_email_user{
 sub _process_running_kill_and_email_user{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "KILL AND EMAIL ABOUT RUNNING BUILDS 10+ DAYS OLD\n";
-    for my $build (@{$self->_running_kill_and_email_user}){
+    for my $build (@{$self->_category_hash->{_running_kill_and_email_user}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -293,11 +296,11 @@ sub _process_running_kill_and_email_user{
 sub _process_succeeded_abandon_builds{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "SUCCEEDED BUILD WITH OLD FAILED BUILD TO ABANDON\n";
-    for my $build (@{$self->_succeeded_abandon_builds}){
+    for my $build (@{$self->_category_hash->{_succeeded_abandon_builds}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -305,11 +308,11 @@ sub _process_succeeded_abandon_builds{
 sub _process_succeeded_eviscerate_builds{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "SUCCEEDED BUILD WITH OLD SUCCEEDED BUILD TO EVISCERATE\n";
-    for my $build (@{$self->_succeeded_eviscerate_builds}){
+    for my $build (@{$self->_category_hash->{_succeeded_eviscerate_builds}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -317,11 +320,11 @@ sub _process_succeeded_eviscerate_builds{
 sub _process_succeeded_kill_running_builds{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "SUCCEEDED BUILD WITH OLD RUNNING BUILD TO KILL\n";
-    for my $build (@{$self->_succeeded_kill_running_builds}){
+    for my $build (@{$self->_category_hash->{_succeeded_kill_running_builds}}){
         print join("\t", $build->model->id, $build->id, $build->date_scheduled, $build->status), "\n"; 
     }
 }
@@ -329,11 +332,11 @@ sub _process_succeeded_kill_running_builds{
 sub _process_abandoned_start_new_builds{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "START BUILD ON MODEL WITH ABANDONED BUILD\n";
-    for my $model (@{$self->_abandoned_start_new_builds}){
+    for my $model (@{$self->_category_hash->{_abandoned_start_new_builds}}){
         print join("\t", $model->id, $model->creation_date), "\n"; 
     }
 }
@@ -341,11 +344,11 @@ sub _process_abandoned_start_new_builds{
 sub _process_failed_start_new_builds{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "START BUILD ON MODEL WITH SINGLE FAILED BUILD\n";
-    for my $model (@{$self->_failed_start_new_builds}){
+    for my $model (@{$self->_category_hash->{_failed_start_new_builds}}){
         print join("\t", $model->id, $model->creation_date), "\n"; 
     }
 }
@@ -353,11 +356,11 @@ sub _process_failed_start_new_builds{
 sub _process_failed_flag_for_manual_attention{
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "FLAG MODEL WITH TWO FAILED BUILDS IN A ROW FOR MANUAL ATTENTION\n";
-    for my $model (@{$self->_failed_flag_for_manual_attention}){
+    for my $model (@{$self->_category_hash->{_failed_flag_for_manual_attention}}){
         print join("\t", $model->id, $model->creation_date), "\n"; 
     }
 }
@@ -365,11 +368,11 @@ sub _process_failed_flag_for_manual_attention{
 sub _process_preserved {
     my $self = shift;
     if($self->take_action){
-        #TODO: iterate the bucket and take appropriate action 
+        #TODO: iterate the category and take appropriate action 
     }
 
     print "IGNORE MODEL WITH PRESERVED BUILD\n";
-    for my $model (@{$self->_preserved}){
+    for my $model (@{$self->_category_hash->{_preserved}}){
         print join("\t", $model->id, $model->creation_date), "\n"; 
     }
 }
@@ -380,86 +383,13 @@ sub _age_in_days {
     return $age_in_days;
 }
 
-sub _bucket_names{
-    my @buckets = qw(_none_email_user _none_kill_and_email_user _scheduled_incorrect_state _running_incorrect_state _running_email_user _running_kill_and_email_user _succeeded_abandon_builds _succeeded_eviscerate_builds _succeeded_kill_running_builds _abandoned_start_new_builds _failed_start_new_builds _failed_flag_for_manual_attention _preserved);
-    return @buckets;
+sub _category_names{
+    my @categories = qw(_none_email_user _none_kill_and_email_user _scheduled_incorrect_state _running_incorrect_state _running_email_user _running_kill_and_email_user _succeeded_abandon_builds _succeeded_eviscerate_builds _succeeded_kill_running_builds _abandoned_start_new_builds _failed_start_new_builds _failed_flag_for_manual_attention _preserved);
+    return @categories;
 }
 
-sub _none_email_user {
+sub _category_hash{
     my ($self, $hash_ref) = @_;
-    $self->{_none_email_user} = $hash_ref if $hash_ref;
-    return $self->{_none_email_user};
-}
-
-sub _none_kill_and_email_user {
-    my ($self, $hash_ref) = @_;
-    $self->{_none_kill_and_email_user} = $hash_ref if $hash_ref;
-    return $self->{_none_kill_and_email_user};
-}
-
-sub _scheduled_incorrect_state {
-    my ($self, $hash_ref) = @_;
-    $self->{_scheduled_incorrect_state} = $hash_ref if $hash_ref;
-    return $self->{_scheduled_incorrect_state};
-}
-
-
-sub _running_incorrect_state {
-    my ($self, $hash_ref) = @_;
-    $self->{_running_incorrect_state} = $hash_ref if $hash_ref;
-    return $self->{_running_incorrect_state};
-}
-
-sub _running_email_user {
-    my ($self, $hash_ref) = @_;
-    $self->{_running_email_user} = $hash_ref if $hash_ref;
-    return $self->{_running_email_user};
-}
-
-sub _running_kill_and_email_user {
-    my ($self, $hash_ref) = @_;
-    $self->{_running_kill_and_email_user} = $hash_ref if $hash_ref;
-    return $self->{_running_kill_and_email_user};
-}
-
-sub _succeeded_abandon_builds {
-    my ($self, $hash_ref) = @_;
-    $self->{_succeeded_abandon_builds} = $hash_ref if $hash_ref;
-    return $self->{_succeeded_abandon_builds};
-}
-
-sub _succeeded_eviscerate_builds {
-    my ($self, $hash_ref) = @_;
-    $self->{_succeeded_eviscerate_builds} = $hash_ref if $hash_ref;
-    return $self->{_succeeded_eviscerate_builds};
-}
-
-sub _succeeded_kill_running_builds {
-    my ($self, $hash_ref) = @_;
-    $self->{_succeeded_kill_running_builds} = $hash_ref if $hash_ref;
-    return $self->{_succeeded_kill_running_builds};
-}
-
-sub _abandoned_start_new_builds {
-    my ($self, $hash_ref) = @_;
-    $self->{_abandoned_start_new_builds} = $hash_ref if $hash_ref;
-    return $self->{_abandoned_start_new_builds};
-}
-
-sub _failed_start_new_builds {
-    my ($self, $hash_ref) = @_;
-    $self->{_failed_start_new_builds} = $hash_ref if $hash_ref;
-    return $self->{_failed_start_new_builds};
-}
-
-sub _failed_flag_for_manual_attention {
-    my ($self, $hash_ref) = @_;
-    $self->{_failed_flag_for_manual_attention} = $hash_ref if $hash_ref;
-    return $self->{_failed_flag_for_manual_attention};
-}
-
-sub _preserved{
-    my ($self, $hash_ref) = @_;
-    $self->{_preserved} = $hash_ref if $hash_ref;
-    return $self->{_preserved};
+    $self->{_category_hash} = $hash_ref if $hash_ref;
+    return $self->{_category_hash};
 }
