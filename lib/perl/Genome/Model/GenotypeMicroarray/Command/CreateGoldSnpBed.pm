4daa6a59e4f0b7930cbc4500e87597805b349965
Brian Derickson <bdericks@genome.wustl.edu>
2011-03-10 13:02:32 -0600

    create gold snp bed command can now just take a build id and figure out all the params it needs to run. existing params (input file, output file, reference) are now optional

--- lib/perl/Genome/Model/GenotypeMicroarray/Command/CreateGoldSnpBed.pm
+++ lib/perl/Genome/Model/GenotypeMicroarray/Command/CreateGoldSnpBed.pm
@@ -12,22 +12,37 @@ class Genome::Model::GenotypeMicroarray::Command::CreateGoldSnpBed {
     has_input => [
         input_file => {
             is => 'File',
+            is_optional => 1,
             doc => 'The input file Genotype Microarray file to convert.',
             shell_args_position => 1,
         },
         output_file => {
             is => 'File',
+            is_optional => 1,
             doc => 'The output BED file to write.',
             shell_args_position => 2,
         },
         reference => {
             is => 'Genome::Model::Build::ImportedReferenceSequence',
+            is_optional => 1,
             id_by => 'reference_id',
             doc => 'The reference sequence build id to use',
             shell_args_position => 3,
         },
         reference_id => {
             is => 'Integer',
+            is_optional => 1,
+        },
+        build_id => {
+            is => 'Number',
+            is_optional => 1,
+            doc => 'ID of genotype microarray build',
+        },
+        build => {
+            is => 'Genome::Model::Build::GenotypeMicroarray',
+            is_optional => 1,
+            id_by => 'build_id',
+            doc => 'GenotypeMicroarray build',
         },
     ],
 };
@@ -36,6 +51,17 @@ sub execute {
     my $self = shift;
     $self->{bases_files} = {};
 
+    my ($input_file, $output_file, $reference) = ($self->input_file, $self->output_file, $self->reference);
+    unless (defined $input_file and defined $output_file and defined $reference) {
+        unless ($self->build) {
+            Carp::confess 'Must give either a build id or an input file, output file, and reference id!';
+        }
+
+        $input_file = $self->build->formatted_genotype_file_path;
+        $output_file = $self->build->snvs_bed;
+        $reference = $self->build->model->reference_sequence_build;
+    }
+
     # Unfortunately, some of these genotype files seem to be unsorted.
     # It would be better sort first, so the reference sequence can be
     # accessed sequentially. However, joinx sort can't deal with -- in the
@@ -43,19 +69,19 @@ sub execute {
     # _convert_file filter those out then sort after.
 
     my $tmpfile = Genome::Sys->create_temp_file_path;
-    my $infile = $self->input_file;
+    my $infile = $input_file;
     my $ifh = new IO::File("<$infile") || die "Failed to open input file $infile";
     my $ofh = new IO::File(">$tmpfile") || die "Failed to open output file $tmpfile";
-    my $rv = $self->_convert_file($self->reference, $ifh, $ofh);
+    my $rv = $self->_convert_file($reference, $ifh, $ofh);
     $ifh->close();
     $ofh->close();
 
     my $sort_cmd = Genome::Model::Tools::Joinx::Sort->create(
         input_files => [$tmpfile],
-        output_file => $self->output_file,
+        output_file => $output_file,
     );
     if (!$sort_cmd->execute) {
-        $self->error_message("Failed to sort input genotype file " . $self->input_file);
+        $self->error_message("Failed to sort input genotype file " . $input_file);
         return;
     }
 
