493522592ef44dd247680cd3e2c7784192254599
Brian Derickson <bdericks@genome.wustl.edu>
2011-03-14 18:14:45 -0500

    added sample_id to library and prettied up class definition

--- lib/perl/Genome/Library.pm
+++ lib/perl/Genome/Library.pm
@@ -8,21 +8,56 @@ class Genome::Library {
     is => ['Genome::Notable'],
     table_name => 'GSC.LIBRARY_SUMMARY',
     id_by => [
-        library_id          => { is => 'Number', len => 20, column_name => 'LIBRARY_ID', },
+        library_id => { 
+            is => 'Number', 
+            len => 20, 
+            column_name => 'LIBRARY_ID', 
+        },
     ],
     has => [
-        name                => { is => 'Text', len => 64, column_name => 'FULL_NAME', doc => 'Name of the library. Usually has the sample name and an extension.', },
-        sample              => { is => 'Genome::Sample', id_by => 'sample_id', doc => 'Sample that this library came from.', },
-        sample_name         => { is => 'Text', via => 'sample', to => 'name' },
+        name => { 
+            is => 'Text', 
+            len => 64, 
+            column_name => 'FULL_NAME', 
+            doc => 'Name of the library. Usually has the sample name and an extension.', 
+        },
+        sample_id => { 
+            is => 'Text', 
+            column_name => 'SAMPLE_ID',
+        },
+        sample => { 
+            is => 'Genome::Sample', 
+            id_by => 'sample_id', 
+            doc => 'Sample that this library came from.', 
+        },
+        sample_name => { 
+            is => 'Text', 
+            via => 'sample', 
+            to => 'name' 
+        },
     ],
     has_optional => [
-        fragment_size_range     => { is => 'Text', len => 64, 
-                                    column_name => 'LIBRARY_INSERT_SIZE',
-                                    doc => 'intended size range of fragments from library construction' },
-        taxon_id                => { is => 'Number', via => 'sample', },
-        taxon                   => { is => 'Genome::Taxon', via => 'sample', },
-        species_name            => { is => 'Text', via => 'taxon', },
-        protocol_name           => { is_transient => 1, is => 'Text', },
+        fragment_size_range => { 
+            is => 'Text', len => 64, 
+            column_name => 'LIBRARY_INSERT_SIZE',
+            doc => 'intended size range of fragments from library construction' 
+        },
+        taxon_id => { 
+            is => 'Number', 
+            via => 'sample', 
+        },
+        taxon => { 
+            is => 'Genome::Taxon', 
+            via => 'sample', 
+        },
+        species_name => { 
+            is => 'Text', 
+            via => 'taxon', 
+        },
+        protocol_name => { 
+            is_transient => 1, 
+            is => 'Text', 
+        },
     ],
     data_source => 'Genome::DataSource::GMSchema',
 };
