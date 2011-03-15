ec4998508609bf4ce1629c4b1ffdc6416f473035
Brian Derickson <bdericks@genome.wustl.edu>
2011-03-15 16:26:28 -0500

    added allocation, file retrieval, etc to subject

--- lib/perl/Genome/Subject.pm
+++ lib/perl/Genome/Subject.pm
@@ -42,13 +42,41 @@ class Genome::Subject {
             is_mutable => 1,
             where => [ attribute_label => 'nomenclature', nomenclature => 'WUGC' ],
         },
+        disk_allocation => {
+            is => 'Genome::Disk::Allocation', 
+            calculate_from => [ 'subclass_name', 'id' ],
+            calculate => q{
+                my ($class, $id) = @_;
+                my $disk_allocation = Genome::Disk::Allocation->get(
+                    owner_class_name => $class,
+                    owner_id => $id,
+                );
+                return $disk_allocation;
+            },
+        },
+        data_directory => { 
+            is => 'Text', 
+            calculate_from => 'disk_allocation',
+            calculate => q{
+                my $disk_allocation = shift;
+                return unless $disk_allocation;
+                return $disk_allocation->absolute_path;
+            },
+        },
     ],
     table_name => 'GENOME_SUBJECT',
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
-    doc => 'Contains all information about a particular subject (library, sample, etc)',
+    doc => 'Contains all information about a particular subject (sample, individual, taxon, etc)',
 };
 
+sub __display_name__ {
+    my $self = shift;
+    my $name = $self->name . ' ' if defined $self->name;
+    $name .= '(' . $self->id . ')';
+    return $name;
+}
+    
 sub create {
     my ($class, %params) = @_;
 
@@ -79,6 +107,79 @@ sub create {
     }
 
     return $self;
+}
+
+sub add_file {
+    my ($self, $file) = @_;
+
+    $self->status_message('Add file to '. $self->__display_name__);
+
+    Carp::confess('No file to add') if not $file;
+    my $size = -s $file;
+    Carp::confess("File ($file) to add does not have any size") if not $size;
+    my $base_name = File::Basename::basename($file);
+    Carp::confess("Could not get basename for file ($file)") if not $base_name;
+    
+    my $disk_allocation = $self->disk_allocation;
+    if ( not $disk_allocation ) {
+        # Create
+        $disk_allocation = Genome::Disk::Allocation->allocate(
+            disk_group_name => 'info_genome_models',
+            allocation_path => '/model_data/'.$self->id,
+            kilobytes_requested => $size,
+            owner_class_name => $self->class,
+            owner_id => $self->id
+        );
+        if ( not $disk_allocation ) {
+            Carp::confess('Failed to create disk allocation to add file');
+        }
+    }
+    else { 
+        # Make sure we don't overwrite
+        if ( grep { $base_name eq $_ } map { File::Basename::basename($_) } glob($disk_allocation->absolute_path.'/*') ) {
+            Carp::confess("File ($base_name) to add already exists in path (".$disk_allocation->absolute_path.")");
+        }
+        # Reallocate w/ move to accomodate the file
+        my $realloc = eval{
+            $disk_allocation->reallocate(
+                kilobytes_requested => $disk_allocation->kilobytes_requested + $size,
+                allow_reallocate_with_move => 1,
+            );
+        };
+        if ( not $realloc ) {
+            Carp::confess("Cannot reallocate (".$disk_allocation->id.") to accomadate the file ($file)");
+        }
+    }
+
+    my $absolute_path = $disk_allocation->absolute_path;
+    if ( not -d $absolute_path ){
+        Carp::confess('Absolute path does not exist for disk allocation: '.$disk_allocation->id);
+    }
+    my $to = $absolute_path.'/'.$base_name;
+    
+    $self->status_message("Copy $file to $to");
+    my $copy = File::Copy::copy($file, $to);
+    if ( not $copy ) {
+        Carp::confess('Copy of file failed');
+    }
+
+    my $new_size = -s $to;
+    if ( $new_size != $size ) {
+        Carp::confess("Copy of file ($file) succeeded, but file ($to) has different size.");
+    }
+
+    $self->status_message('Add file...OK');
+
+    return 1;
+}
+
+sub get_files {
+    my $self = shift;
+
+    my $disk_allocation = $self->disk_allocation;
+    return if not $disk_allocation;
+
+    return glob($disk_allocation->absolute_path.'/*');
 }
 
 1;
