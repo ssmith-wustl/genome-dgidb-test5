e7465b0c89960b761a7beaa52afdf5a11aeb3027
Justin Lolofie <jlolofie@genome.wustl.edu>
2011-03-19 10:37:29 -0500

    processing profile view had constant

--- lib/perl/Genome/ProcessingProfile/View/Solr/Xml.pm
+++ lib/perl/Genome/ProcessingProfile/View/Solr/Xml.pm
@@ -7,7 +7,7 @@ use Genome;
 
 class Genome::ProcessingProfile::View::Solr::Xml {
     is => 'Genome::View::Solr::Xml',
-    has_constant => [
+    has => [
         type => {
             is => 'Text',
             default => 'processing_profile'
