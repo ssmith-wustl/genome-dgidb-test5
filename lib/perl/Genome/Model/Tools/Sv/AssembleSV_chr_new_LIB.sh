cdb2d76a58cb5017d8215bb7e3387932cc0fd9ce
Scott Smith <ssmith@genome.wustl.edu>
2011-03-25 09:56:38 -0500

    Debugged. 032511

--- lib/perl/Genome/Model/Tools/Sv/AssembleSV_chr_new_LIB.sh
+++ lib/perl/Genome/Model/Tools/Sv/AssembleSV_chr_new_LIB.sh
@@ -3,7 +3,12 @@ ID=$1;  #Sample ID, e.g., BRC6, AML2
 LIB=$2;  #Normal Libraries
 NOVO=$3;
 DIR=$4;
-MOUSE=$5;
+if [ $# == 4 ]; then
+    MOUSE=0;
+else
+    MOUSE=$5;
+fi
+
 cd ${DIR};
 PATH_=`pwd`;
 echo $PATH_;
@@ -82,10 +87,10 @@ for s in normal tumor; do
                 #K=`bjobs -J cf${ID}ctx${i}_${s}_NEW -u all`;
                 #if [ "$K" == "" ]; then
                 #bsub -q apipe -N -u $USER@watson.wustl.edu -M 8000000 -R "select[type=LINUX64] select[mem>8000] rusage[mem=8000]" -o $f.csv -e $f.log -J cf${ID}ctx${i}_$s /gscuser/xfan/git/genome/lib/perl/Genome/Model/Tools/Sv/SV_assembly_pipeline/AssemblyValidation.pl -c $i -f $f.fasta -r $f.cm -Q 40 -L $LIB ${ID}$NOVO.ctx.annot $s.bam; 
-                if [ $MOUSE == 0 ]; then
+                if [ $MOUSE == 1 ]; then
                     bsub -q apipe -N -u $USER@watson.wustl.edu -M 8000000 -R "select[type==LINUX64] select[mem>8000] rusage[mem=8000]" -e $f.log -J cf${ID}ctx${i}_${s}_NEW "gmt sv assembly-validation --bam-files $s.bam --sv-file ${ID}$NOVO.ctx.annot --specify-chr $i --breakpoint-seq-file ${f}.fasta --cm-aln-file ${f}.cm --min-breakdancer-score 40 --intermediate-read-dir ${PATH_}/assembly_inter_$s --output-file ${f}.csv --min-size-of-confirm-asm-sv 10 --skip-libraries ${LIB} --assemble-mouse --reference-file /gscmnt/839/info/medseq/reference_sequences/NCBI-mouse-build37/all_sequences.fa";
                 fi
-                if [ $MOUSE != 0 ]; then
+                if [ $MOUSE != 1 ]; then
                     bsub -q apipe -N -u $USER@watson.wustl.edu -M 8000000 -R "select[type==LINUX64] select[mem>8000] rusage[mem=8000]" -e $f.log -J cf${ID}ctx${i}_${s}_NEW "gmt sv assembly-validation --bam-files $s.bam --sv-file ${ID}$NOVO.ctx.annot --specify-chr $i --breakpoint-seq-file ${f}.fasta --cm-aln-file ${f}.cm --min-breakdancer-score 40 --intermediate-read-dir ${PATH_}/assembly_inter_$s --output-file ${f}.csv --min-size-of-confirm-asm-sv 10 --skip-libraries ${LIB}";
                 fi
 
