e6945ef4b7cb26e870507c048e9cd776e3a6000c
Justin Lolofie <jlolofie@genome.wustl.edu>
2011-03-29 10:42:20 -0500

    search loading scripts chunking workorder xml icon typo

--- genome-webserver/solr/scripts/gcsearch_load_from_db.sh
+++ genome-webserver/solr/scripts/gcsearch_load_from_db.sh
@@ -5,11 +5,25 @@ set -o pipefail  # fail if any command in a pipe fails
 set -o nounset   # fail if an env var is used but unset
 
 
-bsub -u jlolofie@genome.wustl.edu -q apipe perl /gsc/scripts/bin/gcsearch_load_from_db --add processing_profile,model,model_group --lock 1
-bsub -u jlolofie@genome.wustl.edu -q apipe perl /gsc/scripts/bin/gcsearch_load_from_db --add individual,flowcell,sample,population_group --lock 2
-bsub -u jlolofie@genome.wustl.edu -q apipe perl /gsc/scripts/bin/gcsearch_load_from_db --add taxon,libary,disk_group,disk_volume,work_order --lock 3
+# updating solr-dev
+# TODO: disk_group, disk_volume, flowcell
 
 
+#bsub -u jlolofie@genome.wustl.edu -q apipe GENOME_DEV_MODE=1 perl /gsc/scripts/bin/gcsearch_load_from_db --add individual,sample,population_group,taxon --lock 2
+#bsub -u jlolofie@genome.wustl.edu -q apipe GENOME_DEV_MODE=1 perl /gsc/scripts/bin/gcsearch_load_from_db --add processing_profile,model_group,library,work_order --lock 3
+
+
+for i in `seq 0 20`; do
+echo "loading flowcell chunk number $i (21 total)"
+GENOME_DEV_MODE=1 perl /gscuser/jlolofie/dev/git/clean_genome/genome-webserver/solr/scripts/gcsearch_load_from_db.pl --add flowcell --lock 4 --chunk $i
+done
+
+
+#for i in `seq 0 20`; do
+#echo "loading model chunk number $i (21 total)"
+##GENOME_DEV_MODE=1 perl /gsc/scripts/bin/gcsearch_load_from_db --add model --lock 1 --chunk $i
+#GENOME_DEV_MODE=1 perl /gscuser/jlolofie/dev/git/clean_genome/genome-webserver/solr/scripts/gcsearch_load_from_db.pl --add model --lock 1 --chunk $i
+#done
 
 
 
