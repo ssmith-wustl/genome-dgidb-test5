fbf22ed7d0db47a7631b70dae04ade26de06a369
Thomas Mooney <tmooney@genome.wustl.edu>
2011-04-06 13:56:03 -0500

    Write the readcount file out to a file and then read it in line by line.
    This should reduce the memory usage for readcounts to a third. If the
    datasets are still too large, the "throw everything in a hash" approach
    may need to be revisited.

--- lib/perl/Genome/Model/Tools/Somatic/FilterFalseIndels.pm
+++ lib/perl/Genome/Model/Tools/Somatic/FilterFalseIndels.pm
@@ -296,24 +296,26 @@ sub execute {
     print "Running BAM Readcounts...\n";
 #    my $cmd = readcount_program() . " -b 15 " . $self->bam_file . " -l $temp_path";
     my $cmd = readcount_program() . " -b 1 " . $self->bam_file . " -l $temp_path";
-    my $readcounts = `$cmd 2>/dev/null`;
-    chomp($readcounts) if($readcounts);
+    my $readcount_path = $self->output_file . ".readcounts";
+
+    $cmd .= '> $readcount_path 2> /dev/null';
+    Genome::Sys->shellcmd(
+        cmd => $cmd,
+        input_files => [$self->bam_file],
+        output_files => [$readcount_path],
+    );
 
     ## Load the results of the readcounts ##
-    
     my %readcounts_by_position = ();
 
-    open(OUTTEMP, ">" . $self->output_file . ".readcounts") or die "Can't open output file: !$\n";
-
-    my @readcounts = split(/\n/, $readcounts);
-    foreach my $rc_line (@readcounts)
+    my $readcount_fh = Genome::Sys->open_file_for_reading($readcount_path);
+    while(my $rc_line = $readcount_fh->readline)
     {
-	print OUTTEMP "$rc_line\n";
+     chomp($rc_line);
 	(my $chrom, my $pos) = split(/\t/, $rc_line);
 	$readcounts_by_position{"$chrom\t$pos"} = $rc_line;
    }
 
-    close(OUTTEMP);
     print "Readcounts loaded\n";
 
     
