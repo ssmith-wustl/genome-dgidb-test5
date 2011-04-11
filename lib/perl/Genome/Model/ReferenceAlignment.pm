49e8b6e084bd70e798038311222eadb67d1aba58
Nathan Nutter <nnutter@genome.wustl.edu>
2011-04-11 11:36:45 -0500

    add target_region_set_name to ref-align models

--- lib/perl/Genome/Model/ReferenceAlignment.pm
+++ lib/perl/Genome/Model/ReferenceAlignment.pm
@@ -201,6 +201,8 @@ class Genome::Model::ReferenceAlignment {
         },
         filter_ruleset_name   => { via => 'processing_profile' },
         filter_ruleset_params => { via => 'processing_profile' },
+        target_region_set_value     => { is_many => 1, is_mutable => 1, is => 'UR::Value', via => 'inputs', to => 'value', where => [ name => 'target_region_set_name'] },
+        target_region_set_name      => { via => 'target_region_set_value', to => 'id', },
     ],
     doc => 'A genome model produced by aligning DNA reads to a reference sequence.' 
 };
