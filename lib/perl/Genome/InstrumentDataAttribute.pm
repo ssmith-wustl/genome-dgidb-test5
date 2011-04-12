cb2bd77b7c5d30c59d7dcaa2e5d2e0f0236df8be
Brian Derickson <bdericks@genome.wustl.edu>
2011-04-12 10:27:15 -0500

    moved nomenclature into the id_by block and overrode create to give it a default value

--- lib/perl/Genome/InstrumentDataAttribute.pm
+++ lib/perl/Genome/InstrumentDataAttribute.pm
@@ -19,14 +19,11 @@ class Genome::InstrumentDataAttribute {
         attribute_value => {
             is => 'Text',
         },
-    ],
-    has => [
-        # TODO Should be in id_by, but currently can't have a property in id_by that
-        # also has a default value
         nomenclature => {
             is => 'Text',
-            default => 'WUGC',
         },
+    ],
+    has => [
         instrument_data => {
             is => 'Genome::InstrumentData',
             id_by => 'instrument_data_id',
@@ -34,5 +31,16 @@ class Genome::InstrumentDataAttribute {
     ],
 };
 
+sub create {
+    my $class = shift;
+    my $bx = $class->define_boolexpr(@_);
+    # TODO This is a workaround that allows nomenclature to be in the id_by block
+    # and have a default value. Doing so in the class definition doesn't work due
+    # to some sort of UR bug that Tony is aware of.
+    unless ($bx->specifies_value_for('nomenclature')) {
+        $bx = $bx->add_filter('nomenclature' => 'WUGC');
+    }
+    return $class->SUPER::create($bx);
+}
 1;
 
