e370fb5f7f87589920fb144306a301a8666578bc
Justin Lolofie <jlolofie@genome.wustl.edu>
2011-04-13 10:34:06 -0500

    Crud was having a problem with ProjectPart entity/entity_id set up project / project-part commands

---
+++ lib/perl/Genome/ProjectPart/Command.pm
@@ -0,0 +1,26 @@
+package Genome::ProjectPart::Command;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::ProjectPart::Command {
+    is => 'Command',
+    is_abstract => 1,
+    doc => 'work with ProjectParts',
+};
+
+use Genome::Command::Crud;
+
+Genome::Command::Crud->init_sub_commands(
+    target_class => 'Genome::ProjectPart',
+    target_name => 'project-part',
+    list => { show => 'project_id,entity_class_name,entity_id,label,role' },
+    update => { only_if_null => 1, },
+    delete => { do_not_init => 1, },
+);
+
+1;
+
+
