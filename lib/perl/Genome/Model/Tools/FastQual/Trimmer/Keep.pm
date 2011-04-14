bebe49f5a88201582e68c7d2cef3a5d925f5be15
Eddie Belter <ebelter@genome.wustl.edu>
2011-04-14 15:12:35 -0500

    FastQual: added trimmers for remove keep

---
+++ lib/perl/Genome/Model/Tools/FastQual/Trimmer/Keep.pm
@@ -0,0 +1,46 @@
+package Genome::Model::Tools::FastQual::Trimmer::Keep;
+
+use strict;
+use warnings;
+
+use Genome;            
+
+use Regexp::Common;
+
+class Genome::Model::Tools::FastQual::Trimmer::Keep {
+    is => 'Genome::Model::Tools::FastQual::Trimmer',
+    has => [
+        length => {
+            is => 'Integer',
+            doc => 'The number of bases to keep',
+        },    
+     ],
+};
+
+sub __errors__ {
+    my $self = shift;
+    my @errors = $self->SUPER::__errors__(@_);
+    return @errors if @errors;
+    if ( $self->length !~ /^$RE{num}{int}$/ or $self->length < 1 ) {
+        push @errors, UR::Object::Tag->create(
+            type => 'invalid',
+            properties => [qw/ length /],
+            desc => 'Keep length (length) is not a integer greater than 0 => '.$self->length,
+        );
+    }
+    return @errors;
+}
+
+sub _trim {
+    my ($self, $seqs) = @_;
+
+    for my $seq (@$seqs) {
+        $seq->{seq} = substr($seq->{seq}, 0, $self->length);
+        $seq->{qual} = substr($seq->{qual},0, $self->length);
+    }
+
+    return $seqs;
+}
+
+1;
+
