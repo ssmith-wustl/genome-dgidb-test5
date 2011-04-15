c00eab8d44b703ee39e46da6b5ab75c95b476732
Nathan Nutter <nnutter@genome.wustl.edu>
2011-04-15 14:20:21 -0500

    add Genome::Site::WUGC::SnpConcordance

---
+++ lib/perl/Genome/Site/WUGC/SnpConcordance.pm
@@ -0,0 +1,59 @@
+package Genome::Site::WUGC::SnpConcordance;
+
+use strict;
+use warnings;
+use Genome;
+
+class Genome::Site::WUGC::SnpConcordance {
+    table_name => 'GSC.SNP_CONCORDANCE',
+    data_source => 'Genome::DataSource::GMSchema',
+    id_by => [
+        creation_event_id => { is => 'Number' },
+    ],
+    has => [
+        seq_id => { is => 'Number' },
+        replicate_seq_id => { is => 'Number' },
+    ],
+    has_optional => [
+        no_data => { is => 'Number' },
+        match => { is => 'Number' },
+        no_data_1 => { is => 'Number' },
+        no_data_2 => { is => 'Number' },
+        reverse_complemented => { is => 'Number' },
+        loh => { is => 'Number' },
+        not_match => { is => 'Number' },
+        total => { is => 'Number' },
+    ],
+};
+
+
+sub match_percent {
+    my $self = shift;
+    
+    my $match = $self->match;
+    my $total = $self->total;
+
+    my $match_percent;
+    if (defined $match && $total) { # match could be zero but total shouldn't
+        $match_percent = $match / $total;
+    }
+
+    return $match_percent;
+}
+
+
+sub is_external_comparison {
+    my $self = shift;
+    my $replicate_seq_id = $self->replicate_seq_id;
+
+    my $genotype;
+    if ($replicate_seq_id) {
+        $genotype = Genome::Site::WUGC::ExternalGenotyping->get($replicate_seq_id);
+    }
+
+    return ($genotype ? 1 : 0);
+}
+
+
+1;
+
