b01b82f6da6b9558ee2762e32c0fa53163ef75d1
Jim Weible <jweible@genome.wustl.edu>
2011-04-29 13:12:23 -0500

    New version that syncs based on gsc.organism_sample
    New version that syncs from gsc.organism_sample.  This should only be used
    until the trigger is updated

--- lib/perl/Genome/Site/WUGC/ReconcileSampleGenotypeData.pm
+++ lib/perl/Genome/Site/WUGC/ReconcileSampleGenotypeData.pm
@@ -6,24 +6,28 @@ use Genome;
 
 class Genome::Site::WUGC::ReconcileSampleGenotypeData {
     is => 'Genome::Command::Base',
-    doc => '',
+    doc => 'Updates the default_genotype_data column for Genome::Samples using data from the Organism Sample table',
 };
 
 sub execute {
     my $self = shift;
-
-    my @misc_updates = sort{$a->edit_date cmp $b->edit_date} Genome::MiscUpdate->get(
-                                                                                    is_reconciled => '0', 
-                                                                                    subject_class_name => 'gsc.organism_sample', 
-                                                                                    description => 'UPDATE', 
-                                                                                    subject_property_name => 'DEFAULT_GENOTYPE_SEQ_ID',
-                                                                                    );
-    for my $update (@misc_updates){
-        my $sample = Genome::Sample->get($update->subject_id);
-        print "No Genome::Sample for subject: " , $update->subject_id , "on update: ", $update->id, " , skipping\n" and next unless $sample;
-        my $new_value = $update->new_value;
-        $sample->set_default_genotype_data($new_value);
-        $update->is_reconciled(1);
+    my @organism_samples = Genome::Site::WUGC::Sample->get();
+    my @imported_instrument_data = Genome::InstrumentData::Imported->get();
+    @organism_samples = grep($_->default_genotype_seq_id, @organism_samples);
+    for my $organism_sample (@organism_samples){
+        my $genome_sample = Genome::Sample->get($organism_sample->id);
+        print "No Genome::Sample for organism_sample: ", $organism_sample->id, "\n" and next unless $genome_sample;
+        next if defined $genome_sample->default_genotype_data and $genome_sample->default_genotype_data->id eq $organism_sample->default_genotype_seq_id;
+        my $genotype_instrument_data = Genome::InstrumentData::Imported->get($organism_sample->default_genotype_seq_id);
+        print "No imported instrument data for id: ", $organism_sample->default_genotype_seq_id, "\n" and next unless $genotype_instrument_data;
+        eval{
+            $genome_sample->set_default_genotype_data($genotype_instrument_data);
+        };
+        if($@){
+            print "Failed to set default genotype data: ", $genotype_instrument_data->id, " for Genome::Sample: ", $genome_sample->id, " (err: $@)\n";
+        }else{
+            print "Successfully updated Genome::Sample ", $genome_sample->id, "\n";
+        }
     }
 }
 
