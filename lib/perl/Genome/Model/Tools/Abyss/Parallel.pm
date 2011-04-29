c9a04fea82c3595fefd6c64493a8a4367291a323
Travis Abbott <tabbott@genome.wustl.edu>
2011-04-29 13:28:44 -0500

    changed syntax of abyss k-mer size ranges:
    0..10 step 2 becomes
    0..10:2
    
    for all even integers in [0,10]

--- lib/perl/Genome/Model/Tools/Abyss/Parallel.pm
+++ lib/perl/Genome/Model/Tools/Abyss/Parallel.pm
@@ -81,15 +81,12 @@ sub parse_kmer_range {
     my ($self, $range) = @_;
 
     my @kmer_sizes;
-    if (my ($start, $end, $step) = $range =~ /^(\d+)\.\.(\d+)/) {
+    if (my ($start, $end, $junk, $step) = $range =~ /^(\d+)\.\.(\d+)(:(-{0,1}\d+)|)/) {
         die "invalid kmer size range '$range', end=$end < start=$start. abort." if $end < $start;
-        if (my ($step) = $range =~ / step (.*)/) { # .* instead of \d+ so we can complain about bad input
-            die "invalid kmer size range '$range', step=$step <= 0, abort." if $step <= 0;
-            for (my $i = $start; $i <= $end; $i += $step) {
-                push(@kmer_sizes, $i);
-            }    
-        } else {
-            push(@kmer_sizes, $start..$end); 
+        $step = 1 unless defined $step;
+        die "invalid kmer size range '$range', step=$step <= 0, abort." if $step <= 0;
+        for (my $i = $start; $i <= $end; $i += $step) {
+            push(@kmer_sizes, $i);
         }
     } else {
         die "Invalid number '$range'" unless $range =~ /^\d+$/;
