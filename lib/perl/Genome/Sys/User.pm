0b136395d46c5bc0c4e4310edb884bc044930c78
Justin Lolofie <jlolofie@genome.wustl.edu>
2011-05-02 13:25:59 -0500

    User is backed by a table now

--- lib/perl/Genome/Sys/User.pm
+++ lib/perl/Genome/Sys/User.pm
@@ -1,12 +1,45 @@
 package Genome::Sys::User;
+
 use strict;
 use warnings;
 use Genome;
 
 class Genome::Sys::User {
-    is => 'UR::Value',
-    doc => 'A real or potential system user, identified by username@domain',
+    schema_name => 'GMSchema',
+    data_source => 'Genome::DataSource::GMSchema',
+    table_name => 'genome_sys_user',
+    id_by => [
+        email => { is => 'VARCHAR2', len => 255, column_name => 'EMAIL' },
+    ],
+    has_optional => [
+        name => { is => 'VARCHAR2', len => 64, column_name => 'NAME' },
+        username => {
+            calculate_from => ['email'],
+            calculate => sub { 
+                my ($e) = @_;
+                my ($u) = $e =~ /(.+)\@/; 
+                return $u;
+            }
+        }
+    ]
 };
+
+
+
+sub get {
+
+    my ($self, %p) = @_;
+
+    if (defined($p{'email'}) 
+        && $p{'email'} !~ /\@/) {
+        my $old = $p{'email'};
+        my $new = join('@',$p{'email'},Genome::Config::domain());
+        warn "Trying to get() for '$old' - assuming you meant '$new'";
+        $p{'email'} = $new;
+    }
+
+    return $self->SUPER::get(%p);
+}
 
 1;
 
