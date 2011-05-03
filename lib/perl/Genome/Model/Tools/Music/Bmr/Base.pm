073ab0d38deeb9967973dafb6e32c4ad931bcd2f
Nathan Dees <ndees@wustl.edu>
2011-05-03 17:03:17 -0500

    new Base for modules in music-bmr dir

---
+++ lib/perl/Genome/Model/Tools/Music/Bmr/Base.pm
@@ -0,0 +1,59 @@
+package Genome::Model::Tools::Music::Bmr::Base;
+
+use strict;
+use warnings;
+use Genome;
+
+our $VERSION = $Genome::Model::Tools::Music::VERSION;
+
+class Genome::Model::Tools::Music::Bmr::Base {
+    is => ['Command::V2'],
+    is_abstract => 1,
+    attributes_have => [
+        file_format => {
+            is => 'Text',
+            is_optional => 1,
+        }
+    ],
+    doc => "Mutational Significance In Cancer, BMR Calculations"
+};
+
+sub _doc_copyright_years {
+    (2010,2011);
+}
+
+sub _doc_license {
+    my $self = shift;
+    my (@y) = $self->_doc_copyright_years;  
+    return <<EOS
+Copyright (C) $y[0]-$y[1] Washington University in St. Louis.
+
+It is released under the Lesser GNU Public License (LGPL) version 3.  See the 
+associated LICENSE file in this distribution.
+EOS
+}
+
+sub _doc_authors {
+    return ('',
+        'Cyriac Kandoth, Ph.D.',
+    );
+}
+
+sub _doc_see_also {
+    return ('',
+        'B<genome-music-bmr>(1)',
+        'B<genome-music>(1)',
+        'B<genome>(1)');
+}
+
+sub _doc_manual_body {
+    return shift->help_detail;
+}
+
+sub help_detail {
+    return "This tool is part of the MuSiC suite. See:\n" 
+        . join("\n",shift->_doc_see_also)
+        . "\n";
+}
+
+1;
