8118752d422be99906606e49afdb19caea7c7f92
Rob Long <rlong@genome.wustl.edu>
2011-05-04 10:40:53 -0500

    gmt fast-tier bitmask-to-bed - subtract one from end before writing to bed

--- lib/perl/Genome/Model/Tools/FastTier/BitmaskToBed.pm
+++ lib/perl/Genome/Model/Tools/FastTier/BitmaskToBed.pm
@@ -79,11 +79,10 @@ sub execute {
                 $DB::single=1 if ($start==67167422); 
             }
             elsif($bit) {
-                $start=$index+1;
+                $start=$index;
             }
             elsif(!$bit && $start) {
-                my $end=$index;
-                $start = $start -1;
+                my $end=$index-1;
                 $out_fh->print("$chr\t$start\t$end\n");
                 $start = 0;
             }
