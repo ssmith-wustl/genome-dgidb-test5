f33104f032d416bed6e658b18a324459ce6e309b
Nathan Nutter <nnutter@genome.wustl.edu>
2011-05-10 13:11:10 -0500

    Genotype Microarray use job_dispatch => inline as a workaround for a Workflow bug

--- lib/perl/Genome/ProcessingProfile/GenotypeMicroarray.pm
+++ lib/perl/Genome/ProcessingProfile/GenotypeMicroarray.pm
@@ -4,6 +4,14 @@ use Genome;
 
 class Genome::ProcessingProfile::GenotypeMicroarray {
     is => 'Genome::ProcessingProfile',
+    has => [
+        job_dispatch => {
+            is_constant => 1,
+            is_class_wide => 1,
+            value => 'inline',
+            doc => 'lsf queue to submit jobs or \'inline\' to run them in the launcher'
+        },
+    ],
     has_param => [
         input_format => {
             doc => 'file format, defaults to "wugc", which is currently the only format supported',
