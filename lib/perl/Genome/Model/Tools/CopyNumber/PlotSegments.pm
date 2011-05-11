517b4ccd407b1629baa9cf2165802f7eddd35a9c
Chris Miller <cmiller@genome.wustl.edu>
2011-05-11 10:46:58 -0500

    removing testing param

--- lib/perl/Genome/Model/Tools/CopyNumber/PlotSegments.pm
+++ lib/perl/Genome/Model/Tools/CopyNumber/PlotSegments.pm
@@ -214,6 +214,7 @@ sub convertSegs{
         }
     }
 
+    print STDERR join(",",@newfiles) . "\n";
     return join(",",@newfiles);
 }
 
@@ -239,8 +240,6 @@ sub cnvHmmToCbs{
 	die;
     }
     
-    $newfile = "/tmp/output.dat";
-
     open(OUTFILE,">$newfile") || die "can't open temp segs file for writing ($newfile)\n";
 
 
