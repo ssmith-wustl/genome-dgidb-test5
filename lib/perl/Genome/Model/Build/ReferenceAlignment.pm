0f240f372591157feb465702aaaadfcd88a2d312
Travis Abbott <tabbott@genome.wustl.edu>
2011-05-16 15:40:50 -0500

    add get_alignments as well as get_alignment_bams to refalign builds
    in case you need to get your hands on the AlignmentResult objects
    associated with a build.

--- lib/perl/Genome/Model/Build/ReferenceAlignment.pm
+++ lib/perl/Genome/Model/Build/ReferenceAlignment.pm
@@ -199,11 +199,15 @@ sub compare_snps_file {
     return $self->qc_directory . 'compare_snps';
 }
 
+sub get_alignments {
+    my $self = shift;
+    return map { $self->model->processing_profile->results_for_instrument_data_assignment($_) }
+        $self->instrument_data_assignments;
+}
+
 sub get_alignment_bams {
     my $self = shift;
-    my @alignments = map { $self->model->processing_profile->results_for_instrument_data_assignment($_) }
-        $self->instrument_data_assignments;
-    return map { $_->alignment_bam_file_paths } @alignments;
+    return map { $_->alignment_bam_file_paths } $self->get_alignments;
 }
 
 sub calculate_estimated_kb_usage {
