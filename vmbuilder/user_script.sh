ea9378379e0e2d57446ff0cb0753ac054342003b
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-05-18 15:56:46 -0500

    cleanup to work wherever the repo is checked out to

--- vmbuilder/user_script.sh
+++ vmbuilder/user_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SCRIPT_PATH=`readlink -f $0`
 DIR_PATH=`dirname $SCRIPT_PATH`
+# DIR_PATH is the location of the vmbuilder directory
+GENOME_DIR=`dirname $DIR_PATH`
 
 if [ "$UID" -eq "0" ]
 then
@@ -8,9 +10,16 @@ then
   exit 1
 fi
 
-cp $DIR_PATH/tnsnames.org $HOME/
+# source lsf file if it exists
+cat <<EOF | tee -a $HOME/.bashrc
+if [-f /usr/local/lsf/conf/profile.lsf ]; then
+    . /usr/local/lsf/conf/profile.lsf
+fi
 
-echo "export PERL5LIB=$HOME/genome/lib/perl:\$PERL5LIB" >> $HOME/.bashrc
-echo "export PATH=$HOME/genome/bin:\$PATH" >> $HOME/.bashrc
-echo "export TNS_ADMIN=$HOME" >> $HOME/.bashrc
+EOF
+
+echo "export PERL5LIB=$GENOME_DIR/lib/perl:\$PERL5LIB" >> $HOME/.bashrc
+echo "export PATH=$GENOME_DIR/bin:\$PATH" >> $HOME/.bashrc
+echo "export TNS_ADMIN=$DIR_PATH" >> $HOME/.bashrc
 echo "export ORACLE_HOME=/opt/oracle-instantclient" >> $HOME/.bashrc
+
