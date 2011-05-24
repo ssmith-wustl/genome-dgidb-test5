61330bd9acaab128e7281e711b2f7a4d6bcf3b8f
Brian Derickson <bdericks@genome.wustl.edu>
2011-05-24 15:06:06 -0500

    population groups now have a member hash, which is a hash created by a sorted list of their members' ids. this is useful for finding population groups that contains a certain set of individuals. also updated test to cover these additions.

--- lib/perl/Genome/PopulationGroup.pm
+++ lib/perl/Genome/PopulationGroup.pm
@@ -1,8 +1,3 @@
-# bdericks: Taxon is currently a required parameter of population groups due to the 
-# GSC.POPULATION_GROUP table this class is based on (which is nonnullable). The docs
-# seem to indicate that a population grouping can be arbitrary, which would require
-# that the taxon be defined as something useless. Can we just remove taxon entirely?
-
 package Genome::PopulationGroup;
 
 use strict;
@@ -18,6 +13,39 @@ class Genome::PopulationGroup {
             is_constant => 1, 
             value => 'population group',
         },
+        member_hash => {
+            is => 'Text',
+            via => 'attributes',
+            to => 'attribute_value',
+            where => [ attribute_label => 'member_hash' ],
+            is_mutable => 1,
+            doc => 'Makes it easier to figure out if another group with the exact set of individuals already exists',
+        },
+    ],
+    has_many => [
+        member_ids => {
+            is => 'Number',
+            via => 'attributes',
+            to => 'attribute_value',
+            where => [ attribute_label => 'member' ],
+            is_mutable => 1,
+        },
+        members => { 
+            is => 'Genome::Individual',
+            via => 'attributes',
+            to => '_individual',
+            where => [ attribute_label => 'member' ],
+        },
+        samples => { 
+            is => 'Genome::Sample', 
+            reverse_id_by => 'source',
+        },
+        sample_names => {
+            via => 'samples',
+            to => 'name',
+        },
+    ],
+    has_optional => [
         taxon_id => {
             is => 'Number',
             via => 'attributes',
@@ -31,32 +59,95 @@ class Genome::PopulationGroup {
         },
         species_name => { via => 'taxon' },
     ],
-    has_many => [
-        member_ids => {
-            is => 'Number',
-            via => 'attributes',
-            to => 'attribute_value',
-            where => [ attribute_label => 'member' ],
-            is_mutable => 1,
-        },
-        members => { 
-            is => 'Genome::Individual', 
-            id_by => 'member_ids',
-        },
-        samples => { 
-            is => 'Genome::Sample', 
-            reverse_id_by => 'source',
-        },
-        sample_names => {
-            via => 'samples',
-            to => 'name',
-        },
-    ],
-    doc => 'an defined, possibly arbitrary, group of individual organisms',
+    doc => 'a possibly arbitrary group of individual organisms',
 };
 
-sub common_name { # not in the table, but exepected by views
-    return $_[0]->name;
+sub create {
+    my $class = shift;
+    my $self = $class->SUPER::create(@_);
+    my $member_hash = $self->generate_hash_for_individuals($self->members);
+    $self->member_hash($member_hash);
+    return $self;
+}
+
+# Return any other population groups that have the same member hash as the one provided
+sub existing_population_groups_with_hash {
+    my ($self, $hash) = @_;
+    return Genome::PopulationGroup->get(
+        member_hash => $hash,
+        'id ne' => $self->id,
+    );
+}
+
+# Generate an md5 hash based on the ids of the provided individuals
+sub generate_hash_for_individuals {
+    my ($self, @individuals) = @_;
+    my @ids = sort map { $_->id } @individuals;
+    # Returns a valid answer even if individuals is undef
+    my $hash = Digest::MD5::md5_hex(@ids);
+    return $hash;
+}
+
+# Filter the provided list of individuals to include only unique individuals not already in the group
+sub remove_non_unique_individuals {
+    my ($self, @individuals) = @_;
+
+    my %unique;
+    map { $unique{$_->id} = $_ } @individuals;
+
+    for my $member_id ($self->member_ids) {
+        delete $unique{$member_id} if exists $unique{$member_id};
+    }
+
+    return values %unique;
+}
+
+sub add_member {
+    my ($self, $individual) = @_;
+    return $self->add_members($individual);
+}
+
+# Filters the given list of individuals and adds any that pass filtering to the group
+sub add_members {
+    my ($self, @individuals) = @_;
+    my @addable_individuals = $self->remove_non_unique_individuals(@individuals);
+    return 1 unless @addable_individuals; # If all of the provided individuals are already added/redundant, just do nothing
+
+    for my $addable (@addable_individuals) {
+        my $attribute = Genome::SubjectAttribute->create(
+            attribute_label => 'member',
+            attribute_value => $addable->id,
+            subject_id => $self->id,
+        );
+        unless ($attribute) {
+            Carp::confess "Could not add individual " . $addable->id . " to population group " . $self->id;
+        }
+    }
+
+    my $member_hash = $self->generate_hash_for_individuals($self->members);
+    $self->member_hash($member_hash);
+    return 1;
+}
+
+# Removes members from the group
+sub remove_members {
+    my ($self, @individuals) = @_;
+    my @removed;
+    for my $individual (@individuals) {
+        my $attribute = Genome::SubjectAttribute->get(
+            attribute_label => 'member',
+            attribute_value => $individual->id,
+            subject_id => $self->id,
+        );
+        next unless $attribute;
+        $attribute->delete;
+        push @removed, $individual->id;
+    }
+    return 1 unless @removed; # Not removing anything is not an error, just return
+
+    my $member_hash = $self->generate_hash_for_individuals($self->members);
+    $self->member_hash($member_hash);
+    return 1;
 }
 
 1;
