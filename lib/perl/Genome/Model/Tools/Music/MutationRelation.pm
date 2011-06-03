6ae3bb388a9f0220957f2118f36edc18512ccf41
Cyriac Kandoth <ckandoth@wustl.edu>
2011-06-03 17:07:10 -0500

    Fixed a problem where samples without any mutations are skipped since they are not in the MAF file. For correlation and mutation relation tests, they need to be in the tested set.

--- lib/perl/Genome/Model/Tools/Music/MutationRelation.pm
+++ lib/perl/Genome/Model/Tools/Music/MutationRelation.pm
@@ -10,36 +10,39 @@ use POSIX qw( WIFEXITED );
 our $VERSION = $Genome::Model::Tools::Music::VERSION;
 
 class Genome::Model::Tools::Music::MutationRelation {
-    is => 'Command::V2',                       
-    has_input => [ 
-        output_file => {
-            is => 'Text',
-            doc => "results of mutation-relation tool",
-        },
-        maf_file => { 
-            is => 'Text',
-            doc => "list of mutations in MAF format",
-        },
-        permutations => {
-            is => 'Number',
-            doc => "number of permutations used to determine P-values",
-            is_optional => 1,
-            default => 100,
-        },
-    ],
-    doc => 'Identify relationships between mutated genes.'
+  is => 'Command::V2',
+  has_input => [
+    bam_list => { is => 'Text', doc => "Tab delimited list of BAM files [sample_name, normal_bam, tumor_bam] (See Description)" },
+    maf_file => { is => 'Text', doc => "List of mutations in MAF format" },
+    output_file => { is => 'Text', doc => "Results of mutation-relation tool" },
+    permutations => { is => 'Number', doc => "Number of permutations used to determine P-values", is_optional => 1, default => 100 },
+  ],
+  doc => "Identify relationships between mutated genes.",
 };
 
 sub help_synopsis {
-    return <<EOS
- ... music mutation-relation --maf-file /path/myMAF.tsv --permutations 1000 --output-file /path/mutation_relation.csv
-EOS
+  return <<HELP
+... music mutation-relation --maf-file /path/myMAF.tsv --permutations 1000 --output-file /path/mutation_relation.csv
+HELP
 }
 
 sub help_detail {
-    return <<EOS
-    This module parses a list of mutations in MAF format and attempts to determin relationships among mutated genes. The module employs a correlation test to see whether or not any two genes are mutated concurrently (positive correlation) or exclusively (negative correlation). Because of the possibility of largely varying numbers of mutations present in different genes, P-values are calculated using restricted permutations that take into account the distribution of mutation counts among the samples. In the output file, 'pand' is the P-value for concurrent mutation events, and 'pexc' is the P-value for exclusive mutation events.
-EOS
+  return <<HELP
+This module parses a list of mutations in MAF format and attempts to determin relationships among
+mutated genes. The module employs a correlation test to see whether or not any two genes are
+mutated concurrently (positive correlation) or exclusively (negative correlation). Because of the
+possibility of largely varying numbers of mutations present in different genes, P-values are
+calculated using restricted permutations that take into account the distribution of mutation
+counts among the samples. In the output file, 'pand' is the P-value for concurrent mutation
+events, and 'pexc' is the P-value for exclusive mutation events.
+
+ARGUMENTS:
+--bam-list
+  Provide a file containing sample names and normal/tumor BAM locations for each. Use the tab-
+  delimited format [sample_name normal_bam tumor_bam] per line. This tool only needs sample_name,
+  so all other columns can be skipped. The sample_name must be the same as the tumor sample names
+  used in the MAF file (16th column, with the header Tumor_Sample_Barcode).
+HELP
 }
 
 sub _doc_authors {
@@ -53,12 +56,25 @@ sub execute {
 
     #parse input arguments
     my $self = shift;
+    my $bam_list = $self->bam_list;
+    my $maf_file = $self->maf_file;
     my $output_file = $self->output_file;
     my $permutations = $self->permutations;
-    my $maf_file = $self->maf_file;
+    my @all_sample_names; # names of all the samples, no matter if it's mutated or not
+
+    # Parse out the names of the samples which should match the names in the MAF file
+    my $sampleFh = IO::File->new( $bam_list ) or die "Couldn't open $bam_list. $!\n";
+    while( my $line = $sampleFh->getline )
+    {
+      next if ( $line =~ m/^#/ );
+      chomp( $line );
+      my ( $sample ) = split( /\t/, $line );
+      push( @all_sample_names, $sample );
+    }
+    $sampleFh->close;
 
     #create sample-gene matrix
-    my $matrix_file = create_sample_gene_matrix($maf_file);
+    my $matrix_file = create_sample_gene_matrix($maf_file, @all_sample_names);
 
     #perform mutation-relation test using R
     my $R_cmd = "R --slave --args < " . __FILE__ . ".R $matrix_file $permutations $output_file";
@@ -70,7 +86,7 @@ sub execute {
 
 sub create_sample_gene_matrix {
 
-    my ($maf_file) = @_;
+    my ($maf_file, @all_sample_names) = @_;
 
     #create hash of mutations from the MAF file
     my %mutations;
@@ -120,7 +136,7 @@ sub create_sample_gene_matrix {
     $matrix_fh->print("$header\n");
 
     #print mutation relation input matrix
-    for my $sample (sort keys %mutations) {
+    for my $sample (sort @all_sample_names) {
         $matrix_fh->print($sample);
         for my $gene (@all_genes) {
             if (exists $mutations{$sample}{$gene}) {
@@ -137,4 +153,3 @@ sub create_sample_gene_matrix {
 }
 
 1;
-
