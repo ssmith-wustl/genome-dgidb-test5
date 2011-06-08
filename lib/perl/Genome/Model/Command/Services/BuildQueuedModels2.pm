8d79b5c43018094f15b378ee30e9c6c0d5475de9
Nathan Nutter <nnutter@genome.wustl.edu>
2011-06-07 19:13:13 -0500

    allow parralelization of BQM2

--- lib/perl/Genome/Model/Command/Services/BuildQueuedModels2.pm
+++ lib/perl/Genome/Model/Command/Services/BuildQueuedModels2.pm
@@ -13,6 +13,16 @@ class Genome::Model::Command::Services::BuildQueuedModels2 {
             is => 'Integer',
             default => 50,
         },
+        channels => {
+            is => 'Integer',
+            default => 1,
+            doc => 'number of "channels" to parallelize models by',
+        },
+        channel => {
+            is => 'Integer',
+            default => 0,
+            doc => 'zero-based channel to use',
+        },
     ],
 };
 
@@ -31,7 +41,11 @@ EOS
 sub execute {
     my $self = shift;
 
-    my $lock_resource = '/gsc/var/lock/genome_model_services_builed_queued_models_2';
+    unless ($self->channel < $self->channels) {
+        die $self->error_message('--channel must be less than --channels');
+    }
+
+    my $lock_resource = '/gsc/var/lock/genome_model_services_builed_queued_models_2_' . $self->channel . '_' . $self->channels;
 
     my $lock = Genome::Sys->lock_resource(resource_lock => $lock_resource, max_try => 1);
     unless ($lock) {
@@ -60,12 +74,13 @@ sub execute {
     my $builds_started = 0;
     my $total_count = 0;
     while (my $model = $models->next) {
+        next unless ($model->id % $self->channels == $self->channel);
         if ($builds_started >= $max_builds_to_start){
             $self->status_message("Already started max builds $builds_started, quitting...");
             last; 
         }
 
-        $self->status_message("Trying to start " . $model->__display_name__ . "...");
+        $self->status_message("Trying to start #" . ($builds_started + 1) . ': ' . $model->__display_name__ . "...");
         $total_count++;
         
         my $transaction = UR::Context::Transaction->begin();
@@ -110,7 +125,7 @@ sub num_builds_to_start {
     my $scheduled_build_count = 0;
     while ($scheduled_builds->next && ++$scheduled_build_count < $self->max_scheduled_builds) { 1; }
     
-    return ($self->max_scheduled_builds - $scheduled_build_count);
+    return int ( ($self->max_scheduled_builds - $scheduled_build_count) / $self->channels);
 }
 
 
