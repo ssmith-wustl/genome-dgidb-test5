02c801959acfb5eaedc3d072ea112bd7557e4984
Thomas Mooney <tmooney@genome.wustl.edu>
2011-06-13 18:02:19 -0500

    Handle the build input value being undefined.
    (This typically happens because the object gets deleted out from
    under the input, since no foreign key constraints exist for inputs.)

--- lib/perl/Genome/Model/Build.pm
+++ lib/perl/Genome/Model/Build.pm
@@ -1822,7 +1822,7 @@ sub input_differences_from_model {
         for my $value_class_name (keys %{ $build_inputs{$name} }) {
             for my $build_input_not_found (values %{ $build_inputs{$name}{$value_class_name} }) {
                 my $value = $build_input_not_found->value;
-                if($value->isa('Genome::Model::Build') and $value->model and my ($model_input) = grep($_->value eq $value->model, @model_inputs_not_found) ) {
+                if($value and $value->isa('Genome::Model::Build') and $value->model and my ($model_input) = grep($_->value eq $value->model, @model_inputs_not_found) ) {
                     @model_inputs_not_found = grep($_ ne $model_input, @model_inputs_not_found);
                 } else {
                     push @build_inputs_not_found, $build_input_not_found;
