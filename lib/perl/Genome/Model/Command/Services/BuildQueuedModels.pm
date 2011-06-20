3e6369d7ed6dd8b0a17bd985fb9d6596ce7b7aaf
Nathan Nutter <nnutter@genome.wustl.edu>
2011-06-20 16:53:09 -0500

    fix for never seeing max_scheduled_builds

--- lib/perl/Genome/Model/Command/Services/BuildQueuedModels.pm
+++ lib/perl/Genome/Model/Command/Services/BuildQueuedModels.pm
@@ -123,10 +123,10 @@ sub num_builds_to_start {
     );
     
     my $scheduled_build_count = 0;
-    while ($scheduled_builds->next && ++$scheduled_build_count < $self->max_scheduled_builds) { 1; }
+    while ($scheduled_builds->next && ++$scheduled_build_count <= $self->max_scheduled_builds) { 1; }
     
     my $max_per_channel = int($self->max_scheduled_builds / $self->channels);
-    if ($scheduled_build_count > $self->max_scheduled_builds) {
+    if ($scheduled_build_count >= $self->max_scheduled_builds) {
         return 0;
     }
     elsif (($scheduled_build_count + $max_per_channel) > $self->max_scheduled_builds) {
