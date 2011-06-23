ea6dbad9faa5a61e607cc56ccc8475bf7a4cdb2a
Jim Weible <jweible@genome.wustl.edu>
2011-06-23 13:19:45 -0500

    Do not make RNA models automatically in the cron

--- lib/perl/Genome/Model/Command/Services/AssignQueuedInstrumentData.pm
+++ lib/perl/Genome/Model/Command/Services/AssignQueuedInstrumentData.pm
@@ -191,76 +191,82 @@ sub execute {
 
         if ($subject_class_name and $subject_id and @processing_profile_ids) {
             my $subject      = $subject_class_name->get($subject_id);
+            
+            if($subject->isa("Genome::Sample") and $subject->extraction_type eq 'rna'){
+                #record that the above code was skipped so we could reattempt it if we decide to do model generation for rna samples later
+                $pse->add_param('no_model_generation_attempted',1);
+                $self->status_message('No model generation attempted for PSE ' . $pse->id . ' representing RNA data');
+            }else{
+                PP:
+                foreach my $processing_profile_id (@processing_profile_ids) {
+                    my $processing_profile = Genome::ProcessingProfile->get( $processing_profile_id );
 
-            PP: 
-            foreach my $processing_profile_id (@processing_profile_ids) {
-                my $processing_profile = Genome::ProcessingProfile->get( $processing_profile_id );
-
-                unless ($processing_profile) {
-                    $self->error_message(
-                        'Failed to get processing profile'
-                        . " '$processing_profile_id' for inprogress pse "
-                        . $pse->pse_id );
-                    push @process_errors, $self->error_message;
-                    next PP;
-                }
-
-                my @reference_sequence_builds = ( undef ); # this allows to use a loop to assign
-                # These pps require imported reference seq build
-                if( $processing_profile->isa('Genome::ProcessingProfile::ReferenceAlignment')
-                        or $processing_profile->isa('Genome::ProcessingProfile::GenotypeMicroarray') ) {
-                    my @reference_sequence_build_ids = $pse->reference_sequence_build_param_for_processing_profile($processing_profile);
-                    if ( not @reference_sequence_build_ids ) {
-                        $self->error_message('No imported reference sequence build id found on pse ('.$pse->id.') to create '.$processing_profile->type_name.' model');
+                    unless ($processing_profile) {
+                        $self->error_message(
+                            'Failed to get processing profile'
+                            . " '$processing_profile_id' for inprogress pse "
+                            . $pse->pse_id );
                         push @process_errors, $self->error_message;
                         next PP;
                     }
 
-                    @reference_sequence_builds = Genome::Model::Build::ImportedReferenceSequence->get(\@reference_sequence_build_ids);
-                    if ( not @reference_sequence_builds or @reference_sequence_builds ne @reference_sequence_build_ids ) {
-                        $self->error_message("Failed to get imported reference sequence builds for ids: @reference_sequence_build_ids");
-                        push @process_errors, $self->error_message;
-                        next PP;
-                    }
-
-                }
-
-                my @models = Genome::Model->get(
-                    subject_id            => $subject_id,
-                    subject_class_name    => $subject_class_name,
-                    processing_profile_id => $processing_profile->id,
-                    auto_assign_inst_data => 1,
-                );
-
-                for my $reference_sequence_build ( @reference_sequence_builds ) {
-                    my @assigned = $self->assign_instrument_data_to_models($genome_instrument_data, $reference_sequence_build, @models);
-
-                    #returns an explicit undef on error
-                    if(scalar(@assigned) eq 1 and not defined $assigned[0]) {
-                        push @process_errors, $self->error_message;
-                        next PP;
-                    }
-
-                    if(scalar(@assigned > 0)) {
-                        for my $m (@assigned) {
-                            $pse->add_param('genome_model_id', $m->id);
-                        }
-                        #find or create somatic models if applicable
-                        $self->find_or_create_somatic_variation_models(@assigned);
-
-                    } else {
-                        # no model found for this PP, make one (or more) and assign all applicable data
-                        $DB::single = $DB::stopper;
-                        my @new_models = $self->create_default_models_and_assign_all_applicable_instrument_data($genome_instrument_data, $subject, $processing_profile, $reference_sequence_build, $pse);
-                        unless(@new_models) {
+                    my @reference_sequence_builds = ( undef ); # this allows to use a loop to assign
+                    # These pps require imported reference seq build
+                    if( $processing_profile->isa('Genome::ProcessingProfile::ReferenceAlignment')
+                            or $processing_profile->isa('Genome::ProcessingProfile::GenotypeMicroarray') ) {
+                        my @reference_sequence_build_ids = $pse->reference_sequence_build_param_for_processing_profile($processing_profile);
+                        if ( not @reference_sequence_build_ids ) {
+                            $self->error_message('No imported reference sequence build id found on pse ('.$pse->id.') to create '.$processing_profile->type_name.' model');
                             push @process_errors, $self->error_message;
                             next PP;
                         }
-                        #find or create somatic models if applicable
-                        $self->find_or_create_somatic_variation_models(@new_models);
+
+                        @reference_sequence_builds = Genome::Model::Build::ImportedReferenceSequence->get(\@reference_sequence_build_ids);
+                        if ( not @reference_sequence_builds or @reference_sequence_builds ne @reference_sequence_build_ids ) {
+                            $self->error_message("Failed to get imported reference sequence builds for ids: @reference_sequence_build_ids");
+                            push @process_errors, $self->error_message;
+                            next PP;
+                        }
+
                     }
-                }
-            } # looping through processing profiles for this instdata, finding or creating the default model
+
+                    my @models = Genome::Model->get(
+                        subject_id            => $subject_id,
+                        subject_class_name    => $subject_class_name,
+                        processing_profile_id => $processing_profile->id,
+                        auto_assign_inst_data => 1,
+                    );
+
+                    for my $reference_sequence_build ( @reference_sequence_builds ) {
+                        my @assigned = $self->assign_instrument_data_to_models($genome_instrument_data, $reference_sequence_build, @models);
+
+                        #returns an explicit undef on error
+                        if(scalar(@assigned) eq 1 and not defined $assigned[0]) {
+                            push @process_errors, $self->error_message;
+                            next PP;
+                        }
+
+                        if(scalar(@assigned > 0)) {
+                            for my $m (@assigned) {
+                                $pse->add_param('genome_model_id', $m->id);
+                            }
+                            #find or create somatic models if applicable
+                            $self->find_or_create_somatic_variation_models(@assigned);
+
+                        } else {
+                            # no model found for this PP, make one (or more) and assign all applicable data
+                            $DB::single = $DB::stopper;
+                            my @new_models = $self->create_default_models_and_assign_all_applicable_instrument_data($genome_instrument_data, $subject, $processing_profile, $reference_sequence_build, $pse);
+                            unless(@new_models) {
+                                push @process_errors, $self->error_message;
+                                next PP;
+                            }
+                            #find or create somatic models if applicable
+                            $self->find_or_create_somatic_variation_models(@new_models);
+                        }
+                    }
+                } # looping through processing profiles for this instdata, finding or creating the default model
+            }
         } else {
             #record that the above code was skipped so we could reattempt it if more information gained later
             $pse->add_param('no_model_generation_attempted',1);
