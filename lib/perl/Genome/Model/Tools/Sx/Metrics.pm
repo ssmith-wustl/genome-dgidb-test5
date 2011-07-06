88d2ecc157c201306927e530d8238f09975dcaf9
Eddie Belter <ebelter@genome.wustl.edu>
2011-07-06 15:33:21 -0500

    SX: added metrics to reader; writer metrics get added in strategy subs

--- lib/perl/Genome/Model/Tools/Sx/Metrics.pm
+++ lib/perl/Genome/Model/Tools/Sx/Metrics.pm
@@ -22,12 +22,20 @@ class Genome::Model::Tools::Sx::Metrics {
     ],
 };
 
-sub add {
+sub add_sequence {
+    my ($self, $seq) = @_;
+
+    $self->_metrics->{bases} += length($seq->{seq});
+    $self->_metrics->{count}++;
+
+    return 1;
+}
+
+sub add_sequences {
     my ($self, $seqs) = @_;
 
     for my $seq ( @$seqs ) {
-        $self->_metrics->{bases} += length($seq->{seq});
-        $self->_metrics->{count}++;
+        $self->add_sequence($seq);
     }
 
     return 1;
@@ -42,6 +50,62 @@ sub to_string {
     }
 
     return $string;
+}
+
+sub read_from_file {
+    my ($class, $file) = @_;
+
+    if ( not $file ) {
+        $class->error_message('No file given to create metrics from file');
+        return;
+    }
+
+    if ( not -s $file ) {
+        $class->error_message('Failed to read metrics from file. File ('.$file.') does not exist.');
+        return;
+    }
+
+    my $fh = eval{ Genome::Sys->open_file_for_reading($file); };
+    if ( not $fh ) {
+        $class->error_message("Failed to open file ($file)");
+        return;
+    }
+
+    my %metrics;
+    while ( my $line = $fh->getline ) {
+        chomp $line;
+        my ($key, $val) = split('=', $line);
+        $metrics{$key} = $val;
+    }
+    $fh->close;
+
+    my $self = Genome::Model::Tools::Sx::Metrics->create(_metrics => \%metrics);
+    if ( not $self ) {
+        $class->error_message("Failed to create metrics object from file ($file) with metrics: ".Data::Dumper::Dumper(\%metrics));
+        return;
+    }
+
+    return $self;
+}
+
+sub write_to_file {
+    my ($self, $file) = @_;
+
+    if ( not $file ) {
+        $self->error_message('No file given to create metrics from file');
+        return;
+    }
+
+    unlink $file;
+    my $fh = eval{ Genome::Sys->open_file_for_writing($file); };
+    if ( not $fh ) {
+        $self->error_message("Failed to open file ($file)");
+        return;
+    }
+    $fh->print($self->to_string);
+    $fh->close;
+
+    return 1;
 }
 
 1;
