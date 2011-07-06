256564f1c403487b0eb0f4dee69c901e0634b590
Brian Derickson <bdericks@genome.wustl.edu>
2011-07-06 16:33:56 -0500

    CHANGELOG: Model define command now uses Command::DynamicTree, which will automatically generate define commands for subclasses (as before) and resolve objects based on id, name, or query (just like Genome::Command::Base or Command::V2). The model subject is optional and can be inferred from the instrument data given (if it can't be inferred, the command fails).

--- lib/perl/Genome/Model/Command/Define.pm
+++ lib/perl/Genome/Model/Command/Define.pm
@@ -1,6 +1,3 @@
-#FIXME ebelter
-# Long: Stop sub classing, handle iniput defining
-#
 package Genome::Model::Command::Define;
 
 use strict;
@@ -15,51 +12,26 @@ require Genome::Sys;
 ###################################################
 
 class Genome::Model::Command::Define {
-    is => 'Command::DynamicSubCommands',
+    is => 'Command::DynamicTree', 
     is_abstract => 1,
     has => [
-        subject_name => {
-            is => 'Text',
-            len => 255,
+        processing_profile => {
+            is => 'Genome::ProcessingProfile',
             is_input => 1,
-            doc => 'The name of the subject all the reads originate from'
+            doc => 'Processing profile to be used by model, can provide either a name or an id',
         },
     ],
     has_optional => [
+        subject => {
+            is => 'Genome::Subject',
+            is_input => 1,
+            doc => 'Subject for the model, can provide either a name or an id. If instrument data is provided and this is not, ' .
+                'an attempt will be made to resolve it based on the provided instrument data'
+        },
         model_name => {
             is => 'Text',
-            len => 255,
             is_input => 1,
-            doc => 'User meaningful name for this model (default value: $SUBJECT_NAME.$PP_NAME)'
-        },
-        processing_profile_name => {
-            is => 'Text', 
-            is_input => 1,
-            doc => 'identifies the processing profile by name' 
-        },
-        processing_profile_id => {
-            is => 'Integer',
-            is_input => 1,
-            doc => 'identifies the processing profile by id'
-        },
-        subject_type => {
-            is => 'Text',
-            len => 255,
-            is_input => 1,
-            doc => 'The type of subject all the reads originate from (sample_name is assumed if none given)',
-            valid_values => ["species_name","sample_group","sample_name"],
-        },
-        subject_id => {
-            is => 'Number',
-            len => 15,
-            is_input => 1,
-            doc => 'The ID of the subject all the reads originate from (may specify this and subject_class_name in lieu of subject_type)',  
-        },
-        subject_class_name => {
-            is => 'Text',
-            len => 500,
-            is_input => 1,
-            doc => 'The Perl class name of the subject whose ID is subject_id'  
+            doc => 'User meaningful name for this model, a default is used if none is provided',
         },
         auto_assign_inst_data => {
             is => 'Boolean',
@@ -74,29 +46,33 @@ class Genome::Model::Command::Define {
             doc => 'The building of the model is performed automatically',
         },
         result_model_id => {
-            is => 'Integer',
+            is => 'Number',
             is_output => 1,
-        },
-        bare_args => {
-            is_many => 1,
-            is_optional => 1,
-            shell_args_position => 99
-        },
-        groups => {
-            is_optional => 1,
-            doc => 'Model group(s) to which this model will be assigned upon creation. Provide a comma separated list of model group id\'s or names.',
+            is_transient => 1,
+            doc => 'Stores the ID of the newly created model, useful when running this command from a script',
         },
     ],
-    schema_name => 'Main',
+    has_many_optional => [
+        instrument_data => {
+            is => 'Genome::InstrumentData',
+            is_input => 1,
+            doc => 'Instrument data to be assigned to the model, can provide a query to resolve, a list of ids, etc'
+        },
+        groups => {
+            is => 'Genome::ModelGroup',
+            is_input => 1,
+            doc => 'Model groups to put the newly created model into',
+        },
+        bare_args => {
+            shell_args_position => 99
+        },
+    ],
 };
 
 # Compiling this class autogenerates one sub-command per processing profile.
 # These are the commands which actually execute, and inherit from this
 # for general execution logic.
-
 sub _sub_commands_from { 'Genome::ProcessingProfile' }
-
-sub sub_command_sort_position { 1 }
 
 sub help_brief {
     my $self = shift;
@@ -116,8 +92,8 @@ sub help_synopsis {
     return <<"EOS"
 genome model define reference-alignment 
   --model-name test5
-  --subject-name ley_aml_patient1_tumor
-  --processing-profile-name nature_aml_08
+  --subject ley_aml_patient1_tumor
+  --processing-profile nature_aml_08
 EOS
 }
 
@@ -130,159 +106,131 @@ The first build of a model must be explicitly requested after the model is defin
 EOS
 }
 
+# Should be overridden in subclasses
+sub type_specific_parameters_for_create {
+    my $self = shift;
+    return (); 
+}
+
 sub execute {
     my $self = shift;
 
-    # Make sure there aren't any bare args
     if (my @args = $self->bare_args) {
-        $self->error_message("extra arguments: @args");
+        $self->error_message("Extra arguments: @args");
         $self->usage_message($self->help_usage_complete_text);
         return;
     }
 
-    if (defined($self->processing_profile_name)) {
-        my @pp = Genome::ProcessingProfile->get(name => $self->processing_profile_name);
-        if (@pp > 1){
-            $self->error_message("ProcessingProfile name returned multiple processing profiles.");
-            return;
-        }
-        my $pp = $pp[0];
-        unless($pp){
-            $self->error_message("The processing profile name ".$self->processing_profile_name." was not found.");
-            return;
-        }
-        # If both name and id are provided, make sure they agree
-        if (defined($self->processing_profile_id)) {
-            unless ($pp->id == $self->processing_profile_id) {
-                $self->error_message("The provided processing profile name " . $self->processing_profile_name . " and processing profile id " . $self->processing_profile_id . " do not match.");
-                return;
-            }
-        } else {
-            $self->processing_profile_id($pp->id);
-        }
-    } elsif (defined($self->processing_profile_id)){
-        my @pp = Genome::ProcessingProfile->get(id => $self->processing_profile_id);
-        unless(@pp==1){
-            $self->error_message("ProcessingProfile id returned multiple processing profiles.");
-            return;
-        }
-        my $pp = $pp[0];
-        unless($pp){
-            $self->error_message("The processing profile id ".$self->processing_profile_id." was not found.");
-            return;
-        }
-        $self->processing_profile_name($pp->name);
+    my $processing_profile = $self->validate_processing_profile;
+    unless ($processing_profile) {
+        Carp::confess "Could not validate processing profile!";
     }
 
-    $self->compare_pp_and_model_type;
-
-    #attempt derive subject_type if not passed as an arg
-    #die if subject type isnt sample_name for now
-    my ($subject_type, $subject_id, $subject_class_name);
-    if ($self->subject_id and $self->subject_class_name) {
-        $subject_class_name = $self->subject_class_name;
-        $subject_id = $self->subject_id;
-        
-        my $subject = $subject_class_name->get($subject_id);
-        
-        unless($subject) {
-            $self->error_message('Subject not found for subject with id ' . $subject_id . ' and class ' . $subject_class_name);
-            return;
+    unless ($self->subject) {
+        my $subject = $self->deduce_subject_from_instrument_data;
+        unless ($subject) {
+            Carp::confess "Not given subject and could not derive subject from instrument data!";
         }
+        $self->subject($subject);
     }
             
-    # Create the model
-    my %model_params = (
+    my $model = Genome::Model->create(
+        subject_id => $self->subject->id,
+        subject_class_name => $self->subject->class,
+        processing_profile_id => $self->processing_profile->id,
         name => $self->model_name,
-        processing_profile_id => $self->processing_profile_id,
-        subject_name => $self->subject_name,
-        subject_type => $self->subject_type,
-        subject_id => $subject_id,
-        subject_class_name => $subject_class_name,
         auto_assign_inst_data => $self->auto_assign_inst_data,
         auto_build_alignments => $self->auto_build_alignments,
+        instrument_data => [$self->instrument_data],
         $self->type_specific_parameters_for_create,
     );
-    if(defined($self->subject_class_name)&&defined($self->subject_name)){
-        if($self->subject_class_name eq "Genome::Sample"){
-            my $sample = Genome::Sample->get(name=>$self->subject_name);
-            unless( $sample ){
-                $self->error_message("Subject was a Genome::Sample, but no Genome::Sample named ".$self->subject_name." could be retrieved.");
-                die $self->error_message;
-            }
-            $model_params{sample_name}= $self->subject_name;
-        }elsif($self->subject_class_name eq "Genome::Library"){
-            my $library = Genome::Library->get(name=>$self->subject_name);
-            unless($library){
-                $self->error_message("Subject is a library, however the library named ".$self->subject_name." cannot be found.");
-                die $self->error_message;
-            }
-            my $sample = Genome::Sample->get(name => $library->sample_name);
-            unless($sample){
-                $self->error_message("The sample named ".$library->sample_name." associated with the subject/library could not be found.");
-                die $self->error_message;
-            }
-            $model_params{sample_name}=$sample->name;
-        }
+    unless ($model) {
+        Carp::confess "Could not create a model!";
+    }
+    $self->result_model_id($model->id);
+
+    unless ($self->assign_model_to_groups($model)) {
+        Carp::confess "Encountered problems when trying to assign model to groups!";
     }
 
-    my $model = Genome::Model->create(%model_params);
-    unless ( $model ) {
-        $self->error_message('Could not create a model for: '. $self->subject_name);
-        return;
-    }
+    #if (my $rule = $model->create_rule_limiting_instrument_data) {
+    #    $model->limit_inputs_to_id($rule);
+    #}
 
-    if ( my @problems = $model->__errors__ ) {
-        $self->error_message(
-            "Error creating model:\n\t".  join("\n\t", map { $_->desc } @problems)
-        );
-        $model->delete;
-        return;
-    }
-
-    # Add the model to any model groups requested
-    if ($self->groups) {
-        my @groups = split ",", $self->groups;
-        for my $model_group_id (@groups) {
-            # Try to get model group by name or id, allow a negative ID due to test cases
-            my $model_group;
-            if ($model_group_id =~ /^-?\d+$/) {
-                $model_group = Genome::ModelGroup->get($model_group_id);
-            } else {
-                $model_group = Genome::ModelGroup->get(name => $model_group_id);
-            }
-            unless ($model_group) {
-                $self->error_message("Could not find a model group with the id or name of: $model_group. Please use a valid id/name or create a model-group.");
-                die;
-            }
-            $model_group->assign_models($model);
-        }
-    }
-
-    $self->status_message("Created model:");
-    my $list = Genome::Model::Command::List->create(
-        filter => 'id='.$model->id,
-        show => join(
-            ',', 
-            ($self->listed_params),
-            #$model->processing_profile->params_for_class,
-        ),
-        style => 'pretty',
-    );
-    $list->execute;
+    $self->display_model_information($model);
     $self->result_model_id($model->id);
 
     return 1;
+}
+
+sub deduce_subject_from_instrument_data {
+    my $self = shift;
+
+    my @instrument_data = $self->instrument_data;
+    unless (@instrument_data) {
+        Carp::confess "No instrument data provided, cannot deduce subject!";
+    }
+
+    my @samples = map { $_->sample } @instrument_data;
+    unless (grep { $_->id ne $samples[0]->id } @samples) {
+        return $samples[0];
+    }
+
+    my @individuals = map { $_->source } @samples;
+    unless (grep { $_->id ne $individuals[0]->id } @individuals) {
+        return $individuals[0];
+    }
+
+    my @taxons = map { $_->taxon } @individuals;
+    unless (grep { $_->id ne $taxons[0]->id } @taxons) {
+        return $taxons[0];
+    }
+
+    Carp::confess "Could not deduce model subject from provided instrument data!";
+}
+
+sub assign_model_to_groups {
+    my ($self, $model) = @_;
+    return 1 unless $self->groups;
+
+    for my $group ($self->groups) {
+        my $rv = $group->assign_models($model);
+        unless ($rv) {
+            Carp::confess "Could not assign model " . $model->__display_name__ . " to group " . $group->__display_name__;
+        }
+    }
+
+    return 1;
+}
+
+sub display_model_information {
+    my ($self, $model) = @_;
+
+    $self->status_message("Created model:");
+    my $list = Genome::Model::Command::List->create(
+        filter => 'id=' . $model->id,
+        show => join(',', $self->listed_params),
+        style => 'pretty',
+    );
+    return $list->execute;
 }
 
 sub listed_params {
     return qw/ id name subject_name subject_type processing_profile_id processing_profile_name /;
 }
 
-sub type_specific_parameters_for_create {
+sub validate_processing_profile {
     my $self = shift;
+    
+    unless ($self->processing_profile) {
+        Carp::confess 'Could not resolve a processing profile from provided parameters!';
+    }
 
-    return (); #This exists to be overwritten by subclasses
+    unless ($self->compare_pp_and_model_type) {
+        Carp::confess 'Model and processing profile types do not match!';
+    }
+    
+    return 1;
 }
 
 sub compare_pp_and_model_type {
@@ -294,24 +242,14 @@ sub compare_pp_and_model_type {
     $model_subclass =~ s/$package//;
     
     # Determine the subclass of the processing profile
-    my $pp = Genome::ProcessingProfile->get(id=>$self->processing_profile_id);
+    my $pp = Genome::ProcessingProfile->get(id => $self->processing_profile->id);
     unless($pp){
-        $self->error_message("Couldn't find the processing profile identified by the #: ".$self->processing_profile_id);
+        $self->error_message("Couldn't find the processing profile identified by the #: " . $self->processing_profile->id);
         die $self->error_message;
     }
     my $pp_subclass = $pp->subclass_name;
     $pp_subclass =~ s/Genome::ProcessingProfile:://;
     
-
-    #check for special cases where processing-profile-name and model subclass have different names
-    if ($model_subclass =~ /GenotypeMicroarray/) {
-        unless($pp->name =~ /wugc/){
-            $self->error_message("GenotypeMicroarray Models must use one of the [microarray-type]/wugc processing-profiles.");
-            die $self->error_message;
-        }
-        return 1;
-    }
-
     unless ($model_subclass eq $pp_subclass) {
         my ($shortest, $longest) = ($model_subclass, $pp_subclass);
         ($shortest, $longest) = ($longest, $shortest) if length $pp_subclass < length $model_subclass;
@@ -320,11 +258,8 @@ sub compare_pp_and_model_type {
             confess;
         }
     }
+
     return 1;
 }
 
-
 1;
-
-#$HeadURL$
-#$Id$
