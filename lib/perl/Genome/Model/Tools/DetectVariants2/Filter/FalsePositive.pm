9394c6a9963721037c8317e1c4ba44142093bfeb
Rob Long <rlong@genome.wustl.edu>
2011-07-15 08:29:52 -0500

    SomaticVariation Pipeline now gets dispatched to the apipe queue

--- lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalsePositive.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalsePositive.pm
@@ -103,9 +103,6 @@ class Genome::Model::Tools::DetectVariants2::Filter::FalsePositive {
         },
     ],
     has_param => [
-         lsf_queue => {
-             default_value => 'long',
-         }, 
          lsf_resource => {
              default_value => "-M 8000000 -R 'select[type==LINUX64 && mem>8000] rusage[mem=8000]'",
          },
