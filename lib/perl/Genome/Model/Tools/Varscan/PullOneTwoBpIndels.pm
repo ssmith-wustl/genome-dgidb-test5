04fef1562b809058b781d1557fa9bc34ae5bf69e
Dave Larson <dlarson@genome.wustl.edu>
2011-07-28 12:23:46 -0500

    made large indel file optional and added in suitable defaults to the purities etc

--- lib/perl/Genome/Model/Tools/Varscan/PullOneTwoBpIndels.pm
+++ lib/perl/Genome/Model/Tools/Varscan/PullOneTwoBpIndels.pm
@@ -35,9 +35,9 @@ class Genome::Model::Tools::Varscan::PullOneTwoBpIndels {
 		output_snp	=> { is => 'Text', doc => "gmt varscan validate input" , is_optional => 0},
 		final_output_file	=> { is => 'Text', doc => "process-validation-indels output file" , is_optional => 0},
 		skip_if_output_present	=> { is => 'Boolean', doc => "Skip Creating new Bam Files if they exist" , is_optional => 1, default => ""},
-	        realigned_bam_file_directory => { is => 'Text', doc => "Where to dump the realigned bam file", is_optional => 0},
-        	normal_purity => { is => 'Float', doc => "Normal purity param to pass to varscan", is_optional => 0, default => 1},
-        	min_var_frequency => { is => 'Float', doc => "Minimum variant frequency to pass to varscan", is_optional => 0, default => 0.08},
+        realigned_bam_file_directory => { is => 'Text', doc => "Where to dump the realigned bam file", is_optional => 0},
+        normal_purity => { is => 'Float', doc => "Normal purity param to pass to varscan", is_optional => 0, default => 1},
+        min_var_frequency => { is => 'Float', doc => "Minimum variant frequency to pass to varscan", is_optional => 0, default => 0.08},
 	],
 };
 
@@ -89,11 +89,13 @@ sub execute {                               # replace with real execution logic.
 	$small_indel_list_nobed =~ s/\.bed//;
 	$small_indel_list_nobed = "$small_indel_list_nobed.txt";
 
-	my $large_indel_list_nobed = $large_indel_list;
-	$large_indel_list_nobed =~ s/\.bed//;
-	$large_indel_list_nobed = "$large_indel_list_nobed.txt";
+    my $large_indel_list_nobed = $large_indel_list;
+    if($large_indel_list) {
+        $large_indel_list_nobed =~ s/\.bed//;
+        $large_indel_list_nobed = "$large_indel_list_nobed.txt";
+    }
 
-        my $realigned_bam_file_directory = $self->realigned_bam_file_directory;
+    my $realigned_bam_file_directory = $self->realigned_bam_file_directory;
 
 	my $realigned_normal_bam_file = basename($normal_bam,qr{\.bam});
 	$realigned_normal_bam_file = "$realigned_bam_file_directory/$realigned_normal_bam_file.realigned.bam";
@@ -115,10 +117,12 @@ sub execute {                               # replace with real execution logic.
 	my $nobed_indel_outfile = $small_indel_list_nobed;
 	open(INDELS_OUT, ">$bed_indel_outfile") or die "Can't open output file: $!\n";
 	open(NOBED_INDELS_OUT, ">$nobed_indel_outfile") or die "Can't open output file: $!\n";
-	my $large_bed_indel_outfile = $large_indel_list;
-	my $large_nobed_indel_outfile = $large_indel_list_nobed;
-	open(LARGE_INDELS_OUT, ">$large_bed_indel_outfile") or die "Can't open output file: $!\n";
-	open(LARGE_NOBED_INDELS_OUT, ">$large_nobed_indel_outfile") or die "Can't open output file: $!\n";
+    if($large_indel_list) {
+        my $large_bed_indel_outfile = $large_indel_list;
+        my $large_nobed_indel_outfile = $large_indel_list_nobed;
+        open(LARGE_INDELS_OUT, ">$large_bed_indel_outfile") or die "Can't open output file: $!\n";
+        open(LARGE_NOBED_INDELS_OUT, ">$large_nobed_indel_outfile") or die "Can't open output file: $!\n";
+    }
 	my $file_input = new FileHandle ($file_list_file);
     unless($file_input) {
         $self->error_message("Unable to open $file_list_file");
@@ -168,7 +172,7 @@ sub execute {                               # replace with real execution logic.
 				print INDELS_OUT "$chr\t$bedstart\t$bedstop\t$ref\t$var\n";
 				print NOBED_INDELS_OUT "$chr\t$start\t$stop\t$ref\t$var\n";
 			}
-			elsif ( $size > 2) {
+			elsif ( $size > 2 && $large_indel_list) {
 				print LARGE_INDELS_OUT "$chr\t$bedstart\t$bedstop\t$ref\t$var\t$type\n";
 				print LARGE_NOBED_INDELS_OUT "$chr\t$start\t$stop\t$ref\t$var\t$type\n";
 			}
@@ -180,6 +184,7 @@ sub execute {                               # replace with real execution logic.
     my $min_freq = $self->min_var_frequency;
     my $normal_purity = $self->normal_purity;
     my $varscan_params = "--validation 1 --somatic-p-value 1.0e-02 --p-value 0.10 --min-coverage 8 --min-var-freq $min_freq --normal-purity $normal_purity";
+    my $default_varscan_params = "--validation 1 --somatic-p-value 1.0e-02 --p-value 0.10 --min-coverage 8 --min-var-freq 0.08 --normal-purity 1";
 
 	my $bsub = 'bsub -q long -R "select[model!=Opteron250 && type==LINUX64 && mem>16000 && tmp>10000] rusage[mem=16000, tmp=10000]" -M 16000000 ';
 	my ($jobid1, $jobid2, $jobid3, $jobid4, $jobid5, $jobid6);
@@ -264,11 +269,11 @@ sub execute {                               # replace with real execution logic.
 		   $jobid8=~/<(\d+)>/;
 		   $jobid8= $1;
 		   print "$jobid8\n";
-		my $jobid9 = `$bsub -J varscan_validation_relnor -w \'ended($jobid5) && ended($jobid7)\' \'gmt varscan validation --normal-bam $realigned_normal_bam_file --tumor-bam $realigned_relapse_bam_file --output-indel $output_indel.relnor --output-snp $output_snp.relnor --varscan-params "$varscan_params"\'`;
+		my $jobid9 = `$bsub -J varscan_validation_relnor -w \'ended($jobid5) && ended($jobid7)\' \'gmt varscan validation --normal-bam $realigned_normal_bam_file --tumor-bam $realigned_relapse_bam_file --output-indel $output_indel.relnor --output-snp $output_snp.relnor --varscan-params "$default_varscan_params"\'`;
 		   $jobid9=~/<(\d+)>/;
 		   $jobid9= $1;
 		   print "$jobid9\n";
-		my $jobid10 = `$bsub -J varscan_validation_reltum -w \'ended($jobid6) && ended($jobid7)\' \'gmt varscan validation --normal-bam $realigned_tumor_bam_file --tumor-bam $realigned_relapse_bam_file --output-indel $output_indel.reltum --output-snp $output_snp.reltum --varscan-params "$varscan_params"\'`;
+		my $jobid10 = `$bsub -J varscan_validation_reltum -w \'ended($jobid6) && ended($jobid7)\' \'gmt varscan validation --normal-bam $realigned_tumor_bam_file --tumor-bam $realigned_relapse_bam_file --output-indel $output_indel.reltum --output-snp $output_snp.reltum --varscan-params "$default_varscan_params"\'`;
 		   $jobid10=~/<(\d+)>/;
 		   $jobid10= $1;
 		   print "$jobid10\n";
