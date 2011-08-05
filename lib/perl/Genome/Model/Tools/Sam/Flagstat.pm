7c4854ad319bef75d8425e06f6c60bbba24b39f4
Feiyu Du <fdu@genome.wustl.edu>
2011-08-05 10:23:08 -0500

    1. Make Flagstat samtools new version compatible 2. Turn off mpileup for samtools r963 for now. Better solution is on the way

--- lib/perl/Genome/Model/Tools/Sam/Flagstat.pm
+++ lib/perl/Genome/Model/Tools/Sam/Flagstat.pm
@@ -42,47 +42,50 @@ sub parse_file_into_hashref {
     
     my %data;
     my @lines = <$flag_fh>;
-    
-    for (@lines){
-        chomp $_;
-    }
-    
-    while (scalar @lines and $lines[0] =~ /^\[.*\]/){
+    $flag_fh->close;
+    my $line_ct = scalar @lines;
+            
+    while ($line_ct and $lines[0] =~ /^\[.*\]/){
         push @{ $data{errors} }, shift @lines;
     }
 
-    unless (scalar @lines == 12) {
+    unless ($line_ct =~ /^1[12]$/) {#samtools 0.1.15 (r949), older versions get 12 lines, newer get 11 line with QC passed/failed separate in each line
         warn 'Unexpected output from flagstat. Check ' . $flag_file;
         return;
     }
 
-    my ($total, $qc_failure, $duplicates, $mapped, $paired, $read1, $read2, $properly_paired, $mate_mapped, $singletons, $mate_different, $mate_different_hq) = @lines;
-    
-    ($data{total_reads})             = $total      =~ /^(\d+) in total$/;
-    ($data{reads_marked_failing_qc}) = $qc_failure =~ /^(\d+) QC failure$/;
-    ($data{reads_marked_duplicates}) = $duplicates =~ /^(\d+) duplicates$/;
-    
-    ($data{reads_mapped}, $data{reads_mapped_percentage}) =
-        $mapped =~ /^(\d+) mapped \((\d{1,3}\.\d{2}|nan)\%\)$/;
-    undef($data{reads_mapped_percentage}) if $data{reads_mapped_percentage} eq 'nan';
-    
-    ($data{reads_paired_in_sequencing}) = $paired =~ /^(\d+) paired in sequencing$/;
-    ($data{reads_marked_as_read1})      = $read1  =~ /^(\d+) read1$/;
-    ($data{reads_marked_as_read2})      = $read2  =~ /^(\d+) read2$/;
-    
-    ($data{reads_mapped_in_proper_pairs}, $data{reads_mapped_in_proper_pairs_percentage}) =
-        $properly_paired =~ /^(\d+) properly paired \((\d{1,3}\.\d{2}|nan)\%\)$/;
-    undef($data{reads_mapped_in_proper_pairs_percentage}) if $data{reads_mapped_in_proper_pairs_percentage} eq 'nan';
-    
-    ($data{reads_mapped_in_pair}) = $mate_mapped =~ /^(\d+) with itself and mate mapped$/;
-    
-    ($data{reads_mapped_as_singleton}, $data{reads_mapped_as_singleton_percentage}) =
-        $singletons =~ /^(\d+) singletons \((\d{1,3}\.\d{2}|nan)\%\)$/;
-    undef($data{reads_mapped_as_singleton_percentage}) if $data{reads_mapped_as_singleton_percentage} eq 'nan';
-    
-    ($data{reads_mapped_in_interchromosomal_pairs})    = $mate_different    =~ /^(\d+) with mate mapped to a different chr$/;
-    ($data{hq_reads_mapped_in_interchromosomal_pairs}) = $mate_different_hq =~ /^(\d+) with mate mapped to a different chr \(mapQ>=5\)$/;
-    
-    $flag_fh->close;
+   for (@lines) {
+        chomp;
+        $data{total_reads}             = $1 if /^(\d+) (\+\s\d+\s)?in total/;
+        $data{reads_marked_failing_qc} = $1 if /^\d+ \+ (\d+) in total/;
+        $data{reads_marked_failing_qc} = $1 if /^(\d+) QC failure/;
+        $data{reads_marked_duplicates} = $1 if /^(\d+) (\+\s\d+\s)?duplicates$/;
+        ($data{reads_mapped}, $data{reads_mapped_percentage}) = ($1, $3)
+            if /^(\d+) (\+\s\d+\s)?mapped \((\d{1,3}\.\d{2}|nan)\%[\:\)]/;
+        undef($data{reads_mapped_percentage}) 
+            if $data{reads_mapped_percentage} && $data{reads_mapped_percentage} eq 'nan';
+
+        $data{reads_paired_in_sequencing} = $1 if /^(\d+) (\+\s\d+\s)?paired in sequencing$/;
+        $data{reads_marked_as_read1}      = $1 if /^(\d+) (\+\s\d+\s)?read1$/;
+        $data{reads_marked_as_read2}      = $1 if /^(\d+) (\+\s\d+\s)?read2$/;
+
+        ($data{reads_mapped_in_proper_pairs}, $data{reads_mapped_in_proper_pairs_percentage}) = ($1, $3)
+            if /^(\d+) (\+\s\d+\s)?properly paired \((\d{1,3}\.\d{2}|nan)\%[\:\)]/;
+        undef($data{reads_mapped_in_proper_pairs_percentage}) 
+            if $data{reads_mapped_in_proper_pairs_percentage} && $data{reads_mapped_in_proper_pairs_percentage} eq 'nan';
+
+        $data{reads_mapped_in_pair} = $1 if /^(\d+) (\+\s\d+\s)?with itself and mate mapped$/;
+
+        ($data{reads_mapped_as_singleton}, $data{reads_mapped_as_singleton_percentage}) = ($1, $3)
+            if /^(\d+) (\+\s\d+\s)?singletons \((\d{1,3}\.\d{2}|nan)\%[\:\)]/;
+        undef($data{reads_mapped_as_singleton_percentage}) 
+            if $data{reads_mapped_as_singleton_percentage} && $data{reads_mapped_as_singleton_percentage} eq 'nan';
+
+        $data{reads_mapped_in_interchromosomal_pairs}    = $1 if /^(\d+) (\+\s\d+\s)?with mate mapped to a different chr$/;
+        $data{hq_reads_mapped_in_interchromosomal_pairs} = $1 if /^(\d+) (\+\s\d+\s)?with mate mapped to a different chr \(mapQ>=5\)$/;
+    }
+
     return \%data;
 }
+
+
