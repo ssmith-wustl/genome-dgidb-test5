edf7cb9b347723bfa1e68c358f3a6424bcc10e39
Gabriel Sanderson <gsanders@genome.wustl.edu>
2011-08-09 16:56:31 -0500

    Set default bam readcount version to 0.3 temporarily until processing profiles are backfilled.

--- lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalseIndel.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalseIndel.pm
@@ -91,6 +91,7 @@ class Genome::Model::Tools::DetectVariants2::Filter::FalseIndel {
        bam_readcount_version => {
            is => 'Text',
            is_input => 1,
+           default => "0.3",
            doc => 'version of bam-readcount to use',
        },
        bam_readcount_min_base_quality => {
