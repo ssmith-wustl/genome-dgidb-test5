8f88a47cbaa04e1b07f87c28880c8842cf475eee
Eddie Belter <ebelter@genome.wustl.edu>
2011-08-23 15:09:08 -0500

    SX: added writingto gzip output; updated docs/tests

--- lib/perl/Genome/Model/Tools/Sx/SeqWriter.pm
+++ lib/perl/Genome/Model/Tools/Sx/SeqWriter.pm
@@ -27,10 +27,18 @@ sub create {
             $self->error_message("File property ($property_name) is required");
             return;
         }
-        my $method = ( $file eq '-' ? 'open_file_for_writing' : 'open_file_for_appending' ); # STDOUT
-        my $fh = eval{ Genome::Sys->$method($file); };
+        my ($fh, $cmd);
+        if ( $file eq '-' ) {
+            $fh = eval{ Genome::Sys->open_file_for_writing($file); };
+        }
+        elsif ( $cmd = $self->_cmd_for_file($file) ) {
+            $fh = IO::File->new($cmd);
+        }
+        else {
+            $fh = eval{ Genome::Sys->open_file_for_appending($file); };
+        }
         if ( not $fh ) {
-            $self->error_message("Failed to open file ($file) for appending");
+            $self->error_message("Failed to open file ($file".( $cmd ? " [$cmd]" : '' ).") for appending");
             return;
         }
         $fh->autoflush(1);
@@ -38,6 +46,16 @@ sub create {
     }
 
     return $self;
+}
+
+sub _cmd_for_file {
+    my ($self, $file) = @_;
+
+    if ( $file =~ /\.gz$/ ) {
+        return '| gzip > '.$file;
+    }
+
+    return;
 }
 
 sub flush {
