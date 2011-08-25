3bb56659effe209e0160dd28566077ae006e0f5a
Brian Derickson <bdericks@genome.wustl.edu>
2011-08-25 14:25:42 -0500

    added repeat masker gff file to egap workflow
    repeat masker now produces gff files along with its regular output,
    which is useful for converting the ace file to the appropriate format
    (the conversion tool was also added to workflow). the gffs are merged
    after all of the fasta chunks are done be analyzed.

--- lib/perl/Genome/ProcessingProfile/GenePrediction/Eukaryotic.pm
+++ lib/perl/Genome/ProcessingProfile/GenePrediction/Eukaryotic.pm
@@ -99,6 +99,7 @@ sub _map_workflow_inputs {
         prediction_directory => $build->prediction_directory,
         skip_repeat_masker => $self->skip_repeat_masker,
         repeat_masker_ace_file => $build->repeat_masker_ace_file,
+        repeat_masker_gff_file => $build->repeat_masker_gff_file,
         predictions_ace_file => $build->predictions_ace_file,
         rna_predictions_ace_file => $build->rna_predictions_ace_file,
         rna_predictions_only_flag => 1; # This is just used to tell the step makes the rna predictions ace
