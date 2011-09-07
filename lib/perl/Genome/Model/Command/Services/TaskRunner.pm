5b34074ba5b44ed33d7bdb9df609c417a2f6432f
Ben Oberkfell <boberkfe@genome.wustl.edu>
2011-09-07 16:04:59 -0500

    Add code for running scheduled tasks.
    Squashed commit of the following:
    
    commit a9d7d7ec3895bbd87290584442652253f3d4c06e
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Wed Sep 7 16:04:19 2011 -0500
    
        task runner daemon
    
    commit f2429bac459ec34c93e3a153aa1c444d4e579ed4
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Wed Sep 7 14:36:29 2011 -0500
    
        spruce up test cases for task run cmd
    
    commit 6a5874c6e55fa27efe3b8060860bf9129a65a614
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Wed Sep 7 14:13:15 2011 -0500
    
        test coverage, task cleanup
    
    commit 45a0e135a73920ded289b94ce1a2e9bee2d8b67c
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Wed Sep 7 12:27:00 2011 -0500
    
        resolve params from modules
    
    commit 90faf1ccbd27e3e6b43f631a09b976eda9850283
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Tue Sep 6 17:13:26 2011 -0500
    
        add docs
    
    commit d446df82cfb14ba2c16d8137eb37622d9bdea49a
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Tue Sep 6 17:08:12 2011 -0500
    
        initial import for task runner service
    
    commit f5adc1f322e431585c1f65bc423c7de7995fae4c
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Tue Sep 6 17:07:32 2011 -0500
    
        method rename
    
    commit 4fea195e0146d8c3a22186ffae58d8b65abce608
    Author: Ben Oberkfell <boberkfe@genome.wustl.edu>
    Date:   Tue Sep 6 17:06:46 2011 -0500
    
        command to run a task

---
+++ lib/perl/Genome/Model/Command/Services/TaskRunner.pm
@@ -0,0 +1,96 @@
+package Genome::Model::Command::Services::TaskRunner;
+
+use warnings;
+use strict;
+use Genome;
+use JSON::XS;
+use File::Path;
+
+class Genome::Model::Command::Services::TaskRunner {
+    is => 'Genome::Command::Base',
+    has => [
+        output_basedir => {
+            is => 'String',
+            doc => 'Directory to put stdout/stderr from run jobs',    
+            is_optional => 1,
+        },
+        restart_file => {
+            is => 'String',
+            doc => 'File to watch for restart trigger - touch this file to restart daemon',
+            is_optional => 1
+        },
+        _restart_file_mtime => {
+            is => 'Integer',
+            is_optional => 1,
+            is_transient => 1
+        }
+    ],
+};
+
+sub execute {
+    my $self = shift;
+        
+    if ($self->restart_file) {
+        $self->_restart_file_mtime(0);
+        
+        if (-f $self->restart_file) {
+            $self->_restart_file_mtime((stat($self->restart_file))[9]);
+        }
+    }
+
+    while (1) {
+        $self->task_loop();
+        $self->check_restart_loop() if $self->restart_file;
+        sleep 15;
+    }
+
+}
+
+
+sub task_loop {
+    my $self = shift;
+    
+    my $ds = $UR::Context::current->resolve_data_sources_for_class_meta_and_rule(Genome::Task->__meta__);
+    my $dbh = $ds->get_default_dbh;
+
+    # get lock on pending jobs and bypass object cache
+    my $res = $dbh->selectall_arrayref("SELECT id FROM genome_task WHERE status='submitted' FOR UPDATE");
+   
+    if (@$res > 0) { 
+        my ($id) = $res->[0]->[0];
+         
+        my $task = Genome::Task->get($id); 
+        # before we fork, we'll drop the db connection and lose our lock.  
+        # so first, update the status so that nobody else takes it in the meantime.
+        $task->status("pending_execute");
+        UR::Context->commit;
+        
+        my $pid = UR::Context::Process->fork();
+        if (!$pid) {
+           $self->status_message("Forked $$, running $id");
+           Genome::Task::Command::Run->create(task=>$task, output_basedir=>$self->output_basedir)->execute; 
+           UR::Context->commit;
+           exit(0);
+        } else {
+           $task->unload; 
+        }
+    }
+     
+    UR::Context->commit;
+}
+
+sub check_restart_loop() {
+    my $self = shift;
+
+    if (-f $self->restart_file) {
+        my $mtime = (stat($self->restart_file))[9];
+        if ($mtime > $self->_restart_file_mtime) {
+            $self->status_message("Restart file updated. Restarting as requested.");
+            exec("genome model services task-runner");
+        }
+    }
+    
+}
+
+
+1;
---
+++ lib/perl/Genome/Task/Command/Run.pm
@@ -0,0 +1,89 @@
+package Genome::Task::Command::Run;
+
+use warnings;
+use strict;
+use Genome;
+use JSON::XS;
+use File::Path;
+
+class Genome::Task::Command::Run {
+    is => 'Genome::Command::Base',
+    has => [
+        task => {
+            is => 'Genome::Task',
+            doc => 'Task to run.  Resolved from command line via text string',
+            shell_args_position => 1,
+        },
+        output_basedir => {
+            is => 'String',
+            doc => 'Directory to put stdout/stderr',    
+            is_optional => 1,
+        }
+    ],
+};
+
+sub execute {
+    my $self = shift;
+    
+    my $class_object;
+    eval {$class_object = $self->task->command_class->class};
+    if (!$class_object) {
+        $self->error_message(sprintf("Failed to execute, %s couldn't be loaded", $self->task->command_class));
+        return $self->handle_failure;
+    }
+
+    my $cmd_object = $self->task->command_object;
+    if (!$cmd_object) {
+        $self->error_message(sprintf("Failed to execute, %s couldn't be instantiated", $self->task->command_class));
+        return $self->handle_failure;
+    }
+
+    my $old_stdout;
+    my $old_stderr;
+    my $log_basedir;
+    if ($self->output_basedir) {
+        $log_basedir = $self->output_basedir;
+        if (!-d $log_basedir) {
+            unless(mkpath($log_basedir)) {
+                $self->error_message("Couldn't create output dir $log_basedir");
+                return;
+            }
+        }
+         
+        open ($old_stdout, ">&STDOUT");
+        open ($old_stderr, ">&STDERR");
+       
+        open (STDERR, ">$log_basedir/".$self->task->id .".stderr"); 
+        open (STDOUT, ">$log_basedir/".$self->task->id .".stdout"); 
+    }
+        
+    my $result;
+    eval {
+        my %attrs_to_update = (status => 'running', time_started => UR::Time->now());
+        if ($self->output_basedir) {
+            $attrs_to_update{stdout_pathname} = $log_basedir. "/" . $self->task->id. "/stdout"; 
+            $attrs_to_update{stderr_pathname} = $log_basedir. "/" . $self->task->id. "/stderr"; 
+        }
+
+        $self->task->out_of_band_attribute_update(%attrs_to_update);
+        $result = $cmd_object->execute;
+    };
+    
+    if ($@ || !$result) {
+        $self->error_message("COMMAND FAILURE:  $@ -- " . $cmd_object->error_message);
+        $self->task->status("failed");
+    } else {
+        $self->task->status("succeeded");
+    }
+
+    if ($self->output_basedir) {
+        open (STDOUT, ">&", $old_stdout);
+        open (STDERR, ">&", $old_stderr);
+    }
+    
+    $self->task->time_finished(UR::Time->now);
+
+    return $result;
+}
+
+1;
