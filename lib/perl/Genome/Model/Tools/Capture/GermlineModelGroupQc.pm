bb2a93143bb30b888c0f7e86847743be40d72d26
James Koval <jkoval@genome.wustl.edu>
2011-09-09 15:41:29 -0500

    Fix formatting of poorly styled gmt scripts

--- lib/perl/Genome/Model/Tools/Capture/GermlineModelGroupQc.pm
+++ lib/perl/Genome/Model/Tools/Capture/GermlineModelGroupQc.pm
@@ -1,29 +1,10 @@
-
-package Genome::Model::Tools::Capture::GermlineModelGroupQc;     # rename this when you give the module file a different name <--
-
-#####################################################################################################################################
-# ModelGroup - Build Genome Models for Germline Capture Datasets
-#					
-#	AUTHOR:		Will Schierding
-#
-#	CREATED:	2/09/2011 by W.S.
-#	MODIFIED:	2/09/2011 by W.S.
-#
-#	NOTES:	
-#			
-#####################################################################################################################################
-
+package Genome::Model::Tools::Capture::GermlineModelGroupQc;
 use strict;
 use warnings;
-
 use FileHandle;
-
-use Genome;                                 # using the namespace authorizes Class::Autouse to lazy-load modules under it
-
-## Declare global statistics hash ##
+use Genome;
 
 my %stats = ();
-
 my %already_reviewed = ();
 my %wildtype_sites = my %germline_sites = ();
 
@@ -42,215 +23,185 @@ class Genome::Model::Tools::Capture::GermlineModelGroupQc {
 };
 
 sub sub_command_sort_position { 12 }
-
-sub help_brief {                            # keep this to just a few words <---
+sub help_brief {
     "Operate on germline capture model groups"                 
 }
-
 sub help_synopsis {
     return <<EOS
 Operate on capture somatic model groups
 EXAMPLE:	gmt capture germline-model-group-qc --group-id XXXX --output-dir --dbsnp-build
 EOS
 }
-
-sub help_detail {                           # this is what the user will see with the longer version of help. <---
-    return <<EOS 
-
-EOS
+sub help_detail {
+    return;
 }
 
+sub execute {
+    my $self = shift;
 
-################################################################################################
-# Execute - the main program logic
-#
-################################################################################################
+    my $group_id = $self->group_id;
+    my $db_snp_build = $self->dbsnp_build;
+    my $limit_snps_file = $self->limit_snps_file;
+    my $data_source = $self->data_source;
+    my $skip_if_output_present = $self->skip_if_output_present;
+    my $summary_file;
+    if ($self->summary_file) {
+        $summary_file = $self->summary_file;
+        $skip_if_output_present = 1;
+        unless (open(ALL_MODELS,">$summary_file")) {
+            die "Could not open input file '$summary_file' for reading";
+        }
+        print ALL_MODELS "Dbsnp_Build\tSample_id\tSNPsCalled\tWithGenotype\tMetMinDepth\tReference\tRefMatch\tRefWasHet\tRefWasHom\tVariant\tVarMatch\tHomWasHet\tHetWasHom\tVarMismatch\tVarConcord\tRareHomConcord\tOverallConcord\n";
+    }
 
-sub execute {                               # replace with real execution logic.
-	my $self = shift;
+    # Correct the reference build name to what the database recognizes
+    my $reference;
+    my $build_number;
+    if( $db_snp_build <= 130) {
+        $reference = 'reference';
+        $build_number = 36;
+    }
+    else {
+        $reference = 'GRCh37';
+        $build_number = 37;
+    }
+    my %snp_limit_hash;
+    if($self->limit_snps_file) {
+        my $snp_input = new FileHandle ($self->limit_snps_file);
+        unless($snp_input) {
+            $self->error_message("Unable to open ".$self->limit_snps_file);
+            return;
+        }
 
-	my $group_id = $self->group_id;
-	my $db_snp_build = $self->dbsnp_build;
-	my $limit_snps_file = $self->limit_snps_file;
-	my $data_source = $self->data_source;
-	my $skip_if_output_present = $self->skip_if_output_present;
-	my $summary_file;
-	if ($self->summary_file) {
-		$summary_file = $self->summary_file;
-		$skip_if_output_present = 1;
-		unless (open(ALL_MODELS,">$summary_file")) {
-		    die "Could not open input file '$summary_file' for reading";
-		}
-		print ALL_MODELS "Dbsnp_Build\tSample_id\tSNPsCalled\tWithGenotype\tMetMinDepth\tReference\tRefMatch\tRefWasHet\tRefWasHom\tVariant\tVarMatch\tHomWasHet\tHetWasHom\tVarMismatch\tVarConcord\tRareHomConcord\tOverallConcord\n";
-	}
+        while (my $line = <$snp_input>) {
+            chomp($line);
+            my ($id) = split(/\t/, $line);
+            $snp_limit_hash{$id}++;
+        }
+    }
+    ## Get the models in each model group ##
 
-	# Correct the reference build name to what the database recognizes
-	my $reference;
-	my $build_number;
-	if( $db_snp_build <= 130) {
-		$reference = 'reference';
-		$build_number = 36;
-	}
-	else {
-		$reference = 'GRCh37';
-		$build_number = 37;
-	}
-	my %snp_limit_hash;
-	if($self->limit_snps_file) {
-		my $snp_input = new FileHandle ($self->limit_snps_file);
-	        unless($snp_input) {
-	            $self->error_message("Unable to open ".$self->limit_snps_file);
-	            return;
-	        }
+    my $model_group = Genome::ModelGroup->get($group_id);
+    my @model_bridges = $model_group->model_bridges;
 
-		while (my $line = <$snp_input>) {
-			chomp($line);
-			my ($id) = split(/\t/, $line);
-			$snp_limit_hash{$id}++;
-		}
-	}
-	## Get the models in each model group ##
-
-	my $model_group = Genome::ModelGroup->get($group_id);
-	my @model_bridges = $model_group->model_bridges;
-
-	foreach my $model_bridge (@model_bridges)
-	{
-	     my $model = Genome::Model->get($model_bridge->model_id);
-		my $model_id = $model->genome_model_id;
-		my $subject_name = $model->subject_name;
+    foreach my $model_bridge (@model_bridges)
+    {
+        my $model = Genome::Model->get($model_bridge->model_id);
+        my $model_id = $model->genome_model_id;
+        my $subject_name = $model->subject_name;
 #		my $sample_name = $model->sample_name;
-		$subject_name = "Model" . $model_id if(!$subject_name);
-		if ($subject_name =~ m/Pooled/) {next;}
-		if($model->last_succeeded_build_directory) {
-			my $build = $model->last_succeeded_build;
-			my $build_id = $build->id;
-			my $last_build_dir = $model->last_succeeded_build_directory;
-			my $bam_file = $build->whole_rmdup_bam_file;
+        $subject_name = "Model" . $model_id if(!$subject_name);
+        if ($subject_name =~ m/Pooled/) {next;}
+        if($model->last_succeeded_build_directory) {
+            my $build = $model->last_succeeded_build;
+            my $build_id = $build->id;
+            my $last_build_dir = $model->last_succeeded_build_directory;
+            my $bam_file = $build->whole_rmdup_bam_file;
 
-			if($self->output_dir) {
-				my $qc_dir = $self->output_dir . "/$subject_name/";
-				mkdir($qc_dir);
-				my $genofile = "$qc_dir/$subject_name.dbsnp$db_snp_build.genotype";
-				my $qcfile = "$qc_dir/$subject_name.dbsnp$db_snp_build.qc";
-	
-				if ($skip_if_output_present && -s $genofile &&1&&1) { #&&1&&1 to make gedit show colors correctly after a -s check
-				}
-				elsif ($self->summary_file) {
-					die "You specified summary file but the script thinks there are unfinished qc files, please run this script to finish making qc files first\nReason: file $genofile does not exist as a non-zero file\n";
-				}
-				else {
-					open(GENOFILE, ">" . $genofile) or die "Can't open outfile: $!\n";
+            if($self->output_dir) {
+                my $qc_dir = $self->output_dir . "/$subject_name/";
+                mkdir($qc_dir);
+                my $genofile = "$qc_dir/$subject_name.dbsnp$db_snp_build.genotype";
+                my $qcfile = "$qc_dir/$subject_name.dbsnp$db_snp_build.qc";
 
-					my $db_snp_info = GSC::SNP::DB::Info->get( snp_db_build => $db_snp_build );
-					my $type = 'HuRef';
-		 
-					# Get the sample
-					my $organism_sample = GSC::Organism::Sample->get( sample_name => $subject_name );
-	
-					unless ($organism_sample) {
+                if ($skip_if_output_present && -s $genofile &&1&&1) { #&&1&&1 to make gedit show colors correctly after a -s check
+                }
+                elsif ($self->summary_file) {
+                    die "You specified summary file but the script thinks there are unfinished qc files, please run this script to finish making qc files first\nReason: file $genofile does not exist as a non-zero file\n";
+                }
+                else {
+                    open(GENOFILE, ">" . $genofile) or die "Can't open outfile: $!\n";
+
+                    my $db_snp_info = GSC::SNP::DB::Info->get( snp_db_build => $db_snp_build );
+                    my $type = 'HuRef';
+
+                    # Get the sample
+                    my $organism_sample = GSC::Organism::Sample->get( sample_name => $subject_name );
+
+                    unless ($organism_sample) {
 #					    $self->warning_message("failed to find sample $subject_name by external name, trying internal name...");
-					    $organism_sample = GSC::Organism::Sample->get( full_name => $subject_name );
-					    unless( defined $organism_sample ) {
-					        warn "Skipping unrecognized sample name: $subject_name!\n";
-					        next;
-					    }
-					}
-	
-					my @genotypes;
-					if( $data_source eq 'iscan' || $data_source eq 'internal') {
-						@genotypes = $organism_sample->get_genotype;
-					}
-					elsif( $data_source eq 'external') {
-						@genotypes = $organism_sample->get_external_genotype;
-					}
-	
-					# Get all external genotypes for this sample
-					foreach my $genotype (@genotypes) {
-		 
-					    #LSF: For Affymetrix, this will be the birdseed file.
-					    my $ab_file = $genotype->get_genotype_file_ab;
-		 
-					    # Get the data adapter (DataAdapter::GSGMFinalReport class object)
-					    my $filter       = DataAdapter::Result::Filter::Nathan->new();
-					    my $data_adapter = $genotype->get_genotype_data_adapter(
-					         genome_build => $db_snp_info->genome_build,
-					         snp_db_build => $db_snp_info->snp_db_build,
-					         filter       => $filter,
+                        $organism_sample = GSC::Organism::Sample->get( full_name => $subject_name );
+                        unless( defined $organism_sample ) {
+                            warn "Skipping unrecognized sample name: $subject_name!\n";
+                            next;
+                        }
+                    }
+
+                    my @genotypes;
+                    if( $data_source eq 'iscan' || $data_source eq 'internal') {
+                        @genotypes = $organism_sample->get_genotype;
+                    }
+                    elsif( $data_source eq 'external') {
+                        @genotypes = $organism_sample->get_external_genotype;
+                    }
+
+                    # Get all external genotypes for this sample
+                    foreach my $genotype (@genotypes) {
+
+                        #LSF: For Affymetrix, this will be the birdseed file.
+                        my $ab_file = $genotype->get_genotype_file_ab;
+
+                        # Get the data adapter (DataAdapter::GSGMFinalReport class object)
+                        my $filter       = DataAdapter::Result::Filter::Nathan->new();
+                        my $data_adapter = $genotype->get_genotype_data_adapter(
+                            genome_build => $db_snp_info->genome_build,
+                            snp_db_build => $db_snp_info->snp_db_build,
+                            filter       => $filter,
 #					         ( $type ? ( type => $type ) : () ),
-					         ( $reference ? ( type => $reference ) : () ),
-					    );
-		 
-					    # Next if there is no genotype data.
-					    next unless($data_adapter);
-					 
-					    # Loop through the result row (DataAdapter::Result::GSGMFinalReport class object)
-						    while ( my $result = $data_adapter->next_result ) {
-							if (!$self->limit_snps_file || defined $snp_limit_hash{$result->snp_name}) {
-							        print GENOFILE join "\t",
-							            ( $result->chromosome, $result->position, $result->alleles, $result->snp_name);
-							        print GENOFILE "\n";
-							}
-						    }
-					}
-				close(GENOFILE);
-				}
+                            ( $reference ? ( type => $reference ) : () ),
+                        );
 
-				my $bsub = "bsub -N -M 4000000 -J $subject_name.dbsnp$db_snp_build.qc -o $qc_dir/$subject_name.dbsnp$db_snp_build.qc.out -e $qc_dir/$subject_name.dbsnp$db_snp_build.qc.err -R \"select[model!=Opteron250 && type==LINUX64 && mem>4000 && tmp>1000] rusage[mem=4000, tmp=1000]\"";
-				my $cmd = $bsub." \'"."gmt analysis lane-qc compare-snps --genotype-file $genofile --bam-file $bam_file --output-file $qcfile --sample-name $subject_name --min-depth-het 20 --min-depth-hom 20 --flip-alleles 1 --verbose 1 --reference-build $build_number"."\'";
-				if ($skip_if_output_present && -s $qcfile &&1&&1) { #&&1&&1 to make gedit show colors correctly after a -s check
-				}
-				elsif ($self->summary_file) {
-					die "You specified summary file but the script thinks there are unfinished qc files, please run this script to finish making qc files first\nReason: file $qcfile does not exist as a non-zero file\n";
-				}
-				else {
-					system("$cmd");
-				}
-#	$return = Genome::Sys->shellcmd(
-#                          cmd => "$cmd",
-#                          output_files => [$qc_file],
-#                          skip_if_output_is_present => $skip_if_output_present,
-#                     );
-#	unless($return) { 
-#		$self->error_message("Failed to execute QC: QC Returned $return");
-#		die $self->error_message;
-#		die "Failed to execute QC: QC Returned $return";
-#	}
+                        # Next if there is no genotype data.
+                        next unless($data_adapter);
 
-				if ($self->summary_file) {
-					my $qc_input = new FileHandle ($qcfile);
-					my $qc_header = <$qc_input>;
-					my $qc_line = <$qc_input>;
-					chomp($qc_line);
-					print ALL_MODELS "$db_snp_build\t$qc_line\n";
-				}
-			}
-		}
+                        # Loop through the result row (DataAdapter::Result::GSGMFinalReport class object)
+                        while ( my $result = $data_adapter->next_result ) {
+                            if (!$self->limit_snps_file || defined $snp_limit_hash{$result->snp_name}) {
+                                print GENOFILE join "\t",
+                                ( $result->chromosome, $result->position, $result->alleles, $result->snp_name);
+                                print GENOFILE "\n";
+                            }
+                        }
+                    }
+                    close(GENOFILE);
+                }
 
-          UR::Context->commit() or die 'commit failed';
-          UR::Context->clear_cache(dont_unload => ['Genome::ModelGroup', 'Genome::ModelGroupBridge']);
-	}
-	if ($self->summary_file) {
-		close(ALL_MODELS);
-	}
-	return 1;
+                my $bsub = "bsub -N -M 4000000 -J $subject_name.dbsnp$db_snp_build.qc -o $qc_dir/$subject_name.dbsnp$db_snp_build.qc.out -e $qc_dir/$subject_name.dbsnp$db_snp_build.qc.err -R \"select[model!=Opteron250 && type==LINUX64 && mem>4000 && tmp>1000] rusage[mem=4000, tmp=1000]\"";
+                my $cmd = $bsub." \'"."gmt analysis lane-qc compare-snps --genotype-file $genofile --bam-file $bam_file --output-file $qcfile --sample-name $subject_name --min-depth-het 20 --min-depth-hom 20 --flip-alleles 1 --verbose 1 --reference-build $build_number"."\'";
+                if ($skip_if_output_present && -s $qcfile &&1&&1) { #&&1&&1 to make gedit show colors correctly after a -s check
+                }
+                elsif ($self->summary_file) {
+                    die "You specified summary file but the script thinks there are unfinished qc files, please run this script to finish making qc files first\nReason: file $qcfile does not exist as a non-zero file\n";
+                }
+                else {
+                    system("$cmd");
+                }
+                if ($self->summary_file) {
+                    my $qc_input = new FileHandle ($qcfile);
+                    my $qc_header = <$qc_input>;
+                    my $qc_line = <$qc_input>;
+                    chomp($qc_line);
+                    print ALL_MODELS "$db_snp_build\t$qc_line\n";
+                }
+            }
+        }
+
+        UR::Context->commit() or die 'commit failed';
+        UR::Context->clear_cache(dont_unload => ['Genome::ModelGroup', 'Genome::ModelGroupBridge']);
+    }
+    if ($self->summary_file) {
+        close(ALL_MODELS);
+    }
+    return 1;
 }
-
-
-
-
-
 
 sub byChrPos
 {
-	my ($chr_a, $pos_a) = split(/\t/, $a);
-	my ($chr_b, $pos_b) = split(/\t/, $b);
-	
-	$chr_a cmp $chr_b
-	or
-	$pos_a <=> $pos_b;
+    my ($chr_a, $pos_a) = split(/\t/, $a);
+    my ($chr_b, $pos_b) = split(/\t/, $b);
+
+    $chr_a cmp $chr_b
+        or
+    $pos_a <=> $pos_b;
 }
-
-
 1;
-
