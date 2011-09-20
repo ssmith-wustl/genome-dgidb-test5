8dde7abb308c7c0b924735ac98f3948badba2c8b
Jim Weible <jweible@genome.wustl.edu>
2011-09-20 13:54:46 -0500

    added snapshot_date to genbank build creation

--- lib/perl/Genome/Model/Event/Build/ImportedAnnotation/Run.pm
+++ lib/perl/Genome/Model/Event/Build/ImportedAnnotation/Run.pm
@@ -79,6 +79,8 @@ sub execute {
         confess "Could not get annotation data directory for build" unless defined $annotation_data_directory;
         my $sym_rv = symlink $annotation_data_source_directory, $annotation_data_directory;
         confess "Could not create symlink from $annotation_data_source_directory to $annotation_data_directory" unless $sym_rv;
+        my $snapshot_date = $self->calculate_snapshot_date($genbank_file);
+        $build->snapshot_date($snapshot_date);
     }
     elsif ($source =~ /^ensembl$/i) {
         my ($host, $user, $pass) = $self->get_ensembl_info($version);
@@ -189,6 +191,14 @@ sub generate_rna_seq_files {
     }
 
     return 1;
+}
+
+sub calculate_snapshot_date {
+    my ($self, $genbank_file) = @_;
+    my $output = `ls -l $genbank_file`;
+    my @parts = split(" ", $output);
+    my $date = $parts[5];
+    return $date;
 }
 
 1;
