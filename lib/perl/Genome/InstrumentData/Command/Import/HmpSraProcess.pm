93d347bd385079c36f2e1230374fb76d8a59ffb2
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2011-09-27 12:44:04 -0500

    Forgot to prefix warehouse tables with 'gsc.'

--- lib/perl/Genome/InstrumentData/Command/Import/HmpSraProcess.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/HmpSraProcess.pm
@@ -62,7 +62,7 @@ sub execute {
         my $sample = Genome::Sample->get(sql=>qq/
             select os.*
             from gsc.organism_sample os
-            join sra_organism_sample sos on sos.organism_sample_id=os.organism_sample_id
+            join gsc.sra_organism_sample sos on sos.organism_sample_id=os.organism_sample_id
             join gsc.sra_experiment ex on ex.sra_sample_id=sos.sra_sample_id
             join gsc.sra_run ru on ru.sra_experiment_id=ex.sra_item_id
             join gsc.sra_item rui on rui.sra_item_id=ru.sra_item_id
@@ -86,7 +86,7 @@ sub execute {
     my $dbh = Genome::DataSource::GMSchema->get_default_handle();
     my ($fc_id, $lane) = $dbh->selectrow_array(qq/select ii.flow_cell_id, ii.lane
     from gsc.organism_sample os 
-    join sra_organism_sample sos on sos.organism_sample_id=os.organism_sample_id
+    join gsc.sra_organism_sample sos on sos.organism_sample_id=os.organism_sample_id
     join gsc.sra_experiment ex on ex.sra_sample_id=sos.sra_sample_id
     join gsc.sra_run ru on ru.sra_experiment_id=ex.sra_item_id 
     join gsc.sra_item rui on rui.sra_item_id=ru.sra_item_id 
