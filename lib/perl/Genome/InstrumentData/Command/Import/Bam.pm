0317378fda3bcd86b17a5abcc86493230f961ffc
Rob Long <rlong@genome.wustl.edu>
2011-09-26 21:24:55 -0500

    added genome model reference-alignment command downsample
    gmt picard -- added version 1.52 to allow use of DownsampleSam
    
    gmt picard downsample is added to wrap DownsampleSam
    
    added a call to gmt picard downsample
    
    Changed return value to be the instrument-data id of the imported bam.
    
    Added bam importation code.

--- lib/perl/Genome/InstrumentData/Command/Import/Bam.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/Bam.pm
@@ -91,7 +91,6 @@ class Genome::InstrumentData::Command::Import::Bam {
 
 sub execute {
     my $self = shift;
-
     # If the target region is set to whole genome, then we want the imported instrument data's
     # target_region_set_name column set to undef. Otherwise, we need to make sure the target region
     # name corresponds to only one Genome::FeatureList.
@@ -108,7 +107,6 @@ sub execute {
         $self->error_message("Unable to find the sample name based on the parameter: ".$self->sample);
         my $possible_name;
         if($self->sample =~ /TCGA/){
-            print "got here.\n";
             $possible_name = GSC::Organism::Sample->get(sample_name => $self->sample);
             if($possible_name){
                 $self->error_message("There is an organism_sample which matches the TCGA name, which has a full_name of ".$possible_name->full_name);
@@ -137,7 +135,6 @@ sub execute {
             die $self->error_message;
         }
         $self->status_message("Created a library named ".$library->name);
-        print $self->status_message;
     }
 
     unless (-s $bam_path and $bam_path =~ /\.bam$/) {
@@ -166,6 +163,7 @@ sub execute {
     $params{reference_sequence_build_id} = $self->reference_sequence_build_id if $self->reference_sequence_build_id;
     $params{library_id} = $library->id;
     $params{target_region_set_name} = $self->target_region;
+    delete $params{sample} if exists($params{sample});
     
     my $import_instrument_data = Genome::InstrumentData::Imported->create(%params);  
     unless ($import_instrument_data) {
@@ -267,7 +265,7 @@ sub execute {
     $self->status_message("Importation of BAM completed successfully.");
     $self->status_message("Your instrument-data id is ".$instrument_data_id);
 
-    return 1;
+    return $instrument_data_id;
 }
 
 sub _add_stats {
