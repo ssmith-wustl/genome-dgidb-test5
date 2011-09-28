1bd8ca5a090341324019da700c99690000bb9ff8
Nathan Nutter <nnutter@genome.wustl.edu>
2011-09-28 10:21:16 -0500

    removed some files that look like they were accidentally committed

--- lib/perl/Genome/project-model-group.sql
+++
@@ -1,9 +0,0 @@
---uuid
-select *
-from model_group g
---join genome_project p on p.name = g.name
-where g.uuid is null;
-
---user name
-select * from model_group where user_name is null;
-
--- lib/perl/Genome/sketkark_insert.sql
+++
@@ -1,2 +0,0 @@
-select * from genome_sys_user where email like 'sket%';
-insert into genome_sys_user (name, email) values ('Shamika Ketkar-Kulkarni', 'sketkark@genome.wustl.edu');
