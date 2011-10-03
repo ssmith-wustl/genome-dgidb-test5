531bcc89a3de838f570f502fa831b3d20d8441d0
Brian Derickson <bdericks@genome.wustl.edu>
2011-10-03 18:23:28 -0500

    added slurp method to reader

--- lib/perl/Genome/Data/Reader.pm
+++ lib/perl/Genome/Data/Reader.pm
@@ -16,5 +16,15 @@ sub next {
     return $adaptor->parse_next_from_file(@_);
 }
 
+sub slurp {
+    my $self = shift;
+    my $adaptor = $self->data_adaptor();
+    my @objects;
+    while (my $object = $adaptor->parse_next_from_file(@_)) {
+        push @objects, $object;
+    }
+    return @objects;
+}
+
 1;
 
