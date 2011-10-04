83098d7bcddc63ae939bd0b9dfe407d82143fa7b
Eddie Belter <ebelter@genome.wustl.edu>
2011-10-04 17:40:11 -0500

    SX: bam reader uses cmd for file syntax

--- lib/perl/Genome/Model/Tools/Sx/BamReader.pm
+++ lib/perl/Genome/Model/Tools/Sx/BamReader.pm
@@ -5,30 +5,21 @@ use warnings;
 
 use Genome;
 
+require File::Which;
+
 class Genome::Model::Tools::Sx::BamReader {
     is => 'Genome::Model::Tools::Sx::SamReader',
 };
 
-sub create {
-    my $class = shift;
+sub _cmd_for_file {
+    my ($self, $file) = @_;
 
-    my $self = $class->SUPER::create(@_);
-    return if not $self;
-
-    my $file = $self->file; 
-    if ( not $file ) {
-        $self->error_message("File is required");
-        return;
+    my $executable = File::Which::which('samtools');
+    if ( not $executable ) {
+        die "Failed to find executable 'samtools'. Cannot read from BAM file: $file";
     }
-    my $cmd = 'samtools view '.$file.' |';
-    my $fh = IO::File->new($cmd);
-    if ( not $fh ) {
-        $self->error_message("Failed to open samtools command with file ($file): $@");
-        return;
-    }
-    $self->{_file} = $fh;
 
-    return $self;
+    return'samtools view '.$file.' |';
 }
 
 1;
