35f5d87b6e0bf230ef7db05c44ca3b9f984b0ef1
Eddie Belter <ebelter@genome.wustl.edu>
2011-10-06 13:36:04 -0500

    MC16s: validate amplicon processor when creating a pp

--- lib/perl/Genome/ProcessingProfile/MetagenomicComposition16s.pm
+++ lib/perl/Genome/ProcessingProfile/MetagenomicComposition16s.pm
@@ -78,9 +78,18 @@ sub create {
         $self->$method; # dies if error
     }
     
+    # Validate amplicon processor
+    $self->status_message('Validate amplicon processor: '.$self->amplicon_processor);
+    if ( not eval { $self->amplicon_processor_commands } ) {
+        $self->error_message('Failed to validate amplicon processor: '.$self->amplicon_processor);
+        $self->delete;
+        return;
+    }
+    $self->status_message('Validate amplicon processor OK');
+
     # Validate classifier version
     # TODO
-    
+
     return $self;
 }
 
