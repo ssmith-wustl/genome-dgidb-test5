17d3b66c7fcda13a71522d560d04433beeec765d
Nathan Nutter <nnutter@genome.wustl.edu>
2011-10-19 15:56:19 -0500

    more object_cache_size changes, removing from create and adding to execute

--- lib/perl/Genome/Model/Tools/Annotate/TranscriptVariants.pm
+++ lib/perl/Genome/Model/Tools/Annotate/TranscriptVariants.pm
@@ -349,8 +349,8 @@ sub execute {
     my $self = shift;
 
     # enable object cache pruning
-    UR::Context->object_cache_size_highwater(200_000);
-    UR::Context->object_cache_size_lowwater(20_000);
+    my ($orig_lowater, $orig_highwater) = Genome::Model::Tools::Annotate->object_cache_sizes();
+    Genome::Model::Tools::Annotate->object_cache_sizes(20_000, 200_000);
 
     unless($self->variant_file xor $self->variant_bed_file){
         $self->error_message("Please specify either a --variant-file or a --variant-bed-file");
@@ -622,6 +622,7 @@ sub execute {
         $self->status_message("$processed_variants variants processed " . scalar(localtime)) unless ($processed_variants % 10000);
     }
     $we_are_done_flag = 1;
+    Genome::Model::Tools::Annotate->object_cache_sizes($orig_lowater, $orig_highwater);
 
     my $annotation_loop_stop_time = time();
 
