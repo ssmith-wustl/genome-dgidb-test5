b42c24a5e32235337618c01801d7f499a580beb8
Scott Smith <ssmith@genome.wustl.edu>
2011-10-21 18:06:31 -0500

    restored GENOME_DB_ENSEMBL_HOST.  fixed typo in GENOME_SYS_SERVICES_MEMCACHE.

--- lib/perl/Genome/Env/GENOME_SYS_SERVICES_MEMCACHE.pm
+++ lib/perl/Genome/Env/GENOME_SYS_SERVICES_MEMCACHE.pm
@@ -2,11 +2,11 @@
 
 =head1 NAME
 
-GENOME_SYS_SERVICES_MEMCACHED
+GENOME_SYS_SERVICES_MEMCACHE
 
 =head1 DESCRIPTION
 
-The GENOME_SYS_SERVICES_MEMCACHED environment variable points to the host:port of the memcached server, if present. 
+The GENOME_SYS_SERVICES_MEMCACHE environment variable points to the host:port of the memcached server, if present. 
 
 =head1 DEFAULT VALUE
 
