1ac836fb60c561efadeed74f58595118beeccd84
Eddie Belter <ebelter@genome.wustl.edu>
2011-10-26 17:33:31 -0500

    AQID: Stop processing 454 negative control

--- lib/perl/Genome/Model/Command/Services/AssignQueuedInstrumentData.pm
+++ lib/perl/Genome/Model/Command/Services/AssignQueuedInstrumentData.pm
@@ -230,6 +230,13 @@ sub execute {
         if ($subject_class_name and $subject_id and @processing_profile_ids) {
             my $subject      = $subject_class_name->get($subject_id);
 
+            if ( $instrument_data_type =~ /454/i and $subject->name eq 'n-ctrl' ) { 
+                print Data::Dumper::Dumper($subject);
+                # Do not process 454 negative control (n-ctrl)
+                $self->status_message('Skipping n-ctrl PSE '.$pse->id);
+                next PSE;
+            }
+
             PP:
             foreach my $processing_profile_id (@processing_profile_ids) {
                 my $processing_profile = Genome::ProcessingProfile->get( $processing_profile_id );
