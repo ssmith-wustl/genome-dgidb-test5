ffcebd8895cf212ec680db314968039efd840a9f
Jim Weible <jweible@genome.wustl.edu>
2011-10-27 12:34:57 -0500

    DGI aggregator classes

--- lib/perl/Genome/DrugGeneInteraction.pm
+++ lib/perl/Genome/DrugGeneInteraction.pm
@@ -9,38 +9,23 @@ class Genome::DrugGeneInteraction {
     is => 'UR::Object',
     id_generator => '-uuid',
     table_name => 'drug_gene_interaction',
-    schema_name => 'public',
+    schema_name => 'subject',
     data_source => 'Genome::DataSource::Main',
     id_by => [
         id => { is => 'Number' },
     ],
     has => [
-        drug_name_id => { is => 'Text'},
-        drug_name => {
-            is => 'Genome::DrugName',
-            id_by => 'drug_name_id',
-            constraint_name => 'drug_gene_interaction_drug_name_id_fkey',
-        },
-        gene_name_id => { is => 'Text'},
-        gene_name => {
-            is => 'Genome::GeneName',
-            id_by => 'gene_name_id',
-            constraint_name => 'drug_gene_interaction_gene_name_id_fkey',
-        },
-        interaction_type => { is => 'Text'}, 
-        description => { is => 'Text' },
-        drug_gene_interaction_attributes => {
-            is => 'Genome::DrugGeneInteractionAttribute',
-            reverse_as => 'drug_gene_interaction',
-            is_many => 1,
+        drug_name => { is => 'Text'},
+        gene_name => { is => 'Text'},
+#TODO: accessors for the associated Genome::GeneName and Genome::DrugName
+        drug_gene_interaction_reports => {
+            calculate_from => ['drug_name', 'gene_name'],
+            calculate => q|
+                return Genome::DrugGeneInteractionReport->get(gene_name_report.id => $gene_name, drug_name_report.id => $drug_name);
+            |,
         },
     ],
     doc => 'Claim regarding an interaction between a drug name and a gene name',
 };
-
-sub __display_name__ {
-    my $self = shift;
-    return "Interaction of " . $self->drug_name->__display_name__ . " and " . $self->gene_name->__display_name__;
-}
 
 1;
