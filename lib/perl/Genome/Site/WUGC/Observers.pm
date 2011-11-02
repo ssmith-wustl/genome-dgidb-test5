65ba1a4131086de4e3be487f60d5a8b8c908c9a2
Anthony Brummett <abrummet@genome.wustl.edu>
2011-11-02 15:21:38 -0500

    Delay adding observers for Genome::ModelGroup and Genome::Project until they load
    Doing it this way avoids loading both the class and the observers if you're
    not using those classes

--- lib/perl/Genome/Site/WUGC/Observers.pm
+++ lib/perl/Genome/Site/WUGC/Observers.pm
@@ -3,8 +3,18 @@ package Genome::Site::WUGC::Observers;
 use strict;
 use warnings;
 
-use Genome::Site::WUGC::Observers::ModelGroup;
-use Genome::Site::WUGC::Observers::Project;
+UR::Object::Type->add_observer(
+    aspect => 'load',
+    callback => sub {
+        my $meta = shift;
+        my $class_name = $meta->class_name;
+        if ($class_name eq 'Genome::ModelGroup') {
+            eval "use Genome::Site::WUGC::Observers::ModelGroup;";
+        } elsif ($class_name eq 'Genome::Project') {
+            eval "use Genome::Site::WUGC::Observers::Project;";
+        }
+    },
+);
 
 1;
 
