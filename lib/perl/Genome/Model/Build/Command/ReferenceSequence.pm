875172ec8022453c70b5a76803d0a761d17db59f
Brian Derickson <bdericks@genome.wustl.edu>
2011-11-08 16:14:34 -0600

    added reference sequence lister

---
+++ lib/perl/Genome/Model/Build/Command/ReferenceSequence.pm
@@ -0,0 +1,13 @@
+package Genome::Model::Build::Command::ReferenceSequence;
+
+use strict;
+use warnings;
+use Genome;
+
+class Genome::Model::Build::Command::ReferenceSequence {
+    is => 'Command::Tree',
+    doc => 'Commands for reference sequence builds',
+};
+
+1;
+
