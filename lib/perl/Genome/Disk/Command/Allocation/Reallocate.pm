aab587af5549bd3df7b4d18666b8cfdd5cd0e930
Brian Derickson <bdericks@genome.wustl.edu>
2011-11-08 13:43:05 -0600

    removed force move from reallocate command

--- lib/perl/Genome/Disk/Command/Allocation/Reallocate.pm
+++ lib/perl/Genome/Disk/Command/Allocation/Reallocate.pm
@@ -26,11 +26,6 @@ class Genome::Disk::Command::Allocation::Reallocate {
             default => 0,
             doc => 'Allow the allocation to be moved to a new volume if current volume is too small.',
         },
-        force_move => {
-            is => 'Boolean',
-            default => 0,
-            doc => 'If set, allocations will be moved to a new volume regardless of available space',
-        },
     ],
     doc => 'This command changes the requested kilobytes for a target allocation',
 };
@@ -61,7 +56,6 @@ sub execute {
         $params{allocation_id} = $allocation->id;
         $params{kilobytes_requested} = $self->kilobytes_requested if defined $self->kilobytes_requested;
         $params{allow_reallocate_with_move} = $self->allow_reallocate_with_move;
-        $params{force_move} = $self->force_move;
 
         my $transaction = UR::Context::Transaction->begin();
         my $successful = eval {Genome::Disk::Allocation->reallocate(%params) };
