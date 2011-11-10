7ac4cbcec387bb4689f5207c575161a26a7b87e0
Jim Weible <jweible@genome.wustl.edu>
2011-11-10 09:03:08 -0600

    added GeneNameInteractionAssociation structure

---
+++ lib/perl/Genome/DruggableGene/Command/GeneNameInteractionAssociation/GenerateGeneNameInteractionAssociations.pm
@@ -0,0 +1,34 @@
+package Genome::DruggableGene::Command::GeneNameInteractionAssociation::GenerateGeneNameInteractionAssociations;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::DruggableGene::Command::GeneNameInteractionAssociation::GenerateGeneNameInteractionAssociations {
+    is => 'Genome::Command::Base',
+    has => [
+
+    ],
+};
+
+
+sub help_brief {
+    #TODO: write me
+}
+
+sub help_synopsis {
+    #TODO: write me
+}
+
+sub help_detail {
+    #TODO: write me
+}
+
+sub execute {
+    my $self = shift;
+    #TODO: iterate everything and make the table 
+    
+}
+
+1;
