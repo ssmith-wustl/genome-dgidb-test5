d34b778c5f10e28956192a2c5ab64ca75cff3e60
Rob Long <rlong@genome.wustl.edu>
2011-11-14 13:47:50 -0600

    genome instrument-data composite workflow -- fixed bug where options were not being passed through

--- lib/perl/Genome/InstrumentData/Composite/Workflow.pm
+++ lib/perl/Genome/InstrumentData/Composite/Workflow.pm
@@ -323,7 +323,7 @@ sub _create_operations_for_alignment_tree {
 
     return @operations if exists $tree->{$self->_operation_key($instrument_data, %options)}; #generation already complete for this subtree
 
-    push @operations, $self->_generate_operation($tree, $instrument_data);
+    push @operations, $self->_generate_operation($tree, $instrument_data, %options);
     if(exists $tree->{parent}) {
         push @operations, $self->_create_operations_for_alignment_tree($tree->{parent}, $instrument_data);
     }
