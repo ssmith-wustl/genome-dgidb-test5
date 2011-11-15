35513e1e4a080bbd61b31f1a8dc386e54a77d3b5
James Koval <jkoval@genome.wustl.edu>
2011-11-15 14:32:47 -0600

    Reasonably reduce error message max length stored

--- lib/perl/Genome/Model/Build/ErrorLogEntry.pm
+++ lib/perl/Genome/Model/Build/ErrorLogEntry.pm
@@ -18,14 +18,14 @@ class Genome::Model::Build::ErrorLogEntry {
         auto_truncate_inferred_message => { is => 'Boolean', default => '1', is_transient => 1},
     ],
     has_optional => [
-        message     => { is => 'Text', len => 4000, default => '' },
+        message     => { is => 'Text', len => 1000, default => '' },
         line        => { is => 'Integer' },
         file        => { is => 'Text' },
         package     => { is => 'Text' },
         subroutine  => { is => 'Text' },
 
         #The die message is parsed with a regex to glean extra information
-        inferred_message => { is => 'Text', len => 4000, default => '' },
+        inferred_message => { is => 'Text', len => 1000, default => '' },
         inferred_file => { is => 'Text' },
         inferred_line => { is => 'Integer' },
 
