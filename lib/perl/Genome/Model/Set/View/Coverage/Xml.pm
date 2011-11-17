a693cf461cad3928943e9ef7a12021fb663eb843
Thomas Mooney <tmooney@genome.wustl.edu>
2011-11-16 18:07:17 -0600

    Support result-based coverage builds in the view.

--- lib/perl/Genome/Model/Set/View/Coverage/Xml.pm
+++ lib/perl/Genome/Model/Set/View/Coverage/Xml.pm
@@ -50,7 +50,6 @@ sub _generate_content {
     my @members = $self->members;
     my @model_ids = map($_->id, @members);
     my @builds = Genome::Model::Build->get(model_id => \@model_ids);
-    my @events = Genome::Model::Event->get(build_id => [map($_->id, @builds)]);
 
     my $xml_doc = XML::LibXML->createDocument();
     $self->_xml_doc($xml_doc);
@@ -90,12 +89,20 @@ sub get_last_succeeded_coverage_stats_build_from_model {
     my $model = shift;
     my @sorted_builds = sort { $b->id <=> $a->id } $model->builds();
     for my $build (@sorted_builds) {
-        my @events = $build->the_events;
-        my @coverage_stats_events = grep { $_->class eq 'Genome::Model::Event::Build::ReferenceAlignment::CoverageStats' } @events;
-        if (scalar(@coverage_stats_events) == 1) {
-            my $coverage_stats_event = $coverage_stats_events[0];
-            if ($coverage_stats_event->event_status eq 'Succeeded') {
-                return $build;
+        my @r = $build->result_users('software_result.subclass_name' => 'Genome::InstrumentData::AlignmentResult::Merged::CoverageStats');
+        if(scalar @r) {
+            return $build;
+        }
+
+        #old way of storing coverage report
+        if($build->isa('Genome::Model::Build::ReferenceAlignment')){
+            my @events = $build->the_events;
+            my @coverage_stats_events = grep { $_->class eq 'Genome::Model::Event::Build::ReferenceAlignment::CoverageStats' } @events;
+            if (scalar(@coverage_stats_events) == 1) {
+                my $coverage_stats_event = $coverage_stats_events[0];
+                if ($coverage_stats_event->event_status eq 'Succeeded') {
+                    return $build;
+                }
             }
         }
     }
@@ -114,93 +121,105 @@ sub get_enrichment_factor_node {
 
         if ($build) {
             push(@included_models, $model);
-            my $model_node = $ef_node->addChild( $xml_doc->createElement('model') );
-            $model_node->addChild( $xml_doc->createAttribute('id', $model->id) );
-            $model_node->addChild( $xml_doc->createAttribute('subject_name', $model->subject_name) );
-            $model_node->addChild( $xml_doc->createAttribute('model_name',$model->name));
 
-            my @idata = $model->instrument_data;
+            my @results = $self->_results_for_build($build);
 
-            $model_node->addChild( $xml_doc->createAttribute('lane_count', scalar(@idata)));
+            for my $result (@results) {
+                my $model_node = $self->_get_model_node($model, $result);
+                $ef_node->addChild( $model_node );
 
-            # get BED file
-            my $bedf;
-            my $refcovd = $build->data_directory . "/reference_coverage";
-            opendir(my $refcovdh, $refcovd) or die "Could not open reference coverage directory at $refcovd";
+                # get BED file
+                my $bedf;
+                my $refcovd;
 
-            while (my $file = readdir($refcovdh)) {
-                if ($file =~ /.*.bed/) { $bedf = $refcovd . "/" . $file; }
-            }
+                if($result->isa('Genome::SoftwareResult')) {
+                    $refcovd = $result->output_dir;
+                } else {
+                    $refcovd = $result->reference_coverage_directory;
+                }
+                opendir(my $refcovdh, $refcovd) or die "Could not open reference coverage directory at $refcovd";
 
-            # calculate target_total_bp
-            my $target_total_bp;
-
-            open(my $bedfh, "<", $bedf) or die "Could not open BED file at $bedf";
-
-            while (<$bedfh>) {
-                chomp;
-                my @f      = split (/\t/, $_);
-                my $start  = $f[1];
-                my $stop   = $f[2];
-                my $length = ($stop - $start);
-                $target_total_bp += $length;
-            }
-
-            # calculate genome_total_bp from reference sequence seqdict.sam
-            my $genome_total_bp;
-            my $seqdictf = $build->model->reference_sequence_build->data_directory . "/seqdict/seqdict.sam";
-
-            my $genomes_total_bp = $self->_genomes_total_bp;
-            if($genomes_total_bp and $genomes_total_bp->{$seqdictf}) {
-                $genome_total_bp = $genomes_total_bp->{$seqdictf};
-            } else {
-                open(my $seqdictfh, "<", $seqdictf) or die "Could not open seqdict at $seqdictf";
-
-                while (<$seqdictfh>) {
-                    chomp;
-                    unless($_ =~ /$@HD/) { # skip the header row
-                        my @f = split(/\t/, $_);
-                        my $ln = $f[2];
-                        $ln =~ s/LN://;
-                        $genome_total_bp += $ln;
-                    }
+                while (my $file = readdir($refcovdh)) {
+                    if ($file =~ /.*.bed/) { $bedf = $refcovd . "/" . $file; }
                 }
 
-                $genomes_total_bp->{$seqdictf} = $genome_total_bp;
-                $self->_genomes_total_bp($genomes_total_bp);
+                # calculate target_total_bp
+                my $target_total_bp;
+
+                open(my $bedfh, "<", $bedf) or die "Could not open BED file at $bedf";
+
+                while (<$bedfh>) {
+                    chomp;
+                    my @f      = split (/\t/, $_);
+                    my $start  = $f[1];
+                    my $stop   = $f[2];
+                    my $length = ($stop - $start);
+                    $target_total_bp += $length;
+                }
+
+                # calculate genome_total_bp from reference sequence seqdict.sam
+                my $genome_total_bp;
+
+                my $refseq_build;
+                if($result->isa('Genome::SoftwareResult')) {
+                    $refseq_build = $result->alignment_result->reference_build;
+                } else {
+                    $refseq_build = $result->reference_sequence_build;
+                }
+
+                my $seqdictf = $refseq_build->data_directory . "/seqdict/seqdict.sam";
+
+                my $genomes_total_bp = $self->_genomes_total_bp;
+                if($genomes_total_bp and $genomes_total_bp->{$seqdictf}) {
+                    $genome_total_bp = $genomes_total_bp->{$seqdictf};
+                } else {
+                    open(my $seqdictfh, "<", $seqdictf) or die "Could not open seqdict at $seqdictf";
+
+                    while (<$seqdictfh>) {
+                        chomp;
+                        unless($_ =~ /$@HD/) { # skip the header row
+                            my @f = split(/\t/, $_);
+                            my $ln = $f[2];
+                            $ln =~ s/LN://;
+                            $genome_total_bp += $ln;
+                        }
+                    }
+
+                    $genomes_total_bp->{$seqdictf} = $genome_total_bp;
+                    $self->_genomes_total_bp($genomes_total_bp);
+                }
+
+                # get wingspan 0 alignment metrics
+                my $ws_zero = $result->alignment_summary_hash_ref->{'0'};
+
+                # calculate enrichment factor!
+                my $myEF = Genome::Model::Tools::TechD::CaptureEnrichmentFactor->execute(
+                    capture_unique_bp_on_target    => $ws_zero->{'unique_target_aligned_bp'},
+                    capture_duplicate_bp_on_target => $ws_zero->{'duplicate_target_aligned_bp'},
+                    capture_total_bp               => $ws_zero->{'total_aligned_bp'} + $ws_zero->{'total_unaligned_bp'},
+                    target_total_bp                => $target_total_bp,
+                    genome_total_bp                => $genome_total_bp
+                );
+
+                my $theoretical_max_enrichment_factor = 0;
+                my $unique_on_target_enrichment_factor = 0;
+                my $total_on_target_enrichment_factor = 0;
+
+                if ($myEF) {
+                    $theoretical_max_enrichment_factor  = $myEF->theoretical_max_enrichment_factor();
+                    $unique_on_target_enrichment_factor = $myEF->unique_on_target_enrichment_factor();
+                    $total_on_target_enrichment_factor  = $myEF->total_on_target_enrichment_factor();
+                }
+
+                my $uotef_node = $model_node->addChild( $xml_doc->createElement('unique_on_target_enrichment_factor') );
+                $uotef_node->addChild( $xml_doc->createTextNode( $unique_on_target_enrichment_factor ) );
+
+                my $totef_node = $model_node->addChild( $xml_doc->createElement('total_on_target_enrichment_factor') );
+                $totef_node->addChild( $xml_doc->createTextNode( $total_on_target_enrichment_factor ) );
+
+                my $tmef_node = $model_node->addChild( $xml_doc->createElement('theoretical_max_enrichment_factor') );
+                $tmef_node->addChild( $xml_doc->createTextNode( $theoretical_max_enrichment_factor ) );
             }
-
-            # get wingspan 0 alignment metrics
-            my $ws_zero = $build->alignment_summary_hash_ref->{'0'};
-
-            # calculate enrichment factor!
-            my $myEF = Genome::Model::Tools::TechD::CaptureEnrichmentFactor->execute(
-                capture_unique_bp_on_target    => $ws_zero->{'unique_target_aligned_bp'},
-                capture_duplicate_bp_on_target => $ws_zero->{'duplicate_target_aligned_bp'},
-                capture_total_bp               => $ws_zero->{'total_aligned_bp'} + $ws_zero->{'total_unaligned_bp'},
-                target_total_bp                => $target_total_bp,
-                genome_total_bp                => $genome_total_bp
-            );
-
-            my $theoretical_max_enrichment_factor = 0;
-            my $unique_on_target_enrichment_factor = 0;
-            my $total_on_target_enrichment_factor = 0;
-
-            if ($myEF) {
-                $theoretical_max_enrichment_factor  = $myEF->theoretical_max_enrichment_factor();
-                $unique_on_target_enrichment_factor = $myEF->unique_on_target_enrichment_factor();
-                $total_on_target_enrichment_factor  = $myEF->total_on_target_enrichment_factor();
-            }
-
-            my $uotef_node = $model_node->addChild( $xml_doc->createElement('unique_on_target_enrichment_factor') );
-            $uotef_node->addChild( $xml_doc->createTextNode( $unique_on_target_enrichment_factor ) );
-
-            my $totef_node = $model_node->addChild( $xml_doc->createElement('total_on_target_enrichment_factor') );
-            $totef_node->addChild( $xml_doc->createTextNode( $total_on_target_enrichment_factor ) );
-
-            my $tmef_node = $model_node->addChild( $xml_doc->createElement('theoretical_max_enrichment_factor') );
-            $tmef_node->addChild( $xml_doc->createTextNode( $theoretical_max_enrichment_factor ) );
-
         }
     }
 
@@ -218,27 +237,25 @@ sub get_alignment_summary_node {
         my $build = $self->get_last_succeeded_coverage_stats_build_from_model($model);
         if ($build) {
             push(@included_models, $model);
-            my $model_node = $as_node->addChild( $xml_doc->createElement('model') );
-            $model_node->addChild( $xml_doc->createAttribute('id',$model->id));
-            $model_node->addChild( $xml_doc->createAttribute('subject_name',$model->subject_name));
-            $model_node->addChild( $xml_doc->createAttribute('model_name',$model->name));
 
-            my @idata = $model->instrument_data;
+            my @results = $self->_results_for_build($build);
 
-            $model_node->addChild( $xml_doc->createAttribute('lane_count', scalar(@idata)) );
+            for my $result (@results) {
+                my $model_node = $self->_get_model_node($model, $result);
+                $as_node->addChild( $model_node );
 
-            my $alignment_summary_hash_ref = $build->alignment_summary_hash_ref;
-            for my $ws_key (keys %{$alignment_summary_hash_ref}) {
-                my $ws_node = $model_node->addChild( $xml_doc->createElement('wingspan') );
-                $ws_node->addChild( $xml_doc->createAttribute('size', $ws_key) );
-                for my $param_key (keys %{$alignment_summary_hash_ref->{$ws_key}}) {
-                    my $key_node = $ws_node->addChild( $xml_doc->createElement($param_key) );
-                    $key_node->addChild( $xml_doc->createTextNode( $alignment_summary_hash_ref->{$ws_key}->{$param_key} ) );
+                my $alignment_summary_hash_ref = $result->alignment_summary_hash_ref;
+                for my $ws_key (keys %{$alignment_summary_hash_ref}) {
+                    my $ws_node = $model_node->addChild( $xml_doc->createElement('wingspan') );
+                    $ws_node->addChild( $xml_doc->createAttribute('size', $ws_key) );
+                    for my $param_key (keys %{$alignment_summary_hash_ref->{$ws_key}}) {
+                        my $key_node = $ws_node->addChild( $xml_doc->createElement($param_key) );
+                        $key_node->addChild( $xml_doc->createTextNode( $alignment_summary_hash_ref->{$ws_key}->{$param_key} ) );
+                    }
                 }
             }
         }
     }
-
     return $as_node;
 }
 
@@ -253,41 +270,48 @@ sub get_coverage_summary_node {
         my $build = $self->get_last_succeeded_coverage_stats_build_from_model($model);
         if ($build) {
             push(@included_models, $model);
-            unless (@min_depths) {
-                @min_depths = sort{ $a <=> $b } @{$build->minimum_depths_array_ref};
-                for my $min_depth (@min_depths) {
-                    my $header_node = $cs_node->addChild( $xml_doc->createElement('minimum_depth_header') );
-                    $header_node->addChild( $xml_doc->createAttribute('value',$min_depth) );
+
+            my @results = $self->_results_for_build($build);
+
+            for my $result (@results) {
+                my @result_depths;
+                if($result->isa('Genome::SoftwareResult')) {
+                    @result_depths = split(',', $result->minimum_depths);
+                } else {
+                    @result_depths = @{ $result->minimum_depths_array_ref };
                 }
-            } else {
-                my @other_min_depths = sort{ $a <=> $b } @{$build->minimum_depths_array_ref};
-                unless (scalar(@min_depths) == scalar(@other_min_depths)) {
-                    die('Model '. $model->name .' has '. scalar(@other_min_depths) .' minimum_depth filters expecting '. scalar(@min_depths) .' minimum_depth filters');
-                }
-                for (my $i = 0; $i < scalar(@min_depths); $i++) {
-                    my $expected_min_depth = $min_depths[$i];
-                    my $other_min_depth = $other_min_depths[$i];
-                    unless ($expected_min_depth == $other_min_depth) {
-                        die('Model '. $model->name .' has '. $other_min_depth .' minimum_depth filter expecting '. $expected_min_depth .' minimum_depth filter');
+
+                unless (@min_depths) {
+                    @min_depths = sort{ $a <=> $b } @result_depths;
+                    for my $min_depth (@min_depths) {
+                        my $header_node = $cs_node->addChild( $xml_doc->createElement('minimum_depth_header') );
+                        $header_node->addChild( $xml_doc->createAttribute('value',$min_depth) );
+                    }
+                } else {
+                    my @other_min_depths = sort{ $a <=> $b } @result_depths;
+                    unless (scalar(@min_depths) == scalar(@other_min_depths)) {
+                        die('Model '. $model->name .' has '. scalar(@other_min_depths) .' minimum_depth filters expecting '. scalar(@min_depths) .' minimum_depth filters');
+                    }
+                    for (my $i = 0; $i < scalar(@min_depths); $i++) {
+                        my $expected_min_depth = $min_depths[$i];
+                        my $other_min_depth = $other_min_depths[$i];
+                        unless ($expected_min_depth == $other_min_depth) {
+                            #die('Model '. $model->name .' has '. $other_min_depth .' minimum_depth filter expecting '. $expected_min_depth .' minimum_depth filter');
+                        }
                     }
                 }
-            }
-            my $model_node = $cs_node->addChild( $xml_doc->createElement('model') );
-            $model_node->addChild( $xml_doc->createAttribute('id',$model->id));
-            $model_node->addChild( $xml_doc->createAttribute('subject_name',$model->subject_name));
-            $model_node->addChild( $xml_doc->createAttribute('model_name',$model->name));
 
-            my @idata = $model->instrument_data;
+                my $model_node = $self->_get_model_node($model, $result);
+                $cs_node->addChild( $model_node );
 
-            $model_node->addChild( $xml_doc->createAttribute('lane_count', scalar(@idata)) );
-
-            my $coverage_stats_summary_hash_ref = $build->coverage_stats_summary_hash_ref;
-            for my $min_depth (keys %{$coverage_stats_summary_hash_ref->{0}}) {
-                my $min_depth_node = $model_node->addChild( $xml_doc->createElement('minimum_depth') );
-                $min_depth_node->addChild( $xml_doc->createAttribute('value',$min_depth) );
-                for my $key (keys %{$coverage_stats_summary_hash_ref->{0}->{$min_depth}}) {
-                    my $key_node = $min_depth_node->addChild( $xml_doc->createElement($key) );
-                    $key_node->addChild( $xml_doc->createTextNode( $coverage_stats_summary_hash_ref->{0}->{$min_depth}->{$key} ) );
+                my $coverage_stats_summary_hash_ref = $result->coverage_stats_summary_hash_ref;
+                for my $min_depth (keys %{$coverage_stats_summary_hash_ref->{0}}) {
+                    my $min_depth_node = $model_node->addChild( $xml_doc->createElement('minimum_depth') );
+                    $min_depth_node->addChild( $xml_doc->createAttribute('value',$min_depth) );
+                    for my $key (keys %{$coverage_stats_summary_hash_ref->{0}->{$min_depth}}) {
+                        my $key_node = $min_depth_node->addChild( $xml_doc->createElement($key) );
+                        $key_node->addChild( $xml_doc->createTextNode( $coverage_stats_summary_hash_ref->{0}->{$min_depth}->{$key} ) );
+                    }
                 }
             }
         }
@@ -295,5 +319,46 @@ sub get_coverage_summary_node {
 
     return $cs_node;
 }
+
+sub _results_for_build {
+    my $self = shift;
+    my $build = shift;
+
+    my @result_users = $build->result_users('software_result.subclass_name' => 'Genome::InstrumentData::AlignmentResult::Merged::CoverageStats');
+    my @results = map($_->software_result, @result_users);
+
+    unless(@results) {
+        push @results, $build;
+    }
+
+    return @results;
+}
+
+sub _get_model_node {
+    my $self = shift;
+    my $model = shift;
+    my $result = shift;
+
+    my $xml_doc = $self->_xml_doc;
+
+    my @idata;
+    if($result->isa('Genome::SoftwareResult')) {
+        @idata = $result->alignment_result->instrument_data;
+    } else { #old build way
+        @idata = $result->instrument_data;
+    }
+
+    my $subject = $idata[0]->sample;
+
+    my $model_node = $xml_doc->createElement('model');
+    $model_node->addChild( $xml_doc->createAttribute('id',$model->id));
+    $model_node->addChild( $xml_doc->createAttribute('subject_name',$subject->name));
+    $model_node->addChild( $xml_doc->createAttribute('model_name',$model->name));
+
+    $model_node->addChild( $xml_doc->createAttribute('lane_count', scalar(@idata)) );
+
+    return $model_node;
+}
+
 
 1;
