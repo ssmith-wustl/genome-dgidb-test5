047cac908330c1df98c08a6e293091ee11839b66
Brian Derickson <bdericks@genome.wustl.edu>
2011-11-28 15:09:21 -0600

    added __errors__ method to role member class
    the method makes sure that the user doesn't already have the role
    specified, and dies if he/she does

--- lib/perl/Genome/Sys/User/RoleMember.pm
+++ lib/perl/Genome/Sys/User/RoleMember.pm
@@ -2,6 +2,7 @@ package Genome::Sys::User::RoleMember;
 
 use strict;
 use warnings;
+
 use Genome;
 
 class Genome::Sys::User::RoleMember {
@@ -22,6 +23,33 @@ class Genome::Sys::User::RoleMember {
         },
     ],
 };
+
+sub __errors__ {
+    my $self = shift;
+    my @tags;
+
+    my @duplicates = eval { 
+        Genome::Sys::User::RoleMember->create(
+            role => $self->role,
+            user => $self->user,
+        )
+    };
+    my $error = $@;
+    if ($error) {
+        # Make a more human-readable error message in the case where a duplicate bridge entity already exists
+        if ($error =~  /An object of type Genome::Sys::User::RoleMember already exists with id/) {
+            $error = "Cannot add role " . $self->role->name . " to user " . $self->user->name . ", that user already has this role!",
+        }
+
+        push @tags, UR::Object::Tag->create(
+            type => 'invalid',
+            properties => ['role', 'user'],
+            desc => $error,
+        );
+    }
+
+    return @tags;
+}
 
 1;
 
