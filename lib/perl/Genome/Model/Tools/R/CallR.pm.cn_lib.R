2c75e84d9da3d27d87cd6721efb67fc35b42ccf0
Nathan Dees <ndees@wustl.edu>
2011-12-05 16:12:13 -0600

    added function to plot copy-number-neutral LOH analysis

--- lib/perl/Genome/Model/Tools/R/CallR.pm.cn_lib.R
+++ lib/perl/Genome/Model/Tools/R/CallR.pm.cn_lib.R
@@ -4,6 +4,20 @@
 
 #Updated 04-08-2010 by NDees for use in the Copy Number Pipeline
 ###########################################################################
+cnn_loh_graph=function(varscan_file)
+{
+
+x<-read.table(file=varscan_file,sep="\t",quote = "");
+x$V8 = as.numeric(sub('%','',(x$V8)));
+x$V12 = as.numeric(sub('%','',(x$V12)));
+outfile = paste(varscan_file,".cnnplot.pdf",sep="");
+pdf(file=outfile,width=10, height=7.5);
+plot(x$V2,x$V8,col='blue',type="p",pch=19,xlab=paste("Chr ",x$V1[1]," Coordinates"),ylab="Tumor (Red) & Normal (Blue) Var. Allele Freq",ylim=c(0,100));
+points(x$V2,x$V12,col='red',type="p",pch=19);
+dev.off();
+
+}
+###########################################################################
 per_sample_cn_graph=function(workdir)
 {
 
