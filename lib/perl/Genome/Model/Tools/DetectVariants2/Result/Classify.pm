bda9b3f5419cc670c5962bd446e7bd8440ad9363
Thomas Mooney <tmooney@genome.wustl.edu>
2011-12-07 14:45:47 -0600

    Software result for LOH.  The first of a new DV2 type: "classifier".

---
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Result/Classify.pm
@@ -0,0 +1,16 @@
+package Genome::Model::Tools::DetectVariants2::Result::Classify;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::DetectVariants2::Result::Classify {
+    is => ['Genome::Model::Tools::DetectVariants2::Result::Base'],
+    is_abstract => 1,
+    doc => '',
+};
+
+
+
+1;
