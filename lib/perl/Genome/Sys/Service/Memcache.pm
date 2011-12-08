35ec93d244a2e86395af9a1128d32bf4f2659c48
James Koval <jkoval@genome.wustl.edu>
2011-12-08 13:18:14 -0600

    Save service stats on singletons' static constants rather than in DB
    This is apipe specific data that might need to move outside our code
    repo

---
+++ lib/perl/Genome/Sys/Service/Memcache.pm
@@ -0,0 +1,21 @@
+package Genome::Sys::Service::Memcache;
+use strict;
+use warnings;
+use Genome;
+
+class Genome::Sys::Service::Memcache {
+    is => ['Genome::Sys::Service','UR::Singleton'],
+    doc => "Memcache service"
+};
+
+sub host { "imp.gsc.wustl.edu" }
+
+sub restart_command { "/etc/init.d/memcached restart" }
+
+sub stop_command { "/etc/init.d/memcached stop" }
+
+sub log_path { "/var/log/memcached.log" }
+
+sub pid_name { "memcached" }
+
+sub url {}
