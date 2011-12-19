a9b63b73eda9a634e7ae3b33a53474db66938a1e
Feiyu Du <fdu@genome.wustl.edu>
2011-12-19 13:45:45 -0600

    replace removed method params_for_result with params_for_detector_result for DV2 detector

--- lib/perl/Genome/Model/Tools/DetectVariants2/Samtools.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Samtools.pm
@@ -384,24 +384,6 @@ sub generate_genotype_detail_file {
 }
 
 
-sub params_for_result {
-    my $self = shift;
-
-    my %params = (
-        detector_name         => $self->class,
-        detector_params       => $self->params,
-        detector_version      => $self->version,
-        aligned_reads         => $self->aligned_reads_input,
-        control_aligned_reads => undef,
-        reference_build_id    => $self->reference_build_id,
-        region_of_interest_id => $self->region_of_interest_id,
-        test_name             => $ENV{GENOME_SOFTWARE_RESULT_TEST_NAME} || undef,
-        chromosome_list       => undef,
-    );
-
-    return \%params;
-}
-
 #TODO clean all of this up. It is usually/should be based on logic from Genome::Model::Tools::Bed::Convert logic in process_source... 
 # this should be smarter about using that work ... perhaps process_source should call a method that just parses one line, and this method can be replaced by a call to that instead
 sub parse_line_for_bed_intersection {
