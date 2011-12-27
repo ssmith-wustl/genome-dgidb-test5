3284dd014a4e3f78f8f8e50027a320e061d09173
Thomas Mooney <tmooney@genome.wustl.edu>
2011-12-27 16:53:04 -0600

    Try a more stable sorting for the LQ union.
    Filenames depend on runtime metadata which might vary between runs
    producing an inconsistent result.

--- lib/perl/Genome/Model/Tools/DetectVariants2/Result/Combine/LqUnion.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Result/Combine/LqUnion.pm
@@ -80,9 +80,26 @@ sub _validate_inputs {
     return (scalar(@r_ids) == scalar(@r));
 }
 
+sub compare_results {
+    my ($first, $second) = @_;
+
+    ($first->class cmp $second->class) or
+    ($first->can('input_result_a') && $second->can('input_result_a')? &compare_results($first->input_result_a, $second->result_a) : 0) or
+    ($first->can('input_result_b') && $second->can('input_result_b')? &compare_results($first->input_result_b, $second->result_b) : 0) or
+    ($first->can('detector_name')? $first->detector_name : '') cmp ($second->can('detector_name')? $second->detector_name : '') or
+    ($first->can('detector_version')? $first->detector_version : '') cmp ($second->can('detector_version')? $second->detector_version : '') or
+    ($first->can('detector_params')? $first->detector_params : '') cmp ($second->can('detector_params')? $second->detector_params : '') or
+    ($first->can('filter_name')? $first->filter_name : '') cmp ($second->can('filter_name')? $second->filter_name : '') or
+    ($first->can('filter_version')? $first->filter_version : '') cmp ($second->can('filter_version')? $second->filter_version : '') or
+    ($first->can('filter_params')? $first->filter_params : '') cmp ($second->can('filter_params')? $second->filter_params : '') or
+    $first->id cmp $second->id #cry
+}
+
 sub _combine_variants {
     my $self = shift;
     my @results = $self->results;
+
+    @results = sort { &compare_results($a, $b); } @results;
 
     my $type = $self->variant_type;
     my $file = $self->_file_for_type($type);
@@ -101,9 +118,8 @@ sub _combine_variants {
         Genome::Sys->create_symlink($files[0], $result_file);
     } else {
         #TODO This approach doesn't really work for cnv or sv... but to date we don't have multiple LQ files to combine.
-        my @sorted_lq_files = sort(@files);
         my $sort_command = Genome::Model::Tools::Joinx::Sort->create(
-            input_files => \@sorted_lq_files,
+            input_files => \@files,
             merge_only => 1,
             output_file => $result_file,
         );
