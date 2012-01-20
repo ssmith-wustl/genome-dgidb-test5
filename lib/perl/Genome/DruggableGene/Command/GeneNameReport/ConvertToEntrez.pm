0152da52b030afb8a2b3f9f83048087982a91013
James Koval <jkoval@genome.wustl.edu>
2012-01-20 14:50:40 -0600

    Fix convert to entrez command to account for empty hash ref

--- lib/perl/Genome/DruggableGene/Command/GeneNameReport/ConvertToEntrez.pm
+++ lib/perl/Genome/DruggableGene/Command/GeneNameReport/ConvertToEntrez.pm
@@ -13,20 +13,6 @@ class Genome::DruggableGene::Command::GeneNameReport::ConvertToEntrez {
             doc => 'Gene identifiers to convert to entrez',
             is_many => 1,
         },
-        _entrez_gene_name_reports => {
-            is => 'Genome::DruggableGene::GeneNameReport',
-            is_many => 1,
-            is_output => 1,
-            is_optional => 1,
-            doc => 'Array of gene name reports produced as output',
-        },
-        _intermediate_gene_name_reports => {
-            is => 'Genome::DruggableGene::GeneNameReport',
-            is_many => 1,
-            is_output => 1,
-            is_optional => 1,
-            doc => 'Array of gene name reports that were used to create _entrez_gene_name_reports',
-        },
     ],
 };
 
@@ -38,24 +24,22 @@ sub help_synopsis {
     'genome druggable-gene gene-name-report convert-to-entrez --gene-identifier ARK1D1';
 }
 
-sub help_detail {
-    #TODO: write me
-}
+sub help_detail { help_brief() }
 
 sub execute {
     my $self = shift;
     for my $gene_identifier ($self->gene_identifiers) {
         my ($entrez_gene_name_reports, $intermediate_gene_name_reports) = Genome::DruggableGene::GeneNameReport->convert_to_entrez($gene_identifier);
         my (@entrez, @intermediate);
-        @entrez = $self->_entrez_gene_name_reports($entrez_gene_name_reports->{$gene_identifier}) if $entrez_gene_name_reports->{$gene_identifier};
-        @intermediate = $self->_intermediate_gene_name_reports($intermediate_gene_name_reports->{$gene_identifier}) if $intermediate_gene_name_reports->{$gene_identifier};
+        @entrez = @{$entrez_gene_name_reports->{$gene_identifier}} if $entrez_gene_name_reports->{$gene_identifier};
+        @intermediate  = @{$intermediate_gene_name_reports->{$gene_identifier}} if $intermediate_gene_name_reports->{$gene_identifier};
         if(@entrez){
             $self->status_message($gene_identifier . " as entrez is:\n");
             $self->status_message($_->name . "\n") for (@entrez);
         }
         if(@intermediate) {
             $self->status_message($gene_identifier . " as intermediate is:\n");
-            $self->status_message($_-> name . "\n") for (@intermediate);
+            $self->status_message($_->name . "\n") for (@intermediate);
         }
     }
     return 1;
