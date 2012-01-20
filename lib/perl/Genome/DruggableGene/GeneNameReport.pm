1c0bad688d8883211ca4b9b94d767d53403d3e3c
Jim Weible <jweible@genome.wustl.edu>
2012-01-20 15:34:06 -0600

    handle gene names with single quotes in them

--- lib/perl/Genome/DruggableGene/GeneNameReport.pm
+++ lib/perl/Genome/DruggableGene/GeneNameReport.pm
@@ -124,6 +124,7 @@ sub _convert_to_entrez_gene_name_reports {
     my $intermediate_gene_name_reports;
 
     @gene_identifiers = $class->_strip_version_numbers(@gene_identifiers);
+    $DB::single = 1; #TODO: remove me
 
     ($entrez_gene_symbol_matches, @gene_identifiers) = $class->_match_as_entrez_gene_symbol(@gene_identifiers);
 
@@ -150,7 +151,8 @@ sub _match_as_entrez_gene_symbol {
     my %matched_identifiers;
     my @unmatched_identifiers;
 
-    my @entrez_gene_name_report_associations = Genome::DruggableGene::GeneNameReportAssociation->get(nomenclature => ['entrez_gene_symbol', 'entrez_gene_synonym'], alternate_name => \@gene_identifiers);
+    my @escaped_gene_identifiers = $class->_escape_gene_identifiers(@gene_identifiers);
+    my @entrez_gene_name_report_associations = Genome::DruggableGene::GeneNameReportAssociation->get(nomenclature => ['entrez_gene_symbol', 'entrez_gene_synonym'], alternate_name => \@escaped_gene_identifiers);
     return {}, @gene_identifiers unless @entrez_gene_name_report_associations;
     for my $gene_identifier(@gene_identifiers){
         my @associations_for_identifier = grep($_->alternate_name eq $gene_identifier, @entrez_gene_name_report_associations);
@@ -172,7 +174,8 @@ sub _match_as_entrez_id {
     my %matched_identifiers;
     my @unmatched_identifiers;
 
-    my @entrez_gene_name_reports = Genome::DruggableGene::GeneNameReport->get(nomenclature => 'entrez_id', name => \@gene_identifiers);
+    my @escaped_gene_identifiers = $class->_escape_gene_identifiers(@gene_identifiers);
+    my @entrez_gene_name_reports = Genome::DruggableGene::GeneNameReport->get(nomenclature => 'entrez_id', name => \@escaped_gene_identifiers);
     return {}, @gene_identifiers unless @entrez_gene_name_reports;
     for my $gene_identifier (@gene_identifiers){
         my @reports_for_identifier = grep($_->name eq $gene_identifier, @entrez_gene_name_reports);
@@ -193,7 +196,8 @@ sub _match_as_ensembl_id {
     my %matched_identifiers;
     my @unmatched_identifiers;
 
-    my @gene_name_reports = Genome::DruggableGene::GeneNameReport->get(source_db_name => 'Ensembl', name => \@gene_identifiers);
+    my @escaped_gene_identifiers = $class->_escape_gene_identifiers(@gene_identifiers);
+    my @gene_name_reports = Genome::DruggableGene::GeneNameReport->get(source_db_name => 'Ensembl', name => \@escaped_gene_identifiers);
     for my $gene_identifier(@gene_identifiers){
         my @reports_for_identifier = grep($_->name eq $gene_identifier, @gene_name_reports);
         unless(@reports_for_identifier){
@@ -220,7 +224,8 @@ sub _match_as_uniprot_id {
     my %intermediate_results_for_identifiers;
     my @unmatched_identifiers;
 
-    my @uniprot_associations = Genome::DruggableGene::GeneNameReportAssociation->get(nomenclature => 'uniprot_id', alternate_name => @gene_identifiers);
+    my @escaped_gene_identifiers = $class->_escape_gene_identifiers(@gene_identifiers);
+    my @uniprot_associations = Genome::DruggableGene::GeneNameReportAssociation->get(nomenclature => 'uniprot_id', alternate_name => @escaped_gene_identifiers);
     for my $gene_identifier(@gene_identifiers){
         my @associations_for_identifier = grep($_->alternate_name => @uniprot_associations);
         unless(@associations_for_identifier){
@@ -252,6 +257,18 @@ sub _strip_version_numbers{
         push @updated_gene_identifiers, $gene_identifier;
     }
     return @updated_gene_identifiers;
+}
+sub _escape_gene_identifiers{
+    my $class = shift;
+    my @gene_identifiers = @_;
+    my @escaped_gene_identifiers;
+    for my $gene_identifier (@gene_identifiers){
+        my $escaped_gene_identifier = $gene_identifier;
+        $escaped_gene_identifier =~ s/'/''/g;
+        push @escaped_gene_identifiers, $escaped_gene_identifier;
+    }
+
+    return @escaped_gene_identifiers;
 }
 
 sub _merge_conversion_results{
