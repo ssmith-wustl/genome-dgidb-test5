2f614896d756b26d801cde4ee3f2a39214a402aa
Scott Smith <ssmith@genome.wustl.edu>
2012-01-26 08:34:35 -0600

    processing profiles and software results now update the new class/id value tracking columns and new name columns, but do not use them

--- lib/perl/Genome/ProcessingProfile/Param.pm
+++ lib/perl/Genome/ProcessingProfile/Param.pm
@@ -9,16 +9,46 @@ class Genome::ProcessingProfile::Param {
     type_name => 'processing profile param',
     table_name => 'PROCESSING_PROFILE_PARAM',
     id_by => [
-              processing_profile => {
-                                   is => 'Genome::ProcessingProfile',
-                                   id_by => 'processing_profile_id',
-                                   constraint_name=> 'PPP_PP_FK',
-                               },
-              name            => { is => 'VARCHAR2', len => 100, column_name => 'PARAM_NAME' },
-              value           => { is => 'VARCHAR2', len => 1000, column_name => 'PARAM_VALUE' },
-          ],
+        processing_profile => {
+            is => 'Genome::ProcessingProfile',
+            id_by => 'processing_profile_id',
+            constraint_name=> 'PPP_PP_FK',
+        },
+        name            => { is => 'VARCHAR2', len => 100, column_name => 'PARAM_NAME' },
+        value           => { is => 'VARCHAR2', len => 1000, column_name => 'PARAM_VALUE' },
+    ],
+    has => [
+        # new, not dependent until old snapshots are gone and data is complete
+        _name                           => { is => 'Text', len => 255, column_name => 'NAME' },
+        value_class_name                => { is => 'Text', len => 255 },
+        value_id                        => { is => 'Text', len => 1000 },
+
+    ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
+};
+
+# this will sync up new columns with the old ones
+# once all old snapshots are gone, we will switch to the new columns and remove this
+sub create {
+    my $self = shift->SUPER::create(@_);
+    return unless $self;
+
+    $self->_name($self->name);
+    $self->value_id($self->value);
+
+    my $sr = $self->processing_profile;
+    if ($sr) {
+        my $p = $sr->__meta__->property($self->name);
+        if ($p) {
+            $self->value_class_name($p->_data_type_as_class_name);
+        }
+        else {
+            $self->value_class_name('UR::Value::Text');
+        }
+    }
+
+    return $self;
 };
 
 1;
--- lib/perl/Genome/SoftwareResult/Param.pm
+++ lib/perl/Genome/SoftwareResult/Param.pm
@@ -14,9 +14,38 @@ class Genome::SoftwareResult::Param {
     has => [
         param_value                     => { is => 'VARCHAR2', len => 1000 },
         software_result                 => { is => 'Genome::SoftwareResult', id_by => 'software_result_id', constraint_name => 'SRP_SR_FK' },
+
+        # new, not dependent until old snapshots are gone and data is complete
+        name                            => { is => 'Text', len => 255 },
+        value_class_name                => { is => 'Text', len => 255 },
+        value_id                        => { is => 'Text', len => 1000 },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
 };
 
+# this will sync up new columns with the old ones
+# once all old snapshots are gone, we will switch to the new columns and remove this
+sub create {
+    my $self = shift->SUPER::create(@_);
+    return unless $self;
+
+    $self->name($self->param_name);
+    $self->value_id($self->param_value);
+
+    my $sr = $self->software_result;
+    if ($sr) {
+        my $p = $sr->__meta__->property($self->param_name);
+        if ($p) {
+            $self->value_class_name($p->_data_type_as_class_name);
+        }
+        else {
+            $self->value_class_name('UR::Value::Text');
+        }
+    }
+
+    return $self;
+};
+
 1;
+
