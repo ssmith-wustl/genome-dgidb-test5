80986fbcd76d226e699f922149dbf77ccdff6b2e
Nathan Nutter <nnutter@genome.wustl.edu>
2012-01-25 18:07:48 -0600

    remove extraneous .gz

--- lib/perl/Genome/Model/Build/ReferenceAlignment.pm
+++ lib/perl/Genome/Model/Build/ReferenceAlignment.pm
@@ -410,7 +410,7 @@ sub get_indels_vcf {
 
 sub get_snvs_vcf {
     my $self = shift;
-    return $self->variants_directory . "/snvs.vcf.gz";
+    return $self->variants_directory . "/snvs.vcf";
 }
 
 sub calculate_estimated_kb_usage {
