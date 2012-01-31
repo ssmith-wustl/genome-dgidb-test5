5b9ecf806acf6b6db1fefa5aab24222066ca0fef
Nathan Nutter <nnutter@genome.wustl.edu>
2012-01-31 10:44:03 -0600

    only reorder if reference_file is specified

--- lib/perl/Genome/Model/Tools/Sam/Debroadify.pm
+++ lib/perl/Genome/Model/Tools/Sam/Debroadify.pm
@@ -65,9 +65,10 @@ sub execute {
     $self->validate_inputs();
 
     my $samtools_output_options = $self->samtools_output_options();
-    my $tmp_output_file = Genome::Sys->create_temp_file_path();
-    my $tmp_output = IO::File->new("| samtools view $samtools_output_options -o $tmp_output_file -");
-    unless ($tmp_output) {
+    # If we have a reference_file we will be reordering it so save to tmp.
+    my $chr_rename_output_file = ($self->reference_file ? Genome::Sys->create_temp_file_path() : $self->output_file);
+    my $chr_rename_output = IO::File->new("| samtools view $samtools_output_options -o $chr_rename_output_file -");
+    unless ($chr_rename_output) {
         die $self->error_message('Could not open the (temp) output pipe.');
     }
 
@@ -77,7 +78,7 @@ sub execute {
         die $self->error_message('Could not open the input pipe.');
     }
     $self->status_message('Converting Broad chromosome references to TGI style.');
-    $self->status_message('output will be at: ' . $tmp_output_file);
+    $self->status_message('output will be at: ' . $chr_rename_output_file);
     while (my $line = $input->getline) {
         if ($line =~ /^\@SQ/ && $line =~ /chr/) {
             $line =~ s/\@SQ\tSN:chrM/\@SQ\tSN:MT/;
@@ -92,22 +93,24 @@ sub execute {
             $sam_fields[6] =~ s/^chr(.*)(_|\w)/$1$2/;
             $line = join("\t", @sam_fields);
         }
-        print $tmp_output $line;
+        print $chr_rename_output $line;
     }
     close $input;
 
-    close $tmp_output;
+    close $chr_rename_output;
 
-    # This needs to be done because Broad's reference is sorted 1-22,X,Y but our is 1-9,X,Y,10-22.
-    $self->status_message('Reordering BAM to match reference.');
-    $self->status_message('output will be at: ' . $self->output_file);
-    my $reorder_cmd = Genome::Model::Tools::Picard::ReorderSam->create(
-        input_file => $tmp_output_file,
-        output_file => $self->output_file,
-        reference_file => $self->reference_file,
-    );
-    unless ($reorder_cmd->execute) {
-        die 'failed to execute reorder_cmd';
+    # This needs to be done because Broad's reference is sorted 1-22,X,Y but ours is 1-9,X,Y,10-22.
+    if ($self->reference_file) {
+        $self->status_message('Reordering BAM to match reference.');
+        $self->status_message('output will be at: ' . $self->output_file);
+        my $reorder_cmd = Genome::Model::Tools::Picard::ReorderSam->create(
+            input_file => $chr_rename_output_file,
+            output_file => $self->output_file,
+            reference_file => $self->reference_file,
+        );
+        unless ($reorder_cmd->execute) {
+            die 'failed to execute reorder_cmd';
+        }
     }
 
     return 1;
