7a42335017a8ba03fdf8aba7749233366ba24c21
James Koval <jkoval@genome.wustl.edu>
2012-02-07 13:25:16 -0600

    Respect entrez only when merging gene groups

--- lib/perl/Genome/DruggableGene/Command/GeneNameReport/GenerateGroups.pm
+++ lib/perl/Genome/DruggableGene/Command/GeneNameReport/GenerateGroups.pm
@@ -24,16 +24,20 @@ sub execute {
 
     print "Loading alternate names and creating hash\n";
     my %alt_to_entrez;
-    for (Genome::DruggableGene::GeneAlternateNameReport->get) { #operate on all associations, which map genes to alternate names
+    for (Genome::DruggableGene::GeneAlternateNameReport->get) { #operate on all alternate names
+    my %alt_to_other;
         next if $_->alternate_name =~ /^.$/;    #ignore single character names
         next if $_->alternate_name =~ /^\d\d$/; #ignore 2 digit names
-        next unless $_->nomenclature eq 'entrez_gene_synonym' or $_->nomenclature eq 'entrez_gene_symbol';
-        push @{ $alt_to_entrez{uc($_->alternate_name)} }, $_;   #Save genes with the same alternate name in an array in a hash with key being the alt-name
+        if($_->nomenclature eq 'entrez_gene_synonym' or $_->nomenclature eq 'entrez_gene_symbol'){
+            push @{ $alt_to_other{$_->alternate_name} }, $_;   #Save genes with the same alternate name in an array in a hash with key being the alt-name
+        } else {
+            push @{ $alt_to_entrez{$_->alternate_name} }, $_;   #Save genes with the same alternate name in an array in a hash with key being the alt-name
+        }
     }
 
     my $progress_counter = 0;
 
-    print "Putting alternate names into groups\n";
+    print "Putting entrez alternate names into groups\n";
     for my $alt (keys %alt_to_entrez) {
         my @genes = map{$_->gene_name_report} @{$alt_to_entrez{$alt}};
         next if @genes > 15;#Ignore alts with more than 15 supposedly synonymous genes
@@ -68,6 +72,30 @@ sub execute {
         $progress_counter++;
         print "Completed $progress_counter, and on $alt\n" if rand() < .001;
     }
+
+    print "Finished $progress_counter. Putting other alternate names into groups\n";
+    for my $alt (keys %alt_to_other) {
+        my @genes = map{$_->gene_name_report} @{$alt_to_other{$alt}};
+        next if @genes > 15;#Ignore alts with more than 15 supposedly synonymous genes
+
+        my @bridges = map{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)}
+        grep{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
+
+        my @groups = map{$_->gene_name_group} @bridges;
+        @groups = uniq @groups;
+        if (@groups) {
+            my @genes_groupless = grep{not Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
+            my $group = shift @groups;
+            Genome::DruggableGene::GeneNameGroupBridge->create(gene_id => $_->id, gene_name_group_id => $group->id) for @genes_groupless;
+            print "$alt added to existing group " . $group->name . "\n" if rand() < .001;
+        } else {
+            my $group = Genome::DruggableGene::GeneNameGroup->create(name => $alt);
+            Genome::DruggableGene::GeneNameGroupBridge->create(gene_id => $_->id, gene_name_group_id => $group->id) for @genes;
+        }
+        $progress_counter++;
+        print "Completed $progress_counter, and on $alt\n" if rand() < .001;
+    }
+    print "Finished $progress_counter\n";
     return 1;
 }
 1;
