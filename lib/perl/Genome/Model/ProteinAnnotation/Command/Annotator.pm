8d5762aea2806ed487c4364286ac0c43c7b6ada3
Scott Smith <ssmith@genome.wustl.edu>
2012-02-08 22:32:01 -0600

    most of the new code from the Protein Annotation branch ...none of which interferes with old code

---
+++ lib/perl/Genome/Model/ProteinAnnotation/Command/Annotator.pm
@@ -0,0 +1,32 @@
+package Genome::Model::ProteinAnnotation::Command::Annotator;
+
+use strict;
+use warnings;
+use Genome;
+
+class Genome::Model::ProteinAnnotation::Command::Annotator {
+    is => 'Command::V2',
+    is_abstract => 1,
+    has => [
+        #version     => { is => 'Text', is_input => 1 },
+        #output_dir  => { is => 'FilesystemPath', is_input => 1, is_output => 1, },
+    ],
+    doc => 'annotator abstract base class',
+};
+
+sub name { 
+    my $self = shift;
+    my $name = $self->command_name_brief;
+    return $name;
+}
+
+sub requires_chunking { die "override requires_chunking() to return 1 or 0 in " . shift }
+
+sub sub_command_category { 'annotators' }
+
+sub help_brief {
+    return 'run the ' . shift->name . ' protein annotator'
+}
+
+1;
+
