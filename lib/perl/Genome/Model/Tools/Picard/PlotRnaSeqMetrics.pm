30abb899a92e2185ba1a4f94eb8ede87d4bb0c6a
Jason Walker <jwalker@genome.wustl.edu>
2012-02-10 10:45:25 -0600

    Add a label to include in the chart title.

--- lib/perl/Genome/Model/Tools/Picard/PlotRnaSeqMetrics.pm
+++ lib/perl/Genome/Model/Tools/Picard/PlotRnaSeqMetrics.pm
@@ -13,9 +13,13 @@ class Genome::Model::Tools::Picard::PlotRnaSeqMetrics {
             doc => 'The metrics file output from CollectRnaSeqMetrics.',
         },
         output_file => {
-            is_optional => 1,
             is => 'String',
             doc => 'The output plot.',
+        },
+        label => {
+            is_optional => 1,
+            is => 'String',
+            doc => 'A label to add to the plot title.',
         },
     ],
 };
@@ -36,13 +40,17 @@ sub execute {
     my $self = shift;
 
     my $metrics_hashref = Genome::Model::Tools::Picard::CollectRnaSeqMetrics->parse_file_into_metrics_hashref($self->input_file);
-    print Data::Dumper::Dumper($metrics_hashref);
+    #print Data::Dumper::Dumper($metrics_hashref);
     my $utr_bases = $metrics_hashref->{UTR_BASES};
     my $coding_bases = $metrics_hashref->{CODING_BASES};
     my $ribosomal_bases = $metrics_hashref->{RIBOSOMAL_BASES};
     my $intergenic_bases = $metrics_hashref->{INTERGENIC_BASES};
     my $intronic_bases = $metrics_hashref->{INTRONIC_BASES};
 
+    my $title = 'RNASeq Alignment Breaddown';
+    if ($self->label) {
+        $title .= ' : '. $self->label;
+    }
     my ($tmp_fh,$tmp_file) = Genome::Sys->create_temp_file;
     print $tmp_fh 'library(plotrix)' ."\n";
     print $tmp_fh 'slices <- c('.$utr_bases .', '. $coding_bases .', '. $ribosomal_bases .', '. $intronic_bases .', '. $intergenic_bases .')' ."\n";
@@ -51,7 +59,7 @@ sub execute {
     print $tmp_fh 'lbls <- paste(lbls, pct)' ."\n"; # add percents to labels
     print $tmp_fh 'lbls <- paste(lbls,"%",sep="")' ."\n"; # ad % to labels 
     print $tmp_fh 'png("'.$self->output_file .'", bg="transparent", width=800, height=600)' ."\n";
-    print $tmp_fh 'pie3D(slices,labels=lbls,explode=0.1,main="RNASeq Alignment Breakdown")' ."\n";
+    print $tmp_fh 'pie3D(slices,labels=lbls,explode=0.1,main="'. $title .'")' ."\n";
     print $tmp_fh 'dev.off()' ."\n";
     $tmp_fh->close;
     my $cmd = 'Rscript '. $tmp_file;
