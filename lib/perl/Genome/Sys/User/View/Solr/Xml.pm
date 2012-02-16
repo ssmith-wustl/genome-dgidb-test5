4563903b6aa345c8d593b0a97815212849c14325
Nathan Nutter <nnutter@genome.wustl.edu>
2012-02-16 14:35:36 -0600

    fix for solr view depending on display_title even when is a child view

--- lib/perl/Genome/Sys/User/View/Solr/Xml.pm
+++ lib/perl/Genome/Sys/User/View/Solr/Xml.pm
@@ -49,6 +49,11 @@ class Genome::Sys::User::View::Solr::Xml {
         display_url3 => {
             is  => 'Text',
         },
+        display_title => {
+            is => 'Text',
+            calculate_from => ['subject'],
+            calculate => sub { return shift->name },
+        },
         default_aspects => {
             is => 'ARRAY',
             default => [
@@ -59,10 +64,6 @@ class Genome::Sys::User::View::Solr::Xml {
                 {
                     name => 'email',
                     position => 'content',
-                },
-                {
-                    name => 'name',
-                    position => 'display_title',
                 },
             ],
         }
