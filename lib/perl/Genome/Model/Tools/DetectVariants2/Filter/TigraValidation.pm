faf7b6e4f29cb95ad8b43d41bd0852cccedd8cba
Thomas Mooney <tmooney@genome.wustl.edu>
2012-02-22 13:21:19 -0600

    Remove the temp directory before reallocating to avoid double-counting.
    Since this module puts its temp directory under the final allocation
    directory, any temporary files would be counted towards the total in
    reallocate even though they would subsequently be removed.

--- lib/perl/Genome/Model/Tools/DetectVariants2/Filter/TigraValidation.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Filter/TigraValidation.pm
@@ -304,6 +304,18 @@ sub _create_temp_directories {
     return $self->SUPER::_create_temp_directories(@_);
 }
 
+sub _promote_staged_data {
+    my $self = shift;
+    my $output_dir = $self->SUPER::_promote_staged_data(@_);
+
+    #since the temp dir is under the output dir, we need to remove it
+    #before the final reallocation or everything is double-counted
+    my $staging_dir = $self->_temp_staging_directory;
+    Genome::Sys->remove_directory_tree($staging_dir);
+
+    return $output_dir;
+}
+
 sub _resolve_output_directory {
     my $self = shift;
 
