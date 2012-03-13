9d92f41efd49cd7e11b3609d5c4d05bc923bd010
James Koval <jkoval@genome.wustl.edu>
2012-03-13 17:59:28 -0500

    Make non-entrez alt names match hugo names and join groups
    Added more interesting output during execution

--- lib/perl/Genome/DruggableGene/Command/GeneNameReport/GenerateGroups.pm
+++ lib/perl/Genome/DruggableGene/Command/GeneNameReport/GenerateGroups.pm
@@ -26,12 +26,15 @@ sub execute {
     my %alt_to_entrez;
     my %alt_to_other;
     for (Genome::DruggableGene::GeneAlternateNameReport->get) { #operate on all alternate names
-        next if $_->alternate_name =~ /^.$/;    #ignore single character names
-        next if $_->alternate_name =~ /^\d\d$/; #ignore 2 digit names
+        my $alt = $_->alternate_name;
+        print "Skipping $alt\n" and next if $alt =~ /^.$/;    #ignore single character names
+        print "Skipping $alt\n" and next if $alt =~ /^\d\d$/; #ignore 2 digit names
+
+        #Save genes with the same alternate name in an array in a hash with key being the alt-name
         if($_->nomenclature eq 'entrez_gene_symbol'){
-            push @{ $alt_to_entrez{$_->alternate_name} }, $_;
+            push @{ $alt_to_entrez{$alt} }, $_;
         } else {
-            push @{ $alt_to_other{$_->alternate_name} }, $_;   #Save genes with the same alternate name in an array in a hash with key being the alt-name
+            push @{ $alt_to_other{$alt} }, $_;
         }
     }
 
@@ -39,64 +42,53 @@ sub execute {
 
     print "Putting " . scalar(keys(%alt_to_entrez)) . " entrez gene symbol alternate names into groups\n";
     for my $alt (keys %alt_to_entrez) {
+        $progress_counter++;
         my @genes = map{$_->gene_name_report} @{$alt_to_entrez{$alt}};
-        next if @genes > 15;#Ignore alts with more than 15 supposedly synonymous genes
+        print "$progress_counter : skipping $alt due to 16+ matches\n" and next if @genes > 15;
 
-        my @bridges = map{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)}
-        grep{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
-
-        my @groups = map{$_->gene_name_group} @bridges;
+        my @groups = Genome::DruggableGene::GeneNameGroup->get(name => $alt); #Existing hugo name groups are used first
+        #Next, look at the groups for genes with this alt name
+        unshift @groups, map{$_->gene_name_group} map{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
         @groups = uniq @groups;
+
         if (@groups) {
             my @genes_groupless = grep{not Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
-
             my $group = shift @groups;
-
-            my ($name) = map{$_->alternate_name}grep{$_->nomenclature eq 'entrez_gene_symbol'} map{$_->gene_alt_names}@genes_groupless;
-            if($name){
-                print "$progress_counter : $name hugo name set for $alt\n";
-                $group->name($name);
-            }
-
             $group->consume(@groups); #gobble other groups and their members, deleting them
             Genome::DruggableGene::GeneNameGroupBridge->create(gene_id => $_->id, gene_name_group_id => $group->id) for @genes_groupless;
-
-            print "$progress_counter : $alt added to existing group " . $group->name . "\n" if rand() < .001;
+            print "$progress_counter : found existing group " . $group->name . " for $alt\n" if rand() < .001;
         } else {
             my $group = Genome::DruggableGene::GeneNameGroup->create(name => $alt);
             Genome::DruggableGene::GeneNameGroupBridge->create(gene_id => $_->id, gene_name_group_id => $group->id) for @genes;
+            print "$progress_counter : created new group for $alt\n" if rand() < .001;
         }
-        $progress_counter++;
-        print "Completed $progress_counter, and on $alt\n" if rand() < .001;
     }
 
-    print "Finished $progress_counter. Now processing " . scalar(keys(%alt_to_other)) . " other alternate names\n";
+    print "\n****\nFinished $progress_counter. Now processing " . scalar(keys(%alt_to_other)) . " other alternate names\n****\n\n";
 
     $progress_counter = 0;
-    my $no_hugo;
+    my $no_group_count = 0;
     for my $alt (keys %alt_to_other) {
+        $progress_counter++;
         my @genes = map{$_->gene_name_report} @{$alt_to_other{$alt}};
-        next if @genes > 15;#Ignore alts with more than 15 supposedly synonymous genes
+        print "$progress_counter : skipping $alt due to 16+ matches\n" and next if @genes > 15;
 
-        my @bridges = map{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)}
-        grep{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
-
-        my @groups = map{$_->gene_name_group} @bridges;
+        my @groups = Genome::DruggableGene::GeneNameGroup->get(name => $alt); #Existing hugo name groups are used first
+        #Next, look at the groups for genes with this alt name
+        unshift @groups, map{$_->gene_name_group} map{Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
         @groups = uniq @groups;
+
         if (@groups) {
             my @genes_groupless = grep{not Genome::DruggableGene::GeneNameGroupBridge->get(gene_id => $_->id)} @genes;
             my $group = shift @groups;
             Genome::DruggableGene::GeneNameGroupBridge->create(gene_id => $_->id, gene_name_group_id => $group->id) for @genes_groupless;
-            print "$alt added to existing group " . $group->name . "\n" if rand() < .001;
+            print "$progress_counter : found existing group " . $group->name . " for $alt\n" if rand() < .001 || $alt eq 'FLT3';
         } else {
-            $no_hugo++;
-            print $no_hugo . " missing hugo; $alt is one of them\n" if rand() < .001;
+            $no_group_count++;
+            print "$progress_counter : $alt failed to find any hugo group - it joins the ranks of $no_group_count others\n" if rand() < .001 || $alt eq 'FTL3';
         }
-        $progress_counter++;
-        print "Completed $progress_counter, and on $alt. Found $no_hugo missing hugo.\n" if rand() < .001;
     }
-    print "Finished $progress_counter\n";
-    print "$no_hugo missing hugo\n";
+    print "Finished $progress_counter with $no_group_count that didn't fit into any hugo group\n";
     return 1;
 }
 1;
