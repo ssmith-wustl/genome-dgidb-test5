99393691e57166b2cfa22a17408cbae399f25df2
Scott Smith <ssmith@genome.wustl.edu>
2012-03-16 17:27:39 -0500

    Updated SoftwareResult to save objects as well as primitives correctly.
    Also updated the SR module_version to match the build software_revision,
    though the column is thinner and needs to be truncated unitl we widen
    it.

--- lib/perl/Genome/SoftwareResult/Param.pm
+++ lib/perl/Genome/SoftwareResult/Param.pm
@@ -1,24 +1,29 @@
 package Genome::SoftwareResult::Param;
-
 use strict;
 use warnings;
-
 use Genome;
+
 class Genome::SoftwareResult::Param {
-    type_name => 'software result param',
     table_name => 'SOFTWARE_RESULT_PARAM',
     id_by => [
-        param_name         => { is => 'VARCHAR2', len => 100 },
-        software_result_id => { is => 'NUMBER', len => 20 },
+        software_result => {
+            is => 'Genome::SoftwareResult',
+            id_by => 'software_result_id',
+        },
+        name                            => { is => 'Text', len => 255, column_name => 'PARAM_NAME' },
     ],
     has => [
-        param_value                     => { is => 'VARCHAR2', len => 1000 },
-        software_result                 => { is => 'Genome::SoftwareResult', id_by => 'software_result_id', constraint_name => 'SRP_SR_FK' },
-
-        # new, not dependent until old snapshots are gone and data is complete
-        name                            => { is => 'Text', len => 255 },
         value_class_name                => { is => 'Text', len => 255 },
-        value_id                        => { is => 'Text', len => 1000 },
+        value_id                        => { is => 'Text', len => 1000, column_name => 'PARAM_VALUE' },
+        value_obj => {
+            is => 'UR::Object',
+            id_by => 'value_id',
+            id_class_by => 'value_class_name'
+        },
+
+        # after the new API is released and old snapshots go away, invert the column assingments with those above
+        _new_name                       => { is => 'Text', len => 255, column_name => 'NAME' },
+        _new_value                      => { is => 'Text', len => 1000, column_name => 'VALUE_ID' },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
@@ -26,26 +31,56 @@ class Genome::SoftwareResult::Param {
 
 # this will sync up new columns with the old ones
 # once all old snapshots are gone, we will switch to the new columns and remove this
+
 sub create {
-    my $self = shift->SUPER::create(@_);
-    return unless $self;
+    my $class = shift;
+    my $bx = $class->define_boolexpr(@_);
 
-    $self->name($self->param_name);
-    $self->value_id($self->param_value);
+    unless ($bx->value_for('value_class_name')) {
+        my $pp_id = $bx->value_for('software_result_id');
 
-    my $sr = $self->software_result;
-    if ($sr) {
-        my $p = $sr->__meta__->property($self->param_name);
-        if ($p) {
-            $self->value_class_name($p->_data_type_as_class_name);
-        }
-        else {
-            $self->value_class_name('UR::Value::Text');
-        }
+        my $pp = Genome::SoftwareResult->get($pp_id);
+        die "invalid software result id $pp_id!" unless $pp;
+
+        my $name = $bx->value_for('name');
+        $name =~ s/-.*//;
+        die "no name specified when constructing a processing profile param!" unless $name;
+
+        my $pmeta = $pp->__meta__->property($name);
+        die "no property $name found on processing profile " . $pp->__display_name__ unless $pmeta;
+
+        my $value_class_name = $pmeta->_data_type_as_class_name;
+
+        $bx = $bx->add_filter(value_class_name => $value_class_name);
     }
 
+    my $self = $class->SUPER::create($bx);
+    return unless $self;
+    $self->_new_name($self->name);
+    $self->_new_value($self->value_id);
     return $self;
-};
+}
+
+# this has the functionality of the old "value" accessor 
+# we wanted to ensure we were no longer dependent on it
+# ..but the HTML view needs something generic which will work
+sub _value_scalar_or_object {
+    my $self = shift;
+    my $name = $self->name;
+    return $self->software_result->$name(@_);
+}
+
+sub value {
+    Carp::confess("The system is calling value() on a Genome::ProcessingProfile::Param.  The old functionality of value() is not compatible with the new.  Code should go throuh the accessor on the processing profile, or call _value_scalar_or_object _IF_ it is internal to the profile")
+}
+
+sub param_name {
+    Carp::confess("using param_name!");
+}
+
+sub param_value {
+    Carp::confess("using param_value!");
+}
 
 1;
 
