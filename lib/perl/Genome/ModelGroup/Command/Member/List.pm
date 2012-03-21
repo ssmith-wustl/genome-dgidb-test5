8a7f6d668f3cd48ebb32049b6606667dccc227b8
Eddie Belter <ebelter@genome.wustl.edu>
2012-03-21 15:56:22 -0500

    Model group member lister update to new technology

--- lib/perl/Genome/ModelGroup/Command/Member/List.pm
+++ lib/perl/Genome/ModelGroup/Command/Member/List.pm
@@ -8,46 +8,18 @@ use Genome;
 class Genome::ModelGroup::Command::Member::List {
     is => 'UR::Object::Command::List',
     has => [
-        model_group => { is => 'Genome::ModelGroup', id_by => 'model_group_id' },
         show => {
             doc => 'properties of the member models to list (comma-delimited)',
             is_optional => 1,
-            default_value => 'id,name',
+            default_value => 'model.id,model.name',
         },
         subject_class_name  => {
             is_constant => 1,
-            value => 'Genome::Model'
+            value => 'Genome::ModelGroupBridge'
         },
     ],
     doc => 'list the member models of a model-group',
 };
-
-sub help_synopsis {
-    return <<"EOS"
-genome model-group member list --model-group-id 21
-EOS
-}
-
-sub help_brief {
-    return "list the members of a model-group";
-}
-
-sub help_detail {
-    return <<EOS
-List the member models for a model-group.
-EOS
-}
-
-sub _resolve_boolexpr {
-    my $self = shift;
-
-    my $bx = $self->SUPER::_resolve_boolexpr(@_);
-
-    my @model_ids = map { $_->id } $self->model_group->models;
-    $bx = $bx->add_filter(id => \@model_ids);
-
-    return $bx;
-}
 
 1;
 
