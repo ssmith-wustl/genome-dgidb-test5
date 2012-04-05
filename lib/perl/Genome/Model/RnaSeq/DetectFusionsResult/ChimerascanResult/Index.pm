608d4f92c0d8232804ea158afd3693b07de98063
Adam Coffman <acoffman@genome.wustl.edu>
2012-04-05 16:38:02 -0500

    Removed a bunch of commented out code

--- lib/perl/Genome/Model/RnaSeq/DetectFusionsResult/ChimerascanResult/Index.pm
+++ lib/perl/Genome/Model/RnaSeq/DetectFusionsResult/ChimerascanResult/Index.pm
@@ -72,40 +72,6 @@ sub prepare_gene_file {
 
     unlink($known_gene_file, $kgXref_file);
 
-    #my $annotation_build = $self->annotation_build;
-
-    #my $itr = $annotation_build->transcript_iterator;
-
-    #my $output_file = Genome::Sys->open_file_for_writing($self->gene_file);
-
-    #while (my $tx = $itr->next()){
-        #next if $tx->coding_region_start eq "NULL";
-
-        #my @line = ();
-        #push @line, $tx->transcript_name, $tx->chrom_name;
-
-        ##strand needs to be +/- instead of +1/-1
-        #(my $strand_val = $tx->strand) =~ s/\+1/\+/;
-        #$strand_val =~ s/-1/-/;
-
-        #push @line, $strand_val, $tx->transcript_start, $tx->transcript_stop, $tx->coding_region_start, $tx->coding_region_stop;
-
-        #my @start, my @stop = ();
-        #for (grep {$_->structure_type =~/cds_exon|utr_exon/ } $tx->ordered_sub_structures){
-            #push @start, $_->structure_start;
-            #push @stop, $_->structure_stop;
-        #}
-
-        ##yes, these trailing commas are needed. no, I don't like it either.
-        #push @line, scalar(@start), join(",",@start) . ",", join(",",@stop) . ",", $tx->gene_name;
-
-        #$output_file->say(join("\t", @line));
-        #Genome::Transcript->unload();
-        #Genome::TranscriptStructure->unload();
-    #}
-
-    #$output_file->close();
-
     return 1;
 }
 
