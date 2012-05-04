14a3c08b0ed7d0024c0b45481c6398f02d0fd35e
Eddie Belter <ebelter@genome.wustl.edu>
2012-05-04 13:53:50 -0500

    Sync: update pidfa query to get pse param for genotype file; update succ pidfa check to exists instead of defined b/c the genotype file is not valid for all inst data

--- lib/perl/Genome/Site/TGI/Synchronize/UpdateApipeClasses.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/UpdateApipeClasses.pm
@@ -96,7 +96,8 @@ sub execute {
     # An unlock observer is added at end of execute (not here) because
     # this command periodically commits (which triggers the observer).
     my $lock = Genome::Sys->lock_resource(
-        resource_lock => '/gsc/var/lock/sychronize-update-apipe-classes',
+        resource_lock => '/gscuser/ebelter/sychronize-update-apipe-classes',
+        #resource_lock => '/gsc/var/lock/sychronize-update-apipe-classes',
         max_try => 1,
     );
     if ( not $lock ) {
@@ -114,6 +115,7 @@ sub execute {
         $self->error_message('Failed to load instruemnt data successful pidfas!');
         return;
     }
+    return 1;
 
     # Stores copied and missing IDs for each type
     my %report;
@@ -281,13 +283,11 @@ sub _get_direct_and_indirect_properties_for_object {
     return (\%direct_properties, \%indirect_properties);
 }
 
-my %successful_pidfas;
+my %instrument_data_with_successful_pidfas;
 sub _load_successful_pidfas {
     my $self = shift;
-    print STDERR "Load instrument data successful pidfas...\n";
-
     # This query/hash loading takes 10-15 secs
-    # Currently, the only 'output' on a pidfa pse is a genotype file, which is only valid for genotype data
+    print STDERR "Load instrument data successful pidfas...\n";
 
     my $dbh = Genome::DataSource::GMSchema->get_default_handle;
     if ( not $dbh ) {
@@ -295,12 +295,12 @@ sub _load_successful_pidfas {
         return;
     }
     my $sql = <<SQL;
-        select p.param_value, pseo.data_value
+        select p1.param_value, p2.param_value
         from process_step_executions\@oltp pse
-        inner join pse_param\@oltp p on p.pse_id = pse.pse_id and p.param_name = 'instrument_data_id'
-        left join process_step_outputs\@oltp pso on pso.ps_ps_id = pse.ps_ps_id and pso.output_description = 'genotype_file'
-        left join pse_data_outputs\@oltp pseo on pseo.pso_pso_id = pso.pso_id
+        inner join pse_param\@oltp p1 on p1.pse_id = pse.pse_id and p1.param_name = 'instrument_data_id'
+        left join pse_param\@oltp p2 on p2.pse_id = pse.pse_id and p2.param_name = 'genotype_file'
         where pse.ps_ps_id = 3870 and pse.pr_pse_result = 'successful'
+        order by p1.param_value desc
 SQL
 
     my $sth = $dbh->prepare($sql);
@@ -314,11 +314,13 @@ SQL
         return;
     }
     while ( my ($instrument_data_id, $genotype_file) = $sth->fetchrow_array ) {
-        $successful_pidfas{$instrument_data_id} = $genotype_file if not defined $successful_pidfas{$instrument_data_id};
+        # Going in reverse id order...use the most recent genotype file for duplicate pidfas
+        $instrument_data_with_successful_pidfas{$instrument_data_id} = $genotype_file if not defined $instrument_data_with_successful_pidfas{$instrument_data_id};
     }
     $sth->finish;
 
-    print STDERR 'Loaded '.scalar(keys %successful_pidfas).' successful PIDFAs';
+    print STDERR 'Loaded '.scalar(keys %instrument_data_with_successful_pidfas)." successful PIDFAs\n";
+    print STDERR 'Loaded '.scalar(grep { defined } values %instrument_data_with_successful_pidfas)." genotype files\n";
     return 1;
 }
 
@@ -326,7 +328,7 @@ sub _create_instrumentdata_solexa {
     my ($self, $original_object, $new_object_class) = @_;
 
     # Successful PIDFA required!
-    return 0 unless $successful_pidfas{$original_object->id};
+    return 0 unless exists $instrument_data_with_successful_pidfas{$original_object->id};
     # Bam path required!
     return 0 unless $original_object->bam_path;
     
@@ -356,7 +358,7 @@ sub _create_instrumentdata_sanger {
     my ($self, $original_object, $new_object_class) = @_;
 
     # Successful PIDFA required!
-    return 0 unless $successful_pidfas{$original_object->id};
+    return 0 unless exists $instrument_data_with_successful_pidfas{$original_object->id};
     # Some sanger instrument don't have a library. If that's the case here, just don't create the object
     return 0 unless defined $original_object->library_id or defined $original_object->library_name
         or defined $original_object->library_summary_id;
@@ -401,7 +403,7 @@ sub _create_instrumentdata_454 {
     my ($self, $original_object, $new_object_class) = @_;
 
     # Successful PIDFA required!
-    return 0 unless $successful_pidfas{$original_object->id};
+    return 0 unless exists $instrument_data_with_successful_pidfas{$original_object->id};
 
     my ($direct_properties, $indirect_properties) = $self->_get_direct_and_indirect_properties_for_object(
         $original_object,
