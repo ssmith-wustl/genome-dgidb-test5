d25c1055ed99ae5a650b91979963b5468477f37b
Scott Smith <ssmith@genome.wustl.edu>
2012-05-11 16:43:35 -0500

    updated logic to hit all models, all builds for the specified individual

--- lib/perl/Genome/Individual/Command/CpuDiskSummary.pm
+++ lib/perl/Genome/Individual/Command/CpuDiskSummary.pm
@@ -31,7 +31,12 @@ class Genome::Individual::Command::CpuDiskSummary {
                         is_many => 1,
                         require_user_verify => 0,
                         doc => 'limit report to these somatic builds'
-                    }
+                    },
+        after       => {
+                        is => 'Date',
+                        is_optional => 1,
+                        doc => 'ignore patients with builds which started before this date (omit obsolete processing)'
+                    },
     ],
     doc => 'summarize resources used for a particular patient'
 };
@@ -41,14 +46,23 @@ sub sub_command_sort_position { 2 }
 sub execute {
     my $self = shift;
 
+    my $after = $self->after;
+
     my @patients = $self->patients;
     my @patient_ids = map { $_->id } @patients; 
     
     my @samples = Genome::Sample->get(source_id => \@patient_ids);
-    my @patient_and_sample_ids = (@patient_ids, map { $_->id } @samples);
+    my @sample_ids = map { $_->id } @samples;
 
-    my @somatic_models = Genome::Model::SomaticVariation->get(subject_id => \@patient_and_sample_ids );
-    my @somatic_builds = Genome::Model::Build->get(model_id => [ map { $_->id } @somatic_models ]);
+    my @all_models = Genome::Model->get(subject_id => [@patient_ids, @sample_ids]);
+    my @all_builds = Genome::Model::Build->get(model_id => [ map { $_->id } @all_models ], -hints => ['disk_allocation','newest_workflow_instance']);
+    my @all_events = Genome::Model::Event->get(build_id => [ map { $_->id } @all_builds ]);
+    
+    $DB::single = 1;
+    my @all_wf_names = map { $_->workflow_name } @all_builds;
+    my @all_workflows = Workflow::Model::Instance->get(name => \@all_wf_names);
+    my @children = Workflow::Operation::Instance->get(parent_instance_id => [map { $_->id } @all_workflows]);
+    my @execs = Workflow::Operation::InstanceExecution->get(instance_id => [ map { $_->id } @children ]);
 
     my @force_build_ids;
     if (my @builds = $self->builds) {
@@ -57,14 +71,14 @@ sub execute {
 
     my @row_names = (
         'Individual',
-        'Tumor Gbases', 
-        'Normal Gbases',
-        'Tumor RefAlign (GB)', 
-        'Normal RefAlign (GB)',
-        'Tumor RefAlign (slot*hr)', 
-        'Normal RefAlign (slot*hr)',
-        'Somatic (GB)', 
-        'Somatic (slot*hr)',
+        'Tumor Reference alignment Gbases', 
+        'Normal Reference alignment Gbases',
+        'Tumor Reference alignment (GB)', 
+        'Normal Reference alignment (GB)',
+        'Tumor Reference alignment (slot*hr)', 
+        'Normal Reference alignment (slot*hr)',
+        'Tumor Somatic variation (GB)', 
+        'Tumor Somatic variation (slot*hr)',
         '---',
     );
     my %expected_row_names;
@@ -72,143 +86,126 @@ sub execute {
    
     my @columns; 
     for my $patient (sort { $a->common_name cmp $b->common_name } @patients) {
+        # one column per individual, with hash of row/key => value pairs
         my %f;
         push @columns, \%f;
 
-        $f{"Individual"} = $patient->common_name;
+        $f{"Individual"} = $patient->common_name || $patient->name;
 
-        my @somatic_models = Genome::Model->get(
-            type_name => ['somatic','somatic variation'],           # old/new
+        my @patient_models = Genome::Model->get(
             subject_id => [$patient->id, map { $_->id } $patient->samples],     # old/new
         );
 
-        unless (@somatic_models) {
-            $self->error_message("no somatic models for " . $patient->__display_name__ . "\n");
+        my @patient_builds = Genome::Model::Build->get(
+            model_id => [ map { $_->id } @patient_models ]
+        );
+
+        my $first_build_date = UR::Context->now;
+        for my $build (@patient_builds) {
+            if ($build->date_scheduled lt $first_build_date) {
+                $first_build_date = $build->date_scheduled;
+            }
+        }
+
+        if ($after and $first_build_date lt $after) {
+            warn "skipping individual " . $patient->__display_name__ . " because its first build date of $first_build_date is before $after";
             next;
         }
 
-        my @somatic_builds;
-        for my $model (reverse @somatic_models) {
-            my @builds = $model->builds(
-                (@force_build_ids ? (id => \@force_build_ids) : ()) # (status => 'Succeeded')) 
-            );
-
-            push @somatic_builds, @builds;
+        my %models_by_type;
+        for my $model (@patient_models) {
+            my $list = $models_by_type{$model->type_name} ||= [];
+            push @$list, $model;
         }
 
-        unless (@somatic_builds) {
-            $self->error_message("no builds on somatic models for patient " . $patient->__display_name__);
-            next;
+        # use the somatic models to identify whether a sample is tumor/normal/unknown
+        my %tumor_normal_status_for_sample_id;
+        my @somatic_models = (
+            map { @$_ }
+            grep { defined $_ }
+            map { $models_by_type{$_} }
+            ('somatic','somatic variation')
+        );
+        if (not @somatic_models) {
+            $self->warning_message("no somatic models for " . $patient->__display_name__ . "\n");
+        }
+        for my $somatic_model (@somatic_models) {
+            my $tumor_id = $somatic_model->tumor_model->subject_id;
+            my $normal_id = $somatic_model->normal_model->subject_id;
+            $tumor_normal_status_for_sample_id{$tumor_id} = 'Tumor';
+            $tumor_normal_status_for_sample_id{$normal_id} ||= 'Normal';  # normal is lower precedence than tumor
         }
 
         my %seen_builds;
         my %seen_dirs;
-        for my $somatic_build (@somatic_builds) {
-            
+        my %subject_gbases;
+
+        for my $build (@patient_builds) {
+            next if $seen_builds{$build->id};
+            $seen_builds{$build->id} = $build;
+
             $self->status_message(
-                "patient " . $patient->__display_name__ . ' using build ' . $somatic_build->__display_name__ . "\n"
+                "patient " . $patient->__display_name__ . ' using build ' . $build->__display_name__ . "\n"
             );
 
-            my $somatic_alloc1 = $somatic_build->disk_allocation;
-
-            $f{"somatic_build"} .= $somatic_build->__display_name__;
-            $f{"Somatic (slot*hr)"} += (eval { $somatic_build->cpu_slot_hours } || 0);
-            $f{"Somatic (GB)"} += $somatic_alloc1->kilobytes_requested / (1024**2);
-
-            my $tumor_model = $somatic_build->model->tumor_model;
-            my $normal_model = $somatic_build->model->normal_model;
-           
-            my @tumor_builds = $tumor_model->builds;
-            my @normal_builds = $normal_model->builds;
-
-            # T
+            my $model = $build->model;
+            my $subject = $model->subject;
+            my $type = ucfirst($model->type_name);
             
-            for my $tumor_build (@tumor_builds) {
-                next if $seen_builds{$tumor_build->id};
-                $seen_builds{$tumor_build->id} = $tumor_build;
+            my $tn_status = $tumor_normal_status_for_sample_id{$subject->id};
+            if ($tn_status) {
+                $type = $tn_status . ' ' . $type;
+            }
 
-                my $tumor_bam_dir;
-                my $gb_regular = 0;
-                if ($tumor_build) {
-                    $f{tumor_build} .= $tumor_build->__display_name__;
-                    $f{"Tumor RefAlign (slot*hr)"} += (eval { $tumor_build->cpu_slot_hours("event_type not like" => '%align-reads%') } || 0);
-                    my $tumor_alloc1 = $tumor_build->disk_allocation;
-                    $gb_regular = ($tumor_alloc1->kilobytes_requested / (1024**2));
-                    $f{"Tumor RefAlign GB regular"} += $gb_regular; 
-                    $tumor_bam_dir = $tumor_build->accumulated_alignments_directory;
+            $f{"$type build"} .= $build->__display_name__ . " ";
+            $f{"$type (slot*hr)"} += (eval { $build->cpu_slot_hours } || 0);
+            
+            my $gb_regular;
+            if (my $alloc1 = $build->disk_allocation) {
+                $gb_regular = ($alloc1->kilobytes_requested / (1024**2));
+            }
+            else {
+                $gb_regular = 0;
+                warn "no allocation for build " . $build->__display_name__;
+            }
 
-                    my $tumor_bam_size;
-                    my $dir = $tumor_bam_dir;
+            if ($build->can('accumulated_alignments_directory')) {
+                my $bam_size = 0;
+                my $bam_dir;
+                $bam_dir = $build->accumulated_alignments_directory;
+                if ($bam_dir) {
+                    my $dir = $bam_dir;
                     while (-l $dir) {
                         $dir = readlink($dir);
                     }
                     $dir =~ s|^.*build_merged_alignments|build_merged_alignments|;
                     if ($seen_dirs{$dir}) {
-                        $tumor_bam_size = 0;
-                        warn "skipping dir $dir\n";
+                        $bam_size = 0;
+                        #warn "skipping dir $dir\n";
                     }
                     else {
                         $seen_dirs{$dir} = 1;
-                        my $tumor_alloc = Genome::Disk::Allocation->get(allocation_path => $dir);
-                        $tumor_bam_size = ($tumor_alloc ? ($tumor_alloc->kilobytes_requested / (1024**2)) : 0); 
-                        $f{"Tumor RefAlign GB bam"} += $tumor_bam_size; 
+                        #warn "not skipping $dir, seen " . join(",",keys %seen_dirs);
+                        my $alloc = Genome::Disk::Allocation->get(allocation_path => $dir);
+                        $bam_size = ($alloc ? ($alloc->kilobytes_requested / (1024**2)) : 0); 
                     }
-                    $f{"Tumor RefAlign (GB)"} += ($gb_regular + $tumor_bam_size);
                 }
-                
-                #$tumor_metric  = $tumor_build->metric(name => 'instrument_data_total kb');
-                $f{"Tumor Gbases"} ||= eval { $tumor_build->metric(name => 'instrument data total kb')->value/1_000_000 };
+                $f{"$type GB bam"} += $bam_size; 
+                $f{"$type GB regular"} += $gb_regular; 
+                $f{"$type (GB)"} += ($gb_regular + $bam_size);
             }
-
-            # N
+            else {
+                $f{"$type (GB)"} += $gb_regular;
+            }
             
-            for my $normal_build (@normal_builds) {
-                next if $seen_builds{$normal_build->id};
-                $seen_builds{$normal_build->id} = $normal_build;
-
-                my $normal_bam_dir;
-                my $gb_regular = 0;
-                if ($normal_build) {
-                    $f{normal_build} .= " " . $normal_build->__display_name__;
-                    $f{"Normal RefAlign (slot*hr)"} += (eval { $normal_build->cpu_slot_hours("event_type not like" => '%align-reads%') } || 0);
-                    my $normal_alloc1 = $normal_build->disk_allocation;
-                    if ($normal_alloc1) {
-                        $gb_regular = ($normal_alloc1->kilobytes_requested / (1024**2));
-                        $f{"Normal RefAlign GB regular"} += $gb_regular; 
-                    }
-                    else {
-                        warn "no allocation for build " . $normal_build->__display_name__;
-                    }
-
-                    $normal_bam_dir = $normal_build->accumulated_alignments_directory;
-                    my $normal_bam_size = 0;
-                    if ($normal_bam_dir) {
-                        my $dir = $normal_bam_dir;
-                        while (-l $dir) {
-                            $dir = readlink($dir);
-                        }
-                        $dir =~ s|^.*build_merged_alignments|build_merged_alignments|;
-                        if ($seen_dirs{$dir}) {
-                            $normal_bam_size = 0;
-                            #warn "skipping dir $dir\n";
-                        }
-                        else {
-                            $seen_dirs{$dir} = 1;
-                            #warn "not skipping $dir, seen " . join(",",keys %seen_dirs);
-                            my $normal_alloc = Genome::Disk::Allocation->get(allocation_path => $dir);
-                            $normal_bam_size = ($normal_alloc ? ($normal_alloc->kilobytes_requested / (1024**2)) : 0); 
-                            $f{"Normal RefAlign GB bam"} += $normal_bam_size; 
-                        }
-                    }
-                    $f{"Normal RefAlign (GB)"} += ($gb_regular + $normal_bam_size);
-                }
-                
-                #$normal_metric  = $normal_build->metric(name => 'instrument_data_total kb');
-                $f{"Normal Gbases"} ||= eval { $normal_build->metric(name => 'instrument data total kb')->value/1_000_000 };
+            my $gbases = eval { $build->metric(name => 'instrument data total kb')->value/1_000_000 } || 0;
+            $subject_gbases{$subject->id} ||= 0;
+            if ($gbases > $subject_gbases{$subject->id}) {
+                $subject_gbases{$subject->id} = $gbases;
+                $f{"$type Gbases"} = eval { $build->metric(name => 'instrument data total kb')->value/1_000_000 };
             }
 
-            # OTHER
-
+            # note the widest value so we can build a table below
             my $max_length = 0;
             for (values %f) {
                 my $length = length($_);
@@ -216,15 +213,17 @@ sub execute {
             }
             $f{max_length} = $max_length;
 
-            # non-essential columns may be added as well
+            # note any new hash keys which appear in %f so we can append them to the report 
             for my $key (keys %f) {
                 unless ($expected_row_names{$key}) {
                     $expected_row_names{$key} = 1;
                     push @row_names, $key;
                 }
             }
-        }
-    }
+        
+        } #next build
+
+    } # next individual
 
     my $outfh = Genome::Sys->open_file_for_writing($self->output_file);
     if ($self->format eq 'table') {
