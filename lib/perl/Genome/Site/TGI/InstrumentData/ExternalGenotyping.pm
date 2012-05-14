80ab33a37c4d54c50c703e8fc0ed080bf0ac4885
Eddie Belter <ebelter@genome.wustl.edu>
2012-05-14 12:02:26 -0500

    Sync: rm commented out psuedo code from genotyping classes

--- lib/perl/Genome/Site/TGI/InstrumentData/ExternalGenotyping.pm
+++ lib/perl/Genome/Site/TGI/InstrumentData/ExternalGenotyping.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Genome;
 
-class Genome::Site::TGI::InstrumentData::ExternalGenotyping {
+class Genome::Site::TGI::InstrumentData::ExternalGenotyping { # 2875456768
     table_name => <<SQL
     (
         select g.seq_id id, g.status status, g.organism_sample_id sample_id,
@@ -26,28 +26,11 @@ SQL
         status => { is => 'Text', },
         sample_id => { is => 'Text', },
         sample_name => { is => 'Text', },
-        #creation_pse_id => { is => 'Text', },
         platform_name => { is => 'Text', },
         import_source_name => { is_constant => 1, calculate => q| return 'external'; |, },
     ],
     data_source => 'Genome::DataSource::GMSchema',
 };
-
-sub Xgenotype_file { # 2875456768
-    my $self = shift;
-    #my $sequence_pse = GSC::Sequence::PSE->get(seq_id => $self->id);
-    #return if not $sequence_pse;
-    #my $creation_pse = GSC::PSE->get($sequence_pse->pse_id);
-    my $creation_pse = GSC::PSE->get($self->creation_pse_id);
-    return if not $creation_pse;
-    my $import_pse = $creation_pse->get_first_active_subsequent_pse_with_process_to('import external genotype');
-    return unless $import_pse;
-    my $path = $import_pse->allocate_disk_space_absolute_path;
-    return if not $path or not -d $path;
-    my $genotype_file = $path . '/' . $self->sample_id . '.genotype';
-    return if not -f $genotype_file;
-    return $genotype_file;
-}
 
 1;
 
--- lib/perl/Genome/Site/TGI/InstrumentData/IlluminaGenotyping.pm
+++ lib/perl/Genome/Site/TGI/InstrumentData/IlluminaGenotyping.pm
@@ -27,29 +27,11 @@ SQL
         status => { is => 'Text', },
         sample_id => { is => 'Text', },
         sample_name => { is => 'Text', },
-        #dna_id => { is => 'Text', is_optional => 1, },
-        #creation_pse_id => { is => 'Text', },
         platform_name => { is_constant => 1, calculate => q| return 'Infinium'; |, },
         import_source_name => { is_constant => 1, calculate => q| return 'wugc'; |, },
     ],
     data_source => 'Genome::DataSource::GMSchema',
 };
-
-sub Xgenotype_file { # 2878428288
-    my $self = shift;
-    my $creation_pse = GSC::PSE->get($self->creation_pse_id);
-    return if not $creation_pse;
-    my $transfer_pse = $creation_pse->get_first_active_subsequent_pse_with_process_to('transfer genome studio analysis');
-    return if not $transfer_pse;
-    my $path = $transfer_pse->allocate_disk_space_absolute_path;
-    return if not $path;
-    my $file = "$path/snpreport/".$self->id;
-    return $file if -f $file;
-    return if not $self->dna_id;
-    $file = "$path/snpreport/".$self->dna_id;
-    return $file if -f $file;
-    return;
-}
 
 1;
 
