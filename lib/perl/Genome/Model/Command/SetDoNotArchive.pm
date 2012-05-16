49e8eca2d69f6753a2eb2a1d33db1e7287ca7c5f
Brian Derickson <bdericks@genome.wustl.edu>
2012-05-16 11:25:37 -0500

    updated model commands that set archivable status

--- lib/perl/Genome/Model/Command/SetDoNotArchive.pm
+++ lib/perl/Genome/Model/Command/SetDoNotArchive.pm
@@ -1,33 +1,33 @@
-package Genome::Model::Command::SetUnarchivable;
+package Genome::Model::Command::SetDoNotArchive;
 
 use strict;
 use warnings;
 use Genome;
 
-class Genome::Model::Command::SetUnarchivable {
+class Genome::Model::Command::SetDoNotArchive {
     is => 'Command::V2',
     has => [
         models => {
             is => 'Genome::Model',
             is_many => 1,
             shell_args_position => 1,
-            doc => 'models to have their latest complete build marked as unarchivable',
+            doc => 'models to have their latest complete build marked so they can\'t be archived',
         },
     ],
     has_optional => [
         reason => {
             is => 'Text',
-            doc => 'reason provided models are to be set as unarchivable',
+            doc => 'reason provided models are to be marked',
         },
     ],
 };
 
 sub help_detail {
-    return 'The last complete build for each model is marked as unarchivable. This process ' .
-        'marks all allocations those builds use as unarchivable, including instrument data, ' .
+    return 'The last complete build for each model is marked such that their allocations can\'t be ' .
+        'archived. This process marks all allocations those builds use, including instrument data, ' .
         'alignment results, variation detection results, and other inputs';
 }
-sub help_brief { return 'marks last complete build of each model as unarchivable' };
+sub help_brief { return 'marks last complete build of each model can unable to be archived' };
 sub help_synopsis { return help_brief() . "\n" };
 
 sub execute {
@@ -38,7 +38,7 @@ sub execute {
             $allocation->archivable(0, $self->reason);
         }
     }
-    $self->status_message("Last complete build of provided models marked as unarchivable");
+    $self->status_message("Last complete build of provided models marked so they can't be archived");
     return 1;
 }
 
