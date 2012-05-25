e203b6af8c29d941d3a138c06ca2b7551e53cacb
apregier <aregier@genome.wustl.edu>
2012-05-25 10:32:00 -0500

    Clean up parameters for music model

--- lib/perl/Genome/Model/MutationalSignificance/Command/PlayMusic.pm
+++ lib/perl/Genome/Model/MutationalSignificance/Command/PlayMusic.pm
@@ -455,7 +455,7 @@ sub _append_command_to_workflow {
         next unless exists $property->{is_input} and $property->{is_input};
         my $property_name = $property->property_name;
         my $property_def = $module_input_exceptions{$operation_name}{$property_name};
-        if ($property_def eq $DONT_USE) {
+        if (defined $property_def and $property_def eq $DONT_USE) {
             $property_def = undef;
         }
         if (!$property_def) {
