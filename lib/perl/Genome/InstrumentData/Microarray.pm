5748ca4b9785d983f160100594f47c510986d366
Eddie Belter <ebelter@genome.wustl.edu>
2012-06-01 13:22:32 -0500

    Inst Data Microarray: get snpid mapping - version attr bug; switch to getting fh instead of loading file; split w/ regexp

--- lib/perl/Genome/InstrumentData/Microarray.pm
+++ lib/perl/Genome/InstrumentData/Microarray.pm
@@ -17,17 +17,21 @@ sub get_snpid_hash_for_variant_list {
     my $chip_attribute = $instrument_data->attributes(attribute_label => 'chip_name');
     my $chip = ( $chip_attribute ? lc($chip_attribute->attribute_value) : '' );
     my $version_attribute = $instrument_data->attributes(attribute_label => 'version');
-    my $version = ( $chip_attribute ? lc($chip_attribute->attribute_value) : '' );
+    my $version = ( $chip_attribute ? lc($version_attribute->attribute_value) : '' );
 
     my $allocation = Genome::Disk::Allocation->get(allocation_path => "microarray_data/$platform-$chip-$version");
     return if not $allocation;
 
-    my $snp_map_path = $allocation->absolute_path . 'mapping.tsv';
+    my $snp_map_path = $allocation->absolute_path . '/mapping.tsv';
+    Carp::confess('No snp id mapping file in '.$allocation->absolute_path) if not -s $snp_map_path;
+
+    my $fh = eval{ Genome::Sys->open_file_for_reading($snp_map_path); };
+    Carp::confess('Failed to open snp id mapping file! '.$snp_map_path) if not $fh;
+
     my $old_to_new_snpid_map;
-    my @mapping = Genome::Sys->read_file($snp_map_path);
-    for my $mapping (@mapping){
+    while ( my $mapping = $fh->getline ) {
         chomp $mapping;
-        my ($old_id, $new_id) = split "\t", $mapping;
+        my ($old_id, $new_id) = split /\t/, $mapping;
         $old_to_new_snpid_map->{$old_id} = $new_id;
     }
     return $old_to_new_snpid_map;
