ea6c8cd598d34c3ba909ba59cd7a0640d111db30
Eddie Belter <ebelter@genome.wustl.edu>
2012-06-01 12:39:44 -0500

    Microarray: update snpid hash method to get chip and version

--- lib/perl/Genome/InstrumentData/Microarray.pm
+++ lib/perl/Genome/InstrumentData/Microarray.pm
@@ -11,13 +11,18 @@ class Genome::InstrumentData::Microarray {
 sub get_snpid_hash_for_variant_list {
     my $class = shift;
     my $instrument_data = shift || die 'No instrument data given';
-    my $chip = shift;
-    my $version = shift;
+    my $variation_list_build = shift || die 'No variation list build';
 
-    my $platform = $instrument_data->sequencing_platform;
-    my $allocation = Genome::Disk::Allocation->get(allocation_path => "microarray_data/$platform-$chip-$version") || die "No mapping available for $platform-$chip-$version";
+    my $platform = lc($instrument_data->sequencing_platform);
+    my $chip_attribute = $instrument_data->attributes(attribute_label => 'chip_name');
+    my $chip = ( $chip_attribute ? $chip_attribute->attribute_value : '' );
+    my $version_attribute = $instrument_data->attributes(attribute_label => 'version');
+    my $version = ( $chip_attribute ? $chip_attribute->attribute_value : '' );
+
+    my $allocation = Genome::Disk::Allocation->get(allocation_path => "microarray_data/$platform-$chip-$version");
+    return if not $allocation;
+
     my $snp_map_path = $allocation->absolute_path . 'mapping.tsv';
-
     my $old_to_new_snpid_map;
     my @mapping = Genome::Sys->read_file($snp_map_path);
     for my $mapping (@mapping){
